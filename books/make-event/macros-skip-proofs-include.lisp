490b57e277629893ceb7b3274018a7d794b5e026
Matthew J Kaufmann <kaufmann@cs.utexas.edu>
2008-06-19 22:40:41 +0000

    Temporarily modified for compatibility with development version of ACL2 while remaining OK for v3-3 (will change after v3-4 is released).

--- books/make-event/macros-skip-proofs-include.lisp
+++ books/make-event/macros-skip-proofs-include.lisp
@@ -16,7 +16,9 @@
      ((LOCAL-FN (X) T))
      (LOCAL (DEFUN LOCAL-FN (X) X))
      (RECORD-EXPANSION (LOCAL (MAKE-EVENT '(DEFUN FOO1 (X) X)))
-                       (LOCAL (DEFUN FOO1 (X) X)))
+; Change for v3-4:
+;                      (LOCAL (DEFUN FOO1 (X) X)))
+                       (LOCAL (value-triple :elided)))
      (RECORD-EXPANSION
       (MY-MAC (SKIP-PROOFS (MAKE-EVENT '(DEFUN FOO2 (X) X)
                                        :CHECK-EXPANSION T)))
@@ -40,8 +42,12 @@
 
 (include-book "misc/file-io" :dir :system)
 
+; Uncomment the following when updating to v3-4, and also perhaps remove
+; comment above v3-4 above.
+#||
 (must-succeed
  (er-let* ((forms (read-list "macros-skip-proofs.cert" 'top state)))
           (let ((erp (not (equal (cadr (member-eq :expansion-alist forms))
                                  *macros-expansion-alist*))))
             (mv erp nil state))))
+||#
