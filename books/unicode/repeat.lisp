d75a6d4e3cdcfe0043b52067423efa41da6f6eb2
Jared C Davis <jared.c.davis@gmail.com>
2008-11-12 20:50:33 +0000

    verify guards on repeat

--- books/unicode/repeat.lisp
+++ books/unicode/repeat.lisp
@@ -26,6 +26,16 @@
                 (cons x (repeat x (- n 1))))
        :exec (make-list-ac n x nil)))
 
+(defthm |(repeat x 0)|
+  (equal (repeat x 0)
+         nil)
+  :hints(("Goal" :in-theory (enable repeat))))
+
+(defthm repeat-under-iff
+  (iff (repeat x n)
+       (not (zp n)))
+  :hints(("Goal" :in-theory (enable repeat))))
+
 (defthm simpler-take-when-not-consp
   (implies (not (consp x))
            (equal (simpler-take n x)
@@ -36,3 +46,35 @@
   (equal (len (repeat x n))
          (nfix n))
   :hints(("Goal" :in-theory (enable repeat))))
+
+(defthm repeat-of-nfix
+  (equal (repeat x (nfix n))
+         (repeat x n))
+  :hints(("Goal" :in-theory (enable repeat))))
+
+(encapsulate
+ ()
+ (local (include-book "append"))
+
+ (local (defun silly-repeat (x n acc)
+          (if (zp n)
+              acc
+            (cons x (silly-repeat x (- n 1) acc)))))
+
+ (local (defthm lemma1
+          (equal (make-list-ac n x acc)
+                 (silly-repeat x n acc))))
+
+ (local (defthm lemma2
+          (equal (silly-repeat x n acc)
+                 (append (repeat x n) acc))
+          :hints(("Goal"
+                  :in-theory (enable repeat)))))
+
+ (local (defthm main-lemma
+          (equal (make-list-ac n x acc)
+                 (append (repeat x n) 
+                         acc))))
+ 
+ (verify-guards repeat
+                :hints(("Goal" :in-theory (enable repeat)))))
