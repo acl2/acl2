9d965837b5f5f239284fd5b1e2e9c31b6c6d9896
Sol Swords <sswords@gmail.com>
2010-06-19 05:16:39 +0000

    improved include-raw, fix unsound-read to work with last mod to serialize

--- books/branches/new-hons/tools/include-raw.lisp
+++ books/branches/new-hons/tools/include-raw.lisp
@@ -25,7 +25,6 @@
 
 (progn!
  (set-raw-mode t)
- 
  (defun raw-compile (name state)
    (compile-file
     (extend-pathname (cbd) name state))
@@ -56,34 +55,47 @@ include-raw form.
 
 The raw Lisp file will be compiled and loaded when the containing book is
 certified.  When including the book, the compiled file will be loaded if
-possible, otherwise the original file will be loaded instead. ~/~/"
-  `(make-event
-    (mv-let (erp val state)
-      ;; This progn!, including the compilation of the file to be loaded,
-      ;; will only happen during make-event expansion; that is, during
-      ;; certification, top-level evaluation, or uncertified inclusion (any
-      ;; other situations?)  Is this correct behavior?
-      ;;  - It is intentional that this _does_ happen during
-      ;; certification, which seems like a good time to compile files
-      ;; associated with the book being certified; in particular, hopefully
-      ;; the same book is not being certified multiple times simultaneously.
-      ;;  - It is intentional that this does _not_ happen during
-      ;; certified inclusion, because this may interfere with parallel
-      ;; certification: certifications of several files may simultaneously
-      ;; include the containing book and try to compile the file, stomping
-      ;; over each other's output.  Furthermore, if the book is certified,
-      ;; then the compiled file should already exist.
-      ;;  - In top-level evaluation/uncertified inclusion, it seems like a
-      ;; toss-up.  We prefer to perform the compilation to ensure the
-      ;; compiled file exists, so that compiled code is loaded and
-      ;; subsequent performance (in Lisps that don't compile automatically)
-      ;; is similar to that obtained by loading the certified book.
-      (progn!
-       (set-raw-mode t)
-       (raw-compile ,fname state))
-      (declare (ignore erp val))
-      (value '(progn!
-               (set-raw-mode t)
-               (raw-load ,fname state)
-               (value-triple ,fname))))))
+possible, otherwise the original file will be loaded instead.
+
+One further note:  In most or all Lisps, compiling foo.lisp and foo.lsp results
+in the same compiled file (named foo.fasl, or something similar depending on
+the Lisp.)  Therefore, it is a mistake to use the same base name for a raw Lisp
+file with .lsp extension and an ACL2 book with .lisp extension, at least when
+using this tool and depending on compilation.~/~/"
+  `(progn
+     (make-event
+      (mv-let (erp val state)
+        ;; This progn!, including the compilation of the file to be loaded,
+        ;; will only happen during make-event expansion; that is, during
+        ;; certification, top-level evaluation, or uncertified inclusion (any
+        ;; other situations?)  Is this correct behavior?
+        ;;  - It is intentional that this _does_ happen during
+        ;; certification, which seems like a good time to compile files
+        ;; associated with the book being certified; in particular, hopefully
+        ;; the same book is not being certified multiple times simultaneously.
+        ;;  - It is intentional that this does _not_ happen during
+        ;; certified inclusion, because this may interfere with parallel
+        ;; certification: certifications of several files may simultaneously
+        ;; include the containing book and try to compile the file, stomping
+        ;; over each other's output.  Furthermore, if the book is certified,
+        ;; then the compiled file should already exist.
+        ;;  - In top-level evaluation/uncertified inclusion, it seems like a
+        ;; toss-up.  We prefer to perform the compilation to ensure the
+        ;; compiled file exists, so that compiled code is loaded and
+        ;; subsequent performance (in Lisps that don't compile automatically)
+        ;; is similar to that obtained by loading the certified book.
+        (progn!
+         (set-raw-mode t)
+         (raw-compile ,fname state))
+        (declare (ignore erp val))
+        (value '(value-triple :invisible))))
+     (progn!
+      (set-raw-mode t)
+      ;; According to Matt, *hcomp-fn-restore-ht* is nonnil only when loading
+      ;; a book's compiled file.  We want to wait until the events of the
+      ;; include-book are being processed to run this, so that our compiled
+      ;; file isn't loaded twice.
+      (when (null *hcomp-fn-restore-ht*)
+        (raw-load ,fname state))
+      (value-triple ,fname))))
 
