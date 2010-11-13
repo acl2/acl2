8673e1064d2a8f624ee6c74d9ecee53d3ebf0e2f
Matthew J Kaufmann <kaufmann@cs.utexas.edu>
2010-11-13 15:27:01 +0000

    Modifications of ordinal books from Pete Manolios

--- books/ordinals/lexicographic-ordering-without-arithmetic.lisp
+++ books/ordinals/lexicographic-ordering-without-arithmetic.lisp
@@ -1,7 +1,6 @@
 ; 2010-09-16 Pete Manolios: Created two versions of our
 ; lexicogrphic-ordering book: one includes
-; arithmetic/top-with-meta (this one) and one doesn't. 
+; arithmetic/top-with-meta and one doesn't (this one).
 
 (in-package "ACL2")
-(include-book "top-with-meta")
 (include-book "lexicographic-book")
