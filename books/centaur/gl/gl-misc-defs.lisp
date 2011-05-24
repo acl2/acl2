0d6da09c035604c903f48152e2b109f1eab09b77
Jared C Davis <jared.c.davis@gmail.com>
2011-05-24 15:36:24 +0000

    GL tweak to cheapen symbolic simulations of (expt 2 n).

--- books/centaur/gl/gl-misc-defs.lisp
+++ books/centaur/gl/gl-misc-defs.lisp
@@ -35,3 +35,41 @@
     :rule-classes :definition))
 
 (table gl::preferred-defs 'evenp 'evenp-is-logbitp)
+
+
+
+
+; Below we tweak the definition that GL uses for EXPT so that (EXPT 2 N) is
+; treated as a shift instead, where N is a natural number.  This might be a bad
+; idea when the base is a variable that is sometimes 2, since it will lead us
+; to consider both the ASH and regular EXPT-style definitions.  But it seems
+; nice for hardware models where (EXPT 2 N) may be the most frequent use of
+; EXPT.
+
+(defund expt-fallback (r i)
+  (declare (xargs :guard (and (acl2-numberp r)
+                              (integerp i)
+                              (not (and (eql r 0) (< i 0))))
+                  :measure (abs (ifix i))))
+  (cond ((zip i) 1)
+        ((= (fix r) 0) 0)
+        ((> i 0) (* r (expt-fallback r (+ i -1))))
+        (t (* (/ r) (expt-fallback r (+ i 1))))))
+
+(defthm expt-fallback-is-expt
+  (equal (expt-fallback r i)
+         (expt r i))
+  :hints(("Goal" :in-theory (enable expt-fallback expt))))
+
+(encapsulate
+  ()
+  (local (include-book "arithmetic/top-with-meta" :dir :system))
+
+  (defthmd optimize-expt-2-for-gl
+    (equal (expt r i)
+           (if (and (equal r 2)
+                    (natp i))
+               (ash 1 i)
+             (expt-fallback r i)))))
+
+(table gl::preferred-defs 'expt 'optimize-expt-2-for-gl)
