378024734e6100f52ea6ae4e54e53cd0a1b6b2e8
Matthew J Kaufmann <kaufmann@cs.utexas.edu>
2012-02-02 19:43:09 +0000

    Modified to cause error until I install patch from Sol and Jared for pcert bug.

--- books/system/pcert/sub.lisp
+++ books/system/pcert/sub.lisp
@@ -12,7 +12,15 @@
  ()
  (local (make-event (list 'defun 'g3 '(x) 'x)))
  (defun g4 (x) x)
- (make-event (list 'defun 'g5 '(x) 'x)))
+ (make-event (er-progn
+; We should not be seeing this make-event when another book includes sub, since
+; in that case sub.acl2 will contain the expansion.  For that matter, we
+; shouldn't be doing make-event expansion when including this book, either.
+              (cond ((global-val 'include-book-path (w state))
+                     (er soft 'testing-pcert
+                         "Unexpected error; see books/system/pcert/sub.lisp."))
+                    (t (value nil)))
+              (value (list 'defun 'g5 '(x) 'x)))))
 
 (defun h (x)
   (if (consp x) (h (cddr x)) x))
