437707c24c5baa6944f88c1484f41d360abe3305
Matthew J Kaufmann <kaufmann@cs.utexas.edu>
2012-03-26 13:29:44 +0000

    Support for new :pcert t argument, for Pcertify+ procedure of provisional certification.

--- books/system/pcert/acl2x-pcert-test-2-include.lisp
+++ books/system/pcert/acl2x-pcert-test-2-include.lisp
@@ -6,7 +6,7 @@
 (must-eval-to-t
  (er-let* ((forms (read-file "acl2x-pcert-test-2.pcert0" state)))
    (value
-    (equal (cadr (member-eq :expansion-alist-nonelided forms))
+    (equal (cadr (member-eq :pcert-info forms))
            '((2 RECORD-EXPANSION
                 (ENCAPSULATE NIL
                              (LOCAL (MAKE-EVENT '(DEFUN G (X) X)))
