6641d6f1abe45af8eff52f0208381510aacd5368
Matthew J Kaufmann <kaufmann@cs.utexas.edu>
2012-04-11 03:36:04 +0000

    Update for recent mod to development copy of the sources.

--- books/hints/consider-hint.lisp
+++ books/hints/consider-hint.lisp
@@ -145,12 +145,14 @@
   (let ((lst (unprettyify term))) ; pair = (hyps . concl)
     (cond
      ((equal (length lst) 1)
-      (mv-let (equiv lhs rhs ttree)
+      (mv-let (msg equiv lhs rhs ttree)
               (interpret-term-as-rewrite-rule
+               'some-name
+               nil ; hyps
                (cdr (car lst))
                (ens state)
                wrld)
-              (declare (ignore equiv rhs ttree))
+              (declare (ignore msg equiv rhs ttree))
               (value lhs)))
      (t (er soft ctx
             "We cannot determine the :lhs of a formula because it ~
