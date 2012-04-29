f0aec770e56e313e08024123d0060a47a24acfff
Matthew J Kaufmann <kaufmann@cs.utexas.edu>
2012-04-29 16:59:47 +0000

    Eliminated character-encoding-test.acl2 because it seems impossible to control the external-format for standard-input for all Lisps, as now explained in character-encoding-test.lisp.

--- books/misc/character-encoding-test.lisp
+++ books/misc/character-encoding-test.lisp
@@ -1,3 +1,25 @@
+; This program is free software; you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation; either version 2 of the License, or
+; (at your option) any later version.
+
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+
+; You should have received a copy of the GNU General Public License
+; along with this program; if not, write to the Free Software
+; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+; Written by:  Matt Kaufmann
+; email:       Kaufmann@cs.utexas.edu
+; Department of Computer Science
+; University of Texas at Austin
+; Austin, TX 78701 U.S.A.
+
+; April, 2012
+
 (in-package "ACL2")
 
 ; This file has been saved using the utf-8 character encoding.  As a result,
@@ -6,9 +28,10 @@
 ; constant is 1, then this file was probably saved with a different encoding,
 ; quite possibly iso-8859-1.
 
-; WARNING: Before saving this file in emacs, evaluate the following form,
-; especially if you have loaded distributed file emacs/emacs-acl2.el into your
-; emacs session, since that file sets the character encoding to iso-8859-1.
+; WARNING: Before saving this file in emacs, evaluate the following form to set
+; the indicated buffer-local variable, especially if you have loaded
+; distributed file emacs/emacs-acl2.el into your emacs session, since that file
+; sets the character encoding to iso-8859-1.
 
 ; (setq save-buffer-coding-system 'utf-8)
 
@@ -16,9 +39,14 @@
 ;   cat character-encoding-test.lisp
 ; may show clearly that the string below has a single character.
 
-(defthm len-test-1
-  (equal (length (test-1)) 2)
-  :rule-classes nil)
+; We have considered defining a function test-1 in file
+; character-encoding-test.acl2, with the same body as test-2 below.  However,
+; in SBCL 1.0.49 we found that our attempt to set the external format to
+; iso-8859-1 (see function acl2-set-character-encoding in the ACL2 sources) did
+; not affect reading directly from standard input.  Since .acl2 files are
+; indeed read that way using the standard ACL2 makefile approach, SBCL was
+; reading that .acl2 file in utf-8 format.  So we have abandoned the use of a
+; .acl2 file and restricted ourselves just to the file-based test below.
 
 (defun test-2 () "ó")
 
