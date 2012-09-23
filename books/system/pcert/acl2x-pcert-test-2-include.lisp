f854cdaa0846a31e6c504440a6c04a8d9a452177
Matthew J Kaufmann <kaufmann@cs.utexas.edu>
2012-09-23 16:43:21 +0000

    Allow turning off of provisional certification in system/pcert/.

--- books/system/pcert/acl2x-pcert-test-2-include.lisp
+++ books/system/pcert/acl2x-pcert-test-2-include.lisp
@@ -4,14 +4,19 @@
 (include-book "misc/eval" :dir :system)
 
 (must-eval-to-t
- (er-let* ((forms (read-file "acl2x-pcert-test-2.pcert0" state)))
-   (value
-    (equal (cadr (member-eq :pcert-info forms))
-           '((2 RECORD-EXPANSION
-                (ENCAPSULATE NIL
-                             (LOCAL (MAKE-EVENT '(DEFUN G (X) X)))
-                             (DEFUN H (X) X))
-                (ENCAPSULATE NIL
-                             (RECORD-EXPANSION (LOCAL (MAKE-EVENT '(DEFUN G (X) X)))
-                                               (LOCAL (DEFUN G (X) X)))
-                             (DEFUN H (X) X))))))))
+ (er-let*
+  ((acl2-pcert (getenv$ "ACL2_PCERT" state)))
+  (if (member-equal acl2-pcert '("" nil))
+      (value t)
+    (er-let*
+     ((forms (read-file "acl2x-pcert-test-2.pcert0" state)))
+     (value
+      (equal (cadr (member-eq :pcert-info forms))
+             '((2 RECORD-EXPANSION
+                  (ENCAPSULATE NIL
+                               (LOCAL (MAKE-EVENT '(DEFUN G (X) X)))
+                               (DEFUN H (X) X))
+                  (ENCAPSULATE NIL
+                               (RECORD-EXPANSION (LOCAL (MAKE-EVENT '(DEFUN G (X) X)))
+                                                 (LOCAL (DEFUN G (X) X)))
+                               (DEFUN H (X) X))))))))))
