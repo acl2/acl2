c7d91ff7f47c7a799052779764c4b83628add4bf
David L Rager <ragerdl@gmail.com>
2012-10-08 18:06:34 +0000

    Adding some example uses of defaggregate

---
+++ books/cutil/defaggregate-tests.lisp
@@ -0,0 +1,37 @@
+; Tests for defaggregate utility.  Consider moving tests from the bottom of
+; defaggregate.lisp into this file.
+
+(in-package "CUTIL")
+
+(include-book "defaggregate")
+
+(encapsulate 
+ ()
+ 
+ (defn foof-p (x)
+   (keywordp x))
+ 
+ (defmacro foom-p (x)
+   (keywordp x))
+ 
+ (defaggregate containerf
+   (thing)
+   :require ((foof-p-of-containerf->thing
+              (foof-p thing)))
+   :tag :containerf)
+ 
+; The following defaggregate call is commented out because using a macro, as is
+; done in the following example, results in a rewrite rule that is
+; unacceptable.  Here is the error:
+
+;;; ACL2 Error in ( DEFTHM FOOM-P-OF-CONTAINERM->THING ...):  A :REWRITE
+;;; rule generated from FOOM-P-OF-CONTAINERM->THING is illegal because
+;;; it rewrites the quoted constant 'NIL.  See :DOC rewrite.
+ 
+ ;; (defaggregate containerm
+ ;;   (thing)
+ ;;   :require ((foom-p-of-containerm->thing
+ ;;              (foom-p thing)))
+ ;;   :tag :containerm)
+ 
+ ) ; encapsulate
