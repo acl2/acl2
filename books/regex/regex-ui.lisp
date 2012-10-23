b50b682a65bb6db91d903dd07777a5b1ffdfed07
Jared C Davis <jared.c.davis@gmail.com>
2012-10-23 18:49:31 +0000

    Better theorem support for using the regex matching b* binder

--- books/regex/regex-ui.lisp
+++ books/regex/regex-ui.lisp
@@ -22,7 +22,8 @@
 (include-book "regex-parse")
 (include-book "regex-exec")
 (include-book "str/case-conversion" :dir :system)
-
+(local (include-book "tools/mv-nth" :dir :system))
+(local (include-book "misc/assert" :dir :system))
 
 (local (in-theory (enable length-equiv length)))
 
@@ -30,7 +31,9 @@
   (declare (xargs :guard (and (regex-p regex)
                               (stringp str)
                               (parse-opts-p opts))))
-  (b* ((transstr (if (parse-options-case-insensitive opts)
+  (b* ((str (mbe :logic (if (stringp str) str "")
+                 :exec str))
+       (transstr (if (parse-options-case-insensitive opts)
                      (str::downcase-string str)
                    str))
        ((mv ?matchp whole substrs)
@@ -43,7 +46,9 @@
   (declare (xargs :guard (and (stringp pat)
                               (stringp str)
                               (parse-opts-p opts))))
-  (b* ((pat (if (parse-options-case-insensitive opts)
+  (b* ((str (mbe :logic (if (stringp str) str "") :exec str))
+       (pat (mbe :logic (if (stringp pat) pat "") :exec pat))
+       (pat (if (parse-options-case-insensitive opts)
                 (str::downcase-string pat)
               pat))
        (regex (regex-do-parse pat opts))
@@ -52,6 +57,124 @@
        ((mv whole substrs)
         (do-regex-match-precomp str regex opts)))
     (mv nil whole substrs)))
+
+
+;; We'll leave the above functions disabled, but prove some basic type lemmas
+;; about them.
+
+(local (defthm l0
+         (implies (not (mv-nth 0 (match-regex-fun regex str untrans-str n)))
+                  (not (mv-nth 1 (match-regex-fun regex str untrans-str n))))
+         :hints(("Goal" :in-theory (enable match-regex-fun)))))
+
+(local (defthm l1
+         (implies (and (stringp str)
+                       (stringp untrans-str))
+                  (equal (stringp (mv-nth 1 (match-regex-fun regex str untrans-str n)))
+                         (if (mv-nth 0 (match-regex-fun regex str untrans-str n))
+                             t
+                           nil)))
+         :hints(("Goal" :in-theory (enable match-regex-fun mv-nth)))))
+
+(local (defthm l2
+         (implies (and (stringp str)
+                       (stringp untrans-str))
+                  (or (stringp (mv-nth 1 (match-regex-fun regex str untrans-str n)))
+                      (not (mv-nth 1 (match-regex-fun regex str untrans-str n)))))
+         :rule-classes :type-prescription))
+
+(local (defthm true-listp-of-match-regex-at-char
+         (true-listp (mv-nth 2 (match-regex-at-char regex str untrans-str n)))
+         :rule-classes :type-prescription
+         :hints(("Goal" :in-theory (enable match-regex-at-char)))))
+
+(local (defthm true-listp-of-match-regex-string
+         (true-listp (mv-nth 2 (match-regex-fun regex str untrans-str n)))
+         :rule-classes :type-prescription
+         :hints(("Goal" :in-theory (enable match-regex-fun)))))
+
+
+
+(defthm type-of-do-regex-match-precomp-0
+  ;; Full match is a string or nil.
+  (or (stringp (mv-nth 0 (do-regex-match-precomp str regex opts)))
+      (not (mv-nth 0 (do-regex-match-precomp str regex opts))))
+  :rule-classes :type-prescription)
+
+(defthm type-of-do-regex-match-precomp-1
+  ;; Substrings are a true-list (for guards of Nth)
+  (true-listp (mv-nth 1 (do-regex-match-precomp str regex opts)))
+  :rule-classes :type-prescription)
+
+
+(defthm type-of-do-regex-match-0
+  ;; Error message is a string or nil.
+  (or (stringp (mv-nth 0 (do-regex-match str regex opts)))
+      (not (mv-nth 0 (do-regex-match str regex opts))))
+  :rule-classes :type-prescription)
+
+(defthm type-of-do-regex-match-1
+  ;; Lemma to show :full is a string (or nil) in the dynamically compiled case.
+  (or (stringp (mv-nth 1 (do-regex-match str regex opts)))
+      (not (mv-nth 1 (do-regex-match str regex opts))))
+  :rule-classes :type-prescription)
+
+(defthm type-of-do-regex-match-2
+  ;; Substrings are a true-list (for guards of Nth)
+  (true-listp (mv-nth 2 (do-regex-match str regex opts)))
+  :rule-classes :type-prescription)
+
+
+
+;; We also want to know that the substrings are strings or NIL.  Since we're
+;; going to lay down Nths bindings, I'll write these in terms of Nth.
+
+(local (defthm c0
+         (implies (string-or-null-listp x)
+                  (equal (stringp (nth n x))
+                         (if (nth n x)
+                             t
+                           nil)))))
+
+(local (defthm c1
+         (implies (string-or-null-listp x)
+                  (equal (stringp (car x))
+                         (if (car x)
+                             t
+                           nil)))))
+
+(defthm type-of-nth-of-do-regex-match-precomp-substrings
+  ;; Lemma for the precompiled case
+  (or (stringp (nth n (mv-nth 1 (do-regex-match-precomp str regex opts))))
+      (not (nth n (mv-nth 1 (do-regex-match-precomp str regex opts)))))
+  :rule-classes :type-prescription)
+
+(defthm type-of-car-do-regex-match-precomp-substrings
+  ;; We prove the CAR lemma also, since if case Nth is enabled then
+  ;; (nth 0 substrings) can turn into (car substrings) before our lemma
+  ;; about nth matches.
+  (or (stringp (car (mv-nth 1 (do-regex-match-precomp str regex opts))))
+      (not (car (mv-nth 1 (do-regex-match-precomp str regex opts)))))
+  :rule-classes :type-prescription)
+
+
+(defthm type-of-nth-of-do-regex-match-substrings
+  ;; Lemma for the dynamically compiled case
+  (or (stringp (nth n (mv-nth 2 (do-regex-match str regex opts))))
+      (not (nth n (mv-nth 2 (do-regex-match str regex opts)))))
+  :rule-classes :type-prescription)
+
+(defthm type-of-car-of-do-regex-match-substrings
+  ;; Lemma for the dynamically compiled case
+  (or (stringp (car (mv-nth 2 (do-regex-match str regex opts))))
+      (not (car (mv-nth 2 (do-regex-match str regex opts)))))
+  :rule-classes :type-prescription)
+
+
+;; That *should* be enough for guards, as long as you're not trying to prove
+;; something about the actual strings you're matching.  But that'd be crazy,
+;; right?
+(in-theory (disable do-regex-match do-regex-match-precomp))
 
 
 (def-b*-binder match
@@ -102,51 +225,89 @@
        ,rest-expr)))
 
 ;; Examples
-(make-event
- (b* ((res1-ok
-       (equal
+(local
+ (make-event
+  (b* ((res1-ok
+        (equal
         
-        (b* (((match "ab([def]*)\\1([gh])" :i
-                     :full f
-                     :substrs (a b))
-              "cdeAbfdEfDeghIj"))
-          (list a b f))
-
-        '("fdE" "g" "AbfdEfDeg")))
-      (res2-ok
-       (equal
-
-        (b* ((pattern "ab([def]*)\\1([gh])")
-             (string "cdeAbfdEfDeghIj")
-             ((match  pattern :i
+         (b* (((match "ab([def]*)\\1([gh])" :i
                       :full f
-                      :substrs (a b)
-                      :error-msg e)
-              string))
-          (list e a b f))
+                      :substrs (a b))
+               "cdeAbfdEfDeghIj"))
+           (list a b f))
 
-        '(NIL "fdE" "g" "AbfdEfDeg")))
+         '("fdE" "g" "AbfdEfDeg")))
+       (res2-ok
+        (equal
 
-      (res3-ok
-       (equal
+         (b* ((pattern "ab([def]*)\\1([gh])")
+              (string "cdeAbfdEfDeghIj")
+              ((match  pattern :i
+                       :full f
+                       :substrs (a b)
+                       :error-msg e)
+               string))
+           (list e a b f))
 
-        ;; using recursive binders
-        (b* ((pattern "ab([def]*)\\1([gh])")
-             (string "cdeAbfdEfDeghIj")
-             ((match  pattern :i
-                      :full ?f  ;; ignorable
-                      :substrs ((the string a) ;; type
-                                &) ;; not bound
-                      ;; error msg not present
-                      )
-              string))
-          (list a f))
+         '(NIL "fdE" "g" "AbfdEfDeg")))
 
-        '("fdE" "AbfdEfDeg"))))
+       (res3-ok
+        (equal
+
+         ;; using recursive binders
+         (b* ((pattern "ab([def]*)\\1([gh])")
+              (string "cdeAbfdEfDeghIj")
+              ((match  pattern :i
+                       :full ?f                ;; ignorable
+                       :substrs ((the string a) ;; type
+                                 &)             ;; not bound
+                       ;; error msg not present
+                       )
+               string))
+           (list a f))
+
+         '("fdE" "AbfdEfDeg"))))
 
       
-   (if (and res1-ok res2-ok res3-ok)
-       '(value-triple :ok)
-     (er hard? 'regex-ui
-         "One of the tests failed~%"))))
+    (if (and res1-ok res2-ok res3-ok)
+        '(value-triple :ok)
+      (er hard? 'regex-ui
+          "One of the tests failed~%")))))
+
+
+
+;; some kind of test to make sure guards verify and we know these variables
+;; are strings or NIL.
+
+(local (defun f (x)
+         (declare (type string x))
+         (b* (((match "([A-Z]+)_([0-9]+)"
+                      :full f
+                      :substrs (word num)) x))
+           (list f word num))))
+
+(local (defthm first-of-f
+         (or (stringp (car (f x)))
+             (not (car (f x))))
+         :rule-classes :type-prescription))
+
+(local (defthm second-of-f
+         (or (stringp (second (f x)))
+             (not (second (f x))))
+         :rule-classes :type-prescription))
+
+(local (defthm third-of-f
+         (or (stringp (third (f x)))
+             (not (third (f x))))
+         :rule-classes :type-prescription))
+
+(local (assert! (equal (f "FOO_37") (list "FOO_37" "FOO" "37"))))
+(local (assert! (equal (f "FOO_37_abc") (list "FOO_37" "FOO" "37"))))
+(local (assert! (equal (f "abc_FOO_37_abc") (list "FOO_37" "FOO" "37"))))
+
+
+
+
+
+
 
