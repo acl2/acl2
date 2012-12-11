c756c3e1fd8fdee08a14702139b229fc1fac876f
Jared C Davis <jared.c.davis@gmail.com>
2012-12-11 16:55:20 +0000

    Add some documentation to rulesets

--- books/tools/rulesets.lisp
+++ books/tools/rulesets.lisp
@@ -1,9 +1,55 @@
-
+; Rulesets -- Extensible alternative to theories
+; Copyright (C) 2008-2012 Centaur Technology
+;
+; Contact:
+;   Centaur Technology Formal Verification Group
+;   7600-C N. Capital of Texas Highway, Suite 300, Austin, TX 78731, USA.
+;   http://www.centtech.com/
+;
+; This program is free software; you can redistribute it and/or modify it under
+; the terms of the GNU General Public License as published by the Free Software
+; Foundation; either version 2 of the License, or (at your option) any later
+; version.  This program is distributed in the hope that it will be useful but
+; WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+; more details.  You should have received a copy of the GNU General Public
+; License along with this program; if not, write to the Free Software
+; Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA 02110-1335, USA.
+;
+; Original authors: Sol Swords and Jared Davis
+;                   {sswords,jared}@centtech.com
 
 (in-package "ACL2")
-
+(include-book "xdoc/top" :dir :system)
 (program)
 
+(defxdoc rulesets
+  :parents (theories deftheory)
+  :short "Table-based alternative to ACL2's named theories."
+
+  :long "<p>Rulesets are like @(see theories), but can be extended with
+additional rules after first being defined.  That is, you can build up a
+ruleset incrementally, across many books, instead of having to define it all at
+once and having it be forever fixed.</p>
+
+<p>Basic usage of rulesets is just like theories.  You can:</p>
+
+<ul>
+<li>Introduce rulesets with @(see def-ruleset)</li>
+<li>Extend existing rulesets with @(see add-to-ruleset).</li>
+<li>Enable/disable rulesets with @(see enable*), @(see disable*), and @(see
+e/d*)</li>
+</ul>
+
+<p>When we define a new package @('FOO'), we often set up @('FOO::enable') as
+an alias for @('enable*'), to make using rulesets more convenient.</p>
+
+<p>Advanced users can do some nifty things with rulesets, e.g., you can have a
+superior ruleset that contains other rulesets, and it will grow as you add
+rules to the contained rulesets.</p>")
+
+; BOZO this documentation is incomplete and doesn't really explain ruleset
+; designators and fancy things like rules-of-class.  Oh well, maybe someday.
 
 (defun get-ruleset (name world)
   (let* ((ruleset-alist (table-alist 'ruleset-table world)))
@@ -93,44 +139,83 @@
     (er soft 'add-to-ruleset
         "~x0 is not already a ruleset.  Use def-ruleset, def-ruleset! ~
              or add-to-ruleset! instead.~%" name)))
-  
+
 (defun ruleset-form-preprocess (form)
   (if (and (symbolp form)
            (not (booleanp form)))
       `'(,form)
     form))
 
+(defsection def-ruleset
+  :parents (rulesets)
+  :short "@(call def-ruleset) creates a new ruleset."
 
-(defmacro def-ruleset (name form)
-  (declare (xargs :guard (symbolp name)))
-  `(make-event
-    (let ((world (w state))
-          (name ',name))
-      (er-progn
-       (check-not-ruleset name world state)
-       (let ((rules ,(ruleset-form-preprocess form)))
-         (def-ruleset-core 
-           name rules world state))))))
+  :long "<p>Examples:</p>
+@({
+ (def-ruleset my-rules
+   '(append reverse))
 
-(defmacro add-to-ruleset (name form)
-  (declare (xargs :guard (symbolp name)))
-  `(make-event
-    (let ((world (w state))
-          (name ',name))
-      (er-progn
-       (check-ruleset name world state)
-       (let ((rules ,(ruleset-form-preprocess form)))
-         (add-to-ruleset-core name rules world state))))))
+ (def-ruleset other-rules
+   '(member-equal my-rules revappend))
+})
 
-(defmacro def-ruleset! (name form)
-  (declare (xargs :guard (symbolp name)))
-  `(make-event
-    (let* ((world (w state))
-           (name ',name)
-           (rules ,(ruleset-form-preprocess form)))
-      (if (is-ruleset name world)
-          (add-to-ruleset-core name rules world state)
-        (def-ruleset-core name rules world state)))))
+<p>The first example creates a new ruleset, @('my-rules'), with only the
+definitions of @('append') and @('reverse').</p>
+
+<p>The section example creates a new ruleset, @('other-rules'), with the
+definitions of @('member-equal') and @('revappend'), and also a link to
+@('my-rules').  When rules are added to @('my-rules'), @('other-rules')
+also grows.</p>"
+
+  (defmacro def-ruleset (name form)
+    (declare (xargs :guard (symbolp name)))
+    `(make-event
+      (let ((world (w state))
+            (name ',name))
+        (er-progn
+         (check-not-ruleset name world state)
+         (let ((rules ,(ruleset-form-preprocess form)))
+           (def-ruleset-core
+             name rules world state)))))))
+
+(defsection add-to-ruleset
+  :parents (rulesets)
+  :short "@(call add-to-ruleset) adds additional rules to an existing
+ruleset."
+
+  :long "<p>Examples:</p>
+@({
+ (add-to-ruleset my-rules
+   '(foop))
+
+ (add-to-ruleset other-rules
+   '(car-cons cdr-cons (force)))
+})"
+
+  (defmacro add-to-ruleset (name form)
+    (declare (xargs :guard (symbolp name)))
+    `(make-event
+      (let ((world (w state))
+            (name ',name))
+        (er-progn
+         (check-ruleset name world state)
+         (let ((rules ,(ruleset-form-preprocess form)))
+           (add-to-ruleset-core name rules world state)))))))
+
+(defsection def-ruleset!
+  :parents (rulesets)
+  :short "Same as @(see def-ruleset) except that it overwrites any already
+existing ruleset instead of causing an error."
+
+  (defmacro def-ruleset! (name form)
+    (declare (xargs :guard (symbolp name)))
+    `(make-event
+      (let* ((world (w state))
+             (name ',name)
+             (rules ,(ruleset-form-preprocess form)))
+        (if (is-ruleset name world)
+            (add-to-ruleset-core name rules world state)
+          (def-ruleset-core name rules world state))))))
 
 (defmacro set-ruleset! (name form)
   (declare (xargs :guard (symbolp name)))
@@ -186,42 +271,80 @@
       (expand-ruleset1 x world)
     (er hard 'expand-ruleset "~x0 is not a valid ruleset.~%" x)))
 
+(defsection enable*
+  :parents (rulesets)
+  :short "Ruleset-aware version of @(see enable)."
+  :long "<p>Examples:</p>
 
-(defmacro enable* (&rest x)
-  `(union-theories-fn
-    (current-theory :here)
-    (expand-ruleset ',x world)
-    t world))
+@({
+ (in-theory (enable* my-rules append car-cons))
 
-(defmacro disable* (&rest x)
-  `(set-difference-theories-fn
-    (current-theory :here)
-    (expand-ruleset ',x world)
-    t world))
+ (defthm ...
+    :hints ((\"Goal\" :in-theory (enable* foo ...))))
+})"
 
+  (defmacro enable* (&rest x)
+    `(union-theories-fn
+      (current-theory :here)
+      (expand-ruleset ',x world)
+      t world)))
 
-(defun e/d*-fn (theory e/d-list enable-p)
-  (declare (xargs :guard (and (true-list-listp e/d-list)
-                              (or (eq enable-p t)
-                                  (eq enable-p nil)))))
-  (cond ((atom e/d-list) theory)
-        (enable-p (e/d*-fn `(UNION-THEORIES ,theory
-                                           (expand-ruleset ',(car e/d-list) world))
-                           (cdr e/d-list) nil))
-        (t (e/d*-fn `(SET-DIFFERENCE-THEORIES ,theory
+(defsection disable*
+  :parents (rulesets)
+  :short "Ruleset-aware version of @(see disable)."
+  :long "<p>Examples:</p>
+
+@({
+ (in-theory (disable* my-rules append car-cons))
+
+ (defthm ...
+    :hints ((\"Goal\" :in-theory (disable* foo ...))))
+})"
+
+  (defmacro disable* (&rest x)
+    `(set-difference-theories-fn
+      (current-theory :here)
+      (expand-ruleset ',x world)
+      t world)))
+
+(defsection e/d*
+  :parents (rulesets)
+  :short "Ruleset-aware version of @(see e/d)."
+  :long "<p>Examples:</p>
+
+@({
+ (in-theory (e/d* (unusual-rules append)
+                  (expensive-rules default-car default-cdr)))
+
+ (defthm ...
+    :hints ((\"Goal\"
+             :in-theory (e/d* (unusual-rules append)
+                              (expensive-rules default-car
+                               default-cdr)))))
+})"
+
+  (defun e/d*-fn (theory e/d-list enable-p)
+    (declare (xargs :guard (and (true-list-listp e/d-list)
+                                (or (eq enable-p t)
+                                    (eq enable-p nil)))))
+    (cond ((atom e/d-list) theory)
+          (enable-p (e/d*-fn `(UNION-THEORIES ,theory
                                               (expand-ruleset ',(car e/d-list) world))
-                    (cdr e/d-list) t))))
+                             (cdr e/d-list) nil))
+          (t (e/d*-fn `(SET-DIFFERENCE-THEORIES ,theory
+                                                (expand-ruleset ',(car e/d-list) world))
+                      (cdr e/d-list) t))))
 
-(defmacro e/d** (&rest theories)
-  (declare (xargs :guard (true-list-listp theories)))
-  (cond ((atom theories) nil)
-        (t (e/d*-fn nil theories t))))
+  (defmacro e/d** (&rest theories)
+    (declare (xargs :guard (true-list-listp theories)))
+    (cond ((atom theories) nil)
+          (t (e/d*-fn nil theories t))))
 
-(defmacro e/d* (&rest theories)
-  (declare (xargs :guard (true-list-listp theories)))
-  (cond ((atom theories) '(current-theory :here))
-        (t (e/d*-fn '(current-theory :here)
-                    theories t))))
+  (defmacro e/d* (&rest theories)
+    (declare (xargs :guard (true-list-listp theories)))
+    (cond ((atom theories) '(current-theory :here))
+          (t (e/d*-fn '(current-theory :here)
+                      theories t)))))
 
 (defmacro ruleset-theory (ruleset)
   `(expand-ruleset (ruleset ,ruleset) world))
@@ -234,31 +357,52 @@
 (local
  (encapsulate
   nil
-  (include-book
+
+(include-book
    ;; This is on a separate line so that this book won't appear to depend on
    ;; the make-event subdir.
    "make-event/assert" :dir :system)
 
- (def-ruleset! foo '(append reverse))
+(def-ruleset! foo '(append reverse))
+(def-ruleset! bar '(foo nth))
 
- (def-ruleset! bar '((:ruleset foo) nth))
+(add-to-ruleset foo '((consp)))
 
- (add-to-ruleset foo '((consp)))
+(in-theory nil)
+(in-theory (enable* foo))
 
- (in-theory (enable* (:ruleset foo)))
+(assert! (let ((ens (ens state)))
+           (and (not (active-runep '(:definition member-equal)))
+                (not (active-runep '(:definition nth)))
+                (active-runep '(:definition binary-append))
+                (active-runep '(:definition reverse))
+                (active-runep '(:executable-counterpart consp)))))
 
- (assert! (let ((ens (ens state)))
-            (active-runep '(:definition binary-append))))
+(in-theory nil)
+(in-theory (enable* bar))
+(assert! (let ((ens (ens state)))
+           (and (not (active-runep '(:definition member-equal)))
+                (active-runep '(:definition nth))
+                (active-runep '(:definition binary-append))
+                (active-runep '(:definition reverse))
+                (active-runep '(:executable-counterpart consp)))))
 
- (in-theory (disable* (:ruleset bar)))
+(in-theory (disable* foo))
+(assert! (let ((ens (ens state)))
+           (and (active-runep '(:definition nth))
+                (not (active-runep '(:definition member-equal)))
+                (not (active-runep '(:definition binary-append)))
+                (not (active-runep '(:definition reverse)))
+                (not (active-runep '(:executable-counterpart consp))))))
 
- (assert! (let ((ens (ens state)))
-            (not (active-runep '(:definition binary-append)))))
+(in-theory nil)
+(in-theory (e/d* ((:ruleset bar)) ((:ruleset foo))))
+(assert! (let ((ens (ens state)))
+           (and (active-runep '(:definition nth))
+                (not (active-runep '(:definition member-equal)))
+                (not (active-runep '(:definition binary-append)))
+                (not (active-runep '(:definition reverse)))
+                (not (active-runep '(:executable-counterpart consp))))))
 
- (in-theory (e/d* ((:ruleset bar)) ((:ruleset foo))))
-
- (assert! (let ((ens (ens state)))
-            (and (not (active-runep '(:definition binary-append)))
-                 (active-runep '(:definition nth)))))))
 
 ||#
