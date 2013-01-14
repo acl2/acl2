6bf24bd4dee74365d721973f7839ee7ae39a1a8a
Sol Swords <sswords@gmail.com>
2013-01-14 17:18:55 +0000

    add stub books for relocated unicode books; exclude them from Makefile-generic build

---
+++ books/unicode/explode-nonnegative-integer.lisp
@@ -0,0 +1,6 @@
+
+(in-package "ACL2")
+
+;; cert_param: (reloc_stub)
+
+(include-book "std/ks/explode-nonnegative-integer" :dir :system)
---
+++ books/unicode/take.lisp
@@ -0,0 +1,6 @@
+
+(in-package "ACL2")
+
+;; cert_param: (reloc_stub)
+
+(include-book "std/lists/take" :dir :system)
