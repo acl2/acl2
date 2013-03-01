331ab305b12866244ea53b71f5b3646de7e02310
Matthew J Kaufmann <kaufmann@cs.utexas.edu>
2013-03-01 21:04:05 +0000

    Disabled forcing to accommodate heuristic change to ACL2 suggested by Robert Krug, to allow forcing before a failure from the ancestors check.  This still certifies with ACL2 6.1 as well.

--- books/sorting/sorts-equivalent3.lisp
+++ books/sorting/sorts-equivalent3.lisp
@@ -9,6 +9,10 @@
 (include-book "qsort")
 (include-book "bsort")
 
+; Matt K.: Disabled forcing after Version  6.1, due to heuristic change that
+; allows forcing before a failure from the ancestors check.
+(local (in-theory (disable (force))))
+
 (defthm msort-is-isort
   (equal (msort x) (isort x))
   :hints (("Goal" :in-theory (enable equisort))))
