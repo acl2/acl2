9bd441b7672f5b8713f2ff8bde471414076e0119
Jared C Davis <jared.c.davis@gmail.com>
2013-05-15 03:34:19 +0000

    Add "linting" tool for finding rewrite rules that are redundant and/or conflicting, etc.  Very primitive.

---
+++ books/system/origin.lisp
@@ -0,0 +1,91 @@
+; ACL2 Event Origin
+; Copyright (C) 2013 Kookamara LLC
+;
+; Contact:
+;   Kookamara LLC
+;   11410 Windermere Meadows
+;   Austin, TX 78759
+;
+; This program is free software; you can redistribute it and/or modify it under
+; the terms of the GNU General Public License as published by the Free Software
+; Foundation; either version 2 of the License, or (at your option) any later
+; version.  This program is distributed in the hope that it will be useful but
+; WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+; more details.  You should have received a copy of the GNU General Public
+; License along with this program; if not, write to the Free Software
+; Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA 02110-1335, USA.
+;
+; Original authors: Jared Davis <jared.c.davis@gmail.com>
+
+(in-package "ACL2")
+(include-book "xdoc/top" :dir :system)
+(set-state-ok t)
+(program)
+
+(defxdoc origin
+  :parents (acl2::pe)
+  :short "Returns a summary of where a @(see logical-name) originates from."
+  :long "<p>Examples:</p>
+@({
+  (include-book \"system/origin\" :dir :system)
+
+  ;; certain built-in commands don't have a command number
+  (origin 'consp)    --> (value (:built-in nil))
+
+  ;; other built-in ACL2 commands have command numbers
+  (origin 'car-cons) --> (value (:built-in -936))
+
+  ;; include-book path is reported for events included from other books
+  (origin 'xdoc::save) --> (value (\"/home/jared/acl2/books/system/origin.lisp\"
+                                   \"/home/jared/acl2/books/xdoc/top.lisp\"))
+
+  (origin 'lnfix) --> (value (\"/home/jared/acl2/books/system/origin.lisp\"
+                              \"/home/jared/acl2/books/xdoc/top.lisp\"
+                              \"/home/jared/acl2/books/xdoc/base.lisp\"))
+
+  ;; some definitions are from the current session, e.g.:
+  (defun f (x) x)
+  (origin 'f)     --> (value 3)
+
+})")
+
+
+(defun origin-fn1 (wrld ev-wrld cmd-wrld)
+  ;; Styled after PE-FN1.  I have no idea what I'm doing.
+  (cond
+   ((equal (access-event-tuple-form (cddar ev-wrld))
+           (access-command-tuple-form (cddar cmd-wrld)))
+    ;; This handles two kinds of things:
+    ;;  (1) built-in things with a defining event, and
+    ;;  (2) things from the current session (not an include-book)
+    ;; :pe-fn1 would do a print-ldd of a make-command-ldd here, which, if you work out
+    ;; the cases, seems to be just doing this:
+    (absolute-to-relative-command-number
+     (access-command-tuple-number (cddar cmd-wrld))
+     wrld))
+   (t
+    (let ((book-path (global-val 'include-book-path ev-wrld)))
+      (cond (book-path
+             (reverse book-path))
+            (t
+             :session?))))))
+
+(defun origin-fn (logical-name ctx state)
+  ;; Styled after PE-FN.  I have no idea what I'm doing.
+  (let ((wrld (w state)))
+    (cond
+     ((and (symbolp logical-name)
+           (not (eq logical-name :here))
+           (zp (getprop logical-name 'absolute-event-number nil 'current-acl2-world wrld)))
+      ;; Things that are built into ACL2 without a defining event
+      (value (list :built-in nil)))
+     (t
+      (er-let*
+       ((ev-wrld  (er-decode-logical-name logical-name wrld ctx state))
+        (cmd-wrld (superior-command-world ev-wrld wrld ctx state)))
+       (value (origin-fn1 wrld ev-wrld cmd-wrld)))))))
+
+(defmacro origin (logical-name)
+  `(origin-fn ,logical-name 'origin state))
+
---
+++ books/tools/lint.lisp
@@ -0,0 +1,235 @@
+; ACL2 Theory Linter
+; Copyright (C) 2013 Kookamara LLC
+;
+; Contact:
+;   Kookamara LLC
+;   11410 Windermere Meadows
+;   Austin, TX 78759
+;
+; This program is free software; you can redistribute it and/or modify it under
+; the terms of the GNU General Public License as published by the Free Software
+; Foundation; either version 2 of the License, or (at your option) any later
+; version.  This program is distributed in the hope that it will be useful but
+; WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+; more details.  You should have received a copy of the GNU General Public
+; License along with this program; if not, write to the Free Software
+; Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA 02110-1335, USA.
+;
+; Original authors: Jared Davis <jared.c.davis@gmail.com>
+
+(in-package "ACL2")
+(include-book "cutil/da-base" :dir :system)
+(include-book "system/origin" :dir :system)
+(include-book "str/cat" :dir :system)
+(include-book "tools/rulesets" :dir :system)
+(include-book "misc/assert" :dir :system)
+(include-book "defsort/duplicated-members" :dir :system)
+(include-book "clause-processors/unify-subst" :dir :system)
+(set-state-ok t)
+(program)
+
+
+;; (DA-DEFREC-EMULATION REC) -- Adds defaggregate-style emulation for DEFREC records
+;;   - Adds foo->bar style accessors
+;;   - Adds defaggregate-like b* binders
+
+(defun flatten-defrec-fields (x)
+  ;; Flatten a defrec field layout (which can be an arbitrary shaped cons tree)
+  ;; into an ordinary list.
+  (if (atom x)
+      (and x (list x))
+    (append (flatten-defrec-fields (car x))
+            (flatten-defrec-fields (cdr x)))))
+
+(defun look-up-defrec-fields (rec world)
+  ;; Horrible awful thing.  The fields for a defrec aren't saved anywhere
+  ;; explicitly, but we can look them up in the body of the MAKE function.
+  ;; See the function MAKE-RECORD-MAKER in the acl2 sources.
+  (b* ((maker (record-maker-function-name rec))
+       (body  (getprop maker 'macro-body nil 'current-acl2-world world))
+       ((unless body)
+        (er hard? 'look-up-defrec-field-layout
+            "Can't find macro-body for maker ~x0 of defrec ~x1.  is ~x1 even ~
+             a defrec?" maker rec))
+       (quoted-layout (third body))
+       ((unless (quotep quoted-layout))
+        (er hard? 'look-up-defrec-field-layout
+            "Sanity check failed, field layout of ~x0 is not a quotep?" rec)))
+    (flatten-defrec-fields
+     (unquote quoted-layout))))
+
+(defun da-accessor-for-defrec-field (rec field)
+  ;; Create a defaggregate-style accessor foo->bar for field bar of defrec foo
+  `(defun-inline ,(cutil::da-accessor-name rec field) (x)
+     (declare (xargs :guard (,(intern$ (str::cat "WEAK-" (symbol-name rec) "-P") "ACL2") x)))
+     (access ,rec x ,(intern$ (symbol-name field) "KEYWORD"))))
+
+(defun da-accessors-for-defrec-fields (rec fields)
+  (if (atom fields)
+      nil
+    (cons (da-accessor-for-defrec-field rec (car fields))
+          (da-accessors-for-defrec-fields rec (cdr fields)))))
+
+(defun da-defrec-emulation-fn (rec world)
+  (let ((fields (look-up-defrec-fields rec world)))
+    `(progn
+       ,@(da-accessors-for-defrec-fields rec fields)
+       ,(cutil::da-make-binder rec fields))))
+
+(defmacro da-defrec-emulation (rec)
+  `(make-event
+    (b* ((world (w state)))
+      (value (da-defrec-emulation-fn ',rec world)))))
+
+(da-defrec-emulation rewrite-rule)
+
+
+
+;; Gather all rewrite rules in the universal theory
+
+(defun find-rules-of-runes (runes world)
+  (if (atom runes)
+      nil
+    (append (find-rules-of-rune (car runes) world)
+            (find-rules-of-runes (cdr runes) world))))
+
+(defun filter-non-rewrite-rules (x)
+  (cond ((atom x)
+         nil)
+        ((not (weak-rewrite-rule-p (car x)))
+         (filter-non-rewrite-rules (cdr x)))
+        (t
+         (cons (car x)
+               (filter-non-rewrite-rules (cdr x))))))
+
+(defun get-all-rewrite-rules (world)
+  (filter-non-rewrite-rules
+   (find-rules-of-runes (rules-of-class :rewrite :here) world)))
+
+
+
+;; Looking for compatible, redundant rules
+
+(defun unforce-hyp (x)
+  (if (and (consp x)
+           (or (eq (ffn-symb x) 'force)
+               (eq (ffn-symb x) 'case-split)))
+      (second x)
+    x))
+
+(defun unforce-hyps (x)
+  (if (atom x)
+      nil
+    (cons (unforce-hyp (car x))
+          (unforce-hyps (cdr x)))))
+
+(defun rule-redundant-p (x y)
+  ;; X is "redundant" with respect to Y if its LHS unifies with the LHS of Y,
+  ;; and it has the same/worse hyps.
+  (b* (((rewrite-rule x) x)
+       ((rewrite-rule y) y)
+
+       ((unless (and (or (eq x.subclass 'backchain)
+                         (eq x.subclass 'abbreviation))
+                     (or (eq y.subclass 'backchain)
+                         (eq y.subclass 'abbreviation))))
+        ;; Don't try to deal with meta/other future rules
+        nil)
+
+       ((when (or (and (eq x.subclass 'backchain) x.heuristic-info)
+                  (and (eq y.subclass 'backchain) y.heuristic-info)))
+        ;; Don't include any rules with loop-stoppers because they're likely to be
+        ;; commutativity rules that look highly redundant with everything else.
+        nil)
+
+       ((unless (eq x.equiv y.equiv))
+        ;; This may be sort of too strong, but might be pretty useful for filtering
+        ;; out noise.
+        nil)
+
+       ((mv okp sigma) (simple-one-way-unify x.lhs y.lhs nil))
+       ((unless okp)
+        nil)
+
+       (xhyps-rw (substitute-into-list (unforce-hyps x.hyps) sigma)))
+    (subsetp-equal xhyps-rw (unforce-hyps y.hyps))))
+
+(defun find-redundancy (a x)
+  (cond ((atom x)
+         nil)
+        ((and (rule-redundant-p a (car x))
+              (not (equal a (car x))))
+         (list (list :redundant a (car x))))
+        (t
+         (find-redundancy a (cdr x)))))
+
+(defun find-redundancies (x y)
+  (if (atom x)
+      nil
+    (append (find-redundancy (car x) y)
+            (find-redundancies (cdr x) y))))
+
+(defun find-redundancies-top (x)
+  (find-redundancies x x))
+
+
+
+(defun summarize-rule (rule state)
+  (b* (((rewrite-rule rule) rule)
+       (concl (list rule.equiv rule.lhs rule.rhs))
+       (summary (cond ((<= 2 (len rule.hyps))
+                       `(implies (and . ,rule.hyps) ,concl))
+                      ((consp rule.hyps)
+                       `(implies ,(car rule.hyps) ,concl))
+                      (t
+                       concl)))
+       (name (second rule.rune))
+       ((mv er origin state) (origin-fn name 'summarize-rule state))
+       (origin (if er
+                   (prog2$ (cw "Error in summarize-rule: ~x0" er)
+                           :error)
+                 origin)))
+    (mv `(defthm ,name ,summary :from ,origin)
+        state)))
+
+(defun summarize-redundancy (x state)
+  (b* (((unless (eq (car x) :redundant))
+        (er hard? 'summarize-redundancy "expected (:redundant rule rule), found ~x0" x)
+        (mv nil state))
+       ((mv sum1 state) (summarize-rule (second x) state))
+       ((mv sum2 state) (summarize-rule (third x) state)))
+    (mv (list :redundant sum1 sum2)
+        state)))
+
+(defun summarize-redundancies (x state)
+  (b* (((when (atom x))
+        (mv nil state))
+       ((mv car state) (summarize-redundancy (car x) state))
+       ((mv cdr state) (summarize-redundancies (cdr x) state)))
+    (mv (cons car cdr) state)))
+
+
+(defun lint-fn (state)
+  (summarize-redundancies
+   (find-redundancies-top (get-all-rewrite-rules (w state)))
+   state))
+
+(defmacro lint ()
+  `(lint-fn state))
+
+
+;; there could probably be some kind of quick filtering based on leading function symbol to
+;; narrow the search down...
+
+
+
+#|| Example:
+
+(include-book
+ "tools/lint" :dir :system)
+(include-book
+ "centaur/bitops/top" :dir :system)
+(lint)
+
+||#
