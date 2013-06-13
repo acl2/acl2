7b6a6127861ffd65e01862f1feb783be977f9045
Sol Swords <sswords@gmail.com>
2013-06-13 13:56:58 +0000

    changed cw-unformatted to use include-raw

--- books/centaur/vl/util/cw-unformatted.lisp
+++ books/centaur/vl/util/cw-unformatted.lisp
@@ -20,6 +20,8 @@
 
 (in-package "ACL2")
 
+(include-book "tools/include-raw" :dir :system)
+
 ; There doesn't seem to be any mechanism for just printing the contents of a
 ; string without any formatting using cw.  Using ~s mostly works, but it will
 ; insert its own line breaks.  Using ~f fixes that, but puts quotes around the
@@ -33,22 +35,10 @@
   (er hard? 'cw-unformatted "Raw lisp definition not installed?"))
 
 (defttag cw-unformatted)
-(progn!
- (set-raw-mode t)
- (defun cw-unformatted (x)
-   (let ((stream (get-output-stream-from-channel *standard-co*)))
-     (write-string x stream)
 
-; [Jared] We added finish-output to make this work better with file streams
-; that we were wanting to monitor.  Unfortunately this turned out to be
-; horribly slow when writing to an NFS-mounted file system.  (Which can happen,
-; for instance, if you are running ACL2 and redirecting output to a file from
-; the shell, or using with-stdout, or similar.)  It seems that using
-; force-output is much, much faster, and at least tries to accomplish something
-; similar, so now we'll just use it.
+; (depends-on "cw-unformatted-raw.lsp")
+(include-raw "cw-unformatted-raw.lsp")
 
-     (force-output stream))
-   nil))
 (defttag nil)
 
 
