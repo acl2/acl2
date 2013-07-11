7af233b1bf207ec80e511b938ce66039c42e6775
David L Rager <ragerdl@gmail.com>
2013-07-11 04:31:58 +0000

    Adding some subtracters.  Does the world now sum to nothing?  :-D

--- books/centaur/regression/subtract.v
+++ books/centaur/regression/subtract.v
@@ -7,103 +7,103 @@ Copyright (C) 2013 David L. Rager
 
 
 
-// add.v
+// subtract.v
 //
-// These are simple adders.
+// These are simple subtracters.
 
-module add1 (
+module subtract1 (
   output [0:0] out,
   input [0:0] abus,
   input [0:0] bbus
 );
-assign out = abus + bbus;
+assign out = abus - bbus;
 endmodule
 
-module add2 (
+module subtract2 (
   output [1:0] out,
   input [1:0] abus,
   input [1:0] bbus
 );
-assign out = abus + bbus;
+assign out = abus - bbus;
 endmodule
 
-module add3 (
+module subtract3 (
   output [2:0] out,
   input [2:0] abus,
   input [2:0] bbus
 );
-assign out = abus + bbus;
+assign out = abus - bbus;
 endmodule
 
-module add4 (
+module subtract4 (
   output [3:0] out,
   input [3:0] abus,
   input [3:0] bbus
 );
-assign out = abus + bbus;
+assign out = abus - bbus;
 endmodule
 
-module add8 (
+module subtract8 (
   output [7:0] out,
   input [7:0] abus,
   input [7:0] bbus
 );
-assign out = abus + bbus;
+assign out = abus - bbus;
 endmodule
 
-module add16 (
+module subtract16 (
   output [15:0] out,
   input [15:0] abus,
   input [15:0] bbus
 );
-assign out = abus + bbus;
+assign out = abus - bbus;
 endmodule
 
-module add32 (
+module subtract32 (
   output [31:0] out,
   input [31:0] abus,
   input [31:0] bbus
 );
-assign out = abus + bbus;
+assign out = abus - bbus;
 endmodule
 
-module add64 (
+module subtract64 (
   output [63:0] out,
   input [63:0] abus,
   input [63:0] bbus
 );
-assign out = abus + bbus;
+assign out = abus - bbus;
 endmodule
 
-module add128 (
+module subtract128 (
   output [127:0] out,
   input [127:0] abus,
   input [127:0] bbus
 );
-assign out = abus + bbus;
+assign out = abus - bbus;
 endmodule
 
-module add256 (
+module subtract256 (
   output [255:0] out,
   input [255:0] abus,
   input [255:0] bbus
 );
-assign out = abus + bbus;
+assign out = abus - bbus;
 endmodule
 
-module add512 (
+module subtract512 (
   output [511:0] out,
   input [511:0] abus,
   input [511:0] bbus
 );
-assign out = abus + bbus;
+assign out = abus - bbus;
 endmodule
 
-module add1024 (
+module subtract1024 (
   output [1023:0] out,
   input [1023:0] abus,
   input [1023:0] bbus
 );
-assign out = abus + bbus;
+assign out = abus - bbus;
 endmodule
 
