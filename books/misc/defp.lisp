54d4eaf5097169c2dc41a26c08c54124a7ae6464
Matthew J Kaufmann <kaufmann@cs.utexas.edu>
2013-08-10 14:55:24 +0000

    Updated to a BSD license (now that included book defpun.lisp also has a BSD license).

--- books/misc/defp.lisp
+++ books/misc/defp.lisp
@@ -1,20 +1,8 @@
 ; Copyright (C) 2013, Regents of the University of Texas
-; Enhancement of defpun to allow more than one tail-recursive call.
 ; Written by Matt Kaufmann, August, 2007
+; License: A 3-clause BSD license.  See the LICENSE file distributed with ACL2.
 
-;  This program is free software; you can redistribute it and/or modify
-;  it under the terms of the GNU General Public License as published by
-;  the Free Software Foundation; either version 2 of the License, or
-;  (at your option) any later version.
-
-;  This program is distributed in the hope that it will be useful,
-;  but WITHOUT ANY WARRANTY; without even the implied warranty of
-;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;  GNU General Public License for more details.
-
-;  You should have received a copy of the GNU General Public License
-;  along with this program; if not, write to the Free Software
-;  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+; Enhancement of defpun to allow more than one tail-recursive call.
 
 ; Notes:
 
