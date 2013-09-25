c85bcc5f847a057bae93a79b6deeb09ce0f924bf
Harsh Raju Chamarthi <harshrc@gmail.com>
2013-09-25 02:32:16 +0000

    slightly modified loop syntax used in line 3792 in function anchor-find to conform to CL spec as implemented by CLISP

--- books/ccg/ccg.lisp
+++ books/ccg/ccg.lisp
@@ -3789,7 +3789,7 @@
       (dolist (scc sccs)
         (simple-anchors (srg-restrict srg scc) ahash ccmfs num-contexts))
       ;; convert the set of anchors to a list.
-      (let ((anchors (loop for k being the hash-key using (hash-value v) of ahash
+      (let ((anchors (loop for k being the hash-keys of ahash using (hash-value v)
                            when v collect k)))
         ;;(format t "simple anchors: ~A~%" anchors)
         ;; if we have found anchors, return them.
