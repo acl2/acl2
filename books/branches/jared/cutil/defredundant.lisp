58af722148c61d0763bc01fecbbdd39e2e9e38fd
Jared C Davis <jared.c.davis@gmail.com>
2013-10-16 00:32:04 +0000

    delete jared branch since it's been reintegrated

--- books/branches/jared/cutil/defredundant.lisp
+++
@@ -1,6 +0,0 @@
-(in-package "CUTIL")
-
-; cert_param: (reloc_stub)
-(include-book "std/util/defredundant" :dir :system)
-
-(moved)
--- books/branches/jared/regex/equal-based-set.lisp
+++
@@ -1,7 +0,0 @@
-
-(in-package "ACL2")
-
-(include-book "defset-macros")
-(local (include-book "defset-encapsulates"))
-
-(def-set-equiv equal :prove-elem-congruences nil)
