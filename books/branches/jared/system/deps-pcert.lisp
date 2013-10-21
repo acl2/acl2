0eeb67cfe433e393603e36cc83d5ea54733e4939
Jared C Davis <jared.c.davis@gmail.com>
2013-10-21 15:54:17 +0000

    delete jared branch, again, for the last time.

--- books/branches/jared/system/deps-pcert.lisp
+++
@@ -1,9 +0,0 @@
-;; Silly file to trick cert.pl into including the right books.
-
-(in-package "ACL2")
-
-#||
-(include-book "misc/eval" :dir :system)
-||#
-
-(value-triple nil)
--- books/branches/jared/xdoc/emacsclient-wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# This is a simple wrapper that can be used to launch emacsclient with 
-# additional options.  A script like this may be useful for connecting
-# your web browser to emacs for .xdoc-link files.
-
-emacsclient --no-wait "$1"
