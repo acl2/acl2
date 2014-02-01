ea9f80ba34dec795d546323fc188d9f417361ff6
David L Rager <ragerdl@gmail.com>
2014-02-01 19:45:59 +0000

    Adding licenses to some files I wrote over the summer.
    fixes issue 129
    
    Note: I'm submitting this from my personal laptop, which is very slow.  I don't want to wait 4 hours to rebuild the books, so while I doubt this commit breaks the books, you might not want to update ths directory to this revision until I send an 'all clear' notice.

--- books/centaur/regression/idiv.v
+++ books/centaur/regression/idiv.v
@@ -1,9 +1,48 @@
 /*
 
-Copyright (C) 2013 David L. Rager
+VL Regression Suite
+Copyright (C) David L. Rager
 
-A derived work from the following, which has no specific license:
-  http://commdspfpga.blogspot.com/2012/09/unsiged-integer-divider-in-verilog.html
+Note: The license below is based on the template at:
+http://opensource.org/licenses/BSD-3-Clause
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+o Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the following disclaimer.
+
+o Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution.
+
+o Neither the name of the University of Texas, Austin nor the names of
+  its contributors may be used to endorse or promote products derived
+  from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+Original author: David Rager <ragerdl@defthm.com>
+
+Other entities should feel free to add to the "VL Regression Suite"
+with files that have different licenses and/or copyrights.
+
+
+This file is a derived work from the following, which has no specific
+  license:
+
+   http://commdspfpga.blogspot.com/2012/09/unsiged-integer-divider-in-verilog.html
 
 
 The module below divides an eight bit number by a four bit number.  We
