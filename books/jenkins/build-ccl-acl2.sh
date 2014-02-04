7aabbfb6787ba0791a49aaf1c8c9ef57ea77cda2
Jared C Davis <jared.c.davis@gmail.com>
2014-02-04 11:34:35 +0000

    add jenkins scripts, README, and set up externals

---
+++ books/jenkins/build-ccl-acl2.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+echo "Starting build-ccl-acl2.sh"
+echo " -- Running in `pwd`"
+echo " -- Running on `hostname`"
+echo " -- PATH is $PATH"
+
+source $JENKINS_HOME/env.sh
+
+ACL2DIR=`pwd`
+
+LISP=`which ccl`
+echo "Using LISP = $LISP"
+echo "Using STARTJOB = `which startjob`"
+
+echo "Making ACL2"
+startjob -c "make acl2 LISP=$LISP &> make.log" \
+  --name "J_CCL_ACL2" \
+  --limits "pmem=4gb,nodes=1:ppn=1,walltime=10:00"
+
+echo "Building the books."
+cd acl2-devel/books
+make ACL2=$ACL2DIR/acl2-devel/saved_acl2 all -j 4 USE_QUICKLISP=1
+
+echo "Build was successful."
+
+exit 0
---
+++ books/jenkins/build-gcl-ansi-acl2h.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+echo "Starting build-gcl-ansi-acl2h.sh"
+echo " -- Running in `pwd`"
+echo " -- Running on `hostname`"
+echo " -- PATH is $PATH"
+
+source $JENKINS_HOME/env.sh
+
+ACL2DIR=`pwd`
+
+LISP=`which gcl-ansi`
+echo "Using LISP = $LISP"
+echo "Using STARTJOB = `which startjob`"
+
+echo "Making ACL2(h)"
+startjob -c "make acl2h LISP=$LISP &> make.log" \
+  --name "J_GCL_ANSI_ACL2H" \
+  --limits "pmem=4gb,nodes=1:ppn=1,walltime=40:00"
+
+echo "Building the books."
+cd acl2-devel/books
+make ACL2=$ACL2DIR/acl2-devel/saved_acl2h all -j 4
+
+echo "Build was successful."
+
+exit 0
