8a729371a4ae60170d5cebc5e624f8855f2121aa
Matthew J Kaufmann <kaufmann@cs.utexas.edu>
2014-04-21 16:03:52 +0000

    Added with-supporters(-after) topics to doc build

--- books/doc/top.lisp
+++ books/doc/top.lisp
@@ -159,7 +159,8 @@
 (include-book "tools/do-not" :dir :system)
 (include-book "tools/plev" :dir :system)
 (include-book "tools/plev-ccl" :dir :system)
-(include-book "tools/remove-hyps" :dir :system)
+(include-book "tools/with-supporters" :dir :system)
+
 
 ; [Jared] removing these to speed up the manual build
 ;(include-book "tutorial/intro")
