c675a663b2c6ad414e4208912dd18e95567ca9dc
Sol Swords <sswords@gmail.com>
2014-07-14 14:15:24 +0000

    add copyright header for bstar.lisp, remove pack.lisp and absorb its contents into bstar since nothing else used it

--- books/tools/bstar.lisp
+++ books/tools/bstar.lisp
@@ -1,3 +1,32 @@
+; b* -- pluggable sequencing/binding macro thing
+; Copyright (C) 2007-2014 Centaur Technology
+;
+; Contact:
+;   Centaur Technology Formal Verification Group
+;   7600-C N. Capital of Texas Highway, Suite 300, Austin, TX 78731, USA.
+;   http://www.centtech.com/
+;
+; License: (An MIT/X11-style license)
+;
+;   Permission is hereby granted, free of charge, to any person obtaining a
+;   copy of this software and associated documentation files (the "Software"),
+;   to deal in the Software without restriction, including without limitation
+;   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+;   and/or sell copies of the Software, and to permit persons to whom the
+;   Software is furnished to do so, subject to the following conditions:
+;
+;   The above copyright notice and this permission notice shall be included in
+;   all copies or substantial portions of the Software.
+;
+;   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+;   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+;   DEALINGS IN THE SOFTWARE.
+;
+; Original author: Sol Swords <sswords@centtech.com>
 (in-package "ACL2")
 
 ;; Contact: Sol Swords <sswords@cs.utexas.edu>
@@ -245,7 +274,39 @@ examples, and additionally PATBIND-NTHS, PATBIND-ER, and so forth.
 
 
 
-(include-book "pack")
+(mutual-recursion
+ (defun pack-list (args)
+   (declare (xargs :measure (acl2-count args)
+                   :guard t
+                   :verify-guards nil))
+   (if (atom args)
+       nil
+     (if (atom (cdr args))
+         (pack-tree (car args))
+       (append (pack-tree (car args))
+               (cons #\Space
+                     (pack-list (cdr args)))))))
+ (defun pack-tree (tree)
+   (declare (xargs :measure (acl2-count tree)
+                   :guard t))
+   (if (atom tree)
+       (if (or (acl2-numberp tree)
+               (characterp tree)
+               (stringp tree)
+               (symbolp tree))
+           (explode-atom tree 10)
+         '(#\Space))
+     (append (cons #\( (pack-tree (car tree)))
+             (cons #\Space (pack-list (cdr tree)))
+             (list #\))))))
+
+(defun pack-term (args)
+  (declare (xargs :guard t
+                  :verify-guards nil))
+  (intern (coerce (pack-list args) 'string) "ACL2"))
+
+(defmacro pack (&rest args)
+  `(pack-term (list ,@args)))
 
 (defun macro-name-for-patbind (binder)
   (intern-in-package-of-symbol
