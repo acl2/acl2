30268bca31879851749755bb3214c266cf568ca5
Matthew J Kaufmann <kaufmann@cs.utexas.edu>
2014-07-24 18:47:37 +0000

    Made a bunch of license-related mods, all with appropriate permsision, including:
    - Changed licenses on Computational Logic books to BSD 3-clause
      licenses, as was authorized by Mike Smith on behalf of CLInterests
      on Friday, 14 Mar 2014.  Mike said:
    
        Yes, this change is approved unanimously by the owners of the
        Computational Logic copyrights.
    
    - Added Centaur copyright/license info (based on centaur/gl/gl.lisp)
      to arithmetic/cert.acl2 and arithmetic/nat-listp.lisp, as authorized
      in emails from Sol Swords.
    
    - Modified license on arithmetic/mod-gcd.lisp and
      arithmetic/rationals.lisp, as authoized by John Cowles.
    
    Made a few trivial edits to acl2-doc.lisp.

--- books/centaur/memoize/old/watch.lisp
+++ books/centaur/memoize/old/watch.lisp
@@ -1,16 +1,9 @@
 ; watch.lisp
 ; Copyright (C) 2013, Regents of the University of Texas
+; License: A 3-clause BSD license.  See the LICENSE file distributed with ACL2.
 
 ; This version of ACL2 is a descendent of ACL2 Version 1.9, Copyright
 ; (C) 1997 Computational Logic, Inc.  See the documentation topic NOTE-2-0.
-
-; This program is free software; you can redistribute it and/or modify
-; it under the terms of the LICENSE file distributed with ACL2.
-
-; This program is distributed in the hope that it will be useful,
-; but WITHOUT ANY WARRANTY; without even the implied warranty of
-; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-; LICENSE for more details.
 
 ; This file was originally part of the HONS version of ACL2.  The original
 ; version of ACL2(h) was contributed by Bob Boyer and Warren A. Hunt, Jr.  The
--- books/workshops/1999/embedded/Exercises/Exercise1-2/private-qr-lemmas.lisp
+++ books/workshops/1999/embedded/Exercises/Exercise1-2/private-qr-lemmas.lisp
@@ -1,15 +1,6 @@
 ; quotient-remainder-lemmas.lisp  --  facts about FLOOR, MOD, TRUNCATE and REM
 ; Copyright (C) 1997  Computational Logic, Inc.
-
-; This book is free software; you can redistribute it and/or modify
-; it under the terms of the GNU General Public License as published by
-; the Free Software Foundation; either version 2 of the License, or
-; (at your option) any later version.
-
-; This book is distributed in the hope that it will be useful,
-; but WITHOUT ANY WARRANTY; without even the implied warranty of
-; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-; GNU General Public License for more details.
+; License: A 3-clause BSD license.  See the LICENSE file distributed with ACL2.
 
 ; You should have received a copy of the GNU General Public License
 ; along with this book; if not, write to the Free Software
