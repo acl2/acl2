a20bbfb528962c7cf0db64445b474b5ee53805c2
Sol Swords <sswords@gmail.com>
2014-07-30 20:59:52 +0000

    Merge 6.5 into trunk up to rev 2910

--- books/system/top.lisp
+++ books/system/top.lisp
@@ -10,11 +10,17 @@
 
 (include-book "hl-addr-combine")
 (include-book "extend-pathname")
-(include-book "too-many-ifs")
+; The following is not needed in support of ACL2_DEVEL builds, the ACL2
+; devel-check `make' target, or the ACL2 constant *system-verify-guards-alist*.
+; If we uncomment it, then this book depends ultimately on many other books,
+; and certification fails for some of those books for ACL2 built with
+; ACL2_DEVEL set.
+; (include-book "too-many-ifs")
 (include-book "verified-termination-and-guards")
 (include-book "sublis-var")
 (include-book "subcor-var")
 (include-book "subst-expr")
+(include-book "subst-var")
 (include-book "convert-normalized-term-to-pairs")
 (include-book "gather-dcls")
 (include-book "meta-extract")
