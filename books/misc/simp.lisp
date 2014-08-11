971160048485cbead2670dd5d662ed3ef4e9e152
Matthew J Kaufmann <kaufmann@cs.utexas.edu>
2014-08-11 19:29:10 +0000

    Adding new tool for simplifying a term.  Documentation will likely come later.

---
+++ books/misc/simp.lisp
@@ -0,0 +1,34 @@
+; Copyright (C) 2014, ForrestHunt, Inc.
+; Written by Matt Kaufmann, August, 2014
+; License: A 3-clause BSD license.  See the LICENSE file distributed with ACL2.
+
+(in-package "ACL2")
+
+(include-book "misc/bash" :dir :system)
+
+(program)
+
+(defun simp-pairs (clauses wrld state acc)
+  (declare (xargs :stobjs state))
+  (cond ((endp clauses)
+         (value acc))
+        (t (let* ((cl (car clauses))
+                  (term (assert$ (consp cl)
+                                 (car (last cl))))
+                  (hyps (dumb-negate-lit-lst (butlast cl 1))))
+             (case-match term
+               (('equal lhs '?)
+                (simp-pairs (cdr clauses)
+                            wrld
+                            state
+                            (cons (cons (untranslate lhs nil wrld)
+                                        (untranslate-lst hyps t wrld))
+                                  acc)))
+               (& (er soft 'simp
+                      "A clause returned was ~x0, which doesn't have a final ~
+                       literal of the form ~x1."
+                      cl '(equal term ?))))))))
+
+(defmacro simp (x)
+  `(er-let* ((clauses (bash-term-to-dnf '(equal ,x ?) nil nil t state)))
+     (simp-pairs clauses (w state) state nil)))
