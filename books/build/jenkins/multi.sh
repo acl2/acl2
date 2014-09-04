50cf47e4c9cb1e6c63f894d01017bdd05da4586f
David L Rager <ragerdl@gmail.com>
2014-09-04 01:22:12 -0500

    Updating jenkins Makefile to avoid irrelevant NFS lag warnings; Adding and updating script of appropriate name for performing multiple builds; Attempted to delete old version of that script

--- books/build/jenkins/multi.sh
+++ books/build/jenkins/multi.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# A parameterizable script for use with Jenkins "multi" build feature.
+# The parameters are predictable, but here are some sample
+# invocations:
+#
+# multi.sh LISP=sbcl ACL2_HONS=t ACL2_PAR="" NONSTD=t
+# multi.sh LISP=ccl ACL2_HONS=t ACL2_PAR=t NONSTD=""
+
+# TODO: make it work with startjob, where startjob is a wrapper for
+# bash (really, the challenge in this is getting the definition of
+# startjob correct... it seems like aliases are disregarded).
+
 # Cause the script to exit immediately upon failure
 set -e
 echo "acl2dir is $ACL2DIR"
@@ -36,17 +47,18 @@ fi
 echo "Using ACL2_SUFFIX = $ACL2_SUFFIX"
 
 
-#echo "Making ACL2(h)"
+echo "Making ACL2(${ACL2_SUFFIX})"
 # need to use single-quote to prevent interpolation of the double
 # quotes in the calling shell.  If your startjob is just a wrapper for
 # bash, you'll want to use $* to pass in the arguments to startjob
-make LISP=$LISP &> make.log #\
+make acl2${ACL2_SUFFIX} -f books/build/jenkins/Makefile LISP=$LISP &> make.log #\
 #  --name "J_CCL_ACL2H" \
 #  --limits "pmem=4gb,nodes=1:ppn=1,walltime=10:00"
+#make LISP=$LISP &> make.log 
 
 echo "Building the books."
 cd books
-make arithmetic-2 ACL2=$WORKSPACE/saved_acl2$ACL2_SUFFIX -j3 $MAKEOPTS USE_QUICKLISP=1
+nice time make arithmetic-2 ACL2=$WORKSPACE/saved_acl2$ACL2_SUFFIX -j3 $MAKEOPTS USE_QUICKLISP=1
 
 #cd acl2-devel/books
 #make ACL2=$ACL2DIR/acl2-devel/saved_acl2h all $MAKEOPTS USE_QUICKLISP=1
