9416bb5aa09c1d45754bd5fb504830566c1b28ad
Matt Kaufmann <kaufmann@cs.utexas.edu>
2014-10-01 20:22:15 -0500

    More aggressive fix for *default-hs*, and miscellaneous tweaks for ACL2(hp).
    Various mods now prevent _some_ ACL2(hp) regression errors.  However,
    I still get "Not owner of hash table" in some book certification
    attempts for ACL2(hp), even when *default-hs* is introduced with
    defvar in parallel-raw.lisp before the definition of
    consume-work-on-work-queue-when-there.  I spent quite some time today
    trying to figure out why we still get that error, and got virtually
    nowhere, though from the debugger I can see that it's from
    hl-hspace-hons-get.  Since ACL2(hp) is labeled "experimental" and I
    have other things to do, I'm moving on; besides, I think it's been a
    long time since an ACL2(hp) regression passed.  If someone cares to
    deal with that, great!

--- books/misc/without-waterfall-parallelism.lisp
+++ books/misc/without-waterfall-parallelism.lisp
@@ -14,16 +14,22 @@
   (declare (xargs :mode :program :stobjs state))
   (let ((curr-waterfall-parallelism-val
          (f-get-global 'waterfall-parallelism state)))
-    `(progn (local (make-event
-                    (er-progn (set-waterfall-parallelism nil)
-                              (value '(value-triple nil)))
-                    :check-expansion t))
+
+; Matt K. mode, 10/1/2014: I removed LOCAL from each MAKE-EVENT below so that
+; this utility works when including uncertified books, for example when
+; certifying a book after (set-write-acl2x '(t) state).  Then I also removed
+; :CHECK-EXPANSION from each, because now that the make-event forms are
+; non-local, the second one could cause waterfall-parallelism to be
+; inadvertently turned on when including a book.
+
+    `(progn (make-event
+             (er-progn (set-waterfall-parallelism nil)
+                       (value '(value-triple nil))))
             ,@events
-            (local (make-event
-                    (er-progn (set-waterfall-parallelism
-                               ',curr-waterfall-parallelism-val)
-                              (value '(value-triple t)))
-                    :check-expansion t)))))
+            (make-event
+             (er-progn (set-waterfall-parallelism
+                        ',curr-waterfall-parallelism-val)
+                       (value '(value-triple t)))))))
 
 (defmacro without-waterfall-parallelism (&rest events)
   `(make-event (without-waterfall-parallelism-fn ',events state)))
