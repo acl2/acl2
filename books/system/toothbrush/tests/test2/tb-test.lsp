5cda1d42b944e8b1f16f8fe75bd939196c42e3c3
MattKaufmann <matthew.j.kaufmann@gmail.com>
2014-11-19 16:48:51 -0600

    Updated toothbrush Makefiles to use TB_LISP instead of LISP and to avoid errors when using certain shells.
    Jared Davis reported inadvertent attempts to do toothbrush testing
    caused by his (quite reasonably) setting LISP, so I changed that
    (thanks, Jared).
    
    The shell issue was reported by David Rager (thanks).
    
    Added role of acl2r.lisp to README.
    
    Extended test2 so that it checks that system functions are
    automatically defined as needed.  Thanks to Keshav Kini for asking a
    question in the ACL2 seminar that led to this extension.

--- books/system/toothbrush/tests/test2/tb-test.lsp
+++ books/system/toothbrush/tests/test2/tb-test.lsp
@@ -4,7 +4,8 @@
 
 (in-package "ACL2")
 
-(let ((st (update-fld 3 *the-live-st*)))
-  (cond ((equal (fld st) 3)
+(let ((st (my-update 3 *the-live-st*)))
+  (cond ((and (equal (fld1 st) 3)
+              (equal (fld2 st) 'a-changed))
          (print "SUCCESS"))
         (t (print "FAILURE"))))
--- books/system/toothbrush/tests/test2/test2.lisp
+++ books/system/toothbrush/tests/test2/test2.lisp
@@ -2,10 +2,30 @@
 ; Written by Matt Kaufmann, November, 2014
 ; License: A 3-clause BSD license.  See the LICENSE file distributed with ACL2.
 
+; Here we test stobjs.  We also check that generated toothbrush application
+; files include ACL2 definitions of system functions not included in the system
+; toothbrush files.
+
 (in-package "ACL2")
 
-(defstobj st fld)
+(defstobj st fld1 fld2)
 
-(defun top (st)
+(verify-termination symbol-name-equal) ; and guards
+
+(defun foo (y)
+  (declare (xargs :guard t))
+  (if (symbol-name-equal y "A")
+      'a-changed
+    y))
+
+(defun my-update (x st)
+
+; Note: symbol-name-equal is defined in ACL2 source file other-events.lisp.
+
   (declare (xargs :stobjs st))
-  (update-fld 3 st))
+  (let ((st (update-fld1 (foo x) st)))
+    (update-fld2 (foo 'a) st)))
+
+(defun top (x st)
+  (declare (xargs :stobjs st))
+  (my-update x st))
