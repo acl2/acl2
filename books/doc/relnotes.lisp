f695e0fea90b7f34ec915737abaf5d25c2d7f5da
Matt Kaufmann <kaufmann@cs.utexas.edu>
2014-11-24 19:30:22 -0600

    Tweaked :doc note-7-0-books to clarify that all legacy doc strings are gone.  Fixed a .gitignore for toothbrush to apply to all test subdirs.

--- books/doc/relnotes.lisp
+++ books/doc/relnotes.lisp
@@ -194,7 +194,7 @@ functional instantiation.</p>
 <p>The new tool, @(see def-saved-obligs), can be used to save proof obligations
 for an event as independent defthms.</p>
 
-<p>Many @(see legacy-documentation) topics have been rewritten into the @(see
+<p>All @(see legacy-documentation) topics have been rewritten into the @(see
 xdoc) format, as a step towards eventually eliminating the legacy documentation
 system.  This notably includes the @(see data-structures) and @(see ihs)
 libraries.</p>
