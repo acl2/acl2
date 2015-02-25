9e620dc21226e80b2768092d3f10c2fd8976e59b
Matt Kaufmann <kaufmann@cs.utexas.edu>
2015-02-25 11:02:50 -0600

    Trivially update purity script checking date, for performance.

--- bin/purity.sh
+++ bin/purity.sh
@@ -12,7 +12,7 @@ fi
 export PAGER=cat
 
 # Matt might update this when confident of purity since the indicated date.
-export SINCE=2015-01-13
+export SINCE=2015-02-15
 
 export basecmd="\
 git log \
