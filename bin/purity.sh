117adce81c25b96e35f1243e1215897f7fdf884f
Matt Kaufmann <kaufmann@cs.utexas.edu>
2015-04-18 13:04:48 -0500

    Speed up purity.sh

--- bin/purity.sh
+++ bin/purity.sh
@@ -12,7 +12,7 @@ fi
 export PAGER=cat
 
 # Matt might update this when confident of purity since the indicated date.
-export SINCE=2015-02-15
+export SINCE=2015-04-10
 
 export basecmd="\
 git log \
