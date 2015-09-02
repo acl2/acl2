4a448c19cc89e438a501e132399a4805d7ee8173
Cuong Kim Chau <ckcuong@cs.utexas.edu>
2015-09-02 11:27:18 -0500

    Added fourier-inner-product.lisp file to Fourier series formalization.

--- books/workshops/2015/chau-kaufmann-hunt/support/fourier-sums.lisp
+++ books/workshops/2015/chau-kaufmann-hunt/support/fourier-sums.lisp
@@ -1,6 +1,6 @@
 ;; Proof of the uniqueness of Fourier sums.
 
-;; Cuong Chau                             March, 2015
+;; Cuong Chau                             August, 2015
 
 ;; === OVERVIEW ===
 
@@ -40,63 +40,63 @@
     (implies (and (equal (a 0) (r 0))
                   (<= i n)
                   (uniqueness-of-fourier-sums-necc-cond n))
-             (equal (fourier-sum x i c)
-                    (fourier-sum-2 x i c)))
-    :hints (("Goal" :in-theory (enable fourier-sum
-                                       fourier-sum-2)))))
+             (equal (fourier-series-1 x i c)
+                    (fourier-series-2 x i c)))
+    :hints (("Goal" :in-theory (enable fourier-series-1
+                                       fourier-series-2)))))
 
  (defthm uniqueness-of-fourier-sums-necc
    (implies (and (equal (a 0) (r 0))
                  (uniqueness-of-fourier-sums-necc-cond i))
-            (equal (fourier-sum x i c)
-                   (fourier-sum-2 x i c)))
+            (equal (fourier-series-1 x i c)
+                   (fourier-series-2 x i c)))
    :hints (("Goal" :use (:instance uniqueness-of-fourier-sums-necc-lemma
                                    (n i))))))
 
 (defun-sk uniqueness-of-fourier-sums-suff-cond (i c)
   (forall x
-          (equal (fourier-sum x i c)
-                 (fourier-sum-2 x i c)))
+          (equal (fourier-series-1 x i c)
+                 (fourier-series-2 x i c)))
   :rewrite :direct)
 
 (in-theory (disable uniqueness-of-fourier-sums-suff-cond))
 
 (local
  (defthm lemma-1
-   (implies (equal (fourier-sum x i c)
-                   (fourier-sum-2 x i c))
-            (and (equal (fourier-sum-*-fixed-cos x i k c)
-                        (fourier-sum-2-*-fixed-cos x i k c))
-                 (equal (fourier-sum-*-fixed-sin x i k c)
-                        (fourier-sum-2-*-fixed-sin x i k c))))
-   :hints (("Goal" :in-theory (enable fourier-sum-*-fixed-cos
-                                      fourier-sum-2-*-fixed-cos
-                                      fourier-sum-*-fixed-sin
-                                      fourier-sum-2-*-fixed-sin)))))
+   (implies (equal (fourier-series-1 x i c)
+                   (fourier-series-2 x i c))
+            (and (equal (fourier-series-1*fixed-cos x i k c)
+                        (fourier-series-2*fixed-cos x i k c))
+                 (equal (fourier-series-1*fixed-sin x i k c)
+                        (fourier-series-2*fixed-sin x i k c))))
+   :hints (("Goal" :in-theory (enable fourier-series-1*fixed-cos
+                                      fourier-series-2*fixed-cos
+                                      fourier-series-1*fixed-sin
+                                      fourier-series-2*fixed-sin)))))
 
 (local
  (defthm lemma-2
    (implies (uniqueness-of-fourier-sums-suff-cond i c)
-            (and (equal (map-fourier-sum-*-fixed-cos p i k c)
-                        (map-fourier-sum-2-*-fixed-cos p i k c))
-                 (equal (map-fourier-sum-*-fixed-sin p i k c)
-                        (map-fourier-sum-2-*-fixed-sin p i k c))))))
+            (and (equal (map-fourier-series-1*fixed-cos p i k c)
+                        (map-fourier-series-2*fixed-cos p i k c))
+                 (equal (map-fourier-series-1*fixed-sin p i k c)
+                        (map-fourier-series-2*fixed-sin p i k c))))))
 
 (local
  (defthm-std lemma-3
    (implies (uniqueness-of-fourier-sums-suff-cond i c)
-            (and (equal (int-fourier-sum-*-fixed-cos a b i k c)
-                        (int-fourier-sum-2-*-fixed-cos a b i k c))
-                 (equal (int-fourier-sum-*-fixed-sin a b i k c)
-                        (int-fourier-sum-2-*-fixed-sin a b i k c))))
-   :hints (("Goal" :in-theory (enable riemann-fourier-sum-*-fixed-cos
-                                      int-fourier-sum-*-fixed-cos
-                                      riemann-fourier-sum-2-*-fixed-cos
-                                      int-fourier-sum-2-*-fixed-cos
-                                      riemann-fourier-sum-*-fixed-sin
-                                      int-fourier-sum-*-fixed-sin
-                                      riemann-fourier-sum-2-*-fixed-sin
-                                      int-fourier-sum-2-*-fixed-sin)))))
+            (and (equal (int-fourier-series-1*fixed-cos a b i k c)
+                        (int-fourier-series-2*fixed-cos a b i k c))
+                 (equal (int-fourier-series-1*fixed-sin a b i k c)
+                        (int-fourier-series-2*fixed-sin a b i k c))))
+   :hints (("Goal" :in-theory (enable riemann-fourier-series-1*fixed-cos
+                                      int-fourier-series-1*fixed-cos
+                                      riemann-fourier-series-2*fixed-cos
+                                      int-fourier-series-2*fixed-cos
+                                      riemann-fourier-series-1*fixed-sin
+                                      int-fourier-series-1*fixed-sin
+                                      riemann-fourier-series-2*fixed-sin
+                                      int-fourier-series-2*fixed-sin)))))
 
 (defthm uniqueness-of-fourier-sums-suff
   (implies (and (uniqueness-of-fourier-sums-suff-cond i (/ (acl2-pi) L))
@@ -109,21 +109,21 @@
                 (equal (a k) (r k))
                 (equal (b k) (s k))))
   :hints (("Goal"
-           :in-theory (disable int-fourier-sum-*-fixed-cos-thm-2
-                               int-fourier-sum-2-*-fixed-cos-thm-2
-                               int-fourier-sum-*-fixed-sin-thm-2
-                               int-fourier-sum-2-*-fixed-sin-thm-2
-                               int-fourier-sum-*-fixed-cos-thm-3
-                               int-fourier-sum-2-*-fixed-cos-thm-3
-                               int-fourier-sum-*-fixed-sin-thm-3
-                               int-fourier-sum-2-*-fixed-sin-thm-3)
-           :use ((:instance int-fourier-sum-*-fixed-cos-thm-2)
-                 (:instance int-fourier-sum-2-*-fixed-cos-thm-2)
-                 (:instance int-fourier-sum-*-fixed-sin-thm-2)
-                 (:instance int-fourier-sum-2-*-fixed-sin-thm-2)
-                 (:instance int-fourier-sum-*-fixed-cos-thm-3)
-                 (:instance int-fourier-sum-2-*-fixed-cos-thm-3)
-                 (:instance int-fourier-sum-*-fixed-sin-thm-3)
-                 (:instance int-fourier-sum-2-*-fixed-sin-thm-3)))))
+           :in-theory (disable int-fourier-series-1*fixed-cos-thm-2
+                               int-fourier-series-2*fixed-cos-thm-2
+                               int-fourier-series-1*fixed-sin-thm-2
+                               int-fourier-series-2*fixed-sin-thm-2
+                               int-fourier-series-1*fixed-cos-thm-3
+                               int-fourier-series-2*fixed-cos-thm-3
+                               int-fourier-series-1*fixed-sin-thm-3
+                               int-fourier-series-2*fixed-sin-thm-3)
+           :use (int-fourier-series-1*fixed-cos-thm-2
+                 int-fourier-series-2*fixed-cos-thm-2
+                 int-fourier-series-1*fixed-sin-thm-2
+                 int-fourier-series-2*fixed-sin-thm-2
+                 int-fourier-series-1*fixed-cos-thm-3
+                 int-fourier-series-2*fixed-cos-thm-3
+                 int-fourier-series-1*fixed-sin-thm-3
+                 int-fourier-series-2*fixed-sin-thm-3))))
 
 
