2c6cb54aaec7be482963c7e4eeea24a9493e2eb1
David L. Rager <david.rager@oracle.com>
2015-09-02 11:51:44 -0500

    Fixing bug in VL[2014] lineup transform msg

--- books/centaur/vl2014/transforms/unparam/lineup.lisp
+++ books/centaur/vl2014/transforms/unparam/lineup.lisp
@@ -208,7 +208,7 @@
              (mv nil
                  (fatal :type :vl-bad-instance
                         :msg "~a0: too many parameter values: ~x1 (non-local) ~
-                              parameter~s2, but is given ~x3 parameter argument~s5."
+                              parameter~s2, but is given ~x3 parameter argument~s4."
                         :args (list ctx
                                     num-formals
                                     (if (eql num-formals 1) "" "s")
