ca0966f26e3f4f0563d32b20e39f20e77212b1fe
Matt Kaufmann <kaufmann@cs.utexas.edu>
2015-11-13 14:14:46 -0600

    At the request of David Russinoff, updated books/projects/quadratic-reciprocity/ and added books/projects/shnf/.

--- books/projects/quadratic-reciprocity/support/mersenne.lisp
+++ books/projects/quadratic-reciprocity/support/mersenne.lisp
@@ -1,14 +1,6 @@
-;; David M. Russinoff
-;; david@russinoff.com
-;; http://www.russinoff.com
-
 (in-package "RTL")
 
-(include-book "support/mersenne")
-
-(set-enforce-redundancy t)
-(set-inhibit-warnings "theory") ; avoid warning in the next event
-(local (in-theory nil))
+(local (include-book "arithmetic-5/top" :dir :system)) ;; It's hard to do any arithmetic without something like this
 
 ;; This book illustrates a simple application of the theory of quadratic
 ;; residues to the Mersenne prime problem.  We show that certain Mersenne
@@ -104,9 +96,77 @@
 
 (comp t)
 
+(local-defthm hack-1
+  (implies (and (not (zp a)) (not (zp b)) (not (zp c)) (not (zp d))
+                (<= a c) (<= b d))
+           (<= (* a b) (* c b)))
+  :rule-classes ())
+
+(local-defthm hack-2
+  (implies (and (not (zp a)) (not (zp b)) (not (zp c)) (not (zp d))
+                (<= a c) (<= b d))
+           (<= (* c b) (* c d)))
+  :rule-classes ())
+
+(local-defthm hack-3
+  (implies (and (not (zp a)) (not (zp b)) (not (zp c)) (not (zp d))
+                (<= a c) (<= b d))
+           (<= (* a b) (* c d)))
+  :hints (("Goal" :use (hack-1 hack-2)
+                  :in-theory (theory 'minimal-theory))))
+
+(local-defthm hack-4
+  (implies (and (not (zp k)) (not (zp ld)) (<= k ld))
+           (<= (* k k) (* ld ld)))
+  :hints (("Goal" :use ((:instance hack-3 (a k) (b k) (c ld) (d ld)))))
+  :rule-classes ())
+
+(defthm least-divisor-rewrite-lemma-1
+    (implies (and (integerp n)
+		  (integerp k)
+		  (< 1 k)
+		  (<= k n)
+		  (<= (* (least-divisor k n) (least-divisor k n)) n))
+	     (equal (least-divisor k n)
+		    (least-divisor-2 k n)))
+  :rule-classes ()
+  :hints (("Subgoal *1/7" :use (least-divisor-divides (:instance hack-4 (ld (least-divisor (1+ k) n)))))
+	  ("Subgoal *1/1" :use (least-divisor-divides (:instance hack-4 (ld (least-divisor (1+ k) n)))))))
+
+(local-defthm hack-5
+  (implies (and (not (zp a)) (not (zp n))
+                (<= 1 (/ n a)))
+           (<= a n))
+  :rule-classes ())
+
+(defthm least-divisor-rewrite-lemma-2
+    (implies (and (integerp n)
+		  (> n 1)
+		  (not (= n (least-divisor 2 n))))
+	     (<= (* (least-divisor 2 n) (least-divisor 2 n)) n))
+  :rule-classes ()
+  :hints (("Goal" :in-theory (enable divides)
+		  :use ((:instance least-divisor-divides (k 2))
+			(:instance least-divisor-is-least (k 2) (d (/ n (least-divisor 2 n))))
+                        (:instance hack-5 (a (* (least-divisor 2 n) (least-divisor 2 n))))))))
+
+(defthm least-divisor-rewrite-lemma-3
+    (implies (and (integerp n)
+		  (integerp k)
+		  (< 1 k)
+		  (<= k n)
+		  (= n (least-divisor k n)))
+	     (= n (least-divisor-2 k n)))
+  :rule-classes ()
+  :hints (("Goal" :use (least-divisor-divides
+			(:instance least-divisor-is-least (d (least-divisor-2 2 n)))))))
+
 (defthm least-divisor-rewrite
     (equal (least-divisor 2 n)
-	   (least-divisor-2 2 n)))
+	   (least-divisor-2 2 n))
+  :hints (("Goal" :use (least-divisor-rewrite-lemma-2
+			(:instance least-divisor-rewrite-lemma-3 (k 2))
+			(:instance least-divisor-rewrite-lemma-1 (k 2))))))
 
 ;; In order to arrange that the more efficient variant is executed, we must
 ;; disable the executable counterparts of primep and least-proper-divisor
@@ -140,17 +200,64 @@
 		  (= (mod p 4) 3)
 		  (primep (1+ (* 2 p))))
 	     (divides (1+ (* 2 p)) (m p)))
-  :rule-classes ())
+  :rule-classes ()
+  :hints (("Goal" :use ((:instance second-supplement (p (1+ (* 2 p))))
+			(:instance euler-criterion (m 2) (p (1+ (* 2 p))))
+			(:instance mod-sum (a 1) (b (* 2 p)) (n 8))
+			(:instance mod-prod (k 2) (m p) (n 4))
+			(:instance divides-leq (x (1+ (* 2 p))) (y 2))
+			(:instance divides-mod-equal (a (expt 2 p)) (b 1) (n (1+ (* 2 p))))))))
 
 ;; With the exception of p = 3 (in which case Mp = 2*p+1), Mp is composite
 ;; for any p satisfying the hypothesis of theorem-103:
+
+(local-defthm hack-6
+  (implies (and (not (zp a))
+                (>= a (* 4 (1- p)))
+                (integerp p)
+		(> p 3))
+	     (>= (* 2 a) (* 4 p)))
+  :rule-classes ())
+
+(local-defthm hack-7
+  (implies (and (>= (expt 2 (1- p)) (* 4 (1- p)))
+                (integerp p)
+		(> p 3))
+	     (>= (expt 2 p) (* 4 p)))
+  :rule-classes ()
+  :hints (("Goal" :use ((:instance hack-6 (a (expt 2 (1- p))))))))
+
+(local-defthm hack-8
+  (implies (and (integerp p)
+		(> p 3))
+	     (>= (expt 2 p) (* 4 p)))
+  :rule-classes ()
+  :hints (("Goal" :induct (fact p))
+          ("Subgoal *1/2" :use hack-7)))
+
+(local-defthm hack-9
+    (implies (and (integerp p)
+		  (> p 3))
+	     (> (* 4 p) (* 2 (1+ p))))
+  :rule-classes ())
+
+(defthm expt-2-p-bnd
+    (implies (and (integerp p)
+		  (> p 3))
+	     (> (expt 2 p) (* 2 (1+ p))))
+  :rule-classes ()
+  :hints (("Goal" :use (hack-8 hack-9)
+                  :in-theory (theory 'minimal-theory))))
 
 (defthm theorem-103-corollary
     (implies (and (primep p)
 		  (= (mod p 4) 3)
 		  (> p 3)
 		  (primep (1+ (* 2 p))))
-	     (not (primep (m p)))))
+	     (not (primep (m p))))
+  :hints (("Goal" :use (theorem-103
+			expt-2-p-bnd
+			(:instance primep-no-divisor (p (1- (expt 2 p))) (d (1+ (* 2 p))))))))
 
 ;; In order to arrange for theorem-103-corollary to be used as a
 ;; rewrite rule, we must disable the logical expansion of both
