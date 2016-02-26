d053ee402b5f0513ef92950d660ca4a7ef20c8c2
Alessandro Coglio <coglio@kestrel.edu>
2016-02-25 18:32:54 -0800

    Add tests for MEASURE and WELL-FOUNDED-RELATION.
    These are two system utilities that were recently added.

--- books/kestrel/system/world-queries-tests.lisp
+++ books/kestrel/system/world-queries-tests.lisp
@@ -1,6 +1,6 @@
 ; World Queries -- Tests
 ;
-; Copyright (C) 2015 Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2015-2016 Kestrel Institute (http://www.kestrel.edu)
 ;
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 ;
@@ -108,6 +108,31 @@
 (must-succeed*
  (defthm th (acl2-numberp (+ x y)))
  (assert-event (not (guard-verifiedp 'th (w state)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(assert-event (equal (measure 'len (w state)) '(acl2-count x)))
+
+(must-succeed*
+ (defun f (x)
+   (declare (xargs :measure (nfix (- 10 x))))
+   (if (and (natp x) (< x 10))
+       (f (1+ x))
+     nil))
+ (assert-event (equal (measure 'f (w state))
+                      '(nfix (binary-+ '10 (unary-- x))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(assert-event (equal (well-founded-relation 'len (w state)) 'o<))
+
+(must-succeed*
+ (defun f (x)
+   (declare (xargs :measure (nfix (- 10 x))))
+   (if (and (natp x) (< x 10))
+       (f (1+ x))
+     nil))
+ (assert-event (equal (well-founded-relation 'f (w state)) 'o<)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
