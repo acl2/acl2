5f9bfc32e67604f8f524960df6003d0b77abf938
Alessandro Coglio <coglio@kestrel.edu>
2016-06-18 17:45:43 -0700

    Clarify documentation of MUST-SUCCEED* and MUST-SUCCEED**.
    The argument forms must be embedded event forms (unlike MUST-SUCCEED, whose
    argument form may be anything returning an error triple), so that earlier forms
    can affect later forms in the sequence.

--- books/kestrel/general/testing.lisp
+++ books/kestrel/general/testing.lisp
@@ -30,7 +30,21 @@
   that takes multiple forms
   and uses default options."
 
-  :long "@(def must-succeed*)"
+  :long
+  "<p>
+  The forms must be
+  <see topic='@(url embedded-event-form)'>embedded event forms</see>,
+  because they are put into a @(tsee progn)
+  so that earlier forms can affect later forms in the sequence.
+  This is a difference with @(tsee must-succeed),
+  whose form is required to return
+  an <see topic='@(url error-triple)'>error triple</see>
+  without necessarily being an embedded event form;
+  since @(tsee must-succeed) takes only one form,
+  there is no issue of earlier forms affecting later forms
+  as in @(tsee must-succeed*).
+  </p>
+  @(def must-succeed*)"
 
   (defmacro must-succeed* (&rest forms)
     `(must-succeed (progn ,@forms))))
@@ -52,6 +66,11 @@
   the @(':with-output-off') and @(':check-expansion') options
   of @(tsee must-succeed).
   The remaining arguments are the forms.
+  </p>
+  <p>
+  As in @(tsee must-succeed*), the forms must be
+  <see topic='@(url embedded-event-form)'>embedded event forms</see>.
+  See the discussion for @(tsee must-succeed*) in this regard.
   </p>
   @(def must-succeed**)"
 
