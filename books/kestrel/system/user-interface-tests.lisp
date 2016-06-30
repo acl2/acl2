39cd65f4aafcf1f38e5f21d45c26887078f8ed10
Alessandro Coglio <coglio@kestrel.edu>
2016-06-30 15:59:41 -0700

    Add new system utilities.
    These are utilities for the user interface of macro libraries.

---
+++ books/kestrel/system/user-interface-tests.lisp
@@ -0,0 +1,65 @@
+; User Interface -- Tests
+;
+; Copyright (C) 2016 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Authors: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; This file contains tests for the user interface utilities
+; in user-interface.lisp.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+(include-book "user-interface")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(encapsulate
+  ()
+  (local
+   (defmacro m () (suppress-output '(make-event '(defun f (x) x)))))
+  (local (m)))
+
+(encapsulate
+  ()
+  (local
+   (defmacro m() '(make-event '(defun f (x) x))))
+  (local (m)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(encapsulate
+  ()
+  (local
+   (defmacro m () (maybe-suppress-output t '(make-event '(defun f (x) x)))))
+  (local (m)))
+
+(encapsulate
+  ()
+  (local
+   (defmacro m () (maybe-suppress-output nil '(make-event '(defun f (x) x)))))
+  (local (m)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(encapsulate
+  ()
+  (local
+   (defmacro m () (control-screen-output nil '(make-event '(defun f (x) x)))))
+  (local (m)))
+
+(encapsulate
+  ()
+  (local
+   (defmacro m () (control-screen-output t '(make-event '(defun f (x) x)))))
+  (local (m)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(progn
+  (cw-event "Message"))
