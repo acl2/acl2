f14d2bc18072c95cab6b8e2cc9d233982edc3785
Alessandro Coglio <coglio@kestrel.edu>
2016-07-12 14:38:30 -0700

    Uniform documentation of system utility.
    For consistency with other utilities, capitalize and end with period every
    :SHORT and every argument documentation string.

--- books/kestrel/system/user-interface.lisp
+++ books/kestrel/system/user-interface.lisp
@@ -78,12 +78,13 @@
               ,form))))
 
 (define control-screen-output-and-maybe-replay
-  ((verbose "t or nil, else indicates replay on failure")
-   (event-p "make an event when true")
+  ((verbose "@('T') or @('nil'), else indicates replay on failure.")
+   (event-p "Make an event when true.")
    (form (pseudo-event-formp form)))
   :returns (new-form pseudo-event-formp :hyp (pseudo-event-formp form))
   :short
-  "Variant of @(tsee control-screen-output) that can replay a failure verbosely"
+  "Variant of @(tsee control-screen-output)
+  that can replay a failure verbosely."
   :long
   "<p>Usage:</p>
 
