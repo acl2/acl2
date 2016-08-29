cfea10db6c99b30213a73d305d1a6ba0d4f913c5
Alessandro Coglio <coglio@kestrel.edu>
2016-08-29 14:40:11 -0700

    Make file description more specific.

--- books/kestrel/utilities/theorems/top.lisp
+++ books/kestrel/utilities/theorems/top.lisp
@@ -8,7 +8,8 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; This file provides some miscellaneous theorems.
+; This file provides some theorems
+; about functions defined outside the Kestrel Books.
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
