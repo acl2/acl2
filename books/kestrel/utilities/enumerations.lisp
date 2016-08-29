f3124549ea191b4302c40a3927a5de40b6b3a92f
Alessandro Coglio <coglio@kestrel.edu>
2016-08-29 14:42:40 -0700

    Split type.lisp into separate files.

---
+++ books/kestrel/utilities/enumerations.lisp
@@ -0,0 +1,35 @@
+; Miscellaneous Enumerations
+;
+; Copyright (C) 2015-2016 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; This file provides some enumerations.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+(include-book "std/util/defenum" :dir :system)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defxdoc miscellaneous-enumerations
+  :parents (kestrel-utilities)
+  :short "Some miscellaneous enumerations.")
+
+(std::defenum logic/program-p (:logic :program)
+  :parents (miscellaneous-enumerations)
+  :short "Enumeration of @(see defun-mode)s.")
+
+(std::defenum logic/program/auto-p (:logic :program :auto)
+  :parents (miscellaneous-enumerations)
+  :short "Enumeration of @(see defun-mode)s and @(':auto').")
+
+(std::defenum t/nil/auto-p (t nil :auto)
+  :parents (miscellaneous-enumerations)
+  :short "Enumeration of booleans and @(':auto').")
--- books/kestrel/utilities/types.lisp
+++ books/kestrel/utilities/types.lisp
@@ -14,40 +14,5 @@
 
 (in-package "ACL2")
 
-(include-book "std/util/defalist" :dir :system)
-(include-book "std/util/defenum" :dir :system)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defxdoc miscellaneous-types
-  :parents (kestrel-utilities)
-  :short "Some miscellaneous types.")
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(std::defalist symbol-symbol-alistp (x)
-  :key (symbolp x)
-  :val (symbolp x)
-  :parents (miscellaneous-types alists)
-  :short "Recognize @('nil')-terminated alists from symbols to symbols."
-  :keyp-of-nil t
-  :valp-of-nil t
-  :true-listp t)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(std::defenum logic/program-p (:logic :program)
-  :parents (miscellaneous-types)
-  :short "Recognize @(see defun-mode)s.")
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(std::defenum logic/program/auto-p (:logic :program :auto)
-  :parents (miscellaneous-types)
-  :short "Recognize @(see defun-mode)s and @(':auto').")
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(std::defenum t/nil/auto-p (t nil :auto)
-  :parents (miscellaneous-types)
-  :short "Recognize booleans and @(':auto').")
+(include-book "enumerations")
+(include-book "symbol-symbol-alists")
