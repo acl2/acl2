4faaa3fd1b9be464e4989cbe466d78ae3d5fe68b
Shilpi Goel <shigoel@cs.utexas.edu>
2016-09-01 14:32:26 -0500

    x86isa: Added documentation about which rules to monitor in all the modes of operation to debug failed proof attempts.
    See :doc x86-programs-proof-debugging for details.

--- books/projects/x86isa/proofs/dissertation-examples/clc-stc-programmer-mode.lisp
+++ books/projects/x86isa/proofs/dissertation-examples/clc-stc-programmer-mode.lisp
@@ -52,4 +52,46 @@
                                    (create-canonical-address-list
                                     (create-canonical-address-list))))))
 
+
+#||
+
+;; An illustration of how we discover preconditions:
+
+(defun-nx preconditions-incomplete (x86)
+  (and
+   ;; The x86 state is well-formed.
+   (x86p x86)
+   ;; The model is operating in the programmer-level mode.
+   (programmer-level-mode x86)
+   ;; The program is located at linear addresses ranging from (rip
+   ;; x86) to (+ -1 (len *program*) (rip x86)).
+   (program-at (create-canonical-address-list (len *program*) (rip x86))
+               *program* x86)
+   ;; The addresses where the program is located are canonical.
+   (canonical-address-p (rip x86))
+   (canonical-address-p (+ -1 (len *program*) (rip x86)))
+   ;; The initial state is error-free.
+   (equal (ms x86) nil)
+   (equal (fault x86) nil)))
+
+(acl2::why x86-fetch-decode-execute-opener)
+
+(defthm program-effects-2
+  (implies (preconditions-incomplete x86)
+           (equal (x86-run 2 x86)
+                  xxx))
+  :hints (("Goal" :in-theory (e/d* (x86-cmc/clc/stc/cld/std)
+                                   (create-canonical-address-list
+                                    (create-canonical-address-list))))))
+
+;; Monitoring this rule tells us that our preconditions are incomplete
+;; because they only state that addresses (rip x86) to (+ -1 (len
+;; *program*) (rip x86)) are canonical.  We also need (+ 2 (rip x86))
+;; to be canonical because the second instruction advances the
+;; instruction pointer to point to this address.  So we modify the
+;; preconditions appropriately.
+
+
+||#
+
 ;; ======================================================================
