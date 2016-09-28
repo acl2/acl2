4d8c221493b5aac2c4a23d57221f30d6c22b1add
Matt Kaufmann <kaufmann@cs.utexas.edu>
2016-09-28 08:26:53 -0500

    Added :doc for dft, using words from J about his dft utility for "proof-checking-like macro that allows you to chain together equalities".

--- books/doc/top.lisp
+++ books/doc/top.lisp
@@ -234,6 +234,7 @@
 (include-book "misc/defpun" :dir :system)
 (include-book "misc/install-not-normalized" :dir :system)
 (include-book "misc/defmac" :dir :system)
+(include-book "misc/dft" :dir :system)
 
 (include-book "make-event/proof-by-arith" :dir :system)
 
