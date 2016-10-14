6c9353f48e69c17b5511223d56ec61c07327c8eb
Keshav Kini <keshav.kini@oracle.com>
2016-10-13 17:10:31 -0700

    Verilog indentation

--- books/centaur/sv/tutorial/lib/flop.v
+++ books/centaur/sv/tutorial/lib/flop.v
@@ -30,12 +30,12 @@
 //                   Sol Swords <sswords@centtech.com>
 
 module flop (q, d, clk);
-   parameter width = 1;
+  parameter width = 1;
 
   output reg [width-1:0] q;
   input [width-1:0] d;
   input clk;
 
-   always @(posedge clk) q <= d;
+  always @(posedge clk) q <= d;
 
 endmodule // flop
