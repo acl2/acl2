ffff1d6f81592566b2fee3eae3df3baaba561344
Matt Kaufmann <kaufmann@cs.utexas.edu>
2016-12-05 09:53:00 -0600

    Removed note-7-3-books.  Made some comment mods for ACL2_COMP in books/GNUmakefile.  Updated date in purity script.
    We can restore note-7-3-books if someone volunteers to complete that
    before the end of this week.

--- bin/purity.sh
+++ bin/purity.sh
@@ -12,7 +12,7 @@ fi
 export PAGER=cat
 
 # Matt might update this when confident of purity since the indicated date.
-export SINCE=2016-10-16
+export SINCE=2016-12-04
 
 export basecmd="\
 git log \
