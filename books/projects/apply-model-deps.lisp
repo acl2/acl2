ce6a794eb702e4d7dd95b9de2278edb8a2319ffb
Matt Kaufmann <matthew.j.kaufmann@gmail.com>
2017-03-02 16:47:35 -0600

    Modified certification procedure for books/projects/apply-model/.
    Thanks to Eric Smith for pointing out warnings that led me to re-work
    the infrastructure for certifying the apply-model/ books, and thanks
    to Sol Swords for adding the new --include-excludes option for cert.pl
    in support of this change.
    
    Fixed a few other related things while I was at it.

---
+++ books/projects/apply-model-deps.lisp
@@ -0,0 +1,10 @@
+;; Silly file to trick cert.pl into including the right books.
+
+(in-package "ACL2")
+
+#||
+(include-book "ordinals/lexicographic-ordering-without-arithmetic" :dir :system)
+(include-book "tools/flag" :dir :system)
+(include-book "arithmetic-5/top" :dir :system)
+(include-book "misc/eval" :dir :system)
+||#
