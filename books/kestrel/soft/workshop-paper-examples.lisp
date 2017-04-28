2ad1f487358bde25a7404e51bc73aceb7c65ac8b
Alessandro Coglio <coglio@kestrel.edu>
2017-04-28 16:44:50 -0700

    Update copyright year in file header.

--- books/kestrel/soft/workshop-paper-examples.lisp
+++ books/kestrel/soft/workshop-paper-examples.lisp
@@ -1,6 +1,6 @@
 ; SOFT ('Second-Order Functions and Theorems') -- Workshop Paper Examples
 ;
-; Copyright (C) 2015-2016 Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2015-2017 Kestrel Institute (http://www.kestrel.edu)
 ;
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 ;
