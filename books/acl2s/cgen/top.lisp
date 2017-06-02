6f3d3f65f993f56a0b9e5a64ce4afce6b7c36666
Harsh Raju Chamarthi <harshraju.chamarthi@ge.com>
2017-06-02 11:22:46 -0400

    minor changes to include-book parameters in various books. Also its better to have testing-enabled t since it adds a backtrack no-op hint in override-hints which can have wierd behavior when running this command in include-book context and otherwise causing other-wise redundant event to be non-redundant.

--- books/acl2s/cgen/top.lisp
+++ books/acl2s/cgen/top.lisp
@@ -464,8 +464,8 @@ information in a human-readable form. </p>
 "
 )
 
-
-
+;; Put the test-checkpoint no-op backtrack hint in the override-hints
+(acl2s-defaults :set testing-enabled t)
 
 
 ; [2016-04-03 Sun] Add fixers support to Cgen
