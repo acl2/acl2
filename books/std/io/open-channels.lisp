ea349409a08e875f0fff435a263d0e3d1d9caee2
Keshav Kini <keshav.kini@gmail.com>
2017-06-19 21:03:10 -0700

    xdoc-ify the std/io/open-channels book

--- books/std/io/open-channels.lisp
+++ books/std/io/open-channels.lisp
@@ -27,41 +27,8 @@
 
 (in-package "ACL2")
 
+(include-book "xdoc/top" :dir :system)
 (include-book "std/util/bstar" :dir :system)
-
-; OVERVIEW
-
-; When programming with I/O, you may need to write functions that
-; modify state in some way (perhaps by doing I/O) and exit returning a
-; state which has some channels open, perhaps for further reading and
-; writing later in your program.  Then, to prove guard theorems
-; elsewhere, you'll need returns theorems about your function showing
-; that it doesn't close those channels.
-
-; This book contains lemmas that can help you prove such returns
-; theorems.  For each built-in I/O function, there is a lemma saying
-; that, under appropriate hypotheses, it doesn't close your open input
-; or output channels.
-
-; Lemmas for other state-modifying builtins might be useful too, but I
-; figured I had to stop somewhere, so I decided to limit this book to
-; the functions mentioned in :DOC io.  I suppose you could add to this
-; book the corresponding lemmas about other built-in functions as
-; well, as you find yourself needing them.
-
-; Here are the builtins about which we're proving the lemmas:
-
-(local (in-theory (enable open-input-channel
-                          open-output-channel
-                          close-input-channel
-                          close-output-channel
-                          read-char$
-                          read-byte$
-                          read-object
-                          princ$
-                          write-byte$
-                          print-object$
-                          set-serialize-character)))
 
 
 ; Here is a small theory tweak we need:
@@ -79,7 +46,49 @@
                    (assoc-equal name1 alist)))))
 
 
-; And now here are the lemmas.
+; And now here is the main part of the book.
+
+(defsection open-channel-lemmas
+  :parents (std/io)
+  :short "Lemmas about when various @(see state)-modifying functions
+  do and don't affect the list of open channels."
+
+  :long
+  "<p>When programming with I/O, you may need to write functions that
+      modify state in some way (perhaps by doing I/O) and exit
+      returning a state which has some channels open, perhaps for
+      further reading and writing later in your program.  Then, to
+      prove guard theorems elsewhere, you'll need returns theorems
+      about your function showing that those channels are open, either
+      because they were open before the function ran and the function
+      didn't close them, or because the function itself opened
+      them.</p>
+
+   <p>This book contains lemmas that can help you prove such returns
+      theorems.  For each built-in I/O function, there is a lemma
+      saying that, under appropriate hypotheses, it doesn't close your
+      open input or output channels.</p>"
+
+; Here are the builtins about which we're proving the lemmas:
+
+  (local (in-theory (enable open-input-channel
+                            open-output-channel
+                            close-input-channel
+                            close-output-channel
+                            read-char$
+                            read-byte$
+                            read-object
+                            princ$
+                            write-byte$
+                            print-object$
+                            set-serialize-character)))
+
+; Lemmas for other state-modifying builtins might be useful too, but I
+; figured I had to stop somewhere, so I decided to limit this book to
+; the functions mentioned in :DOC io.  I suppose you could add to this
+; book the corresponding lemmas about other built-in functions as
+; well, as you find yourself needing them.
+
 
 ; NOTE: Several of the lemmas below have a weird hypothesis, marked
 ; with a (*).  These hypotheses should all be unconditionally true
@@ -94,150 +103,155 @@
 ;;;; Open input channels stay open
 ;;;;;;;;;;;;;;;;
 
-(defthm open-input-channel-p1-under-open-input-channel
-  ;; Desired version:
-  ;; (implies (and (open-input-channel-p1 channel type state)
-  ;;               (state-p1 state))
-  ;;          (b* (((mv & state)
-  ;;                (open-input-channel fname other-type state)))
-  ;;            (open-input-channel-p1 channel type state)))
-  (implies (open-input-channel-p1 channel type state)
-           (b* (((mv other-channel state)
-                 (open-input-channel fname other-type state)))
-             (implies (implies (equal other-channel channel) ; (*)
-                               (equal other-type type))
-                      (open-input-channel-p1 channel type state)))))
+  "<p>First are lemmas about @(tsee open-input-channel-p1).</p>"
 
-(defthm open-input-channel-p1-under-open-output-channel
-  (implies (open-input-channel-p1 channel type state)
-           (b* (((mv & state)
-                 (open-output-channel fname other-type state)))
-             (open-input-channel-p1 channel type state))))
+  (defthm open-input-channel-p1-under-open-input-channel
+    ;; Desired version:
+    ;; (implies (and (open-input-channel-p1 channel type state)
+    ;;               (state-p1 state))
+    ;;          (b* (((mv & state)
+    ;;                (open-input-channel fname other-type state)))
+    ;;            (open-input-channel-p1 channel type state)))
+    (implies (open-input-channel-p1 channel type state)
+             (b* (((mv other-channel state)
+                   (open-input-channel fname other-type state)))
+               (implies (implies (equal other-channel channel) ; (*)
+                                 (equal other-type type))
+                        (open-input-channel-p1 channel type state)))))
 
-(defthm open-input-channel-p1-under-close-input-channel
-  ;; Desired version:
-  ;; (implies (and (open-input-channel-p1 channel type state)
-  ;;               (state-p1 state))
-  ;;          (b* ((state (close-input-channel other-channel state)))
-  ;;            (iff (open-input-channel-p1 channel type state)
-  ;;                 (not (equal channel other-channel)))))
-  (implies (and (open-input-channel-p1 channel type state)
-                (not (equal channel other-channel))) ; (*)
-           (b* ((state (close-input-channel other-channel state)))
-             (open-input-channel-p1 channel type state))))
+  (defthm open-input-channel-p1-under-open-output-channel
+    (implies (open-input-channel-p1 channel type state)
+             (b* (((mv & state)
+                   (open-output-channel fname other-type state)))
+               (open-input-channel-p1 channel type state))))
 
-(defthm open-input-channel-p1-under-close-output-channel
-  (implies (open-input-channel-p1 channel type state)
-           (b* ((state (close-output-channel other-channel state)))
-             (open-input-channel-p1 channel type state))))
+  (defthm open-input-channel-p1-under-close-input-channel
+    ;; Desired version:
+    ;; (implies (and (open-input-channel-p1 channel type state)
+    ;;               (state-p1 state))
+    ;;          (b* ((state (close-input-channel other-channel state)))
+    ;;            (iff (open-input-channel-p1 channel type state)
+    ;;                 (not (equal channel other-channel)))))
+    (implies (and (open-input-channel-p1 channel type state)
+                  (not (equal channel other-channel))) ; (*)
+             (b* ((state (close-input-channel other-channel state)))
+               (open-input-channel-p1 channel type state))))
 
-(defthm open-input-channel-p1-under-read-char$
-  (implies (open-input-channel-p1 channel type state)
-           (b* (((mv & state) (read-char$ other-channel state)))
-             (open-input-channel-p1 channel type state))))
+  (defthm open-input-channel-p1-under-close-output-channel
+    (implies (open-input-channel-p1 channel type state)
+             (b* ((state (close-output-channel other-channel state)))
+               (open-input-channel-p1 channel type state))))
 
-(defthm open-input-channel-p1-under-read-byte$
-  (implies (open-input-channel-p1 channel type state)
-           (b* (((mv & state) (read-byte$ other-channel state)))
-             (open-input-channel-p1 channel type state))))
+  (defthm open-input-channel-p1-under-read-char$
+    (implies (open-input-channel-p1 channel type state)
+             (b* (((mv & state) (read-char$ other-channel state)))
+               (open-input-channel-p1 channel type state))))
 
-(defthm open-input-channel-p1-under-read-object
-  (implies (open-input-channel-p1 channel type state)
-           (b* (((mv & & state) (read-object other-channel state)))
-             (open-input-channel-p1 channel type state))))
+  (defthm open-input-channel-p1-under-read-byte$
+    (implies (open-input-channel-p1 channel type state)
+             (b* (((mv & state) (read-byte$ other-channel state)))
+               (open-input-channel-p1 channel type state))))
 
-(defthm open-input-channel-p1-under-princ$
-  (implies (open-input-channel-p1 channel type state)
-           (b* ((state (princ$ x other-channel state)))
-             (open-input-channel-p1 channel type state))))
+  (defthm open-input-channel-p1-under-read-object
+    (implies (open-input-channel-p1 channel type state)
+             (b* (((mv & & state) (read-object other-channel state)))
+               (open-input-channel-p1 channel type state))))
 
-(defthm open-input-channel-p1-under-write-byte$
-  (implies (open-input-channel-p1 channel type state)
-           (b* ((state (write-byte$ byte other-channel state)))
-             (open-input-channel-p1 channel type state))))
+  (defthm open-input-channel-p1-under-princ$
+    (implies (open-input-channel-p1 channel type state)
+             (b* ((state (princ$ x other-channel state)))
+               (open-input-channel-p1 channel type state))))
 
-(defthm open-input-channel-p1-under-print-object$
-  (implies (open-input-channel-p1 channel type state)
-           (b* ((state (print-object$ byte other-channel state)))
-             (open-input-channel-p1 channel type state))))
+  (defthm open-input-channel-p1-under-write-byte$
+    (implies (open-input-channel-p1 channel type state)
+             (b* ((state (write-byte$ byte other-channel state)))
+               (open-input-channel-p1 channel type state))))
 
-(defthm open-input-channel-p1-under-set-serialize-character
-  (implies (open-input-channel-p1 channel type state)
-           (b* ((state (set-serialize-character c state)))
-             (open-input-channel-p1 channel type state))))
+  (defthm open-input-channel-p1-under-print-object$
+    (implies (open-input-channel-p1 channel type state)
+             (b* ((state (print-object$ byte other-channel state)))
+               (open-input-channel-p1 channel type state))))
+
+  (defthm open-input-channel-p1-under-set-serialize-character
+    (implies (open-input-channel-p1 channel type state)
+             (b* ((state (set-serialize-character c state)))
+               (open-input-channel-p1 channel type state))))
+
 
 ;;;;;;;;;;;;;;;;
 ;;;; Open output channels stay open
 ;;;;;;;;;;;;;;;;
 
-(defthm open-output-channel-p1-under-open-input-channel
-  (implies (open-output-channel-p1 channel type state)
-           (b* (((mv & state)
-                 (open-input-channel fname other-type state)))
-             (open-output-channel-p1 channel type state))))
+  "<p>Next are lemmas about @(tsee open-output-channel-p1).</p>"
 
-(defthm open-output-channel-p1-under-open-output-channel
-  ;; Desired version:
-  ;; (implies (and (open-output-channel-p1 channel type state)
-  ;;               (state-p1 state))
-  ;;          (b* (((mv & state)
-  ;;                (open-output-channel fname other-type state)))
-  ;;            (open-output-channel-p1 channel type state)))
-  (implies (open-output-channel-p1 channel type state)
-           (b* (((mv other-channel state)
-                 (open-output-channel fname other-type state)))
-             (implies (implies (equal other-channel channel) ; (*)
-                               (equal other-type type))
-                      (open-output-channel-p1 channel type state)))))
+  (defthm open-output-channel-p1-under-open-input-channel
+    (implies (open-output-channel-p1 channel type state)
+             (b* (((mv & state)
+                   (open-input-channel fname other-type state)))
+               (open-output-channel-p1 channel type state))))
 
-(defthm open-output-channel-p1-under-close-input-channel
-  (implies (open-output-channel-p1 channel type state)
-           (b* ((state (close-input-channel other-channel state)))
-             (open-output-channel-p1 channel type state))))
+  (defthm open-output-channel-p1-under-open-output-channel
+    ;; Desired version:
+    ;; (implies (and (open-output-channel-p1 channel type state)
+    ;;               (state-p1 state))
+    ;;          (b* (((mv & state)
+    ;;                (open-output-channel fname other-type state)))
+    ;;            (open-output-channel-p1 channel type state)))
+    (implies (open-output-channel-p1 channel type state)
+             (b* (((mv other-channel state)
+                   (open-output-channel fname other-type state)))
+               (implies (implies (equal other-channel channel) ; (*)
+                                 (equal other-type type))
+                        (open-output-channel-p1 channel type state)))))
 
-(defthm open-output-channel-p1-under-close-output-channel
-  ;; Desired version:
-  ;; (implies (and (open-output-channel-p1 channel type state)
-  ;;               (state-p1 state))
-  ;;          (b* ((state (close-output-channel other-channel state)))
-  ;;            (iff (open-output-channel-p1 channel type state)
-  ;;                 (not (equal channel other-channel)))))
-  (implies (and (open-output-channel-p1 channel type state)
-                (not (equal channel other-channel))) ; (*)
-           (b* ((state (close-output-channel other-channel state)))
-             (open-output-channel-p1 channel type state))))
+  (defthm open-output-channel-p1-under-close-input-channel
+    (implies (open-output-channel-p1 channel type state)
+             (b* ((state (close-input-channel other-channel state)))
+               (open-output-channel-p1 channel type state))))
 
-(defthm open-output-channel-p1-under-read-char$
-  (implies (open-output-channel-p1 channel type state)
-           (b* (((mv & state) (read-char$ other-channel state)))
-             (open-output-channel-p1 channel type state))))
+  (defthm open-output-channel-p1-under-close-output-channel
+    ;; Desired version:
+    ;; (implies (and (open-output-channel-p1 channel type state)
+    ;;               (state-p1 state))
+    ;;          (b* ((state (close-output-channel other-channel state)))
+    ;;            (iff (open-output-channel-p1 channel type state)
+    ;;                 (not (equal channel other-channel)))))
+    (implies (and (open-output-channel-p1 channel type state)
+                  (not (equal channel other-channel))) ; (*)
+             (b* ((state (close-output-channel other-channel state)))
+               (open-output-channel-p1 channel type state))))
 
-(defthm open-output-channel-p1-under-read-byte$
-  (implies (open-output-channel-p1 channel type state)
-           (b* (((mv & state) (read-byte$ other-channel state)))
-             (open-output-channel-p1 channel type state))))
+  (defthm open-output-channel-p1-under-read-char$
+    (implies (open-output-channel-p1 channel type state)
+             (b* (((mv & state) (read-char$ other-channel state)))
+               (open-output-channel-p1 channel type state))))
 
-(defthm open-output-channel-p1-under-read-object
-  (implies (open-output-channel-p1 channel type state)
-           (b* (((mv & & state) (read-object other-channel state)))
-             (open-output-channel-p1 channel type state))))
+  (defthm open-output-channel-p1-under-read-byte$
+    (implies (open-output-channel-p1 channel type state)
+             (b* (((mv & state) (read-byte$ other-channel state)))
+               (open-output-channel-p1 channel type state))))
 
-(defthm open-output-channel-p1-under-princ$
-  (implies (open-output-channel-p1 channel type state)
-           (b* ((state (princ$ x other-channel state)))
-             (open-output-channel-p1 channel type state))))
+  (defthm open-output-channel-p1-under-read-object
+    (implies (open-output-channel-p1 channel type state)
+             (b* (((mv & & state) (read-object other-channel state)))
+               (open-output-channel-p1 channel type state))))
 
-(defthm open-output-channel-p1-under-write-byte$
-  (implies (open-output-channel-p1 channel type state)
-           (b* ((state (write-byte$ byte other-channel state)))
-             (open-output-channel-p1 channel type state))))
+  (defthm open-output-channel-p1-under-princ$
+    (implies (open-output-channel-p1 channel type state)
+             (b* ((state (princ$ x other-channel state)))
+               (open-output-channel-p1 channel type state))))
 
-(defthm open-output-channel-p1-under-print-object$
-  (implies (open-output-channel-p1 channel type state)
-           (b* ((state (print-object$ byte other-channel state)))
-             (open-output-channel-p1 channel type state))))
+  (defthm open-output-channel-p1-under-write-byte$
+    (implies (open-output-channel-p1 channel type state)
+             (b* ((state (write-byte$ byte other-channel state)))
+               (open-output-channel-p1 channel type state))))
 
-(defthm open-output-channel-p1-under-set-serialize-character
-  (implies (open-output-channel-p1 channel type state)
-           (b* ((state (set-serialize-character c state)))
-             (open-output-channel-p1 channel type state))))
+  (defthm open-output-channel-p1-under-print-object$
+    (implies (open-output-channel-p1 channel type state)
+             (b* ((state (print-object$ byte other-channel state)))
+               (open-output-channel-p1 channel type state))))
+
+  (defthm open-output-channel-p1-under-set-serialize-character
+    (implies (open-output-channel-p1 channel type state)
+             (b* ((state (set-serialize-character c state)))
+               (open-output-channel-p1 channel type state)))))
