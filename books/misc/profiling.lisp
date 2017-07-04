2053b28d3569c972166e48049c13383a2f57ad5a
Matt Kaufmann <matthew.j.kaufmann@gmail.com>
2017-07-04 15:17:49 -0700

    Fixed misc/profiling.lisp to certify with github distributions of CCL (addressing GitHub Issue #744).
    Github distributions of CCL do not include the contrib/ directory,
    which up till now has been assumed to exist by the book,
    books/misc/profiling.lisp.  My fix for this is a bit ugly, but
    adequate.  If there are additional such issues in the future I might
    try to make a prettier fix, perhaps switching to the use of
    include-raw.  It didn't seem worth the effort to do that at this
    point.
    
    I've had correspondence with a CCL developer.  If he decides to
    include contrib/ in future CCL distributions, then I'll probably
    revert this change; but it seems more likely that he'll put contrib/
    into its own github repository.  Attempts to use github CCL with the
    misc/profiling book now result in messages pointing to that future
    possibility.

--- books/misc/profiling.lisp
+++ books/misc/profiling.lisp
@@ -38,9 +38,66 @@
 
 (defttag :profiling)
 
+(set-state-ok t)
+
+(defun with-profiling-ccl-dir-warning (prof-dir state)
+  (declare (xargs :mode :program))
+  (warning$ 'with-profiling nil
+            "The CCL profiling routines used by books/misc/profiling.lisp ~
+             depend on directory ~x0, which does not exist in your CCL ~
+             distribution.  Such a directory exists in svn distributions of ~
+             CCL and may appear in a future ccl-contrib github repository."
+            prof-dir))
+
+(defun with-profiling-ccl-dir (state)
+  (declare (xargs :mode :program))
+  (cond
+   ((not (eq (f-get-global 'host-lisp state) :ccl))
+    (er soft 'with-profiling
+        "Function ~x0 should only be called when the host Lisp is CCL. ~
+         Something is amiss!"
+        'with-profiling-ccl-dir))
+   (t (mv-let (erp ccl-dir state)
+        (getenv$ "CCL_DEFAULT_DIRECTORY" state)
+        (declare (ignore erp))
+        (assert$
+         ccl-dir
+         (value (concatenate 'string
+                             ccl-dir
+                             "/contrib/huebner/advice-profiler/")))))))
+
 (progn!
  (set-raw-mode t)
- (load (concatenate 'string (cbd) "profiling-raw.lsp")))
+ (cond
+  ((and (eq (f-get-global 'host-lisp state) :ccl)
+        (not (eq (os (w state)) :mswindows)))
+   (er-let* ((prof-dir (with-profiling-ccl-dir state)))
+     (cond
+      ((our-probe-file prof-dir)
+       (prog2$
+        (let ((*readtable* *host-readtable*))
+          (load (concatenate 'string prof-dir "package.lisp"))
+          (load (concatenate 'string prof-dir "profiler.lisp"))
+          (load (concatenate 'string (cbd) "profiling-raw.lsp")))
+        (value nil)))
+      (t
+       (with-profiling-ccl-dir-warning prof-dir state)
+
+; The calls of error below avoid having to deal with multiple values, as is
+; done by the uses of our-multiple-value-prog1 in profiling-raw.lsp.
+
+       (eval `(defmacro with-profiling-raw (syms form)
+                (declare (ignore syms form))
+                '(progn
+                   (with-profiling-ccl-dir-warning
+                    ,prof-dir *the-live-state*)
+                   (error "Directory does not exist (see warning above):~%~a"
+                          ,prof-dir))))
+       (eval '(defmacro with-sprofiling-internal-raw (options form)
+                (declare (ignore options form))
+                (error "The macro ~s does not do any profiling in CCL."
+                       'with-sprofiling)))))))
+  (t (load (concatenate 'string (cbd) "profiling-raw.lsp")))))
 
 (defmacro-last with-profiling)
 
