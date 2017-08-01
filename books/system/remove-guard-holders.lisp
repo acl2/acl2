3ad496a05330952c4cfec0df0d3340c0a13a1551
Matt Kaufmann <kaufmann@cs.utexas.edu>
2017-08-01 11:10:06 -0500

    Separated out useful lemmas about remove-guard-holders from books/system/remove-guard-holders.lisp, as requested by Eric Smith, into new book, books/system/remove-guard-holders-lemmas.lisp.

--- books/system/remove-guard-holders.lisp
+++ books/system/remove-guard-holders.lisp
@@ -5,38 +5,18 @@
 (in-package "ACL2")
 
 (include-book "subcor-var")
-(local (include-book "tools/flag" :dir :system))
-(local (include-book "pseudo-termp-lemmas"))
 
-(verify-termination (remove-guard-holders1
-                     (declare (xargs :verify-guards nil))))
+; This use of make-event is awkward.  But I couldn't find a better way to avoid
+; problems caused by make-event storing a redundant event for
+; verify-termination of remove-guard-holders1 during the first pass, which
+; causes a failure during the second pass.
+(make-event
+ (er-let* ((event (verify-termination-fn '(remove-guard-holders1) state)))
+   (value `(defmacro verify-termination-remove-guard-holders1 ()
+             ',event))))
 
-(local (make-flag remove-guard-holders1))
+(local (include-book "remove-guard-holders-lemmas"))
 
-(local (defthm equal-len-0-rewrite
-         (equal (equal 0 (len x))
-                (not (consp x)))))
-
-(local (defthm len-mv-nth-1-remove-guard-holders1-lst
-         (equal (len (mv-nth 1 (remove-guard-holders1-lst term)))
-                (len term))))
-
-(local (defthm pseudo-termp-car-last
-         (implies (pseudo-term-listp term)
-                  (pseudo-termp (car (last term))))))
-
-(local (defthm-flag-remove-guard-holders1
-         (defthm pseudo-termp-remove-guard-holders1
-           (implies (pseudo-termp term)
-                    (pseudo-termp
-                     (mv-nth 1 (remove-guard-holders1 changedp0 term))))
-           :flag remove-guard-holders1)
-         (defthm pseudo-term-listp-remove-guard-holders1-lst
-           (implies (pseudo-term-listp lst)
-                    (pseudo-term-listp
-                     (mv-nth 1 (remove-guard-holders1-lst lst))))
-           :flag remove-guard-holders1-lst)))
-
-(verify-guards remove-guard-holders1) ; and remove-guard-holders1-lst
+(verify-termination-remove-guard-holders1) ; and guards
 
 (verify-termination remove-guard-holders) ; and guards
