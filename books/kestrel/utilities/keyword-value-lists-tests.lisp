b57289d34018e8607455b3887cc8a1ae5a7a02aa
Alessandro Coglio <coglio@kestrel.edu>
2017-10-14 01:34:05 -0700

    Add STRIP-KEYWORDS utility.
    This returns the list of keywords of a keyword-value list, in the same order.

--- books/kestrel/utilities/keyword-value-lists-tests.lisp
+++ books/kestrel/utilities/keyword-value-lists-tests.lisp
@@ -25,3 +25,16 @@
 
 (assert-equal (keyword-value-list-to-alist '(:x :abc :y #\9))
               '((:x . :abc) (:y . #\9)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(assert-equal (strip-keywords nil) nil)
+
+(assert-equal (strip-keywords '(:one 1 :two 2 :three 3))
+              '(:one :two :three))
+
+(assert-equal (strip-keywords '(:one 1 :one 2 :three 3))
+              '(:one :one :three))
+
+(assert-equal (strip-keywords '(:x :abc :y #\9))
+              '(:x :y))
