9725c143c9c66896e6d9291b82454bbb17158daa
Alessandro Coglio <coglio@kestrel.edu>
2018-04-10 19:27:27 -0700

    Add more tests for RECURSIVE-CALLS.

--- books/kestrel/utilities/world-queries-tests.lisp
+++ books/kestrel/utilities/world-queries-tests.lisp
@@ -1,6 +1,6 @@
 ; World Queries -- Tests
 ;
-; Copyright (C) 2017 Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2018 Kestrel Institute (http://www.kestrel.edu)
 ;
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 ;
@@ -1010,6 +1010,39 @@
                                  (not (= n '1))))
                         (equal (access tests-and-call rc2 :call)
                                '(fib (binary-+ '-2 n)))))))))
+
+(must-succeed*
+ (defun fib (n)
+   (declare (xargs :mode :program))
+   (if (zp n)
+       1
+     (if (= n 1)
+         1
+       (+ (fib (- n 1))
+          (fib (- n 2))))))
+ (assert! (let ((rc (recursive-calls 'fib (w state))))
+            (and (pseudo-tests-and-call-listp rc)
+                 (= (len rc) 2)
+                 (let ((rc1 (first rc)))
+                   (and (equal (access tests-and-call rc1 :tests)
+                               '((not (zp n))
+                                 (not (= n '1))))
+                        (equal (access tests-and-call rc1 :call)
+                               '(fib (binary-+ '-1 n)))))
+                 (let ((rc2 (second rc)))
+                   (and (equal (access tests-and-call rc2 :tests)
+                               '((not (zp n))
+                                 (not (= n '1))))
+                        (equal (access tests-and-call rc2 :call)
+                               '(fib (binary-+ '-2 n)))))))))
+
+(must-succeed*
+ (defun nonrec (x) (cons x x))
+ (assert-equal (recursive-calls 'nonrec (w state)) nil))
+
+(must-succeed*
+ (defun nonrec (x) (declare (xargs :mode :program)) (cons x x))
+ (assert-equal (recursive-calls 'nonrec (w state)) nil))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
