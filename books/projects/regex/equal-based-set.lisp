321d7d087911c47e00fed38db40698a281ac9106
Sol Swords <sswords@centtech.com>
2018-05-03 09:51:49 -0500

    Fix a bug in VL warnings about latches.  Add copyright headers to projects/regex books.

--- books/projects/regex/equal-based-set.lisp
+++ books/projects/regex/equal-based-set.lisp
@@ -1,4 +1,8 @@
+; Copyright (C) 2004, Regents of the University of Texas
+; Written by Sol Swords
+; License: A 3-clause BSD license.  See the LICENSE file distributed with ACL2.
 
+; Note: this book doesn't appear to be used anywhere.  Consider deleting it.
 (in-package "ACL2")
 
 (include-book "defset-macros")
