6aa3733ae5d40e23cc4537567f6cc086466b860c
Alessandro Coglio <coglio@kestrel.edu>
2018-05-14 23:33:07 -0700

    Extend MAKE-EVENT-TERSE utility.
    This now accepts an optional :SUPPRESS-ERRORS argument (NIL by default) to also
    suppress error output (to be probably used only in special circumstances,
    e.g. programmatically).

--- books/kestrel/utilities/user-interface-tests.lisp
+++ books/kestrel/utilities/user-interface-tests.lisp
@@ -1,10 +1,10 @@
 ; User Interface -- Tests
 ;
-; Copyright (C) 2017 Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2018 Kestrel Institute (http://www.kestrel.edu)
 ;
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 ;
-; Authors: Alessandro Coglio (coglio@kestrel.edu)
+; Author: Alessandro Coglio (coglio@kestrel.edu)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -64,6 +64,14 @@
 
 (make-event-terse
  '(defun a (x) x))
+
+(make-event-terse
+ '(defun a1 (x) x)
+ :suppress-errors nil)
+
+(make-event-terse
+ '(defun a2 (x) x)
+ :suppress-errors t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
