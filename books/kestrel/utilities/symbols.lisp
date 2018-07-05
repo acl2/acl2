2375a90add81d705314c6c97fadd3566c3f67ba5
Alessandro Coglio <coglio@kestrel.edu>
2018-07-04 17:14:47 -0700

    Use XDOC constructors in :DOC SYMBOL-PACKAGE-NAME-SAFE.

--- books/kestrel/utilities/symbols.lisp
+++ books/kestrel/utilities/symbols.lisp
@@ -28,25 +28,23 @@
   :enabled t
   :parents (symbol-utilities)
   :short "The @(tsee symbol-package-name) of a symbol, but not \"COMMON-LISP\"."
-  :long "
-  <p>
-  This function is just @(tsee symbol-package-name), except that it it is
-  ``safe'' in the sense that it returns a string that is a legal package name
-  for @(see intern)ing a string.  It does this by avoiding a return value of
-  \"COMMON-LISP\".  Simply put: when @('symbol-package-name') returns
-  \"COMMON-LISP\" then @('symbol-package-name-safe') returns \"ACL2\", and
-  otherwise the two functions agree.  Example:
-  </p>
-
- @({
- ACL2 !>(symbol-package-name-safe 'car)
- \"ACL2\"
- ACL2 !>(symbol-package-name 'car)
- \"COMMON-LISP\"
- ACL2 !>
- })
-  "
-
+  :long
+  (xdoc::topapp
+   (xdoc::p
+    "This function is just @(tsee symbol-package-name), except that it it is
+     ``safe'' in the sense that it returns a string that is a legal package name
+     for @(see intern)ing a string.  It does this by avoiding a return value of
+     \"COMMON-LISP\".  Simply put: when @('symbol-package-name') returns
+     \"COMMON-LISP\" then @('symbol-package-name-safe') returns \"ACL2\", and
+     otherwise the two functions agree.")
+   (xdoc::p
+    "Example:")
+   (xdoc::code
+    "ACL2 !>(symbol-package-name-safe 'car)"
+    "\"ACL2\""
+    "ACL2 !>(symbol-package-name 'car)"
+    "\"COMMON-LISP\""
+    "ACL2 !>"))
   (let ((pkg (symbol-package-name sym)))
     (if (equal pkg *main-lisp-package-name*)
         "ACL2"
@@ -59,8 +57,9 @@
   :parents (symbol-utilities)
   :short "Lift @(tsee symbol-package-name) to lists."
   :long
-  (xdoc::p
-   "This function is named similarly to the built-in @('symbol-name-lst').")
+  (xdoc::topapp
+   (xdoc::p
+    "This function is named similarly to the built-in @('symbol-name-lst')."))
   (cond ((endp syms) nil)
         (t (cons (symbol-package-name (car syms))
                  (symbol-package-name-lst (cdr syms))))))
