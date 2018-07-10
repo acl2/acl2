86a717b5dc4ce77b07ca55cdbfd566cdef47ed23
Matt Kaufmann <kaufmann@cs.utexas.edu>
2018-07-09 23:49:15 -0500

    Added new argument to fmt-to-comment-window and fmt-to-comment-window!, print-base-radix, to allow control of the print-base and print-radix.  Similarly added new utilities, cw-print-base-radix and cw-print-base-radix!.
    Quoting :doc note-8-1:
    
      Both [fmt-to-comment-window] and fmt-to-comment-window! have an extra
      argument, print-base-radix, which is the same as the first argument
      to the new utilities, [cw-print-base-radix] and
      [cw-print-base-radix!] (see below).
    
      New utilities, [cw-print-base-radix] and [cw-print-base-radix!], are
      like [cw] and [cw!], respectively, except for an additional
      argument that specifies the print-base and, optionally, the
      print-radix.  Thanks to Eric Smith for requesting
      [cw-print-base-radix].

--- books/centaur/sv/svtv/process.lisp
+++ books/centaur/sv/svtv/process.lisp
@@ -76,7 +76,7 @@
           ((eq ent 'acl2::x) (svex-quote (4vec-x)))
           ((eq ent :ones)    (svex-quote -1))
           (t (svex-var ent)))))
-  
+
 
 (define svtv-inputs->assigns ((x svtv-lines-p) (phase natp))
   :verbosep t
@@ -465,16 +465,16 @@
     (clear-memoize-table 'svex-compose)
     (clear-memoize-table 'svex-compose-svtv-phases-call)
     (mv outalist final-state)))
-       
-    
-       
 
 
-    
-  
 
-  
-  
+
+
+
+
+
+
+
 
 
 
@@ -804,7 +804,7 @@
     (fast-alist-free updates-for-outs)
     (mv (make-svtv :name           name
                    :outexprs       outexprs
-                   :nextstate     final-state     
+                   :nextstate     final-state
                    :inmasks        (append inmasks override-inmasks)
                    :outmasks       outmasks
                    :orig-ins       orig-ins
@@ -1434,7 +1434,7 @@ decomposition proof.</li>
                             #\5 #\6 #\7 #\8 #\9)
                           (list (caar al) 23
                                 (str::hexify (2vec->val (cdar al)))))
-                3 nil)
+                3 nil nil)
                (cw! back)))
              (t
               (let* ((upper (str::hexify (4vec->upper (cdar al))))
@@ -1454,7 +1454,7 @@ decomposition proof.</li>
                   (pairlis2 '(#\0 #\1 #\2 #\3 #\4
                               #\5 #\6 #\7 #\8 #\9)
                             (list (caar al) 23 pad-u upper))
-                  3 nil)
+                  3 nil nil)
                  (cw! "~t0. ~_1~s2" 23 pad-l lower)
                  (cw! back)
                  (cw! ";;;    non-Boolean mask: ~_0~s1~%" pad-m mask)))))))
@@ -1861,8 +1861,8 @@ irrelevant inputs are removed.</p>"
        ((list params auto-bindings ?arrange-keyword arrange) case1))
     (cons (list params (svtv-flex-bindings svtv auto-bindings :arrange arrange))
           (svtv-flex-param-bindings svtv (cdr in-alist)))))
-       
-       
+
+
 
 
 
