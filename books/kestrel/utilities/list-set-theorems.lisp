19a68b6a241ab2c443616d707005d33c25ab8219
Alessandro Coglio <coglio@kestrel.edu>
2018-08-25 14:42:02 -0700

    Move theorems about lists to subdirectory.
    This will be a subdirectory that collects Kestrel's list utilities.
    
    The old files at the upper level are temporarily retained as "pointers" to the
    files in the subdirectory.

--- books/kestrel/utilities/list-set-theorems.lisp
+++ books/kestrel/utilities/list-set-theorems.lisp
@@ -1,53 +1,7 @@
-; Theorems about Sets Represented as Lists
-;
-; Copyright (C) 2017 Kestrel Institute (http://www.kestrel.edu)
-;
-; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
-;
-; Author: Alessandro Coglio (coglio@kestrel.edu)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; This file is temporary. It will be removed once it is no longer referenced.
+; Files that reference this file should be changed
+; to reference directly files under lists/.
 
 (in-package "ACL2")
 
-(include-book "std/util/defrule" :dir :system)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defsection theorems-about-sets-represented-as-lists
-
-  :parents (theorems-about-non-kestrel-books)
-
-  :short "Theorems about sets represented as lists."
-
-  (defrule true-listp-of-add-to-set-equal
-    :parents (add-to-set)
-    (equal (true-listp (add-to-set-equal a x))
-           (true-listp x)))
-
-  (defrule true-listp-of-add-to-set-equal-type
-    :parents (add-to-set)
-    (implies (true-listp x)
-             (true-listp (add-to-set-equal a x)))
-    :rule-classes :type-prescription)
-
-  (defrule true-listp-of-union-equal
-    :parents (union$)
-    (equal (true-listp (union-equal x y))
-           (true-listp y)))
-
-  (defrule true-listp-of-union-equal-type
-    :parents (union$)
-    (implies (true-listp y)
-             (true-listp (union-equal x y)))
-    :rule-classes :type-prescription)
-
-  (defrule true-listp-of-intersection-equal
-    :parents (intersection$)
-    (true-listp (intersection-equal x y))
-    :rule-classes (:rewrite :type-prescription))
-
-  (defrule true-listp-of-set-difference-equal
-    :parents (set-difference$)
-    (true-listp (set-difference-equal x y))
-    :rule-classes (:rewrite :type-prescription)))
+(include-book "lists/set-theorems")
---
+++ books/kestrel/utilities/lists/theorems.lisp
@@ -0,0 +1,78 @@
+; Theorems about Lists
+;
+; Copyright (C) 2017 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+(include-book "std/lists/list-fix" :dir :system)
+(include-book "std/util/defrule" :dir :system)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defsection theorems-about-lists
+
+  :parents (theorems-about-non-kestrel-books)
+
+  :short "Theorems about lists."
+
+  :long
+  "<p>
+   The theorems @('equal-len-const'), @('gteq-len-const'), and @('gt-len-const')
+   are disabled by default.
+   They can be enabled to turn
+   assertions about lengths and constants
+   into assertions about @(tsee consp) and @(tsee cdr):
+   the expansion terminates because of the @(tsee syntaxp) restriction.
+   </p>"
+
+  (defruled equal-len-const
+    (implies (syntaxp (quotep c))
+             (equal (equal (len x) c)
+                    (if (natp c)
+                        (if (equal c 0)
+                            (not (consp x))
+                          (and (consp x)
+                               (equal (len (cdr x))
+                                      (1- c))))
+                      nil))))
+
+  (defruled gteq-len-const
+    (implies (syntaxp (quotep c))
+             (equal (>= (len x) c)
+                    (or (<= (fix c) 0)
+                        (and (consp x)
+                             (>= (len (cdr x))
+                                 (1- c)))))))
+
+  (defruled gt-len-const
+    (implies (syntaxp (quotep c))
+             (equal (> (len x) c)
+                    (or (< (fix c) 0)
+                        (and (consp x)
+                             (> (len (cdr x))
+                                (1- c))))))
+    :use lemma
+    :prep-lemmas
+    ((defruled lemma
+       (implies (and (consp x)
+                     (or (< (fix c) 0)
+                         (> (len (cdr x))
+                            (1- c))))
+                (> (len x) c)))))
+
+  (defrule last-of-cdr
+    (equal (last (cdr x))
+           (if (consp (cdr x))
+               (last x)
+             (cdr x))))
+
+  (defrule list-of-car-when-one
+    (implies (and (consp list)
+                  (not (consp (cdr list))))
+             (equal (list (car list)) (list-fix list)))))
