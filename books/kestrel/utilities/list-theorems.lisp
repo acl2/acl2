444cae60228154a536fe0ad49acfcd00206eb97e
Alessandro Coglio <coglio@kestrel.edu>
2018-08-30 14:48:40 -0700

    Remove obsolete file.

--- books/kestrel/utilities/list-theorems.lisp
+++
@@ -1,9 +0,0 @@
-; This file is temporary. It will be removed once it is no longer referenced.
-; Files that reference this file should be changed
-; to reference directly files under lists/.
-
-(in-package "ACL2")
-
-(include-book "lists/last-theorems")
-(include-book "lists/len-const-theorems")
-(include-book "lists/primitive-theorems")
