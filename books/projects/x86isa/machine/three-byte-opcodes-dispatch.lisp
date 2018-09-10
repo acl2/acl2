4748fab435dd4c33950676dcb7403baf6c745737
Shilpi Goel <shilpi@centtech.com>
2018-09-10 15:28:52 -0500

    [x86isa] Separated out x86.lisp into different books

---
+++ books/projects/x86isa/machine/three-byte-opcodes-dispatch.lisp
@@ -0,0 +1,282 @@
+; X86ISA Library
+
+; Note: The license below is based on the template at:
+; http://opensource.org/licenses/BSD-3-Clause
+
+; Contact:
+;   Centaur Technology Formal Verification Group
+;   7600-C N. Capital of Texas Highway, Suite 300, Austin, TX 78731, USA.
+;   http://www.centtech.com/
+
+; Copyright (C) 2018, Centaur Technology, Inc.
+; All rights reserved.
+
+; Redistribution and use in source and binary forms, with or without
+; modification, are permitted provided that the following conditions are
+; met:
+
+; o Redistributions of source code must retain the above copyright
+;   notice, this list of conditions and the following disclaimer.
+
+; o Redistributions in binary form must reproduce the above copyright
+;   notice, this list of conditions and the following disclaimer in the
+;   documentation and/or other materials provided with the distribution.
+
+; o Neither the name of the copyright holders nor the names of its
+;   contributors may be used to endorse or promote products derived
+;   from this software without specific prior written permission.
+
+; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+; "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+; LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+; A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+; HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+; SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+; LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+; DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+; THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+; OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+; Original Author(s):
+; Shilpi Goel         <shilpi@centtech.com>
+
+(in-package "X86ISA")
+
+(include-book "instructions/top"
+	      :ttags (:include-raw :syscall-exec :other-non-det :undef-flg))
+(include-book "dispatch-macros")
+(include-book "cpuid")
+(include-book "std/strings/hexify" :dir :system)
+
+(local (include-book "dispatch-utils"))
+(local (include-book "centaur/bitops/ihs-extensions" :dir :system))
+(local (include-book "centaur/bitops/signed-byte-p" :dir :system))
+
+(local (in-theory (e/d ()
+		       (app-view-rml08-no-error
+			(:meta acl2::mv-nth-cons-meta)
+			rme08-value-when-error
+			member-equal))))
+
+(local (xdoc::set-default-parents x86-decoder))
+
+;; ----------------------------------------------------------------------
+
+
+(make-event
+ (b* (((mv table-doc-string dispatch)
+       (create-dispatch-from-opcode-map
+	*0F-38-three-byte-opcode-map-lst*
+	(w state)
+	:escape-bytes #ux0F_38_00)))
+
+   `(progn
+      (define first-three-byte-opcode-execute
+	((proc-mode        :type (integer 0 #.*num-proc-modes-1*))
+	 (start-rip        :type (signed-byte   #.*max-linear-address-size*))
+	 (temp-rip         :type (signed-byte   #.*max-linear-address-size*))
+	 (prefixes         :type (unsigned-byte #.*prefixes-width*))
+	 (mandatory-prefix :type (unsigned-byte 8))
+	 (rex-byte         :type (unsigned-byte 8))
+	 (opcode           :type (unsigned-byte 8))
+	 (modr/m           :type (unsigned-byte 8))
+	 (sib              :type (unsigned-byte 8))
+	 x86)
+
+	;; #x0F #x38 are the first two opcode bytes.
+
+	:parents (x86-decoder)
+	;; The following arg will avoid binding __function__ to
+	;; first-three-byte-opcode-execute. The automatic __function__ binding
+	;; that comes with define causes stack overflows during the guard proof
+	;; of this function.
+	:no-function t
+	:ignore-ok t
+	:short "First three-byte opcode dispatch function."
+	:long "<p>@('first-three-byte-opcode-execute) is the doorway to the
+     first three-byte opcode map, i.e., to all three-byte opcodes whose first
+     two opcode bytes are @('0F 38').</p>"
+	:guard-hints (("Goal"
+		       :do-not '(preprocess)
+		       :in-theory
+		       (e/d
+			()
+			(unsigned-byte-p signed-byte-p))))
+
+	(case opcode ,@dispatch)
+
+	///
+
+	(defthm x86p-first-three-byte-opcode-execute
+	  (implies (and (x86p x86)
+			(canonical-address-p temp-rip))
+		   (x86p
+		    (first-three-byte-opcode-execute
+		     proc-mode start-rip temp-rip prefixes
+		     mandatory-prefix rex-byte opcode modr/m sib x86)))))
+
+      (defsection 0F-38-three-byte-opcodes-table
+	:parents (implemented-opcodes)
+	:short "@('x86isa') Support for Opcodes in the @('0F 38') Three Byte
+	Map; see @(see implemented-opcodes) for details."
+	:long ,table-doc-string))))
+
+(make-event
+ (b* (((mv table-doc-string dispatch)
+       (create-dispatch-from-opcode-map
+	*0F-3A-three-byte-opcode-map-lst*
+	(w state)
+	:escape-bytes #ux0F_3A_00)))
+
+   `(progn
+      (define second-three-byte-opcode-execute
+	((proc-mode        :type (integer 0 #.*num-proc-modes-1*))
+	 (start-rip        :type (signed-byte   #.*max-linear-address-size*))
+	 (temp-rip         :type (signed-byte   #.*max-linear-address-size*))
+	 (prefixes         :type (unsigned-byte #.*prefixes-width*))
+	 (mandatory-prefix :type (unsigned-byte 8))
+	 (rex-byte         :type (unsigned-byte 8))
+	 (opcode           :type (unsigned-byte 8))
+	 (modr/m           :type (unsigned-byte 8))
+	 (sib              :type (unsigned-byte 8))
+	 x86)
+
+	;; #x0F #x3A are the first two opcode bytes.
+
+	:parents (x86-decoder)
+	;; The following arg will avoid binding __function__ to
+	;; second-three-byte-opcode-execute. The automatic __function__ binding that
+	;; comes with define causes stack overflows during the guard proof of this
+	;; function.
+	:no-function t
+	:ignore-ok t
+	:short "Second three-byte opcode dispatch function."
+	:long "<p>@('second-three-byte-opcode-execute) is the doorway to the second
+     three-byte opcode map, i.e., to all three-byte opcodes whose second two
+     opcode bytes are @('0F 3A').</p>"
+	:guard-hints (("Goal"
+		       :do-not '(preprocess)
+		       :in-theory (e/d () (unsigned-byte-p signed-byte-p))))
+
+	(case opcode ,@dispatch)
+
+	///
+
+	(defthm x86p-second-three-byte-opcode-execute
+	  (implies (and (x86p x86)
+			(canonical-address-p temp-rip))
+		   (x86p (second-three-byte-opcode-execute
+			  proc-mode start-rip temp-rip prefixes
+			  mandatory-prefix rex-byte opcode modr/m sib x86)))))
+
+      (defsection 0F-3A-three-byte-opcodes-table
+	:parents (implemented-opcodes)
+	:short "@('x86isa') Support for Opcodes in the @('0F 3A') Three Byte
+	Map; see @(see implemented-opcodes) for details."
+	:long ,table-doc-string))))
+
+(define three-byte-opcode-decode-and-execute
+  ((proc-mode          :type (integer 0 #.*num-proc-modes-1*))
+   (start-rip          :type (signed-byte #.*max-linear-address-size*))
+   (temp-rip           :type (signed-byte #.*max-linear-address-size*))
+   (prefixes           :type (unsigned-byte #.*prefixes-width*))
+   (rex-byte           :type (unsigned-byte 8))
+   (second-escape-byte :type (unsigned-byte 8))
+   x86)
+
+  :ignore-ok t
+  :guard (or (equal second-escape-byte #x38)
+	     (equal second-escape-byte #x3A))
+  :guard-hints (("Goal" :do-not '(preprocess)
+		 :in-theory (e/d*
+			     (add-to-*ip add-to-*ip-is-i48p-rewrite-rule)
+			     (unsigned-byte-p
+			      (:type-prescription bitops::logand-natp-type-2)
+			      (:type-prescription bitops::ash-natp-type)
+			      acl2::loghead-identity
+			      not
+			      tau-system
+			      (tau-system)))))
+  :parents (x86-decoder)
+  :short "Decoder and dispatch function for three-byte opcodes, where @('0x0F
+  0x38') are the first two opcode bytes"
+  :long "<p>Source: Intel Manual, Volume 2, Appendix A-2</p>"
+
+  (b* ((ctx 'three-byte-opcode-decode-and-execute)
+
+       ((mv flg0 (the (unsigned-byte 8) opcode) x86)
+	(rme08 proc-mode temp-rip #.*cs* :x x86))
+       ((when flg0)
+	(!!ms-fresh :opcode-byte-access-error flg0))
+
+       ;; Possible values of opcode: all values denote opcodes of the first or
+       ;; second three-byte map, depending on the value of second-escape-byte.
+       ;; The function first-three-byte-opcode-execute or
+       ;; second-three-byte-opcode-execute case-splits on this opcode byte and
+       ;; calls the appropriate instruction semantic function.
+
+       ((mv flg temp-rip) (add-to-*ip proc-mode temp-rip 1 x86))
+       ((when flg) (!!ms-fresh :increment-error flg))
+
+       ((mv msg (the (unsigned-byte 8) mandatory-prefix))
+	(compute-mandatory-prefix-for-three-byte-opcode
+	 proc-mode second-escape-byte opcode prefixes))
+       ((when msg)
+	(x86-illegal-instruction msg start-rip temp-rip x86))
+
+       (modr/m?
+	(three-byte-opcode-ModR/M-p
+	 proc-mode mandatory-prefix second-escape-byte opcode))
+       ((mv flg1 (the (unsigned-byte 8) modr/m) x86)
+	(if modr/m?
+	    (rme08 proc-mode temp-rip #.*cs* :x x86)
+	  (mv nil 0 x86)))
+       ((when flg1) (!!ms-fresh :modr/m-byte-read-error flg1))
+
+       ((mv flg temp-rip) (if modr/m?
+			      (add-to-*ip proc-mode temp-rip 1 x86)
+			    (mv nil temp-rip)))
+       ((when flg) (!!ms-fresh :increment-error flg))
+
+       (sib? (and modr/m?
+		  (b* ((p4? (eql #.*addr-size-override*
+				 (prefixes->adr prefixes)))
+		       (16-bit-addressp (eql 2 (select-address-size proc-mode p4? x86))))
+		    (x86-decode-SIB-p modr/m 16-bit-addressp))))
+       ((mv flg2 (the (unsigned-byte 8) sib) x86)
+	(if sib?
+	    (rme08 proc-mode temp-rip #.*cs* :x x86)
+	  (mv nil 0 x86)))
+       ((when flg2)
+	(!!ms-fresh :sib-byte-read-error flg2))
+
+       ((mv flg temp-rip) (if sib?
+			      (add-to-*ip proc-mode temp-rip 1 x86)
+			    (mv nil temp-rip)))
+       ((when flg) (!!ms-fresh :increment-error flg)))
+
+    (case second-escape-byte
+      (#x38
+       (first-three-byte-opcode-execute
+	proc-mode start-rip temp-rip prefixes rex-byte
+	mandatory-prefix opcode modr/m sib x86))
+      (#x3A
+       (second-three-byte-opcode-execute
+	proc-mode start-rip temp-rip prefixes rex-byte
+	mandatory-prefix opcode modr/m sib x86))
+      (otherwise
+       ;; Unreachable.
+       (!!ms-fresh :illegal-value-of-second-escape-byte second-escape-byte))))
+
+  ///
+
+  (defrule x86p-three-byte-opcode-decode-and-execute
+    (implies (and (canonical-address-p temp-rip)
+		  (x86p x86))
+	     (x86p (three-byte-opcode-decode-and-execute
+		    proc-mode start-rip temp-rip prefixes rex-byte escape-byte x86)))
+    :enable add-to-*ip-is-i48p-rewrite-rule
+    :disable signed-byte-p))
+
+;; ----------------------------------------------------------------------
---
+++ books/projects/x86isa/machine/vex-opcodes-dispatch.lisp
@@ -0,0 +1,402 @@
+; X86ISA Library
+
+; Note: The license below is based on the template at:
+; http://opensource.org/licenses/BSD-3-Clause
+
+; Contact:
+;   Centaur Technology Formal Verification Group
+;   7600-C N. Capital of Texas Highway, Suite 300, Austin, TX 78731, USA.
+;   http://www.centtech.com/
+
+; Copyright (C) 2018, Centaur Technology, Inc.
+; All rights reserved.
+
+; Redistribution and use in source and binary forms, with or without
+; modification, are permitted provided that the following conditions are
+; met:
+
+; o Redistributions of source code must retain the above copyright
+;   notice, this list of conditions and the following disclaimer.
+
+; o Redistributions in binary form must reproduce the above copyright
+;   notice, this list of conditions and the following disclaimer in the
+;   documentation and/or other materials provided with the distribution.
+
+; o Neither the name of the copyright holders nor the names of its
+;   contributors may be used to endorse or promote products derived
+;   from this software without specific prior written permission.
+
+; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+; "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+; LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+; A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+; HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+; SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+; LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+; DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+; THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+; OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+; Original Author(s):
+; Shilpi Goel         <shilpi@centtech.com>
+
+(in-package "X86ISA")
+
+(include-book "instructions/top"
+	      :ttags (:include-raw :syscall-exec :other-non-det :undef-flg))
+(include-book "dispatch-macros")
+(include-book "cpuid")
+(include-book "std/strings/hexify" :dir :system)
+
+(local (include-book "dispatch-utils"))
+(local (include-book "centaur/bitops/ihs-extensions" :dir :system))
+(local (include-book "centaur/bitops/signed-byte-p" :dir :system))
+
+(local (in-theory (e/d ()
+		       (app-view-rml08-no-error
+			(:meta acl2::mv-nth-cons-meta)
+			rme08-value-when-error
+			member-equal))))
+
+(local (xdoc::set-default-parents x86-decoder))
+
+;; ----------------------------------------------------------------------
+
+(local
+ (defthm dumb-integerp-of-mv-nth-1-rme08-rule
+   (implies (force (x86p x86))
+	    (integerp (mv-nth 1 (rme08 proc-mode eff-addr seg-reg r-x x86))))
+   :rule-classes (:rewrite :type-prescription)))
+
+(local
+ (defthm unsigned-byte-p-24-of-vex-prefixes-rule
+   (implies
+    (unsigned-byte-p 8 byte)
+    (and (unsigned-byte-p 24 (logior #xC400 (ash byte 16)))
+	 (unsigned-byte-p 24 (logior #xC500 (ash byte 16)))))))
+
+(make-event
+ `(define vex-0F-execute
+    ((start-rip              :type (signed-byte   #.*max-linear-address-size*))
+     (temp-rip               :type (signed-byte   #.*max-linear-address-size*)
+			     "@('temp-rip') points to the byte following the
+			      opcode byte")
+     (vex-prefixes           :type (unsigned-byte 24)
+			     "Completely populated when this function is
+			      called")
+     (opcode                 :type (unsigned-byte 8))
+     (modr/m                 :type (unsigned-byte 8))
+     (sib                    :type (unsigned-byte 8))
+     x86)
+
+    :ignore-ok t
+
+    :parents (x86-decoder)
+    :no-function t
+    :short "Dispatch function for VEX-encoded instructions in the two-byte opcode map"
+    :guard (and (vex-prefixes-byte0-p vex-prefixes)
+		(modr/m-p modr/m)
+		(sib-p sib))
+    :guard-hints (("Goal"
+		   :do-not '(preprocess)
+		   :in-theory (e/d ()
+				   (unsigned-byte-p
+				    signed-byte-p
+				    (:forward-chaining acl2::unsigned-byte-p-forward)
+				    ash
+				    (tau-system)))))
+    :returns (x86 x86p :hyp (and (canonical-address-p temp-rip)
+				 (x86p x86)))
+
+    (case opcode
+      ,@(avx-case-gen *vex-0F-opcodes* t state))))
+
+(make-event
+ `(define vex-0F38-execute
+    ((start-rip              :type (signed-byte   #.*max-linear-address-size*))
+     (temp-rip               :type (signed-byte   #.*max-linear-address-size*)
+			     "@('temp-rip') points to the byte following the
+			     opcode byte")
+     (vex-prefixes           :type (unsigned-byte 24)
+			     "Completely populated when this function is
+			      called")
+     (opcode                 :type (unsigned-byte 8))
+     (modr/m                 :type (unsigned-byte 8))
+     (sib                    :type (unsigned-byte 8))
+     x86)
+
+    :ignore-ok t
+
+    :parents (x86-decoder)
+    :no-function t
+    :short "Dispatch function for VEX-encoded instructions in the first
+    three-byte opcode map"
+    :guard (and (vex-prefixes-byte0-p vex-prefixes)
+		(modr/m-p modr/m)
+		(sib-p sib))
+    :guard-hints (("Goal"
+		   :do-not '(preprocess)
+		   :in-theory (e/d ()
+				   (unsigned-byte-p
+				    signed-byte-p
+				    (:forward-chaining acl2::unsigned-byte-p-forward)
+				    ash
+				    (tau-system)))))
+
+    :returns (x86 x86p :hyp (and (canonical-address-p temp-rip)
+				 (x86p x86)))
+
+    (case opcode
+      ,@(avx-case-gen *vex-0F38-opcodes* t state))))
+
+(make-event
+ `(define vex-0F3A-execute
+    ((start-rip              :type (signed-byte   #.*max-linear-address-size*))
+     (temp-rip               :type (signed-byte   #.*max-linear-address-size*)
+			     "@('temp-rip') points to the byte following the
+			    opcode byte")
+     (vex-prefixes           :type (unsigned-byte 24)
+			     "Completely populated when this function is
+			      called")
+     (opcode                 :type (unsigned-byte 8))
+     (modr/m                 :type (unsigned-byte 8))
+     (sib                    :type (unsigned-byte 8))
+     x86)
+
+    :ignore-ok t
+
+    :parents (x86-decoder)
+    :no-function t
+    :short "Dispatch function for VEX-encoded instructions in the second
+    three-byte opcode map"
+    :guard (and (vex-prefixes-byte0-p vex-prefixes)
+		(modr/m-p modr/m)
+		(sib-p sib))
+    :guard-hints (("Goal"
+		   :do-not '(preprocess)
+		   :in-theory (e/d ()
+				   (unsigned-byte-p
+				    signed-byte-p
+				    (:forward-chaining acl2::unsigned-byte-p-forward)
+				    ash
+				    (tau-system)))))
+
+    :returns (x86 x86p :hyp (and (canonical-address-p temp-rip)
+				 (x86p x86)))
+
+    (case opcode
+      ,@(avx-case-gen *vex-0F3A-opcodes* t state))))
+
+(define vex-decode-and-execute
+  ((proc-mode              :type (integer 0 #.*num-proc-modes-1*))
+   (start-rip              :type (signed-byte   #.*max-linear-address-size*))
+   (temp-rip               :type (signed-byte   #.*max-linear-address-size*)
+			   "@('temp-rip') points to the byte following the
+			    first two VEX prefixes that were already read and
+			    placed in the @('vex-prefixes') structure in @(tsee
+			    x86-fetch-decode-execute).")
+   (prefixes               :type (unsigned-byte #.*prefixes-width*))
+   (rex-byte               :type (unsigned-byte 8))
+   (vex-prefixes           :type (unsigned-byte 24)
+			   "Only @('byte0') and @('byte1') fields are populated
+			    when this function is called.")
+   x86)
+
+  :guard (and (prefixes-p prefixes)
+	      (vex-prefixes-byte0-p vex-prefixes))
+
+  :guard-hints
+  (("Goal"
+    :in-theory
+    (e/d (modr/m-p
+	  vex-prefixes-byte0-p
+	  vex-prefixes-map-p add-to-*ip
+	  add-to-*ip-is-i48p-rewrite-rule)
+	 (bitops::logand-with-negated-bitmask not (tau-system)))))
+  :prepwork
+  ((local
+    (defthm vex-decode-and-execute-guard-helper
+      (implies (and (unsigned-byte-p 8 byte-1)
+		    (unsigned-byte-p 8 byte-2))
+	       (and
+		(<
+		 (logior
+		  (logand #xffffff00
+			  (logior (logand #xffffffff00ffffff vex-prefixes)
+				  (ash byte-1
+				       24)))
+		  byte-2)
+		 4294967296)
+		(<=
+		 0
+		 (logior byte-1
+			 (logand #xffffff00
+				 (logior (logand #xffffffff00ffffff vex-prefixes)
+					 (ash byte-2 24)))))
+		(<
+		 (logior byte-1
+			 (logand #xffffff00
+				 (logior (logand #xffffffff00ffffff vex-prefixes)
+					 (ash byte-2 24))))
+		 4294967296)))))
+
+   (local
+    (defthm logtail-16-of-unsigned-byte-p-8
+      (implies (unsigned-byte-p 8 byte)
+	       (equal (logtail 16 byte) 0))))
+
+   (local
+    (defthm loghead-ash-0
+      (implies (and (natp i)
+		    (natp j)
+		    (natp x)
+		    (<= i j))
+	       (equal (loghead i (ash x j))
+		      0))
+      :hints (("Goal"
+	       :in-theory (e/d* (acl2::ihsext-inductions
+				 acl2::ihsext-recursive-redefs)
+				()))))))
+
+  :parents (x86-decoder)
+
+  :long "<p>@('vex-decode-and-execute') dispatches control to VEX-encoded
+  instructions.</p>
+
+  <p><i>Reference: Intel Vol. 2A, Section 2.3: Intel Advanced Vector
+   Extensions (Intel AVX)</i></p>"
+
+  (b* ((ctx 'vex-decode-and-execute)
+       ;; Reference for the following checks that lead to #UD:
+       ;; Intel Vol. 2,
+       ;; Section 2.3.2 - VEX and the LOCK prefix
+       ;; Section 2.3.3 - VEX and the 66H, F2H, and F3H prefixes
+       ;; Section 2.3.4 - VEX and the REX prefix
+
+       ;; Any VEX-encoded instruction with mandatory (SIMD) prefixes, lock
+       ;; prefix, and REX prefixes (i.e., 66, F2, F3, F0, and 40-4F) preceding
+       ;; VEX will #UD.  Therefore, we fetch VEX prefixes after the legacy
+       ;; prefixes (function get-prefixes) and the REX prefix have been
+       ;; detected and fetched in x86-fetch-decode-execute.
+
+       ((when (not (equal rex-byte 0)))
+	(!!fault-fresh :ud :vex-prefixes vex-prefixes :rex rex-byte))
+       ;; TODO: Intel Vol. 2A Sections 2.3.2 and 2.3.3 say "Any VEX-encoded
+       ;; instruction with a LOCK/66H/F2H/F3H prefix preceding VEX will #UD."
+       ;; So, should I check :last-byte here instead?
+       ((when (equal (the (unsigned-byte 8) (prefixes->lck prefixes)) #.*lock*))
+	(!!fault-fresh :ud :vex-prefixes vex-prefixes :lock-prefix))
+       ((when (equal (the (unsigned-byte 8) (prefixes->rep prefixes)) #.*mandatory-f2h*))
+	(!!fault-fresh :ud :vex-prefixes vex-prefixes :F2-prefix))
+       ((when (equal (the (unsigned-byte 8) (prefixes->rep prefixes)) #.*mandatory-f3h*))
+	(!!fault-fresh :ud :vex-prefixes vex-prefixes :F3-prefix))
+       ((when (equal (the (unsigned-byte 8) (prefixes->opr prefixes)) #.*mandatory-66h*))
+	(!!fault-fresh :ud :vex-prefixes vex-prefixes :66-prefix))
+
+       (vex2-prefix?
+	(equal (vex-prefixes-slice :byte0 vex-prefixes) #.*vex2-byte0*))
+       (vex3-prefix?
+	(equal (vex-prefixes-slice :byte0 vex-prefixes) #.*vex3-byte0*))
+       (vex-byte1 (vex-prefixes-slice :byte1 vex-prefixes))
+       ;; VEX3 Byte 1 #UD Checks for M-MMMM field:
+       ;; References: Intel Vol. 2, Figure 2-9 and Section 2.3.6.1
+       ((mv vex3-0F-map? vex3-0F38-map? vex3-0F3A-map?)
+	(if vex3-prefix?
+	    (mv (equal (vex3-byte1-slice :m-mmmm vex-byte1) #.*v0F*)
+		(equal (vex3-byte1-slice :m-mmmm vex-byte1) #.*v0F38*)
+		(equal (vex3-byte1-slice :m-mmmm vex-byte1) #.*v0F3A*))
+	  (mv nil nil nil)))
+       ((when (and vex3-prefix?
+		   (not (or vex3-0F-map? vex3-0F38-map? vex3-0F3A-map?))))
+	(!!fault-fresh :ud :vex-prefixes vex-prefixes :m-mmmm vex-byte1))
+
+       ;; Completely populating the vex-prefixes structure --- filling out only
+       ;; next-byte for vex2-prefixes and both byte2 and next-byte for
+       ;; vex3-prefix:
+       ((mv flg0 (the (unsigned-byte 8) byte2/next-byte) x86)
+	(rme08 proc-mode temp-rip #.*cs* :x x86))
+       ((when flg0)
+	(!!ms-fresh :vex-byte2/next-byte-read-error flg0))
+       ((mv flg1 temp-rip)
+	(add-to-*ip proc-mode temp-rip 1 x86))
+       ((when flg1)
+	(!!ms-fresh :increment-error flg1))
+       (vex-prefixes
+	(if vex3-prefix?
+	    (!vex-prefixes-slice :byte2 byte2/next-byte vex-prefixes)
+	  vex-prefixes))
+       ((mv flg2 (the (unsigned-byte 8) next-byte) x86)
+	(if vex3-prefix?
+	    (rme08 proc-mode temp-rip #.*cs* :x x86)
+	  (mv nil 0 x86)))
+       ((when flg2)
+	(!!ms-fresh :next-byte-read-error flg2))
+       ((mv flg3 temp-rip)
+	(if vex3-prefix?
+	    (add-to-*ip proc-mode temp-rip 1 x86)
+	  (mv nil temp-rip)))
+       ((when flg3)
+	(!!ms-fresh :increment-error flg3))
+
+       (opcode
+	(the (unsigned-byte 8)
+	  (if vex3-prefix? next-byte byte2/next-byte)))
+
+       (modr/m? (vex-opcode-ModR/M-p vex-prefixes opcode))
+       ((mv flg4 (the (unsigned-byte 8) modr/m) x86)
+	(if modr/m?
+	    (rme08 proc-mode temp-rip #.*cs* :x x86)
+	  (mv nil 0 x86)))
+       ((when flg4)
+	(!!ms-fresh :modr/m-byte-read-error flg4))
+       ((mv flg5 temp-rip)
+	(if modr/m?
+	    (add-to-*ip proc-mode temp-rip 1 x86)
+	  (mv nil temp-rip)))
+       ((when flg5) (!!ms-fresh :increment-error flg5))
+
+       (sib? (and modr/m?
+		  (b* ((p4? (eql #.*addr-size-override*
+				 (the (unsigned-byte 8) (prefixes->adr prefixes))))
+		       (16-bit-addressp (eql 2 (select-address-size proc-mode p4? x86))))
+		    (x86-decode-SIB-p modr/m 16-bit-addressp))))
+       ((mv flg6 (the (unsigned-byte 8) sib) x86)
+	(if sib?
+	    (rme08 proc-mode temp-rip #.*cs* :x x86)
+	  (mv nil 0 x86)))
+       ((when flg6)
+	(!!ms-fresh :sib-byte-read-error flg6))
+       ((mv flg7 temp-rip)
+	(if sib?
+	    (add-to-*ip proc-mode temp-rip 1 x86)
+	  (mv nil temp-rip)))
+       ((when flg7) (!!ms-fresh :increment-error flg7)))
+
+    (cond
+     ((mbe :logic (vex-prefixes-map-p #ux0F vex-prefixes)
+	   :exec (or vex2-prefix? (and vex3-prefix? vex3-0F-map?)))
+      (vex-0F-execute start-rip temp-rip vex-prefixes opcode modr/m sib x86))
+     ((mbe :logic (vex-prefixes-map-p #ux0F_38 vex-prefixes)
+	   :exec (and vex3-prefix? vex3-0F38-map?))
+      (vex-0F38-execute start-rip temp-rip vex-prefixes opcode modr/m sib x86))
+     ((mbe :logic (vex-prefixes-map-p #ux0F_3A vex-prefixes)
+	   :exec (and vex3-prefix? vex3-0F3A-map?))
+      (vex-0F3A-execute start-rip temp-rip vex-prefixes opcode modr/m sib x86))
+     (t
+      ;; Unreachable.
+      (!!ms-fresh :illegal-value-of-VEX-m-mmmm))))
+
+  ///
+
+  (defthm x86p-vex-decode-and-execute
+    (implies (and (x86p x86)
+		  (canonical-address-p temp-rip))
+	     (x86p
+	      (vex-decode-and-execute
+	       proc-mode
+	       start-rip temp-rip prefixes rex-byte vex-prefixes x86)))
+    :hints (("Goal" :in-theory (e/d (add-to-*ip add-to-*ip-is-i48p-rewrite-rule)
+				    ())))))
+
+;; ----------------------------------------------------------------------
