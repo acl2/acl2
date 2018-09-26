11745f87a5db33c7de319c869706920a02cc6def
Matt Kaufmann <kaufmann@cs.utexas.edu>
2018-09-26 18:06:39 -0500

    Updated to a more recent date in purity script, for efficiency.

--- bin/purity.sh
+++ bin/purity.sh
@@ -12,7 +12,7 @@ fi
 export PAGER=cat
 
 # Matt might update this when confident of purity since the indicated date.
-export SINCE=2018-04-11
+export SINCE=2018-09-24
 
 export basecmd="\
 git log \
