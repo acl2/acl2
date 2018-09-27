806a5d858124aaaf7ba3be52bd9843c04334e1e4
pete@atlas <pmanolios@gmail.com>
2018-09-27 17:28:34 -0400

    Added a proof builder macro that repeats instructions until all goals have been proved

---
+++ books/acl2s/utilities.lisp
@@ -0,0 +1,29 @@
+; Copyright (C) 2018, Northeastern University
+; Written by Pete Manolios
+; License: A 3-clause BSD license.  See the LICENSE file distributed with ACL2.
+(include-book ;; Newline to fool ACL2/cert.pl dependency scanner
+ "portcullis")
+(begin-book t :ttags :all);$ACL2s-Preamble$|#
+
+(in-package "ACL2S")
+(include-book "kestrel/utilities/proof-builder-macros" :dir :system)
+
+(defxdoc acl2-pc::repeat-until-done
+  :parents (proof-builder-commands)
+  :short "(macro)
+Repeat the given instructions until all goals have been proved"
+  :long "@({
+ Example:
+ (repeat-until-done induct (repeat bash))
+
+ General Form:
+ (repeat-until-done instr1 ... instrk)
+ })
+
+ <p>where each @('instri') is a proof-builder instruction.</p>")
+
+(define-pc-macro repeat-until-done (&rest instrs)
+  (value
+   `(repeat (do-all
+             ,@(append instrs 
+                       `((negate (when-not-proved fail))))))))
