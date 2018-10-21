a91d76310ef5cbf40e8bc038af3f497aa671412d
Alessandro Coglio <coglio@kestrel.edu>
2018-10-21 10:51:26 -0700

    Extend the DIGITS-ANY-BASE library.
    Add a macro to generate additional return type theorems for the conversions from
    natural numbers to digits.
    
    The library uses the recognizers DAB-DIGITP and DAB-DIGIT-LISTP, which are
    parameterized on an arbitrary base >= 2. In particular, the coversions from
    natural numbers to digits have return type theorems with DAB-DIGIT-LISTP.
    
    Since these digit recognizers are, for certain bases, equivalent to certain
    other predicates (e.g. (DAB-DIGIT(-LIST)P 256 X) = (UNSIGNED-BYTE-(LIST)P 8 X)),
    it is convenient to generate additional return type theorems with these other
    predicates for the aforementioned conversions. This new macro does that.
    
    This new macro supersedes the less general macro DEFTHM-DIGIT-BYTE-RETURN-TYPES
    in pow2.lisp. This less general macro will be removed, and its uses will be
    replaced with uses of the new macro.
    
    Also add some tests for the new macro.

--- books/kestrel/utilities/digits-any-base/core.lisp
+++ books/kestrel/utilities/digits-any-base/core.lisp
@@ -1358,3 +1358,169 @@
                                                                 digits2)))
     :disable (nat=>bendian-of-bendian=>nat
               bendian=>nat-of-dab-digit-list-fix-digits)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define defthm-dab-return-types-fn ((eq-thm-name symbolp)
+                                    (prefix symbolp)
+                                    (topic symbolp)
+                                    (topicp booleanp)
+                                    (parents symbol-listp)
+                                    (parentsp booleanp)
+                                    (short stringp)
+                                    (shortp booleanp)
+                                    (long stringp)
+                                    (longp booleanp)
+                                    (wrld plist-worldp))
+  :returns (event "A @(tsee pseudo-event-formp).")
+  :mode :program
+  :parents (defthm-dab-return-types)
+  :short "Event form to introduce additional return type theorems for
+          the conversions from natural numbers to digits."
+  :long
+  (xdoc::topp
+   "See @(tsee defthm-dab-return-types).")
+  (b* ((equality (formula eq-thm-name nil wrld))
+       (dab-digit-listp-call (cadr equality))
+       (base (cadr dab-digit-listp-call))
+       (var (caddr dab-digit-listp-call))
+       (term (caddr equality))
+       (nat=>bendian*-thm-name (packn (list prefix '-nat=>bendian*)))
+       (nat=>bendian+-thm-name (packn (list prefix '-nat=>bendian+)))
+       (nat=>bendian-thm-name (packn (list prefix '-nat=>bendian)))
+       (nat=>lendian*-thm-name (packn (list prefix '-nat=>lendian*)))
+       (nat=>lendian+-thm-name (packn (list prefix '-nat=>lendian+)))
+       (nat=>lendian-thm-name (packn (list prefix '-nat=>lendian)))
+       (nat=>bendian*-call (fcons-term* 'nat=>bendian* base 'nat))
+       (nat=>bendian+-call (fcons-term* 'nat=>bendian+ base 'nat))
+       (nat=>bendian-call (fcons-term* 'nat=>bendian base 'width 'nat))
+       (nat=>lendian*-call (fcons-term* 'nat=>lendian* base 'nat))
+       (nat=>lendian+-call (fcons-term* 'nat=>lendian+ base 'nat))
+       (nat=>lendian-call (fcons-term* 'nat=>lendian base 'width 'nat))
+       (nat=>bendian*-term (fsubcor-var
+                            (list var) (list nat=>bendian*-call) term))
+       (nat=>bendian+-term (fsubcor-var
+                            (list var) (list nat=>bendian+-call) term))
+       (nat=>bendian-term (fsubcor-var
+                           (list var) (list nat=>bendian-call) term))
+       (nat=>lendian*-term (fsubcor-var
+                            (list var) (list nat=>lendian*-call) term))
+       (nat=>lendian+-term (fsubcor-var
+                            (list var) (list nat=>lendian+-call) term))
+       (nat=>lendian-term (fsubcor-var
+                           (list var) (list nat=>lendian-call) term))
+       (theorems
+        `((defthm ,nat=>bendian*-thm-name
+            ,nat=>bendian*-term
+            :hints (("Goal"
+                     :use ((:instance return-type-of-nat=>bendian* (base ,base))
+                           (:instance ,eq-thm-name (,var ,nat=>bendian*-call)))
+                     :in-theory nil)))
+          (defthm ,nat=>bendian+-thm-name
+            ,nat=>bendian+-term
+            :hints (("Goal"
+                     :use ((:instance return-type-of-nat=>bendian+ (base ,base))
+                           (:instance ,eq-thm-name (,var ,nat=>bendian+-call)))
+                     :in-theory nil)))
+          (defthm ,nat=>bendian-thm-name
+            ,nat=>bendian-term
+            :hints (("Goal"
+                     :use ((:instance return-type-of-nat=>bendian (base ,base))
+                           (:instance ,eq-thm-name (,var ,nat=>bendian-call)))
+                     :in-theory nil)))
+          (defthm ,nat=>lendian*-thm-name
+            ,nat=>lendian*-term
+            :hints (("Goal"
+                     :use ((:instance return-type-of-nat=>lendian* (base ,base))
+                           (:instance ,eq-thm-name (,var ,nat=>lendian*-call)))
+                     :in-theory nil)))
+          (defthm ,nat=>lendian+-thm-name
+            ,nat=>lendian+-term
+            :hints (("Goal"
+                     :use ((:instance return-type-of-nat=>lendian+ (base ,base))
+                           (:instance ,eq-thm-name (,var ,nat=>lendian+-call)))
+                     :in-theory nil)))
+          (defthm ,nat=>lendian-thm-name
+            ,nat=>lendian-term
+            :hints (("Goal"
+                     :use ((:instance return-type-of-nat=>lendian (base ,base))
+                           (:instance ,eq-thm-name (,var ,nat=>lendian-call)))
+                     :in-theory nil))))))
+    (if topicp
+        `(defsection ,topic
+           ,@(and parentsp (list :parents parents))
+           ,@(and shortp (list :short short))
+           ,@(and longp (list :long long))
+           ,@theorems)
+      `(encapsulate
+         ()
+         ,@theorems))))
+
+(defsection defthm-dab-return-types
+  :short "Introduce additional return type theorems for
+          the conversions from natural numbers to digits."
+  :long
+  (xdoc::topapp
+   (xdoc::p
+    "Given an equality theorem of the form
+     @('(equal (dab-digit-listp BASE VAR) TERM<VAR>)'),
+     where @('BASE') is an integer greater than 1,
+     @('VAR') is a variable,
+     and @('TERM<VAR>') is a term with @('VAR') as the only free variable,
+     this macro generates six theorems of the form
+     @('TERM<(CONV BASE nat)>') or @('TERM<(CONV BASE width nat)>'),
+     where @('CONV') is one of the conversions
+     @(tsee nat=>bendian*), @(tsee nat=>bendian+), @(tsee nat=>bendian),
+     @(tsee nat=>lendian*), @(tsee nat=>lendian+), and @(tsee nat=>lendian),
+     where @('TERM<(CONV BASE nat)>') is the result of
+     replacing @('VAR') with @('(CONV BASE nat)') in @('TERM'),
+     and where @('TERM<(CONV BASE width nat)>') is the result of
+     replacing @('VAR') with @('(CONV BASE width nat)') in @('TERM')
+     These are additional return type theorems for these conversions.")
+   (xdoc::p
+    "The name of the equality theorem
+     is the first argument of this macro.
+     The name of each generated return type theorem is @('PREFIX-CONV'),
+     where @('PREFIX') is the second argument of this macro
+     and @('CONV') is the conversion.")
+   (xdoc::p
+    "The theorems are generated inside a @(tsee defsection)
+     whose topic (name), @(':parents'), @(':short'), and @(':long')
+     are supplied as keyword arguments.
+     If the topic is not supplied, no @(tsee defsection) is supplied).
+     If any of @(':parents'), @(':short'), or @(':long') is not supplied,
+     the @(tsee defsection) has no corresponding
+     @(':parents'), @(':short'), or @(':long').")
+   (xdoc::p
+    "This macro does not do a thorough validation of the inputs.
+     However, its implementation is quite simple,
+     and understanding failures due to incorrect inputs should be easy.
+     Regardless, this macro may be extended
+     to do more thorough input validation in the future.")
+   (xdoc::def "defthm-dab-return-types"))
+
+  (defmacro defthm-dab-return-types (eq-thm-name
+                                     prefix
+                                     &key
+                                     (topic 'nil topicp)
+                                     (parents 'nil parentsp)
+                                     (short '"" shortp)
+                                     (long '"" longp))
+    (declare (xargs :guard (and (symbolp eq-thm-name)
+                                (symbolp prefix)
+                                (symbolp topic)
+                                (symbol-listp parents)
+                                (stringp short)
+                                (stringp long))))
+    `(make-event (defthm-dab-return-types-fn
+                   ',eq-thm-name
+                   ',prefix
+                   ',topic
+                   ',topicp
+                   ',parents
+                   ',parentsp
+                   ',short
+                   ',shortp
+                   ',long
+                   ',longp
+                   (w state)))))
