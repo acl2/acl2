73b8d628366b920d4cace207bc8a9249a93e5a06
Alessandro Coglio <coglio@kestrel.edu>
2018-11-09 09:11:42 -0800

    Improve/clarify some documentation.

--- books/projects/x86isa/machine/segmentation.lisp
+++ books/projects/x86isa/machine/segmentation.lisp
@@ -280,13 +280,19 @@
    <p>
    In 32-bit mode,
    the effective address is added to the base address of the segment;
-   the result is truncated to 32 bits, in case;
-   this truncation should not actually happen with well-formed segments.
+   the result is truncated to 32 bits,
+   because the addition should be modulo 2^32,
+   given that the result must be 32 bits
+   (cf. aforementioned figures in Intel and AMD manuals).
    In 64-bit mode,
    the addition of the base address of the segment is performed
    only if the segments are in registers FS and GS;
-   the result is truncated to 64 bits, in case;
-   this truncation should not actually happen with well-formed segments.
+   the result is truncated to 64 bits,
+   because the addition should be modulo 2^64,
+   given that the result must be 32 bits
+   (cf. aforementioned figures in Intel and AMD manuals);
+   since in our model addresses are signed,
+   we use @('i64') instead of @('n64') to perform this truncation.
    </p>
    <p>
    If the translation is successful,
