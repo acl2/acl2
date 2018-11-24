62d2e6b963950e5d872f468006b90f730f1a294d
Alessandro Coglio <coglio@kestrel.edu>
2018-11-23 16:01:31 -0800

    Remove some extra spaces.

--- books/projects/x86isa/portcullis/sharp-dot-constants.lisp
+++ books/projects/x86isa/portcullis/sharp-dot-constants.lisp
@@ -325,8 +325,8 @@
 (defconst *fp-b*   15) ;; FPU Busy
 
 (defconst *fp-status-names*
-  (list *fp-ie* *fp-de* *fp-ze* *fp-oe*  *fp-ue*  *fp-pe* *fp-sf*
-        *fp-es* *fp-c0* *fp-c1* *fp-c2*  *fp-top* *fp-c3* *fp-b*))
+  (list *fp-ie* *fp-de* *fp-ze* *fp-oe* *fp-ue* *fp-pe* *fp-sf*
+        *fp-es* *fp-c0* *fp-c1* *fp-c2* *fp-top* *fp-c3* *fp-b*))
 
 ;; MXCSR (Intel Manual, Feb'14, Vol. 1, Section 10.2.3)
 
@@ -355,9 +355,9 @@
 
 
 (defconst *mxcsr-names*
-  (list *mxcsr-ie* *mxcsr-de*  *mxcsr-ze* *mxcsr-oe* *mxcsr-ue*
+  (list *mxcsr-ie* *mxcsr-de* *mxcsr-ze* *mxcsr-oe* *mxcsr-ue*
         *mxcsr-pe* *mxcsr-daz* *mxcsr-im* *mxcsr-dm* *mxcsr-zm*
-        *mxcsr-om* *mxcsr-um*  *mxcsr-pm* *mxcsr-rc* *mxcsr-fz*
+        *mxcsr-om* *mxcsr-um* *mxcsr-pm* *mxcsr-rc* *mxcsr-fz*
         *mxcsr-reserved*))
 
 
@@ -687,7 +687,7 @@
 (defun define-general-purpose-registers ()
 
   `(defconsts (*RAX* *RCX* *RDX* *RBX* *RSP* *RBP* *RSI* *RDI*
-                     *R8*  *R9*  *R10* *R11* *R12* *R13* *R14* *R15*
+                     *R8* *R9* *R10* *R11* *R12* *R13* *R14* *R15*
                      *64-bit-general-purpose-registers-len*)
      ,(b* ((lst (gl-int 0 1 16))
            (len  (len lst)))
@@ -696,7 +696,7 @@
 (defun define-32-bit-general-purpose-registers ()
 
   `(defconsts (*EAX* *ECX* *EDX* *EBX* *ESP* *EBP* *ESI* *EDI*
-                     *R8d*  *R9d*  *R10d* *R11d* *R12d* *R13d* *R14d* *R15d*)
+                     *R8d* *R9d* *R10d* *R11d* *R12d* *R13d* *R14d* *R15d*)
      ,(b* ((lst (gl-int 0 1 15))
            (len  (len lst)))
         (cons 'mv (append lst (list len))))))
@@ -741,7 +741,7 @@
                      ;; (Task Priority Register) available only in 64
                      ;; bit mode
                ;; cr9 thru cr15 are not implemented in our model yet.
-               *CR9*  *CR10* *CR11* *CR12* *CR13* *CR14* *CR15*
+               *CR9* *CR10* *CR11* *CR12* *CR13* *CR14* *CR15*
                *XCR0*
                *control-register-names-len*)
      ,(b* ((lst (gl-int 0 1 17))
