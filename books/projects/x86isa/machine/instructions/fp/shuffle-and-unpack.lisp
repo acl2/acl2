4ab995e81eea25e1903b51c83d58632c32a151cc
Alessandro Coglio <coglio@kestrel.edu>
2018-12-29 17:40:25 -0800

    [X86ISA] Extend SHUFPD to 32-bit mode.

--- books/projects/x86isa/machine/instructions/fp/shuffle-and-unpack.lisp
+++ books/projects/x86isa/machine/instructions/fp/shuffle-and-unpack.lisp
@@ -115,8 +115,7 @@
         (xmmi-size 16 xmm-index x86))
 
        (p2 (prefixes->seg prefixes))
-       (p4? (eql #.*addr-size-override*
-                 (prefixes->adr prefixes)))
+       (p4? (eql #.*addr-size-override* (prefixes->adr prefixes)))
 
        (seg-reg (select-segment-register proc-mode p2 p4? mod r/m x86))
 
@@ -210,12 +209,9 @@
   :body
   (b* ((ctx 'x86-shufpd-Op/En-RMI)
 
-       ((when (not (equal proc-mode #.*64-bit-mode*)))
-        (!!ms-fresh :unimplemented-in-32-bit-mode))
-
-       (r/m (the (unsigned-byte 3) (modr/m->r/m  modr/m)))
-       (mod (the (unsigned-byte 2) (modr/m->mod  modr/m)))
-       (reg (the (unsigned-byte 3) (modr/m->reg  modr/m)))
+       (r/m (the (unsigned-byte 3) (modr/m->r/m modr/m)))
+       (mod (the (unsigned-byte 2) (modr/m->mod modr/m)))
+       (reg (the (unsigned-byte 3) (modr/m->reg modr/m)))
 
        ((the (unsigned-byte 4) xmm-index)
         (reg-index reg rex-byte #.*r*))
@@ -224,52 +220,50 @@
 
        (p2 (prefixes->seg prefixes))
        (p4? (eql #.*addr-size-override* (prefixes->adr prefixes)))
+
+       (seg-reg (select-segment-register proc-mode p2 p4? mod r/m x86))
+
        ;; Cuong: Although this requirement is not specified in the
        ;; Intel manual, I got a segmentation fault when trying with
        ;; non 16-byte aligned addresses on a real machine.
        (inst-ac? ;; Exceptions Type 4
         t) ;; This should be nil according to the Intel manuals, but
        ;; see comment above.
-
        ((mv flg0
             (the (unsigned-byte 128) xmm/mem)
             (the (integer 0 4) increment-RIP-by)
-            (the (signed-byte 64) ?v-addr) x86)
-        (x86-operand-from-modr/m-and-sib-bytes proc-mode
-         #.*xmm-access* 16 inst-ac?
-         nil ;; Not a memory pointer operand
-         p2 p4? temp-rip rex-byte r/m mod sib
-         1 ;; One-byte immediate operand
-         x86))
-
+            (the (signed-byte 64) ?addr) x86)
+        (x86-operand-from-modr/m-and-sib-bytes$ proc-mode
+                                                #.*xmm-access*
+                                                16
+                                                inst-ac?
+                                                nil ;; Not a memory pointer operand
+                                                seg-reg
+                                                p4?
+                                                temp-rip
+                                                rex-byte
+                                                r/m
+                                                mod
+                                                sib
+                                                1 ;; One-byte immediate operand
+                                                x86))
        ((when flg0)
         (!!ms-fresh :x86-operand-from-modr/m-and-sib-bytes flg0))
 
-       ((the (signed-byte #.*max-linear-address-size+1*) temp-rip)
-        (+ temp-rip increment-RIP-by))
-
-       ((when (mbe :logic (not (canonical-address-p temp-rip))
-                   :exec (<= #.*2^47*
-                             (the (signed-byte
-                                   #.*max-linear-address-size+1*)
-                               temp-rip))))
-        (!!ms-fresh :temp-rip-not-canonical temp-rip))
+       ((mv flg (the (signed-byte #.*max-linear-address-size*) temp-rip))
+        (add-to-*ip proc-mode temp-rip increment-RIP-by x86))
+       ((when flg) (!!ms-fresh :rip-increment-error flg))
 
        ((mv flg1 (the (unsigned-byte 8) imm) x86)
-        (rml-size 1 (the (signed-byte #.*max-linear-address-size*)
-                     temp-rip) :x x86))
-
+        (rme-size-opt proc-mode 1
+                      (the (signed-byte #.*max-linear-address-size*) temp-rip)
+                      #.*cs* :x nil x86 :mem-ptr? nil))
        ((when flg1)
-        (!!ms-fresh :rml-size-error flg1))
+        (!!ms-fresh :imm-rme-size-error flg1))
 
-       ((the (signed-byte #.*max-linear-address-size+1*) temp-rip)
-        (1+ temp-rip))
-       ((when (mbe :logic (not (canonical-address-p temp-rip))
-                   :exec (<= #.*2^47*
-                             (the (signed-byte
-                                   #.*max-linear-address-size+1*)
-                               temp-rip))))
-        (!!ms-fresh :temp-rip-not-canonical temp-rip))
+       ((mv flg (the (signed-byte #.*max-linear-address-size*) temp-rip))
+        (add-to-*ip proc-mode temp-rip 1 x86))
+       ((when flg) (!!ms-fresh :rip-increment-error flg))
 
        (badlength? (check-instruction-length start-rip temp-rip 0))
        ((when badlength?)
@@ -286,8 +280,10 @@
        ;; Update the x86 state:
        (x86 (!xmmi-size 16 xmm-index result x86))
 
-       (x86 (!rip temp-rip x86)))
-    x86))
+       (x86 (write-*ip proc-mode temp-rip x86)))
+    x86)
+
+  :guard-hints (("Goal" :in-theory (enable rme-size))))
 
 (def-inst x86-unpck?ps-Op/En-RM
 
