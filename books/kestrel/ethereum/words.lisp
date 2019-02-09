a895a921974000fbc6f11a10bd848fc33ed728fc
Alessandro Coglio <coglio@kestrel.edu>
2019-02-08 21:29:46 -0800

    [ETHEREUM] Add a model of words (256-bit).

---
+++ books/kestrel/ethereum/words.lisp
@@ -0,0 +1,31 @@
+; Ethereum Library -- Words
+;
+; Copyright (C) 2019 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ETHEREUM")
+
+(include-book "kestrel/utilities/fixbytes/defbyte" :dir :system)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defxdoc words
+  :parents (basics)
+  :short "Words."
+  :long
+  (xdoc::topp
+   "[YP:9.1] defines the size of words as 256 bits.
+    We formalize words as
+    the elements of the set @($\\mathbb{N}_{256}$) [YP:3],
+    i.e. natural numbers below 256."))
+
+(fty::defbyte 256
+  :type word
+  :pred wordp
+  :parents (words)
+  :description "words")
