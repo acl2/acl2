e142ec1067696a0b4c05d4f551b0b8b8b4f16840
pete@atlas <pmanolios@gmail.com>
2019-02-14 19:47:12 -0500

    updated the documentation for ACL2s utilities and added a well-founded relation on the natural numbers, which is useful for checking explicit measures in the undergraduate CS2800 course

--- books/acl2s/utilities.lisp
+++ books/acl2s/utilities.lisp
@@ -12,21 +12,28 @@
 (defxdoc ACL2s-utilities
   :parents (acl2::acl2-sedan)
   :short "Utilities used in ACL2s."
-  :long "This is a collection of utilities used in ACL2s, the ACL2 Sedan.")
+  :long "<p>
+This is a collection of utilities used in ACL2s, the ACL2 Sedan.
+</p>
+")
 
 (defxdoc acl2-pc::repeat-until-done
   :parents (acl2::proof-builder-commands acl2s-utilities)
   :short "A proof-builder command that repeats the given instructions
   until all goals have been proved" 
-  :long "@({
+  :long "<p>
+@({
  Example:
  (repeat-until-done induct (repeat bash))
 
  General Form:
  (repeat-until-done instr1 ... instrk)
  })
+</p>
 
- <p>where each @('instri') is a proof-builder instruction.</p>")
+<p>where each @('instri') is a proof-builder instruction.
+</p>
+")
 
 (define-pc-macro repeat-until-done (&rest instrs)
   (value
@@ -40,7 +47,8 @@
 creates an arbitrary-arity macro given a binary function
 and associates the function name with the macro name using
 @(see add-macro-fn)."
-  :long "@({
+  :long "<p>
+@({
  Examples:
  (make-n-ary-macro set-union binary-set-union nil t)
 
@@ -49,8 +57,9 @@ and associates the function name with the macro name using
  General Form:
  (make-n-ary-macro new-macro-name binary-fun-name identity right-associate-p)
  })
-
- <p>where @('new-macro-name') is the name of the macro to define,
+</p>
+ 
+<p>where @('new-macro-name') is the name of the macro to define,
 @('binary-fun-name') is the name of an existing binary function and
 @('identity') is what the macro should return with no arguments.
 @('right-associate-p') is an optional argument, which when set to
@@ -75,7 +84,8 @@ the function. For example:
 
 and so on.
 })
-</p>")
+</p>
+")
 
 (defmacro make-n-ary-macro (macro bin-fun id &optional
                                   right-associate-p)
@@ -89,15 +99,16 @@ and so on.
      (add-macro-fn ,macro ,bin-fun ,right-associate-p)))
 
 (defxdoc test-then-skip-proofs
-  :parents (acl2s-utilities)
-  :short "The ACL2s version of skip-proofs."
-  :long
-"A macro that is similar to skip-proofs, except that we first perform
+  :parents (acl2s-utilities cgen)
+  :short "The ACL2s version of @('skip-proofs')."
+  :long"<p>
+A macro that is similar to @('skip-proofs'), except that we first perform
 testing. The macro supports testing for @(see thm), 
 @(see defthm), @(see defcong), @(see defequiv), and
 @(see defrefinement) forms. All other forms are just turned into
-skip-proofs forms, without testing."
-)
+@('skip-proof')s forms, without testing.
+</p>
+")
 
 ;; If there are opportunities to do so, we should extend
 ;; test-then-skip-proofs so that it supports more forms.
@@ -120,6 +131,15 @@ skip-proofs forms, without testing."
                                (skip-proofs ,',thm))))))
    (t `(skip-proofs ,thm))))
 
+(defxdoc thm-no-test
+  :parents (acl2s-utilities cgen)
+  :short "A version of @('thm') with testing disabled."
+  :long"<p>
+A macro that uses @('with-outer-locals') to locally turn off
+@('cgen') testing and then calls @('thm').
+</p>
+")
+
 (defmacro thm-no-test (&rest args)
   `(acl2::with-outer-locals
     (local (acl2s-defaults :set testing-enabled nil))
@@ -128,6 +148,15 @@ skip-proofs forms, without testing."
                         (declare (ignore val))
                         (cond (erp (er soft 'thm "The thm failed"))
                               (t (value `(value-triple :passed))))))))
+
+(defxdoc defthm-no-test
+  :parents (acl2s-utilities cgen)
+  :short "A version of @('defthm') with testing disabled."
+  :long"<p>
+A macro that uses @('with-outer-locals') to locally turn off
+@('cgen') testing and then calls @('defthm').
+</p>
+")
 
 (defmacro defthm-no-test (name &rest args)
   `(acl2::with-outer-locals
@@ -173,3 +202,29 @@ skip-proofs forms, without testing."
   (declare (xargs :guard (alistp alist)))
   (cdr (assoc-equal key alist)))
 
+(defxdoc n<
+  :parents (acl2s-utilities acl2::well-founded-relation)
+  :short "The well-founded less-than relation on natural numbers."
+  :long "<p>
+If @('x') and @('y') are both natural numbers then @('(n< x y)') is true
+iff @('x') is strictly less than @('y'). @('n<') is well-founded on the natural
+numbers and is useful for beginners who want to use measure
+functions over natural numbers.
+</p>
+")
+
+(defun nat-id (x)
+  (declare (xargs :guard (natp x)))
+  x)
+
+(defun n< (x y)
+  (declare (xargs :guard (and (natp x) (natp y))))
+  (< x y))
+
+(defthm less-than-is-well-founded-relation
+  (and (implies (natp x) (o-p (nat-id x)))
+       (implies (and (natp x)
+                     (natp y)
+                     (n< x y))
+                (o< (nat-id x) (nat-id y))))
+  :rule-classes :well-founded-relation)
