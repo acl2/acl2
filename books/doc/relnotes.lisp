e86344233967e788df685aed0ef6284a76bbadee
Alessandro Coglio <coglio@kestrel.edu>
2019-02-21 19:37:27 -0800

    [BITCOIN] Add placeholder for RIPEMD-160.

--- books/doc/relnotes.lisp
+++ books/doc/relnotes.lisp
@@ -184,8 +184,9 @@
 
  <p>Improved several names by using @('byte') instead of @('ubyte8').</p>
 
- <p>Renamed `cryptographic interface' to `cryptographic placeholders'.  Add
- placeholders for HMAC-SHA-512 and the secp256k1 elliptic curve.</p>
+ <p>Renamed `cryptographic interface' to `cryptographic placeholders'.  Added
+ placeholders for RIPEMD-160, HMAC-SHA-512, and the secp256k1 elliptic
+ curve.</p>
 
  <p>Added a formalization of BIP (Bitcoin Improvement Proposal) 32 key
  derivation and key trees.</p>
--- books/kestrel/bitcoin/crypto.lisp
+++ books/kestrel/bitcoin/crypto.lisp
@@ -100,6 +100,67 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defsection ripemd-160
+  :short "RIPEMD-160 placeholder."
+  :long
+  (xdoc::topapp
+   (xdoc::p
+    "RIPEMD-160 is specified in
+     <a href=\"https://homes.esat.kuleuven.be/~bosselae/ripemd160/pdf/AB-9601/AB-9601.pdf\"
+     >the `RIPEMD-160: A Strengthened Version of RIPEMD' document</a>.")
+   (xdoc::p
+    "According to the aforementioned document,
+     the input of RIPEMD-256 is a sequence of any number of bits,
+     or any number of bytes.
+     This is formalized by the guard of the constrained function.")
+   (xdoc::p
+    "According to the aforementioned document,
+     the output of RIPEMD-160 is a sequence of exactly 160 bits, or 20 bytes.
+     We constrain our function to return a list of 20 bytes unconditionally.")
+   (xdoc::p
+    "We also constrain our function to fix its argument to a list of bytes.")
+   (xdoc::def "ripemd-160"))
+
+  (encapsulate
+
+    (((ripemd-160 *) => *
+      :formals (bytes)
+      :guard (byte-listp bytes)))
+
+    (local
+     (defun ripemd-160 (bytes)
+       (declare (ignore bytes))
+       (make-list 20 :initial-element 0)))
+
+    (defrule byte-listp-of-ripemd-160
+      (byte-listp (ripemd-160 bytes)))
+
+    (defrule len-of-ripemd-160
+      (equal (len (ripemd-160 bytes))
+             20))
+
+    (defrule ripemd-160-fixes-input
+      (equal (ripemd-160 (byte-list-fix bytes))
+             (ripemd-160 bytes))))
+
+  (defrule true-listp-of-ripemd-160
+    (true-listp (ripemd-160 bytes))
+    :rule-classes :type-prescription)
+
+  (defrule consp-of-ripemd-160
+    (consp (ripemd-160 bytes))
+    :rule-classes :type-prescription
+    :use len-of-ripemd-160
+    :disable len-of-ripemd-160)
+
+  (defcong byte-list-equiv equal (ripemd-160 bytes) 1
+    :hints (("Goal"
+             :use (ripemd-160-fixes-input
+                   (:instance ripemd-160-fixes-input (bytes bytes-equiv)))
+             :in-theory (disable ripemd-160-fixes-input)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defsection hmac-sha-512
   :short "HMAC-SHA-512 placeholder."
   :long
