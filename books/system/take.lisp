4120f89fb24a07d79724e27d18dc0d2744c2ef97
Mihir Mehta <mihir@cs.utexas.edu>
2019-04-15 12:29:11 +0530

    Add a guard verification book

---
+++ books/system/take.lisp
@@ -0,0 +1,9 @@
+; Verify Termination and Guards of Take
+; Written by Mihir Mehta, April, 2019
+; License: A 3-clause BSD license.  See the LICENSE file distributed with ACL2.
+
+(in-package "ACL2")
+
+(verify-termination take)
+
+(verify-guards take)
