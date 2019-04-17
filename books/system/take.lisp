731c2eddf065fa28ae8da517926ef2d1953012cd
Mihir Mehta <mihir@cs.utexas.edu>
2019-04-17 09:02:36 +0530

    Remove a book which turns out to be unnecessary

--- books/system/take.lisp
+++
@@ -1,9 +0,0 @@
-; Verify Termination and Guards of Take
-; Written by Mihir Mehta, April, 2019
-; License: A 3-clause BSD license.  See the LICENSE file distributed with ACL2.
-
-(in-package "ACL2")
-
-(verify-termination take)
-
-(verify-guards take)
