3d0ae9505865227c31a149a790c8d2c4852aa73f
Alessandro Coglio <coglio@kestrel.edu>
2019-04-29 15:12:58 -0700

    [Ethereum] Update reference to latest Yellow Paper git commit.

--- books/kestrel/ethereum/top.lisp
+++ books/kestrel/ethereum/top.lisp
@@ -39,7 +39,7 @@
      "The <a href=\"https://github.com/ethereum/wiki/wiki\">Ethereum Wiki</a>,
       referenced as `[Wiki]' in the documentation of this library.")
     (xdoc::li
-     "The BYZANTIUM VERSION 78d7b9a of
+     "The BYZANTIUM VERSION 53edd02 of
       the <a href=\"https://github.com/ethereum/yellowpaper\">Ethereum
       Yellow Paper</a>,
       referenced as `[YP]' in the documentation of this library.
