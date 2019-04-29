07d714d0a16022bf44d0e0ec64c147146d178c01
Alessandro Coglio <coglio@kestrel.edu>
2019-04-28 21:05:43 -0700

    [Digits-Any-Base] Add a macro DEFDIGIT-GROUPING.
    This macro generates specialized versions of the functions to group and ungroup
    digits between two bases the larger of which is a positive power (greater than
    1) of the smaller base. It also generates specialized versions of some of the
    theorems.

--- books/doc/relnotes.lisp
+++ books/doc/relnotes.lisp
@@ -242,6 +242,11 @@
  some library fixtypes (e.g. bits and bytes as digits in base 2 and 256
  respectively).</p>
 
+ <p>A macro @(tsee defdigit-grouping) has been added, to generate specialized
+ versions of the digit grouping functions, and some theorems about them, for
+ specific pair of bases such that the larger base is a positive power, greater
+ than 1, of the smaller base.</p>
+
  <h4><see topic='@(url error-checking)'>Error-Checking Utilities</see></h4>
 
  <p>Added more error-checking functions.</p>
