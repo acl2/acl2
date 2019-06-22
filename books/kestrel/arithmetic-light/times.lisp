20dd2fce9df8a0b8127e23585c1cdf54e88a33c8
Eric Smith <ewsmith@gmail.com>
2019-06-22 01:30:51 -0700

    [arithmetic-light] Improve book about times.

--- books/kestrel/arithmetic-light/times.lisp
+++ books/kestrel/arithmetic-light/times.lisp
@@ -81,23 +81,37 @@
   :hints (("Goal" :in-theory (e/d (--BECOMES-*-OF--1)
                                   ( *-of--1)))))
 
+(defthm <-of-*-and-0
+  (implies (and (rationalp x)
+                (rationalp y))
+           (equal (< (* x y) 0)
+                  (or (and (< x 0)
+                           (< 0 y))
+                      (and (< y 0)
+                           (< 0 x)))))
+  :hints (("Goal" :cases ((< x 0)
+                          (and (< 0 X) (< Y 0))
+                          (and (< 0 X) (< 0 Y))))))
+
+;See also <-of-*-and-*-gen below.
 (defthmd <-of-*-and-*
   (implies (and (< x1 x2)
+                (< 0 y)
                 (rationalp x1)
                 (rationalp x2)
-                (rationalp y)
-                (< 0 y))
+                (rationalp y))
            (< (* x1 y) (* x2 y)))
   :hints (("Goal" :use (:instance positive
                                   (x (- x2 x1))
                                   ))))
 
+;See also <-of-*-and-*-gen below.
 (defthmd <=-of-*-and-*
   (implies (and (<= x1 x2)
+                (< 0 y)
                 (rationalp x1)
                 (rationalp x2)
-                (rationalp y)
-                (< 0 y))
+                (rationalp y))
            (<= (* x1 y) (* x2 y)))
   :hints (("Goal" :use (:instance <-of-*-and-*)
            :in-theory (disable <-of-*-and-*))))
@@ -113,14 +127,13 @@
            :in-theory (enable <-of-*-and-*
                               <=-of-*-and-*))))
 
-(defthm <-of-*-and-0
-  (implies (and (rationalp x)
-                (rationalp y))
-           (equal (< (* x y) 0)
-                  (or (and (< x 0)
-                           (< 0 y))
-                      (and (< y 0)
-                           (< 0 x)))))
-  :hints (("Goal" :cases ((< x 0)
-                          (and (< 0 X) (< Y 0))
-                          (and (< 0 X) (< 0 Y))))))
+(defthm <-of-*-cancel-2
+  (implies (and (< 0 y)
+                (rationalp y)
+                (rationalp x))
+           (equal (< (* x y) y)
+                  (< x 1)))
+  :hints (("Goal" :use (:instance <-of-*-and-*-gen
+                                  (x1 x)
+                                  (x2 1))
+           :in-theory (disable <-of-*-and-*-gen))))
