8637581e75948ffa7d6bb91b4263db36e48e52fd
Eric Smith <ewsmith@gmail.com>
2019-06-23 22:50:19 -0700

    [arithmetic-light] Add more of our material on expt to expt.lisp.

--- books/kestrel/arithmetic-light/expt.lisp
+++ books/kestrel/arithmetic-light/expt.lisp
@@ -1,6 +1,7 @@
 ; A lightweight book about the built-in function expt.
 ;
-; Copyright (C) 2019 Kestrel Institute
+; Copyright (C) 2008-2011 Eric Smith and Stanford University
+; Copyright (C) 2013-2019 Kestrel Institute
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
 ;
@@ -10,7 +11,105 @@
 
 (in-package "ACL2")
 
+;; This books deals with EXPT applied to an arbitrary first argument (the base
+;; of the exponentiation).  The book expt2.lisp deals with the special case
+;; when the base is 2.
+
+(in-theory (disable expt))
+
 (defthm integerp-of-expt
   (implies (and (integerp r)
                 (<= 0 i))
-           (integerp (expt r i))))
+           (integerp (expt r i)))
+  :hints (("Goal" :in-theory (enable expt))))
+
+(local (include-book "arithmetic-3/top" :dir :system))
+
+;; Note that RATIONALP-EXPT-TYPE-PRESCRIPTION and
+;; EXPT-TYPE-PRESCRIPTION-NON-ZERO-BASE are built-in.
+
+(defthm natp-of-expt
+  (implies (and (natp r)
+                (<= 0 i))
+           (natp (expt r i)))
+  :rule-classes (:rewrite :type-prescription))
+
+(defthm integerp-of-expt-type
+  (implies (and (integerp r)
+                (<= 0 i))
+           (integerp (expt r i)))
+  :rule-classes :type-prescription)
+
+(defthm <-of-0-and-expt
+  (implies (and (< 0 r)
+                (rationalp r))
+           (< 0 (expt r i)))
+  :rule-classes (:rewrite :type-prescription)
+  :hints (("Goal" :in-theory (enable zip expt))))
+
+(defthm equal-of-0-and-expt
+  (equal (equal 0 (expt r i))
+         (and (equal 0 (fix r))
+              (integerp i)
+              (not (equal 0 i)) ;since r^0 = 1
+              ))
+  :hints (("Goal" :in-theory (enable zip expt))))
+
+;todo: param names
+(defthm *-of-expt-combine-lemma
+  (implies (and (integerp x)
+                (integerp y)
+                (acl2-numberp r)
+                (not (equal 0 r)))
+           (equal (* (expt r (+ -1 x))
+                     (expt r (+ 1 y)))
+                  (* (expt r x) (expt r y)))))
+
+(local
+ (defthm integerp-of-expt-helper
+   (implies (posp r)
+            (equal (integerp (expt r i))
+                   (or (equal r 1)
+                       (not (integerp i))
+                       (<= 0 i))))
+   :hints (("Goal" :cases ((equal 1 r))
+            :in-theory (enable expt)))))
+
+(defthm integerp-of-expt-when-natp
+  (implies (natp r)
+           (equal (integerp (expt r i))
+                  (or (equal r 0)
+                      (equal r 1)
+                      (not (integerp i))
+                      (<= 0 i))))
+  :hints (("Goal" :cases ((equal 1 r))
+           :in-theory (enable expt))))
+
+(defthm expt-of-0-arg1
+  (equal (expt 0 i)
+         (if (zip i)
+             1
+           0))
+  :hints (("Goal" :in-theory (enable expt))))
+
+(defthm expt-of-0-arg2
+  (equal (expt r 0)
+         1)
+  :hints (("Goal" :in-theory (enable expt))))
+
+;try to enable?
+(defthmd expt-of-+
+  (implies (and (integerp i1)
+                (integerp i2))
+           (equal (expt r (+ i1 i2))
+                  (if (equal 0 (fix r))
+                      (if (equal (+ i1 i2) 0)
+                          1 ;0^0=1
+                        0)
+                    (* (expt r i1)
+                       (expt r i2))))))
+
+;expt-minus would have a name clash with rtl
+(defthm expt-of-unary--
+  (equal (expt r (- i))
+         (/ (expt r i))))
