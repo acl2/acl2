8892752fcbd545c0c23b026f04785a5e64211d35
Alessandro Coglio <coglio@kestrel.edu>
2019-06-25 11:19:36 -0700

    Tweak doc and use XDOC constructors.

--- books/kestrel/top.lisp
+++ books/kestrel/top.lisp
@@ -34,33 +34,38 @@
   :short "A collection of ACL2 books contributed mainly by Kestrel Institute."
 
   :long
-  "<img src='res/kestrel-images/kestrel-logo.png'/>
-   <p>
-   The <b>Kestrel Books</b> are a collection of ACL2 books
-   contributed mainly by <a href='http://www.kestrel.edu'>Kestrel Institute</a>.
-   The Kestrel Books are freely available under a liberal license.
-   Specific copyright, author, and license information
-   is provided in the individual files and subdirectories.
-   </p>
-   <p>
-   As they become more stable,
-   parts of the Kestrel Books may be moved
-   to other locations in the
-   <see topic='@(url community-books)'>Community Books</see>.
-   For example,
-   <see topic='@(url std)'>STD</see> and
-   <see topic='@(url x86isa)'>X86ISA</see>
-   include some Kestrel contributions.
-   </p>
-   <p>
-   The Kestrel Books build upon, and are meant to extend and be compatible with,
-   the ACL2 system code
-   and various existing libraries such as
-   <see topic='@(url std)'>STD</see>,
-   <see topic='@(url fty)'>FTY</see>,
-   <see topic='@(url seq)'>Seq</see>,
-   and others.
-   </p>")
+
+  (xdoc::topstring
+
+   (xdoc::img :src "res/kestrel-images/kestrel-logo.png")
+
+   (xdoc::p
+    "The <b>Kestrel Books</b> are a collection of ACL2 books
+     contributed mainly by "
+    (xdoc::a :href "http://www.kestrel.edu" "Kestrel Institute")
+    ". The Kestrel Books are freely available under a liberal license.
+     Specific copyright, author, and license information
+     is provided in the individual files and subdirectories.")
+
+   (xdoc::p
+    "As they become more stable,
+     parts of the Kestrel Books may be moved
+     to other locations in the "
+    (xdoc::seeurl "community-books" "Community Books")
+    ". For example, "
+    (xdoc::seeurl "std" "STD")
+    " and "
+    (xdoc::seeurl "x86isa" "X86ISA")
+    " include some Kestrel contributions.")
+
+   (xdoc::p
+    "Many of the Kestrel Books build upon,
+     and are meant to extend and be compatible with,
+     the ACL2 system code
+     and various existing libraries such as "
+    (xdoc::seeurl "std" "STD") ", "
+    (xdoc::seeurl "fty" "FTY") ", "
+    (xdoc::seeurl "seq" "Seq") ", and others.")))
 
 (xdoc::add-resource-directory "kestrel-images" "images")
 
