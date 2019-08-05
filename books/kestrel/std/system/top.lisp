775708445e45b8319b7ab0bb255bcbdc32829497
Alessandro Coglio <coglio@kestrel.edu>
2019-08-04 22:49:53 -0700

    [Std] Start an Std/system library.
    This is meant to complement the built-in system utilities.
    
    It could be perhaps moved under [books]/std/ at some point.

---
+++ books/kestrel/std/system/top.lisp
@@ -0,0 +1,27 @@
+; Std/system - Standard System Library
+;
+; Copyright (C) 2019 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+(include-book "xdoc/constructors" :dir :system)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defxdoc std/system
+  :parents (std-extensions std)
+  :short
+  (xdoc::topstring
+   "A library that complements the "
+   (xdoc::seeurl "system-utilities" "built-in system utilities")
+   " with theorems and non-built-in system utilities.")
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "These could be moved under @('[books]/std') at some point.")))
--- books/kestrel/std/top.lisp
+++ books/kestrel/std/top.lisp
@@ -11,6 +11,7 @@
 (in-package "ACL2")
 
 (include-book "basic/top")
+(include-book "system/top")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
