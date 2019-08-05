ecfa4ffaccd56b5618037361c560ffd18861847c
Alessandro Coglio <coglio@kestrel.edu>
2019-08-04 23:00:55 -0700

    [Std/system] Add some theorems about CONJOIN and CONJOIN2.

--- books/kestrel/std/system/top.lisp
+++ books/kestrel/std/system/top.lisp
@@ -10,6 +10,8 @@
 
 (in-package "ACL2")
 
+(include-book "conjoin")
+
 (include-book "xdoc/constructors" :dir :system)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
