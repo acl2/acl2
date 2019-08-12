6433b2fdb4cdc3d5036eb0c499d20d9947345bfb
Alessandro Coglio <coglio@kestrel.edu>
2019-08-11 20:51:00 -0700

    [APT & DEFARBREC] Use XDOC constructor for design notes.

--- books/kestrel/apt/tailrec-design.lisp
+++ books/kestrel/apt/tailrec-design.lisp
@@ -10,89 +10,46 @@
 
 (in-package "APT")
 
-(include-book "kestrel/utilities/xdoc/constructors" :dir :system)
+(include-book "kestrel/utilities/event-macros/xdoc-constructors" :dir :system)
 
 ; (depends-on "design-notes/tailrec.pdf")
 ; (depends-on "kestrel/design-notes/notation.pdf" :dir :system)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; documentation topic for the design notes,
-; which are in design-notes/tailrec.pdf:
-
-(defxdoc tailrec-design
-
-  :parents (design-notes tailrec)
-
-  :short "Design notes for the APT tail recursion transformation."
-
-  :long
-
-  (xdoc::topstring
-
-   (xdoc::p
-    "The design of the transformation is described in "
-    (xdoc::a :href "res/kestrel-apt-design-notes/tailrec.pdf" "these notes")
-    ", which use "
-    (xdoc::a :href "res/kestrel-design-notes/notation.pdf" "this notation")
-    ".")
-
-   (xdoc::p
-    "The correspondence between the design notes and the reference documentation
-     is the following:")
-
-   (xdoc::ul
-    (xdoc::li
-     "@($f$) corresponds to @('old').")
-    (xdoc::li
-     "@($a(\\overline{x})$) corresponds to @('test<x1,...,xn>').")
-    (xdoc::li
-     "@($b(\\overline{x})$) corresponds to @('base<x1,...,xn>').")
-    (xdoc::li
-     "@($c(\\overline{x})$) corresponds to @('nonrec<x1,...,xn>').")
-    (xdoc::li
-     "@($d_i(\\overline{x})$) corresponds to @('update-xi<x1,...,xn>').")
-    (xdoc::li
-     "@($D$) corresponds to @('domain').")
-    (xdoc::li
-     "@($*$) corresponds to @('combine').")
-    (xdoc::li
-     "@($D{}b$) corresponds to @(':domain-of-base').")
-    (xdoc::li
-     "@($D{}c$) corresponds to @(':domain-of-nonrec').")
-    (xdoc::li
-     "@($D\\!*$) corresponds to @(':domain-of-combine').")
-    (xdoc::li
-     "@($D\\!*'$) corresponds to @(':domain-of-combine-uncond').")
-    (xdoc::li
-     "@($A{}S{}C$) corresponds to @(':combine-associativity').")
-    (xdoc::li
-     "@($A{}S{}C'$) corresponds to @(':combine-associativity-uncond').")
-    (xdoc::li
-     "@($L{}I$) corresponds to @(':combine-left-identity').")
-    (xdoc::li
-     "@($R{}I$) corresponds to @(':combine-right-identity').")
-    (xdoc::li
-     "@($G{}D$) corresponds to @(':domain-guard').")
-    (xdoc::li
-     "@($G\\!*$) corresponds to @(':combine-guard').")
-    (xdoc::li
-     "@($G{}D{}c$) corresponds to @(':domain-of-nonrec-when-guard').")
-    (xdoc::li
-     "@($f'$) corresponds to @('new').")
-    (xdoc::li
-     "@($\\tilde{f}$) corresponds to @('wrapper').")
-    (xdoc::li
-     "@($f{}f'$) corresponds to @('old-to-new').")
-    (xdoc::li
-     "@($f{}\\tilde{f}$) corresponds to @('old-to-wrapper')."))
-
-   (xdoc::p
+(xdoc::evmac-topic-design-notes
+ tailrec
+ "res/kestrel-apt-design-notes/tailrec.pdf"
+ :additional-parents (design-notes)
+ :correspondences
+  ("@($f$) corresponds to @('old')."
+   "@($a(\\overline{x})$) corresponds to @('test<x1,...,xn>')."
+   "@($b(\\overline{x})$) corresponds to @('base<x1,...,xn>')."
+   "@($c(\\overline{x})$) corresponds to @('nonrec<x1,...,xn>')."
+   "@($d_i(\\overline{x})$) corresponds to @('update-xi<x1,...,xn>')."
+   "@($D$) corresponds to @('domain')."
+   "@($*$) corresponds to @('combine')."
+   "@($D{}b$) corresponds to @(':domain-of-base')."
+   "@($D{}c$) corresponds to @(':domain-of-nonrec')."
+   "@($D\\!*$) corresponds to @(':domain-of-combine')."
+   "@($D\\!*'$) corresponds to @(':domain-of-combine-uncond')."
+   "@($A{}S{}C$) corresponds to @(':combine-associativity')."
+   "@($A{}S{}C'$) corresponds to @(':combine-associativity-uncond')."
+   "@($L{}I$) corresponds to @(':combine-left-identity')."
+   "@($R{}I$) corresponds to @(':combine-right-identity')."
+   "@($G{}D$) corresponds to @(':domain-guard')."
+   "@($G\\!*$) corresponds to @(':combine-guard')."
+   "@($G{}D{}c$) corresponds to @(':domain-of-nonrec-when-guard')."
+   "@($f'$) corresponds to @('new')."
+   "@($\\tilde{f}$) corresponds to @('wrapper')."
+   "@($f{}f'$) corresponds to @('old-to-new')."
+   "@($f{}\\tilde{f}$) corresponds to @('old-to-wrapper').")
+  :additional-doc
+  ((xdoc::p
     "The decomposition of the recursive branch
      described in the reference documentation
      does not handle all the cases described in the design notes
      about the decomposition of the old function.")
-
    (xdoc::p
     "The transformation requires the term @('base<x1,...,xn>')
      to be ground when the variant is @(':monoid') or @(':monoid-alt').
@@ -101,7 +58,6 @@
      Thus, with the @(':monoid') or @(':monoid-alt') variants,
      the transformation is always in the special case of a ground base value
      described in the design notes.")
-
    (xdoc::p
     "The transformation does not yet handle
      left and right identity independently,
--- books/kestrel/utilities/defarbrec/design.lisp
+++ books/kestrel/utilities/defarbrec/design.lisp
@@ -10,55 +10,25 @@
 
 (in-package "ACL2")
 
-(include-book "xdoc/constructors" :dir :system)
+(include-book "kestrel/utilities/event-macros/xdoc-constructors" :dir :system)
 
 ; (depends-on "kestrel/design-notes/notation.pdf" :dir :system)
 ; (depends-on "kestrel/utilities/design-notes/defarbrec.pdf" :dir :system)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defxdoc defarbrec-design
-
-  :parents (defarbrec)
-
-  :short "Design notes for @(tsee defarbrec)."
-
-  :long
-
-  (xdoc::topstring
-
-   (xdoc::p
-    "The design of @(tsee defarbrec) is described in "
-    (xdoc::a :href "res/kestrel-utilities-design-notes/defarbrec.pdf"
-      "these notes")
-    ", which use "
-    (xdoc::a :href "res/kestrel-design-notes/notation.pdf"
-      "this notation")
-    ".")
-
-   (xdoc::p
-    "The correspondence between the design notes and the reference documentation
-     is the following:")
-   (xdoc::ul
-    (xdoc::li
-     "@($f$) corresponds to the program-mode function.")
-    (xdoc::li
-     "@($a(\\overline{x})$) corresponds to @('test<x1,...,xn>')")
-    (xdoc::li
-     "@($b(\\overline{x})$) corresponds to @('base<x1,...,xn>').")
-    (xdoc::li
-     "@($c(\\overline{x},y)$) corresponds to @('combine<x1,...,xn,y>').")
-    (xdoc::li
-     "@($d_i(\\overline{x})$) corresponds to @('update-xi<x1,...,xn>').")
-    (xdoc::li
-     "@($d_i^{k}(\\overline{x})$) corresponds to @('update*-xi k x1 ... xn').")
-    (xdoc::li
-     "@($t$) corresponds to @('terminates').")
-    (xdoc::li
-     "@($\\phi$) corresponds to @(tsee nfix).")
-    (xdoc::li
-     "@($\\epsilon_t$) corresponds to @('terminates-witness').")
-    (xdoc::li
-     "@($\\nu$) corresponds to @('measure').")
-    (xdoc::li
-     "@($\\hat{f}$) corresponds to the logic-mode function."))))
+(xdoc::evmac-topic-design-notes
+ defarbrec
+ "res/kestrel-utilities-design-notes/defarbrec.pdf"
+ :correspondences
+  ("@($f$) corresponds to the program-mode function."
+   "@($a(\\overline{x})$) corresponds to @('test<x1,...,xn>')"
+   "@($b(\\overline{x})$) corresponds to @('base<x1,...,xn>')."
+   "@($c(\\overline{x},y)$) corresponds to @('combine<x1,...,xn,y>')."
+   "@($d_i(\\overline{x})$) corresponds to @('update-xi<x1,...,xn>')."
+   "@($d_i^{k}(\\overline{x})$) corresponds to @('update*-xi k x1 ... xn')."
+   "@($t$) corresponds to @('terminates')."
+   "@($\\phi$) corresponds to @(tsee nfix)."
+   "@($\\epsilon_t$) corresponds to @('terminates-witness')."
+   "@($\\nu$) corresponds to @('measure')."
+   "@($\\hat{f}$) corresponds to the logic-mode function."))
