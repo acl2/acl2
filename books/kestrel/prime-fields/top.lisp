52f4d4d568e9d6f37ed64336fc912f27de408349
Eric Smith <ewsmith@gmail.com>
2019-08-28 15:56:13 -0700

    [prime-fields] Add top.lisp file.

---
+++ books/kestrel/prime-fields/top.lisp
@@ -0,0 +1,15 @@
+; Prime fields library
+;
+; Copyright (C) 2019 Kestrel Institute
+;
+; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
+;
+; Author: Eric Smith (eric.smith@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "PFIELD")
+
+(include-book "prime-fields")
+;(include-book "prime-fields-alt") ;incompatible
+(include-book "prime-fields-rules")
