48d29b3bfc05e25764bbe99f99dd24fa8096c5ce
Eric Smith <ewsmith@gmail.com>
2019-08-28 19:47:26 -0700

    [padding] Add file header.

--- books/kestrel/crypto/padding/top.lisp
+++ books/kestrel/crypto/padding/top.lisp
@@ -1,3 +1,13 @@
+; Crypto Library: Padding
+;
+; Copyright (C) 2019 Kestrel Institute
+;
+; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
+;
+; Author: Eric Smith (eric.smith@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (in-package "PADDING")
 
 (include-book "pad-to-448")
