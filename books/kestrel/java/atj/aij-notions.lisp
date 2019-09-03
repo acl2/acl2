0ad4eef20c536b7b178ec3990c4b498f24cf42da
Alessandro Coglio <coglio@kestrel.edu>
2019-09-03 15:13:50 -0700

    [Java] Tweak ATJ code.

--- books/kestrel/java/atj/aij-notions.lisp
+++ books/kestrel/java/atj/aij-notions.lisp
@@ -42,7 +42,7 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defsection atj-aij-class-consts
+(defsection atj-aij-class-names
   :short "ACL2 named constants for the AIJ class names."
   (defconst *atj-jclass-char*      "Acl2Character")
   (defconst *atj-jclass-complex*   "Acl2ComplexRational")
@@ -66,33 +66,6 @@
   (defconst *atj-jclass-term*      "Acl2Term")
   (defconst *atj-jclass-value*     "Acl2Value")
   (defconst *atj-jclass-var*       "Acl2Variable"))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defsection atj-aij-type-consts
-  :short "ACL2 named constants for the AIJ (class) types."
-  (defconst *atj-jtype-char*      (jtype-class *atj-jclass-char*))
-  (defconst *atj-jtype-complex*   (jtype-class *atj-jclass-complex*))
-  (defconst *atj-jtype-cons*      (jtype-class *atj-jclass-cons*))
-  (defconst *atj-jtype-def-fn*    (jtype-class *atj-jclass-def-fn*))
-  (defconst *atj-jtype-eval-exc*  (jtype-class *atj-jclass-eval-exc*))
-  (defconst *atj-jtype-fn*        (jtype-class *atj-jclass-fn*))
-  (defconst *atj-jtype-fn-app*    (jtype-class *atj-jclass-fn-app*))
-  (defconst *atj-jtype-int*       (jtype-class *atj-jclass-int*))
-  (defconst *atj-jtype-lambda*    (jtype-class *atj-jclass-lambda*))
-  (defconst *atj-jtype-named-fn*  (jtype-class *atj-jclass-named-fn*))
-  (defconst *atj-jtype-native-fn* (jtype-class *atj-jclass-native-fn*))
-  (defconst *atj-jtype-number*    (jtype-class *atj-jclass-number*))
-  (defconst *atj-jtype-pkg*       (jtype-class *atj-jclass-pkg*))
-  (defconst *atj-jtype-pkg-name*  (jtype-class *atj-jclass-pkg-name*))
-  (defconst *atj-jtype-qconst*    (jtype-class *atj-jclass-qconst*))
-  (defconst *atj-jtype-ratio*     (jtype-class *atj-jclass-ratio*))
-  (defconst *atj-jtype-rational*  (jtype-class *atj-jclass-rational*))
-  (defconst *atj-jtype-string*    (jtype-class *atj-jclass-string*))
-  (defconst *atj-jtype-symbol*    (jtype-class *atj-jclass-symbol*))
-  (defconst *atj-jtype-term*      (jtype-class *atj-jclass-term*))
-  (defconst *atj-jtype-value*     (jtype-class *atj-jclass-value*))
-  (defconst *atj-jtype-var*       (jtype-class *atj-jclass-var*)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -123,6 +96,33 @@
   ///
   (assert-event (string-listp *atj-aij-class-names*))
   (assert-event (no-duplicatesp-equal *atj-aij-class-names*)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defsection atj-aij-class-types
+  :short "ACL2 named constants for the AIJ class types."
+  (defconst *atj-jtype-char*      (jtype-class *atj-jclass-char*))
+  (defconst *atj-jtype-complex*   (jtype-class *atj-jclass-complex*))
+  (defconst *atj-jtype-cons*      (jtype-class *atj-jclass-cons*))
+  (defconst *atj-jtype-def-fn*    (jtype-class *atj-jclass-def-fn*))
+  (defconst *atj-jtype-eval-exc*  (jtype-class *atj-jclass-eval-exc*))
+  (defconst *atj-jtype-fn*        (jtype-class *atj-jclass-fn*))
+  (defconst *atj-jtype-fn-app*    (jtype-class *atj-jclass-fn-app*))
+  (defconst *atj-jtype-int*       (jtype-class *atj-jclass-int*))
+  (defconst *atj-jtype-lambda*    (jtype-class *atj-jclass-lambda*))
+  (defconst *atj-jtype-named-fn*  (jtype-class *atj-jclass-named-fn*))
+  (defconst *atj-jtype-native-fn* (jtype-class *atj-jclass-native-fn*))
+  (defconst *atj-jtype-number*    (jtype-class *atj-jclass-number*))
+  (defconst *atj-jtype-pkg*       (jtype-class *atj-jclass-pkg*))
+  (defconst *atj-jtype-pkg-name*  (jtype-class *atj-jclass-pkg-name*))
+  (defconst *atj-jtype-qconst*    (jtype-class *atj-jclass-qconst*))
+  (defconst *atj-jtype-ratio*     (jtype-class *atj-jclass-ratio*))
+  (defconst *atj-jtype-rational*  (jtype-class *atj-jclass-rational*))
+  (defconst *atj-jtype-string*    (jtype-class *atj-jclass-string*))
+  (defconst *atj-jtype-symbol*    (jtype-class *atj-jclass-symbol*))
+  (defconst *atj-jtype-term*      (jtype-class *atj-jclass-term*))
+  (defconst *atj-jtype-value*     (jtype-class *atj-jclass-value*))
+  (defconst *atj-jtype-var*       (jtype-class *atj-jclass-var*)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
