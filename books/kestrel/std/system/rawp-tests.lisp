25e8fcca58b24ca4ecb0cc5e845e5bab8a8bdcb2
Alessandro Coglio <coglio@kestrel.edu>
2019-09-19 21:24:07 -0700

    [Std/system] Add some tests for RAWP.

---
+++ books/kestrel/std/system/rawp-tests.lisp
@@ -0,0 +1,38 @@
+; Standard System Library
+;
+; Copyright (C) 2019 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+(include-book "rawp")
+
+(include-book "misc/assert" :dir :system)
+(include-book "misc/eval" :dir :system)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(assert! (rawp 'len state))
+
+(assert! (rawp 'fmt-to-comment-window state))
+
+(assert! (rawp 'floor state))
+
+(assert! (rawp 'cw-gstack-fn state))
+
+(assert! (rawp 'certify-book-fn state))
+
+(assert! (rawp 'ev-w state))
+
+(assert! (not (rawp 'cons state)))
+
+(assert! (not (rawp 'parilis$ state)))
+
+(must-succeed*
+ (defun f (x) x)
+ (assert! (not (rawp 'f state))))
