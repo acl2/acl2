fe49e30802124db8eb45d7e1602b3764c7295a9a
Alessandro Coglio <coglio@kestrel.edu>
2019-09-19 21:17:36 -0700

    [Std/system] Move some tests to new files.
    These are tests for utilities that have moved to individual files from the large
    world-queries.lisp file, and so now their tests have been accordingly moved to
    individual files.

---
+++ books/kestrel/std/system/macro-symbolp-tests.lisp
@@ -0,0 +1,28 @@
+; Standard System Library
+;
+; Copyright (C) 2019 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+(include-book "macro-symbolp")
+
+(include-book "misc/assert" :dir :system)
+(include-book "misc/eval" :dir :system)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(assert! (macro-symbolp 'append (w state)))
+
+(assert! (not (macro-symbolp 'cons (w state))))
+
+(assert! (not (macro-symbolp 'aaaaaaaaaa (w state))))
+
+(must-succeed*
+ (defmacro m (x) `(list ,x))
+ (assert! (macro-symbolp 'm (w state))))
