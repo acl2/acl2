adf038d30f664727f518f12a531fd3aa136b0b67
Alessandro Coglio <coglio@kestrel.edu>
2019-09-21 20:43:27 -0700

    [Std/strings] Start some Kestrel Std/strings extensions.

---
+++ books/kestrel/std/strings/top.lisp
@@ -0,0 +1,29 @@
+; Standard Strings Library
+;
+; Copyright (C) 2019 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "STR")
+
+(include-book "xdoc/constructors" :dir :system)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defxdoc std/strings-extensions
+  :parents (acl2::std-extensions std/strings)
+  :short
+  (xdoc::topstring "Extensions of "
+                   (xdoc::seetopic "std/strings" "Std/strings")
+                   " in the "
+                   (xdoc::seetopic "kestrel-books" "Kestrel Books")
+                   ".")
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "These extensions could be moved under @('[books]/std/strings')
+     at some point.")))
