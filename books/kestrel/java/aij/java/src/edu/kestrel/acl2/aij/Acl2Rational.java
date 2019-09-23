4b2ec76c8b7ff7bb097565435a240f0c760449fb
Alessandro Coglio <coglio@kestrel.edu>
2019-09-23 14:07:03 -0700

    [Java] Extend and improve some AIJ Javadoc.

--- books/kestrel/java/aij/java/src/edu/kestrel/acl2/aij/Acl2Rational.java
+++ books/kestrel/java/aij/java/src/edu/kestrel/acl2/aij/Acl2Rational.java
@@ -8,7 +8,7 @@ package edu.kestrel.acl2.aij;
 
 /**
  * Representation of ACL2 rationals.
- * These are the ACL2 values that satisfy {@code rationalp}.
+ * These are the values that satisfy {@code rationalp}.
  * <p>
  * They consist of
  * integers (subclass {@link Acl2Integer} and
@@ -29,8 +29,10 @@ public abstract class Acl2Rational extends Acl2Number {
     }
 
     /**
-     * Returns {@code true},
-     * consistently with the {@code rationalp} ACL2 function.
+     * Checks if this rational is a rational, which is always true.
+     * This is consistent with the {@code rationalp} ACL2 function.
+     *
+     * @return The symbol {@code t}.
      */
     @Override
     Acl2Symbol rationalp() {
@@ -38,8 +40,10 @@ public abstract class Acl2Rational extends Acl2Number {
     }
 
     /**
-     * Negates (arithmetically) this ACL2 rational,
+     * Negates (arithmetically) this rational,
      * consistently with the {@code unary--} ACL2 function.
+     *
+     * @return The negation of this rational.
      */
     @Override
     Acl2Rational negate() {
@@ -50,8 +54,10 @@ public abstract class Acl2Rational extends Acl2Number {
     }
 
     /**
-     * Reciprocates (arithmetically) this ACL2 rational,
+     * Reciprocates (arithmetically) this rational,
      * consistently with the {@code unary-/} ACL2 function.
+     *
+     * @return The reciprocal of this rational.
      */
     @Override
     Acl2Rational reciprocate() {
@@ -64,8 +70,11 @@ public abstract class Acl2Rational extends Acl2Number {
     }
 
     /**
-     * Adds the argument ACL2 value to this ACL2 rational,
+     * Adds the argument value to this rational,
      * consistently with the {@code binary-+} ACL2 function.
+     *
+     * @param other The value to add to this rational. It is never {@code null}.
+     * @return The sum of this rational with the argument value.
      */
     @Override
     Acl2Number addValue(Acl2Value other) {
@@ -73,8 +82,12 @@ public abstract class Acl2Rational extends Acl2Number {
     }
 
     /**
-     * Adds the argument ACL2 number to this ACL2 rational,
+     * Adds the argument number to this rational,
      * consistently with the {@code binary-+} ACL2 function.
+     *
+     * @param other The number to add to this rational.
+     *              It is never {@code null}.
+     * @return The sum of this rational with the argument number.
      */
     @Override
     Acl2Number addNumber(Acl2Number other) {
@@ -82,8 +95,12 @@ public abstract class Acl2Rational extends Acl2Number {
     }
 
     /**
-     * Adds the argument ACL2 rational to this ACL2 rational,
+     * Adds the argument rational to this rational,
      * consistently with the {@code binary-+} ACL2 function.
+     *
+     * @param other The rational to add to this raational.
+     *              It is never {@code null}.
+     * @return The sum of this rational with the argument rational.
      */
     Acl2Rational addRational(Acl2Rational other) {
         // a/b+c/d is (a*(lcm/b)+c*(lcm/d))/lcm,
@@ -101,8 +118,12 @@ public abstract class Acl2Rational extends Acl2Number {
     }
 
     /**
-     * Adds the argument ACL2 integer to this ACL2 rational,
+     * Adds the argument integer to this rational,
      * consistently with the {@code binary-+} ACL2 function.
+     *
+     * @param other The integer to add to this rational.
+     *              It is never {@code null}.
+     * @return The sum of this rational with the argument integer.
      */
     @Override
     Acl2Rational addInteger(Acl2Integer other) {
@@ -114,8 +135,12 @@ public abstract class Acl2Rational extends Acl2Number {
     }
 
     /**
-     * Multiplies the argument ACL2 value to this ACL2 rational,
+     * Multiplies the argument value to this rational,
      * consistently with the {@code binary-*} ACL2 function.
+     *
+     * @param other The value by which to multiply this rational.
+     *              It is never {@code null}.
+     * @return The product of this rational with the argument value.
      */
     @Override
     Acl2Number multiplyValue(Acl2Value other) {
@@ -123,8 +148,12 @@ public abstract class Acl2Rational extends Acl2Number {
     }
 
     /**
-     * Multiplies the argument ACL2 number to this ACL2 rational,
+     * Multiplies the argument number to this rational,
      * consistently with the {@code binary-*} ACL2 function.
+     *
+     * @param other The number by which to multiply this rational.
+     *              It is never {@code null}.
+     * @return The product of this rational with the argument number.
      */
     @Override
     Acl2Number multiplyNumber(Acl2Number other) {
@@ -132,8 +161,12 @@ public abstract class Acl2Rational extends Acl2Number {
     }
 
     /**
-     * Multiplies the argument ACL2 rational to this ACL2 rational,
+     * Multiplies the argument rational to this rational,
      * consistently with the {@code binary-*} ACL2 function.
+     *
+     * @param other The rational by which to multiply this rational.
+     *              It is never {@code null}.
+     * @return The product of this rational with the argument rational.
      */
     @Override
     Acl2Rational multiplyRational(Acl2Rational other) {
@@ -146,8 +179,12 @@ public abstract class Acl2Rational extends Acl2Number {
     }
 
     /**
-     * Multiplies the argument ACL2 integer to this ACL2 rational,
+     * Multiplies the argument integer to this rational,
      * consistently with the {@code binary-*} ACL2 function.
+     *
+     * @param other The integer by which to multiply this rational.
+     *              It is never {@code null}.
+     * @return The product of this rational with the argument integer.
      */
     @Override
     Acl2Rational multiplyInteger(Acl2Integer other) {
@@ -158,25 +195,30 @@ public abstract class Acl2Rational extends Acl2Number {
     }
 
     /**
-     * Returns the numerator of this ACL2 rational,
+     * Returns the numerator of this rational,
      * consistently with the {@code numerator} ACL2 function.
+     *
+     * @return The numerator of this rational.
      */
     Acl2Integer numerator() {
         return this.getNumerator();
     }
 
     /**
-     * Returns the denominator of this ACL2 rational,
+     * Returns the denominator of this rational,
      * consistently with the {@code denominator} ACL2 function.
+     *
+     * @return The denominator of this rational.
      */
     Acl2Integer denominator() {
         return this.getDenominator();
     }
 
     /**
-     * Coerce this ACL2 rational to an ACL2 rational,
-     * i.e. just return this ACL2 rational.
+     * Coerce this rational to a rational, which is a no-op.
      * This is consistent with the {@code rfix} ACL2 function.
+     *
+     * @return This rational, unchanged.
      */
     @Override
     Acl2Rational rfix() {
@@ -184,23 +226,25 @@ public abstract class Acl2Rational extends Acl2Number {
     }
 
     /**
-     * Compares this ACL2 rational with the argument ACL2 number for order.
+     * Compares this rational with the argument number for order.
      * This is consistent with the {@code lexorder} ACL2 function.
      *
-     * @return a negative integer, zero, or a positive integer as
-     * this rational is less than, equal to, or greater than the argument
+     * @param o The number to compare this rational with.
+     * @return A negative integer, zero, or a positive integer as
+     * this rational is less than, equal to, or greater than the argument.
      */
     @Override
     int compareToNumber(Acl2Number o) {
-        return - o.compareToRational(this);
+        return -o.compareToRational(this);
     }
 
     /**
-     * Compares this ACL2 rational with the argument ACL2 rational for order.
+     * Compares this rational with the argument rational for order.
      * This is consistent with the {@code lexorder} ACL2 function.
      *
-     * @return a negative integer, zero, or a positive integer as
-     * this rational is less than, equal to, or greater than the argument
+     * @param o The rational to compare this rational with.
+     * @return A negative integer, zero, or a positive integer as
+     * this rational is less than, equal to, or greater than the argument.
      */
     @Override
     int compareToRational(Acl2Rational o) {
@@ -215,11 +259,12 @@ public abstract class Acl2Rational extends Acl2Number {
     }
 
     /**
-     * Compares this ACL2 rational with the argument ACL2 integer for order.
+     * Compares this rational with the argument integer for order.
      * This is consistent with the {@code lexorder} ACL2 function.
      *
-     * @return a negative integer, zero, or a positive integer as
-     * this number is less than, equal to, or greater than the argument
+     * @param o The integer to compare this rational with.
+     * @return A negative integer, zero, or a positive integer as
+     * this number is less than, equal to, or greater than the argument.
      */
     @Override
     int compareToInteger(Acl2Integer o) {
@@ -234,28 +279,33 @@ public abstract class Acl2Rational extends Acl2Number {
     //////////////////////////////////////// public members:
 
     /**
-     * Compares this ACL2 rational with the argument ACL2 value for order.
+     * Compares this rational with the argument value for order.
      * This is consistent with the {@code lexorder} ACL2 function.
      *
-     * @return a negative integer, zero, or a positive integer as
-     * this value is less than, equal to, or greater than the argument
-     * @throws NullPointerException if the argument is null
+     * @param o The object to compare this rational with.
+     * @return A negative integer, zero, or a positive integer as
+     * this value is less than, equal to, or greater than the argument.
+     * @throws NullPointerException If the argument is {@code null}.
      */
     @Override
     public int compareTo(Acl2Value o) {
         if (o == null)
             throw new NullPointerException();
-        return - o.compareToRational(this);
+        return -o.compareToRational(this);
     }
 
     /**
-     * Returns an ACL2 rational whose numeric value is
+     * Returns a rational whose numeric value is
      * the fraction of the given numerator and denominator.
-     * If the fraction is actually an integer, the result is an ACL2 integer,
+     * If the fraction is actually an integer, the result is an integer,
      * according to the rule of rational canonicalization in Common Lisp.
      *
-     * @throws IllegalArgumentException if numerator or denominator is null,
-     *                                  or denominator is 0
+     * @param numerator   The numerator of the rational.
+     * @param denominator The denominator of the rational.
+     * @return The rational.
+     * @throws IllegalArgumentException If {@code numerator} or
+     *                                  {@code denominator} is {@codee null},
+     *                                  or {@code denominator} is 0.
      */
     public static Acl2Rational make(Acl2Integer numerator,
                                     Acl2Integer denominator) {
@@ -283,24 +333,29 @@ public abstract class Acl2Rational extends Acl2Number {
     }
 
     /**
-     * Returns the numerator of this ACL2 rational.
+     * Returns the numerator of this rational.
      * The numerator is in reduced form,
      * i.e. it is coprime with the denominator,
      * and its sign is consistent with a positive denominator.
+     *
+     * @return The numerator of this rational.
      */
     public abstract Acl2Integer getNumerator();
 
     /**
-     * Returns the denominator of this ACL2 rational.
+     * Returns the denominator of this rational.
      * The denominator is in reduced form,
      * i.e. it is positive and coprime with the numerator.
      * If this rational is an integer, the result is 1.
+     *
+     * @return The denominator of this rational.
      */
     public abstract Acl2Integer getDenominator();
 
     /**
-     * Returns the real part of this ACL2 rational.
-     * The result is the rational itself.
+     * Returns the real part of this rational, which is the rational itself.
+     *
+     * @return This rational.
      */
     @Override
     public Acl2Rational getRealPart() {
@@ -308,8 +363,9 @@ public abstract class Acl2Rational extends Acl2Number {
     }
 
     /**
-     * Returns the imaginary part of this ACL2 rational.
-     * The result is 0.
+     * Returns the imaginary part of this rational, which is always 0.
+     *
+     * @return The integer 0.
      */
     @Override
     public Acl2Rational getImaginaryPart() {
