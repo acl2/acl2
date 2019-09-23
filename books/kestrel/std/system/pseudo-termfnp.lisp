54ea7822062676c8f8c480132d7b4294f8f8ec56
Alessandro Coglio <coglio@kestrel.edu>
2019-09-23 14:20:41 -0700

    [Std/system] Split a file.
    Put each function into its own file, for greater modularity.

---
+++ books/kestrel/std/system/pseudo-termfnp.lisp
@@ -0,0 +1,43 @@
+; Standard System Library
+;
+; Copyright (C) 2019 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+(include-book "pseudo-lambdap")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define pseudo-termfnp (x)
+  :returns (yes/no booleanp)
+  :parents (term-function-recognizers)
+  :short
+  (xdoc::topstring
+   "Recognize pseudo-term-functions,
+    i.e. functions in "
+   (xdoc::seetopic "pseudo-termp" "pseudo-terms")
+   ".")
+  (or (symbolp x)
+      (pseudo-lambdap x))
+  ///
+
+  (defrule pseudo-termfnp-when-pseudo-termp
+    (implies (and (pseudo-termp term)
+                  (consp term))
+             (pseudo-termfnp (car term)))
+    :enable pseudo-lambdap)
+
+  (defrule pseudo-termp-when-pseudo-termfnp
+    (implies (and (pseudo-termfnp fn)
+                  (pseudo-term-listp terms)
+                  (or (atom fn)
+                      (equal (len terms) (len (lambda-formals fn))))
+                  (not (eq fn 'quote)))
+             (pseudo-termp (cons fn terms)))
+    :enable pseudo-lambdap))
