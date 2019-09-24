4c837e97d05687f793c7050258f1721b573e8aa5
Alessandro Coglio <coglio@kestrel.edu>
2019-09-24 16:00:22 -0700

    [Std/system] Split tests into separate files.
    This mirrors the splitting of the tested utilities into separate files.

--- books/kestrel/std/system/pseudo-lambdap-tests.lisp
+++ books/kestrel/std/system/pseudo-lambdap-tests.lisp
@@ -10,7 +10,7 @@
 
 (in-package "ACL2")
 
-(include-book "term-function-recognizers")
+(include-book "pseudo-lambdap")
 
 (include-book "misc/assert" :dir :system)
 (include-book "misc/eval" :dir :system)
@@ -41,38 +41,3 @@
  (defconst *term* '((lambda (x) (1+ x)) y))
  (assert! (pseudo-termp *term*))
  (assert! (pseudo-lambdap (ffn-symb *term*))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(assert! (pseudo-termfnp 'f))
-
-(assert! (pseudo-termfnp '(lambda (x) x)))
-
-(assert! (not (pseudo-termfnp 33)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(assert! (lambdap '(lambda (x y) (binary-+ x (len (cons '3 'nil)))) (w state)))
-
-(assert! (not (lambdap '(lambda (x) (fffff x)) (w state))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(assert! (not (termfnp "cons" (w state))))
-
-(assert! (not (termfnp 'fffffffff (w state))))
-
-(assert! (termfnp 'cons (w state)))
-
-(assert! (termfnp 'len (w state)))
-
-(assert! (not (termfnp 'car-cdr-elim (w state))))
-
-(must-succeed*
- (defun h (x) x)
- (assert! (termfnp 'h (w state))))
-
-(assert!
- (termfnp '(lambda (x y) (binary-+ x (len (cons '3 'nil)))) (w state)))
-
-(assert! (not (termfnp '(lambda (x) (fffff x)) (w state))))
---
+++ books/kestrel/std/system/termfnp-tests.lisp
@@ -0,0 +1,37 @@
+; Standard System Library
+;
+; Copyright (C) 2019 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+(include-book "term-function-recognizers")
+
+(include-book "misc/assert" :dir :system)
+(include-book "misc/eval" :dir :system)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(assert! (not (termfnp "cons" (w state))))
+
+(assert! (not (termfnp 'fffffffff (w state))))
+
+(assert! (termfnp 'cons (w state)))
+
+(assert! (termfnp 'len (w state)))
+
+(assert! (not (termfnp 'car-cdr-elim (w state))))
+
+(must-succeed*
+ (defun h (x) x)
+ (assert! (termfnp 'h (w state))))
+
+(assert!
+ (termfnp '(lambda (x y) (binary-+ x (len (cons '3 'nil)))) (w state)))
+
+(assert! (not (termfnp '(lambda (x) (fffff x)) (w state))))
