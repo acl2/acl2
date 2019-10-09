884f9ee44a281b1c52f8818039c0ba2091c0ca45
Alessandro Coglio <coglio@kestrel.edu>
2019-10-09 13:34:55 -0700

    [Std/typed-alists] Add alists from symbols to positive integers.

--- books/kestrel/std/typed-alists/symbol-pos-alistp-tests.lisp
+++ books/kestrel/std/typed-alists/symbol-pos-alistp-tests.lisp
@@ -10,15 +10,24 @@
 
 (in-package "ACL2")
 
-(include-book "string-symbollist-alistp")
-(include-book "symbol-nat-alistp")
 (include-book "symbol-pos-alistp")
-(include-book "symbol-pseudoterm-alistp")
-(include-book "symbol-string-alistp")
-(include-book "symbol-symbol-alistp")
+
+(include-book "misc/assert" :dir :system)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defxdoc std/typed-alists
-  :parents (std)
-  :short "Typed alists.")
+(assert! (symbol-pos-alistp nil))
+
+(assert! (symbol-pos-alistp '((a . 2))))
+
+(assert! (symbol-pos-alistp '((t . 88) (:kwd . 1))))
+
+(assert! (not (symbol-pos-alistp 3)))
+
+(assert! (not (symbol-pos-alistp '(3))))
+
+(assert! (not (symbol-pos-alistp '((x . 3) (2/3 . 10)))))
+
+(assert! (not (symbol-pos-alistp '((xx . 1) (t . 0)))))
+
+(assert! (not (symbol-pos-alistp '((xx . -100) (t . 1)))))
