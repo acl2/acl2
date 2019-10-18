f6064ae3a0a1b6b52c3ec093afef7b4c7cf71827
Alessandro Coglio <coglio@kestrel.edu>
2019-10-17 17:03:13 -0700

    [Std/system] Improve the usefulness of ARITY+.
    The guard on the world argument of this utility has been weakened from
    PLIST-WORLDP-WITH-FORMALS to PLIST-WORLDP, because the latter is what system
    utilities normally use.

--- books/kestrel/std/system/arity-plus.lisp
+++ books/kestrel/std/system/arity-plus.lisp
@@ -17,10 +17,8 @@
 
 (define arity+ ((fn (or (function-namep fn wrld)
                         (pseudo-lambdap fn)))
-                (wrld plist-worldp-with-formals))
-  :returns (result natp
-                   :hyp (or (function-namep fn wrld) (pseudo-lambdap fn))
-                   :hints (("Goal" :in-theory (enable arity pseudo-lambdap))))
+                (wrld plist-worldp))
+  :returns (result natp)
   :parents (std/system/function-queries)
   :short (xdoc::topstring
           (xdoc::seetopic "std/system/logic-friendly" "Logic-friendly")
@@ -28,7 +26,24 @@
   :long
   (xdoc::topstring
    (xdoc::p
-    "This returns the same result as @(tsee arity),
-     but it has a stronger guard."))
-  (arity fn wrld)
+    "This returns the same result as @(tsee arity)
+     on named functions and lambda expressions,
+     but it has a guard that is stronger on @('fn') but weaker on @('wrld').
+     The reason for weakening the guard on @('wrld'),
+     from @('plist-worldp-with-formals') to @(tsee plist-worldp)
+     is a practical one:
+     when doing system programming,
+     currently @(tsee plist-worldp) is normally used for the ACL2 @(see world),
+     so using @('plist-worldp-with-formals') in some system utilities
+     (like @('arity+') here)
+     would essentially force its use in many or all the other utilities.")
+   (xdoc::p
+    "Since @(tsee arity) has a stronger guard on the world,
+     in order for @('arity+') to be guard-verified,
+     it cannot call @(tsee arity).
+     Thus, here we replicate the (simple) code of @(tsee arity),
+     with the only slight difference that we return 0
+     if @('fn') does not satisfy the guard."))
+  (cond ((flambdap fn) (len (lambda-formals fn)))
+        (t (len (getpropc fn 'formals t wrld))))
   :guard-hints (("Goal" :in-theory (enable pseudo-lambdap))))
