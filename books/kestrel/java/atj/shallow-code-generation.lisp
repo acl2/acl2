b350e8638ac5f6d54a7719c0af3aaa1c76ab9885
Alessandro Coglio <coglio@kestrel.edu>
2019-10-18 14:08:03 -0700

    [Java] Update some developer doc.

--- books/kestrel/java/atj/shallow-code-generation.lisp
+++ books/kestrel/java/atj/shallow-code-generation.lisp
@@ -48,9 +48,9 @@
     "We extract all the quoted constants
      from the pre-translated bodies of the ACL2 functions,
      and we create a static final field for each.
-     For now we only do this for quoted numbers and characters,
+     For now we only do this for quoted numbers, characters, and strings,
      but we will cover the other quoted values soon.
-     The fields for quoted numbers and characters
+     The fields for these quoted constants
      are declared in the generated main class;
      they are named in a way that describes their value,
      e.g. see @(tsee atj-gen-shallow-number-field-name).")))
