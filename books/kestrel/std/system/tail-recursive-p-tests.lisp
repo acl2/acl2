4c219db806bc203b1d6e2c25280ce2b55c3362af
Alessandro Coglio <coglio@kestrel.edu>
2019-10-23 09:13:38 -0700

    [Std/system] Add tests for TAIL-RECURSIVE-P.

---
+++ books/kestrel/std/system/tail-recursive-p-tests.lisp
@@ -0,0 +1,56 @@
+; Standard System Library
+;
+; Copyright (C) 2019 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+(include-book "tail-recursive-p")
+
+(include-book "misc/assert" :dir :system)
+(include-book "misc/eval" :dir :system)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(assert! (tail-recursive-p 'nthcdr (w state)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(assert! (not (tail-recursive-p 'take (w state))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(must-succeed*
+
+ (defun fact (n)
+   (declare (xargs :guard (natp n)))
+   (if (zp n)
+       1
+     (* n (fact (1- n)))))
+
+ (defun fact-tr (n acc)
+   (declare (xargs :guard (and (natp n) (natp acc))))
+   (if (zp n)
+       acc
+     (fact-tr (1- n) (* n acc))))
+
+ (include-book "arithmetic/top-with-meta" :dir :system)
+
+ (defthm fact-tr-correct-lemma
+   (implies (natp acc)
+            (equal (fact-tr n acc)
+                   (* acc (fact n)))))
+
+ (defthm fact-tr-correct
+   (equal (fact-tr n 1)
+          (fact n))
+   :hints (("Goal" :in-theory (disable fact-tr fact))))
+
+ (assert! (not (tail-recursive-p 'fact (w state))))
+
+ (assert! (tail-recursive-p 'fact-tr (w state))))
