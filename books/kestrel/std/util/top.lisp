9ffec8a098480f6236bd8395b6cf9bceef19e06f
Alessandro Coglio <coglio@kestrel.edu>
2019-10-27 17:07:55 -0700

    [Std/util] Start some Std/util extensions in the Kestrel Books.
    Like other Std extensions in the Kestrel Books, these may be eventually moved
    under [books]/std/, when they are more stable.

---
+++ books/kestrel/std/util/top.lisp
@@ -0,0 +1,29 @@
+; Standard Utilities Library
+;
+; Copyright (C) 2019 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "STD")
+
+(include-book "xdoc/constructors" :dir :system)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defxdoc std/util-extensions
+  :parents (acl2::std-extensions std/util)
+  :short
+  (xdoc::topstring "Extensions of "
+                   (xdoc::seetopic "std/util" "Std/util")
+                   " in the "
+                   (xdoc::seetopic "acl2::kestrel-books" "Kestrel Books")
+                   ".")
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "These extensions could be moved under @('[books]/std/util')
+     at some point.")))
