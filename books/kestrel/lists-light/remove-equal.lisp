97cecdd9c52777bc4b1fd94f0ac3c859179ae915
Eric Smith <ewsmith@gmail.com>
2019-11-21 16:29:34 -0800

    [lists-light] Add lightweight books about remove-equal and remove1-equal.

---
+++ books/kestrel/lists-light/remove-equal.lisp
@@ -0,0 +1,56 @@
+; A lightweight book about the built-in function remove-equal.
+;
+; Copyright (C) 2008-2011 Eric Smith and Stanford University
+; Copyright (C) 2013-2019 Kestrel Institute
+;
+; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
+;
+; Author: Eric Smith (eric.smith@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+(in-theory (disable remove-equal))
+
+;; A simple/abbreviation rule.
+(defthm remove-equal-of-nil
+  (equal (remove-equal x nil)
+         nil)
+  :hints (("Goal" :in-theory (enable remove-equal))))
+
+(defthmd remove-equal-when-not-consp
+  (implies (not (consp l))
+           (equal (remove-equal x l)
+                  nil))
+  :hints (("Goal" :in-theory (enable remove-equal))))
+
+(defthm remove-equal-when-not-consp-cheap
+  (implies (not (consp l))
+           (equal (remove-equal x l)
+                  nil))
+  :rule-classes ((:rewrite :backchain-limit-lst (0)))
+  :hints (("Goal" :in-theory (enable remove-equal))))
+
+(defthm remove-equal-of-cons
+  (equal (remove-equal x (cons y l))
+         (if (equal x y)
+             (remove-equal x l)
+           (cons y (remove-equal x l))))
+  :hints (("Goal" :in-theory (enable remove-equal))))
+
+(defthm remove-equal-of-cons-same
+  (equal (remove-equal x (cons x l))
+         (remove-equal x l))
+  :hints (("Goal" :in-theory (enable remove-equal))))
+
+(defthm remove-equal-of-car-same
+  (equal (remove-equal (car l) l)
+         (remove-equal (car l) (cdr l)))
+  :hints (("Goal" :in-theory (enable remove-equal))))
+
+(defthm len-of-remove-equal-linear
+  (<= (len (remove-equal x l))
+      (len l))
+  :rule-classes ((:linear :trigger-terms ((len (remove-equal x l)))))
+  :hints (("Goal" :in-theory (enable remove-equal))))
