1f3bbc1c15f13920a563a5feae169eff2fd084a3
Alessandro Coglio <coglio@kestrel.edu>
2019-12-05 21:57:36 -0800

    [Std/system] Move CHECK-USER-TERM here.

--- books/kestrel/utilities/system/terms-tests.lisp
+++ books/kestrel/utilities/system/terms-tests.lisp
@@ -15,43 +15,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(assert-equal (mv-list 2 (check-user-term 3 (w state)))
-              '('3 (nil)))
-
-(assert-equal (mv-list 2 (check-user-term 'x (w state)))
-              '(x (nil)))
-
-(assert-equal (mv-list 2 (check-user-term '(len x) (w state)))
-              '((len x) (nil)))
-
-(assert-equal (mv-list 2 (check-user-term '(mv x y z) (w state)))
-              '((cons x (cons y (cons z 'nil))) (nil nil nil)))
-
-(assert-equal (mv-list 2 (check-user-term 'state (w state)))
-              '(state (state)))
-
-(assert-equal (mv-list 2 (check-user-term '(mv state 1) (w state)))
-              '((cons state (cons '1 'nil)) (state nil)))
-
-(must-succeed*
- (defstobj s)
- (assert-equal (mv-list 2 (check-user-term '(mv s 0 state) (w state)))
-               '((cons s (cons '0 (cons state 'nil))) (s nil state))))
-
-(assert-equal (mv-list 2 (check-user-term '(+ x y) (w state)))
-              '((binary-+ x y) (nil)))
-
-(assert-equal (mv-list 2 (check-user-term '(+ (len x) 55) (w state)))
-              '((binary-+ (len x) '55) (nil)))
-
-(assert-equal
- (mv-list 2 (check-user-term '(let ((x 4)) (+ x (len y))) (w state)))
- '(((lambda (x y) (binary-+ x (len y))) '4 y) (nil)))
-
-(assert! (msgp (nth 0 (mv-list 2 (check-user-term '(f x) (w state))))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 (assert! (msgp (nth 0 (mv-list 2 (check-user-lambda
                                   "(lambda (x) x)" (w state))))))
 
