e419725a688a41617607c7fc39e6fc39da22b4db
Alessandro Coglio <coglio@kestrel.edu>
2019-12-22 17:18:35 -0800

    [Std/system] Add an MVIFY utility.
    This operates on translated terms that are obtained either from translating a
    multi-value term or from transforming translated multi-value term in a way that
    preserves the "well-formedness" w.r.t. being multi-valued.
    
    This utility replaces translated LIST terms (i.e. CONS nests ending with quoted
    NILs) with MV terms, and descends into IF's 2nd and 3rd arguments, RETURN-LAST's
    2nd and 3rd arguments, and lambda expressions' bodies.
    
    See the documentation for details.

---
+++ books/kestrel/std/system/mvify-tests.lisp
@@ -0,0 +1,83 @@
+; Standard System Library
+;
+; Copyright (C) 2019 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+(include-book "mvify")
+
+(include-book "misc/assert" :dir :system)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(assert-equal (mvify '(cons x (cons y 'nil)))
+              '(mv x y))
+
+(assert-equal (mvify '(cons x (cons y (cons z (cons w 'nil)))))
+              '(mv x y z w))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(assert-equal (mvify '(f a b))
+              '(f a b))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(assert-equal (mvify '(if a (f u) (g v)))
+              '(if a (f u) (g v)))
+
+(assert-equal (mvify '(if (cons x (cons y 'nil)) (f u) (g v)))
+              '(if (cons x (cons y 'nil)) (f u) (g v)))
+
+(assert-equal (mvify '(if a (cons x (cons y 'nil)) (g v)))
+              '(if a (mv x y) (g v)))
+
+(assert-equal (mvify '(if a (f u) (cons x (cons y 'nil))))
+              '(if a (f u) (mv x y)))
+
+(assert-equal (mvify '(if a (cons x (cons y 'nil)) (cons z (cons w 'nil))))
+              '(if a (mv x y) (mv z w)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(assert-equal (mvify '(return-last a (f u) (g v)))
+              '(return-last a (f u) (g v)))
+
+(assert-equal (mvify '(return-last (cons x (cons y 'nil)) (f u) (g v)))
+              '(return-last (cons x (cons y 'nil)) (f u) (g v)))
+
+(assert-equal (mvify '(return-last a (cons x (cons y 'nil)) (g v)))
+              '(return-last a (mv x y) (g v)))
+
+(assert-equal (mvify '(return-last a (f u) (cons x (cons y 'nil))))
+              '(return-last a (f u) (mv x y)))
+
+(assert-equal (mvify
+               '(return-last a (cons x (cons y 'nil)) (cons z (cons w 'nil))))
+              '(return-last a (mv x y) (mv z w)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(assert-equal (mvify '((lambda (a b) (f a b)) 'anything :anything))
+              '((lambda (a b) (f a b)) 'anything :anything))
+
+(assert-equal (mvify '((lambda (u) (cons x (cons y 'nil))) anything))
+              '((lambda (u) (mv x y)) anything))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(assert-equal (mvify
+               '((lambda (u) (if a
+                                 (cons x (cons y (cons z 'nil)))
+                               (return-last a (h a) (cons u (cons v 'nil)))))
+                 '3/4))
+              '((lambda (u) (if a
+                                (mv x y z)
+                              (return-last a (h a) (mv u v))))
+                '3/4))
---
+++ books/kestrel/std/system/mvify.lisp
@@ -0,0 +1,91 @@
+; Standard System Library
+;
+; Copyright (C) 2019 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+(include-book "check-list-call")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define mvify ((term pseudo-termp))
+  :returns (new-term pseudo-termp :hyp :guard)
+  :parents (std/system/term-transformations)
+  :short "Turn a single-value term into a multi-value term."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "When a multi-value term is translated by ACL2,
+     it looks like a single-value term,
+     i.e. @('(mv x y z)') and @('(list x y x)')
+     are the same in translated form.
+     Thus, if the term, or a transformed version of it,
+     is untranslated (via @(tsee untranslate)) back,
+     the result is a single-value term, unlike the original term.")
+   (xdoc::p
+    "This utility can help obtain a multi-value untranslated term instead,
+     by turning certain translated occurrences of @('(list ...)')
+     into occurrences of @('(mv ...)') with the same arguments.
+     The resulting term is not quite a valid translated term
+     because @(tsee mv) is a macro and not a function,
+     but it is a pseudo-term,
+     and @(tsee untranslate) should handle occurrences of @(tsee mv)
+     as if it were a function (i.e. by leaving it unchanged).")
+   (xdoc::p
+    "This utility operates on translated terms,
+     assuming that they are obtained
+     either by translating valid untranslated multi-value terms,
+     or by transforming translated multi-value terms
+     in a way that preserves well-formedness with respect to multiple values
+     (i.e. that they always return
+     lists of two or more terms of the same length).")
+   (xdoc::p
+    "This utility replaces occurrences of translated @('(list ...)') subterms
+     at the ``leaves'' of the term.
+     We only consider @(tsee if), @(tsee return-last), and lambda applications
+     as non-leaf tree nodes:
+     @(tsee if) has two subtrees for the `then' and `else' arguments;
+     @(tsee return-last) has two subtree for the second and  third arguments;
+     and a lambda application has one subtree for the body.
+     In other words, we descend down (certain arguments of)
+     @(tsee if)s and @(tsee return-last)s,
+     and we descend down bodies of lambda expressions.
+     This is of course related to the fact that
+     the value of the built-in constant @('*stobjs-out-invalid*')
+     is @('(if return-last)').")
+   (xdoc::p
+    "All the other function calls are left unchanged
+     (i.e. they are considered tree leaves),
+     because presumably such functions already return multi-value results.
+     If a variable or quoted constant is encountered, it is an error:
+     a variable or quoted constant can never return a multiple value;
+     this utility must be applied to a term that is a function/lambda call,
+     and the recursion will stop before reaching any variable or quoted constant
+     if the term satisfies the assumptions stated earlier."))
+  (b* (((when (or (variablep term) (fquotep term)))
+        (raise "Unexpected term: ~x0." term))
+       (fn (ffn-symb term))
+       ((when (flambdap fn))
+        (fcons-term (make-lambda (lambda-formals fn)
+                                 (mvify (lambda-body fn)))
+                    (fargs term)))
+       ((when (eq fn 'if))
+        (fcons-term 'if
+                    (list (fargn term 1)
+                          (mvify (fargn term 2))
+                          (mvify (fargn term 3)))))
+       ((when (eq fn 'return-last))
+        (fcons-term 'return-last
+                    (list (fargn term 1)
+                          (mvify (fargn term 2))
+                          (mvify (fargn term 3)))))
+       ((mv list-call-p args) (check-list-call term)))
+    (if list-call-p
+        (fcons-term 'mv args)
+      term)))
