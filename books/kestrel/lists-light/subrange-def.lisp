24f21817e47bf32899edbbabd25c5f533c587302
Eric Smith <ewsmith@gmail.com>
2020-01-09 13:57:18 -0800

    [lists-light] Add the subrange function and theorems about it.

---
+++ books/kestrel/lists-light/subrange-def.lisp
@@ -0,0 +1,20 @@
+; A function to extract a contiguous segment of a list.
+;
+; Copyright (C) 2008-2011 Eric Smith and Stanford University
+; Copyright (C) 2013-2019 Kestrel Institute
+;
+; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
+;
+; Author: Eric Smith (eric.smith@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+;; Return a list of the elements of LST from START (inclusive) to END
+;; (inclusive), where the numbering is 0-based.
+(defund subrange (start end lst)
+  (declare (type (integer 0 *) start)
+           (type (integer 0 *) end)
+           (xargs :guard (true-listp lst)))
+  (nthcdr start (take (+ 1 end) lst)))
