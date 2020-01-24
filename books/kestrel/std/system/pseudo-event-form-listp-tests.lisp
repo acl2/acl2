9675df362a23a695f741dd2f78cdea07a97d9cfe
Alessandro Coglio <coglio@kestrel.edu>
2020-01-24 14:46:53 +0100

    [Std/system] Move PSEUDO-EVENT-FORM-LISTP tests here.

--- books/kestrel/std/system/pseudo-event-form-listp-tests.lisp
+++ books/kestrel/std/system/pseudo-event-form-listp-tests.lisp
@@ -1,6 +1,6 @@
-; System Utilities -- Event Form Lists -- Tests
+; Standard System Library
 ;
-; Copyright (C) 2018 Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2020 Kestrel Institute (http://www.kestrel.edu)
 ;
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 ;
@@ -10,8 +10,9 @@
 
 (in-package "ACL2")
 
-(include-book "event-form-lists")
-(include-book "kestrel/utilities/testing" :dir :system)
+(include-book "pseudo-event-form-listp")
+
+(include-book "misc/assert" :dir :system)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
