81560d5e3ae3b16c5749a4d03e5eeeb0621397fe
Alessandro Coglio <coglio@kestrel.edu>
2020-01-26 14:25:30 +0100

    [XDOC] Move DEFXDOC+ under [books]/xdoc.
    This utility was under the Kestrel Books ([books]/kestrel/utilities/xdoc/).
    
    It extends DEFXDOC with two conveniences:
    1. Order all the subtopics of the topic.
    2. Book-locally set the topic as default XDOC parent(s).
    
    Using DEFXDOC+ is shorter and more readable than DEFXDOC followed by a call of
    XDOC::ORDER-SUBTOPICS and XDOC::SET-DEFAULT-PARENTS.

--- books/xdoc/defxdoc-plus-tests.lisp
+++ books/xdoc/defxdoc-plus-tests.lisp
@@ -1,6 +1,6 @@
-; XDOC Utilities -- An Extension of DEFXDOC -- Tests
+; XDOC Documentation System for ACL2
 ;
-; Copyright (C) 2018 Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2020 Kestrel Institute (http://www.kestrel.edu)
 ;
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 ;
@@ -10,9 +10,10 @@
 
 (in-package "ACL2")
 
-(include-book "kestrel/utilities/testing" :dir :system)
-
 (include-book "defxdoc-plus")
+
+(include-book "misc/assert" :dir :system)
+(include-book "misc/eval" :dir :system)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
