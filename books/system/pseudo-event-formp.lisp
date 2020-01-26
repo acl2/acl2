d592f27695aa770f5c2220815f79f7e4da5cd1ff
Alessandro Coglio <coglio@kestrel.edu>
2020-01-26 15:36:05 +0100

    Add :DOC PSEUDO-EVENT-FORMP in [books]/system/.
    This kind of change was previously agreed upon with Matt and J.
    
    The previously existing :DOC PSEUDO-EVENT-FORMP in Std/system has been chnaged
    to Std/system/pseudo-event-formp, and only contains theorems.

--- books/system/pseudo-event-formp.lisp
+++ books/system/pseudo-event-formp.lisp
@@ -5,18 +5,28 @@
 
 (in-package "ACL2")
 
-; Here we formalize some constraints on untranslated event forms.  Because of
-; macros it is almost impossible to put constraints on event forms.  For example,
-; with an appropriate defmacro of barf, this could be a form (barf (1 . 2)).
-; But even macros have to be symbols and take a true list of args.  So we know
-; that much at the top but all bets are off after that.  The most rigorous test
-; would translate the alleged form, but that would require state and the
-; specification of translate's many options like whether stobjs are treated
-; specially.  Until we need it, we're not going to try to formalize the
-; stronger test.
+(include-book "xdoc/top" :dir :system)
 
-(defun pseudo-event-formp (x)
-  (declare (xargs :guard t))
-  (and (consp x)
-       (true-listp x)
-       (symbolp (car x)))) ; This symbolp could be a macro or a function.
+(defsection pseudo-event-formp
+  :parents (system-utilities-non-built-in)
+  :short "Recognize well-formed event forms."
+  :long
+  "<p>Here we formalize some constraints on untranslated event forms.
+      Because of macros it is almost impossible
+      to put constraints on event forms.
+      For example, with an appropriate @(tsee defmacro) of @('barf'),
+      this could be a form @('(barf (1 . 2))').
+      But even macros have to be symbols and take a true list of args.
+      So we know that much at the top but all bets are off after that.</p>
+   <p>The most rigorous test would translate the alleged form,
+      but that would require state
+      and the specification of translate's many options
+      like whether stobjs are treated specially.
+      Until we need it,
+      we are not going to try to formalize the stronger test.</p>"
+
+  (defun pseudo-event-formp (x)
+    (declare (xargs :guard t))
+    (and (consp x)
+         (true-listp x)
+         (symbolp (car x))))) ; This symbolp could be a macro or a function.
