ffdb015efccfdadfe95a187eb226d79fa703ecf4
Alessandro Coglio <coglio@kestrel.edu>
2020-01-28 17:17:16 +0100

    Add :DOC PSEUDO-COMMAND-FORMP.
    This kind of addition was previously agreed with Matt and J.

--- books/system/pseudo-command-formp.lisp
+++ books/system/pseudo-command-formp.lisp
@@ -5,12 +5,15 @@
 
 (in-package "ACL2")
 
-; Recognize command forms.
+(include-book "xdoc/top" :dir :system)
 
-(defun pseudo-command-formp (x)
+(defsection pseudo-command-formp
+  :parents (system-utilities-non-built-in)
+  :short "Recognize well-formed command forms."
+  :long
+  "<p>We see no reasonable way to restrict the form of a command,
+      other than to insist that it is a true list.</p>"
 
-; We see no reasonable way to restrict the form of a command, other than to
-; insist that it is a true list.
-
-  (declare (xargs :guard t))
-  (true-listp x))
+  (defun pseudo-command-formp (x)
+    (declare (xargs :guard t))
+    (true-listp x)))
