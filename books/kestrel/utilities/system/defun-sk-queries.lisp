7076b07e50527a048fa1509e1175ba04d6dc0c14
Alessandro Coglio <coglio@kestrel.edu>
2020-02-05 13:06:34 +0100

    Remove now-obsolete wrapper file.

--- books/kestrel/utilities/system/defun-sk-queries.lisp
+++
@@ -1,5 +0,0 @@
-; Temporary redirection wrapper file.
-
-(in-package "ACL2")
-
-(include-book "kestrel/std/system/defun-sk-queries" :dir :system)
--- books/kestrel/utilities/system/top.lisp
+++ books/kestrel/utilities/system/top.lisp
@@ -10,7 +10,6 @@
 
 (in-package "ACL2")
 
-(include-book "defun-sk-queries")
 (include-book "event-forms")
 (include-book "fresh-names")
 (include-book "install-not-norm-event")
