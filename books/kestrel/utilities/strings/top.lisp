f33a438ef2b28ddb15c1df3f0734713008444e82
Alessandro Coglio <coglio@kestrel.edu>
2020-02-07 00:33:50 +0100

    [strings] Add fixing theorems about CHARS=>NATS.
    This is in a separate file because it depends on FTY. So one can optionally
    include this file, for greater modularity.

--- books/kestrel/utilities/strings/top.lisp
+++ books/kestrel/utilities/strings/top.lisp
@@ -1,6 +1,6 @@
 ; String Utilities
 ;
-; Copyright (C) 2018 Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2020 Kestrel Institute (http://www.kestrel.edu)
 ;
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 ;
@@ -13,6 +13,7 @@
 (include-book "char-code-theorems")
 (include-book "char-kinds")
 (include-book "chars-codes")
+(include-book "chars-codes-fty")
 (include-book "hexchars")
 (include-book "hex-digit-char-theorems")
 (include-book "hexstrings")
