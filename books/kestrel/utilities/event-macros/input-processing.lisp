f079b2877186f36913f2f0d98c7b7b6eac523c76
Alessandro Coglio <coglio@kestrel.edu>
2020-02-12 14:15:21 +0100

    [event macros] Remove obsolete wrappers.

--- books/kestrel/utilities/event-macros/input-processing.lisp
+++
@@ -1,5 +0,0 @@
-; Temporary redirection wrapper file.
-
-(in-package "ACL2")
-
-(include-book "kestrel/event-macros/input-processing" :dir :system)
--- books/kestrel/utilities/event-macros/xdoc-constructors.lisp
+++
@@ -1,5 +0,0 @@
-; Temporary redirection wrapper file.
-
-(in-package "ACL2")
-
-(include-book "kestrel/event-macros/xdoc-constructors" :dir :system)
