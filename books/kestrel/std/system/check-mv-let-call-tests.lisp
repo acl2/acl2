2bc0438a36863bdc8de40930a273c5f9a6fc76b1
Alessandro Coglio <coglio@kestrel.edu>
2020-02-21 23:18:08 +0100

    [Std/system] Improve CHECK-MV-LET-CALL.
    Extend to handle cases in which the lambda expressions in the translated MV-LET
    have additional trivial variables (i.e. applied to themselves) to keep the
    lambda expressions closed. In cases like this the variable bound by the outer
    lambda to the multi-value may not be the symbol MV, if that occurs free in the
    untranslated term. So the utility now also returns the symbol used for that
    variable (often MV, but not always).
    
    Adapted the existing tests to the new result, and added new tests that exercise
    the situations with extra variables described above.

--- books/kestrel/std/system/check-mv-let-call-tests.lisp
+++ books/kestrel/std/system/check-mv-let-call-tests.lisp
@@ -12,65 +12,83 @@
 
 (include-book "check-mv-let-call")
 
-(include-book "misc/assert" :dir :system)
+(include-book "std/testing/assert" :dir :system)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(assert-equal (mv-list 4 (check-mv-let-call 'var))
-              '(nil nil nil nil))
+(defmacro check-mv-let-call-untrans (x)
+  `(b* (((mv & term &)
+         (translate1-cmp ',x :stobjs-out '((:stobjs-out . :stobjs-out))
+                         t 'top (w state) (default-state-vars nil))))
+     (mv-list 5 (check-mv-let-call term))))
 
-(assert-equal (mv-list 4 (check-mv-let-call '(quote "const")))
-              '(nil nil nil nil))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(assert-equal (mv-list 4 (check-mv-let-call '(f a b c)))
-              '(nil nil nil nil))
+(assert-equal (mv-list 5 (check-mv-let-call 'var))
+              '(nil nil nil nil nil))
 
-(assert-equal (mv-list 4 (check-mv-let-call '((lambda (x) (cons x x)) y)))
-              '(nil nil nil nil))
+(assert-equal (mv-list 5 (check-mv-let-call '(quote "const")))
+              '(nil nil nil nil nil))
 
-(assert-equal (mv-list 4 (check-mv-let-call '((lambda (mv)
+(assert-equal (mv-list 5 (check-mv-let-call '(f a b c)))
+              '(nil nil nil nil nil))
+
+(assert-equal (mv-list 5 (check-mv-let-call '((lambda (x) (cons x x)) y)))
+              '(nil nil nil nil nil))
+
+(assert-equal (mv-list 5 (check-mv-let-call '((lambda (mv)
                                                 ((lambda () (body))))
                                               mv-val)))
-              '(t nil mv-val (body)))
+              '(t mv nil mv-val (body)))
 
-(assert-equal (mv-list 4 (check-mv-let-call '((lambda (mv)
+(assert-equal (mv-list 5 (check-mv-let-call '((lambda (mv)
                                                 ((lambda (a) (body a))
                                                  (mv-nth '0 mv)))
                                               mv-val)))
-              '(t (a) mv-val (body a)))
+              '(t mv (a) mv-val (body a)))
 
-(assert-equal (mv-list 4 (check-mv-let-call '((lambda (mv)
+(assert-equal (mv-list 5 (check-mv-let-call '((lambda (mv)
                                                 ((lambda (a b) (body a b))
                                                  (mv-nth '0 mv)
                                                  (mv-nth '1 mv)))
                                               mv-val)))
-              '(t (a b) mv-val (body a b)))
+              '(t mv (a b) mv-val (body a b)))
 
-(assert-equal (mv-list 4 (check-mv-let-call '((lambda (mv)
+(assert-equal (mv-list 5 (check-mv-let-call '((lambda (mv)
                                                 ((lambda (a b c) (body a b c))
                                                  (mv-nth '0 mv)
                                                  (mv-nth '1 mv)
                                                  (mv-nth '2 mv)))
                                               mv-val)))
-              '(t (a b c) mv-val (body a b c)))
+              '(t mv (a b c) mv-val (body a b c)))
 
-(assert-equal (mv-list 4 (check-mv-let-call '((lambda (mvvv)
+(assert-equal (mv-list 5 (check-mv-let-call '((lambda (mvvv)
                                                 ((lambda (a b) (body a b))
                                                  (mv-nth '0 mv)
                                                  (mv-nth '1 mv)))
                                               mv-val)))
-              '(nil nil nil nil))
+              '(nil nil nil nil nil))
 
-(assert-equal (mv-list 4 (check-mv-let-call '((lambda (mv)
+(assert-equal (mv-list 5 (check-mv-let-call '((lambda (mv)
                                                 ((lambda (a b) (body a b))
                                                  (mv-nth '1 mv)
                                                  (mv-nth '0 mv)))
                                               mv-val)))
-              '(nil nil nil nil))
+              '(nil nil nil nil nil))
 
-(assert-equal (mv-list 4 (check-mv-let-call '((lambda (mv)
+(assert-equal (mv-list 5 (check-mv-let-call '((lambda (mv)
                                                 ((lambda (a b) (body a b))
                                                  (f mv)
                                                  (g mv)))
                                               mv-val)))
-              '(nil nil nil nil))
+              '(nil nil nil nil nil))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun f (x) (mv x x))
+
+(assert-equal (check-mv-let-call-untrans (mv-let (x y) (f u) (cons x y)))
+              '(t mv (x y) (f u) (cons x y)))
+
+(assert-equal (check-mv-let-call-untrans (mv-let (x y) (f mv) (list x y mv)))
+              '(t mv0 (x y) (f mv) (cons x (cons y (cons mv 'nil)))))
--- books/kestrel/std/system/check-mv-let-call.lisp
+++ books/kestrel/std/system/check-mv-let-call.lisp
@@ -10,15 +10,14 @@
 
 (in-package "ACL2")
 
-(include-book "std/util/define" :dir :system)
-(include-book "xdoc/constructors" :dir :system)
-
 (include-book "dumb-occur-var-open")
+(include-book "remove-trivial-vars")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define check-mv-let-call ((term pseudo-termp))
   :returns (mv (yes/no booleanp)
+               (mv-var symbolp :hyp :guard)
                (vars symbol-listp :hyp :guard)
                (mv-term pseudo-termp :hyp :guard)
                (body-term pseudo-termp :hyp :guard))
@@ -42,8 +41,9 @@
     "This utility checks if a translated term has the form above;
      it also checks that @('mv') does not occur free in @('body-term')
      (via @(tsee dumb-occur-var-open) for greater flexibility).
-     If it does, it returns @('t') as first result,
-     and additionally the list @('(var1 ... varn)'),
+     If all these checks pass, it returns @('t') as first result,
+     and additionally the variable @('mv'),
+     the list @('(var1 ... varn)'),
      the term @('mv-term-trans'),
      and the term @('body-term-trans').
      If the input term does not have that form,
@@ -61,34 +61,90 @@
      However, the result of translating @(tsee mv-let)
      necessarily has the form above.")
    (xdoc::p
+    "Note, however, that lambda expressions are always closed
+     in translated terms directly obtained from untranslated terms.
+     ACL2 accomplishes this closure
+     by adding formal paramaters to the lambda expressions
+     for the otherwise free variables,
+     and adding actual arguments identical to those variables;
+     see @(tsee remove-trivial-vars).
+     This means that the lambda expressions above may have extra variables.
+     To ``correct'' for this, before examing the two lambda expressions,
+     we remove all their formal parameters
+     that are identical to the corresponding arguments,
+     via @(tsee remove-trivial-vars)'s auxiliary function
+     @('remove-trivial-vars-aux'), which does exactly that.
+     We do not use @(tsee remove-trivial-vars) because that one
+     removes the trivial variables at all levels,
+     while here we only want to remove the ones
+     from the two lambda expressions being examined.")
+   (xdoc::p
+    "Note also that, due to this lambda expression closure,
+     the @('mv') variable is not always the symbol `@('mv')':
+     if that symbol happens to be a free variable,
+     ACL2's translation picks a different symbol
+     for the first formal argument of the outer lambda expression above.
+     For instance,")
+   (xdoc::codeblock
+    ":trans (let ((mv 0)) (mv-let (x y) (f mv) (list x y mv)))")
+   (xdoc::p
+    "produces a translated term with the symbol `@('mv0')'
+     as the variable bound to the multiple value.
+     This is why this utility returns this variable
+     as one of its results.")
+   (xdoc::p
     "This utility is essentially the inverse of @(tsee make-mv-let-call)."))
-  (b* (((when (variablep term)) (mv nil nil nil nil))
-       ((when (fquotep term)) (mv nil nil nil nil))
-       (lambda-mv (ffn-symb term))
-       ((unless (flambdap lambda-mv)) (mv nil nil nil nil))
-       (list-mv (lambda-formals lambda-mv))
-       ((unless (equal list-mv (list 'mv))) (mv nil nil nil nil))
-       (mv-term (fargn term 1))
-       (lambda-vars-of-mv-nths (lambda-body lambda-mv))
-       ((when (variablep lambda-vars-of-mv-nths)) (mv nil nil nil nil))
-       ((when (fquotep lambda-vars-of-mv-nths)) (mv nil nil nil nil))
-       (lambda-vars (ffn-symb lambda-vars-of-mv-nths))
-       ((unless (flambdap lambda-vars)) (mv nil nil nil nil))
-       (vars (lambda-formals lambda-vars))
-       (body-term (lambda-body lambda-vars))
-       ((when (dumb-occur-var-open 'mv body-term)) (mv nil nil nil nil))
-       (mv-nths (fargs lambda-vars-of-mv-nths))
-       ((unless (check-mv-let-call-aux mv-nths 0)) (mv nil nil nil nil)))
-    (mv t vars mv-term body-term))
+  (b* (((when (variablep term)) (mv nil nil nil nil nil))
+       ((when (fquotep term)) (mv nil nil nil nil nil))
+       ((unless (flambda-applicationp term)) (mv nil nil nil nil nil))
+       (outer-lambda-expr (ffn-symb term))
+       (formals (lambda-formals outer-lambda-expr))
+       (actuals (fargs term))
+       ((mv list-mv list-mv-term) (remove-trivial-vars-aux formals actuals))
+       ((unless (and (consp list-mv)
+                     (not (consp (cdr list-mv)))))
+        (mv nil nil nil nil nil))
+       (mv-var (car list-mv))
+       ((unless (and (consp list-mv-term)
+                     (not (consp (cdr list-mv-term)))))
+        (mv nil nil nil nil nil))
+       (mv-term (car list-mv-term))
+       (inner-lambda-expr-call (lambda-body outer-lambda-expr))
+       ((when (variablep inner-lambda-expr-call)) (mv nil nil nil nil nil))
+       ((when (fquotep inner-lambda-expr-call)) (mv nil nil nil nil nil))
+       ((unless (flambda-applicationp inner-lambda-expr-call))
+        (mv nil nil nil nil nil))
+       (inner-lambda-expr (ffn-symb inner-lambda-expr-call))
+       (formals (lambda-formals inner-lambda-expr))
+       (actuals (fargs inner-lambda-expr-call))
+       ((mv vars mv-nths) (remove-trivial-vars-aux formals actuals))
+       (body-term (lambda-body inner-lambda-expr))
+       ((when (dumb-occur-var-open mv-var body-term)) (mv nil nil nil nil nil))
+       ((unless (check-mv-let-call-aux mv-nths 0 mv-var))
+        (mv nil nil nil nil nil)))
+    (mv t mv-var vars mv-term body-term))
 
   :prepwork
-  ((define check-mv-let-call-aux ((terms pseudo-term-listp) (index natp))
+
+  ((define check-mv-let-call-aux ((terms pseudo-term-listp)
+                                  (index natp)
+                                  (mv-var symbolp))
      :returns (yes/no booleanp)
      (or (endp terms)
-         (and (equal (car terms) `(mv-nth ',index mv))
-              (check-mv-let-call-aux (cdr terms) (1+ index))))))
+         (and (equal (car terms) `(mv-nth ',index ,mv-var))
+              (check-mv-let-call-aux (cdr terms) (1+ index) mv-var))))
+
+   (local (include-book "std/typed-lists/symbol-listp" :dir :system))
+   (local (include-book "std/typed-lists/pseudo-term-listp" :dir :system)))
 
   ///
+
+  (local
+   (defthm acl2-count-of-check-mv-let-call.mv-term-lemma
+     (implies (consp x)
+              (< (acl2-count (car x))
+                 (acl2-count x)))
+     :rule-classes :linear))
 
   (defret acl2-count-of-check-mv-let-call.mv-term
     (implies yes/no
