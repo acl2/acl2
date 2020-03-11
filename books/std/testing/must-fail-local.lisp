ab99d9a0272331ea366de76b4526e747723978c8
Alessandro Coglio <coglio@kestrel.edu>
2020-03-11 12:23:08 -0700

    [Std/testing] Move MUST-FAIL-LOCAL into a separate file.
    This was discussed on the ACL2 Books mailing list.

---
+++ books/std/testing/must-fail-local.lisp
@@ -0,0 +1,27 @@
+; Standard Testing Library
+;
+; Copyright (C) 2020 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+(include-book "must-fail")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defsection must-fail-local
+  :parents (std/testing errors must-fail)
+  :short "A @(see local) variant of @(tsee must-fail)."
+  :long
+  "<p>
+   This is useful to overcome the problem discussed in the caveat
+   in the documentation of @(tsee must-fail).
+   </p>
+   @(def must-fail-local)"
+  (defmacro must-fail-local (&rest args)
+    `(local (must-fail ,@args))))
