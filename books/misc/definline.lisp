b5a3fa14f3e1d09d2dd32de17166bf25a8f72584
David L. Rager <ragerdl@gmail.com>
2020-03-17 12:25:59 -0500

    Take definline.lisp back out of public domain
    If this book becomes a real hassle for anyone, we should replace all calls of
    definline with defun-inline and just delete it.  ACL2 has usurped this functionality
    at this point

--- books/misc/definline.lisp
+++ books/misc/definline.lisp
@@ -1,10 +1,34 @@
 ;; definline.lisp - The definline and definlined macros.
-;; Jared Davis <jared@cs.utexas.edu>
-;;
-;; This file is in the public domain.  You can freely redistribute it and
-;; modify it for any purpose.  This file comes with absolutely no warranty,
-;; including the implied warranties of merchantibility and fitness for a
-;; particular purpose.
+; Copyright (C) 2005-2009 Kookamara LLC
+;
+; Contact:
+;
+;   Kookamara LLC
+;   11410 Windermere Meadows
+;   Austin, TX 78759, USA
+;   http://www.kookamara.com/
+;
+; License: (An MIT/X11-style license)
+;
+;   Permission is hereby granted, free of charge, to any person obtaining a
+;   copy of this software and associated documentation files (the "Software"),
+;   to deal in the Software without restriction, including without limitation
+;   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+;   and/or sell copies of the Software, and to permit persons to whom the
+;   Software is furnished to do so, subject to the following conditions:
+;
+;   The above copyright notice and this permission notice shall be included in
+;   all copies or substantial portions of the Software.
+;
+;   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+;   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+;   DEALINGS IN THE SOFTWARE.
+;
+; Original author: Jared Davis <jared@cs.utexas.edu>
 
 (in-package "ACL2")
 
