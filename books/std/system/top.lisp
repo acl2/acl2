9fc4c6fba274998547d9295c35ba7b02559fa275
Alessandro Coglio <coglio@kestrel.edu>
2020-03-17 17:16:57 -0700

    [Std/system] Add a top-level doc.
    This is in the "official" Std library under [books]/std/system.
    
    As discussed on the ACL2 Books mailing list, material from
    [books]/kestrel/std/system will be gradually moved to [books]/std/system.
    The addition of this top-level doc is the first step in this process.

--- books/std/system/top.lisp
+++ books/std/system/top.lisp
@@ -1,7 +1,31 @@
-; This book is a wrapper book for the library that we might consider to
-; constitute a "standard approach" to reasoning about ACL2 system functions.
+; Standard System Library
+;
+; Copyright (C) 2020 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (in-package "ACL2")
 
 (include-book "system/top" :dir :system)
 (include-book "non-parallel-book")
+
+(include-book "xdoc/constructors" :dir :system)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defxdoc std/system
+  :parents (std)
+  :short
+  (xdoc::topstring
+   "A library that complements the "
+   (xdoc::seetopic "system-utilities" "built-in system utilities")
+   " with theorems and with non-built-in system utilities.")
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "This is just a start towards a comprehensive library.
+     Some candidate utilities are under @('[books]/kestrel/std').")))
