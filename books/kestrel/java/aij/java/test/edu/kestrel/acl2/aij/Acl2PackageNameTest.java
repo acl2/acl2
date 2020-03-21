d509c92b1bebd85005f3cb88d9b792eec969a437
Alessandro Coglio <coglio@kestrel.edu>
2020-03-20 18:25:32 -0700

    [Java] Start adding JUnit tests to AIJ.
    This commit adds a single simple test, but also adds the necessary libraries to
    the IntelliJ IDEA project, and also a run configuration for the tests.
    
    The test code is in a directory 'test' that is parallel to 'src', which seems
    adequate since for now (and probably for the foreseeable future) AIJ will only
    include Java code.
    
    The AIJ makefile does not run the JUnit tests yet from the command line. These
    tests run in IntelliJ IDEA.
    
    The tests remain outside the AIJ jar, which only contains AIJ proper. The
    compiled class files for the tests go into a separate directory from the class
    files of AIJ proper. This is all handled by IntelliJ IDEA, via a few GUI setting
    selections.
    
    More JUnit tests are easy to add now.

---
+++ books/kestrel/java/aij/java/test/edu/kestrel/acl2/aij/Acl2PackageNameTest.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2020 Kestrel Institute (http://www.kestrel.edu)
+ * License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+ * Author: Alessandro Coglio (coglio@kestrel.edu)
+ */
+
+package edu.kestrel.acl2.aij;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Acl2PackageNameTest {
+
+    @Test
+    void makeFail() {
+        assertThrows(IllegalArgumentException.class,
+                () -> Acl2PackageName.make(null));
+    }
+}
