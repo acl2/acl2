21b9f7f2dafb6705689b38ba289643045b254ae5
Alessandro Coglio <coglio@kestrel.edu>
2020-03-21 21:41:19 -0700

    [Java] Add more AIJ unit tests.
    These are for testing the creation of packages with invalid names. The tests
    assert that an IllegalArgumentException must be thrown.

--- books/kestrel/java/aij/java/test/edu/kestrel/acl2/aij/Acl2PackageNameTest.java
+++ books/kestrel/java/aij/java/test/edu/kestrel/acl2/aij/Acl2PackageNameTest.java
@@ -13,8 +13,71 @@ import static org.junit.jupiter.api.Assertions.*;
 class Acl2PackageNameTest {
 
     @Test
-    void makeFail() {
+    void makeNull() {
         assertThrows(IllegalArgumentException.class,
                 () -> Acl2PackageName.make(null));
     }
+
+    @Test
+    void makeEmpty() {
+        assertThrows(IllegalArgumentException.class,
+                () -> Acl2PackageName.make(""));
+    }
+
+    @Test
+    void makeLowercase() {
+        assertThrows(IllegalArgumentException.class,
+                () -> Acl2PackageName.make("p"));
+        assertThrows(IllegalArgumentException.class,
+                () -> Acl2PackageName.make("abc"));
+        assertThrows(IllegalArgumentException.class,
+                () -> Acl2PackageName.make("MYpkg"));
+        assertThrows(IllegalArgumentException.class,
+                () -> Acl2PackageName.make("myPKG"));
+        assertThrows(IllegalArgumentException.class,
+                () -> Acl2PackageName.make("aBc"));
+    }
+
+    @Test
+    void makeNonASCII() {
+        assertThrows(IllegalArgumentException.class,
+                () -> Acl2PackageName.make("\200"));
+        assertThrows(IllegalArgumentException.class,
+                () -> Acl2PackageName.make("\200\240\300"));
+        assertThrows(IllegalArgumentException.class,
+                () -> Acl2PackageName.make("PKG\200"));
+        assertThrows(IllegalArgumentException.class,
+                () -> Acl2PackageName.make("\200PKG"));
+        assertThrows(IllegalArgumentException.class,
+                () -> Acl2PackageName.make("MY\200PKG"));
+    }
+
+    @Test
+    void makeControlASCII() { // i.e. ASCII characters below 32 (except 10)
+        assertThrows(IllegalArgumentException.class,
+                () -> Acl2PackageName.make("\b"));
+        assertThrows(IllegalArgumentException.class,
+                () -> Acl2PackageName.make("\b\t\f\r"));
+        assertThrows(IllegalArgumentException.class,
+                () -> Acl2PackageName.make("\bPKG"));
+        assertThrows(IllegalArgumentException.class,
+                () -> Acl2PackageName.make("PKG\b"));
+        assertThrows(IllegalArgumentException.class,
+                () -> Acl2PackageName.make("MY\bPKG"));
+    }
+
+    @Test
+    void makeDelete() { // i.e. ASCII character 127
+        assertThrows(IllegalArgumentException.class,
+                () -> Acl2PackageName.make("\377"));
+        assertThrows(IllegalArgumentException.class,
+                () -> Acl2PackageName.make("\377\377"));
+        assertThrows(IllegalArgumentException.class,
+                () -> Acl2PackageName.make("\377PKG"));
+        assertThrows(IllegalArgumentException.class,
+                () -> Acl2PackageName.make("PKG\377"));
+        assertThrows(IllegalArgumentException.class,
+                () -> Acl2PackageName.make("MY\377PKG"));
+    }
+
 }
