52bab6672cc9cc340a27c15c79ddea7e4b3c81d4
Alessandro Coglio <coglio@kestrel.edu>
2020-03-28 20:03:30 -0700

    [event macros] Add a new :HINTS input processor.
    This is meant to be used in conjunction with the new, recently introduced
    applicability condition utilities. It will eventually replace the similarly
    named one, once the latter is no longer used.

--- books/kestrel/event-macros/input-processing.lisp
+++ books/kestrel/event-macros/input-processing.lisp
@@ -10,11 +10,11 @@
 
 (in-package "ACL2")
 
+(include-book "evmac-input-hints-p")
 (include-book "evmac-input-print-p")
 
 (include-book "kestrel/utilities/error-checking/top" :dir :system)
 (include-book "kestrel/utilities/keyword-value-lists" :dir :system)
-(include-book "xdoc/defxdoc-plus" :dir :system)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -31,6 +31,11 @@
   :short "Process the @(':hints') input of an event macro."
   :long
   (xdoc::topstring
+   (xdoc::p
+    "This is being replaced by @(tsee evmac-process-input-hints$),
+     and will be removed when it is no longer used.
+     At that point, @(tsee evmac-process-input-hints$)
+     will be renamed to @('evmac-process-input-hints').")
    (xdoc::p
     "This is for event macros that have a @(':hints') input
      for user-supplied hints to prove applicability conditions.")
@@ -68,6 +73,61 @@
     (value alist))
   ;; for guard verification and return type proofs:
   :prepwork ((local (in-theory (enable ensure-keyword-value-list)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define evmac-process-input-hints$ (hints ctx state)
+  :returns (mv erp (hints$ evmac-input-hints-p) state)
+  :short "Process the @(':hints') input of an event macro."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "This is a replacement for @(tsee evmac-process-input-hints).
+     When that utility is no longer used, it will be removed,
+     and this new utility will be renamed to @('evmac-process-input-hints').")
+   (xdoc::p
+    "This is for event macros that have a @(':hints') input
+     for user-supplied hints to prove applicability conditions.")
+   (xdoc::p
+    "See the discussion in @(tsee evmac-input-hints-p)
+     about the possible forms of the @(':hints') input of an event macro.
+     This utility validates the @(':hints') input
+     and returns it in processed form.")
+   (xdoc::p
+    "If the @(':hints') input is a keyword-value list,
+     we check that the keywords are all distinct,
+     and return it in alist form.
+     We do not check that the keywords identify
+     applicability conditions that are actually present,
+     as this would complicate this input processing function.
+     Instead, as discussed in @(tsee evmac-appcond-theorem),
+     we let the event macro handle the situation.")
+   (xdoc::p
+    "If the @(':hints') input is not a keyword-value list,
+     we ensure that it is at least a true list,
+     and return it unchanged.")
+   (xdoc::p
+    "Note that if the input is (perhaps by default) @('nil'),
+     it is recognized as a keyword-value list with unique (no) keywords,
+     and returned unchanged as an alist, i.e. @('nil')."))
+  (if (keyword-value-listp hints)
+      (b* ((hints$ (keyword-value-list-to-alist hints))
+           (kwds (strip-cars hints$))
+           ((er &)
+            (ensure-list-no-duplicates$ kwds
+                                        (msg "The list of keywords ~x0 ~
+                                              in the keyword-value list ~
+                                              that forms the :HINTS input"
+                                             kwds)
+                                        t nil)))
+        (value hints$))
+    (if (true-listp hints)
+        (value hints)
+      (er-soft+ ctx t nil
+                "The :HINTS input must be ~
+                 either a keyword-value list or a true list, ~
+                 but it is ~x0 instead, which is neither."
+                hints))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
