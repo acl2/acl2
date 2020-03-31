f28b3a35ddc529577519da15266cea05f12f71e6
Alessandro Coglio <coglio@kestrel.edu>
2020-03-30 18:43:01 -0700

    [Std/system] Reduce book inclusion dependencies.

--- books/kestrel/std/system/arity-plus-tests.lisp
+++ books/kestrel/std/system/arity-plus-tests.lisp
@@ -1,6 +1,6 @@
 ; Standard System Library
 ;
-; Copyright (C) 2019 Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2020 Kestrel Institute (http://www.kestrel.edu)
 ;
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 ;
@@ -12,7 +12,7 @@
 
 (include-book "arity-plus")
 
-(include-book "std/testing/assert" :dir :system)
+(include-book "std/testing/assert-equal" :dir :system)
 (include-book "std/testing/eval" :dir :system)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
