56b3d554eeb0f1e3120634e4f5140d9e4eb9f0ff
Alessandro Coglio <coglio@kestrel.edu>
2020-04-02 18:06:01 -0700

    [Std/system] Reduce book inclusion dependencies.

--- books/kestrel/std/system/check-mv-let-call-tests.lisp
+++ books/kestrel/std/system/check-mv-let-call-tests.lisp
@@ -12,7 +12,7 @@
 
 (include-book "check-mv-let-call")
 
-(include-book "std/testing/assert" :dir :system)
+(include-book "std/testing/assert-equal" :dir :system)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
