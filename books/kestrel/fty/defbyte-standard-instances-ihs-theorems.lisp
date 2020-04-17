f6828aa93205374e9342ef8f3f874c4e8c1c2c1c
Alessandro Coglio <coglio@kestrel.edu>
2020-04-16 22:58:31 -0700

    [FTY] Add IHS theorems for some DEFBYTE standard instances.
    We start with SBYTE32 and SBYTE64 -- inspired by their upcoming use in the Java
    language formalization.
    
    We also add an XDOC topic to collect all of these.

---
+++ books/kestrel/fty/defbyte-standard-instances-ihs-theorems.lisp
@@ -0,0 +1,22 @@
+; FTY Library
+;
+; Copyright (C) 2020 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "FTY")
+
+(include-book "sbyte32-ihs-theorems")
+(include-book "sbyte64-ihs-theorems")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defxdoc defbyte-standard-instances-ihs-theorems
+  :parents (defbyte-standard-instances)
+  :short (xdoc::topstring
+          "Theorems about @(tsee defbyte) standard instances and "
+          (xdoc::seetopic "ihs" "IHS") " functions."))
---
+++ books/kestrel/fty/sbyte32-ihs-theorems.lisp
@@ -0,0 +1,24 @@
+; FTY Library
+;
+; Copyright (C) 2020 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+(include-book "sbyte32")
+
+(include-book "defbyte-ihs-theorems")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defsection sbyte32-ihs-theorems
+  :parents (sbyte32 defbyte-standard-instances-ihs-theorems)
+  :short (xdoc::topstring "Theorems about @(tsee sbyte32) and "
+                          (xdoc::seetopic "ihs" "IHS") " functions.")
+
+  (fty::defbyte-ihs-theorems sbyte32))
