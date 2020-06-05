82fa163c977c54add08c15c28dc7bdaddd44805a
Alessandro Coglio <coglio@kestrel.edu>
2020-06-04 21:47:26 -0700

    [Java] Add some ATJ tests.
    These exercise various aspects of the automatic mapping from ACL2 booleans to
    Java booleans.

---
+++ books/kestrel/java/atj/tests/booleans-deep-unguarded.lisp
@@ -0,0 +1,36 @@
+; Java Library
+;
+; Copyright (C) 2020 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+(include-book "booleans")
+
+(include-book "../implementation" :ttags (:open-input-channel :oslib :quicklisp :quicklisp.osicat))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Generate Java code for the functions on ACL2 booleans, with testing code.
+
+(java::atj negation
+           conjunction
+           disjunction
+           equality
+           nonequality
+           project1
+           project2
+           addition
+           tosymbol
+           fromsymbol
+           tovalue
+           fromvalue
+           :deep t
+           :guards nil
+           :java-class "BooleansDeepUnguarded"
+           :tests *boolean-tests*)
