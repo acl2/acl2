421cb0e39af03d0894ec5eee3e15ccbf6e3d0048
Alessandro Coglio <coglio@kestrel.edu>
2020-06-06 14:45:14 -0700

    [Std/util] Simplify DEFISO implementation.
    Now DEFISO is a thin wrapper of DEFMAPPING.
    
    Adapted ISODATA and PROPAGATE-ISO to the change.

--- books/doc/relnotes.lisp
+++ books/doc/relnotes.lisp
@@ -370,7 +370,9 @@
    (xdoc::p
     "A new event macro @(tsee defmapping) has been added,
      to verify and record mappings that must be (based on supplied options)
-     injective, surjective, or bijective.")
+     injective, surjective, or bijective.
+     The @(tsee defiso) event macro has been re-implemented
+     as a thin wrapper of @(tsee defmapping).")
 
    (xdoc::p
     "Support for @(':prepwork') has been added to @(tsee std::deflist).")
