a600dd976c4432eb03eeeb1dad859329fb367c17
Alessandro Coglio <coglio@kestrel.edu>
2020-07-17 22:36:55 -0700

    [Std/system] Add ALPHORDER to PURE-RAW-P.
    This is a whitelist of built-in functions with raw Lisp code but without side
    effects, and such that its unnormalized body accurately describes its
    input/output behavior.

--- books/kestrel/std/system/pure-raw-p.lisp
+++ books/kestrel/std/system/pure-raw-p.lisp
@@ -1,6 +1,6 @@
 ; Standard System Library
 ;
-; Copyright (C) 2019 Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2020 Kestrel Institute (http://www.kestrel.edu)
 ;
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 ;
@@ -43,6 +43,7 @@
        abs
        acons
        alpha-char-p
+       alphorder
        assoc-equal
        atom
        ash
