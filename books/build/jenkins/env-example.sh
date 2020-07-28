b9386434ad4b253d85f6588b9a87e14d61259062
David L. Rager <ragerdl@gmail.com>
2020-07-28 17:40:48 -0500

    Trivial change 02
    Another change to test build server

--- books/build/jenkins/env-example.sh
+++ books/build/jenkins/env-example.sh
@@ -3,6 +3,7 @@
 # The below alias doesn't seem to work for me.
 # alias startjob='bash'
 
+
 # Make sure that path is fine and includes ccl, sbcl, etc.
 git config --global user.email <your github email>
 git config --global user.name <your github name>
