616554311d97b767f26aeecb73f67d0b08467eff
David L. Rager <ragerdl@gmail.com>
2020-07-28 17:54:14 -0500

    Trivial change 03
    Another test for jenkins build configurations

--- books/build/jenkins/env-example.sh
+++ books/build/jenkins/env-example.sh
@@ -4,6 +4,7 @@
 # alias startjob='bash'
 
 
+
 # Make sure that path is fine and includes ccl, sbcl, etc.
 git config --global user.email <your github email>
 git config --global user.name <your github name>
