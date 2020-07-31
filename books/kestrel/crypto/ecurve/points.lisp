b67afebb1c15f6979f5771e29eeeba42a539356c
Alessandro Coglio <coglio@kestrel.edu>
2020-07-31 00:44:23 -0700

    [ecurve] Add XDOCumentation for points.

--- books/kestrel/crypto/ecurve/points.lisp
+++ books/kestrel/crypto/ecurve/points.lisp
@@ -12,13 +12,16 @@
 
 (in-package "ECURVE")
 
+(include-book "std/util/define" :dir :system)
+(include-book "xdoc/defxdoc-plus" :dir :system)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; Point Predicates
-; ----------------
-
-; This book introduces predicates
-; for points of elliptic curves over prime fields.
+(defxdoc+ points
+  :parents (elliptic-curves)
+  :short "Predicates for points of elliptic curves over prime fields."
+  :order-subtopics t
+  :default-parent t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -28,8 +31,18 @@
 ; This type of points is perhaps more general then elliptic curves,
 ; and thus it might be factored out into some more general library.
 
-(defund pointp (point)
-  (declare (xargs :guard t))
+(define pointp (point)
+  :short "Recognizer all possible points of all possible elliptic curves."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "This models (at least)
+     all possible points of all possible elliptic curves.
+     A point, as modeled here,
+     is either a pair of natural numbers or a special point at infinity.")
+   (xdoc::p
+    "This type of points is perhaps more general then elliptic curves,
+     and thus it might be factored out into some more general library."))
   (or (and (consp point)
            (natp (car point))
            (natp (cdr point)))
@@ -37,26 +50,36 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; The following predicate checks if a point (as defined above)
-; is in the cartesian product {0,...,p-1} * {0,...,p-1}, where p >= 2.
-; In the context of elliptic curves, p is the prime,
-; and this predicate checks if the point is in the "plane" of the curve.
-; This applies to finite points that are pairs of natural numbers;
-; the point at infinity satisfies this predicate,
-; because it is "outside" the cartesian product of the field with itself.
-; The purpose of this predicate is to provide a preliminary constraint
-; for points of curves in specific fields (described by p),
-; and thus we must include the point at infinity in this predicate.
-
-(defund point-in-pxp-p (point p)
-  (declare (xargs :guard (and (natp p) (<= 2 p)
-                              (pointp point))
-                  :guard-hints (("Goal" :in-theory (enable pointp)))))
+(define point-in-pxp-p ((point pointp) (p natp))
+  :guard (<= 2 p)
+  :short "Check if a point is in the cartesian product of a prime field."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "This predicate checks if a point (as defined in @(tsee pointp))
+     is in the cartesian product
+     @($\\{0, \\ldots, p-1\\} \\times \\{0, \\ldots, p-1\\}$),
+     where @($p \\geq 2$).
+     In the context of elliptic curves, @($p$) is the prime,
+     and this predicate checks if the point is in the ``plane'' of the curve.
+     This applies to finite points that are pairs of natural numbers;
+     the point at infinity satisfies this predicate,
+     because it is ``outside'' the cartesian product of the field with itself.
+     The purpose of this predicate is to provide a preliminary constraint
+     for points of curves in specific fields (described by @($p$)),
+     and thus we must include the point at infinity in this predicate.")
+   (xdoc::p
+    "We do not require the parameter @('p') to be prime here.
+     It suffices, for the purpose of defining this predicate,
+     for @('p') to be an integer that is at least 2."))
   (or (eq point :infinity)
       (and (< (car point) p)
-           (< (cdr point) p))))
+           (< (cdr point) p)))
+  :guard-hints (("Goal" :in-theory (enable pointp)))
 
-(defthm point-in-pxp-p-of-infinity
-  (implies (< 0 p)
-           (point-in-pxp-p :infinity p))
-  :hints (("Goal" :in-theory (enable point-in-pxp-p))))
+  ///
+
+  (defthm point-in-pxp-p-of-infinity
+    (implies (< 0 p)
+             (point-in-pxp-p :infinity p))
+    :hints (("Goal" :in-theory (enable point-in-pxp-p)))))
