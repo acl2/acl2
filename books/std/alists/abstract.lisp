68aa680a45ee561a0d4d19cf174eb469c57ecb64
Mihir Mehta <mihir@cs.utexas.edu>
2020-08-06 13:54:34 -0500

    Automatically generate a rule

--- books/std/alists/abstract.lisp
+++ books/std/alists/abstract.lisp
@@ -404,7 +404,16 @@ about keyval-alistp."
     (implies (and (keyval-alist-p x))
              (iff (keyval-alist-p (put-assoc-equal name val x))
                   (and (keytype-p name) (valtype-p val))))
-    :tags (:alistp)))
+    :tags (:alistp))
+
+  (def-alistp-rule valtype-p-of-cdr-of-assoc-when-keyval-alist-p-valtype-p-nil
+    (implies (and (keyval-alist-p x)
+                  (valtype-p nil))
+             (valtype-p (cdr (assoc-equal k x))))
+    :name valtype-p-of-cdr-of-assoc-when-keyval-alist-p
+    :requirement valtype-p-of-nil
+    :body (implies (keyval-alist-p x)
+                   (valtype-p (cdr (assoc-equal k x))))))
 
 
 ;; expensive...
