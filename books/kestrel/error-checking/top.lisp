a1c427b95b02dccdf45657c0fd4913960b830301
Alessandro Coglio <coglio@kestrel.edu>
2020-08-27 18:42:14 -0700

    [error checking] Add an improved error checker.

--- books/kestrel/error-checking/top.lisp
+++ books/kestrel/error-checking/top.lisp
@@ -22,6 +22,7 @@
 (include-book "ensure-value-is-string")
 (include-book "ensure-value-is-symbol")
 (include-book "ensure-value-is-symbol-list")
+(include-book "ensure-value-is-untranslated-term")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
