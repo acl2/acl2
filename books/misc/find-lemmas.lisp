8cb73d2d85e658ee606ff811d7d8bef404fe3ebe
Mihir Mehta <mihir@cs.utexas.edu>
2020-09-23 16:04:20 -0500

    Make a small mod to find-lemmas

--- books/misc/find-lemmas.lisp
+++ books/misc/find-lemmas.lisp
@@ -71,6 +71,65 @@
          (fns `(deref-macro-name-list ',fns (macro-aliases (w state)))))
     `(find-lemmas-fn ,fns ',omit-boot-strap nil (w state) (w state))))
 
+(defun find-funs-fn (fns omit-boot-strap acc wrld-tail wrld)
+  (declare (xargs :mode :program))
+  (if (or (endp wrld-tail)
+          (and omit-boot-strap
+               (and (eq (caar wrld-tail) 'command-landmark)
+                    (eq (cadar wrld-tail) 'global-value)
+                    (equal (access-command-tuple-form (cddar wrld-tail))
+                           '(exit-boot-strap-mode)))))
+      acc
+    (let* ((trip (car wrld-tail))
+           (ev-tuple (and (consp trip)
+                          (eq (car trip) 'event-landmark)
+                          (eq (cadr trip) 'global-value)
+                          (cddr trip)))
+           (type (and ev-tuple (access-event-tuple-type ev-tuple)))
+           (namex (and type (access-event-tuple-namex ev-tuple)))
+           (formula (and namex
+                         (symbolp namex)
+                         (member-eq type '(defun))
+                         (formula namex t wrld))))
+      (if (and formula
+               (subsetp-eq fns (all-fnnames formula)))
+          (find-funs-fn fns omit-boot-strap
+                          (cons (access-event-tuple-form ev-tuple) acc)
+                          (cdr wrld-tail)
+                          wrld)
+        (find-funs-fn fns omit-boot-strap acc (cdr wrld-tail) wrld)))))
+
+(defmacro find-funs (fns &optional (omit-boot-strap 't))
+  (declare (xargs :guard (let ((fns (if (and (true-listp fns)
+                                             (eq (car fns) 'quote)
+                                             (eql (length fns) 2))
+                                        (cadr fns)
+                                      fns)))
+                           (or (symbolp fns)
+                               (symbol-listp fns)))))
+
+; (find-funs (fn1 fn2 ...)) returns all lemmas in which all of the indicated
+; function symbols occur, except those lemmas in the ground-zero world.  In
+; order to include those as well, give a second argument of nil:
+; (find-funs (fn1 fn2 ...) nil).
+
+; If fns is a symbol, then fns is replaced by (list fns).
+
+  (let* ((fns (if (and (true-listp fns)
+                       (eq (car fns) 'quote)
+                       (eql (length fns) 2))
+                  (cadr fns)
+                fns))
+         (fns (cond
+               ((symbolp fns) (list fns))
+               ((symbol-listp fns) fns)
+               (t (er hard 'find-funs
+                      "The first argument to find-funs must be a symbol or ~
+                       a list of symbols, but ~x0 is not."
+                      fns))))
+         (fns `(deref-macro-name-list ',fns (macro-aliases (w state)))))
+    `(find-funs-fn ,fns ',omit-boot-strap nil (w state) (w state))))
+
 ; Documentation:
 
 (include-book "xdoc/top" :dir :system)
