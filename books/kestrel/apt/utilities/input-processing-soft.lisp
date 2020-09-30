6134906005dfc63c7b60588a2e9b5caefe592a29
Alessandro Coglio <coglio@kestrel.edu>
2020-09-29 18:33:57 -0700

    [APT] Add an input processor.
    This is for selecting an input variable of a function variable call in a
    specification of a certain form.

--- books/kestrel/apt/utilities/input-processing-soft.lisp
+++ books/kestrel/apt/utilities/input-processing-soft.lisp
@@ -10,6 +10,7 @@
 
 (in-package "APT")
 
+(include-book "kestrel/error-checking/ensure-value-is-in-list" :dir :system)
 (include-book "kestrel/soft/core" :dir :system)
 (include-book "kestrel/utilities/error-checking/top" :dir :system)
 
@@ -66,7 +67,8 @@
      the quantified variables,
      where the selected one @('x') may appear anywhere in the list
      (in fact, the exact @('x') is selected
-     via some other input of the transformation);
+     via some other input of the transformation:
+     see @(tsee process-input-select-old-soft-io));
      the arguments of the (only) call of the function variable,
      where the selected one @('x') may appear anywhere in the list
      (see parenthetical observation above);
@@ -142,3 +144,91 @@
        (iorel-body (subst-expr y ?f-call matrix))
        (iorel (make-lambda (append x-x1...xn (list y)) iorel-body)))
     (value (list old ?f x-x1...xn x-a1...am y iorel))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define process-input-select-old-soft-io (select-input-value
+                                          (select-input-keyword keywordp)
+                                          (old symbolp)
+                                          (?f symbolp)
+                                          (x-x1...xn symbol-listp)
+                                          (x-a1...am pseudo-term-listp)
+                                          ctx
+                                          state)
+  :returns (mv erp
+               (x symbolp
+                  :hints (("Goal"
+                           :in-theory
+                           (enable acl2::ensure-value-is-in-list))))
+               state)
+  :short "Process the input of an APT transformation
+          that selects an input of the function variable call in
+          a SOFT shallow pop-refinement specification of a certain form."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "This is related to @(tsee process-input-old-soft-io-sel-mod),
+     which checks that the target function of an APT transformation
+     is a SOFT shallow pop-refinement specification
+     of the form referenced in the documentation of that input processor.
+     That documentation mentions that the selected input
+     is specified via some other input of the transformation.
+     This input processor serves to process that input.")
+   (xdoc::p
+    "Some of the arguments of this input processor
+     are the homonymous results of @(tsee process-input-old-soft-io-sel-mod),
+     some of which are just for error messages.
+     There are two arguments about the transformation input to process:
+     its value and its keyword (the latter is just for error messages).")
+   (xdoc::p
+    "If the input being processed is @(':auto'),
+     which must be the default (this is assumed by an error messages below),
+     then the call of the function variable must have a single argument,
+     which must necessarily be a variable because of the checks
+     performed by @(tsee process-input-select-old-soft-io);
+     in this case, this is the argument selected by the input being processed.
+     Otherwise, the input being processed must be a variable
+     among the arguments of the call of the function variable."))
+  (b* (((er x) (if (eq select-input-value :auto)
+                   (if (= (len x-a1...am) 1)
+                       (value (car x-a1...am))
+                     (er-soft+ ctx t nil
+                               "The ~x0 input is ~
+                                (perhaps by default) :AUTO, ~
+                                but this is allowed only if ~
+                                the call of ~x1 in ~x2 ~
+                                has exactly one argument, ~
+                                but it has ~x3 arguments instead."
+                               select-input-keyword ?f old (len x-a1...am)))
+                 (b* (((er &) (ensure-value-is-in-list$
+                               select-input-value
+                               x-a1...am
+                               (msg "one of the arguments ~x0 of ~
+                                     the call of ~x1 in ~x2"
+                                    x-a1...am ?f old)
+                               (msg "The ~x0 input" select-input-keyword)
+                               t
+                               nil)))
+                   (value select-input-value))))
+       ((unless (symbolp x))
+        (er-soft+ ctx t nil
+                  "The argument ~x0 of the call of ~x1 in ~x2, ~
+                   specified (perhaps by default) by the ~x3 input, ~
+                   must be a variable, but it is not."
+                  x ?f old select-input-keyword))
+       ((unless (member-eq x x-x1...xn))
+        (value (raise "Internal error: ~
+                       the variable ~x0 in the call of ~x1 in ~x2 ~
+                       is not among the bound variables ~x3."
+                      x ?f old x-x1...xn)))
+       (a1...am (remove1-eq x x-a1...am))
+       ((when (member-eq x (all-vars (cons :dummy-fn a1...am))))
+        (er-soft+ ctx t nil
+                  "Aside from the argument ~x0 itself, ~
+                   the other arguments ~x1 of the call of ~x2 ~
+                   must not depend on ~x0, but they do."
+                  x a1...am ?f)))
+    (value x))
+  :prepwork
+  ((local (include-book "kestrel/std/system/all-vars" :dir :system))
+   (local (include-book "std/typed-lists/pseudo-term-listp" :dir :system))))
