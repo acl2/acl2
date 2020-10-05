908040da48decca6b18a03b7798ee18acc575e2e
Alessandro Coglio <coglio@kestrel.edu>
2020-10-04 22:39:18 -0700

    [C] Shorten references to the C18 standard.

--- books/kestrel/c/top.lisp
+++ books/kestrel/c/top.lisp
@@ -35,13 +35,13 @@
     (xdoc::ahref "https://www.iso.org/standard/74528.html"
                  "ISO/IEC 9899:2018 specification of C")
     ". In the documentation of this library,
-     this standard is referenced as `[C18]';
+     this standard is referenced as `[C]';
      sections are referenced
      by appending their designations separated by colon,
-     e.g. `[C18:6.2.6]' references Section 6.2.6;
+     e.g. `[C:6.2.6]' references Section 6.2.6;
      paragraphs are referenced
      by further appending their numbers separated by slash,
-     e.g. `[C18:6.2.5/2]' references Paragraph 2 of Section 6.2.5.
+     e.g. `[C:6.2.5/2]' references Paragraph 2 of Section 6.2.5.
      These square-bracketed references may be used
      as nouns or parenthetically."))
   :order-subtopics t)
