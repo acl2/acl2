6a86338fe9e16ae76a3de4692f4754662516fc9f
Eric Smith <ewsmith@gmail.com>
2020-10-24 17:34:50 -0700

    [axe] Fix/add copyrights.

--- books/kestrel/axe/known-booleans.lisp
+++ books/kestrel/axe/known-booleans.lisp
@@ -1,7 +1,7 @@
 ; A tool to track functions that are known to return only t or nil
 ;
-; Copyright (C) 2008-2011 Eric Smith and Stanford University
-; Copyright (C) 2013-2019 Kestrel Institute
+; Copyright (C) 2016-2020 Kestrel Institute
+; Copyright (C) 2016-2020 Kestrel Technology, LLC
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
 ;
