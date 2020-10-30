3c6f9d95252bd2896174d7d161db421733b2b7dc
Eric Smith <ewsmith@gmail.com>
2020-10-29 23:48:45 -0700

    [bv-lists] Combine two tiny books about all-unsigned-byte-p.

--- books/kestrel/bv-lists/all-unsigned-byte-p2.lisp
+++ books/kestrel/bv-lists/all-unsigned-byte-p2.lisp
@@ -1,4 +1,4 @@
-; A tiny book that provides the theorem all-unsigned-byte-p-of-reverse-list.
+; More theorems about all-unsigned-byte-p
 ;
 ; Copyright (C) 2008-2011 Eric Smith and Stanford University
 ; Copyright (C) 2013-2019 Kestrel Institute
@@ -11,8 +11,17 @@
 
 (in-package "ACL2")
 
+;; This book mixes all-unsigned-byte-p with other non-buit-in functions
+
 (include-book "all-unsigned-byte-p")
-(include-book "../lists-light/reverse-list")
+(include-book "../lists-light/repeat")
+(include-book "../lists-light/reverse-list-def")
+
+(defthm all-unsigned-byte-p-of-repeat
+  (equal (all-unsigned-byte-p width (repeat n x))
+         (or (zp n)
+             (unsigned-byte-p width x)))
+  :hints (("Goal" :in-theory (enable all-unsigned-byte-p repeat))))
 
 (defthm all-unsigned-byte-p-of-reverse-list
   (equal (all-unsigned-byte-p width (reverse-list x))
