bd0f0ed5823e5ce115212a5fd8be7e14dd32526a
Eric Smith <ewsmith@gmail.com>
2020-11-09 15:28:43 -0800

    [arithmetic-light] Add books about rem and ash.

---
+++ books/kestrel/arithmetic-light/rem.lisp
@@ -0,0 +1,17 @@
+; A lightweight book about the built-in function rem
+;
+; Copyright (C) 2008-2011 Eric Smith and Stanford University
+; Copyright (C) 2013-2020 Kestrel Institute
+;
+; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
+;
+; Author: Eric Smith (eric.smith@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+(defthm rem-of-0
+  (equal (rem x 0)
+         (fix x))
+  :hints (("Goal" :in-theory (enable rem))))
