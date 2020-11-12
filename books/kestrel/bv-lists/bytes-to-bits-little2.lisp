6daf272aeabf3e2588670d0df0488b7f230ec42d
Eric Smith <ewsmith@gmail.com>
2020-11-12 08:24:27 -0800

    [bv-lists] Add rule about bytes-to-bits-little.

--- books/kestrel/bv-lists/bytes-to-bits-little2.lisp
+++ books/kestrel/bv-lists/bytes-to-bits-little2.lisp
@@ -12,6 +12,17 @@
 (in-package "ACL2")
 
 (include-book "bytes-to-bits-little")
+(include-book "len-mult-of-8p")
 (include-book "../utilities/defopeners")
+(local (include-book "kestrel/arithmetic-light/mod" :dir :system))
 
 (defopeners bytes-to-bits-little :disable t)
+
+(defthm len-mult-of-8p-of-bytes-to-bits-little
+  (len-mult-of-8p (bytes-to-bits-little x))
+  :hints (("Goal" :in-theory (enable len-mult-of-8p))))
+
+;; (defthmd byte-to-bits-little-redef
+;;   (equal (byte-to-bits-little byte)
+;;          (reverse-list (unpackbv 8 1 byte)))
+;;   :hints (("Goal" :in-theory (enable byte-to-bits-little CAR-BECOMES-NTH-OF-0))))
