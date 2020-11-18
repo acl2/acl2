e9788f6da8070fe1fa92db063eb66b7f511921e4
Eric Smith <ewsmith@gmail.com>
2020-11-18 00:45:04 -0800

    [axe] Add worklist algorithm support.

---
+++ books/kestrel/axe/merge-sort-less-than.lisp
@@ -0,0 +1,17 @@
+; Mergesort using <
+;
+; Copyright (C) 2008-2011 Eric Smith and Stanford University
+; Copyright (C) 2013-2020 Kestrel Institute
+;
+; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
+;
+; Author: Eric Smith (eric.smith@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+(include-book "kestrel/utilities/defmergesort" :dir :system)
+
+;; will have all-rationalp as a guard
+(defmergesort merge-< merge-sort-< < rationalp)
