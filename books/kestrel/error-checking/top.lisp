39fc7707738d1d7bd3ba700f76ef526736caac57
Alessandro Coglio <coglio@kestrel.edu>
2020-11-20 19:44:09 -0800

    [error checking] Move here and improve an error checker.

--- books/kestrel/error-checking/top.lisp
+++ books/kestrel/error-checking/top.lisp
@@ -16,6 +16,7 @@
 (include-book "ensure-function-is-guard-verified")
 (include-book "ensure-function-is-logic-mode")
 (include-book "ensure-function-is-pure-if-raw")
+(include-book "ensure-list-has-no-duplicates")
 (include-book "ensure-symbol-is-fresh-event-name")
 (include-book "ensure-value-is-boolean")
 (include-book "ensure-value-is-function-name")
