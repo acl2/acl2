da6391774b4a86997e1b1773e216dac53208b301
Eric Smith <ewsmith@gmail.com>
2020-11-23 19:24:38 -0800

    [axe] Add file header.

--- books/kestrel/axe/prover-support.lisp
+++ books/kestrel/axe/prover-support.lisp
@@ -1,3 +1,15 @@
+; Supporting utilities for the Axe Prover(s)
+;
+; Copyright (C) 2008-2011 Eric Smith and Stanford University
+; Copyright (C) 2013-2020 Kestrel Institute
+; Copyright (C) 2016-2020 Kestrel Technology, LLC
+;
+; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
+;
+; Author: Eric Smith (eric.smith@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (in-package "ACL2")
 
 (include-book "kestrel/typed-lists-light/all-natp" :dir :system)
