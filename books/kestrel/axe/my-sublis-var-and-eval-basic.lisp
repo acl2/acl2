b430e73342a04de2f4b2c4461d95598b018cb3d8
Eric Smith <ewsmith@gmail.com>
2020-12-08 08:19:51 -0800

    [axe] Improve some param names.

--- books/kestrel/axe/my-sublis-var-and-eval-basic.lisp
+++ books/kestrel/axe/my-sublis-var-and-eval-basic.lisp
@@ -46,39 +46,40 @@
 ;todo: try cons-with-hint here?
 ;; This handles lambda applications correctly (by handling their args) but does not beta reduce.
 (mutual-recursion
+ ;; Returns a new term.
  (defund my-sublis-var-and-eval-basic (alist ;maps vars to nodenums/quoteps
-                                        form interpreted-function-alist)
+                                       term interpreted-function-alist)
    (declare (xargs :verify-guards nil ;done below
                    :guard (and (symbol-alistp alist)
                                (all-dargp (strip-cdrs alist))
-                               (pseudo-termp form)
+                               (pseudo-termp term)
                                (interpreted-function-alistp interpreted-function-alist))))
-   (cond ((variablep form)
-          (let ((a (assoc-eq form alist)))
+   (cond ((variablep term)
+          (let ((a (assoc-eq term alist)))
             (cond (a (cdr a))
-                  (t form) ;TODO: can we drop this case and the check above for some uses of this function (e.g., no free vars in a rule RHS or lambda body)
+                  (t term) ;TODO: can we drop this case and the check above for some uses of this function (e.g., no free vars in a rule RHS or lambda body)
                   )))
-         ((fquotep form) form)
-         (t (let ((fn (ffn-symb form)))
+         ((fquotep term) term)
+         (t (let ((fn (ffn-symb term)))
               (if (and (eq fn 'if) ;bozo, consider also handling bvif, boolif, myif, maybe boolor and booland...
-                       (= 3 (len (fargs form))))
-                  (let* ((test (second form))
+                       (= 3 (len (fargs term))))
+                  (let* ((test (second term))
                          (test-result (my-sublis-var-and-eval-basic alist test interpreted-function-alist)))
                     (if (quotep test-result)
                         (my-sublis-var-and-eval-basic alist (if (unquote test-result) ;if the test is not nil
-                                                          (third form) ;then part
-                                                        (fourth form) ;else part
-                                                        )
-                                                interpreted-function-alist)
+                                                                (third term) ;then part
+                                                              (fourth term) ;else part
+                                                              )
+                                                      interpreted-function-alist)
                       ;;couldn't resolve if-test:
                       (list 'if
                             test-result
-                            (my-sublis-var-and-eval-basic alist (third form) interpreted-function-alist) ;then part
-                            (my-sublis-var-and-eval-basic alist (fourth form) interpreted-function-alist) ;else part
+                            (my-sublis-var-and-eval-basic alist (third term) interpreted-function-alist) ;then part
+                            (my-sublis-var-and-eval-basic alist (fourth term) interpreted-function-alist) ;else part
                             )))
                 ;;regular function call or lambda
                 (mv-let (ground-termp args)
-                  (my-sublis-var-and-eval-basic-lst alist (fargs form) interpreted-function-alist)
+                  (my-sublis-var-and-eval-basic-lst alist (fargs term) interpreted-function-alist)
                   (if ground-termp
                       ;;ffixme, call something different here depending on whether it's an ifn (could get the body of the ifn by doing an assoc-eq above):
                       (mv-let (erp res)
@@ -95,65 +96,64 @@
                     ;; (cons fn args)))
                     (cons fn args))))))))
 
- ;;returns (mv ground-termp args)
- (defund my-sublis-var-and-eval-basic-lst (alist l interpreted-function-alist)
+ ;; Returns (mv ground-termp args).
+ (defund my-sublis-var-and-eval-basic-lst (alist terms interpreted-function-alist)
    (declare (xargs
              :verify-guards nil
              :guard (and (symbol-alistp alist)
                          (all-dargp (strip-cdrs alist)) ;gen?  really just need that things whose cars are 'quote are myquoteps
-                         ;(pseudo-term-listp (strip-cdrs alist))
-                         (pseudo-term-listp l)
+                         (pseudo-term-listp terms)
                          (interpreted-function-alistp interpreted-function-alist))))
-   (if (atom l)
+   (if (atom terms)
        (mv t nil)
-     (let ((new-car (my-sublis-var-and-eval-basic alist (car l) interpreted-function-alist)))
+     (let ((new-car (my-sublis-var-and-eval-basic alist (first terms) interpreted-function-alist)))
        (mv-let (cdr-ground-termp new-cdr)
-         (my-sublis-var-and-eval-basic-lst alist (cdr l) interpreted-function-alist)
+         (my-sublis-var-and-eval-basic-lst alist (rest terms) interpreted-function-alist)
          (mv (and cdr-ground-termp (quotep new-car))
              (cons new-car new-cdr)))))))
 
 (make-flag my-sublis-var-and-eval-basic)
 
 (defthm len-of-mv-nth-1-of-my-sublis-var-and-eval-basic-lst
-  (equal (len (mv-nth 1 (my-sublis-var-and-eval-basic-lst alist l interpreted-function-alist)))
-         (len l))
-  :hints (("Goal" :induct (len l) :in-theory (enable my-sublis-var-and-eval-basic-lst (:i len)))))
+  (equal (len (mv-nth 1 (my-sublis-var-and-eval-basic-lst alist terms interpreted-function-alist)))
+         (len terms))
+  :hints (("Goal" :induct (len terms) :in-theory (enable my-sublis-var-and-eval-basic-lst (:i len)))))
 
 (defthm true-listp-of-mv-nth-1-of-my-sublis-var-and-eval-basic-lst
-  (true-listp (mv-nth 1 (my-sublis-var-and-eval-basic-lst alist l interpreted-function-alist)))
-  :hints (("Goal" :induct (len l) :in-theory (enable my-sublis-var-and-eval-basic-lst (:i len)))))
+  (true-listp (mv-nth 1 (my-sublis-var-and-eval-basic-lst alist terms interpreted-function-alist)))
+  :hints (("Goal" :induct (len terms) :in-theory (enable my-sublis-var-and-eval-basic-lst (:i len)))))
 
 ;; maybe we should prove that it returns an axe-tree...
 ;; (defthm-flag-my-sublis-var-and-eval-basic
 ;;   (defthm pseudo-termp-of-my-sublis-var-and-eval-basic
-;;     (implies (and (pseudo-termp form)
+;;     (implies (and (pseudo-termp term)
 ;;                   (symbol-alistp alist)
 ;;                   ;(pseudo-term-listp (strip-cdrs alist))
 ;;                   (alistp interpreted-function-alist))
-;;              (pseudo-termp (my-sublis-var-and-eval-basic alist form interpreted-function-alist)))
+;;              (pseudo-termp (my-sublis-var-and-eval-basic alist term interpreted-function-alist)))
 ;;     :flag my-sublis-var-and-eval-basic)
 ;;   (defthm pseudo-term-listp-of-my-sublis-var-and-eval-basic-lst
-;;     (implies (and (pseudo-term-listp l)
+;;     (implies (and (pseudo-term-listp terms)
 ;;                   (symbol-alistp alist)
 ;;                   ;(pseudo-term-listp (strip-cdrs alist))
 ;;                   (alistp interpreted-function-alist))
-;;              (pseudo-term-listp (mv-nth 1 (my-sublis-var-and-eval-basic-lst alist l interpreted-function-alist))))
+;;              (pseudo-term-listp (mv-nth 1 (my-sublis-var-and-eval-basic-lst alist terms interpreted-function-alist))))
 ;;     :flag my-sublis-var-and-eval-basic-lst)
 ;;   :hints (("Goal" :in-theory (disable list::memberp-of-cons))))
 
 (defthm-flag-my-sublis-var-and-eval-basic
   (defthm myquotep-of-my-sublis-var-and-eval-basic
-    (implies (and (eq 'quote (car (my-sublis-var-and-eval-basic alist form interpreted-function-alist)))
+    (implies (and (eq 'quote (car (my-sublis-var-and-eval-basic alist term interpreted-function-alist)))
                   (all-dargp (strip-cdrs alist))
-                  (pseudo-termp form)
+                  (pseudo-termp term)
                   )
-             (myquotep (my-sublis-var-and-eval-basic alist form interpreted-function-alist)))
+             (myquotep (my-sublis-var-and-eval-basic alist term interpreted-function-alist)))
     :flag my-sublis-var-and-eval-basic)
   (defthm all-myquotep-of-mv-nth-1-of-my-sublis-var-and-eval-basic-lst
-    (implies (and (mv-nth 0 (my-sublis-var-and-eval-basic-lst alist l interpreted-function-alist))
+    (implies (and (mv-nth 0 (my-sublis-var-and-eval-basic-lst alist terms interpreted-function-alist))
                   (all-dargp (strip-cdrs alist))
-                  (pseudo-term-listp l))
-             (all-myquotep (mv-nth 1 (my-sublis-var-and-eval-basic-lst alist l interpreted-function-alist))))
+                  (pseudo-term-listp terms))
+             (all-myquotep (mv-nth 1 (my-sublis-var-and-eval-basic-lst alist terms interpreted-function-alist))))
     :flag my-sublis-var-and-eval-basic-lst)
   :hints (("Goal" :in-theory (e/d (my-sublis-var-and-eval-basic my-sublis-var-and-eval-basic-lst)
                                   (myquotep)))))
@@ -161,40 +161,40 @@
 (verify-guards my-sublis-var-and-eval-basic
   :hints (("Goal"
            :use (:instance myquotep-of-my-sublis-var-and-eval-basic
-                           (form (CADR FORM)))
+                           (term (CADR TERM)))
            :in-theory (e/d ()
                            (myquotep SYMBOL-ALISTP STRIP-CDRS myquotep-of-my-sublis-var-and-eval-basic)))))
 
 (defthm-flag-my-sublis-var-and-eval-basic
   (defthm axe-treep-of-my-sublis-var-and-eval-basic
-    (implies (and ;(eq 'quote (car (my-sublis-var-and-eval-basic alist form interpreted-function-alist)))
+    (implies (and ;(eq 'quote (car (my-sublis-var-and-eval-basic alist term interpreted-function-alist)))
                   (all-dargp (strip-cdrs alist))
-                  (pseudo-termp form)
+                  (pseudo-termp term)
                   )
-             (axe-treep (my-sublis-var-and-eval-basic alist form interpreted-function-alist)))
+             (axe-treep (my-sublis-var-and-eval-basic alist term interpreted-function-alist)))
     :flag my-sublis-var-and-eval-basic)
   (defthm all-axe-treep-of-mv-nth-1-of-my-sublis-var-and-eval-basic-lst
-    (implies (and ;(mv-nth 0 (my-sublis-var-and-eval-basic-lst alist l interpreted-function-alist))
+    (implies (and ;(mv-nth 0 (my-sublis-var-and-eval-basic-lst alist terms interpreted-function-alist))
                   (all-dargp (strip-cdrs alist))
-                  (pseudo-term-listp l))
-             (all-axe-treep (mv-nth 1 (my-sublis-var-and-eval-basic-lst alist l interpreted-function-alist))))
+                  (pseudo-term-listp terms))
+             (all-axe-treep (mv-nth 1 (my-sublis-var-and-eval-basic-lst alist terms interpreted-function-alist))))
     :flag my-sublis-var-and-eval-basic-lst)
   :hints (("Goal" :in-theory (e/d (my-sublis-var-and-eval-basic my-sublis-var-and-eval-basic-lst)
                                   (myquotep MYQUOTEP-OF-MY-SUBLIS-VAR-AND-EVAL-BASIC)))))
 
 (defthm-flag-my-sublis-var-and-eval-basic
   (defthm bounded-axe-treep-of-my-sublis-var-and-eval-basic
-    (implies (and ;(eq 'quote (car (my-sublis-var-and-eval-basic alist form interpreted-function-alist)))
+    (implies (and ;(eq 'quote (car (my-sublis-var-and-eval-basic alist term interpreted-function-alist)))
                   (all-dargp-less-than (strip-cdrs alist) dag-len)
-                  (pseudo-termp form)
+                  (pseudo-termp term)
                   )
-             (bounded-axe-treep (my-sublis-var-and-eval-basic alist form interpreted-function-alist) dag-len))
+             (bounded-axe-treep (my-sublis-var-and-eval-basic alist term interpreted-function-alist) dag-len))
     :flag my-sublis-var-and-eval-basic)
   (defthm all-bounded-axe-treep-of-mv-nth-1-of-my-sublis-var-and-eval-basic-lst
-    (implies (and ;(mv-nth 0 (my-sublis-var-and-eval-basic-lst alist l interpreted-function-alist))
+    (implies (and ;(mv-nth 0 (my-sublis-var-and-eval-basic-lst alist terms interpreted-function-alist))
                   (all-dargp-less-than (strip-cdrs alist) dag-len)
-                  (pseudo-term-listp l))
-             (all-bounded-axe-treep (mv-nth 1 (my-sublis-var-and-eval-basic-lst alist l interpreted-function-alist)) dag-len))
+                  (pseudo-term-listp terms))
+             (all-bounded-axe-treep (mv-nth 1 (my-sublis-var-and-eval-basic-lst alist terms interpreted-function-alist)) dag-len))
     :flag my-sublis-var-and-eval-basic-lst)
   :hints (("Goal" :in-theory (e/d (my-sublis-var-and-eval-basic
                                    my-sublis-var-and-eval-basic-lst
