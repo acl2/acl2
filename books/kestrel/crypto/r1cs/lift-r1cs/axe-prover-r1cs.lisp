430ec4f8ccbe3b40d985d2bd86755d2093bef0a9
Eric Smith <ewsmith@gmail.com>
2020-12-12 17:42:59 -0800

    [r1cs] Have the R1CS prover use a custom syntaxp-evaluator.
    This was introduced in a previous commit.

--- books/kestrel/crypto/r1cs/lift-r1cs/axe-prover-r1cs.lisp
+++ books/kestrel/crypto/r1cs/lift-r1cs/axe-prover-r1cs.lisp
@@ -12,11 +12,12 @@
 
 (include-book "kestrel/axe/make-prover-simple" :dir :system)
 (include-book "axe-evaluator-r1cs")
+(include-book "axe-syntaxp-evaluator-r1cs")
 ;; At least for now, the basic versions of these are fine:
 (include-book "kestrel/axe/axe-syntaxp-evaluator-basic" :dir :system)
 (include-book "kestrel/axe/axe-bind-free-evaluator-basic" :dir :system)
 
 (make-prover-simple r1cs
                     apply-axe-evaluator-r1cs-to-quoted-args
-                    eval-axe-syntaxp-expr-basic
+                    eval-axe-syntaxp-expr-r1cs
                     eval-axe-bind-free-function-application-basic)
