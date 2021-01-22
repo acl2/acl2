f38df08c68c2c621ef692878d2576f1d2953214d
Eric Smith <ewsmith@gmail.com>
2021-01-22 14:53:01 -0800

    [r1cs] Switch package of R1CS doc topics.

--- books/kestrel/crypto/r1cs/sparse/doc.lisp
+++ books/kestrel/crypto/r1cs/sparse/doc.lisp
@@ -8,25 +8,26 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(in-package "ACL2")
+(in-package "R1CS")
 
 ;; (depends-on r1cs.lisp")
 
 (include-book "kestrel/utilities/gen-xdoc-for-file" :dir :system)
 
 ;; Introduced by defaggregate
-(defxdoc r1cs-constraintp
+(acl2::defxdoc r1cs-constraintp
   :short "Recognize An R1CS constraint"
   :long "An R1CS constraint is a defaggregate with 3 fields, A, B, and C, each of which is a sparse vector.")
 
-(gen-xdoc-for-file "r1cs.lisp"
-                   ((pseudo-varp "Recognize a pseudo-variable (a symbol or 1).")
-                     (pseudo-var-listp "Recognize a true-list of pseudo-variables.")
-                     (sparse-vectorp "Recognize a sparse vector in an R1CS constraint.")
-                     ;; r1cs-constraintp documented above
-                     (r1cs-constraint-listp "Recognize a true list of R1CS constraints.")
-                     (dot-product "Dot product of R1CS coefficients and values.")
-                     (r1cs-constraint-holdsp "Check whether a valuation satisfies an R1CS constraint")
-                     (r1cs-constraints-holdp "Check whether a valuation satisfies a list of R1CS constraints")
-                     (r1cs-holdsp "Check whether a valuation satisifies an R1CS."))
-                   (r1cs))
+(acl2::gen-xdoc-for-file
+ "r1cs.lisp"
+ ((pseudo-varp "Recognize a pseudo-variable (a symbol or 1).")
+  (pseudo-var-listp "Recognize a true-list of pseudo-variables.")
+  (sparse-vectorp "Recognize a sparse vector in an R1CS constraint.")
+  ;; r1cs-constraintp documented above
+  (r1cs-constraint-listp "Recognize a true list of R1CS constraints.")
+  (dot-product "Dot product of R1CS coefficients and values.")
+  (r1cs-constraint-holdsp "Check whether a valuation satisfies an R1CS constraint")
+  (r1cs-constraints-holdp "Check whether a valuation satisfies a list of R1CS constraints")
+  (r1cs-holdsp "Check whether a valuation satisifies an R1CS."))
+ (r1cs))
