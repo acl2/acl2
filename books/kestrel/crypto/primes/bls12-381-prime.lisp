f0d86810b7ec816ea9808cf89e01c45aa500d59e
Eric Smith <ewsmith@gmail.com>
2021-01-25 08:47:04 -0800

    [primes] Move defprime to new number-theory library.

--- books/kestrel/crypto/primes/bls12-381-prime.lisp
+++ books/kestrel/crypto/primes/bls12-381-prime.lisp
@@ -24,7 +24,7 @@
 
 (in-package "PRIMES")
 
-(include-book "defprime")
+(include-book "kestrel/number-theory/defprime" :dir :system)
 
 (defprime bls12-381-scalar-field-prime
   52435875175126190479447740508185965837690552500527637822603658699938581184513
