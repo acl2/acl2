9eb2da9f3546b42464ed6b851f14b34f685c1c85
Eric Smith <ewsmith@gmail.com>
2021-02-11 07:25:33 -0800

    [r1cs] Add lift-r1cs/top.lisp.

---
+++ books/kestrel/crypto/r1cs/lift-r1cs/top.lisp
@@ -0,0 +1,15 @@
+; Top book for R1CS tools dir
+;
+; Copyright (C) 2020-2021 Kestrel Institute
+;
+; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
+;
+; Author: Eric Smith (eric.smith@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "R1CS")
+
+(include-book "lift-r1cs") ;brings in skip-proofs
+(include-book "lift-r1cs-new")
+(include-book "axe-prover-r1cs")
