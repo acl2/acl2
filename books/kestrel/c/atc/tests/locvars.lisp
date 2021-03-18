77422c6e6209130edd381ffa3920564a1662e339
Alessandro Coglio <coglio@kestrel.edu>
2021-03-18 16:06:33 -0700

    [ATC] Add more local variable tests.
    These are the ones used in the tutorial page.

--- books/kestrel/c/atc/tests/locvars.lisp
+++ books/kestrel/c/atc/tests/locvars.lisp
@@ -19,11 +19,27 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defun |f| (|x| |y|)
+  (declare (xargs :guard (and (c::sintp |x|) (c::sintp |y|))))
+  (let ((|z| (c::sint-lt |x| |y|)))
+    (c::sint-lognot |z|)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun |g| (|x| |y|)
+  (declare (xargs :guard (and (c::sintp |x|) (c::sintp |y|))))
+  (let* ((|x_lt_y| (c::sint-lt |x| |y|))
+         (|x_eq_y| (c::sint-eq |x| |y|))
+         (|x_le_y| (c::sint-logor |x_lt_y| |x_eq_y|)))
+    (c::sint-lognot |x_le_y|)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (encapsulate ()
 
   (local (include-book "arithmetic-5/top" :dir :system))
 
-  (defun |f| (|x| |y|)
+  (defun |h| (|x| |y|)
     (declare (xargs :guard (and (c::sintp |x|)
                                 (c::sintp |y|)
                                 ;; -10 <= x <= 10:
@@ -52,7 +68,7 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(c::atc |f| :output-file "locvars.c")
+(c::atc |f| |g| |h| :output-file "locvars.c")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
