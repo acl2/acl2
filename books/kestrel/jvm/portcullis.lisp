0346df096c3deabf2c63727eac16cf9ce6c918d7
Eric Smith <ewsmith@gmail.com>
2021-03-18 17:26:23 -0700

    [jvm] Start adding Kestrel's JVM model.
    Just add the package for now, as it is needed for a file I want to open source in Axe.

---
+++ books/kestrel/jvm/portcullis.lisp
@@ -0,0 +1,14 @@
+; JVM Library: Portcullis book
+;
+; Copyright (C) 2008-2011 Eric Smith and Stanford University
+; Copyright (C) 2013-2021 Kestrel Institute
+;
+; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
+;
+; Author: Eric Smith (eric.smith@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+;empty book that includes package.lsp in its portcullis
