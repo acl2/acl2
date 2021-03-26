f5c33392a161cd5bac2f4e02f5d7522a874b53ae
Eric McCarthy <mccarthy@kestrel.edu>
2021-03-25 21:39:19 -0700

    improve proof of properties of has-square-root?

--- books/kestrel/number-theory/quadratic-residue.lisp
+++ books/kestrel/number-theory/quadratic-residue.lisp
@@ -14,7 +14,9 @@
 (in-package "PRIMES")
 
 (include-book "arithmetic-3/floor-mod/mod-expt-fast" :dir :system)
-(include-book "kestrel/prime-fields/top" :dir :system)
+
+;; For the future: remove this dependency on prime-fields:
+(local (include-book "kestrel/prime-fields/top" :dir :system))
 
 (include-book "euler2-support")
 
@@ -22,14 +24,15 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (encapsulate ()
+  ;; We use arithmetic-3 locally here because it prevents later definitions
+  ;; from being accepted.
   (local (include-book "arithmetic-3/top" :dir :system))
 
   (defun has-square-root? (a p)
     (declare (xargs :guard (and (natp a) (natp p) (< 2 p) (< a p) (oddp p))))
-    (if (not (and (natp a) (natp p) (< 2 p) (< a p) (oddp p)))
-        nil
-      (equal (acl2::mod-expt-fast a (/ (- p 1) 2) p)
-             1)))
+      (or (= a 0)
+          (equal (acl2::mod-expt-fast a (/ (- p 1) 2) p)
+                 1)))
   )
 
 
@@ -38,41 +41,36 @@
 ;; Show that if (has-square-root? x) is NIL,
 ;; there is no y such that (mul y y p) equals x.
 
-
 ;; ----------------
 ;; 1. note that
 ;; (acl2::mod-expt-fast m (/ (- p 1) 2) p)
 ;; = (mod (expt m (/ (1- p) 2)) p)
 ;; in :logic mode.
+;; (Note: this is no longer used here, but someone might want it.)
 
-#||
-(defthm mod-expt-fast-instance-meaning
+(defthmd mod-expt-fast-instance-meaning
   (implies (and (rtl::primep p)
-                (fep m p))
+                (natp m) (< m p))
            (equal (acl2::mod-expt-fast m (/ (- p 1) 2) p)
                   (mod (expt m (/ (1- p) 2)) p)))
   :hints (("Goal" :in-theory (enable acl2::mod-expt-fast))))
-||#
+
 
 ;; ----------------
 ;; 2. prove that (not (has-square-root? m p)) implies (not (rtl::residue m p))
 ;; See rtl::euler-criterion
-;; [NOTE: what I really want in the consequent is a more general
-;;   (equal (rtl::residue m p) (has-square-root? m p))
-;;  so that I can use this to rewrite rtl::residue into has-square-root? which
-;;  is faster.]
 
 (defthm residue-meaning
   (implies (and (rtl::primep p)
                 (not (= p 2))
                 (oddp p)
-                (fep m p)
-                (not (equal 0 m))
-                ;(not (rtl::divides p m))
-                ) ; I would like a thm that fep implies this
+                (natp m) (< m p)
+                (not (equal 0 m)) ; This is needed because rtl::residue
+                                  ; returns false for m=0.
+                )
            (equal (rtl::residue m p)
                   (has-square-root? m p)))
-  :hints (("Goal" :in-theory (enable has-square-root? acl2::mod-expt-fast)
+  :hints (("Goal" :in-theory (enable has-square-root? acl2::mod-expt-fast rtl::residue)
                   :use ((:instance rtl::euler-criterion-2a (acl2::p p) (acl2::m m))
                         (:instance rtl::euler-criterion-2b (acl2::p p) (acl2::m
                                                                        m))))))
@@ -81,7 +79,7 @@
   (implies (and (rtl::primep p)
                 (not (= p 2))
                 (oddp p)
-                (fep m p)
+                (natp m) (< m p)
                 (not (equal 0 m))
                 ;(not (rtl::divides p m))
                 ) ; I would like a thm that fep implies this
@@ -91,49 +89,22 @@
 (theory-invariant (incompatible (:rewrite residue-meaning) (:rewrite residue-meaning-backwards)))
 
 
-;; (has-square-root? ..) is defined to be (equal (acl2::mod-expt-fast ..) 1)
-;; We can use something like rtl::euler-criterion-2a
-
-
-#||
-;; Something like this should be adequate for proving no-square-root-forall
-(defthm residue-meaning
-  (implies (and (rtl::primep p)
-                (> p 2)
-                (fep m p)
-                (not (has-square-root? m p)))
-           (not (rtl::residue m p)))
-  :hints (("Goal" :in-theory (enable rtl::euler-criterion-2a has-square-root?)))
-  )
-||#
-
-
 ;; 3. Prove if some x doesn't have a square root, it means y*y is never
 ;; equal to x.
 ;; See rtl::not-res-no-root
-
-#||
-(defthm no-square-root-forall
-  (implies (and (primep p)
-                (fep x p) (fep y p)
-                (not (has-square-root? x p)))
-           (equal (equal x (mul y y p)) nil)))
-||#
-
 
 (defthm no-square-root-forall
   (implies (and (not (has-square-root? x p)) ; conjunct ordering
                 (natp p)
                 (not (equal p 2))
                 (oddp p)
-                (fep x p)
-                (fep y p)
+                (natp x) (< x p)
+                (natp y) (< y p)
                 (rtl::primep p))
-           (equal (equal x (mul y y p))
-                  (if (equal x 0) ; it is good we negated and moved (not (= x 0)) into the conclusion since it made the rule more general
-                      (equal 0 (mul y y p))
+           (equal (equal x (mod (* y y) p))
+                  (if (equal x 0)
+                      (equal 0 (mod (* y y) p))
                     nil))
-   ;        (not (equal x (mul y y p)))
            )
   :hints (("Goal" :in-theory (e/d (residue-meaning-backwards mul) (residue-meaning
                                                                has-square-root?))
