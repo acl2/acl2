bf46e0b55a08dd8d0cd212550536057a0ed07536
Eric Smith <ewsmith@gmail.com>
2021-04-13 08:59:01 -0700

    [semaphore] Slightly improve proof examples.

--- books/kestrel/ethereum/semaphore/uint32xor-proof.lisp
+++ books/kestrel/ethereum/semaphore/uint32xor-proof.lisp
@@ -15,48 +15,35 @@
 (include-book "kestrel/crypto/r1cs/tools/axe-rules-r1cs" :dir :system)
 (include-book "kestrel/crypto/r1cs/proof-support" :dir :system)
 
-;; Load the R1CS:
-;; (depends-on "json/uint32xor.json")
-(local (acl2::load-circom-json "json/uint32xor.json" *BABY-JUBJUB-PRIME*))
-
-;;;
-;;; Unroll the R1CS
-;;;
-
-(local
- (lift-semaphore-r1cs *uint32xor-r1cs-lifted*
-                      (acl2::uint32xor-vars)
-                      (acl2::uint32xor-constraints)))
-
 ;;;
 ;;; The spec
 ;;;
 
 ;; Checks that the 32 output bits represent the bitwise XOR of the 32-bit inputs a and b.
-(defun spec (|main.a_bits[0]| |main.a_bits[1]| |main.a_bits[2]| |main.a_bits[3]|
-             |main.a_bits[4]| |main.a_bits[5]| |main.a_bits[6]| |main.a_bits[7]|
-             |main.a_bits[8]| |main.a_bits[9]| |main.a_bits[10]| |main.a_bits[11]|
-             |main.a_bits[12]| |main.a_bits[13]| |main.a_bits[14]| |main.a_bits[15]|
-             |main.a_bits[16]| |main.a_bits[17]| |main.a_bits[18]| |main.a_bits[19]|
-             |main.a_bits[20]| |main.a_bits[21]| |main.a_bits[22]| |main.a_bits[23]|
-             |main.a_bits[24]| |main.a_bits[25]| |main.a_bits[26]| |main.a_bits[27]|
-             |main.a_bits[28]| |main.a_bits[29]| |main.a_bits[30]| |main.a_bits[31]|
-             |main.b_bits[0]| |main.b_bits[1]| |main.b_bits[2]| |main.b_bits[3]|
-             |main.b_bits[4]| |main.b_bits[5]| |main.b_bits[6]| |main.b_bits[7]|
-             |main.b_bits[8]| |main.b_bits[9]| |main.b_bits[10]| |main.b_bits[11]|
-             |main.b_bits[12]| |main.b_bits[13]| |main.b_bits[14]| |main.b_bits[15]|
-             |main.b_bits[16]| |main.b_bits[17]| |main.b_bits[18]| |main.b_bits[19]|
-             |main.b_bits[20]| |main.b_bits[21]| |main.b_bits[22]| |main.b_bits[23]|
-             |main.b_bits[24]| |main.b_bits[25]| |main.b_bits[26]| |main.b_bits[27]|
-             |main.b_bits[28]| |main.b_bits[29]| |main.b_bits[30]| |main.b_bits[31]|
-             |main.out_bits[0]| |main.out_bits[1]| |main.out_bits[2]| |main.out_bits[3]|
-             |main.out_bits[4]| |main.out_bits[5]| |main.out_bits[6]| |main.out_bits[7]|
-             |main.out_bits[8]| |main.out_bits[9]| |main.out_bits[10]| |main.out_bits[11]|
-             |main.out_bits[12]| |main.out_bits[13]| |main.out_bits[14]| |main.out_bits[15]|
-             |main.out_bits[16]| |main.out_bits[17]| |main.out_bits[18]| |main.out_bits[19]|
-             |main.out_bits[20]| |main.out_bits[21]| |main.out_bits[22]| |main.out_bits[23]|
-             |main.out_bits[24]| |main.out_bits[25]| |main.out_bits[26]| |main.out_bits[27]|
-             |main.out_bits[28]| |main.out_bits[29]| |main.out_bits[30]| |main.out_bits[31]|)
+(defun uint32xor-spec (|main.a_bits[0]| |main.a_bits[1]| |main.a_bits[2]| |main.a_bits[3]|
+                       |main.a_bits[4]| |main.a_bits[5]| |main.a_bits[6]| |main.a_bits[7]|
+                       |main.a_bits[8]| |main.a_bits[9]| |main.a_bits[10]| |main.a_bits[11]|
+                       |main.a_bits[12]| |main.a_bits[13]| |main.a_bits[14]| |main.a_bits[15]|
+                       |main.a_bits[16]| |main.a_bits[17]| |main.a_bits[18]| |main.a_bits[19]|
+                       |main.a_bits[20]| |main.a_bits[21]| |main.a_bits[22]| |main.a_bits[23]|
+                       |main.a_bits[24]| |main.a_bits[25]| |main.a_bits[26]| |main.a_bits[27]|
+                       |main.a_bits[28]| |main.a_bits[29]| |main.a_bits[30]| |main.a_bits[31]|
+                       |main.b_bits[0]| |main.b_bits[1]| |main.b_bits[2]| |main.b_bits[3]|
+                       |main.b_bits[4]| |main.b_bits[5]| |main.b_bits[6]| |main.b_bits[7]|
+                       |main.b_bits[8]| |main.b_bits[9]| |main.b_bits[10]| |main.b_bits[11]|
+                       |main.b_bits[12]| |main.b_bits[13]| |main.b_bits[14]| |main.b_bits[15]|
+                       |main.b_bits[16]| |main.b_bits[17]| |main.b_bits[18]| |main.b_bits[19]|
+                       |main.b_bits[20]| |main.b_bits[21]| |main.b_bits[22]| |main.b_bits[23]|
+                       |main.b_bits[24]| |main.b_bits[25]| |main.b_bits[26]| |main.b_bits[27]|
+                       |main.b_bits[28]| |main.b_bits[29]| |main.b_bits[30]| |main.b_bits[31]|
+                       |main.out_bits[0]| |main.out_bits[1]| |main.out_bits[2]| |main.out_bits[3]|
+                       |main.out_bits[4]| |main.out_bits[5]| |main.out_bits[6]| |main.out_bits[7]|
+                       |main.out_bits[8]| |main.out_bits[9]| |main.out_bits[10]| |main.out_bits[11]|
+                       |main.out_bits[12]| |main.out_bits[13]| |main.out_bits[14]| |main.out_bits[15]|
+                       |main.out_bits[16]| |main.out_bits[17]| |main.out_bits[18]| |main.out_bits[19]|
+                       |main.out_bits[20]| |main.out_bits[21]| |main.out_bits[22]| |main.out_bits[23]|
+                       |main.out_bits[24]| |main.out_bits[25]| |main.out_bits[26]| |main.out_bits[27]|
+                       |main.out_bits[28]| |main.out_bits[29]| |main.out_bits[30]| |main.out_bits[31]|)
   (equal (acl2::packbv '32 '1
                        (list |main.out_bits[31]| |main.out_bits[30]|
                              |main.out_bits[29]| |main.out_bits[28]|
@@ -109,35 +96,56 @@
                                           |main.b_bits[3]| |main.b_bits[2]|
                                           |main.b_bits[1]| |main.b_bits[0]|)))))
 
-;; Do the proof:
+;;;
+;;; Load the R1CS
+;;;
+
+;; (depends-on "json/uint32xor.json")
+(local (acl2::load-circom-json "json/uint32xor.json" *BABY-JUBJUB-PRIME*))
+
+;;;
+;;; Unroll the R1CS
+;;;
+
+(local
+ (lift-semaphore-r1cs *uint32xor-r1cs-lifted*
+                      (acl2::uint32xor-vars)
+                      (acl2::uint32xor-constraints)))
+
+;;;
+;;; Verify the R1CS
+;;;
+
 (verify-semaphore-r1cs
  ;; The R1CS:
  *uint32xor-r1cs-lifted*
  ;; The spec:
- (spec |main.a_bits[0]| |main.a_bits[1]| |main.a_bits[2]| |main.a_bits[3]|
-   |main.a_bits[4]| |main.a_bits[5]| |main.a_bits[6]| |main.a_bits[7]|
-   |main.a_bits[8]| |main.a_bits[9]| |main.a_bits[10]| |main.a_bits[11]|
-   |main.a_bits[12]| |main.a_bits[13]| |main.a_bits[14]| |main.a_bits[15]|
-   |main.a_bits[16]| |main.a_bits[17]| |main.a_bits[18]| |main.a_bits[19]|
-   |main.a_bits[20]| |main.a_bits[21]| |main.a_bits[22]| |main.a_bits[23]|
-   |main.a_bits[24]| |main.a_bits[25]| |main.a_bits[26]| |main.a_bits[27]|
-   |main.a_bits[28]| |main.a_bits[29]| |main.a_bits[30]| |main.a_bits[31]|
-   |main.b_bits[0]| |main.b_bits[1]| |main.b_bits[2]| |main.b_bits[3]|
-   |main.b_bits[4]| |main.b_bits[5]| |main.b_bits[6]| |main.b_bits[7]|
-   |main.b_bits[8]| |main.b_bits[9]| |main.b_bits[10]| |main.b_bits[11]|
-   |main.b_bits[12]| |main.b_bits[13]| |main.b_bits[14]| |main.b_bits[15]|
-   |main.b_bits[16]| |main.b_bits[17]| |main.b_bits[18]| |main.b_bits[19]|
-   |main.b_bits[20]| |main.b_bits[21]| |main.b_bits[22]| |main.b_bits[23]|
-   |main.b_bits[24]| |main.b_bits[25]| |main.b_bits[26]| |main.b_bits[27]|
-   |main.b_bits[28]| |main.b_bits[29]| |main.b_bits[30]| |main.b_bits[31]|
-   |main.out_bits[0]| |main.out_bits[1]| |main.out_bits[2]| |main.out_bits[3]|
-   |main.out_bits[4]| |main.out_bits[5]| |main.out_bits[6]| |main.out_bits[7]|
-   |main.out_bits[8]| |main.out_bits[9]| |main.out_bits[10]| |main.out_bits[11]|
-   |main.out_bits[12]| |main.out_bits[13]| |main.out_bits[14]| |main.out_bits[15]|
-   |main.out_bits[16]| |main.out_bits[17]| |main.out_bits[18]| |main.out_bits[19]|
-   |main.out_bits[20]| |main.out_bits[21]| |main.out_bits[22]| |main.out_bits[23]|
-   |main.out_bits[24]| |main.out_bits[25]| |main.out_bits[26]| |main.out_bits[27]|
-   |main.out_bits[28]| |main.out_bits[29]| |main.out_bits[30]| |main.out_bits[31]|)
+ (uint32xor-spec |main.a_bits[0]| |main.a_bits[1]| |main.a_bits[2]|
+                 |main.a_bits[3]| |main.a_bits[4]| |main.a_bits[5]| |main.a_bits[6]|
+                 |main.a_bits[7]| |main.a_bits[8]| |main.a_bits[9]| |main.a_bits[10]|
+                 |main.a_bits[11]| |main.a_bits[12]| |main.a_bits[13]| |main.a_bits[14]|
+                 |main.a_bits[15]| |main.a_bits[16]| |main.a_bits[17]| |main.a_bits[18]|
+                 |main.a_bits[19]| |main.a_bits[20]| |main.a_bits[21]| |main.a_bits[22]|
+                 |main.a_bits[23]| |main.a_bits[24]| |main.a_bits[25]| |main.a_bits[26]|
+                 |main.a_bits[27]| |main.a_bits[28]| |main.a_bits[29]| |main.a_bits[30]|
+                 |main.a_bits[31]| |main.b_bits[0]| |main.b_bits[1]| |main.b_bits[2]|
+                 |main.b_bits[3]| |main.b_bits[4]| |main.b_bits[5]| |main.b_bits[6]|
+                 |main.b_bits[7]| |main.b_bits[8]| |main.b_bits[9]| |main.b_bits[10]|
+                 |main.b_bits[11]| |main.b_bits[12]| |main.b_bits[13]| |main.b_bits[14]|
+                 |main.b_bits[15]| |main.b_bits[16]| |main.b_bits[17]| |main.b_bits[18]|
+                 |main.b_bits[19]| |main.b_bits[20]| |main.b_bits[21]| |main.b_bits[22]|
+                 |main.b_bits[23]| |main.b_bits[24]| |main.b_bits[25]| |main.b_bits[26]|
+                 |main.b_bits[27]| |main.b_bits[28]| |main.b_bits[29]| |main.b_bits[30]|
+                 |main.b_bits[31]| |main.out_bits[0]| |main.out_bits[1]| |main.out_bits[2]|
+                 |main.out_bits[3]| |main.out_bits[4]| |main.out_bits[5]| |main.out_bits[6]|
+                 |main.out_bits[7]| |main.out_bits[8]| |main.out_bits[9]| |main.out_bits[10]|
+                 |main.out_bits[11]| |main.out_bits[12]| |main.out_bits[13]|
+                 |main.out_bits[14]| |main.out_bits[15]| |main.out_bits[16]|
+                 |main.out_bits[17]| |main.out_bits[18]| |main.out_bits[19]|
+                 |main.out_bits[20]| |main.out_bits[21]| |main.out_bits[22]|
+                 |main.out_bits[23]| |main.out_bits[24]| |main.out_bits[25]|
+                 |main.out_bits[26]| |main.out_bits[27]| |main.out_bits[28]|
+                 |main.out_bits[29]| |main.out_bits[30]| |main.out_bits[31]|)
  :bit-inputs
  '(|main.a_bits[0]| |main.a_bits[1]| |main.a_bits[2]| |main.a_bits[3]|
    |main.a_bits[4]| |main.a_bits[5]| |main.a_bits[6]| |main.a_bits[7]|
@@ -222,22 +230,22 @@
                 r1cs::equal-of-xor-idiom-b
                 r1cs::equal-of-xor-idiom-b-alt
                 )
-               (spec
-                 car-cons
-                 cdr-cons
-                 acl2::equal-same
-                 acl2::packbv-opener
-                 acl2::bvcat-equal-rewrite
-                 acl2::bvcat-equal-rewrite-alt
-                 acl2::bvchop-of-bvcat-cases
-                 acl2::slice-becomes-getbit
-                 acl2::getbit-of-bvxor
-                 acl2::bvchop-of-bvxor
-                 (acl2::unsigned-byte-p-rules)
-                 acl2::getbit-of-bitxor-all-cases
-                 acl2::getbit-of-bvcat-all
-                 acl2::getbit-of-0-when-bitp
-                 acl2::bvxor-1-becomes-bitxor
-                 acl2::bitxor-of-bvcat-irrel-arg1
-                 acl2::bitxor-of-bvcat-irrel-arg2
-                 acl2::bitxor-commutative-increasing-dag)))
+               (uint32xor-spec
+                car-cons
+                cdr-cons
+                acl2::equal-same
+                acl2::packbv-opener
+                acl2::bvcat-equal-rewrite
+                acl2::bvcat-equal-rewrite-alt
+                acl2::bvchop-of-bvcat-cases
+                acl2::slice-becomes-getbit
+                acl2::getbit-of-bvxor
+                acl2::bvchop-of-bvxor
+                (acl2::unsigned-byte-p-rules)
+                acl2::getbit-of-bitxor-all-cases
+                acl2::getbit-of-bvcat-all
+                acl2::getbit-of-0-when-bitp
+                acl2::bvxor-1-becomes-bitxor
+                acl2::bitxor-of-bvcat-irrel-arg1
+                acl2::bitxor-of-bvcat-irrel-arg2
+                acl2::bitxor-commutative-increasing-dag)))
