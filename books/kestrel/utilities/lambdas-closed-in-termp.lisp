306f5663eeb051c448e45612482fb5fdbc9281c4
Eric Smith <ewsmith@gmail.com>
2021-04-14 10:23:20 -0700

    [utilities] Fix lambdas-closed-in-termp and keep it disabled.

--- books/kestrel/utilities/lambdas-closed-in-termp.lisp
+++ books/kestrel/utilities/lambdas-closed-in-termp.lisp
@@ -14,7 +14,7 @@
 
 ;; Checks that all free vars in lambda bodies are among the corresponding lambda vars
 (mutual-recursion
- (defun lambdas-closed-in-termp (term)
+ (defund lambdas-closed-in-termp (term)
    (declare (xargs :guard (pseudo-termp term)))
    (if (variablep term)
        t
@@ -23,12 +23,35 @@
            t
          (and (lambdas-closed-in-termsp (fargs term))
               (if (consp fn) ; fn is (lambda (...vars...) body)
-                  (subsetp-equal (vars-in-term (third fn))
-                                 (second fn))
+                  (and (lambdas-closed-in-termp (third fn))
+                       (subsetp-equal (vars-in-term (third fn))
+                                      (second fn)))
                 t))))))
- (defun lambdas-closed-in-termsp (terms)
+ (defund lambdas-closed-in-termsp (terms)
    (declare (xargs :guard (pseudo-term-listp terms)))
    (if (endp terms)
        t
      (and (lambdas-closed-in-termp (first terms))
           (lambdas-closed-in-termsp (rest terms))))))
+
+(defthm lambdas-closed-in-termsp-of-cdr
+  (implies (lambdas-closed-in-termsp terms)
+           (lambdas-closed-in-termsp (cdr terms)))
+  :hints (("Goal" :in-theory (enable lambdas-closed-in-termsp))))
+
+(defthm lambdas-closed-in-termp-of-car
+  (implies (lambdas-closed-in-termsp terms)
+           (lambdas-closed-in-termp (car terms)))
+  :hints (("Goal" :in-theory (enable lambdas-closed-in-termsp))))
+
+(defthm lambdas-closed-in-termsp-of-when-lambdas-closed-in-termp
+   (implies (and (lambdas-closed-in-termp term)
+                 (not (equal 'quote (car term))))
+           (lambdas-closed-in-termsp (cdr term)))
+  :hints (("Goal" :in-theory (enable lambdas-closed-in-termp))))
+
+(defthm lambdas-closed-in-termsp-of-caddr-of-car
+  (implies (and (lambdas-closed-in-termp term)
+                (not (equal 'quote (car term))))
+           (lambdas-closed-in-termp (caddr (car term))))
+  :hints (("Goal" :in-theory (enable lambdas-closed-in-termp))))
