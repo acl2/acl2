1af383f6b412effb76f6909ee54a0b6e9c68e810
Eric Smith <ewsmith@gmail.com>
2021-04-21 11:13:50 -0700

    [r1cs] Document usage of verify-r1cs.

--- books/kestrel/crypto/r1cs/tools/verify-r1cs.lisp
+++ books/kestrel/crypto/r1cs/tools/verify-r1cs.lisp
@@ -10,24 +10,25 @@
 
 (in-package "R1CS")
 
+(include-book "kestrel/utilities/doc" :dir :system)
 (include-book "kestrel/axe/conjoin-term-with-dag" :dir :system)
 (include-book "kestrel/crypto/r1cs/fe-listp-fast" :dir :system)
 (include-book "kestrel/crypto/r1cs/proof-support" :dir :system) ;for make-bitp-claims
 
 ;; Note that this only does the "forward" direction of the proof
-(defmacro verify-r1cs (lifted-r1cs ; a DAG
-                       spec-term ; a term over the input and output vars, not evaluated
-                       prime     ; the prime for the R1CS
-                       &key
-                       (bit-inputs 'nil) ; bitp assumptions will be generated for these
-                       ;; same as for acl2::prove-implication-with-r1cs-prover:
-                       (tactic ''(:rep :rewrite :subst))
-                       (rule-lists 'nil) ;todo: improve by building some in and allowing :extra-rules and :remove-rules?
-                       (global-rules 'nil) ;; rules to be added to every rule-list
-                       (interpreted-function-alist 'nil)
-                       (no-splitp 't) ; whether to prevent splitting into cases (note that we change the default here)
-                       (monitor 'nil)
-                       (print ':brief))
+(acl2::defmacrodoc verify-r1cs (lifted-r1cs
+                                spec-term
+                                prime
+                                &key
+                                (bit-inputs 'nil)
+                                ;; same as for acl2::prove-implication-with-r1cs-prover:
+                                (tactic ''(:rep :rewrite :subst))
+                                (rule-lists 'nil) ;todo: improve by building some in and allowing :extra-rules and :remove-rules? ;todo: what if we give these but no :rewrite tactic
+                                (global-rules 'nil) ;; rules to be added to every rule-list
+                                (interpreted-function-alist 'nil)
+                                (no-splitp 't) ; whether to prevent splitting into cases (note that we change the default here)
+                                (monitor 'nil)
+                                (print ':brief))
   `(encapsulate ()
      ;; Print the prime more clearly:
      (table acl2::evisc-table ,prime "<p>") ;a bit scary since it makes p look like a var
@@ -50,4 +51,19 @@
       :interpreted-function-alist ,interpreted-function-alist ;todo
       :no-splitp ,no-splitp
       :monitor ,monitor
-      :print ,print)))
+      :print ,print))
+  :parents (r1cs-verification-with-axe)
+  :short "A tool to verify an R1CS"
+  :long "See @(tsee r1cs-verification-with-axe)."
+  :inputs (lifted-r1cs "A DAG representing the lifted R1CS"
+                       spec-term "A term over the input and output vars (this input is not evaluated)"
+                       prime "The prime for the R1CS"
+                       :bit-inputs "Variables for which to generate BITP assumptions"
+                       :tactic "The Axe tactic to use"
+                       :rule-lists "A sequence of Axe rule sets, each of which is a list of rule names and/or calls of 0-ary functions that return lists of rule names.  These are applied one after the other."
+                       :global-rules "Rules to add to every rule-list in the sequence"
+                       :interpreted-function-alist "An interpreted-function-alist to evaluate ground terms" ;todo: document
+                       :no-splitp "Whether to split into cases" ;todo: switch it to :splitp? or :allow-splitting?  why is splitting not a tactic?!
+                       :monitor "Rules to monitor during rewriting"
+                       :print "Axe print argument" ;todo: document
+                       ))
