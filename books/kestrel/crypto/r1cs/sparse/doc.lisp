2b4fd44ad40efeb820837d1262ddbd77ab68107e
Eric Smith <ewsmith@gmail.com>
2021-04-23 22:50:02 -0700

    [r1cs] Tweak.

--- books/kestrel/crypto/r1cs/sparse/doc.lisp
+++ books/kestrel/crypto/r1cs/sparse/doc.lisp
@@ -10,8 +10,6 @@
 
 (in-package "R1CS")
 
-;; (depends-on r1cs.lisp")
-
 (include-book "kestrel/utilities/gen-xdoc-for-file" :dir :system)
 
 ;; Introduced by defaggregate
@@ -19,6 +17,7 @@
   :short "Recognize An R1CS constraint"
   :long "An R1CS constraint is a defaggregate with 3 fields, A, B, and C, each of which is a sparse vector.")
 
+;; (depends-on r1cs.lisp")
 (acl2::gen-xdoc-for-file
  "r1cs.lisp"
  ((pseudo-varp "Recognize a pseudo-variable (a symbol or 1).")
