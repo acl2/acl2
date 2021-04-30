ddc30b89ff23079b69d1953934cf711211316991
Alessandro Coglio <coglio@kestrel.edu>
2021-04-29 23:07:08 -0700

    [ATC] Add an assignment test.
    This produces the correct C code, but the proof takes a long time to run (and
    has not been observed to complete), so this needs to be investigated further.

--- books/kestrel/c/atc/tests/assign.lisp
+++ books/kestrel/c/atc/tests/assign.lisp
@@ -66,6 +66,25 @@
              (|x| (c::bitand-sint-sint |p| |x|)))
         (c::bitxor-sint-sint |x| |q|)))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; The following function is translated to correct C,
+; but the proof seems to take a very long time
+; (it has not even been observed to complete).
+; So no code is generated for this for now;
+; the slowness of its proof must be investigated.
+
+(defun |j| (|x|)
+  (declare (xargs :guard (c::sintp |x|)))
+  (let ((|y| (c::sint-const 0)))
+    (let ((|y| (if (c::boolean-from-sint
+                    (c::lt-sint-sint |x| (c::sint-const 100)))
+                   (let ((|y| (c::bitior-sint-sint |y| (c::sint-const 6666))))
+                     |y|)
+                 (let ((|y| (c::bitxor-sint-sint |y| (c::sint-const 7777))))
+                   |y|))))
+      (c::bitand-sint-sint |x| |y|))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (c::atc |f| |g| |h| |i| :output-file "assign.c")
