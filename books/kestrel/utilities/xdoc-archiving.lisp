23030f6e91c19a348000a29c4a0d946a0ea4f5b4
Eric Smith <ewsmith@gmail.com>
2021-05-04 12:43:37 -0700

    [utilities] Add more xdoc archiving utils.
    These are based on ideas from Sol Swords.

--- books/kestrel/utilities/xdoc-archiving.lisp
+++ books/kestrel/utilities/xdoc-archiving.lisp
@@ -12,6 +12,13 @@
 
 (include-book "xdoc/archive-matching-topics" :dir :system)
 
+;; This book builds on some xdoc archiving ideas and tools developed by Sol Swords.
+
+;; To archive the xdoc stuff from a directory in books/:
+;; Locally include all the xdoc in the directory
+;; Call xdoc::archive-topics-for-books-tree
+;; Call xdoc::archive-current-resource-dirs
+
 ;; Returns a list of the XDOC resource directories registered in WRLD.
 ;;move?
 (defun get-xdoc-resource-dirs (wrld)
@@ -20,6 +27,34 @@
     (if (not (alistp alist))
         (er hard? 'get-xdoc-resource-dirs "Bad alist.")
       (cdr (assoc-eq 'xdoc::resource-dirs alist)))))
+
+;; Count how many xdoc topics are currently defined.
+;move?
+(defun count-documented-topics (wrld)
+  (declare (xargs :guard (plist-worldp wrld)))
+  (let ((alist (table-alist 'xdoc::xdoc wrld)))
+    (if (not (alistp alist))
+        (er hard? 'get-xdoc-resource-dirs "Bad alist.")
+      (len (cdr (assoc-eq 'doc alist))))))
+
+;; Generates a table form that, when the containing book is included, will
+;; extend the resource dirs in the xdoc table with all the resource
+;; dirs present when the book was certified
+(defun archive-current-resource-dirs-fn (wrld)
+  (declare (xargs :guard (plist-worldp wrld)))
+  (let ((current-xdoc-resource-dirs (get-xdoc-resource-dirs wrld)))
+    `(table xdoc::xdoc
+            'xdoc::resource-dirs
+            (union-equal
+             ;; quoted constant computed on first pass of certify-book,
+             ;; when local stuff is included:
+             ',current-xdoc-resource-dirs
+             ;; Table expressions can mention world
+             (get-xdoc-resource-dirs world)))))
+
+;; Extend the list of resource-dirs with
+(defmacro archive-current-resource-dirs ()
+  '(make-event (archive-current-resource-dirs-fn (w state))))
 
 ;; Generate a call to archive-matching-topics that archives all topics in the
 ;; indicated subtree of books/.
