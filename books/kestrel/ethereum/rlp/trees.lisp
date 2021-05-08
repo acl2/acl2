3e7f29e8584b0a5e6c407c73ab12e47ed6e44d8a
Alessandro Coglio <coglio@kestrel.edu>
2021-05-07 18:02:58 -0700

    [Ethereum] Avoid XDOC topic with no actual documentation.

--- books/kestrel/ethereum/rlp/trees.lisp
+++ books/kestrel/ethereum/rlp/trees.lisp
@@ -1,6 +1,6 @@
 ; Ethereum Library
 ;
-; Copyright (C) 2019 Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2021 Kestrel Institute (http://www.kestrel.edu)
 ;
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 ;
@@ -17,6 +17,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (fty::deftypes rlp-trees
+  :parents nil
 
   (fty::deftagsum rlp-tree
     :parents (rlp)
