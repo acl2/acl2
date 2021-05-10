1a60b8b1371983acf9d674846f1b11deb7c07b32
Eric Smith <ewsmith@gmail.com>
2021-05-10 11:54:50 -0700

    [axe] Improve xdoc a bit.

--- books/kestrel/axe/doc.lisp
+++ books/kestrel/axe/doc.lisp
@@ -38,10 +38,10 @@ A DAG contains a set of nodes, each of which has a node number (a natural number
 
 A quoted constant, or
 
-The application of a function symbol (almost always a defined ACL2 function) to a list of arguments.  Each argument (or \"darg\" = \"DAG argument\")) should be either a quoted constant or the number of a DAG node, which must be smaller than the number of the node that contains this expression.  Since the expression for a given node can only refer to nodes with smaller numbers, the DAG is acyclic.")
+The application of a function symbol (almost always a defined ACL2 function) to a list of arguments.  Each argument (or \"darg\" = \"DAG argument\") should be either a quoted constant or the number of a DAG node, which must be smaller than the number of the node that contains this expression.  Since the expression for a given node can only refer to nodes with smaller numbers, the DAG is acyclic.")
 
    (xdoc::topparas
-    "The nodes in a DAG are listed in decreasing order, with each node number consed onto its expression.  Here is an example DAG:
+    "The nodes in a DAG are listed in decreasing order, with each node number consed onto its expression.  Here is an example DAG containing 5 nodes:
 
 @({
 ((4 foo 1 3)
@@ -51,4 +51,8 @@ The application of a function symbol (almost always a defined ACL2 function) to 
  (0 . x))
 }).
 
-The variables in this DAG are @('x') and @('y'), and the functions it calls are @('foo), @('bar'), and @('binary-+').")))
+The variables in this DAG are @('x') and @('y'), and the functions it calls are @('foo), @('bar'), and @('binary-+').  Node 4 represents a call of the function @('foo') whose two arguments are nodes 1 and 3.  Node 3 represents a call of the function @('bar') whose two arguments are the constant 2 and node 2.  The term represented by this DAG is:
+
+@({(foo (binary-* '2 x) (bar '2 y))}).
+
+")))
