b110c2868a0639c461077a82a1dd093d871b1c6e
Eric Smith <ewsmith@gmail.com>
2021-05-13 16:44:10 -0700

    [utilities] Factor hex-string-to-bytes, etc, into new book.

--- books/kestrel/crypto/blake/blake2s-extended-tests.lisp
+++ books/kestrel/crypto/blake/blake2s-extended-tests.lisp
@@ -10,21 +10,12 @@
 
 (in-package "BLAKE")
 
+(include-book "blake2s-extended")
 (include-book "std/testing/assert-equal" :dir :system)
 (include-book "kestrel/utilities/integers-from-to" :dir :system)
-
-(include-book "blake2s-extended")
-(include-book "blake2s-tests") ;todo: reduce, for hex-string-to-bytes
+(include-book "kestrel/utilities/hex-string-to-bytes" :dir :system)
 
 ;; TODO: Add more tests (e.g., one with a key)
-
-;; Suppresses any errors.
-(defund hex-string-to-bytes! (s)
-  (declare (xargs :guard (stringp s)))
-  (mv-let (erp val)
-    (hex-string-to-bytes s)
-    (declare (ignore erp))
-    val))
 
 ;; A test with no salt or personalization
 (acl2::assert-equal (blake2s-extended '(0 1 2)
@@ -32,7 +23,7 @@
                                       nil ;salt
                                       nil ;personalization
                                       32)
-                    (hex-string-to-bytes! "e8f91c6ef232a041452ab0e149070cdd7dd1769e75b3a5921be37876c45c9900"))
+                    (acl2::hex-string-to-bytes! "e8f91c6ef232a041452ab0e149070cdd7dd1769e75b3a5921be37876c45c9900"))
 
 
 ;; A test with a salt
@@ -41,7 +32,7 @@
                                       '(78 97 67 108 56 56 56 56) ;salt
                                       nil ;personalization
                                       32)
-                    (hex-string-to-bytes! "500b8ddc2ab792780fe152de2d99f1e97f39fafb6691ae4126168b48acf91a87"))
+                    (acl2::hex-string-to-bytes! "500b8ddc2ab792780fe152de2d99f1e97f39fafb6691ae4126168b48acf91a87"))
 
 ;;;
 ;;; Tests with personalization:
@@ -52,7 +43,7 @@
                                       nil ;salt
                                       '(90 99 97 115 104 95 80 72) ;personalization "Zcash_PH"
                                       32)
-                    (hex-string-to-bytes! "79e9fa3e5f9eb03e114b97a81b0104c19724726d1f23f9ec1810239195be01ca"))
+                    (acl2::hex-string-to-bytes! "79e9fa3e5f9eb03e114b97a81b0104c19724726d1f23f9ec1810239195be01ca"))
 
 ;; This test is from github.com/zkcrypto/bellman/blob/main/src/gadgets/blake2s.rs
 (acl2::assert-equal (blake2s-extended '()
@@ -60,7 +51,7 @@
                                       nil ;salt
                                       '(49 50 51 52 53 54 55 56) ;personalization "12345678"
                                       32)
-                    (hex-string-to-bytes! "c59f682376d137f3f255e671e207d1f2374ebe504e9314208a52d9f88d69e8c8"))
+                    (acl2::hex-string-to-bytes! "c59f682376d137f3f255e671e207d1f2374ebe504e9314208a52d9f88d69e8c8"))
 
 ;; Test with personalization and 64 bytes of input.
 ;; Result checked against both Python and Rust implementations.
@@ -69,4 +60,4 @@
                                       nil ;salt
                                       '(90 99 97 115 104 95 80 72) ; personalization "Zcash_PH"
                                       32)
-                    (hex-string-to-bytes! "80EAC167076FC215A0AE83EF83F9E04C59F2AE3D14C0C9AFAA9D76A41791D902"))
+                    (acl2::hex-string-to-bytes! "80EAC167076FC215A0AE83EF83F9E04C59F2AE3D14C0C9AFAA9D76A41791D902"))
