88b493d5fecbf7cc5a235b7603d19ab0b0a3e475
Eric Smith <ewsmith@gmail.com>
2021-05-22 10:50:02 -0700

    [file-io-light] Improve read-object-from-file.  Have it close the channel.

--- books/kestrel/file-io-light/read-object-from-file.lisp
+++ books/kestrel/file-io-light/read-object-from-file.lisp
@@ -11,6 +11,12 @@
 (in-package "ACL2")
 
 (local (include-book "open-input-channel"))
+(local (include-book "read-object"))
+
+(local (in-theory (disable UPDATE-OPEN-INPUT-CHANNELS
+                           OPEN-INPUT-CHANNELS
+                           OPEN-INPUT-CHANNEL-ANY-P1
+                           read-object)))
 
 ;; Returns (mv erp object state) where either ERP is non-nil (meaning an error
 ;; occurred) or else OBJECT is the first Lisp object in the file.
@@ -23,11 +29,19 @@
     (if (not channel)
         ;; Error:
         (mv `(:could-not-open-channel ,filename) nil state)
-      (mv-let (eof maybe-object state)
-        (read-object channel state)
-        (if eof
-            ;; Error (no objects in file):
-            (mv `(:end-of-file ,filename) nil state)
-          (mv nil ; no error
-              maybe-object
-              state))))))
+      (if ;; This check is needed for the guard of close-input-channel (can
+          ;; this ever happen?):
+          (member-eq channel
+                     '(acl2-input-channel::standard-object-input-0
+                       acl2-input-channel::standard-character-input-0))
+          ;; Error:
+          (mv `(:bad-channel ,filename) nil state)
+        (mv-let (eof maybe-object state)
+          (read-object channel state)
+          (let ((state (close-input-channel channel state)))
+            (if eof
+                ;; Error (no objects in file):
+                (mv `(:end-of-file ,filename) nil state)
+              (mv nil ; no error
+                  maybe-object
+                  state))))))))
