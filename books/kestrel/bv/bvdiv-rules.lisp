0b998f45346444269d98b7b33c8dae877b38bbe9
Eric Smith <ewsmith@gmail.com>
2021-05-25 15:33:03 -0700

    [bv] Add some bvdiv rules.
    Thanks to Karthik Nukala for examples that led to these and other improvements.

--- books/kestrel/bv/bvdiv-rules.lisp
+++ books/kestrel/bv/bvdiv-rules.lisp
@@ -31,3 +31,41 @@
                       nil
                     (not (bvlt size x y)))))
   :hints (("Goal" :in-theory (enable bvdiv bvlt))))
+
+;gen?
+(defthmd bvdiv-of-bvdiv-arg2
+  (implies (and (natp size)
+                (unsigned-byte-p size y1)
+                (unsigned-byte-p size y2)
+                (unsigned-byte-p size (* y1 y2)))
+           (equal (bvdiv size (bvdiv size x y1) y2)
+                  (bvdiv size
+                         x
+                         (* y1 y2) ;(bvchop size (* y1 y2))
+                         )))
+  :hints (("Goal" :in-theory (e/d (bvdiv) ( ;BVCHOP-IDENTITY
+                                           ;;todo: clean these up:
+                                           BVCHOP-TIMES-CANCEL-BETTER-ALT
+                                           BVCHOP-TIMES-CANCEL-BETTER
+                                           BVCHOP-OF-*-OF-BVCHOP-ARG2
+                                           BVCHOP-TIMES-CANCEL
+                                           BVCHOP-N-TIMES-DROP
+                                           BVCHOP-OF-*-OF-BVCHOP
+                                           )))))
+
+;gen?
+(defthm bvdiv-of-bvdiv-arg2-combine-constants
+  (implies (and (syntaxp (and (quotep size)
+                              (quotep y1)
+                              (quotep y2)))
+                ;; all get computed:
+                (natp size)
+                (unsigned-byte-p size y1)
+                (unsigned-byte-p size y2)
+                (unsigned-byte-p size (* y1 y2)))
+           (equal (bvdiv size (bvdiv size x y1) y2)
+                  (bvdiv size
+                         x
+                         (* y1 y2) ;(bvchop size (* y1 y2))
+                         )))
+  :hints (("Goal" :in-theory (enable bvdiv-of-bvdiv-arg2))))
