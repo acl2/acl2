5e897dd5ecd1a80a9595d3a3a8524372b306e6f0
Alessandro Coglio <coglio@kestrel.edu>
2021-06-03 23:57:15 -0700

    [ATC] Add a modular factorial example/test.
    This demonstrates the generation of C loops.

---
+++ books/kestrel/c/atc/tests/loops.lisp
@@ -0,0 +1,62 @@
+; C Library
+;
+; Copyright (C) 2021 Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2021 Kestrel Technology LLC (http://kestreltechnology.com)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+(include-book "kestrel/c/atc/atc" :dir :system :ttags ((:quicklisp) (:quicklisp.osicat) (:oslib) (:open-output-channel!)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; A modular factorial.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(encapsulate
+  ()
+  (local (include-book "arithmetic-3/top" :dir :system))
+  (defun |f$loop| (|n| |r|)
+    (declare (xargs :guard (and (c::uintp |n|)
+                                (c::uintp |r|))
+                    :measure (c::uint->get |n|)
+                    :hints (("Goal" :in-theory (enable c::ne-uint-uint
+                                                       c::sub-uint-uint
+                                                       c::uint-integer-fix
+                                                       c::uint-mod)))))
+    (if (c::boolean-from-sint (c::ne-uint-uint |n| (c::uint-dec-const 0)))
+        (let* ((|r| (c::mul-uint-uint |r| |n|))
+               (|n| (c::sub-uint-uint |n| (c::uint-dec-const 1))))
+          (|f$loop| |n| |r|))
+      (mv |n| |r|))))
+
+(encapsulate
+  ()
+  (set-ignore-ok t)
+  (defun |f| (|n|)
+    (declare (xargs :guard (c::uintp |n|)))
+    (let ((|r| (c::uint-dec-const 1)))
+      (mv-let (|n| |r|)
+        (|f$loop| |n| |r|)
+        |r|))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(c::atc |f$loop| |f| :output-file "loops.c" :proofs nil)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+#|
+
+On macOS or Linux, you can compile and run this code as follows:
+
+  gcc -o loops loops.c loops-test.c
+  ./f
+
+|#
