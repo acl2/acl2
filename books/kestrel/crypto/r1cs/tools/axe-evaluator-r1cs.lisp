7447e82e7267fc32e7d0a40360aabb6eb4ec2b3f
Eric Smith <ewsmith@gmail.com>
2021-06-09 23:00:09 -0700

    [r1cs] Add functions to evaluator and syntax evaluator.

--- books/kestrel/crypto/r1cs/tools/axe-evaluator-r1cs.lisp
+++ books/kestrel/crypto/r1cs/tools/axe-evaluator-r1cs.lisp
@@ -16,6 +16,7 @@
 
 (include-book "kestrel/axe/evaluator-basic" :dir :system)
 (include-book "kestrel/prime-fields/unguarded-defuns" :dir :system)
+(include-book "kestrel/arithmetic-light/mod-expt-fast-unguarded" :dir :system)
 
 ;; TODO: Consider adding primep (now that we have add-io-pairs), but we'd have to ensure it's only called on known primes.
 ;; TODO: Add more functions!  Add more bv functions.
@@ -33,7 +34,8 @@
             rtl::primep ;; could be dangerous if called on a large prime for which add-io-pairs has not been called
             (acl2::floor acl2::floor-unguarded)
             (acl2::slice acl2::slice-unguarded)
-            bitp)))
+            bitp
+            (acl2::mod-expt-fast acl2::mod-expt-fast-unguarded))))
 
 ;; Make the evaluator:
 (acl2::make-evaluator-simple axe-evaluator-r1cs *axe-evaluator-r1cs-fns-and-aliases*)
