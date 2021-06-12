aef00c13e86e543c8f7dcbb7bfda4d1287bf04bb
Alessandro Coglio <coglio@kestrel.edu>
2021-06-11 18:11:38 -0700

    [ecurve] Make some proofs more modular.

--- books/kestrel/crypto/ecurve/twisted-edwards.lisp
+++ books/kestrel/crypto/ecurve/twisted-edwards.lisp
@@ -1217,19 +1217,20 @@
 (defsection montogomery-zero-identity
   :short "Left and right identity properties of the neutral point."
 
-  (defrulel mod-of-small
-    (implies (and (natp x)
-                  (< x p))
-             (equal (mod x p) x))
-    :prep-books ((include-book "kestrel/arithmetic-light/mod" :dir :system)))
-
   (defrule twisted-edwards-add-of-twisted-edwards-zero-left
     (implies (point-on-twisted-edwards-p point curve)
              (equal (twisted-edwards-add (twisted-edwards-zero) point curve)
                     (point-fix point)))
     :enable (twisted-edwards-add
              twisted-edwards-zero
-             point-on-twisted-edwards-p))
+             point-on-twisted-edwards-p)
+    :prep-lemmas
+    ((defrule mod-of-small
+       (implies (and (natp x)
+                     (< x p))
+                (equal (mod x p) x))
+       :prep-books
+       ((include-book "kestrel/arithmetic-light/mod" :dir :system)))))
 
   (defrule twisted-edwards-add-of-twisted-edwards-zero-right
     (implies (point-on-twisted-edwards-p point curve)
@@ -1276,17 +1277,18 @@
            (twisted-edwards-zero))
     :enable twisted-edwards-zero)
 
-  (defrulel mod-of-small
-    (implies (and (natp x)
-                  (< x p))
-             (equal (mod x p) x))
-    :prep-books ((include-book "kestrel/arithmetic-light/mod" :dir :system)))
-
   (defrule twisted-edwards-neg-of-neg
     (implies (point-on-twisted-edwards-p point curve)
              (equal (twisted-edwards-neg (twisted-edwards-neg point curve) curve)
                     (point-fix point)))
-    :enable point-on-twisted-edwards-p))
+    :enable point-on-twisted-edwards-p
+    :prep-lemmas
+    ((defrule mod-of-small
+       (implies (and (natp x)
+                     (< x p))
+                (equal (mod x p) x))
+       :prep-books
+       ((include-book "kestrel/arithmetic-light/mod" :dir :system))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
