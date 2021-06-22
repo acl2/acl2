80adebf1821f9ba155bfbabf2b1f92c53a13c6f8
Eric Smith <ewsmith@gmail.com>
2021-06-22 11:37:41 -0700

    [bv-lists] Add some rules about bits-to-byte-little.
    Thanks to Karthik Nukala for working an example that led to these.

--- books/kestrel/bv-lists/top.lisp
+++ books/kestrel/bv-lists/top.lisp
@@ -25,6 +25,7 @@
 
 (include-book "bits-to-byte")
 (include-book "bits-to-byte-little")
+(include-book "bits-to-byte-little-rules")
 (include-book "bits-to-bytes")
 (include-book "bits-to-bytes2")
 (include-book "bits-to-bytes-little")
