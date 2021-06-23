9fcb30b188db1bc6407e7a44131cd6e4cad612bb
Pete Manolios <pmanolios@gmail.com>
2021-06-23 14:02:16 -0400

    [ACL2s] Slight cleanup of cert-blow-up.lisp

--- books/acl2s/cert-blow-up.lisp
+++ books/acl2s/cert-blow-up.lisp
@@ -1,10 +1,6 @@
 #|$ACL2s-Preamble$;
 (include-book ;; Newline to fool ACL2/cert.pl dependency scanner
  "portcullis")
-(set-serialize-character-system nil state)
-(set-bad-lisp-consp-memoize nil)
-(set-inhibit-output-lst '(proof-tree event))
-(set-write-acl2x t state)
 (begin-book t :ttags :all);$ACL2s-Preamble$|#
 
 (in-package "ACL2S")
@@ -16,7 +12,7 @@
   :load-compiled-file nil)
 
 ;; If you comment out the following, then the following cert command works
-;; cert.pl ba11
+;; cert.pl cert-blow-up.lisp
 ;; But if you leave it in, then it fails.
 
 (definec pos-ind (n :pos) :pos
