fcc0576d9afef4fa0a5bb9ee7fff670caa266a3f
Eric Smith <ewsmith@gmail.com>
2021-07-02 12:23:57 -0700

    [x86] Add file header.

--- books/kestrel/x86/parsers/parser-utils.lisp
+++ books/kestrel/x86/parsers/parser-utils.lisp
@@ -1,3 +1,14 @@
+; Utilities for parsing x86 binaries
+;
+; Copyright (C) 2016-2019 Kestrel Technology, LLC
+; Copyright (C) 2020-2021 Kestrel Institute
+;
+; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
+;
+; Author: Eric Smith (eric.smith@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (in-package "ACL2")
 
 (include-book "std/util/bstar" :dir :system)
