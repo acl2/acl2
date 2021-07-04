c451300f48188cb91239938ab500f86cd8f8e4f1
Eric Smith <ewsmith@gmail.com>
2021-07-04 12:02:31 -0700

    [x86] Remove stray chars in file headers.

--- books/kestrel/x86/tools/support32.lisp
+++ books/kestrel/x86/tools/support32.lisp
@@ -2,7 +2,7 @@
 ;
 ; Copyright (C) 2016-2019 Kestrel Technology, LLC
 ; Copyright (C) 2020-2021 Kestrel Institute
-;;
+;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
 ;
 ; Author: Eric Smith (eric.smith@kestrel.edu)
