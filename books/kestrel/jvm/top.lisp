0cf913d4cd5689079648c537987885d84d625ca6
Eric Smith <ewsmith@gmail.com>
2021-07-05 08:20:30 -0700

    [jvm] Add top.lisp book for JVM formalization.

---
+++ books/kestrel/jvm/top.lisp
@@ -0,0 +1,15 @@
+; Top level book for JVM model
+;
+; Copyright (C) 2021 Kestrel Institute
+;
+; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
+;
+; Author: Eric Smith (eric.smith@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+;; The order of these roughly reflects the dependencies:
+(include-book "types")
+(include-book "floats")
