4b4b1e3553c74feee9b4ecb6cc7d6ec2cfe625dd
Eric Smith <ewsmith@gmail.com>
2021-07-07 10:07:08 -0700

    [kestrel] Include jvm/top in top, but commented out for now due to rapid changing.

--- books/kestrel/top.lisp
+++ books/kestrel/top.lisp
@@ -38,6 +38,7 @@
 (include-book "fty/top")
 (include-book "isar/top")
 (include-book "java/top")
+;; (include-book "jvm/top")  ;; TODO: Uncomment when stable
 (include-book "json/top")
 ;(include-book "lists-light/top") ; TODO: Name clash on take-opener
 (include-book "number-theory/top")
