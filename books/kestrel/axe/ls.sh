cca0a57473598904a4c7a2e74d70f2a647ee64bb
Eric Smith <ewsmith@gmail.com>
2021-07-11 14:15:37 -0700

    [axe] Add machinery for calling axe helper scripts.

---
+++ books/kestrel/axe/ls.sh
@@ -0,0 +1,13 @@
+# Example script for call-axe-script
+#
+# Copyright (C) 2021 Kestrel Institute
+#
+# License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
+#
+# Author: Eric Smith (eric.smith@kestrel.edu)
+
+################################################################################
+
+# A trivial script that just calls ls, for testing call-axe-script
+
+ls -l ${1}
