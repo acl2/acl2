861c96f06625ffc58d101c00d28abdfc0f1baac3
Alessandro Coglio <coglio@kestrel.edu>
2021-07-20 17:32:35 -0700

    [ATC] Explicate shallow embedding.
    Add an XDOC topic, and a file that collects the constituents of the shallow
    embedding.

--- books/kestrel/c/atc/top.lisp
+++ books/kestrel/c/atc/top.lisp
@@ -1,7 +1,7 @@
 ; C Library
 ;
-; Copyright (C) 2020 Kestrel Institute (http://www.kestrel.edu)
-; Copyright (C) 2020 Kestrel Technology LLC (http://kestreltechnology.com)
+; Copyright (C) 2021 Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2021 Kestrel Technology LLC (http://kestreltechnology.com)
 ;
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 ;
@@ -14,4 +14,5 @@
 (include-book "atc")
 (include-book "doc")
 (include-book "tutorial")
+(include-book "shallow-embedding")
 (include-book "term-recognizers")
