bc1e7c7710e236fbb88544a686cb1e2e42efad45
Alessandro Coglio <coglio@kestrel.edu>
2021-07-22 00:27:46 -0700

    [Yul] List differences between the two grammars.

--- books/kestrel/yul/concrete-syntax.lisp
+++ books/kestrel/yul/concrete-syntax.lisp
@@ -38,5 +38,39 @@
      we initially formalized and parsed the old grammar,
      after which we were told that the other grammar is new;
      but since we have the old one formalized and parsed already,
-     we like to keep it for now, along with the new one."))
+     we like to keep it for now, along with the new one.")
+   (xdoc::p
+    "The old and new grammar have the following differences:")
+   (xdoc::ul
+    (xdoc::li
+     "The old grammar allows dots in identifiers,
+      while the new grammar does not.
+      However, the new grammar introduces a notion of path,
+      which is a sequence of one or more dot-separated identifiers;
+      paths can be used as expressions and can be assigned to.")
+    (xdoc::li
+     "The old grammar includes type names, defined as identifiers,
+      while the new grammar does not have that notion.
+      In particular, no optional types are allowed by the new grammar
+      for literals, declared variables, and function inputs and outputs.")
+    (xdoc::li
+     "The old grammar allows any expression as statement,
+      while the new grammar allows only function calls.")
+    (xdoc::li
+     "The old grammar allows any expression
+      to initialize multiple variables or to assign to multiple variables,
+      while the new grammar allows only function calls.")
+    (xdoc::li
+     "The old grammar allows leading zeros in decimal numbers,
+      while the new grammar disallows them.")
+    (xdoc::li
+     "The old and new grammar have
+      somewhat different definitions of string literals.
+      In particular, the new grammar clarifies the underlying character set,
+      which was implicit in the old grammar due to the use of
+      a complement and a wildcard.
+      The old grammar only allows surrounding double quotes,
+      while the new grammar also allows surrounding single quotes.")
+    (xdoc::li
+     "The new grammar adds hex strings to the possible literals.")))
   :order-subtopics t)
