e8479267fe036a2b610642158fd7036947b18af6
Alessandro Coglio <coglio@kestrel.edu>
2021-07-21 18:42:58 -0700

    [ATC] Refactor some ATC code.
    Move the code for the ATC table to a separate file.

--- books/kestrel/c/atc/atc.lisp
+++ books/kestrel/c/atc/atc.lisp
@@ -20,6 +20,7 @@
 (include-book "let-designations")
 (include-book "proof-support")
 (include-book "input-processing")
+(include-book "table")
 
 (include-book "kestrel/event-macros/applicability-conditions" :dir :system)
 (include-book "kestrel/event-macros/cw-event" :dir :system)
@@ -34,9 +35,7 @@
 (include-book "kestrel/std/system/check-mv-let-call" :dir :system)
 (include-book "kestrel/std/system/formals-plus" :dir :system)
 (include-book "kestrel/std/system/fresh-logical-name-with-dollars-suffix" :dir :system)
-(include-book "kestrel/std/system/maybe-pseudo-event-formp" :dir :system)
 (include-book "kestrel/std/system/measure-plus" :dir :system)
-(include-book "kestrel/std/system/table-alist-plus" :dir :system)
 (include-book "kestrel/std/system/ubody-plus" :dir :system)
 (include-book "kestrel/std/system/uguard-plus" :dir :system)
 (include-book "kestrel/std/system/well-founded-relation-plus" :dir :system)
@@ -123,80 +122,6 @@
    any target function is recursive or not."
 
   xdoc::*evmac-topic-implementation-item-names-to-avoid*))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defxdoc+ atc-table
-  :parents (atc-implementation)
-  :short "Table of @(tsee atc) calls."
-  :long
-  (xdoc::topstring
-   (xdoc::p
-    "Every successful call of @(tsee atc) is recorded in a table.
-     This is used to support redundancy checking (see @(tsee atc-fn))."))
-  :order-subtopics t
-  :default-parent t)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defval *atc-table*
-  :short "Name of the table of @(tsee atc) calls."
-  'atc-table)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(std::defaggregate atc-call-info
-  :short "Information associated to an @(tsee atc) call
-          in the table of @(tsee atc) calls."
-  :long
-  (xdoc::topstring
-   (xdoc::p
-    "For now we only record the generated encapsulate.
-     More information may be recorded in the future."))
-  ((encapsulate pseudo-event-formp))
-  :pred atc-call-infop)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define atc-maybe-call-infop (x)
-  :short "Optional information associated to an @(tsee atc) call
-          in the table of @(tsee atc) calls."
-  (or (atc-call-infop x)
-      (eq x nil)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defsection atc-table-definition
-  :short "Definition of the table of @(tsee atc) calls."
-  :long
-  (xdoc::topstring
-   (xdoc::p
-    "The keys of the table are calls of @(tsee atc).
-     The values of the table are the associated information."))
-
-  (make-event
-   `(table ,*atc-table* nil nil
-      :guard (and (pseudo-event-formp acl2::key)
-                  (atc-call-infop acl2::val)))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define atc-table-lookup ((call pseudo-event-formp) (wrld plist-worldp))
-  :returns (info? atc-maybe-call-infop)
-  :short "Look up an @(tsee atc) call in the table."
-  (b* ((table (acl2::table-alist+ *atc-table* wrld))
-       (info? (cdr (assoc-equal call table))))
-    (if (atc-maybe-call-infop info?)
-        info?
-      (raise "Internal error: value ~x0 of key ~x1 in the ATC table.")))
-  :prepwork ((local (include-book "std/alists/top" :dir :system))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define atc-table-record-event ((call pseudo-event-formp) (info atc-call-infop))
-  :returns (event pseudo-event-formp)
-  :short "Event to update the table of @(tsee atc) calls."
-  `(table ,*atc-table* ',call ',info))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
