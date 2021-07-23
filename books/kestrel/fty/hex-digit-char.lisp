b2e577037332242f627689e031d2b10200b8984b
Alessandro Coglio <coglio@kestrel.edu>
2021-07-22 21:37:57 -0700

    [FTY] Add fixtype for digit characters.
    These are for decimal, hexadecimal, octal, and binary from Std/strings.

---
+++ books/kestrel/fty/hex-digit-char.lisp
@@ -0,0 +1,36 @@
+; FTY Library
+;
+; Copyright (C) 2021 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "STR")
+
+(include-book "kestrel/std/util/deffixer" :dir :system)
+(include-book "std/strings/hex" :dir :system)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defsection hex-digit-char
+  :short "Fixtype of hexadecimal digit characters."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "This is a type introduced by @(tsee fty::deffixtype)."))
+
+  (std::deffixer hex-digit-char-fix
+    :pred hex-digit-char-p
+    :body-fix #\0
+    :parents (hex-digit-char)
+    :short "Fixer for @(tsee hex-digit-char).")
+
+  (fty::deffixtype hex-digit-char
+    :pred hex-digit-char-p
+    :fix hex-digit-char-fix
+    :equiv hex-digit-char-equiv
+    :define t
+    :forward t))
