bfc3a77111f4253ff8012d7e8dbda3452e95786e
Eric Smith <ewsmith@gmail.com>
2021-07-31 19:19:57 -0700

    [axe] Include x86/top in top.

--- books/kestrel/axe/top.lisp
+++ books/kestrel/axe/top.lisp
@@ -219,8 +219,6 @@
 (include-book "stp-clause-processor")
 (include-book "defthm-stp")
 
-(include-book "jvm/top")
-
 ;; Legacy Axe tools:
 (include-book "evaluator")
 (include-book "instantiate-hyp")
@@ -230,3 +228,7 @@
 (include-book "rewriter")
 (include-book "prune")
 (include-book "tactic-prover")
+
+(include-book "jvm/top")
+
+(include-book "x86/top")
