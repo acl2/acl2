ba3acb89f993fac8b40e3afe91a9282a2b923a2d
Eric Smith <ewsmith@gmail.com>
2021-08-02 15:13:26 -0700

    [r1cs] Improve description of file.

--- books/kestrel/crypto/r1cs/tools/axe-evaluator-r1cs.lisp
+++ books/kestrel/crypto/r1cs/tools/axe-evaluator-r1cs.lisp
@@ -1,7 +1,7 @@
 ; An evaluator supporting R1CS Axe reasoning
 ;
 ; Copyright (C) 2008-2011 Eric Smith and Stanford University
-; Copyright (C) 2013-2020 Kestrel Institute
+; Copyright (C) 2013-2021 Kestrel Institute
 ; Copyright (C) 2016-2020 Kestrel Technology, LLC
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
@@ -12,7 +12,7 @@
 
 (in-package "PFIELD")
 
-;; A simple version of the Axe evaluator with verified guards and without skip-proofs.
+;; An Axe evaluator for R1CS proofs (really, for proofs about prime-fields).
 
 (include-book "kestrel/axe/evaluator-basic" :dir :system)
 (include-book "kestrel/prime-fields/unguarded-defuns" :dir :system)
