8310a9a495c405290defb489ca321b7d4e3d0aa1
Alessandro Coglio <coglio@kestrel.edu>
2021-08-12 19:50:43 -0700

    [ATC] Refactor some code.
    Avoid local book inclusions in repeatedly generated events.
    
    Also re-generate useless runes file, which was otherwise causing a failure with
    the changes to the .lisp files made in this commit.

--- books/kestrel/c/atc/integers.lisp
+++ books/kestrel/c/atc/integers.lisp
@@ -19,6 +19,12 @@
 
 (local (include-book "arithmetic-3/top" :dir :system))
 
+;; to have FTY::DEFLIST generate theorems about NTH:
+(local (include-book "std/lists/nth" :dir :system))
+
+;; to have FTY::DEFLIST generate theorems about UPDATE-NTH:
+(local (include-book "std/lists/update-nth" :dir :system))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defxdoc+ atc-integers
