7a220e9f15ecb28ea27b8bca78e5522d6b876095
Eric Smith <ewsmith@gmail.com>
2021-08-12 19:19:34 -0700

    [r1cs] Clean up range check gadget a bit.

--- books/kestrel/crypto/r1cs/sparse/gadgets/range-check.lisp
+++ books/kestrel/crypto/r1cs/sparse/gadgets/range-check.lisp
@@ -12,6 +12,8 @@
 
 ;; STATUS: DRAFT
 
+;; This is based on "A.3.2.2 Range check" in the Zcash Protocol Specification.
+
 (include-book "boolean")
 (include-book "kestrel/crypto/r1cs/sparse/r1cs" :dir :system)
 (include-book "kestrel/bv/bvchop" :dir :system)
@@ -24,7 +26,18 @@
 (local (include-book "kestrel/arithmetic-light/mod" :dir :system))
 (local (include-book "kestrel/prime-fields/prime-fields-rules" :dir :system))
 
-(in-theory (disable RTL::PRIMEP)) ;todo
+(in-theory (disable rtl::primep)) ;todo
+
+(local
+ (defthm member-equal-of-cdr-of-assoc-equal-when-subsetp-equal-of-strip-cdrs
+   (implies (and (subsetp-equal (strip-cdrs alist) vals)
+                 (assoc-equal key alist))
+            (member-equal (cdr (assoc-equal key alist)) vals))))
+
+(local
+ (defthm symbolp-of-cdr-of-assoc-equal-when-symbol-listp-of-strip-cdrs
+   (implies (symbol-listp (strip-cdrs alist))
+            (symbolp (cdr (assoc-equal key alist))))))
 
 ;; Make a constraint asserting that a * b = c
 (defund make-product-constraint (a b c)
@@ -51,10 +64,11 @@
                                      dot-product
                                      integerp-of-lookup-equal))))
 
+;; Make a constraint asserting that var1 = var2.
 (defund make-equality-constraint (var1 var2)
   (declare (xargs :guard (and (symbolp var1)
                               (symbolp var2))))
-  ;; 1 * var1 = var2
+  ;; (1*1) * (1*var1) = (1*var2):
   (r1cs-constraint (list `(1 1))
                    (list `(1 ,var1))
                    (list `(1 ,var2))))
@@ -72,65 +86,61 @@
                                      dot-product
                                      integerp-of-lookup-equal))))
 
-(local
- (defthm member-equal-of-cdr-of-assoc-equal-when-subsetp-equal-of-strip-cdrs
-   (implies (and (subsetp-equal (strip-cdrs alist) vals)
-                 (assoc-equal key alist))
-            (member-equal (cdr (assoc-equal key alist)) vals))))
-
-(local
- (defthm symbolp-of-cdr-of-assoc-equal-when-symbol-listp-of-strip-cdrs
-   (implies (symbol-listp (strip-cdrs alist))
-            (symbolp (cdr (assoc-equal key alist))))))
-
-; Returns (mv constraints pi-var-renaming)
-;; The generated constraints assert that all of the a-vars, from bit i down to bit t-var, are at least as big as the corresponding bits of c.
+;; Returns (mv constraints pi-var-renaming), where the constraints added to
+;; CONSTRAINTS-ACC assert that all of the (bit) values of the avars, from a_i
+;; down through a_tvar, are at least as big as the corresponding bits of c.
 (defund make-pi-constraints (i ; index of the next bit to check, counts down
-                            t-var ; lowest index to check (can't just call this "t", hence "t-var")
-                            a-vars  ; a_0 through a_(n-1)
-                            pi-vars ; pi_0 through pi_(n-1)
-                            c ; the constant to which are are comparing the packed A-VARS
-                            acc
-                            pi-var-renaming ; some vars are known to simply be equal to higher-numbered vars, the keys may include any vars about pi_i
-                            )
-  (declare (xargs :guard (and (symbol-listp a-vars)
-                              (symbol-listp pi-vars)
-                              (equal (len a-vars)
-                                     (len pi-vars))
-                              (integerp i)
-                              (natp t-var)
-                              ;; (<= (+ -1 t-var) i)
-                              (< i (+ -1 (len a-vars))) ; subtracting 1 ensures that pi_i+1 is among the pi-vars
+                             tvar ; lowest index to check (can't just call this "t", hence "tvar")
+                             avars  ; a_0 through a_(n-1)
+                             pivars ; pi_0 through pi_(n-1)
+                             c ; the constant to which we are comparing the (packed) AVARS
+                             constraints-acc
+                             pi-var-renaming ; maps pivars to equivalent, higher-numbered pivars (the keys may include any vars above pi_i)
+                             )
+  (declare (xargs :guard (and (integerp i)
+                              (natp tvar)
+                              (symbol-listp avars)
+                              (symbol-listp pivars)
+                              (equal (len avars)
+                                     (len pivars))
+                              ;; (<= (+ -1 tvar) i)
+                              (< (+ 1 i) (len pivars)) ; ensures that pi_i+1 is among the pivars
                               (natp c)
                               (symbol-alistp pi-var-renaming)
-                              (subsetp-equal (strip-cdrs pi-var-renaming) pi-vars))
+                              (subsetp-equal (strip-cdrs pi-var-renaming) pivars))
                   :measure (nfix (+ 1 (- i t)))))
-  (if (or (not (mbt (natp t-var)))
+  (if (or (not (mbt (natp tvar)))
           (not (mbt (integerp i)))
-          (< i t-var))
-      (mv acc pi-var-renaming)
+          (< i tvar))
+      (mv constraints-acc pi-var-renaming)
     (let* ((c_i (getbit i c))
-           (pi_i (nth i pi-vars))
-           (pi_i+1 (nth (+ 1 i) pi-vars))
+           (pi_i (nth i pivars))
+           (pi_i+1 (nth (+ 1 i) pivars))
            ;; possibly rename pi_i+1 to some higher var:
            (pi_i+1 (let ((res (assoc-eq pi_i+1 pi-var-renaming)))
                      (if res
                          (cdr res)
                        pi_i+1))))
       (if (equal c_i 0)
-          (make-pi-constraints (+ -1 i)
-                               t-var
-                               a-vars pi-vars c
-                               acc
-                               (acons pi_i pi_i+1 pi-var-renaming) ; records the fact that pi_i is whatever var pi_i+1 truly is
+          (make-pi-constraints (+ -1 i) tvar avars pivars c
+                               ;; no actual constraint added:
+                               constraints-acc
+                               ;; records the fact that pi_i is just pi_i+1
+                               ;; (but pi_i+1 may have been equated to some
+                               ;; higher numbered pi var -- see above):
+                               (acons pi_i pi_i+1 pi-var-renaming)
                                )
-        (let ((a_i (nth i a-vars)))
-          (make-pi-constraints (+ -1 i)
-                               t-var
-                               a-vars pi-vars c
-                               (cons (make-product-constraint pi_i+1 a_i pi_i) acc)
-                               pi-var-renaming ; records the fact that pi_i is whatever var pi_i+1 truly is
-                               ))))))
+        (let ((a_i (nth i avars)))
+          (make-pi-constraints (+ -1 i) tvar avars pivars c
+                               ;; Add the constraint that pi_i+1 * a_i = pi_i
+                               ;; That is, pi_i ("all the a's down through a_i
+                               ;; are >= the corresponding c's") iff both p_i+1
+                               ;; ("all the a's down through a_i+1 are >= the
+                               ;; corresponding c's") AND a_i is >= c_i (note
+                               ;; that c_i here is known to be 1, so a_i must
+                               ;; also be 1 if it is to be >= c_i).
+                               (cons (make-product-constraint pi_i+1 a_i pi_i) constraints-acc)
+                               pi-var-renaming))))))
 
 ;; (make-pi-constraints 12 ; bit 13 is handled separately
 ;;                      4
@@ -142,19 +152,19 @@
 
 (defthm symbol-alistp-of-mv-nth-1-of-make-pi-constraints
   (implies (and (symbol-alistp pi-var-renaming)
-                (symbol-listp pi-vars))
-           (symbol-alistp (mv-nth 1 (make-pi-constraints i t-var a-vars pi-vars c acc pi-var-renaming))))
+                (symbol-listp pivars))
+           (symbol-alistp (mv-nth 1 (make-pi-constraints i tvar avars pivars c constraints-acc pi-var-renaming))))
   :hints (("Goal" :in-theory (enable make-pi-constraints))))
 
 (defthm subsetp-equal-of-strip-cdrs-of-mv-nth-1-of-make-pi-constraints
   (implies (and (symbol-alistp pi-var-renaming)
-                (symbol-listp pi-vars)
-                (subsetp-equal (strip-cdrs pi-var-renaming) pi-vars)
-                (< i (+ -1 (len a-vars)))
-                (equal (len a-vars)
-                       (len pi-vars)))
-           (subsetp-equal (strip-cdrs (mv-nth 1 (make-pi-constraints i t-var a-vars pi-vars c acc pi-var-renaming)))
-                          pi-vars))
+                (symbol-listp pivars)
+                (subsetp-equal (strip-cdrs pi-var-renaming) pivars)
+                (< i (+ -1 (len avars)))
+                (equal (len avars)
+                       (len pivars)))
+           (subsetp-equal (strip-cdrs (mv-nth 1 (make-pi-constraints i tvar avars pivars c constraints-acc pi-var-renaming)))
+                          pivars))
   :hints (("Goal" :in-theory (enable make-pi-constraints))))
 
 
@@ -170,8 +180,8 @@
 ;;                 (rtl::primep p)
 ;;                 (< 2 p))
 ;;            (equal (r1cs-constraints-holdp (make-pi-constraints i
-;;                                                                a-vars
-;;                                                                pi-vars
+;;                                                                avars
+;;                                                                pivars
 ;;                                                                c
 ;;                                                                pi-var-renaming)
 ;;                                           valuation p)
@@ -244,30 +254,30 @@
                                      pfield::mul-of--1-becomes-neg))))
 
 (defund make-range-check-a-constraints (i
-                                        a-vars
-                                        pi-vars
+                                        avars
+                                        pivars
                                         c
                                         acc
                                         pi-var-renaming)
   (declare (xargs :guard (and (integerp i)
-                              (symbol-listp a-vars)
-                              (symbol-listp pi-vars)
+                              (symbol-listp avars)
+                              (symbol-listp pivars)
                               (natp c)
                               (symbol-alistp pi-var-renaming)
-                              (subsetp-equal (strip-cdrs pi-var-renaming) pi-vars))
+                              (subsetp-equal (strip-cdrs pi-var-renaming) pivars))
                   :measure (nfix (+ 1 i))))
   (if (not (natp i))
       acc
     (let* ((c_i (getbit i c))
-           (a_i (nth i a-vars))
-           (pi_i+1 (nth (+ 1 i) pi-vars))
+           (a_i (nth i avars))
+           (pi_i+1 (nth (+ 1 i) pivars))
            (pi_i+1 (let ((res (assoc-eq pi_i+1 pi-var-renaming)))
                      (if res
                          (cdr res)
                        pi_i+1))))
       (make-range-check-a-constraints (+ -1 i)
-                                      a-vars
-                                      pi-vars
+                                      avars
+                                      pivars
                                       c
                                       (cons
                                        (if (equal c_i 0)
@@ -277,47 +287,46 @@
                                       pi-var-renaming))))
 
 (defthm true-listp-of-make-range-check-a-constraints
-  (equal (true-listp (make-range-check-a-constraints i a-vars pi-vars c acc pi-var-renaming))
+  (equal (true-listp (make-range-check-a-constraints i avars pivars c acc pi-var-renaming))
          (true-listp acc))
   :hints (("Goal" :in-theory (enable make-range-check-a-constraints))))
 
 
-;; Make constraints checking that the packed A-VARS are <= the N-bit constant C
-;; and also checking that the A-VARS are bits.  The PI-VARS are used for helper
+;; Make constraints checking that the packed AVARS are <= the N-bit constant C
+;; and also checking that the AVARS are bits.  The PIVARS are used for helper
 ;; constraints.
-;; See "A.3.2.2 Range check" in the Zcash Protocol Specification.
-(defund make-range-check-constraints (a-vars ; a_0 through a_(n-1)
-                                     pi-vars ; pi_0 through pi_(n-1), possibly not all used
-                                     c ; the constant to which the packed A-VARS will be compared
+(defund make-range-check-constraints (avars ; a_0 through a_(n-1)
+                                     pivars ; pi_0 through pi_(n-1), possibly not all used
+                                     c ; the constant to which the packed AVARS will be compared
                                      n ; the number of bits in c
                                      )
-  (declare (xargs :guard (and (symbol-listp a-vars)
-                              (symbol-listp pi-vars)
-                              (no-duplicatesp-eq (append a-vars pi-vars))
-                              (equal n (len a-vars))
-                              (equal n (len pi-vars))
+  (declare (xargs :guard (and (symbol-listp avars)
+                              (symbol-listp pivars)
+                              (no-duplicatesp-eq (append avars pivars))
+                              (equal n (len avars))
+                              (equal n (len pivars))
                               (posp n)
-                              (unsigned-byte-p (len a-vars) c)
+                              (unsigned-byte-p (len avars) c)
                               (equal 1 (getbit (+ -1 n) c)) ;; leading 1
                               ;; (posp c) ; if c=0 there would be no leading 1
                               )))
   (b* (;; reduce n if necessary so that c_(n-1) is the leading 1:
        ;;(n (+ 1 position-of-leading-1))
-       (pi_n-1 (nth (+ -1 n) pi-vars))
-       (a_n-1 (nth (+ -1 n) a-vars))
-       (t-var (index-of-lowest-0 0 c))
+       (pi_n-1 (nth (+ -1 n) pivars))
+       (a_n-1 (nth (+ -1 n) avars))
+       (tvar (index-of-lowest-0 0 c))
        ((mv pi-constraints pi-var-renaming)
         (make-pi-constraints (+ -2 n)
-                             t-var
-                             a-vars
-                             pi-vars
+                             tvar
+                             avars
+                             pivars
                              c
                              (list (make-equality-constraint pi_n-1 a_n-1) ; constraint for position n-1
                                    )
                              nil))
        (a-constraints (make-range-check-a-constraints (+ -1 n)
-                                          a-vars
-                                          pi-vars
+                                          avars
+                                          pivars
                                           c
                                           nil
                                           pi-var-renaming)))
@@ -373,25 +382,25 @@
                  (< 1000000 prime)
                  (rtl::primep prime)
                  (r1cs-valuationp valuation prime)
-                 (equal n (len a-vars))
-                 (equal n (len pi-vars))
-                 (valuation-binds-allp valuation (append a-vars pi-vars))
-                 (no-duplicatesp-eq (append a-vars pi-vars))
+                 (equal n (len avars))
+                 (equal n (len pivars))
+                 (valuation-binds-allp valuation (append avars pivars))
+                 (no-duplicatesp-eq (append avars pivars))
                  (equal 1 (getbit (+ -1 n) c)) ; constant must have a leading 1
                  )
             ;; todo: prove other direction!
             (implies (r1cs-constraints-holdp (make-range-check-constraints
-                                              a-vars
-                                              pi-vars
+                                              avars
+                                              pivars
                                               c
                                               n
                                               )
                                              valuation
                                              prime)
-                     (and (ACL2::ALL-UNSIGNED-BYTE-P 1 (acl2::lookup-eq-lst a-vars valuation))
+                     (and (ACL2::ALL-UNSIGNED-BYTE-P 1 (acl2::lookup-eq-lst avars valuation))
                           (<= (acl2::packbv n ;14
                                             1
-                                            (acl2::lookup-eq-lst (acl2::reverse-list a-vars) valuation))
+                                            (acl2::lookup-eq-lst (acl2::reverse-list avars) valuation))
                               c)))))
  :hints (("Goal" :in-theory (e/d (ACL2::LOOKUP-EQ-LST
                                   ;; R1CS-CONSTRAINT-HOLDSP
