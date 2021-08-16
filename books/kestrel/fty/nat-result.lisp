90a9dbc0affeca5d3e55972607e5321545ae7387
Alessandro Coglio <coglio@kestrel.edu>
2021-08-16 16:20:42 -0700

    [FTY] Add DEFRESULT instance for natural numbers.

---
+++ books/kestrel/fty/nat-result.lisp
@@ -0,0 +1,20 @@
+; FTY Library
+;
+; Copyright (C) 2021 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+(include-book "defresult")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(fty::defresult nat-result
+  :short "Fixtype of natural number and error results."
+  :ok nat
+  :pred nat-resultp)
