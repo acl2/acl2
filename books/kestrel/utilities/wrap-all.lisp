42719621e8cf33b227149197f154ad14e10028a9
Eric Smith <ewsmith@gmail.com>
2021-08-18 23:34:26 -0700

    [utilities] Update/improve comments.

--- books/kestrel/utilities/wrap-all.lisp
+++ books/kestrel/utilities/wrap-all.lisp
@@ -1,7 +1,7 @@
 ; A utility to apply a unary wrapper to all items in a list
 ;
 ; Copyright (C) 2008-2011 Eric Smith and Stanford University
-; Copyright (C) 2013-2020 Kestrel Institute
+; Copyright (C) 2013-2021 Kestrel Institute
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
 ;
@@ -11,9 +11,8 @@
 
 (in-package "ACL2")
 
-;; STATUS: IN-PROGRESS
-
-;; Turn each of the ITEMS into a unary call of FN on that item.
+;; Turn each of the ITEMS into a unary call of FN on that item.  FN is usually
+;; a symbol but may be a lambdas.
 (defun wrap-all (fn items)
   (declare (xargs :guard (true-listp items)))
   (if (endp items)
