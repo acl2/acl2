2408edc5fb11d436c96fbdd54b421873e1ad1229
Alessandro Coglio <coglio@kestrel.edu>
2021-08-22 16:44:48 -0700

    [FTY] Add a fixtype for natural numbers and lists thereof.

--- books/kestrel/fty/top.lisp
+++ books/kestrel/fty/top.lisp
@@ -45,6 +45,7 @@
 (include-book "maybe-string")
 (include-book "maybe-string-result")
 (include-book "nat-list-result")
+(include-book "nat-natlist")
 (include-book "nat-result")
 (include-book "nat-set")
 (include-book "nati")
