64bf29e764c351b83fa587cb815cdb7d7abc9d8f
Eric Smith <ewsmith@gmail.com>
2021-08-26 17:20:38 -0700

    [utilities] Improve add-conjunct-to-uterm.
    Avoid conjoining when the conjunct is already clearly present as a conjunct or is equal to the entire uterm.

--- books/kestrel/untranslated-terms/add-conjunct-to-uterm.lisp
+++ books/kestrel/untranslated-terms/add-conjunct-to-uterm.lisp
@@ -13,23 +13,33 @@
 
 (include-book "kestrel/utilities/forms" :dir :system)
 
-;; Conjoin CONJUNCT to ITEM, adding it as the last conjunct (in case its guard
-;; requires ITEM to be true), unless it is already clearly present as a
-;; conjunct.  CONJUNCT and ITEM are untranslated terms.
-;; TODO: Handle the case of CONJUNCT equal to or already present in ITEM.
-(defund add-conjunct-to-uterm (conjunct item)
+;; Conjoin CONJUNCT to UTERM, adding it as the last conjunct (in case its guard
+;; requires UTERM to be true), unless it is already clearly present as a
+;; conjunct.  CONJUNCT and UTERM are untranslated terms.
+;; TODO: Consider recognizing an IF nest that represents an AND and treating it
+;; like we treat a call of AND below.
+(defund add-conjunct-to-uterm (conjunct uterm)
   (declare (xargs :guard t))
-  (if (or (equal conjunct *t*)
-          (eq conjunct 't))
-      ;; Special case (conjoining t has no effect):
-      item
-    (if (or (equal item *t*)
-            (eq item 't))
-        ;; Special case (item as just "true"):
-        conjunct
-      (if (and (call-of 'and item)
-               (true-listp item) ;for the guard proof
-               )
-          ;; Special case (avoid creating a nested AND):
-          `(and ,@(fargs item) ,conjunct)
-        `(and ,item ,conjunct)))))
+  (if (equal conjunct uterm)
+      ;; Special case (they are the same, so just keep one):
+      uterm
+    (if (or (equal conjunct *t*)
+            (eq conjunct 't))
+        ;; Special case (conjoining t has no effect):
+        uterm
+      (if (or (equal uterm *t*)
+              (eq uterm 't))
+          ;; Special case (uterm is just "true"):
+          conjunct
+        (if (and (call-of 'and uterm)
+                 (true-listp uterm) ;for the guard proof
+                 )
+            ;; uterm is already an AND:
+            (let ((existing-conjuncts (fargs uterm)))
+              (if (member-equal conjunct existing-conjuncts)
+                  ;; Special case (conjunct already present):
+                  uterm
+                ;; Special case (avoid creating a nested AND):
+                `(and ,@existing-conjuncts ,conjunct)))
+          ;; Normal case:
+          `(and ,uterm ,conjunct))))))
