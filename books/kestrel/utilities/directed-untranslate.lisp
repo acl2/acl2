ec936502f7f8c88f3d82076b023c9eeae85dd9c9
Matt Kaufmann <matthew.j.kaufmann@gmail.com>
2021-09-10 19:03:07 -0700

    Restored MBT handing in directed-untranslate (to support at least one APT transformation mentioned by Stephen Westfold)

--- books/kestrel/utilities/directed-untranslate.lisp
+++ books/kestrel/utilities/directed-untranslate.lisp
@@ -616,6 +616,32 @@
          (('if x *nil* *t*)
           (mv t (fcons-term* 'null x)))
          (& (mv nil sterm))))
+      (('return-last ''mbe1-raw *t* x) ; ('mbt x)
+
+; This COND branch was originally intended to assist the apt::simplify
+; transformation in its handling of MBT.  That task is best handled instead by
+; that transformation, which is doing so as of this writing.  However, other
+; transformations apparently depend on this as well (as reported by Stephen
+; Westfold), so this branch remains for now.
+
+       (cond ((not (ffn-symb-p sterm 'return-last))
+              (cond ((equal sterm x)
+                     (mv t tterm))
+                    ((if-tautologyp `(iff ,sterm ,x))
+                     (mv t
+                         (fcons-term* 'return-last
+                                      ''mbe1-raw
+                                      *t*
+                                      sterm)))
+                    ((if-tautologyp `(iff ,(dumb-negate-lit sterm) ,x))
+                     (mv t
+                         (fcons-term* 'not
+                                      (fcons-term* 'return-last
+                                                   ''mbe1-raw
+                                                   *t*
+                                                   (dumb-negate-lit sterm)))))
+                    (t (mv nil sterm))))
+             (t (mv nil sterm))))
       (& (mv nil sterm)))
     (or
      (case-match tterm
