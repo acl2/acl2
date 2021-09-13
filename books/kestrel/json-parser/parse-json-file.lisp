1bda8c87f107c62faa7d397d015842563351d2b9
Eric Smith <ewsmith@gmail.com>
2021-09-13 16:45:22 -0700

    [json-parser] Refactor to allow easy access to chars read from file.
    Thanks to Eric McCarthy for a request that led to this and other recent improvements.

--- books/kestrel/json-parser/parse-json-file.lisp
+++ books/kestrel/json-parser/parse-json-file.lisp
@@ -14,26 +14,34 @@
 (include-book "kestrel/utilities/file-existsp" :dir :system)
 (include-book "kestrel/file-io-light/read-file-into-character-list" :dir :system)
 
-;; Returns (mv erp parsed-value state).
-;; Example call: (parse-file-as-json "example.json" state)
-(defund parse-file-as-json (filename state)
+;; Returns (mv erp parsed-value chars-from-file state).
+(defund parse-file-as-json-aux (filename state)
   (declare (xargs :stobjs state
                   :guard (stringp filename)))
   (b* (((mv existsp state)
         (file-existsp filename state))
        ((when (not existsp))
         (progn$ (er hard? 'parse-file-as-json "JSON file does not exist: ~x0." filename)
-                (mv `(:file-does-not-exist ,filename) nil state)))
+                (mv `(:file-does-not-exist ,filename) nil nil state)))
        (chars ; note that state is not returned!
         (read-file-into-character-list filename state))
        ((when (not (consp chars)))
         (prog2$ (er hard? 'parse-file-as-json "Failed to read any character from file: ~x0." filename)
-                (mv `(:failed-to-read-from-file ,filename) nil state)))
+                (mv `(:failed-to-read-from-file ,filename) nil nil state)))
        ;; Parse the characters read:
        ((mv erp parsed-json)
         (parse-json chars))
        ((when erp)
         (prog2$ (er hard? 'parse-file-as-json "ERROR (~x0) parsing JSON in ~x1" erp filename)
-                (mv `(:error-parsing-json ,filename) nil state))))
+                (mv `(:error-parsing-json ,filename) nil chars state))))
     (mv nil ; no error
-        parsed-json state)))
+        parsed-json chars state)))
+
+;; Returns (mv erp parsed-value state).
+;; Example call: (parse-file-as-json "example.json" state)
+(defund parse-file-as-json (filename state)
+  (declare (xargs :stobjs state
+                  :guard (stringp filename)))
+  (b* (((mv erp parsed-value & state) ; ignore the chars
+        (parse-file-as-json-aux filename state)))
+    (mv erp parsed-value state)))
