21bb8a83ba88f055358e1e8d5d0aad8501aa6c76
Eric Smith <ewsmith@gmail.com>
2021-09-14 03:41:42 -0700

    [utilities] Prove that fresh names are indeed fresh.

--- books/kestrel/utilities/fresh-names.lisp
+++ books/kestrel/utilities/fresh-names.lisp
@@ -14,9 +14,12 @@
 ;; STATUS: IN-PROGRESS
 
 (include-book "pack")
+(local (include-book "intern-in-package-of-symbol"))
+(local (include-book "kestrel/lists-light/remove-equal" :dir :system))
+;(local (include-book "kestrel/lists-light/member-equal" :dir :system))
 
 ;rename fresh-symbol-aux ?
-(defun fresh-var-name (base-name current-num syms-to-avoid)
+(defund fresh-var-name (base-name current-num syms-to-avoid)
   (declare (type (integer 0 *) current-num)
            (xargs :measure (len syms-to-avoid)
                   :guard (true-listp syms-to-avoid)))
@@ -27,6 +30,50 @@
                       (+ 1 current-num)
                       (remove-eq candidate
                                  syms-to-avoid)))))
+
+(local
+ (defthm fresh-var-name-of-remove-equal-irrel
+   (implies (and (< current-num current-num+)
+                 (natp current-num)
+                 (natp current-num+))
+            (equal (FRESH-VAR-NAME BASE-NAME
+                                   CURRENT-NUM+
+                                   (REMOVE-EQUAL
+                                    (pack$ base-name current-num)
+                                    SYMS-TO-AVOID))
+                   (FRESH-VAR-NAME BASE-NAME
+                                   CURRENT-NUM+
+                                   SYMS-TO-AVOID)))
+   :hints (("Goal" :induct (FRESH-VAR-NAME BASE-NAME CURRENT-NUM+ SYMS-TO-AVOID)
+            :expand (FRESH-VAR-NAME
+                     BASE-NAME CURRENT-NUM+
+                     (REMOVE-EQUAL
+                      (INTERN-IN-PACKAGE-OF-SYMBOL (BINARY-PACK BASE-NAME CURRENT-NUM)
+                                                   'REWRITE)
+                      SYMS-TO-AVOID))
+            :in-theory (enable FRESH-VAR-NAME)))))
+
+
+(local
+ (defthm not-equal-of-fresh-var-name-and-pack$-lower-num
+   (implies (and (< current-num current-num+)
+                 (natp current-num)
+                 (natp current-num+))
+            (not (equal (fresh-var-name base-name current-num+ syms-to-avoid)
+                        (pack$ base-name current-num))))
+   :hints (("Goal" :in-theory (enable fresh-var-name)))))
+
+(defthm not-member-equal-of-fresh-var-name-same
+  (implies (natp current-num)
+           (not (member-equal (fresh-var-name base-name current-num syms-to-avoid)
+                              syms-to-avoid)))
+  :hints (("Goal" :in-theory (enable fresh-var-name))))
+
+;; (defthm not-member-equal-of-fresh-var-name
+;;   (implies (subsetp-equal syms syms-to-avoid)
+;;            (not (member-equal (fresh-var-name base-name current-num syms-to-avoid)
+;;                               syms)))
+;;   :hints (("Goal" :in-theory (enable fresh-var-name))))
 
 ;rename fresh-symbol ?
 ;example: (make-fresh-name 'x '(x x1 x2 x3 x5)) = x4
