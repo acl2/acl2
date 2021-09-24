e6947e7742989502924ceb32a6b495bcecbc0150
Alessandro Coglio <coglio@kestrel.edu>
2021-09-24 16:33:38 -0700

    [ATC] Improve some symbolic execution rules.
    Add SYNTAXP hyps to ensure a rewriting order.

--- books/kestrel/c/atc/execution-rules.lisp
+++ books/kestrel/c/atc/execution-rules.lisp
@@ -1014,65 +1014,85 @@
      while the @('boolean-from-<type>') terms occur
      in the ACL2 functions that represent the C code."))
 
-  (defruled exec-test-when-scharp
-    (implies (scharp x)
-             (equal (exec-test x)
-                    (boolean-from-schar x)))
-    :enable exec-test)
+  (make-event
+   `(defruled exec-test-when-scharp
+      (implies (and ,(atc-syntaxp-hyp-for-expr-pure 'x)
+                    (scharp x))
+               (equal (exec-test x)
+                      (boolean-from-schar x)))
+      :enable exec-test))
 
-  (defruled exec-test-when-ucharp
-    (implies (ucharp x)
-             (equal (exec-test x)
-                    (boolean-from-uchar x)))
-    :enable exec-test)
+  (make-event
+   `(defruled exec-test-when-ucharp
+      (implies (and ,(atc-syntaxp-hyp-for-expr-pure 'x)
+                    (ucharp x))
+               (equal (exec-test x)
+                      (boolean-from-uchar x)))
+      :enable exec-test))
 
-  (defruled exec-test-when-sshortp
-    (implies (sshortp x)
-             (equal (exec-test x)
-                    (boolean-from-sshort x)))
-    :enable exec-test)
+  (make-event
+   `(defruled exec-test-when-sshortp
+      (implies (and ,(atc-syntaxp-hyp-for-expr-pure 'x)
+                    (sshortp x))
+               (equal (exec-test x)
+                      (boolean-from-sshort x)))
+      :enable exec-test))
 
-  (defruled exec-test-when-ushortp
-    (implies (ushortp x)
-             (equal (exec-test x)
-                    (boolean-from-ushort x)))
-    :enable exec-test)
+  (make-event
+   `(defruled exec-test-when-ushortp
+      (implies (and ,(atc-syntaxp-hyp-for-expr-pure 'x)
+                    (ushortp x))
+               (equal (exec-test x)
+                      (boolean-from-ushort x)))
+      :enable exec-test))
 
-  (defruled exec-test-when-sintp
-    (implies (sintp x)
-             (equal (exec-test x)
-                    (boolean-from-sint x)))
-    :enable exec-test)
+  (make-event
+   `(defruled exec-test-when-sintp
+      (implies (and ,(atc-syntaxp-hyp-for-expr-pure 'x)
+                    (sintp x))
+               (equal (exec-test x)
+                      (boolean-from-sint x)))
+      :enable exec-test))
 
-  (defruled exec-test-when-uintp
-    (implies (uintp x)
-             (equal (exec-test x)
-                    (boolean-from-uint x)))
-    :enable exec-test)
+  (make-event
+   `(defruled exec-test-when-uintp
+      (implies (and ,(atc-syntaxp-hyp-for-expr-pure 'x)
+                    (uintp x))
+               (equal (exec-test x)
+                      (boolean-from-uint x)))
+      :enable exec-test))
 
-  (defruled exec-test-when-slongp
-    (implies (slongp x)
-             (equal (exec-test x)
-                    (boolean-from-slong x)))
-    :enable exec-test)
+  (make-event
+   `(defruled exec-test-when-slongp
+      (implies (and ,(atc-syntaxp-hyp-for-expr-pure 'x)
+                    (slongp x))
+               (equal (exec-test x)
+                      (boolean-from-slong x)))
+      :enable exec-test))
 
-  (defruled exec-test-when-ulongp
-    (implies (ulongp x)
-             (equal (exec-test x)
-                    (boolean-from-ulong x)))
-    :enable exec-test)
+  (make-event
+   `(defruled exec-test-when-ulongp
+      (implies (and ,(atc-syntaxp-hyp-for-expr-pure 'x)
+                    (ulongp x))
+               (equal (exec-test x)
+                      (boolean-from-ulong x)))
+      :enable exec-test))
 
-  (defruled exec-test-when-sllongp
-    (implies (sllongp x)
-             (equal (exec-test x)
-                    (boolean-from-sllong x)))
-    :enable exec-test)
+  (make-event
+   `(defruled exec-test-when-sllongp
+      (implies (and ,(atc-syntaxp-hyp-for-expr-pure 'x)
+                    (sllongp x))
+               (equal (exec-test x)
+                      (boolean-from-sllong x)))
+      :enable exec-test))
 
-  (defruled exec-test-when-ullongp
-    (implies (ullongp x)
-             (equal (exec-test x)
-                    (boolean-from-ullong x)))
-    :enable exec-test))
+  (make-event
+   `(defruled exec-test-when-ullongp
+      (implies (and ,(atc-syntaxp-hyp-for-expr-pure 'x)
+                    (ullongp x))
+               (equal (exec-test x)
+                      (boolean-from-ullong x)))
+      :enable exec-test)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
