a01c3e25014d9efbf5527fa217f2a71547c762e4
Alessandro Coglio <coglio@kestrel.edu>
2021-09-23 22:24:51 -0700

    [ATC] Improve a symbolic execution rules.
    Add SYNTAXP hyps to enforce that certain rewrites happen before others.

--- books/kestrel/c/atc/execution-rules.lisp
+++ books/kestrel/c/atc/execution-rules.lisp
@@ -53,6 +53,34 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(define atc-syntaxp-hyp-for-expr-pure ((var symbolp))
+  :returns (hyp pseudo-termp :hyp (symbolp var))
+  :short "Construct a @(tsee syntaxp) hypothesis for
+          a symbolic execution rule for pure expressions."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "We use these hypotheses to ensure that
+     certain execution subterms are rewritten
+     to their shallow embedding counterparts
+     before their enclosing terms are rewritten.
+     These hypotheses require that the (sub)term in question
+     does not contain any of the execution functions
+     that are expected to be rewritten
+     to their shallow embedding counterparts."))
+  `(syntaxp (or (atom ,var)
+                (not (member-eq ,var '(exec-ident
+                                       exec-const
+                                       exec-iconst
+                                       exec-arrsub
+                                       exec-unary
+                                       exec-cast
+                                       exec-binary-strict-pure
+                                       exec-test
+                                       exec-expr-pure))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defxdoc atc-exec-ident-rules
   :short "Rules for executing identifiers."
   :long
@@ -566,7 +594,9 @@
          (atype-array-read-itype
           (pack afixtype '-array-read- ifixtype))
          (name (pack 'exec-arrsub-when- apred '-and- ipred))
-         (formula `(implies (and (pointerp x)
+         (formula `(implies (and ,(atc-syntaxp-hyp-for-expr-pure 'x)
+                                 ,(atc-syntaxp-hyp-for-expr-pure 'y)
+                                 (pointerp x)
                                  (,apred (deref x heap))
                                  (,ipred y)
                                  (,atype-array-itype-index-okp (deref x heap) y))
