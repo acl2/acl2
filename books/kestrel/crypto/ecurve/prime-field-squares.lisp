ecece8567ae670b7a0a816d7865ff44dc10d2f33
Alessandro Coglio <coglio@kestrel.edu>
2021-09-27 20:25:08 -0700

    [ecurve] Update authorship information.

--- books/kestrel/crypto/ecurve/prime-field-squares.lisp
+++ books/kestrel/crypto/ecurve/prime-field-squares.lisp
@@ -4,7 +4,8 @@
 ;
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 ;
-; Author: Alessandro Coglio (coglio@kestrel.edu)
+; Main Author: Alessandro Coglio (coglio@kestrel.edu)
+; Contributing Author: Eric McCarthy (mccarthy@kestrel.edu)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
