ddd874ee810ca535c6ec9ccf984748950eaa8104
Eric Smith <ewsmith@gmail.com>
2021-10-11 13:54:57 -0700

    [evaluators] Add top.lisp.

---
+++ books/kestrel/evaluators/top.lisp
@@ -0,0 +1,14 @@
+; Top book for evaluators library
+;
+; Copyright (C) 2021 Kestrel Institute
+;
+; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
+;
+; Author: Eric Smith (eric.smith@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+(include-book "defevaluator-plus")
+(include-book "empty-eval")
