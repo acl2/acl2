0d4aba9cc5a2e650da8b06d7ca51b4d02839bcce
Eric Smith <ewsmith@gmail.com>
2021-10-11 13:56:42 -0700

    [terms-light] Add comment.

--- books/kestrel/terms-light/expand-lambdas-in-term-proof.lisp
+++ books/kestrel/terms-light/expand-lambdas-in-term-proof.lisp
@@ -93,6 +93,7 @@
 (include-book "kestrel/alists-light/lookup-equal" :dir :system)
 (include-book "kestrel/alists-light/lookup-equal-lst" :dir :system)
 
+;this holds for any evaluator?
 (defthm lambda-eval-list-when-symbol-listp
   (implies (and (symbol-listp vars)
                 (not (member-equal nil vars)))
