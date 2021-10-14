836604c8a9635e8b7f2140c75e8339e2c787de91
Eric Smith <ewsmith@gmail.com>
2021-10-14 11:11:43 -0700

    [my-make-flag] Speed up proofs.
    Use a custom clause processor to speed up termination proof of the flag function.  In certain cases, this was very slow using only ACL2's built-in reasoning for case analysis.  The clause processor speeds up the call (my-make-flag tamep) by 100x, from ~40 seconds to ~.4 seconds.

--- books/kestrel/library-wrappers/my-make-flag.lisp
+++ books/kestrel/library-wrappers/my-make-flag.lisp
@@ -12,11 +12,115 @@
 
 (include-book "tools/flag" :dir :system)
 (include-book "misc/install-not-normalized" :dir :system)
-;(include-book "kestrel/clause-processors/subst-flag" :dir :system)
-;(include-book "kestrel/clause-processors/flatten-literals" :dir :system)
-;(include-book "kestrel/clause-processors/simple-subsumption" :dir :system)
+(include-book "kestrel/clause-processors/subst-flag" :dir :system)
+(include-book "kestrel/clause-processors/flatten-literals" :dir :system)
+(include-book "kestrel/clause-processors/simple-subsumption" :dir :system)
+(include-book "kestrel/clause-processors/push-unary-functions" :dir :system)
+
+(local (in-theory (disable disjoin)))
+
+(local
+ (defthm pseudo-term-listp-of-car-when-pseudo-term-list-listp
+   (implies (pseudo-term-list-listp lists)
+            (pseudo-term-listp (car lists)))
+   :hints (("Goal" :in-theory (enable pseudo-term-list-listp)))))
 
 ;; TODO: Have my-make-flag (or make-flag) put in the :ruler-extenders of the old function by default.
+
+(defevaluator+ my-make-flag-eval if equal eql eq not
+  booland boolor boolif myif ;todo
+  )
+
+;changes the evaluator
+(defthm resolve-ifs-in-clause-correct-new
+  (iff (my-make-flag-eval (disjoin (resolve-ifs-in-clause clause nil)) a)
+       (my-make-flag-eval (disjoin clause) a))
+  :hints (("Goal" :use (:instance (:functional-instance
+                                   resolve-ifs-in-clause-correct-special
+                                   (equality-eval my-make-flag-eval)
+                                   (equality-eval-list my-make-flag-eval-list)))
+           :in-theory (enable MY-MAKE-FLAG-EVAL-OF-FNCALL-ARGS))))
+
+;changes the evaluator
+(defthm my-make-flag-eval-of-disjoin-of-flatten-disjuncts
+  (iff (my-make-flag-eval (disjoin (flatten-disjuncts clause)) a)
+       (my-make-flag-eval (disjoin clause) a))
+  :hints (("Goal" :use (:instance (:functional-instance
+                                   con-and-dis-eval-of-disjoin-of-flatten-disjuncts
+                                   (con-and-dis-eval my-make-flag-eval)
+                                   (con-and-dis-eval-list my-make-flag-eval-list)))
+           :in-theory (enable MY-MAKE-FLAG-EVAL-OF-FNCALL-ARGS))))
+
+;changes the evaluator
+(defthm my-make-flag-eval-of-disjoin-of-sublis-var-and-simplify-lst
+  (implies (and (alistp a)
+                (pseudo-term-listp clause))
+           (iff (my-make-flag-eval (disjoin (sublis-var-and-simplify-lst nil clause nil nil)) a)
+                (my-make-flag-eval (disjoin clause) a)))
+  :hints (("Goal" :use (:instance (:functional-instance
+                                   equality-eval-of-disjoin-of-sublis-var-and-simplify-lst-special
+                                   (equality-eval my-make-flag-eval)
+                                   (equality-eval-list my-make-flag-eval-list)))
+           :in-theory (enable MY-MAKE-FLAG-EVAL-OF-FNCALL-ARGS))))
+
+;changes the evaluator
+(defthm my-make-flag-eval-of-disjoin-of-push-unary-functions-in-literals
+  (implies (and (symbol-listp unary-fns)
+                (alistp a)
+                (pseudo-term-listp clause))
+           (iff (my-make-flag-eval (disjoin (push-unary-functions-in-literals clause unary-fns)) a)
+                (my-make-flag-eval (disjoin clause) a)))
+  :hints (("Goal" :use (:instance (:functional-instance
+                                   if-eval-of-disjoin-of-push-unary-functions-in-literals
+                                   (if-eval my-make-flag-eval)
+                                   (if-eval-list my-make-flag-eval-list)))
+           :in-theory (enable MY-MAKE-FLAG-EVAL-OF-FNCALL-ARGS))))
+
+;changes the evaluator
+(defthm my-make-flag-eval-of-conjoin-of-disjoin-lst-of-clause-to-clause-list
+  (iff (my-make-flag-eval (conjoin (disjoin-lst (clause-to-clause-list clause))) a)
+       (my-make-flag-eval (disjoin clause) a))
+  :hints (("Goal" :use (:instance (:functional-instance
+                                   equality-eval-of-conjoin-of-disjoin-lst-of-clause-to-clause-list
+                                   (equality-eval my-make-flag-eval)
+                                   (equality-eval-list my-make-flag-eval-list)))
+           :in-theory (enable MY-MAKE-FLAG-EVAL-OF-FNCALL-ARGS))))
+
+;changes the evaluator
+(defthm my-make-flag-eval-of-disjoin-of-handle-constant-literals
+  (iff (my-make-flag-eval (disjoin (handle-constant-literals clause)) a)
+       (my-make-flag-eval (disjoin clause) a))
+  :hints (("Goal" :use (:instance (:functional-instance
+                                   equality-eval-of-disjoin-of-handle-constant-literals
+                                   (equality-eval my-make-flag-eval)
+                                   (equality-eval-list my-make-flag-eval-list)))
+           :in-theory (enable MY-MAKE-FLAG-EVAL-OF-FNCALL-ARGS))))
+
+
+(defun my-make-flag-clause-processor (clause)
+  (declare (xargs :guard (pseudo-term-listp clause)))
+  (let* ((clause (first (sublis-var-and-simplify-clause-processor clause)))
+         (clause (first (flatten-literals-clause-processor clause)))
+         (clause (first (push-o-p-clause-processor clause)))
+         (clauses (simple-subsumption-clause-processor clause))  ;todo: doesn't yet deal with the o-p claims because they appear not as conjuncts
+         )
+    clauses))
+
+;todo: add :well-formedness proof
+(defthm my-make-flag-clause-processor-correct
+  (implies (and (pseudo-term-listp clause)
+                (alistp a)
+                (my-make-flag-eval (conjoin-clauses (my-make-flag-clause-processor clause)) a))
+           (my-make-flag-eval (disjoin clause) a))
+  :rule-classes :clause-processor
+  :hints (("Goal" :in-theory (e/d ( ;sublis-var-and-simplify-clause-processor
+                                   simple-subsumption-clause-processor
+                                   FLATTEN-LITERALS-CLAUSE-PROCESSOR
+                                   SUBLIS-VAR-AND-SIMPLIFY-CLAUSE-PROCESSOR
+                                   PUSH-O-P-CLAUSE-PROCESSOR
+                                   )
+                                  (DISJOIN-LST)))))
+
 
 ;dup
 (defun make-doublets (xs ys)
@@ -97,12 +201,8 @@
                          ;; :in-theory nil ;;too restrictive
                          :in-theory (theory 'minimal-theory) ;;still too restrictive?
                          )
-                        ;; todo: combine these:
                         ;; todo: consider also handling o-p of if
-                        ;; ("goal'" :clause-processor (sublis-var-and-simplify-clause-processor clause))
-                        ;; ("goal''" :clause-processor (flatten-literals-clause-processor clause))
-                        ;; ("goal'''" :clause-processor (simple-subsumption-clause-processor clause))
-                        )))))
+                        ("goal'" :clause-processor (my-make-flag-clause-processor clause)))))))
 
 ;; This is a wrapper around make-flag that attempts to be more robust.  It uses
 ;; the :termination-theorem of the given function in the :hints supplied to
