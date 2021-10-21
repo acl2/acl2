dded0a9e6ed10bf85ed02bee89bc932655c58564
Eric Smith <ewsmith@gmail.com>
2021-10-21 13:33:58 -0700

    [apt] Add file header.

--- books/kestrel/apt/wrap-output-tests.lisp
+++ books/kestrel/apt/wrap-output-tests.lisp
@@ -1,3 +1,13 @@
+; Tests of the wrap-output transformation
+;
+; Copyright (C) 2014-2021 Kestrel Institute
+;
+; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
+;
+; Author: Eric Smith (eric.smith@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (in-package "ACL2")
 
 (include-book "wrap-output")
