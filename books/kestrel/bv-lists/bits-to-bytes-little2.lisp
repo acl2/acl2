42e500b92a96c589f404fb45c63eb2314344fe0c
Eric Smith <ewsmith@gmail.com>
2021-10-27 22:34:06 -0700

    [bv-lists] Add rules about bits-to-bytes-little.
    Also keep it disabled.

--- books/kestrel/bv-lists/bits-to-bytes-little2.lisp
+++ books/kestrel/bv-lists/bits-to-bytes-little2.lisp
@@ -13,5 +13,14 @@
 
 (include-book "kestrel/bv-lists/bits-to-bytes-little" :dir :system)
 (include-book "kestrel/utilities/defopeners" :dir :system)
+(include-book "unsigned-byte-listp")
 
 (defopeners bits-to-bytes-little :disable t)
+
+(defthm unsigned-byte-listp-of-bits-to-bytes-little
+  (unsigned-byte-listp 8 (bits-to-bytes-little bits))
+  :hints (("Goal" :in-theory (enable unsigned-byte-listp-rewrite))))
+
+(defthm all-integerp-of-bits-to-bytes-little
+  (all-integerp (bits-to-bytes-little bits))
+  :hints (("Goal" :in-theory (enable bits-to-bytes-little))))
