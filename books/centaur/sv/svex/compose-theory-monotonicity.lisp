91b8a6351dc9629bc6c0fa92f0e2c47485ce78a2
Sol Swords <sswords@centtech.com>
2021-10-29 15:24:03 -0500

    improve compose-theory-monotonicity, fix broken books

--- books/centaur/sv/svex/compose-theory-monotonicity.lisp
+++ books/centaur/sv/svex/compose-theory-monotonicity.lisp
@@ -180,23 +180,349 @@
                       (neteval-ordering-or-null-compile x signal network2)))
     :hints(("Goal" :in-theory (enable svex-[=)))))
 
+(defsection netcomp-[=
+  (defun-sk netcomp-[= (comp decomp)
+    (exists ordering
+            (svex-alist-compose-[= comp
+                                   (neteval-ordering-compile ordering decomp))))
 
-(defthm compose-conservatively-modified-ok
-  (implies (and (netcomp-p part-composed orig)
-                (svex-alist-compose-[= modified part-composed)
-                (netcomp-p final-composed modified)
-                (svex-alist-monotonic-p orig))
-           (let ((witness (neteval-ordering-compose
-                           (netcomp-p-eval-equiv-witness final-composed modified)
-                           (netcomp-p-eval-equiv-witness part-composed orig))))
-             (svex-alist-[= final-composed
-                            (neteval-ordering-compile witness orig))))
-  :hints(("Goal" :in-theory (enable netcomp-p-redef)
-          :use ((:instance neteval-ordering-compile-preserves-[=
-                 (x (netcomp-p-eval-equiv-witness final-composed modified))
-                 (network1 modified)
-                 (network2 part-composed))
-                (:instance neteval-ordering-compile-monotonic
-                 (x (netcomp-p-eval-equiv-witness part-composed orig))
-                 (network orig))))))
-         
+  
+  (in-theory (disable netcomp-[= netcomp-[=-suff))
+
+
+
+
+  (deffixcong svex-alist-equiv equal (netcomp-[= comp decomp) comp
+    :hints (("goal" :cases ((netcomp-[= comp decomp))
+             :in-theory (enable netcomp-[=)
+             :use ((:instance netcomp-[=-suff
+                    (comp (svex-alist-fix comp))
+                    (ordering (netcomp-[=-witness comp decomp)))
+                   (:instance netcomp-[=-suff
+                    (ordering (netcomp-[=-witness (svex-alist-fix comp) decomp)))))))
+
+  (deffixcong svex-alist-equiv equal (netcomp-[= comp decomp) decomp
+    :hints (("goal" :cases ((netcomp-[= comp decomp))
+             :in-theory (enable netcomp-[=)
+             :use ((:instance netcomp-[=-suff
+                    (decomp (svex-alist-fix decomp))
+                    (ordering (netcomp-[=-witness comp decomp)))
+                   (:instance netcomp-[=-suff
+                    (ordering (netcomp-[=-witness comp (svex-alist-fix decomp))))))))
+
+  (defcong svex-alist-eval-equiv equal (netcomp-[= comp decomp) 1
+    :hints (("goal" :cases ((netcomp-[= comp decomp))
+             :in-theory (enable netcomp-[=)
+             :use ((:instance netcomp-[=-suff
+                    (comp comp-equiv)
+                    (ordering (netcomp-[=-witness comp decomp)))
+                   (:instance netcomp-[=-suff
+                    (ordering (netcomp-[=-witness comp-equiv decomp)))))))
+
+  (defcong svex-alist-eval-equiv equal (netcomp-[= comp decomp) 2
+    :hints (("goal" :cases ((netcomp-[= comp decomp))
+             :in-theory (enable netcomp-[=)
+             :use ((:instance netcomp-[=-suff
+                    (decomp decomp-equiv)
+                    (ordering (netcomp-[=-witness comp decomp)))
+                   (:instance netcomp-[=-suff
+                    (ordering (netcomp-[=-witness comp decomp-equiv)))))))
+
+  (define netcomp-[=-same-keys-witness ((comp svex-alist-p)
+                                        (decomp svex-alist-p))
+    :non-executable t
+    :returns (ord neteval-ordering-p)
+    :verify-guards nil
+    ;; Despite only requiring that comp is compose-[= netcomp-[= actually
+    ;; assures that there is an ordering that is eval-equiv.
+    (neteval-ordering-extract (svex-alist-keys comp)
+                              (netcomp-[=-witness (svex-alist-fix comp)
+                                                  (svex-alist-fix decomp)))
+    ///
+
+    (defret alist-keys-of-<fn>
+      (equal (alist-keys ord)
+             (svex-alist-keys comp)))
+
+    ;; (defthm svex-alist-compextract-keys-under-compose-equiv
+    ;;   (svex-alist-[= (svex-alist-compextract (svex-alist-keys x) x) x)
+    ;;   :hints(("Goal" :in-theory (enable svex-alist-eval-equiv))
+    ;;          (and stable-under-simplificationp
+    ;;               '(:in-theory (enable svex-compose-lookup)))))
+
+    (defret netcomp-[=-implies-same-keys-witness
+      (implies (netcomp-[= comp decomp)
+               (svex-alist-[= comp (neteval-ordering-compile ord decomp)))
+      :hints (("goal" 
+               :in-theory (e/d (svex-alist-[=-in-terms-of-lookup)
+                               (svex-alist-compose-[=-necc))
+               :use ((:instance netcomp-[=
+                      (comp (svex-alist-fix comp))
+                      (decomp (svex-alist-fix decomp)))
+                     (:instance svex-alist-compose-[=-necc
+                      (x comp) (y (neteval-ordering-compile
+                                   (netcomp-[=-witness (svex-alist-fix comp)
+                                                       (svex-alist-fix decomp))
+                                   decomp))
+                      (var (svex-alist-[=-lookup-witness
+                            comp (svex-alist-compextract
+                                  (svex-alist-keys comp)
+                                  (neteval-ordering-compile
+                                   (netcomp-[=-witness (svex-alist-fix comp)
+                                                       (svex-alist-fix decomp))
+                                   decomp)))))))
+              (and stable-under-simplificationp
+                   '(:in-theory (enable svex-compose-lookup)))))
+
+    (defthmd netcomp-[=-redef
+      (equal (netcomp-[= comp decomp)
+             (let ((ordering (netcomp-[=-same-keys-witness comp decomp)))
+               (svex-alist-[= comp (neteval-ordering-compile ordering decomp))))
+      :hints (("goal" :cases ((netcomp-[= comp decomp))
+               :use ((:instance netcomp-[=-suff
+                      (ordering (netcomp-[=-same-keys-witness comp decomp))))
+               :in-theory (e/d (netcomp-[=-implies-same-keys-witness)
+                               (netcomp-[=-suff
+                                netcomp-[=-same-keys-witness))))
+      :rule-classes :definition))
+
+
+  (local (defthm svex-compose-lookup-of-append
+           (equal (Svex-compose-lookup key (append x y))
+                  (or (svex-lookup key x)
+                      (svex-compose-lookup key y)))
+           :hints(("Goal" :in-theory (enable svex-compose-lookup)))))
+
+  (local (defthm svex-eval-equiv-of-svex-lookup-when-compose-equiv
+           (implies (and (svex-alist-compose-equiv x y)
+                         (svex-lookup k x) (svex-lookup k y))
+                    (equal (svex-eval-equiv (svex-lookup k x) (svex-lookup k y)) t))
+           :hints (("goal" :use ((:instance svex-alist-compose-equiv-necc
+                                  (var k)))
+                    :in-theory (e/d (svex-compose-lookup)
+                                    (svex-alist-compose-equiv-necc))))))
+  
+
+
+  (defthmd netcomp-[=-transitive
+    (implies (and (netcomp-[= x y)
+                  (netcomp-[= y z)
+                  (or (svex-alist-monotonic-p y)
+                      (svex-alist-monotonic-p z)))
+             (netcomp-[= x z))
+    :hints (("goal" :expand ((netcomp-[= x y)
+                             (netcomp-[= y z))
+             :in-theory (enable svex-alist-[=-transitive-1
+                                svex-alist-[=-transitive-2)
+             :use ((:instance netcomp-[=-suff
+                    (comp x) (decomp z)
+                    (ordering (neteval-ordering-compose (netcomp-[=-same-keys-witness x y)
+                                                        (netcomp-[=-same-keys-witness y z))))))))
+
+  (defthmd netcomp-[=-transitive2
+    (implies (and (netcomp-[= y z)
+                  (netcomp-[= x y)
+                  (or (svex-alist-monotonic-p y)
+                      (svex-alist-monotonic-p z)))
+             (netcomp-[= x z))
+    :hints(("Goal" :in-theory (enable netcomp-[=-transitive))))
+
+  (defthm netcomp-[=-reflexive
+    (netcomp-[= x x)
+    :hints (("goal" :use ((:instance netcomp-[=-suff
+                           (comp x) (decomp x)
+                           (ordering (neteval-ordering-identity (svex-alist-keys x)))))
+             :in-theory (enable svex-alist-eval-equiv))))
+
+  (defthm svex-alist-keys-of-svex-alist-reduce
+    (equal (svex-alist-keys (svex-alist-reduce keys a))
+           (intersection-equal (svarlist-fix keys) (svex-alist-keys a)))
+    :hints(("Goal" :in-theory (enable svex-alist-reduce svex-alist-keys))))
+
+  (defthm svex-alist-[=-of-svex-alist-reduce
+    (implies (svex-alist-[= x y)
+             (svex-alist-[= (svex-alist-reduce keys x)
+                            (svex-alist-reduce keys y)))
+    :hints (("goal" :expand ((:with svex-alist-[=-in-terms-of-lookup
+                              (svex-alist-[= (svex-alist-reduce keys x)
+                                             (svex-alist-reduce keys y)))))))
+
+  (defthm netcomp-[=-of-svex-alist-reduce
+    (implies (netcomp-[= x y)
+             (netcomp-[= (svex-alist-reduce keys x) y))
+    :hints (("goal" :use ((:instance netcomp-[=-suff
+                           (comp (svex-alist-reduce keys x))
+                           (decomp y)
+                           (ordering (neteval-ordering-reduce keys (netcomp-[=-same-keys-witness x y)))))
+             :expand ((netcomp-[= x y)))))
+  
+  (defthm append-svex-alist-eval-when-svex-alist-compose-equiv
+    (implies (svex-alist-compose-equiv x y)
+             (svex-envs-similar (append (svex-alist-eval x env) env)
+                                (append (svex-alist-eval y env) env)))
+    :hints(("Goal" :in-theory (e/d (svex-envs-similar svex-compose-lookup)
+                                   (svex-alist-compose-equiv-necc
+                                    svex-alist-compose-equiv-implies-svex-eval-equiv-svex-compose-lookup-2))
+            :expand ((:free (var) (svex-eval (svex-var var) env)))
+            :use ((:instance svex-alist-compose-equiv-necc
+                   (var (svex-envs-similar-witness
+                         (append (svex-alist-eval x env) env)
+                                (append (svex-alist-eval y env) env)))))))
+    :rule-classes :congruence)
+            
+
+  (defcong svex-alist-compose-equiv svex-eval-equiv
+    (svex-compose x subst) 2
+    :hints(("Goal" :in-theory (enable svex-eval-equiv))))
+
+  (defcong svex-alist-compose-equiv svex-alist-eval-equiv
+    (svex-alist-compose x subst) 2
+    :hints(("Goal" :in-theory (enable svex-alist-eval-equiv))))
+
+  
+  (defthm svex-alist-keys-of-svex-alist-compose
+    (equal (Svex-alist-keys (Svex-alist-compose x y))
+           (svex-alist-keys x))
+    :hints(("Goal" :in-theory (enable svex-alist-compose svex-alist-keys))))
+
+  (defthm svex-alist-[=-of-svex-alist-compose
+    (implies (and (svex-alist-[= x y)
+                  (svex-alist-compose-[= a b)
+                  (or (svex-alist-monotonic-p x)
+                      (svex-alist-monotonic-p y)))
+             (svex-alist-[= (svex-alist-compose x a)
+                            (svex-alist-compose y b)))
+    :hints (("goal" :use ((:instance svex-alist-[=-transitive-1
+                           (x (svex-alist-compose x a))
+                           (y (svex-alist-compose x b))
+                           (z (svex-alist-compose y b)))
+                          (:instance svex-alist-[=-transitive-1
+                           (x (svex-alist-compose x a))
+                           (y (svex-alist-compose y a))
+                           (z (svex-alist-compose y b)))))))
+
+
+  (defthm netcomp-[=-of-svex-alist-compose
+    (implies (and (netcomp-[= x network)
+                  (netcomp-[= subst network)
+                  (or (svex-alist-monotonic-p network)
+                      (svex-alist-monotonic-p x)))
+             (netcomp-[= (svex-alist-compose x subst) network))
+    :hints (("goal" :use ((:instance netcomp-[=-suff
+                           (comp (svex-alist-compose x subst))
+                           (decomp network)
+                           (ordering (neteval-ordering-compose-aux
+                                      (netcomp-[=-same-keys-witness x network)
+                                      (netcomp-[=-same-keys-witness subst network)))))
+             :expand ((netcomp-[= x network)
+                      (netcomp-[= subst network)))))
+
+  (defthm svex-alist-compose-of-svex-identity-subst
+    (implies (subsetp-equal vars (svex-alist-keys x))
+             (equal (svex-alist-compose (svex-identity-subst vars) x)
+                    (svex-alist-reduce vars x)))
+    :hints(("Goal" :in-theory (enable svex-alist-compose
+                                      ;; svex-alist-keys
+                                      svex-identity-subst
+                                      svarlist-fix
+                                      svex-acons
+                                      svex-compose
+                                      pairlis$
+                                      svarlist->svexes
+                                      svex-alist-reduce))))
+
+  (defthm netcomp-[=-of-svex-identity-subst
+    (implies (subsetp-equal (svarlist-fix vars) (svex-alist-keys network))
+             (netcomp-[= (svex-identity-subst vars) network))
+    :hints (("goal" :use ((:instance netcomp-[=-suff
+                           (comp (svex-identity-subst vars))
+                           (decomp network)
+                           (ordering (neteval-ordering-null vars))))
+             :in-theory (enable svex-alist-eval-equiv-in-terms-of-envs-equivalent
+                                svex-envs-equivalent))))
+
+  (defthm svex-alist-keys-of-append
+    (equal (svex-alist-keys (append x y))
+           (append (svex-alist-keys x)
+                   (svex-alist-keys y)))
+    :hints(("Goal" :in-theory (enable svex-alist-keys))))
+
+  (Defthmd svex-lookup-iff-member-svex-alist-keys
+    (iff (svex-lookup v x)
+         (member-equal (svar-fix v) (svex-alist-keys x))))
+
+  (defthm svex-alist-[=-of-append
+    (implies (and (set-equiv (svex-alist-keys x1)
+                             (svex-alist-keys x2))
+                  (svex-alist-[= x1 x2)
+                  (svex-alist-[= y1 y2))
+             (svex-alist-[= (append x1 y1)
+                            (append x2 y2)))
+    :hints (("goal" :expand ((:with svex-alist-[=-in-terms-of-lookup
+                              (svex-alist-[= (append x1 y1)
+                                             (append x2 y2))))
+             :in-theory (e/d (svex-lookup-iff-member-svex-alist-keys)
+                             (member-svex-alist-keys)))))
+
+  (defthm netcomp-[=-of-append
+    (implies (and (netcomp-[= x network)
+                  (netcomp-[= y network))
+             (netcomp-[= (append x y) network))
+    :hints (("goal" :expand ((netcomp-[= x network)
+                             (netcomp-[= y network)
+                             (SVEX-ALIST-compose-equiv
+                              (APPEND X Y)
+                              (APPEND (NETEVAL-ORDERING-COMPILE (NETCOMP-[=-SAME-KEYS-WITNESS X NETWORK)
+                                                                NETWORK)
+                                      (NETEVAL-ORDERING-COMPILE (NETCOMP-[=-SAME-KEYS-WITNESS Y NETWORK)
+                                                                NETWORK))))
+             :in-theory (disable svex-lookup-of-neteval-ordering-compile)
+             :use ((:instance netcomp-[=-suff
+                    (comp (append x y)) (decomp network)
+                    (ordering (append (netcomp-[=-same-keys-witness x network)
+                                      (netcomp-[=-same-keys-witness y network)))))
+             :do-not-induct t)))
+
+  (defthm netcomp-[=-of-nil
+    (netcomp-[= nil network)
+    :hints (("goal" :use ((:instance netcomp-[=-suff
+                           (comp nil) (decomp network)
+                           (ordering nil)))
+             :expand ((neteval-ordering-compile nil network)))))
+
+  (defthmd netcomp-[=-when-[=-netcomp
+    (implies (and (netcomp-p y z)
+                  (svex-alist-compose-[= x y))
+             (netcomp-[= x z))
+    :hints (("goal" :expand ((netcomp-p y z))
+             :use ((:instance netcomp-[=-suff
+                    (comp x) (decomp z)
+                    (ordering (netcomp-p-eval-equiv-witness y z)))))))
+
+  (local (defthm neteval-ordering-compile-preserves-[=-free
+           (implies (and (svex-alist-eval-equiv y (neteval-ordering-compile x network1))
+                         (or (svex-alist-monotonic-p network1)
+                             (svex-alist-monotonic-p network2))
+                         (svex-alist-compose-[= network1 network2))
+                    (svex-alist-[= y (neteval-ordering-compile x network2)))))
+
+  (defthmd netcomp-[=-when-netcomp-of-[=
+    (implies (and (netcomp-p x y)
+                  (svex-alist-compose-[= y z)
+                  (or (svex-alist-monotonic-p y)
+                      (svex-alist-monotonic-p z)))
+             (netcomp-[= x z))
+    :hints (("goal" :expand ((netcomp-p x y))
+             :use ((:instance netcomp-[=-suff
+                    (comp x) (decomp z)
+                    (ordering (netcomp-p-eval-equiv-witness x y)))))))
+
+  (defthm netevalcomp-p-when-netcomp-[=
+    (implies (netcomp-[= x y)
+             (netevalcomp-p (svex-alist-compose x (svarlist-x-subst (svex-alist-keys y))) y))
+    :hints (("goal" :expand ((netcomp-[= x y))
+             :use ((:instance netevalcomp-p-suff
+                    (comp (svex-alist-compose x (svarlist-x-subst (svex-alist-keys y))))
+                    (network y)
+                    (ordering (netcomp-[=-same-keys-witness x y))))))))
+
