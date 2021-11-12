81a70ac8a7efb554dd75bdc263aa53ea2fac9d76
Alessandro Coglio <coglio@kestrel.edu>
2021-11-11 16:37:19 -0800

    [ATC] Move array copy test to file with other array tests.

--- books/kestrel/c/atc/tests/arrays-test.c
+++ books/kestrel/c/atc/tests/arrays-test.c
@@ -15,7 +15,16 @@ void test_write() {
     printf("a[%d] = %d\n", i, a[i]);
 }
 
+void test_copy() {
+  unsigned char a[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  unsigned char b[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  copy(a, b, 10);
+  for (int i = 0; i < 10; ++i)
+    printf("b[%d] = %d\n", i, b[i]);
+}
+
 int main(void) {
   test_read();
   test_write();
+  test_copy();
 }
