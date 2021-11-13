c28b331fd1c5361eb36fee583e8d27b5e8296a5a
Eric Smith <ewsmith@gmail.com>
2021-11-13 13:17:12 -0800

    [prime-fields] Fix xdoc issue.

--- books/kestrel/prime-fields/doc.lisp
+++ books/kestrel/prime-fields/doc.lisp
@@ -39,7 +39,7 @@
     (xdoc::li (xdoc::tt "(sub x y p)") ": Subtraction in the field: Computes the difference of @('x') and @('y') modulo the prime @('p').")
     (xdoc::li (xdoc::tt "(neg x p)") ": Negation (additive inverse) in the field: Computes the (unary) negation of @('x') modulo the prime @('p').")
     (xdoc::li (xdoc::tt "(mul x y p)") ": Multiplication in the field: Computes the product of @('x') and @('y') modulo the prime @('p').")
-    (xdoc::li (xdoc::tt "(pow x n p)") ": Exponentiation in the field: Computes @('x') to the nth power @(x^n) modulo the prime @('p').  Note that @('n') can be any natural.")
+    (xdoc::li (xdoc::tt "(pow x n p)") ": Exponentiation in the field: Computes @('x') to the nth power @('x^n') modulo the prime @('p').  Note that @('n') can be any natural.")
     (xdoc::li (xdoc::tt "(inv x p)") ": Multiplicative inverse in the field: Computes @('1/x') modulo the prime @('p').  Requires @('x') to be non-zero.")
     (xdoc::li (xdoc::tt "(div x y p)") ": Division in the field: Computes @('x/y') modulo the prime @('p').  Requires @('y') to be non-zero.")
     (xdoc::li (xdoc::tt "(minus1 p)") ": Returns @('p-1').  This is often very large (if @('p')is very large), but it can help to think of this value as @('-1')."))))
