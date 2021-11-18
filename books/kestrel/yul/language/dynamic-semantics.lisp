6830d68ac69499480bda0cab6e463997d7cfc2aa
Alessandro Coglio <coglio@kestrel.edu>
2021-11-17 20:14:15 -0800

    [Yul] Refactor and extend some code.
    Move the definition of the fixtype of modes into a separate file, because it
    will also be used in upcoming extensions of the static semantics.
    
    Also add fixtypes for mode osets and for mode oset results.

--- books/kestrel/yul/language/dynamic-semantics.lisp
+++ books/kestrel/yul/language/dynamic-semantics.lisp
@@ -12,6 +12,7 @@
 
 (include-book "abstract-syntax")
 (include-book "literal-evaluation")
+(include-book "modes")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -410,32 +411,6 @@
                                      cstate)))
     cstate)
   :hooks (:fix))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(fty::deftagsum mode
-  :short "Fixtype of modes."
-  :long
-  (xdoc::topstring
-   (xdoc::p
-    "[Yul: Specification of Yul: Formal Specification] introduces
-     the notion of mode, which indicates how a statement completes execution.")
-   (xdoc::p
-    "We disable the executable counterparts of the constructors of this type,
-     because we want to treat them abstractly.
-     Their definition is already disabled by default, as it should,
-     but the fact that their executable counterpart is enabled
-     defeats the disabling of their definition, given that they are nullary."))
-  (:regular ())
-  (:break ())
-  (:continue ())
-  (:leave ())
-  :pred modep
-  ///
-  (in-theory (disable (:e mode-regular)
-                      (:e mode-break)
-                      (:e mode-continue)
-                      (:e mode-leave))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
