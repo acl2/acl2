8a7cfe812ac21a69d0cc7befadb2dfa0782486a3
Alessandro Coglio <coglio@kestrel.edu>
2022-01-05 13:46:51 -0800

    [ATJ] Add a test with Java package names.

--- books/kestrel/java/atj/tests/compile.sh
+++ books/kestrel/java/atj/tests/compile.sh
@@ -4,7 +4,7 @@
 
 # Java Library
 #
-# Copyright (C) 2021 Kestrel Institute (http://www.kestrel.edu)
+# Copyright (C) 2022 Kestrel Institute (http://www.kestrel.edu)
 #
 # License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 #
@@ -25,4 +25,8 @@
 set -e
 
 # generate class files:
-javac -cp ../../aij/java/out/artifacts/AIJ_jar/AIJ.jar *.java
+javac -cp ../../aij/java/out/artifacts/AIJ_jar/AIJ.jar \
+      *.java \
+      packages1/*.java \
+      packages2/p/*.java \
+      packages3/edu/kestrel/*.java
