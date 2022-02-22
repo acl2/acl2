497cdeffc9a55b54efa80f18b32fbea70f3715e4
Eric McCarthy <mccarthy@kestrel.edu>
2022-02-22 09:24:58 -0800

    [xdoc] fix two broken links

--- books/centaur/vl2014/doc.lisp
+++ books/centaur/vl2014/doc.lisp
@@ -147,7 +147,7 @@ and transistor-level constructs.</li>
 <li>(unreleased) We have used it to implement <i>VL-Mangle</i>, a web-based
 Verilog refactoring tool.  A paper describing this tool can be found in: Jared
 Davis. <a
-href='http://www.cs.utexas.edu/users/jared/publications/2013-doform-embedding/embedding.pdf'>Embedding
+href='https://www.kookamara.com/jared/2013-doform-embedding.pdf'>Embedding
 ACL Models in End-User Applications</a>.  In <a
 href='http://www.cs.bham.ac.uk/research/projects/formare/events/aisb2013/'>Do-Form
 2013</a>.  April, 2013, Exeter, UK.</li>
@@ -432,4 +432,3 @@ subsidiary helpers to carry out its work.  This work transforms various parts
 of the module, and meanwhile the warnings are perhaps extended.  Finally, the
 function returns a new @(see vl-module-p) which is updated with the extended
 list of warnings.</p>")
-
