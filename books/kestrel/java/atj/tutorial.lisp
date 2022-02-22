767e41a38042b4b2caaad158455ac72a751edb70
Alessandro Coglio <coglio@kestrel.edu>
2022-02-22 15:10:43 -0800

    [ATJ] Minor user doc fixes and improvements.

--- books/kestrel/java/atj/tutorial.lisp
+++ books/kestrel/java/atj/tutorial.lisp
@@ -1,6 +1,6 @@
 ; Java Library
 ;
-; Copyright (C) 2020 Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2022 Kestrel Institute (http://www.kestrel.edu)
 ;
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 ;
@@ -290,7 +290,7 @@
     prints a message on the screen,
     returning @('nil') and continuing execution.
     But the ACL2 definitions of both of these example functions
-   just return @('nil').")
+    just return @('nil').")
 
   (xdoc::p
    "The program-mode functions
