8db72dc5384703d48a0d36d690d3f532514e0d95
Eric Smith <ewsmith@gmail.com>
2022-03-30 15:55:28 -0700

    [axe] Update copyright dates.

--- books/kestrel/axe/evaluator-basic.lisp
+++ books/kestrel/axe/evaluator-basic.lisp
@@ -1,7 +1,7 @@
 ; An simple evaluator supporting a basic set of functions
 ;
 ; Copyright (C) 2008-2011 Eric Smith and Stanford University
-; Copyright (C) 2013-2021 Kestrel Institute
+; Copyright (C) 2013-2022 Kestrel Institute
 ; Copyright (C) 2016-2020 Kestrel Technology, LLC
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
--- books/kestrel/axe/evaluator-tests.lisp
+++ books/kestrel/axe/evaluator-tests.lisp
@@ -1,7 +1,7 @@
 ; Tests of the evaluator
 ;
 ; Copyright (C) 2008-2011 Eric Smith and Stanford University
-; Copyright (C) 2013-2020 Kestrel Institute
+; Copyright (C) 2013-2022 Kestrel Institute
 ; Copyright (C) 2016-2020 Kestrel Technology, LLC
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
