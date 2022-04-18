bbce2a57ce17e222c3c07dc3a4ebde09e3e3dd09
Eric Smith <ewsmith@gmail.com>
2022-04-17 20:05:52 -0700

    [utilities] Ensure book certifies with a devel copy.

--- books/kestrel/utilities/chk-length-and-keys.lisp
+++ books/kestrel/utilities/chk-length-and-keys.lisp
@@ -10,6 +10,16 @@
 
 (in-package "ACL2")
 
+;; These forms are redudant except when doing a devel regression.  They seem
+;; necessary, because this book supports verifying the guards of some system
+;; functions, which means this book must certify with a "devel" copy of ACL2,
+;; in which certain functions are in :program mode:
+#+acl2-devel
+(progn (verify-termination macro-args) ; and guards
+       (verify-termination er-cmp-fn) ; and guards
+       (verify-termination chk-length-and-keys)
+       )
+
 (defthm keyword-value-listp-when-not-mv-nth-0-of-chk-length-and-keys
   (implies (and (not (mv-nth 0 (chk-length-and-keys actuals form wrld)))
                 (true-listp actuals))
