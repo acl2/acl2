02fa9503275051a43316bf9bcd9ad53c0d6dd6c3
Andrew Walter <mist3rwalter@gmail.com>
2022-05-02 14:36:17 -0400

    Add 2022 ACL2 Workshop materials
    Add the supporting materials for our ACL2s Systems Programming ACL2
    workshop paper.

---
+++ books/workshops/2022/walter-manolios-interface/acl2s-utils/top.lisp
@@ -0,0 +1,8 @@
+(in-package "ACL2")
+(include-book "tools/include-raw" :dir :system)
+(include-book "xdoc/defxdoc-raw" :dir :system)
+(include-book "itest-ithm")
+(include-book "../top")
+(defttag :acl2s-interface)
+(include-raw "additions.lsp")
+(include-raw "extras.lsp")
