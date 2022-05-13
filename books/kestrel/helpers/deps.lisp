107ef14864e695b82fa4a74ca5e3936c32b1b4b5
Eric Smith <ewsmith@gmail.com>
2022-05-13 03:58:35 -0700

    [helpers] Update deps tool for new format of Makefile-deps.lsp.

--- books/kestrel/helpers/deps.lisp
+++ books/kestrel/helpers/deps.lisp
@@ -10,20 +10,24 @@
 
 (in-package "ACL2")
 
-(include-book "kestrel/file-io-light/read-objects-from-file" :dir :system)
+(include-book "kestrel/file-io-light/read-object-from-file" :dir :system)
 
-;; Returns (mv erp book-names state).
-(defund all-books (state)
+;; Returns (mv erp deps-info state).
+(defund read-deps-info (state)
   (declare (xargs :stobjs state))
   (mv-let (erp deps-info state)
-    (read-objects-from-file "../../build/Makefile-deps.lsp" state) ; todo: make portable
+    (read-object-from-file "../../build/Makefile-deps.lsp" state) ; todo: make portable
     (if erp
-        (mv :failed-to-read-deps nil state)
+        (mv :failed-to-read-deps-info nil state)
       (if (not (alistp deps-info))
-          (mv :bad-deps-file nil state)
-        (let ((dependency-map (cdr (assoc-eq :dependency-map deps-info))))
-          (if (not (alistp dependency-map))
-              (mv :bad-deps-info nil state)
-            (mv nil ; no error
-                (strip-cars dependency-map)
-                state)))))))
+          (mv :bad-deps-info nil state)
+        ;; (let ((dependency-map (cdr (assoc-eq :dependency-map deps-info))))
+        ;;   (if (not (alistp dependency-map))
+        ;;       (mv :bad-deps-info nil state)
+        (mv nil           ; no error
+            deps-info     ;(strip-cars dependency-map)
+            state)))))
+
+;; Example usage:
+;; (mv-let (erp deps-info state) (read-deps-info state) (declare (ignore erp)) (assign deps-info deps-info))
+;; (assoc-eq :CERT_PL_CERTS (@ deps-info))
