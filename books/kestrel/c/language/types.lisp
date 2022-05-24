21f39220c153abf2757800190c6f52a6a40e6169
Alessandro Coglio <coglio@kestrel.edu>
2022-05-23 22:51:46 -0700

    [C] Move some code.
    Put member types, and operations on them, into the file for types.

--- books/kestrel/c/language/types.lisp
+++ books/kestrel/c/language/types.lisp
@@ -125,6 +125,120 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(fty::defprod member-type
+  :short "Fixtype of member types."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "This contains information about
+     the members of structure and union types [C:6.7.2.1].
+     This information consists of a name and a type.
+     We do not capture bit fields (including anonymous ones)
+     and we do not capture static assertions.
+     This information mirrors @(tsee struct-declon).")
+   (xdoc::p
+    "We call these `member types' because they are
+     the static counterpart of the member values
+     captured by @(tsee member-value)."))
+  ((name ident)
+   (type type))
+  :tag :member-type
+  :pred member-typep)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(fty::deflist member-type-list
+  :short "Fixtype of lists of member types."
+  :elt-type member-type
+  :true-listp t
+  :elementp-of-nil nil
+  :pred member-type-listp)
+
+;;;;;;;;;;;;;;;;;;;;
+
+(std::defprojection member-type-list->name-list (x)
+  :guard (member-type-listp x)
+  :returns (names ident-listp)
+  :short "Lift @(tsee member-type->name) to lists."
+  (member-type->name x)
+  ///
+  (fty::deffixequiv member-type-list->name-list
+    :args ((x member-type-listp))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(fty::deftagsum member-type-list-option
+  :short "Fixtype of optional lists of member types."
+  (:some ((val member-type-list)))
+  (:none ())
+  :pred member-type-list-optionp)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defresult member-type-list "lists of member types")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define member-type-lookup ((name identp) (members member-type-listp))
+  :returns (type type-optionp)
+  :short "Look up a member by name in a list of member types."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "We search from the beginning and stop at the first hit;
+     since the names are unique in well-formed lists,
+     the exact search strategy makes no difference
+     We return the type of the member if the search is successful."))
+  (b* (((when (endp members)) nil)
+       ((when (equal (ident-fix name)
+                     (member-type->name (car members))))
+        (member-type->type (car members))))
+    (member-type-lookup name (cdr members)))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define member-type-add-first ((name identp)
+                               (type typep)
+                               (members member-type-listp))
+  :returns (new-members member-type-list-optionp)
+  :short "Add a member type at the beginning of a list of member types."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "We check that the a member with the same name is not already in the list,
+     to maintain the well-formedness of the list."))
+  (b* ((found (member-type-lookup name members))
+       ((when found) (member-type-list-option-none)))
+    (member-type-list-option-some
+     (cons (make-member-type :name name :type type)
+           members)))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define member-type-add-last ((name identp)
+                              (type typep)
+                              (members member-type-listp))
+  :returns (new-members member-type-list-optionp)
+  :short "Add a member at the end of a list of member types."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "We check that the a member with the same name is not already in the list,
+     to maintain the well-formedness of the list."))
+  (b* ((found (member-type-lookup name members))
+       ((when found) (member-type-list-option-none)))
+    (member-type-list-option-some
+     (rcons (make-member-type :name name :type type)
+            members)))
+  :guard-hints (("Goal" :in-theory (enable rcons)))
+  ///
+  (fty::deffixequiv member-type-add-last
+    :hints (("Goal" :in-theory (enable rcons)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (define type-signed-integerp ((type typep))
   :returns (yes/no booleanp)
   :short "Check if a type is a signed integer type [C:6.2.5/4]."
