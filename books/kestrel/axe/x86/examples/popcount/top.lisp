4eeb2a75a203f80807fbf2256df52032ae7de22a
Eric Smith <ewsmith@gmail.com>
2022-05-27 08:19:50 -0700

    [axe] Add top.lisp files for examples.

---
+++ books/kestrel/axe/x86/examples/popcount/top.lisp
@@ -0,0 +1,14 @@
+; Top file for popcount directory
+;
+; Copyright (C) 2022 Kestrel Institute
+;
+; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
+;
+; Author: Eric Smith (eric.smith@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+(include-book "popcount-32-proof")
+(include-book "popcount-64-proof")
