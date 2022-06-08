ad2e6ce774da7678332af2a8ac5b9e590cfeb390
Alessandro Coglio <coglio@kestrel.edu>
2022-06-07 22:37:05 -0700

    [ATC] Add a test.
    This demonstrates DEFSTRUCT's support of array members.

---
+++ books/kestrel/c/atc/tests/structs-with-arrays.lisp
@@ -0,0 +1,24 @@
+; C Library
+;
+; Copyright (C) 2022 Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2022 Kestrel Technology LLC (http://kestreltechnology.com)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+(include-book "kestrel/c/atc/atc" :dir :system :ttags ((:quicklisp) (:quicklisp.osicat) (:oslib) (:open-output-channel!)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Some examples to test just DEFSTRUCT with arrays.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(c::defstruct |scalar_and_array|
+              (|scalar| c::sint)
+              (|aggreg| (c::uchar 10)))
