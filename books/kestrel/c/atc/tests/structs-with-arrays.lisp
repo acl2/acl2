6683858dfb7ab5a214b420952f962ff85f2f8613
Alessandro Coglio <coglio@kestrel.edu>
2022-06-08 22:00:15 -0700

    [ATC] Add some tests.
    These demonstrate the generation of code to read and write elements of member
    arrays of structs.

--- books/kestrel/c/atc/tests/structs-with-arrays.lisp
+++ books/kestrel/c/atc/tests/structs-with-arrays.lisp
@@ -22,3 +22,49 @@
 (c::defstruct |scalar_and_array|
               (|scalar| c::sint)
               (|aggreg| (c::uchar 10)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun |read_scalar| (|a|)
+  (declare (xargs :guard (struct-|scalar_and_array|-p |a|)))
+  (struct-|scalar_and_array|-read-|scalar| |a|))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun |write_scalar| (|v| |a|)
+  (declare (xargs :guard (and (c::sintp |v|)
+                              (struct-|scalar_and_array|-p |a|))))
+  (let ((|a| (struct-|scalar_and_array|-write-|scalar| |v| |a|)))
+    |a|))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun |read_aggreg| (|i| |a|)
+  (declare
+   (xargs
+    :guard (and (c::sintp |i|)
+                (struct-|scalar_and_array|-p |a|)
+                (struct-|scalar_and_array|-|aggreg|-sint-index-okp |i| |a|))))
+  (struct-|scalar_and_array|-read-|aggreg|-sint |i| |a|))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun |write_aggreg| (|i| |v| |a|)
+  (declare
+   (xargs
+    :guard (and (c::sintp |i|)
+                (c::ucharp |v|)
+                (struct-|scalar_and_array|-p |a|)
+                (struct-|scalar_and_array|-|aggreg|-sint-index-okp |i| |a|))))
+  (let ((|a| (struct-|scalar_and_array|-write-|aggreg|-sint |i| |v| |a|)))
+    |a|))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(c::atc |scalar_and_array|
+        |read_scalar|
+        |write_scalar|
+        |read_aggreg|
+        |write_aggreg|
+        :output-file "structs-with-arrays.c"
+        :proofs nil)
