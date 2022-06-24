9ec9dae63b0ff85c167883f80800b5bb53977f2a
Alessandro Coglio <coglio@kestrel.edu>
2022-06-23 18:39:20 -0700

    [ATC] Optimize the symbolic execution.
    Simplify a hypothesis of the generated rules for structure writes.
    
    Adapt the generated proof hints as needed.

--- books/kestrel/c/atc/generation.lisp
+++ books/kestrel/c/atc/generation.lisp
@@ -7068,9 +7068,11 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define atc-gen-tag-member-write-thms ((recognizer symbolp)
+(define atc-gen-tag-member-write-thms ((tag identp)
+                                       (recognizer symbolp)
                                        (fixer-recognizer-thm symbolp)
                                        (not-error-thm symbolp)
+                                       (type-of-value-thm symbolp)
                                        (meminfo defstruct-member-infop)
                                        (names-to-avoid symbol-listp)
                                        (wrld plist-worldp))
@@ -7149,11 +7151,11 @@
                        (valuep ptr)
                        (value-case ptr :pointer)
                        (not (value-pointer-nullp ptr))
+                       (equal (value-pointer->reftype ptr)
+                              (type-struct (ident ,(ident->name tag))))
                        (equal struct
                               (read-object (value-pointer->designator ptr)
                                            compst1))
-                       (equal (value-pointer->reftype ptr)
-                              (type-of-value struct))
                        (,recognizer struct))
                   (equal (exec-expr-asg e compst fenv limit)
                          (write-object (value-pointer->designator ptr)
@@ -7196,7 +7198,8 @@
                    ,writer
                    ,not-error-thm
                    ,recognizer
-                   ,fixer-recognizer-thm)
+                   ,fixer-recognizer-thm
+                   ,type-of-value-thm)
                  :use
                  (:instance ,writer-return-thm
                   (val (b* ((right (expr-binary->arg2 e))
@@ -7229,9 +7232,11 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define atc-gen-tag-member-write-all-thms
-  ((recognizer symbolp)
+  ((tag identp)
+   (recognizer symbolp)
    (fixer-recognizer-thm symbolp)
    (not-error-thm symbolp)
+   (type-of-value-thm symbolp)
    (meminfos defstruct-member-info-listp)
    (names-to-avoid symbol-listp)
    (wrld plist-worldp))
@@ -7250,16 +7255,20 @@
      to be in the same order as @('members')."))
   (b* (((when (endp meminfos)) (mv nil nil names-to-avoid))
        ((mv events thms names-to-avoid)
-        (atc-gen-tag-member-write-thms recognizer
+        (atc-gen-tag-member-write-thms tag
+                                       recognizer
                                        fixer-recognizer-thm
                                        not-error-thm
+                                       type-of-value-thm
                                        (car meminfos)
                                        names-to-avoid
                                        wrld))
        ((mv more-events more-thms names-to-avoid)
-        (atc-gen-tag-member-write-all-thms recognizer
+        (atc-gen-tag-member-write-all-thms tag
+                                           recognizer
                                            fixer-recognizer-thm
                                            not-error-thm
+                                           type-of-value-thm
                                            (cdr meminfos)
                                            names-to-avoid
                                            wrld)))
@@ -7324,6 +7333,7 @@
        (recognizer (defstruct-info->recognizer info))
        (fixer-recognizer-thm (defstruct-info->fixer-recognizer-thm info))
        (not-error-thm (defstruct-info->not-error-thm info))
+       (type-of-value-thm (defstruct-info->type-of-value-thm info))
        (struct-declons (atc-gen-struct-declon-list memtypes))
        ((mv read-thm-events read-thm-names names-to-avoid)
         (if proofs
@@ -7337,9 +7347,11 @@
           (mv nil nil names-to-avoid)))
        ((mv write-thm-events write-thm-names names-to-avoid)
         (if proofs
-            (atc-gen-tag-member-write-all-thms recognizer
+            (atc-gen-tag-member-write-all-thms tag-ident
+                                               recognizer
                                                fixer-recognizer-thm
                                                not-error-thm
+                                               type-of-value-thm
                                                meminfos
                                                names-to-avoid
                                                (w state))
