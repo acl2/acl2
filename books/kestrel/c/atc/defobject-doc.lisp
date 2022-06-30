e7d4c0387aa80ab89097a25d88364ec14de64e03
Alessandro Coglio <coglio@kestrel.edu>
2022-06-29 19:20:46 -0700

    [ATC] Fix inadvertent indentation.

--- books/kestrel/c/atc/defobject-doc.lisp
+++ books/kestrel/c/atc/defobject-doc.lisp
@@ -1,11 +1,11 @@
-                                        ; C Library
-                                        ;
-                                        ; Copyright (C) 2022 Kestrel Institute (http://www.kestrel.edu)
-                                        ; Copyright (C) 2022 Kestrel Technology LLC (http://kestreltechnology.com)
-                                        ;
-                                        ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
-                                        ;
-                                        ; Author: Alessandro Coglio (coglio@kestrel.edu)
+; C Library
+;
+; Copyright (C) 2022 Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2022 Kestrel Technology LLC (http://kestreltechnology.com)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
