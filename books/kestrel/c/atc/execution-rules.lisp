c46531a23ca8ff77c77f04c38d65ea46099b1482
Alessandro Coglio <coglio@kestrel.edu>
2022-08-11 21:43:23 -0700

    [ATC] Improve the C dynamic semantics.
    Reformulate the execution of casts in terms of the much simpler integer
    conversion functions, instead of via a lot of different cases and using the
    conversions from the shallow embedding. Adapt the proofs of the symbolic
    execution rules for executing casts, which before had very simple proofs, with
    more elaborate proof hints needed to bridge the just (intentionally) created gap
    between the deep and shallow embedding definitions.

--- books/kestrel/c/atc/execution-rules.lisp
+++ books/kestrel/c/atc/execution-rules.lisp
@@ -1653,6 +1653,35 @@
 
 (defsection atc-exec-cast-rules-generation
   :short "Code to generate the rules for executing cast operations."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "The hints expand
+     @(tsee exec-cast),
+     @(tsee convert-integer-value),
+     @(tsee value-integer), and
+     @(tsee value-integer->get),
+     which produces something like @('(value-<dst> (value-<src>->get ...))'),
+     where @('<src>') is the source type and @('<dst>') is the destination type,
+     with an intervening @(tsee mod) when the destination type is unsigned.
+     We use the bridge rules to turn those constructor and destructors
+     into the shallowly embedded ones (i.e. @('(<dst> (<src>->get ...))'),
+     which are the ones used in the definitions of
+     the shallowly embedded conversion functions,
+     which we also open (unless source and destination types are equal),
+     along with the @('okp') predicates if applicable.
+     We also need open @('u...-mod') to expose the @(tsee mod)
+     in the shallowly embedded conversions to unsigned type,
+     thus matching the @(tsee mod) in @(tsee convert-integer-value).
+     The executable counterparts of the maximum values of the unsigned types
+     serve to match the quoted integers that result
+     from the executable counterpart of @(tsee integer-type-max)
+     that is used in @(tsee convert-integer-value).
+     We open the @('<dst>-integerp') functions
+     to show that the ACL2 integer is in range,
+     i.e. that @(tsee convert-integer-value) does not return an error;
+     for this, we also need (locally included) rules about @(tsee mod),
+     to show that it is never negative as required for an unsigned range."))
 
   (define atc-exec-cast-rules-gen ((dtype typep) (stype typep))
     :guard (and (type-nonchar-integerp dtype)
@@ -1666,33 +1695,81 @@
          (dtyname (type-to-tyname dtype))
          (dtype-from-stype (pack dfixtype '-from- sfixtype))
          (dtype-from-stype-okp (pack dtype-from-stype '-okp))
+         (guardp (and
+                  (not (equal dtype stype))
+                  (or (type-case dtype :schar)
+                      (and (type-case dtype :sshort)
+                           (not (member-eq (type-kind stype)
+                                           '(:schar))))
+                      (and (type-case dtype :sint)
+                           (not (member-eq (type-kind stype)
+                                           '(:schar :sshort))))
+                      (and (type-case dtype :slong)
+                           (not (member-eq (type-kind stype)
+                                           '(:schar :sshort :sint))))
+                      (and (type-case dtype :sllong)
+                           (not (member-eq (type-kind stype)
+                                           '(:schar :sshort :sint :slong)))))))
          (hyps `(and ,(atc-syntaxp-hyp-for-expr-pure 'x)
                      (,spred x)
-                     ,@(and (not (equal dtype stype))
-                            (or (type-case dtype :schar)
-                                (and (type-case dtype :sshort)
-                                     (not (member-eq (type-kind stype)
-                                                     '(:schar))))
-                                (and (type-case dtype :sint)
-                                     (not (member-eq (type-kind stype)
-                                                     '(:schar :sshort))))
-                                (and (type-case dtype :slong)
-                                     (not (member-eq (type-kind stype)
-                                                     '(:schar :sshort :sint))))
-                                (and (type-case dtype :sllong)
-                                     (not (member-eq (type-kind stype)
-                                                     '(:schar :sshort
-                                                       :sint :slong)))))
-                            `((,dtype-from-stype-okp x)))))
+                     ,@(and guardp `((,dtype-from-stype-okp x)))))
          (rhs (if (equal dtype stype)
                   'x
                 `(,dtype-from-stype x)))
          (formula `(implies ,hyps
                             (equal (exec-cast ',dtyname x)
                                    ,rhs)))
+         (hints `(:enable (exec-cast
+                           convert-integer-value
+                           value-integer
+                           value-integer->get
+                           value-schar-to-schar
+                           value-uchar-to-uchar
+                           value-sshort-to-sshort
+                           value-ushort-to-ushort
+                           value-sint-to-sint
+                           value-uint-to-uint
+                           value-slong-to-slong
+                           value-ulong-to-ulong
+                           value-sllong-to-sllong
+                           value-ullong-to-ullong
+                           value-schar->get-to-schar->get
+                           value-uchar->get-to-uchar->get
+                           value-sshort->get-to-sshort->get
+                           value-ushort->get-to-ushort->get
+                           value-sint->get-to-sint->get
+                           value-uint->get-to-uint->get
+                           value-slong->get-to-slong->get
+                           value-ulong->get-to-ulong->get
+                           value-sllong->get-to-sllong->get
+                           value-ullong->get-to-ullong->get
+                           (:e uchar-max)
+                           (:e ushort-max)
+                           (:e uint-max)
+                           (:e ulong-max)
+                           (:e ullong-max)
+                           uchar-mod
+                           ushort-mod
+                           uint-mod
+                           ulong-mod
+                           ullong-mod
+                           schar-integerp-alt-def
+                           uchar-integerp-alt-def
+                           sshort-integerp-alt-def
+                           ushort-integerp-alt-def
+                           sint-integerp-alt-def
+                           uint-integerp-alt-def
+                           slong-integerp-alt-def
+                           ulong-integerp-alt-def
+                           sllong-integerp-alt-def
+                           ullong-integerp-alt-def
+                           ,@(and (not (equal dtype stype))
+                                  (list dtype-from-stype))
+                           ,@(and guardp
+                                  (list dtype-from-stype-okp)))))
          (event `(defruled ,name
                    ,formula
-                   :enable (exec-cast))))
+                   ,@hints)))
       (mv name event)))
 
   (define atc-exec-cast-rules-gen-loop-stypes ((dtype typep)
@@ -1733,6 +1810,7 @@
       `(progn
          (defsection atc-exec-cast-rules
            :short "Rules for executing casts."
+           (local (include-book "kestrel/arithmetic-light/mod" :dir :system))
            ,@events
            (defval *atc-exec-cast-rules*
              '(,@names)))))))
