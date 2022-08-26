e2dcfcb6c633b27fdffca08ccf4a14d6e5821595
Temel, Mert <mert.temel@intel.com>
2022-08-25 22:05:43 -0500

    [sv] Changed the name of def-svtv-override-fact to def-svtv-generalized-thm

--- books/centaur/sv/svtv/process.lisp
+++ books/centaur/sv/svtv/process.lisp
@@ -2024,7 +2024,7 @@ helps symbolic execution speed, but can cause an error like:</p>
 <h3>Decomposition Proofs</h3>
 
 <p>See @(see svtv-decomposition) and in particular @(see
-def-svtv-override-fact) for the recommended method for doing proofs by
+def-svtv-generalized-thm) for the recommended method for doing proofs by
 decomposition on SVTVs.</p>
 
 ")
