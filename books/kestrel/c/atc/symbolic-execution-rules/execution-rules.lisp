d68bc75eec7b006ac6c1de79adb0a3c36f06a5d5
Alessandro Coglio <coglio@kestrel.edu>
2022-08-26 20:21:45 -0700

    [ATC] Refactor a SYNTAXP-related function.

--- books/kestrel/c/atc/symbolic-execution-rules/execution-rules.lisp
+++ books/kestrel/c/atc/symbolic-execution-rules/execution-rules.lisp
@@ -13,6 +13,7 @@
 
 (include-book "../execution")
 
+(include-book "syntaxp")
 (include-book "arrays")
 (include-book "convert-integer-value")
 
@@ -62,36 +63,6 @@
      when they serve multiple symbolic execution purposes."))
   :order-subtopics t
   :default-parent t)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define atc-syntaxp-hyp-for-expr-pure ((var symbolp))
-  :returns (hyp pseudo-termp :hyp (symbolp var))
-  :short "Construct a @(tsee syntaxp) hypothesis for
-          a symbolic execution rule for pure expressions."
-  :long
-  (xdoc::topstring
-   (xdoc::p
-    "We use these hypotheses to ensure that
-     certain execution subterms are rewritten
-     to their shallow embedding counterparts
-     before their enclosing terms are rewritten.
-     These hypotheses require that the (sub)term in question
-     does not contain any of the execution functions
-     that are expected to be rewritten
-     to their shallow embedding counterparts."))
-  `(syntaxp (or (atom ,var)
-                (not (member-eq ,var '(exec-ident
-                                       exec-const
-                                       exec-iconst
-                                       exec-arrsub
-                                       exec-memberp
-                                       exec-arrsub-of-memberp
-                                       exec-unary
-                                       exec-cast
-                                       exec-binary-strict-pure
-                                       exec-test
-                                       exec-expr-pure))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
