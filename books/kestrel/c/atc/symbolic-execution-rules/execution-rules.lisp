f374ed030c7b91451202595fe87dca74cf94b1a3
Alessandro Coglio <coglio@kestrel.edu>
2022-08-28 00:32:01 -0700

    [ATC] Move some rules to a new file.

--- books/kestrel/c/atc/symbolic-execution-rules/execution-rules.lisp
+++ books/kestrel/c/atc/symbolic-execution-rules/execution-rules.lisp
@@ -67,22 +67,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defsection atc-exec-expr-call-rules
-  :short "Rules for @(tsee exec-expr-call)."
-
-  (defruled exec-expr-call-open
-    (implies (and (not (zp limit))
-                  (equal vals (exec-expr-pure-list args compst))
-                  (value-listp vals))
-             (equal (exec-expr-call fun args compst fenv limit)
-                    (exec-fun fun vals compst fenv (1- limit))))
-    :enable exec-expr-call)
-
-  (defval *atc-exec-expr-call-rules*
-    '(exec-expr-call-open)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 (defsection atc-exec-expr-call-or-pure-rules
   :short "Rules for @(tsee exec-expr-call-or-pure)."
 
--- books/kestrel/c/atc/symbolic-execution-rules/proof-support.lisp
+++ books/kestrel/c/atc/symbolic-execution-rules/proof-support.lisp
@@ -25,6 +25,7 @@
 (include-book "exec-test")
 (include-book "exec-arrsub")
 (include-book "exec-expr-pure")
+(include-book "exec-expr-call")
 (include-book "exec-expr-asg")
 (include-book "uaconvert-values")
 (include-book "static-variable-pointers")
