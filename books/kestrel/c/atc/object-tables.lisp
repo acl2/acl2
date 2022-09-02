ab118617a31e7fee5aa740a7e1bea771739ca946
Alessandro Coglio <coglio@kestrel.edu>
2022-09-02 12:25:15 -0700

    [ATC] Fix XDOC link typo.

--- books/kestrel/c/atc/object-tables.lisp
+++ books/kestrel/c/atc/object-tables.lisp
@@ -17,7 +17,7 @@
 
 (defxdoc+ atc-object-tables
   :parents (atc-event-and-code-generation)
-  :short "Tables of @(tsee defobjects)s, and operations on these tables."
+  :short "Tables of @(tsee defobject)s, and operations on these tables."
   :order-subtopics t
   :default-parent t)
 
