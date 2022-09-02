ba0d70b356569ef671269dd39df5f4501c1ee596
Alessandro Coglio <coglio@kestrel.edu>
2022-09-01 17:53:28 -0700

    [ATC] Remove some unneessary hints in generated proofs.

--- books/kestrel/c/atc/symbolic-execution-rules/exec-arrsub.lisp
+++ books/kestrel/c/atc/symbolic-execution-rules/exec-arrsub.lisp
@@ -47,8 +47,6 @@
           (pack atype-array-read '-alt-def))
          (elemtype-when-apred
           (pack 'value-array->elemtype-when- apred))
-         (value-itype->get-to-itype->get
-          (pack 'value- ifixtype '->get-to- ifixtype '->get))
          (name (pack 'exec-arrsub-when- apred '-and- ipred))
          (formula `(implies
                     (and ,(atc-syntaxp-hyp-for-expr-pure 'x)
@@ -70,7 +68,6 @@
                    ,formula
                    :enable (exec-arrsub
                             ,@*atc-value-integer->get-rules*
-                            ,value-itype->get-to-itype->get
                             ,atype-array-itype-index-okp
                             ,atype-array-read-itype
                             ,atype-array-read-alt-def
