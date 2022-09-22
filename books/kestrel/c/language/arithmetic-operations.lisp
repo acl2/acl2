5a110791d564c58ce19553bd2466537a519dc7b9
Alessandro Coglio <coglio@kestrel.edu>
2022-09-22 15:44:37 -0700

    [C] Add a file and topic for real operations.
    These are operations on C real types.

--- books/kestrel/c/language/arithmetic-operations.lisp
+++ books/kestrel/c/language/arithmetic-operations.lisp
@@ -11,7 +11,7 @@
 
 (in-package "C")
 
-(include-book "integer-operations")
+(include-book "real-operations")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
---
+++ books/kestrel/c/language/real-operations.lisp
@@ -0,0 +1,22 @@
+; C Library
+;
+; Copyright (C) 2022 Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2022 Kestrel Technology LLC (http://kestreltechnology.com)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "C")
+
+(include-book "integer-operations")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defxdoc+ real-operations
+  :parents (language)
+  :short "Operations on C real values."
+  :order-subtopics t
+  :default-parent t)
