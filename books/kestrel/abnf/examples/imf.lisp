185db8cc92247f9cb572e722599446f888be0b14
Alessandro Coglio <coglio@kestrel.edu>
2022-10-01 20:28:51 -0700

    [ABNF] Refactor the examples.
    Put one per file.

---
+++ books/kestrel/abnf/examples/imf.lisp
@@ -0,0 +1,61 @@
+; ABNF (Augmented Backus-Naur Form) Library
+;
+; Copyright (C) 2022 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ABNF")
+
+(include-book "../abstract-syntax")
+(include-book "../semantics")
+(include-book "../operations")
+(include-book "../core-rules")
+(include-book "../concrete-syntax")
+(include-book "../parser")
+(include-book "../abstractor")
+
+; (depends-on "imf-grammar.abnf")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defxdoc+ imf-example
+  :parents (examples)
+  :short "The ABNF grammar of the IMF (Internet Message Format) syntax."
+  :long
+  (xdoc::topstring-p
+   "The IMF syntax is specified by "
+   (xdoc::ahref "https://www.rfc-editor.org/info/rfc5322" "RFC 5322")
+   ".")
+  :order-subtopics t)
+
+(defsection *imf-grammar-rules*
+  :parents (imf-example)
+  :short "The IMF grammar rules from RFC 5322."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "The file @('imf-grammar.abnf') contains the IMF grammar rules,
+     copied and pasted from RFC 5322.
+     The ABNF grammar parser and abstractor are used
+     to build an ACL2 representation of the IMF grammar rules,
+     excluding the referenced ABNF core rules.")
+   (xdoc::p
+    "The IMF grammar rules are well-formed.")
+   (xdoc::p
+    "We use @(tsee add-const-to-untranslate-preprocess)
+     to keep this constant unexpanded in output."))
+
+  (make-event
+   (mv-let (tree state)
+     (parse-grammar-from-file (string-append (cbd) "imf-grammar.abnf") state)
+     (value `(defconst *imf-grammar-rules*
+               (abstract-rulelist ',tree)))))
+
+  (add-const-to-untranslate-preprocess *imf-grammar-rules*)
+
+  (defrule rulelist-wfp-of-*imf-grammar-rules*
+    (rulelist-wfp *imf-grammar-rules*)))
