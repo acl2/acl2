cd4d5bed24fa12be197a900065570f243527fda3
Alessandro Coglio <coglio@kestrel.edu>
2022-10-03 17:05:24 -0700

    [C] Improve flexible array member removal.
    Avoid returning an error if an expected invariant is violated.  For now we "fix"
    the value if the invariant is violated (so to speak), but eventually we plan to
    formalize and prove the invariant.

--- books/kestrel/c/language/flexible-array-member-removal.lisp
+++ books/kestrel/c/language/flexible-array-member-removal.lisp
@@ -26,8 +26,7 @@
     "When a structure with a flexible array member is copied,
      as in most other uses of structures with flexible array members,
      the flexible array member is ignored [C:6.7.2.1/18].
-     This means that, when the structure is copied
-     (in a declaration, assignment, or function call),
+     This means that, when the structure is copied (e.g. in an assignment),
      the flexible array member is dropped.")
    (xdoc::p
     "Here we introduce an ACL2 function to do that.
@@ -51,7 +50,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define remove-flexible-array-member ((val valuep))
-  :returns (new-val value-resultp)
+  :returns (new-val valuep)
   :short "Remove the flexible array member,
           if the value is a structure and has such a member."
   :long
@@ -62,17 +61,28 @@
      which we consult to determine whether the last member should be removed
      (in fact, the flag is part of the model of structure values
      exactly to support this operation in a simple and clear way).
-     If the member is removed,
-     we ensure that there is at least another member
-     in order to maintain the invariant in @(tsee value);
-     this should be always the case,
-     but currently @(tsee value) does not capture the invariant that
-     there are at least two members if the flag is set.
-     If the member is removed, we unset the flag,
-     because the structure no longer has the flexible array member."))
+     Besides removing the member, we clear the flag,
+     because the structure no longer has the flexible array member.
+     It should be an invariant that,
+     if the flag is set, the structure has at least two members:
+     thus, removing a flexible array member never fails,
+     and results in a new structure value
+     that still has at least one member,
+     as required by the invariant captured in @(tsee values).
+     However, our current model of values does not capture
+     the previously mentioned variant,
+     i.e. that if the flag is set there are at least two members.
+     Thus, this ACL2 function may receive an input structure value
+     with the flag set and with just one member;
+     in order to avoid returning errors and maintain the other invariant,
+     in this case we just clear the flag without removing members.
+     This should never happen,
+     and we plan to add this other invariant to @(tsee values),
+     and to remove this special case from this ACL2 function."))
   (if (flexible-array-member-p val)
       (b* ((members (value-struct->members val))
-           ((unless (consp (cdr members))) (error :impossible))
+           ((unless (consp (cdr members)))
+            (change-value-struct val :flexiblep nil))
            (new-members (butlast members 1)))
         (change-value-struct val
                              :members new-members
