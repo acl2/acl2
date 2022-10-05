1b9d78ab72395cf23538d3f725234830a940a456
Alessandro Coglio <coglio@kestrel.edu>
2022-10-04 21:29:50 -0700

    [ABNF] Refactor some operations.
    Move the renaming operations to a separate file.
    
    Factor XDOC parents.
    
    Add semicolon lines for readability.

--- books/kestrel/abnf/operations/top.lisp
+++ books/kestrel/abnf/operations/top.lisp
@@ -15,6 +15,7 @@
 (include-book "in-terminal-set")
 (include-book "ambiguity")
 (include-book "plugging")
+(include-book "renaming")
 
 (include-book "../semantics")
 
@@ -38,131 +39,6 @@
   :parents (abnf)
   :short "Operations on ABNF grammars."
   :order-subtopics t)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defxdoc+ renaming
-  :parents (operations)
-  :short "Renaming of rules in ABNF grammars."
-  :long
-  (xdoc::topstring
-   (xdoc::p
-    "It is sometimes useful to systematically rename a rule in a grammar.")
-   (xdoc::p
-    "For example, the HTTP grammar specified in "
-    (xdoc::ahref "https://www.rfc-editor.org/info/rfc7230" "RFC 7230")
-    " includes a rule @('uri-host') defined by a prose value notation
-     that references the rule @('host') from the URI grammar specified in "
-    (xdoc::ahref "https://www.rfc-editor.org/info/rfc3986" "RFC 3968")
-    ". Prior to @(see plugging) the URI grammar rules
-     into the HTTP grammar rules,
-     the rule @('host') in the URI grammar rules
-     should be renamed to @('uri-host')."))
-  :order-subtopics t)
-
-(defines alt/conc/rep/elem-rename-rule
-  :verify-guards nil ; done below
-
-  (define alternation-rename-rule ((alternation alternationp)
-                                   (oldname rulenamep)
-                                   (newname rulenamep))
-    :returns (new-alternation alternationp)
-    :parents (renaming)
-    :short "Rename all the occurrences of a rule name in an alternation
-            to a new rule name."
-    :long (xdoc::topstring-@def "alternation-rename-rule")
-    (cond ((endp alternation) nil)
-          (t (cons (concatenation-rename-rule (car alternation)
-                                              oldname newname)
-                   (alternation-rename-rule (cdr alternation)
-                                            oldname newname))))
-    :measure (two-nats-measure (alternation-count alternation) 0)
-    :no-function t)
-
-  (define concatenation-rename-rule ((concatenation concatenationp)
-                                     (oldname rulenamep)
-                                     (newname rulenamep))
-    :returns (new-concatenation concatenationp)
-    :parents (renaming)
-    :short "Rename all the occurrences of a rule name in a conatenation
-            to a new rule name."
-    :long (xdoc::topstring-@def "concatenation-rename-rule")
-    (cond ((endp concatenation) nil)
-          (t (cons (repetition-rename-rule (car concatenation)
-                                           oldname newname)
-                   (concatenation-rename-rule (cdr concatenation)
-                                              oldname newname))))
-    :measure (two-nats-measure (concatenation-count concatenation) 0)
-    :no-function t)
-
-  (define repetition-rename-rule ((repetition repetitionp)
-                                  (oldname rulenamep)
-                                  (newname rulenamep))
-    :returns (new-repetition repetitionp)
-    :parents (renaming)
-    :short "Rename all the occurrences of a rule name in a repetition
-            to a new rule name."
-    :long (xdoc::topstring-@def "repetition-rename-rule")
-    (make-repetition :range (repetition->range repetition)
-                     :element (element-rename-rule
-                               (repetition->element repetition)
-                               oldname newname))
-    :measure (two-nats-measure (repetition-count repetition) 1)
-    :no-function t)
-
-  (define element-rename-rule ((element elementp)
-                               (oldname rulenamep)
-                               (newname rulenamep))
-    :returns (new-element elementp)
-    :parents (renaming)
-    :short "Rename all the occurrences of a rule name in an element
-            to a new rule name."
-    :long (xdoc::topstring-@def "element-rename-rule")
-    (element-case element
-                  :rulename (if (equal element.get oldname)
-                                (element-rulename newname)
-                              (element-fix element))
-                  :group (element-group (alternation-rename-rule element.get
-                                                                 oldname
-                                                                 newname))
-                  :option (element-option (alternation-rename-rule element.get
-                                                                   oldname
-                                                                   newname))
-                  :char-val (element-fix element)
-                  :num-val (element-fix element)
-                  :prose-val (element-fix element))
-    :measure (two-nats-measure (element-count element) 0)
-    :no-function t)
-
-  ///
-
-  (verify-guards alternation-rename-rule))
-
-(define rule-rename-rule ((rule rulep) (oldname rulenamep) (newname rulenamep))
-  :returns (new-rule rulep)
-  :parents (renaming)
-  :short "Rename all the occurrences of a rule name to a new rule name,
-          both in the definiens of a rule
-          and in the name of the rule itself."
-  (make-rule :name (if (equal (rule->name rule) oldname)
-                       newname
-                     (rule->name rule))
-             :incremental (rule->incremental rule)
-             :definiens (alternation-rename-rule (rule->definiens rule)
-                                                 oldname newname))
-  :no-function t)
-
-(define rulelist-rename-rule ((rules rulelistp)
-                              (oldname rulenamep)
-                              (newname rulenamep))
-  :returns (new-rules rulelistp)
-  :parents (renaming)
-  :short "Rename all the occurrences of a rule name to a new rule name,
-          in all the rules in a list of rules."
-  (cond ((endp rules) nil)
-        (t (cons (rule-rename-rule (car rules) oldname newname)
-                 (rulelist-rename-rule (cdr rules) oldname newname))))
-  :no-function t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
