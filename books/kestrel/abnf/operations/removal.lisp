63b52c2649ee69c3decdd0bfff6f265ec3e26ec6
Alessandro Coglio <coglio@kestrel.edu>
2022-10-04 21:38:53 -0700

    [ABNF] Refactor some operations.
    Move the removal operations to a separate file.
    
    Factor the XDOC parents.
    
    Add semicolon lines for readability.

---
+++ books/kestrel/abnf/operations/removal.lisp
@@ -0,0 +1,50 @@
+; ABNF (Augmented Backus-Naur Form) Library
+;
+; Copyright (C) 2022 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (coglio@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ABNF")
+
+(include-book "../abstract-syntax")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defxdoc+ removal
+  :parents (operations)
+  :short "Removal of rules in ABNF grammars."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "It is sometimes useful to remove from a grammar
+     all the rules that define certain rule names.")
+   (xdoc::p
+    "For example, the SMTP grammar specified in "
+    (xdoc::ahref "https://www.rfc-editor.org/info/rfc5321" "RFC 5321")
+    " references rules defined in the IMF grammar specified in "
+    (xdoc::ahref "https://www.rfc-editor.org/info/rfc5322" "RFC 5322")
+    ". The IMF rules depend on a rule @('atom'),
+     but the SMTP rules provide their own definition of @('Atom')
+     (recall that rule names are case-insensitive).
+     Thus, before @(see plugging) the IMF rules into the SMTP rules,
+     the removal operation can be used to remove, from the IMF rules,
+     @('atom') and possibly any other rule already defined by SMTP."))
+  :order-subtopics t
+  :default-parent t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define remove-rules-that-define ((rulenames rulename-setp) (rules rulelistp))
+  :returns (new-rules rulelistp)
+  :short "Remove from a list of rules all the ones
+          that define one of the given rule names."
+  (cond ((endp rules) nil)
+        (t (if (in (rule->name (car rules)) rulenames)
+               (remove-rules-that-define rulenames (cdr rules))
+             (cons (rule-fix (car rules))
+                   (remove-rules-that-define rulenames (cdr rules))))))
+  :no-function t)
