70f2a149e31fc728c69711bd0e102160c13fb568
Alessandro Coglio <coglio@kestrel.edu>
2022-10-04 21:54:32 -0700

    [ABNF] Remove now-unnecessary book inclusions.

--- books/kestrel/abnf/operations/top.lisp
+++ books/kestrel/abnf/operations/top.lisp
@@ -19,22 +19,6 @@
 (include-book "removal")
 (include-book "rule-utilities")
 
-(include-book "../semantics")
-
-(include-book "kestrel/std/strings/letter-chars" :dir :system)
-(include-book "kestrel/std/strings/letter-digit-dash-chars" :dir :system)
-(include-book "kestrel/utilities/integers-from-to" :dir :system)
-(include-book "kestrel/utilities/osets" :dir :system)
-(include-book "kestrel/utilities/strings/chars-codes" :dir :system)
-(include-book "kestrel/fty/string-list-result" :dir :system)
-
-(local (include-book "kestrel/utilities/lists/len-const-theorems" :dir :system))
-(local (include-book "kestrel/utilities/oset-theorems" :dir :system))
-(local (include-book "kestrel/utilities/typed-lists/nat-list-fix-theorems" :dir :system))
-(local (include-book "std/basic/inductions" :dir :system))
-(local (include-book "std/lists/top" :dir :system))
-(local (include-book "std/typed-lists/top" :dir :system))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defxdoc+ operations
