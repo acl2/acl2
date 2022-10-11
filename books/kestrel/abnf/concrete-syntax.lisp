62a51b157f15d6786ab2ed923117de5648af3edf
Alessandro Coglio <coglio@kestrel.edu>
2022-10-10 23:35:52 -0700

    [ABNF] Reduce some local book inclusions.

--- books/kestrel/abnf/concrete-syntax.lisp
+++ books/kestrel/abnf/concrete-syntax.lisp
@@ -17,12 +17,6 @@
 (include-book "operations/closure")
 (include-book "operations/in-terminal-set")
 
-(local (include-book "kestrel/utilities/oset-theorems" :dir :system))
-(local (include-book "kestrel/utilities/typed-lists/nat-list-fix-theorems" :dir :system))
-(local (include-book "std/basic/inductions" :dir :system))
-(local (include-book "std/lists/top" :dir :system))
-(local (include-book "std/typed-lists/top" :dir :system))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defxdoc+ concrete-syntax
