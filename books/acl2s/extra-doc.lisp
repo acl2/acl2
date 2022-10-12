d121b8895b3356c957f78798e4bf6df586c9cdda
Drew Walter <mist3rwalter@gmail.com>
2022-10-12 19:38:02 -0400

    [acl2s] Fix issues causing warnings when saving extra-doc
    ACL2s' extra-doc.lisp file had some extraneous <a> tags in it that were
    used as anchors for intra-page links. XDOC emitted warnings when it
    saw these tags, so I removed them here.

--- books/acl2s/extra-doc.lisp
+++ books/acl2s/extra-doc.lisp
@@ -358,7 +358,6 @@ pages: <a href=\"http://support.microsoft.com/kb/827218\">Windows</a>.
 <div class=\"left-center\" data-target=\"faq_section_eclipse\">
 <h3>Eclipse-related</h3>
 
-<a name=\"faq_eclipse\"></a>
 <table class=\"rounded striped\">
 <tr> <td>Q:</td>
      <td><strong>What is Eclipse?</strong></td> </tr>
@@ -371,7 +370,6 @@ for Java.</td></tr>
 </table><br/>
 
 
-<a name=\"faq_eclipse_lingo\"></a>
 <table class=\"rounded striped\">
 <tr> <td>Q:</td>
      <td><strong>Where do I learn about all this Eclipse lingo?</strong></td> </tr>
@@ -382,19 +380,7 @@ Tutorial section of the <em>Workbench User Guide</em></a>.
 </table><br/>
 
 
-<a name=\"faq_eclipse_running\"></a>
-<table class=\"rounded striped\">
-<tr> <td>Q:</td>
-     <td><strong>How do I run Eclipse--with the right options?</strong></td> </tr>
-<tr> <td>A:</td>
-     <td>See the \"Running Eclipse\" section of the 3.5 Release Notes.
-To view details about how a currently-running Eclipse was invoked, go to
-<b>Help</b> | <b>About Eclipse SDK</b> and click \"Configuration Details\".
-</td></tr>
-</table><br/>
 
-
-<a name=\"faq_eclipse_java_version\"></a>
 <table class=\"rounded striped\">
 <tr> <td >Q:</td>
      <td><strong>How do I tell what Java version Eclipse is running under, and if its 64bit?</strong></td> </tr>
@@ -409,7 +395,6 @@ example, \"X86_64\" will indicate that you are running a 64bit Eclipse.
 </table><br/>
 
 
-<a name=\"faq_eclipse_docs\"></a>
 <table class=\"rounded striped\">
 <tr> <td>Q:</td>
      <td><strong>Where is Eclipse documentation?</strong></td> </tr>
@@ -419,7 +404,6 @@ example, \"X86_64\" will indicate that you are running a 64bit Eclipse.
 </table><br/>
 
 
-<a name=\"faq_eclipse_multiuser\"></a>
 <table class=\"rounded striped\">
 <tr> <td >Q:</td>
      <td><strong> Can I do a multi-user install of Eclipse?</strong></td> </tr>
@@ -439,10 +423,9 @@ files, and life will be easier.
 </table>
 </div>
 
-<div class=\"left-center\" data-target=\"faq_section_acl2\">
+<div class=\"left-center\">
   <h3>ACL2-related</h3>
 
-  <a name=\"faq_acl2\"></a>
 <table class=\"rounded striped\">
 <tr> <td>Q:</td>
      <td><strong>What is ACL2?</strong></td> </tr>
@@ -453,7 +436,6 @@ href=\"http://www.cs.utexas.edu/~moore/acl2/\">the ACL2 home page</a> for more
 information.</td></tr>
 </table>
 
-<a name=\"faq_car\"></a>
 <table class=\"rounded striped\">
 <tr> <td>Q:</td>
      <td><strong>What is CAR?</strong></td> </tr>
@@ -472,7 +454,6 @@ ordering information.
 </tr>
 </table>
 
-<a name=\"faq_acl2_book\"></a>
 <table class=\"rounded striped\">
 <tr> <td >Q:</td>
      <td><strong>What is an ACL2 book?</strong></td> </tr>
@@ -485,19 +466,6 @@ href=\"http://www.cs.utexas.edu/users/moore/acl2/v8-0/manual/index.html?topic=AC
 for more information.</td></tr>
 </table><br/>
 
-<a name=\"faq_acl2_image_outside\"></a>
-<table class=\"rounded striped\">
-<tr> <td>Q:</td>
-     <td><strong>Can I use the ACL2 image downloaded by Eclipse outside of Eclipse?</strong></td> </tr>
-<tr> <td>A:</td>
-     <td>Absolutely!  From the ACL2 perspective, it has everything you would
-have building ACL2 yourself. Use the following executable script
-<code>myeclipse/plugins/acl2_image.<em>something</em>/run_acl2</code>
-instead of the usual <code>saved_acl2</code>
-</td></tr>
-</table>
-
-<a name=\"impl_acl2_path\"></a>
 <table class=\"rounded striped\">
 <tr> <td>Q:</td>
 <td><strong>Can I use my own version of ACL2? i.e. Finding ACL2 on user's system:</strong></td>
@@ -526,10 +494,9 @@ we just try executing \"acl2\" and see what happens.
 </table>
 </div>
 
-<div class=\"left-center\" data-target=\"faq_section_java\">
+<div class=\"left-center\">
 <h3>Java-related</h3>
 
-<a name=\"faq_already_java\"></a>
 <table class=\"rounded striped\">
 <tr> <td>Q:</td>
      <td><strong>Do I already have Java?  What version?</strong></td> </tr>
@@ -537,14 +504,10 @@ we just try executing \"acl2\" and see what happens.
      <td>The simple answer is to type <code>java -version</code> at
 your operating system's command prompt/terminal/shell.  You might
 still have Java if the command is rejected.
-See also <a href=\"faq.html#faq_eclipse_running\">How do I run Eclipse--with the
-right options?</a> and <a href=\"faq.html#faq_5.0\">Is there a difference
-between Java SDK 1.5.0 and 5.0?</a> for more info.
 </td></tr>
 </table>
 
 
-<a name=\"faq_sdk\"></a>
 <table class=\"rounded striped\">
 <tr> <td>Q:</td>
      <td><strong>Do I need the Java SDK or is the JRE fine?</strong></td> </tr>
@@ -557,31 +520,6 @@ choice. It is recommended to have separate eclipse installations
 </table><br/>
 
 
-<a name=\"faq_5.0\"></a>
-<table class=\"rounded striped\">
-<tr> <td>Q:</td>
-     <td><strong>Is there a difference between Java SDK 1.5.0 and 5.0?</strong></td> </tr>
-<tr> <td>A:</td>
-     <td>No.  \"5.0\" is the version number adjusted for marketing-hype.  See
-<a href=\"http://java.sun.com/j2se/1.5.0/docs/relnotes/version-5.0.html\">
-\"Version 1.5.0 or 5.0?\"</a> on Sun's site. Similarily, there is no
-difference between Java version \"6.0\" and JDK/JRE 1.6.0 etc etc.
-</td></tr>
-</table><br/>
-
-
-<a name=\"faq_netbeans\"></a>
-<table class=\"rounded striped\">
-<tr> <td>Q:</td>
-     <td><strong>What is Netbeans?</strong></td> </tr>
-<tr> <td>A:</td>
-     <td>Netbeans is a Java development environment that feels much like
-Eclipse but is more Java-specific.  You do not need it.
-</td></tr>
-</table><br/>
-
-
-<a name=\"faq_oldjava\"></a>
 <table class=\"rounded striped\">
 <tr> <td >Q:</td>
      <td><strong>Can I use a version 1.* of Java?</strong></td> </tr>
@@ -598,10 +536,8 @@ in JRE 1.7, ACL2s will not work with it. We recommend the use of JRE
 </table><br/>
 </div>
 
-<div class=\"left-center\" data-target=\"faq_section_acl2s\">
 <h3>ACL2s-related</h3>
 
-<a name=\"faq_restrictions\"></a>
 <table class=\"rounded striped\">
 <tr> <td>Q:</td>
      <td><strong>Why won't ACL2s let me do &lt;blah&gt; in a session?</strong></td> </tr>
@@ -627,7 +563,6 @@ modified for ACL2s</a>.
 </td></tr>
 </table><br/>
 
-<a name=\"faq_acl2s_emacs\"></a>
 <table class=\"rounded striped\">
 <tr> <td>Q:</td>
      <td><strong>Can I use ACL2s extensions to ACL2 in an Emacs development environment?</strong></td> </tr>
@@ -667,7 +602,6 @@ To reproduce other sessions modes, follow the above, but replace acl2s-mode.lsp 
 </table><br/>
 
 
-<a name=\"faq_ccg_emacs\"></a>
 <table class=\"rounded striped\">
 <tr> <td>Q:</td>
      <td><strong>Can I use CCG termination analysis and Counterexample
@@ -691,71 +625,9 @@ To reproduce other sessions modes, follow the above, but replace acl2s-mode.lsp 
 </td></tr>
 </table><br/>
 
-</div>
 
-<div class=\"left-center\" data-target=\"faq_section_installation\">
-  <a name=\"faq_section_installation\"></a>
 <h3>Installation Issues</h3>
-<a name=\"faq_installation_problems\"></a>
-<table class=\"rounded striped\">
-<tr> <td>Q:</td>
-     <td><strong>I am having trouble installing/running ACL2s, what should I do?</strong></td> </tr>
-<tr> <td>A:</td>
-     <td>
-<h5>Case: Standard prepackaged installation</h5>
-<ul class=\"disc\"> 
-
-<li><b><sf>ACL2 appears to be stuck on starting (Anti-virus software is getting in the way):</sf></b> Do whatever is necessary to give eclipse/plugins/acl2_image*/run_acl2.exe appropriate privileges. In particular, if on Avast Anti-virus, go to settings and uncheck the DeepScreen feature.
-</li>
-
-<li><b>Old Linux distros</b> On linux machines whose glibc version is older
-than 2.15 the current prepackaged tarball does not work. Please contact
-the ACL2s maintainer to provide you with a custom ACL2s build.
-</li>
-
-</ul>
-<br/>
-<h5> Case: Non-standard Installation:</h5>
-<ul class=\"disc\">
-
-<li><strong><sf>MacOSX/Linux Permissions:</sf></strong> If you
-installed eclipse on your Mac/Linux machine using a package manager
-that comes with the OS, then you will have trouble installing and
-starting ACL2s due to different permissions. Instead follow the
-eclipse download instructions as described above in the website,
-mainly: Download the Eclipse tar.gz and extract (without using sudo)
-the archive in your home directory
-(e.g. <tt>/home/johnd/tools/eclipse</tt>).</li>
-
-<li><b><sf>Cannot Start ACL2:</sf></b> If ACL2s was installed
-correctly, but when you start an ACL2 session, you get an error, then
-there can be other causes like: 
-<br/><font size=\"2\">
-1. You only installed ACl2s plugin and not the ACL2 image and you forgot to specify the build path in <em>Windows&rarr;Preferences&rarr;ACL2s&rarr;ACL2</em>.
-</font>
-<br/><font size=\"2\">
-2. You are using an older ACL2 version, again check the build path in <em>Windows&rarr;Preferences&rarr;ACL2s&rarr;ACL2</em> and modify it to point to a newer version.
-</font>
-<br/><font size=\"2\">
-3. You are using a workspace on network share path. Switch to a workspace on local path.
-</font>
-<br/><font size=\"2\">
-4. Certain exectuable scripts e.g. run_acl2, dx86cl64, wx86cl64.exe etc in your acl2_image* plugin do not have executable permissions.
-</font>
-
-</li>
-
-<li><strong><sf>Java issues:</sf></strong> We recommend JRE version 1.8 (also known as Java 8.0).
-Either Java is not
-installed (not in PATH) or you are using a version of JRE 1.7 that
-has a
-<a href=\"http://java-performance.info/changes-to-string-java-1-7-0_06/\">bug</a>
-in its string library.</li>
-
-</ul>
-</td></tr>
-</table><br/>
-</div>
+See @(see acl2s-installation-faq) as well as the corresponding @(see acl2s-installation) page for your operating system for more information.
 ")
  
 (defxdoc acl2s-user-guide
