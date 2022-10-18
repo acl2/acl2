cf841c4988e3168309201c722bad49fb4b46899f
Eric Smith <ewsmith@gmail.com>
2022-10-18 15:29:41 -0700

    [utilities] Slightly improve book about acl2-count.

--- books/kestrel/utilities/acl2-count.lisp
+++ books/kestrel/utilities/acl2-count.lisp
@@ -1,7 +1,7 @@
 ; A lightweight book about the built-in function acl2-count
 ;
 ; Copyright (C) 2008-2011 Eric Smith and Stanford University
-; Copyright (C) 2013-2020 Kestrel Institute
+; Copyright (C) 2013-2022 Kestrel Institute
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
 ;
@@ -13,58 +13,42 @@
 
 ;; TODO: Consider disabling acl2-count here.
 
-;todo: use polarities?
-(defthm acl2-count-hack
-  (implies (<= (acl2-count x) y)
-           (< (acl2-count x) (+ 1 y))))
+;; a pretty strong linear rule but requires (consp x).
+(defthm acl2-count-when-consp-linear
+  (implies (consp x)
+           (equal (acl2-count x)
+                  (+ 1 (acl2-count (car x)) (acl2-count (cdr x)))))
+  :rule-classes :linear)
 
-;todo: name and trigger-terms
-(defthm acl2-count-car-bound
+;; These next 4 are weaker but don't assume (consp x):
+
+(defthm <=-of-acl2-count-of-car-and-acl2-count-same
+  (<= (acl2-count (car x)) (acl2-count x)))
+
+(defthm <=-of-acl2-count-of-car-and-acl2-count-same-linear
   (<= (acl2-count (car x)) (acl2-count x))
-  :rule-classes (:rewrite :linear)
-  )
+  :rule-classes ((:linear :trigger-terms ((acl2-count (car x))))))
 
-;todo: name and trigger-terms
-(defthm acl2-count-cdr-bound
+(defthm <=-of-acl2-count-of-cdr-and-acl2-count-same
+  (<= (acl2-count (cdr x)) (acl2-count x)))
+
+(defthm <=-of-acl2-count-of-cdr-and-acl2-count-same-linear
   (<= (acl2-count (cdr x)) (acl2-count x))
-  :rule-classes (:rewrite :linear)
-  )
+  :rule-classes ((:linear :trigger-terms ((acl2-count (cdr x))))))
 
 ;; i think the chaining rules let us handle any car, cadr, caadddadadadar, etc.
 
+;; todo: gen the (acl2-count x)?
 (defthm acl2-count-car-chaining
   (implies (<= (acl2-count y) (acl2-count x))
            (<= (acl2-count (car y)) (acl2-count x)))
   :hints (("Goal" :in-theory (disable acl2-count))))
 
+;; todo: gen the (acl2-count x)?
 (defthm acl2-count-cdr-chaining
   (implies (<= (acl2-count y) (acl2-count x))
            (<= (acl2-count (cdr y)) (acl2-count x)))
   :hints (("Goal" :in-theory (disable acl2-count))))
-
-;; ;todo: trigger-terms
-;; (defthm acl2-count-of-car-when-consp
-;;   (implies (consp x)
-;;            (< (acl2-count (car x))
-;;               (acl2-count x)))
-;;   :rule-classes (:rewrite :linear)
-;;   )
-
-;; ;todo: trigger-terms
-;; (defthm acl2-count-of-cdr-when-consp
-;;   (implies (consp x)
-;;            (< (acl2-count (cdr x))
-;;               (acl2-count x)))
-;;   :rule-classes (:rewrite :linear)
-;;   )
-
-;; a pretty strong linear rule
-(defthm acl2-count-when-consp-linear
-  (implies (consp x)
-           (equal (acl2-count x)
-                  (+ 1 (acl2-count (car x))
-                     (acl2-count (cdr x)))))
-  :rule-classes :linear)
 
 (defthm <-of-0-and-acl2-count-when-consp-linear
   (implies (consp x)
@@ -81,11 +65,13 @@
 
 (defthm <-of-acl2-count-of-nthcdr
   (implies (and (consp lst)
-                (posp n))
-           (< (acl2-count (nthcdr n lst))
-              (acl2-count lst)))
-  :hints (("Goal" :induct (NTHCDR N LST)
-           :in-theory (enable nthcdr))))
+                (integerp n))
+           (equal (< (acl2-count (nthcdr n lst))
+                     (acl2-count lst))
+                  (< 0 n)))
+  :hints (("Goal" :induct (nthcdr n lst) :in-theory (enable nthcdr))
+          ("subgoal *1/1" :cases ((< 0 n)))
+          ("subgoal *1/2" :cases ((< 0 n)))))
 
 (defthm acl2-count-of-cons
   (equal (acl2-count (cons x y))
@@ -127,3 +113,8 @@
            (< (acl2-count (+ -1 bound))
               (acl2-count bound)))
   :hints (("Goal" :in-theory (enable acl2-count))))
+
+;todo: use polarities?
+(defthm acl2-count-hack
+  (implies (<= (acl2-count x) y)
+           (< (acl2-count x) (+ 1 y))))
