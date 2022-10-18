b25b4f6a14ca640b2928ec1c684669856905d159
Alessandro Coglio <coglio@kestrel.edu>
2022-10-17 17:28:54 -0700

    [C] Adapt to changes to ABNF library.

--- books/kestrel/c/language/grammar.lisp
+++ books/kestrel/c/language/grammar.lisp
@@ -11,7 +11,7 @@
 
 (in-package "C")
 
-(include-book "kestrel/abnf/parser" :dir :system)
+(include-book "kestrel/abnf/grammar-parser/executable" :dir :system)
 (include-book "kestrel/abnf/notation/syntax-abstraction" :dir :system)
 (include-book "kestrel/abnf/operations/well-formedness" :dir :system)
 (include-book "kestrel/abnf/operations/closure" :dir :system)
