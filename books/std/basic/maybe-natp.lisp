3ef0d60c20ee27b67b28843d9962094820942e00
Alessandro Coglio <coglio@kestrel.edu>
2022-10-20 17:36:30 -0700

    [Std/basic] Refactor some code.
    Move `maybe-natp` to a separate file, for greater modularity.
    
    Also add an XDOC parent to `pos-fix`.

--- books/std/basic/maybe-natp.lisp
+++ books/std/basic/maybe-natp.lisp
@@ -1,6 +1,5 @@
 ; Std/basic - Basic definitions
-;
-; Copyright (C) 2014 Centaur Technology
+; Copyright (C) 2008-2022 Centaur Technology
 ;
 ; Contact:
 ;   Centaur Technology Formal Verification Group
@@ -27,7 +26,7 @@
 ;   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 ;   DEALINGS IN THE SOFTWARE.
 ;
-; Original author: Sol Swords <sswords@centtech.com>
+; Original author: Jared Davis <jared@centtech.com>
 ; Contributing author: Alessandro Coglio <coglio@kestrel.edu>
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -38,23 +37,23 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defsection pos-fix
-  :parents (posp std/basic)
-  :short "@(call pos-fix) is a fixing function for @(see posp): it is the
-identity for positive integers, or returns 1 for any other object."
+(defsection maybe-natp
+  :parents (natp std/basic)
+  :short "Recognizer for naturals and @('nil')."
+  :long "<p>This is like an <a
+href='https://en.wikipedia.org/wiki/Option_type'>option type</a>; when @('x')
+satisfies @('maybe-natp'), then either it is a natural number or nothing.</p>"
 
-  :long "<p>This has guard @('t').  For better efficiency, see @(see lposfix).</p>"
-
-  (defund pos-fix (x)
+  (defund-inline maybe-natp (x)
     (declare (xargs :guard t))
-    (if (posp x) x 1))
+    (or (not x)
+        (natp x)))
 
-  (local (in-theory (enable pos-fix)))
+  (local (in-theory (enable maybe-natp)))
 
-  (defthm posp-of-pos-fix
-    (posp (pos-fix x))
-    :rule-classes :type-prescription)
-
-  (defthm pos-fix-when-posp
-    (implies (posp x)
-             (equal (pos-fix x) x))))
+  (defthm maybe-natp-compound-recognizer
+    (equal (maybe-natp x)
+           (or (not x)
+               (and (integerp x)
+                    (<= 0 x))))
+    :rule-classes :compound-recognizer))
