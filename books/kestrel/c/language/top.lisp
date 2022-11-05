c5f0277698dafd2bba2e66a50556819394f21ee9
Alessandro Coglio <coglio@kestrel.edu>
2022-11-04 17:02:20 -0700

    [C] Update some documentation.

--- books/kestrel/c/language/top.lisp
+++ books/kestrel/c/language/top.lisp
@@ -38,9 +38,5 @@
 
 (defxdoc+ language
   :parents (c)
-  :short "A formal model of (some aspects of) the C language."
-  :long
-  (xdoc::topstring
-   (xdoc::p
-    "This is work in progress."))
+  :short "A formal model of (a subset of) the C language."
   :order-subtopics t)
