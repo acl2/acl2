a68d29095c26af4754be717cc3527322036b97d4
Eric Smith <ewsmith@gmail.com>
2022-11-07 15:01:04 -0800

    [helpers] Improve replay-book-with-advice.
    Track trivial theorems (no hints needed).  Improve printing.

--- books/kestrel/helpers/replay-book-with-advice.lisp
+++ books/kestrel/helpers/replay-book-with-advice.lisp
@@ -21,7 +21,7 @@
 ;; (replay-book-with-advice "../lists-light" "append")
 
 ;; Determines whether the Proof Advice tool can find advice for DEFTHM.  Either way, this also submits the defthm.
-;; Returns (mv erp result state) where result is :yes, :no, or: maybe.
+;; Returns (mv erp result state) where result is :yes, :no, :maybe, or :trivial.
 (defun submit-defthm-event-with-advice (defthm n print state)
   (declare (xargs :mode :program
                   :guard (and (natp n)
@@ -35,6 +35,7 @@
                          (cadr rule-classes-result)
                        ;; means don't put in any :rule-classes arg at all:
                        '(:rewrite)))
+       (- (cw "(ADVICE: ~x0: " theorem-name))
        ((mv erp successp best-rec state)
         (help::get-and-try-advice-for-theorem theorem-name
                                               theorem-body
@@ -47,21 +48,25 @@
                                               100000 ; step-limit
                                               1      ; max-wins
                                               :all   ; model
+                                              t ; suppress warning about trivial rec, because below we ask if "original" is the best rec and handle trivial recs there
                                               state))
        ((when erp) (mv erp :no state)))
     (if (not successp)
-        (prog2$ (cw "ADVICE: NO for ~x0).~%" theorem-name)
+        (prog2$ (cw "NO).~%") ; close paren matches (ADVICE
                 (b* (((mv erp state) (submit-event-helper-core defthm nil state))
                      ((when erp) (mv erp :no state)))
                   (mv nil :no state)))
       (if (eq :add-hyp (help::successful-recommendationp-type best-rec))
-          (prog2$ (cw "ADVICE: ADD-HYP recommendation for ~x0 but submitting original version.~%" theorem-name)
+          (prog2$ (cw "Maybe: hyp added: ~x0)~%" (help::successful-recommendationp-object best-rec)) ; close paren matches (ADVICE
                   (b* (((mv erp state) (submit-event-helper-core defthm nil state))
                        ((when erp) (mv erp :no state)))
                     (mv nil :maybe state)))
-        (b* ((- (progn$ (cw "ADVICE: YES for ~x0: " theorem-name)
-                        (help::show-successful-recommendation best-rec)
-                        (cw ".~%")))
+        (b* ((trivialp (equal "original" (help::successful-recommendationp-name best-rec)))
+             (- (if trivialp
+                    (cw "TRIVIAL (no hints needed))~%") ; close paren matches (ADVICE
+                  (progn$ (cw "YES: ")
+                          (help::show-successful-recommendation best-rec)
+                          (cw ")~%")))) ; close paren matches (ADVICE
              ((mv erp state)
               ;; We submit the event with the hints found by ML, to ensure it works:
               ;; TODO: Instead, have th advice tool check the req and submit the original event here.
@@ -69,19 +74,19 @@
              ((when erp)
               (er hard? 'submit-defthm-event-with-advice "The discovered advice for ~x0 did not work!" theorem-name)
               (mv :advice-didnt-work :no state)))
-          (mv nil :yes state))))))
+          (mv nil (if trivialp :trivial :yes) state))))))
 
-;Returns (mv erp yes-count no-count maybe-count state).
+;Returns (mv erp yes-count no-count maybe-count trivial-count state).
 ;throws an error if any event fails
 ; This uses :brief printing.
-(defun submit-events-with-advice (events n print yes-count no-count maybe-count state)
+(defun submit-events-with-advice (events n print yes-count no-count maybe-count trivial-count state)
   (declare (xargs :guard (and (true-listp events)
                               (acl2::print-levelp print)
                               (natp n))
                   :mode :program
                   :stobjs state))
   (if (endp events)
-      (mv nil yes-count no-count maybe-count state)
+      (mv nil yes-count no-count maybe-count trivial-count state)
     (let ((event (first events)))
       (if (or (call-of 'defthm event) ; todo: maybe handle thms
               (call-of 'defthmd event))
@@ -89,23 +94,24 @@
                 (submit-defthm-event-with-advice event n print state))
                ((when erp)
                 (er hard? 'submit-events-with-advice "ERROR (~x0) with advice attempt for event ~X12.~%" erp event nil)
-                (mv erp yes-count no-count maybe-count state)))
+                (mv erp yes-count no-count maybe-count trivial-count state)))
             (submit-events-with-advice (rest events) n print
                                        (if (eq :yes result) (+ 1 yes-count) yes-count)
                                        (if (eq :no result) (+ 1 no-count) no-count)
                                        (if (eq :maybe result) (+ 1 maybe-count) maybe-count)
+                                       (if (eq :trivial result) (+ 1 trivial-count) trivial-count)
                                        state))
         ;; Not something for which we will try advice, so submit it and continue:
         (b* (((mv erp state)
               (submit-event-helper-core event print state))
              ((when erp)
               (er hard? 'submit-events-with-advice "ERROR (~x0) with event ~X12.~%" erp event nil)
-              (mv erp yes-count no-count maybe-count state))
+              (mv erp yes-count no-count maybe-count trivial-count state))
              (- (cw "~x0~%" (shorten-event event))))
-          (submit-events-with-advice (rest events) n print yes-count no-count maybe-count state))))))
+          (submit-events-with-advice (rest events) n print yes-count no-count maybe-count trivial-count state))))))
 
 ;; Reads and then submits all the events in FILENAME, trying advice for the theorems.
-;; Returns (mv erp state).
+;; Returns (mv erp event state).
 ;; Example: (replay-book-with-advice "helper.lisp" state)
 (defun replay-book-with-advice-fn (dir      ; no trailing slash
                                    bookname ; no extension
@@ -117,34 +123,38 @@
                               (acl2::print-levelp print))
                   :mode :program ; because this ultimately calls trans-eval-error-triple
                   :stobjs state))
-  (b* ( ;; Read all the forms from the file:
+  (b* ((- (cw "REPLAYING ~s0/~s1 with advice:~%~%" dir bookname))
+       ;; Read all the forms from the file:
        ((mv erp events state)
         (read-objects-from-file (concatenate 'string dir "/" bookname ".lisp") state))
-       ((when erp) (mv erp state))
+       ((when erp) (cw "Error: ~x0.~%" erp) (mv erp nil state))
        ;; Ensure we are working in the same dir as the book:
        ((mv erp & state)
         (set-cbd-fn dir state))
-       ((when erp) (mv erp state))
+       ((when erp) (mv erp nil state))
        ;; Make margins wider for nicer printing:
        (state (widen-margins state))
        ;; Submit all the events, trying advice for each defthm:
-       ((mv erp yes-count no-count maybe-count state)
-        (submit-events-with-advice events n print 0 0 0 state))
-       ((when erp) (mv erp state))
+       ((mv erp yes-count no-count maybe-count trivial-count state)
+        (submit-events-with-advice events n print 0 0 0 0 state))
+       ((when erp)
+        (cw "Error: ~x0.~%" erp)
+        (mv erp nil state))
        ;; Print stats:
-       (- (progn$ (cw "Stats for book ~s1/~s2:~%" dir bookname)
-                  (cw "(Used n=~x0.)" n)
-                  (cw "YES  : ~x0~%" yes-count)
-                  (cw "NO   : ~x0~%" no-count)
-                  (cw "MAYBE: ~x0~%" maybe-count)))
+       (- (progn$ (cw "~%SUMMARY for book ~s0/~s1:~%" dir bookname)
+                  (cw "(Asked each model for ~x0 recommendations.)~%" n)
+                  (cw "YES    : ~x0~%" yes-count)
+                  (cw "NO     : ~x0~%" no-count)
+                  (cw "MAYBE  : ~x0~%" maybe-count)
+                  (cw "TRIVIAL: ~x0~%" trivial-count)))
        ;; Undo margin widening:
        (state (unwiden-margins state)))
     ;; No error:
-    (mv nil state)))
+    (mv nil '(value-triple :invisible) state)))
 
-(defmacro replay-book-with-advice (dir ; no trailing slash
+(defmacro replay-book-with-advice (dir      ; no trailing slash
                                    bookname ; no extension
                                    &key
                                    (n '10)
                                    (print 'nil))
-  `(replay-book-with-advice-fn ,dir ,bookname ,n ,print state))
+  `(make-event-quiet (replay-book-with-advice-fn ,dir ,bookname ,n ,print state)))
