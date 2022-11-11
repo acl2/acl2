eabaa0475107f8a714f46019c633ec7d84b6c891
Eric Smith <ewsmith@gmail.com>
2022-11-10 16:58:50 -0800

    [utilities] Add util to split a path into dir and filename.

---
+++ books/kestrel/utilities/split-path.lisp
@@ -0,0 +1,26 @@
+; A utiliy to split a path into a dir and a filename
+;
+; Copyright (C) 2022 Kestrel Institute
+;
+; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
+;
+; Author: Eric Smith (eric.smith@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+;; See tests in split-path-tests.lisp.
+
+(include-book "kestrel/strings-light/split-string-last" :dir :system)
+
+;; Splits a file path into a directory and filename.
+;; Returns (mv dir filename) where DIR has no trailing slash.
+(defun split-path (path)
+  (declare (xargs :guard (stringp path)))
+  (mv-let (foundp dir filename)
+    (split-string-last path #\/)
+    (if (not foundp)
+        ;; No slash, so the whole thing is the filename, and the directory is ".":
+        (mv "." path)
+      (mv dir filename))))
