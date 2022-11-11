e12aacc8207226351a364a6fc86db746ce01ce9b
Alessandro Coglio <coglio@kestrel.edu>
2022-11-11 12:07:48 -0800

    [Std/util] Add utilities for error-value tuples.
    Some utilities for faciiltating error generation, propagation, and handling.
    Just slight variants of existing ideas, targeted to a statically strongly typed
    coding style.
    
    Preliminary/experimental for now.

--- books/kestrel/std/util/top.lisp
+++ books/kestrel/std/util/top.lisp
@@ -1,6 +1,6 @@
 ; Standard Utilities Library
 ;
-; Copyright (C) 2020 Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2022 Kestrel Institute (http://www.kestrel.edu)
 ;
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 ;
@@ -45,6 +45,8 @@
 
 (include-book "deftutorial")
 (include-book "deftutorial-doc")
+
+(include-book "error-value-tuples")
 
 (include-book "tuple")
 
