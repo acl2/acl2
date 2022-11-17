67f9183efda0b2a07d6929528d5e3973d9832437
Eric Smith <ewsmith@gmail.com>
2022-11-16 23:39:00 -0800

    [array-stobj] Add 2 rules.

--- books/kestrel/utilities/array-stobj.lisp
+++ books/kestrel/utilities/array-stobj.lisp
@@ -10,8 +10,17 @@
 
 (in-package "ACL2")
 
+(local (include-book "defstobj-plus"))
+
 (defstobj array-stobj
   (elems :type (array t (1)) :initially nil :resizable t))
+
+;; Provides many rules:
+(local
+ (defstobj+ array-stobj
+   (elems :type (array t (1)) :initially nil :resizable t)))
+
+;; (in-theory (disable update-elemsi create-array-stobj elems-length)) ; todo
 
 ;; Returns the array-stobj.
 (defun load-list-into-array-stobj-aux (list i len array-stobj)
@@ -28,6 +37,17 @@
     (let ((array-stobj (update-elemsi i (first list) array-stobj)))
       (load-list-into-array-stobj-aux (rest list) (+ 1 i) len array-stobj))))
 
+(defthm elems-length-of-load-list-into-array-stobj-aux
+  (implies (and ;(array-stobjp array-stobj)
+            ;(true-listp list)
+            (natp i)
+            (natp len)
+            (equal len (elems-length array-stobj)))
+           (equal (elems-length (load-list-into-array-stobj-aux list i len array-stobj))
+                  (elems-length array-stobj)))
+  :hints (("Goal" :induct t
+           :in-theory (enable load-list-into-array-stobj-aux))))
+
 ;; Returns the array-stobj.
 (defund load-list-into-array-stobj (list array-stobj)
   (declare (xargs :guard (true-listp list)
@@ -35,6 +55,11 @@
   (let* ((len (len list))
          (array-stobj (resize-elems len array-stobj)))
     (load-list-into-array-stobj-aux list 0 len array-stobj)))
+
+(defthm elems-length-of-load-list-into-array-stobj
+  (equal (elems-length (load-list-into-array-stobj list array-stobj))
+         (len list))
+  :hints (("Goal" :in-theory (enable load-list-into-array-stobj))))
 
 ;; Returns the list.
 (defun extract-list-from-array-stobj-aux (i acc array-stobj)
