5f318bac4700e89595fe091c0caf5234c6ead7c8
Eric Smith <ewsmith@gmail.com>
2022-11-28 09:37:51 -0800

    [terms-light] Fix serialize-lambdas-in-term.
    Ensure lambdas are closed.  Also prove this fact.

--- books/kestrel/terms-light/serialize-lambdas-in-term-tests.lisp
+++ books/kestrel/terms-light/serialize-lambdas-in-term-tests.lisp
@@ -1,6 +1,6 @@
 ; Tests of serialize-lambdas.lisp
 ;
-; Copyright (C) 2021 Kestrel Institute
+; Copyright (C) 2021-2022 Kestrel Institute
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
 ;
@@ -13,10 +13,28 @@
 (include-book "serialize-lambdas-in-term")
 (include-book "std/testing/assert-equal" :dir :system)
 
+;;; :trans (let ((x (+ 1 x)) (y (+ 2 y))) (< x y))
 (assert-equal
+ (serialize-lambdas-in-term '((lambda (x y) (< x y))
+                              (binary-+ '1 x)
+                              (binary-+ '2 y))
+                            nil)
+ '((lambda (x y)
+     ((lambda (y x) (< x y))
+      (binary-+ '2 y)
+      x))
+   (binary-+ '1 x)
+   y))
+
+;; An example where we have to introduce a temporary:
+(assert-equal
+ ;; swaps a and b:
  (serialize-lambda-application '((lambda (a b) (foo a b)) b a) nil)
- '((lambda (a-temp)
-     ((lambda (a)
-        ((lambda (b) (foo a b)) a-temp))
-      b))
-   a))
+ ;; TODO: Generate something better?
+ '((lambda (a-temp b)
+     ((lambda (a a-temp)
+        ((lambda (b a) (foo a b)) a-temp a))
+      b
+      a-temp))
+   a
+   b))
