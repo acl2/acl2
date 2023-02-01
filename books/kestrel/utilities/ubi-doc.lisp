70ddbd2698417bbf0123acaed8b1a060842c3e78
Eric McCarthy <mccarthy@kestrel.edu>
2023-01-31 23:39:25 -0800

    [defxdoc] change ubi example

--- books/kestrel/utilities/ubi-doc.lisp
+++ books/kestrel/utilities/ubi-doc.lisp
@@ -8,7 +8,7 @@
 
 (defxdoc ubi
   :parents (kestrel-utilities history)
-  :code-source "kestrel/utilities/ubi :DIR :SYSTEM"
+  :code-source "ubi.lisp"
   :short "Undo back up to longest initial segment containing only calls of
  certain symbols, including @(tsee defpkg) and @(tsee include-book)."
   :long "<p>The following example explains how @(':ubi') works.  We start up
