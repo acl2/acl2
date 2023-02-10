b9f20b345e3782c633af0c3f7bbb500d95c48841
Eric Smith <ewsmith@gmail.com>
2023-02-10 02:48:19 -0800

    [untranslated-terms] Factor out untranslated-constantp.

--- books/kestrel/untranslated-terms-old/untranslated-terms.lisp
+++ books/kestrel/untranslated-terms-old/untranslated-terms.lisp
@@ -28,11 +28,11 @@
 
 ;; TODO: Replace more of the hand-written functions with calls to def-untranslated-term-fold
 
+(include-book "kestrel/untranslated-terms/untranslated-constantp" :dir :system)
 (include-book "kestrel/alists-light/lookup-eq" :dir :system)
 (include-book "kestrel/utilities/terms" :dir :system)
 (include-book "kestrel/utilities/map-symbol-name" :dir :system)
 (include-book "kestrel/utilities/legal-variable-listp" :dir :system)
-(include-book "kestrel/utilities/quote" :dir :system)
 (include-book "kestrel/utilities/lets" :dir :system)
 (include-book "kestrel/utilities/lambdas" :dir :system)
 (include-book "kestrel/utilities/doublets2" :dir :system)
@@ -253,52 +253,6 @@
                   (legal-variable-name-in-acl2-packagep str)))
   :hints (("Goal" :in-theory (enable untranslated-variablep))))
 
-;;;
-;;; untranslated-constantp
-;;;
-
-;; Recognize an untranslated term that is a constant
-(defund untranslated-constantp (x)
-  (declare (xargs :guard t))
-  (or (acl2-numberp x) ;unquoted
-      (characterp x) ;unquoted
-      (stringp x) ;unquoted
-      (and (symbolp x)
-           (or (keywordp x) ;; unquoted keywords are constants
-               ;; t, nil, and symbols that begin and end with * are constants:
-               ;; TODO: Consider disallowing *
-               (legal-constantp1 x)))
-      (myquotep x)))
-
-;; (defthm car-when-untranslated-constantp
-;;   (implies (untranslated-constantp x)
-;;            (equal (car x)
-;;                   (if (consp x)
-;;                       'quote
-;;                     nil)))
-;;   :rule-classes ((:rewrite :backchain-limit-lst (0)))
-;;   :hints (("Goal" :in-theory (enable untranslated-constantp))))
-
-(defthm untranslated-constantp-forward-to-true-listp
-  (implies (and (untranslated-constantp term)
-                (consp term))
-           (true-listp term))
-  :rule-classes :forward-chaining
-  :hints (("Goal" :in-theory (enable untranslated-constantp))))
-
-(defthm untranslated-constantp-forward-to-equal-of-car-and-quote
-  (implies (and (untranslated-constantp term)
-                (consp term))
-           (equal (car term) 'quote))
-  :rule-classes :forward-chaining
-  :hints (("Goal" :in-theory (enable untranslated-constantp))))
-
-(defthm untranslated-constantp-of-cons
-  (equal (untranslated-constantp (cons x y))
-         (and (equal x 'quote)
-              (true-listp y)
-              (equal 1 (len y))))
-  :hints (("Goal" :in-theory (enable untranslated-constantp))))
 
 ;;these are terms that may contain let/let*/b*/cond/case-match/case/mv-let, etc.
 ;ttodo: add support for case, mv-let, and more constructs.
