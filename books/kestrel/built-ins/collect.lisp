b2cf126aa9768aab2cd3fb2b2b9ef386abdc9f48
Matt Kaufmann <matthew.j.kaufmann@gmail.com>
2023-02-11 22:40:32 -0600

    Avoid n assertion for certain "acl2data" runs.

--- books/kestrel/built-ins/collect.lisp
+++ books/kestrel/built-ins/collect.lisp
@@ -199,6 +199,8 @@
 ; We add assertions to this effect, so that if ACL2 ever changes
 ; in a way that these lists are no longer empty, we can detect it.
 
+; Matt K. mod: The following assertion is false for certain "acl2data" runs.
+#-acl2data
 (assert-event (and (null *builtin-defstobj-names*)
                    (null *builtin-includebook-names*)))
 
