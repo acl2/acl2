521e442cdd38a9c7203cd47668e051a33d1a9f45
Alessandro Coglio <coglio@kestrel.edu>
2023-02-17 12:45:09 -0800

    [C deep] Fix typo in XDOC parents.
    This was causing a duplicate parent.

--- books/kestrel/c/language/integer-ranges.lisp
+++ books/kestrel/c/language/integer-ranges.lisp
@@ -707,7 +707,7 @@
   :enable (slong-min sint-min int-bits-vs-long-bits))
 
 (defrule sllong-min-<=-slong-min
-  :parents (slong-min slong-min)
+  :parents (sllong-min slong-min)
   :short "General relation between
           @('signed long long') and @('signed long') minima."
   (<= (sllong-min) (slong-min))
