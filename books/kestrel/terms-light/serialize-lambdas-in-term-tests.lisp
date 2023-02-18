1d1fc26db73e0a898baa32374f1453eaa87d5929
Eric Smith <ewsmith@gmail.com>
2023-02-17 17:30:22 -0800

    [terms-light] Clarify some tests.

--- books/kestrel/terms-light/serialize-lambdas-in-term-tests.lisp
+++ books/kestrel/terms-light/serialize-lambdas-in-term-tests.lisp
@@ -13,12 +13,15 @@
 (include-book "serialize-lambdas-in-term")
 (include-book "std/testing/assert-equal" :dir :system)
 
-;;; :trans (let ((x (+ 1 x)) (y (+ 2 y))) (< x y))
+;; A simple test.  The lambda binds (non-trivially) both X and Y, so
+;; serialize-lambdas-in-term splits it into two separate lambdas.
 (assert-equal
+ ;; :trans (let ((x (+ 1 x)) (y (+ 2 y))) (< x y))
  (serialize-lambdas-in-term '((lambda (x y) (< x y))
                               (binary-+ '1 x)
                               (binary-+ '2 y))
                             nil)
+ ;; :trans (let ((x (+ 1 x))) (let ((y (+ 2 y))) (< x y)))
  '((lambda (x y)
      ((lambda (y x) (< x y))
       (binary-+ '2 y)
@@ -29,11 +32,13 @@
 ;; An example where we have to introduce a temporary:
 (assert-equal
  ;; swaps a and b:
- (serialize-lambda-application '((lambda (a b) (foo a b)) b a) nil)
+ ;; :trans (let ((a b) (b a)) (< a b))
+ (serialize-lambda-application '((lambda (a b) (< a b)) b a) nil)
  ;; TODO: Generate something better?
+ ;; :trans (let ((a-temp a)) (let ((a b)) (let ((b a-temp)) (< a b))))
  '((lambda (a-temp b)
      ((lambda (a a-temp)
-        ((lambda (b a) (foo a b)) a-temp a))
+        ((lambda (b a) (< a b)) a-temp a))
       b
       a-temp))
    a
