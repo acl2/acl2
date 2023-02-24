e0c8dc15cede0cf13a6676b443e8cec8e81e975a
Eric Smith <ewsmith@gmail.com>
2023-02-23 16:37:25 -0800

    [lists-light] Add 2 rules about make-list-ac.

--- books/kestrel/lists-light/make-list-ac.lisp
+++ books/kestrel/lists-light/make-list-ac.lisp
@@ -1,6 +1,6 @@
 ; A lightweight book about the built-in function make-list-ac
 ;
-; Copyright (C) 2022 Kestrel Institute
+; Copyright (C) 2022-2023 Kestrel Institute
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
 ;
@@ -15,4 +15,17 @@
 (defthm len-of-make-list-ac
   (equal (len (make-list-ac n val acc))
          (+ (nfix n) (len acc)))
+  :hints (("Goal" :in-theory (enable make-list-ac))))
+
+(defthm true-listp-of-make-list-ac-type
+  (implies (true-listp acc)
+           (true-listp (make-list-ac n val acc)))
+  :rule-classes :type-prescription
+  :hints (("Goal" :in-theory (enable make-list-ac))))
+
+;; Or this could go in typed-lists-light.
+(defthm character-listp-of-make-list-ac
+  (implies (and (character-listp acc)
+                (characterp val))
+           (character-listp (make-list-ac n val acc)))
   :hints (("Goal" :in-theory (enable make-list-ac))))
