55c7ff351fb30549c79be082057a0e65dbe230f4
Alessandro Coglio <coglio@kestrel.edu>
2023-02-27 16:38:38 -0800

    [FTY] Tweak file header layout.

--- books/kestrel/fty/fty-omap.lisp
+++ books/kestrel/fty/fty-omap.lisp
@@ -1,10 +1,11 @@
 ; FTY Library
 ;
-; Copyright (C) 2020 Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2023 Kestrel Institute (http://www.kestrel.edu)
 ;
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 ;
-; Author: Stephen Westfold (westfold@kestrel.edu), Alessandro Coglio (coglio@kestrel.edu)
+; Authors: Stephen Westfold (westfold@kestrel.edu)
+;          Alessandro Coglio (coglio@kestrel.edu)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
