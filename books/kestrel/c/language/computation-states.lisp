cacef6cda6d4be439ef09a8f858500a9a4aafca8
Alessandro Coglio <coglio@kestrel.edu>
2023-03-06 16:36:12 -0800

    [C deep] Move a theorem.
    This more naturally belongs with the function that it expresses a property of.
    Also, we need to use this theorem in an upcoming extension.

--- books/kestrel/c/language/computation-states.lisp
+++ books/kestrel/c/language/computation-states.lisp
@@ -925,7 +925,15 @@
        :enable (objdesign-of-var-aux
                 len
                 fix
-                nth-of-minus1-and-cdr)))))
+                nth-of-minus1-and-cdr))
+
+     (defruled objdesign-of-var-aux-iff-read-auto-var-aux
+       (iff (objdesign-of-var-aux var frame scopes)
+            (read-auto-var-aux var scopes))
+       :induct t
+       :enable (objdesign-of-var-aux
+                read-auto-var-aux
+                cdr-of-in-when-scopep)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -1214,14 +1222,6 @@
      ((:instance objdesign-of-var-aux-lemma
                  (frame (+ -1 (len (compustate->frames compst))))
                  (scopes (frame->scopes (car (compustate->frames compst)))))))
-
-   (defruled objdesign-of-var-aux-iff-read-auto-var-aux
-     (iff (objdesign-of-var-aux var frame scopes)
-          (read-auto-var-aux var scopes))
-     :induct t
-     :enable (objdesign-of-var-aux
-              read-auto-var-aux
-              cdr-of-in-when-scopep))
 
    (defruled read-var-to-read-object-when-static
      (b* ((objdes (objdesign-of-var var compst))
