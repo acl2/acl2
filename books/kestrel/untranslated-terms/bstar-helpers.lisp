d0934f3e819d23c1253c3b2318a2bd8ec20fee5b
Eric Smith <ewsmith@gmail.com>
2023-03-17 00:17:03 -0700

    [untranslated-terms] Add support for the & b* binder.
    Also improve/strengthen checks and comments.

--- books/kestrel/untranslated-terms/bstar-helpers.lisp
+++ books/kestrel/untranslated-terms/bstar-helpers.lisp
@@ -14,8 +14,10 @@
 
 (include-book "kestrel/utilities/forms" :dir :system) ; for farg1, etc.
 
-;; Recognize a supported b* binding, of the form (<binder> ...<expressions>...)
-;; where the number of allowed expressions depends on the binder.
+;; Recognizes a "supported" b* binding, of the form (<binder> ...<expressions>...)
+;; where the number of allowed expressions depends on the binder.  Tools should
+;; know how to properly handle the supprted bindings.
+;; NOTE: Keep this in sync with the functions below
 (defun supported-b*-bindingp (binding)
   (declare (xargs :guard t))
   (and (true-listp binding)
@@ -24,21 +26,25 @@
              (expressions (rest binding)))
          (if (atom binder)
              ;; A few binders are atoms:
-             (or (eq binder '-) ; (- <term> ... <term>) for any number of terms
-                 ;; todo: uncomment once supported everywhere:
-                 ;; (eq binder '&) ; (& <term> ... <term>) for any number of terms
-                 (and (symbolp binder) ; (<var> <term>)
+             (or (eq binder '-)        ; (- <term>*), with any number of terms
+                 (eq binder '&)        ; (& <term>*), with any number of terms
+                 (and (symbolp binder)
                       (= 1 (len expressions))))
            ;; Most binders are conses:
-           (case (car binder)
-             ;; todo must be at least 1 expression:
-             ((when if unless) (= 1 (len (fargs binder)))) ; ((when <term>) <term> ... <term>)
-             (mv (and (symbol-listp (fargs binder)) ; ((mv <var> ... <var>) <term>)
-                      (= 1 (len expressions))))
-             ;; todo: add more kinds of supported binder
-             (otherwise nil))))))
+           (and (true-listp binder)
+                (case (car binder)
+                  ;; ((when/if/unless <term>) <term>+):
+                  ((when if unless) (and (= 1 (len (fargs binder))) ; todo: check these restrictions
+                                         (<= 1 (len expressions))))
+                  ;; ((mv <var> ... <var>) <term>):
+                  (mv (and (symbol-listp (fargs binder))
+                           (<= 2 (len (fargs binder))) ; must be at least 2 vars
+                           (= 1 (len expressions)) ; must be exactly 1 expression
+                           ))
+                  ;; todo: add more kinds of supported binder
+                  (otherwise nil)))))))
 
-;; Recognize a list of supported b* bindings
+;; Recognizes a list of supported b* bindings
 (defun supported-b*-bindingsp (bindings)
   (declare (xargs :guard t))
   (if (atom bindings)
@@ -46,18 +52,26 @@
     (and (supported-b*-bindingp (first bindings))
          (supported-b*-bindingsp (rest bindings)))))
 
-;; Extracts all the terms in the b* binding, in order
+;; Extracts all the terms in the b* binding, in order.  One can process the
+;; terms and then call recreate-b*-binding to build a new binding.
 (defund extract-terms-from-b*-binding (binding)
   (declare (xargs :guard (supported-b*-bindingp binding)))
-  (let ((binder (first binding)))
+  (let ((binder (first binding))
+        (expressions (rest binding)))
     (if (symbolp binder)
-        (if (eq binder '-) ; (- <term> ... <term>) for any number of terms
-            (fargs binding)
-          ;; it's a variable:
-          (list (farg1 binding)))
+        (if (or (eq binder '-)
+                (eq binder '&))
+            ;; (- <term>*) or (& <term>*):
+            expressions
+          ;; (<var> <term>):
+          expressions ; must only be one
+          )
       (case (car binder)
-        ((when if unless) (cons (farg1 binder) (fargs binding)))
-        (mv (list (farg1 binding)))
+        ;; ((when/if/unless <term>) <term>+):
+        ((when if unless) (cons (farg1 binder) expressions))
+        ;; ((mv <var> ... <var>) <term>)
+        (mv expressions ; must only be one
+            )
         ;; Should never happen:
         (t (er hard 'extract-terms-from-b*-binding "Unsupported b* binder: ~x0." binding))))))
 
@@ -66,7 +80,7 @@
            (true-listp (extract-terms-from-b*-binding binding)))
   :hints (("Goal" :in-theory (enable extract-terms-from-b*-binding))))
 
-;; Extracts all the terms in the b* bindings, in order
+;; Extracts all the terms in the b* bindings, in order.
 (defun extract-terms-from-b*-bindings (bindings)
   (declare (xargs :guard (supported-b*-bindingsp bindings)))
   (if (endp bindings)
@@ -74,37 +88,46 @@
     (append (extract-terms-from-b*-binding (first bindings))
             (extract-terms-from-b*-bindings (rest bindings)))))
 
-;; Returns (mv new-binding rest-new-terms).
+;; Recreates the BINDING by replacing the terms it contains with the NEW-TERMS,
+;; in order.  Returns (mv new-binding rest-new-terms).
 (defun recreate-b*-binding (binding new-terms)
   (declare (xargs :guard (and (supported-b*-bindingp binding)
                               (true-listp new-terms))))
-  (let* ((binder (first binding)))
+  (let* ((binder (first binding))
+         (expressions (rest binding)))
     (if (symbolp binder)
-        (if (eq binder '-) ; (- <term> ... <term>) for any number of terms
-            (let ((num-terms (len (fargs binding))))
-              (mv `(,binder ,@(take num-terms new-terms))
-                  (nthcdr num-terms new-terms)))
-          ;; it's a variable:
-          (mv `(,binder ,(first new-terms)) ; (<var> <term)
+        (if (or (eq binder '-)
+                (eq binder '&))
+            ;; (- <term>*) or (& <term>*):
+            (let ((num-exprs (len expressions)))
+              (mv `(,binder ,@(take num-exprs new-terms))
+                  (nthcdr num-exprs new-terms)))
+          ;; (<var> <term>):
+          (mv `(,binder ,(first new-terms))
               (rest new-terms)))
       (case (car binder)
-        ((when if unless) ; ((when <term>) <term> ... <term>)
-         (let ((num-args (len (fargs binding))))
-           (mv `((,(car binder) ,(first new-terms)) ,@(take num-args (rest new-terms)))
-               (nthcdr (+ 1 num-args) new-terms))))
-        (mv ; ((mv <var> ... <var>) <term>)
-         (mv `((mv ,@(fargs binder)) ,(first new-terms))
-             (rest new-terms)))
+        ((when if unless)
+         ;; ((when/if/unless <term>) <term>+):
+         (let ((num-exprs (len expressions)))
+           (mv `((,(car binder) ,(first new-terms)) ,@(take num-exprs (rest new-terms)))
+               (nthcdr (+ 1 num-exprs) new-terms))))
+        ;; ((mv <var> ... <var>) <term>):
+        (mv (mv `((mv ,@(fargs binder)) ,(first new-terms))
+                (rest new-terms)))
         ;; Should never happen:
         (otherwise (progn$ (er hard 'recreate-b*-binding "Unsupported b* binder: ~x0." binding)
                            (mv nil nil)))))))
 
+;; Recreates the BINDINGS by replacing the terms they contain with the
+;; NEW-TERMS, in order.
 (defun recreate-b*-bindings (bindings new-terms)
   (declare (xargs :guard (and (supported-b*-bindingsp bindings)
                               (true-listp new-terms))))
   (if (endp bindings)
-      nil
-    (mv-let (new-first new-terms)
+      (if (not (endp new-terms))
+          (er hard? 'recreate-b*-bindings "Extra new terms: ~x0." new-terms)
+        nil)
+    (mv-let (new-binding new-terms)
       (recreate-b*-binding (first bindings) new-terms)
-      (let ((new-rest (recreate-b*-bindings (rest bindings) new-terms)))
-        (cons new-first new-rest)))))
+      (let ((new-bindings (recreate-b*-bindings (rest bindings) new-terms)))
+        (cons new-binding new-bindings)))))
