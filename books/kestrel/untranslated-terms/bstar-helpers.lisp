e91c0f5c26eecd571e9da463809637eefd1e3d4d
Eric Smith <ewsmith@gmail.com>
2023-03-17 11:54:04 -0700

    [untranslated-terms] Add some rules about the b* helpers.

--- books/kestrel/untranslated-terms/bstar-helpers.lisp
+++ books/kestrel/untranslated-terms/bstar-helpers.lisp
@@ -13,12 +13,16 @@
 ;; See tests of b* in /data/ewsmithb/acl2/books/kestrel/tests/bstar-tests.lisp.
 
 (include-book "kestrel/utilities/forms" :dir :system) ; for farg1, etc.
+(local (include-book "kestrel/lists-light/take" :dir :system))
+(local (include-book "kestrel/lists-light/append" :dir :system))
+
+(local (in-theory (disable mv-nth)))
 
 ;; Recognizes a "supported" b* binding, of the form (<binder> ...<expressions>...)
 ;; where the number of allowed expressions depends on the binder.  Tools should
 ;; know how to properly handle the supprted bindings.
 ;; NOTE: Keep this in sync with the functions below
-(defun supported-b*-bindingp (binding)
+(defund supported-b*-bindingp (binding)
   (declare (xargs :guard t))
   (and (true-listp binding)
        (consp binding)
@@ -28,7 +32,7 @@
              ;; A few binders are atoms:
              (or (eq binder '-)        ; (- <term>*), with any number of terms
                  (eq binder '&)        ; (& <term>*), with any number of terms
-                 (and (symbolp binder)
+                 (and (symbolp binder) ; todo: check for legal variable name?
                       (= 1 (len expressions))))
            ;; Most binders are conses:
            (and (true-listp binder)
@@ -61,17 +65,20 @@
                   (otherwise nil)))))))
 
 ;; Recognizes a list of supported b* bindings
-(defun supported-b*-bindingsp (bindings)
+(defund supported-b*-bindingsp (bindings)
   (declare (xargs :guard t))
   (if (atom bindings)
       (null bindings)
     (and (supported-b*-bindingp (first bindings))
          (supported-b*-bindingsp (rest bindings)))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Extracts all the terms in the b* binding, in order.  One can process the
 ;; terms and then call recreate-b*-binding to build a new binding.
 (defund extract-terms-from-b*-binding (binding)
-  (declare (xargs :guard (supported-b*-bindingp binding)))
+  (declare (xargs :guard (supported-b*-bindingp binding)
+                  :guard-hints (("Goal" :in-theory (enable supported-b*-bindingp)))))
   (let ((binder (first binding))
         (expressions (rest binding)))
     (if (symbolp binder)
@@ -102,21 +109,37 @@
 (defthm true-listp-of-extract-terms-from-b*-binding
   (implies (supported-b*-bindingp binding)
            (true-listp (extract-terms-from-b*-binding binding)))
+  :hints (("Goal" :in-theory (enable supported-b*-bindingp extract-terms-from-b*-binding))))
+
+(defthm <=-of-acl2-count-of-extract-terms-from-b*-binding-linear
+  (<= (acl2-count (extract-terms-from-b*-binding binding))
+      (acl2-count binding))
+  :rule-classes :linear
   :hints (("Goal" :in-theory (enable extract-terms-from-b*-binding))))
 
 ;; Extracts all the terms in the b* bindings, in order.
-(defun extract-terms-from-b*-bindings (bindings)
-  (declare (xargs :guard (supported-b*-bindingsp bindings)))
+(defund extract-terms-from-b*-bindings (bindings)
+  (declare (xargs :guard (supported-b*-bindingsp bindings)
+                  :guard-hints (("Goal" :in-theory (enable supported-b*-bindingsp)))))
   (if (endp bindings)
       nil
     (append (extract-terms-from-b*-binding (first bindings))
             (extract-terms-from-b*-bindings (rest bindings)))))
 
+(defthm <=-of-acl2-count-of-extract-terms-from-b*-bindings-linear
+  (<= (acl2-count (extract-terms-from-b*-bindings bindings))
+      (acl2-count bindings))
+  :rule-classes :linear
+  :hints (("Goal" :in-theory (enable extract-terms-from-b*-bindings))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Recreates the BINDING by replacing the terms it contains with the NEW-TERMS,
 ;; in order.  Returns (mv new-binding rest-new-terms).
-(defun recreate-b*-binding (binding new-terms)
+(defund recreate-b*-binding (binding new-terms)
   (declare (xargs :guard (and (supported-b*-bindingp binding)
-                              (true-listp new-terms))))
+                              (true-listp new-terms))
+                  :guard-hints (("Goal" :in-theory (enable supported-b*-bindingp)))))
   (let* ((binder (first binding))
          (expressions (rest binding)))
     (if (symbolp binder)
@@ -153,11 +176,18 @@
         (otherwise (progn$ (er hard 'recreate-b*-binding "Unsupported b* binder: ~x0." binding)
                            (mv nil nil)))))))
 
+(local
+ (defthm true-listp-of-mv-nth-1-of-recreate-b*-binding
+   (implies (true-listp new-terms)
+            (true-listp (mv-nth 1 (recreate-b*-binding binding new-terms))))
+   :hints (("Goal" :in-theory (enable recreate-b*-binding)))))
+
 ;; Recreates the BINDINGS by replacing the terms they contain with the
 ;; NEW-TERMS, in order.
-(defun recreate-b*-bindings (bindings new-terms)
+(defund recreate-b*-bindings (bindings new-terms)
   (declare (xargs :guard (and (supported-b*-bindingsp bindings)
-                              (true-listp new-terms))))
+                              (true-listp new-terms))
+                  :guard-hints (("Goal" :in-theory (enable supported-b*-bindingsp)))))
   (if (endp bindings)
       (if (not (endp new-terms))
           (er hard? 'recreate-b*-bindings "Extra new terms: ~x0." new-terms)
@@ -166,3 +196,28 @@
       (recreate-b*-binding (first bindings) new-terms)
       (let ((new-bindings (recreate-b*-bindings (rest bindings) new-terms)))
         (cons new-binding new-bindings)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defthm supported-b*-bindingp-of-mv-nth-0-of-recreate-b*-binding
+  (implies (supported-b*-bindingp binding)
+           (supported-b*-bindingp (mv-nth 0 (recreate-b*-binding binding new-terms))))
+  :hints (("Goal" :in-theory (enable supported-b*-bindingp
+                                     recreate-b*-binding))))
+
+(defthm supported-b*-bindingsp-of-recreate-b*-bindings
+  (implies (supported-b*-bindingsp bindings)
+           (supported-b*-bindingsp (recreate-b*-bindings bindings new-terms)))
+  :hints (("Goal" :in-theory (enable supported-b*-bindingsp
+                                     recreate-b*-bindings))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; TODO: there must be enough terms:
+;; (defthm subsetp-equal-of-extract-terms-from-b*-binding-of-mv-nth-0-of-recreate-b*-binding
+;;   (implies (supported-b*-bindingp binding)
+;;            (subsetp-equal (extract-terms-from-b*-binding (mv-nth 0 (recreate-b*-binding binding new-terms)))
+;;                           new-terms))
+;;   :hints (("Goal" :in-theory (enable supported-b*-bindingp
+;;                                      extract-terms-from-b*-binding
+;;                                      recreate-b*-binding))))
