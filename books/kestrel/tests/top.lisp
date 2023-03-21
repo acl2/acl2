a8b5fc1aea60eb3317176d2dfa63dac0990795f7
Eric Smith <ewsmith@gmail.com>
2023-03-21 15:54:46 -0700

    [tests] Add top.lisp for tests/ dir.

---
+++ books/kestrel/tests/top.lisp
@@ -0,0 +1,14 @@
+; Top book for tests/ directory
+;
+; Copyright (C) 2023 Kestrel Institute
+;
+; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
+;
+; Author: Eric Smith (eric.smith@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+(include-book "bstar-tests")
+(include-book "case-match")
