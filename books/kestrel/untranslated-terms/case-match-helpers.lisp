be01692d749e7eaa40fedabe4cba94ee1f236a53
Eric Smith <ewsmith@gmail.com>
2023-03-22 01:18:56 -0700

    [untranslated-terms] Improve case-match helpers.
    Strengthen guards.

--- books/kestrel/untranslated-terms/case-match-helpers.lisp
+++ books/kestrel/untranslated-terms/case-match-helpers.lisp
@@ -14,7 +14,6 @@
 (local (include-book "kestrel/lists-light/append" :dir :system))
 (local (include-book "kestrel/utilities/acl2-count" :dir :system))
 
-;; TODO: Consider using this as a guard on the functions below:
 (defund legal-case-match-casesp (cases)
   (declare (xargs :guard t))
   (if (atom cases)
@@ -37,35 +36,40 @@
   :rule-classes :forward-chaining
   :hints (("Goal" :in-theory (enable alistp legal-case-match-casesp))))
 
-;eventually drop?
-(defthm legal-case-match-casesp-forward-to-true-list-listp
-  (implies (legal-case-match-casesp cases)
-           (true-list-listp cases))
-  :hints (("Goal" :in-theory (enable legal-case-match-casesp))))
+;; ;eventually drop?
+;; (defthm legal-case-match-casesp-forward-to-true-list-listp
+;;   (implies (legal-case-match-casesp cases)
+;;            (true-list-listp cases))
+;;   :hints (("Goal" :in-theory (enable legal-case-match-casesp))))
 
-;; Extract the bodies of the items.  These are the untranslated terms that need to be handled.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Extract the bodies of the CASES.  These are the untranslated terms that need to be handled.
 ;; TODO: What about a decl of (type (satisfies foo)) where perhaps FOO is a function to replace?
-(defun extract-terms-from-case-match-cases (cases ;each is either (<pat> <body>) or (<pat> <dcl> <body>).
-                                            )
-  (declare (xargs :guard (true-list-listp cases)))
+(defun extract-terms-from-case-match-cases (cases)
+  (declare (xargs :guard (legal-case-match-casesp cases)
+                  :guard-hints (("Goal" :in-theory (enable legal-case-match-casesp)))))
   (if (endp cases)
       nil
-    (let ((case (first cases))) ; (pat ...declares... body)
-      (cons (car (last case))
-            (extract-terms-from-case-match-cases (rest cases))))))
+    (let* ((case (first cases)) ; (pat ...declares... body)
+           (body (car (last case))))
+      (cons body (extract-terms-from-case-match-cases (rest cases))))))
 
 (defthm <=-of-acl2-count-of-extract-terms-from-case-match-cases-linear
   (<= (acl2-count (extract-terms-from-case-match-cases cases))
       (acl2-count cases))
   :rule-classes :linear)
 
-;; Whenever there is a term in the cases, use the corresponding term from new-terms.
-;; Consumes one of the NEW-TERMS for each case.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Whenever there is a term in the CASES, use the corresponding term from NEW-TERMS.
+;; Consumes one of the NEW-TERMS for each of the CASES.
 (defun recreate-case-match-cases (cases new-terms)
-  (declare (xargs :guard (and (true-list-listp cases)
+  (declare (xargs :guard (and (legal-case-match-casesp cases)
                               (true-listp new-terms)
                               ;; (equal (len cases) (len new-terms)) ; uncomment?
-                              )))
+                              )
+                  :guard-hints (("Goal" :in-theory (enable legal-case-match-casesp)))))
   (if (endp cases)
       nil
     (let* ((case (first cases))
@@ -73,9 +77,7 @@
            (declares (butlast (rest case) 1)) ; may be empty
            ;; (body (car (last case))) ; the part being replaced
            )
-      (cons `(,pattern
-              ,@declares
-              ,(first new-terms))
+      (cons `(,pattern ,@declares ,(first new-terms))
             (recreate-case-match-cases (rest cases) (rest new-terms))))))
 
 (defthm legal-case-match-casesp-of-recreate-case-match-cases
