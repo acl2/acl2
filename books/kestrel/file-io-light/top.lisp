2deae34774ad63769eb034c785b05182c9a03e28
Eric Smith <ewsmith@gmail.com>
2023-03-31 15:44:56 -0700

    [file-io-light] Organize top.lisp better.

--- books/kestrel/file-io-light/top.lisp
+++ books/kestrel/file-io-light/top.lisp
@@ -18,43 +18,49 @@
 (include-book "open-output-channel")
 (include-book "open-output-channel-bang")
 
-;; Built-in read functions (one for each kind of channel):
-(include-book "read-byte-dollar")
-(include-book "read-char-dollar")
-(include-book "read-object")
+;; TODO: Consider using "print" instead of "write" for the channel operations:
 
-;; Built-in write functions:
-(include-book "write-byte-dollar")
-(include-book "princ-dollar") ; can write any atom
-(include-book "print-object-dollar-fn")
-(include-book "print-object-dollar")
+;; :byte channels:
+(include-book "read-byte-dollar") ; read-byte$ is built-in
+(include-book "write-byte-dollar") ; write-byte$ is built-in
+(include-book "write-bytes-to-channel")
 
-(include-book "prin1-with-slashes1")
-(include-book "prin1-with-slashes")
-(include-book "prin1-dollar")
+;; :character channels:
+(include-book "read-char-dollar") ; read-char$ is built-in
+(include-book "princ-dollar") ; can write any atom, princ$ is built-in
+(include-book "prin1-with-slashes1") ; built-in
+(include-book "prin1-with-slashes") ; built-in
+(include-book "prin1-dollar") ; prin1$ is built-in, print an atom
+(include-book "write-strings-to-channel") ; uses princ$
+
+;; :object channels:
+(include-book "read-object") ; built-in
+(include-book "print-object-dollar-fn") ; print-object$-fn is built-in
+(include-book "print-object-dollar") ; print-object$ is built-in
+(include-book "write-objects-to-channel")
 
 ;; Functions defined in this library:
 
-(include-book "read-object-from-file") ; reading a single object
-(include-book "read-objects-from-file")
+;; Reading/writing bytes:
 (include-book "read-file-into-byte-list")
-(include-book "read-file-into-character-list")
 (include-book "read-file-into-byte-array-stobj")
 (include-book "read-file-into-byte-array-stobj2")
-(include-book "read-file-into-character-array-stobj")
-
-(include-book "write-bytes-to-channel")
 (include-book "write-bytes-to-file")
 (include-book "write-bytes-to-file-bang")
 
-(include-book "write-strings-to-channel") ; uses princ$
+;; Reading/writing characters:
+(include-book "read-file-into-character-list")
+(include-book "read-file-into-character-array-stobj")
 (include-book "write-strings-to-file")
 (include-book "write-strings-to-file-bang")
 
-(include-book "write-objects-to-channel")
+;; Reading/writing objects:
+(include-book "read-object-from-file") ; reading a single object
+(include-book "read-objects-from-file")
 (include-book "write-objects-to-file")
 (include-book "write-objects-to-file-bang")
 
+;; Misc file utilities:
 (include-book "file-write-date-dollar")
 (include-book "file-length-dollar")
 (include-book "file-is-newer-thanp")
