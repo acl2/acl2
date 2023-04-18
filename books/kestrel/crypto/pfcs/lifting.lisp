b1c90a81d0631c5074e02f80f5989884a5bb3efb
Alessandro Coglio <coglio@kestrel.edu>
2023-04-18 09:26:18 -0700

    [PFCS] Fix XDOC link.

--- books/kestrel/crypto/pfcs/lifting.lisp
+++ books/kestrel/crypto/pfcs/lifting.lisp
@@ -34,7 +34,8 @@
   (xdoc::topstring
    (xdoc::p
     "The "
-    (xdoc::seetopic "semantics-shallow" "shallowly embedded semantics")
+    (xdoc::seetopic "semantics-shallowly-embedded"
+                    "shallowly embedded semantics")
     " of PFCSes includes an ACL2 function @(tsee sesem-definition)
      to turn a deeply embedded PFCS definition
      into a shallowly embedded PFCS definition.
