1042eb27ae6a20196fb9632df09cd66c325f3f98
Eric Smith <ewsmith@gmail.com>
2023-04-21 18:56:18 -0700

    [axe] Rename callstp script.

--- books/kestrel/axe/translate-dag-to-stp.lisp
+++ books/kestrel/axe/translate-dag-to-stp.lisp
@@ -13,7 +13,7 @@
 (in-package "ACL2")
 
 ;; Ensures this book and all STP examples get rebuilt when the script changes:
-;; (depends-on "callstplimited.bash")
+;; (depends-on "callstp.bash")
 
 ;; TODO: Use an array instead of nodenum-type-alist everywhere?
 
@@ -2202,7 +2202,7 @@
                   :stobjs state))
   (b* ((counterexample-arg (if counterexamplep "y" "n"))
        (max-conflicts-string (if max-conflicts (nat-to-string max-conflicts) "-1")) ; -1 means no max
-       ((mv status state) (call-axe-script "callstplimited.bash" (list input-filename output-filename max-conflicts-string counterexample-arg) state))
+       ((mv status state) (call-axe-script "callstp.bash" (list input-filename output-filename max-conflicts-string counterexample-arg) state))
        ;;(- (cw "STP exit status: ~x0~%" status))
        )
     ;;(prog2$ (cw "sys-call status: ~x0~%" status)
