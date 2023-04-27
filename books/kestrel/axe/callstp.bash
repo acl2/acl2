40572d9e004cc6cd4078f23cc74480791df39fb2
Eric Smith <ewsmith@gmail.com>
2023-04-26 23:53:08 -0700

    [axe] Improve STP script.
    The ACL2_STP_VARIETY environment variable now controls how STP is to be called, specifically the max conflicts option.  The default is 2, which should work with the latest STP.

--- books/kestrel/axe/callstp.bash
+++ books/kestrel/axe/callstp.bash
@@ -16,7 +16,7 @@ set -e # Exit immediately on errors
 # Check the number of arguments supplied:
 if [ $# -ne 4 ]
 then
-    echo "callstplimited.bash: ERROR: Arguments must be the input file, output file, max conflicts (-1 for no max), and whether to generate a counterexample (y/n)."
+    echo "callstp.bash: ERROR: Arguments must be the input file, output file, max conflicts (-1 for no max), and whether to generate a counterexample (y/n)."
     exit 1
 fi
 
@@ -25,12 +25,19 @@ OUTPUT_FILE=$2 # Where to write the output (caller can then check whether this c
 MAX_CONFLICTS=$3 # number of conflicts (-1 means no max)
 COUNTEREXAMPLE=$4 # Whether to generate a counterexample ("y" or "n")
 
+# Use STP environment var, if set, otherwise look for 'stp' on the user's path:
+STP=${STP:-stp}
+
+# By default, we use the newest STP syntax, but set ACL2_STP_VARIETY
+# to "0" or "1" to override that.
+ACL2_STP_VARIETY=${ACL2_STP_VARIETY:-2}
+
 if [ ${COUNTEREXAMPLE} = "y" ] ; then
     COUNTEREXAMPLE_ARGS="--print-counterex --print-counterexbin"
 elif [ ${COUNTEREXAMPLE} = "n" ] ; then
     COUNTEREXAMPLE_ARGS=""
 else
-    echo "Error in callstplimited.bash: COUNTEREXAMPLE should be 'y' or 'n'."
+    echo "Error in callstp.bash: COUNTEREXAMPLE should be 'y' or 'n'."
     exit 1
 fi
 
@@ -43,29 +50,25 @@ MULT_OPTIONS=""
 
 #TODO: The STP timeout is hardly graceful.  It says "Aborted..." Try the new STP?  <-- old comment?
 
-# Use STP environment var, if set, otherwise look for 'stp' on the user's path:
-STP=${STP:-stp}
-
-# echo "CALLING ${STP}"
-# Requires a relatively new STP:
-${STP} ${COUNTEREXAMPLE_ARGS} --max_num_confl $MAX_CONFLICTS -r ${MULT_OPTIONS} ${INPUT_FILE} > ${OUTPUT_FILE}
-## For a newer STP, this may be needed (or maybe either is ok, if the boost library is new enough):
-# stp ${COUNTEREXAMPLE_ARGS} --max-num-confl $MAX_CONFLICTS -r ${INPUT_FILE} > ${OUTPUT_FILE}
-
-# if [ -f "${NEWSTP}" ]; then
-#     echo "Using NEWSTP, which is ${NEWSTP}."
-#     ## Call a relatively new version of STP:
-#     ## echo "Calling new STP: ${NEWSTP}."
-#     ${NEWSTP} ${COUNTEREXAMPLE_ARGS} --max_num_confl $MAX_CONFLICTS -r ${INPUT_FILE} > ${OUTPUT_FILE}
-# elif [ -f "${STP}" ]; then
-#     ## Call a relatively old version of STP:
-#     ${STP} ${COUNTEREXAMPLE_ARGS} -g $MAX_CONFLICTS -r ${INPUT_FILE} > ${OUTPUT_FILE}
-# else
-#     echo "ERROR: callstplimited.bash: Cannot find NEWSTP or STP"
-#     echo "(NEWSTP environment var = ${NEWSTP})"
-#     echo "(STP environment var = ${STP})"
-#     exit 201
-# fi
+if [[ $ACL2_STP_VARIETY == "0" ]] ; then
+    # Use -g for max conflicts option:
+    # May be necessary for very old STP versions.
+    # echo "CALLING ${STP} (variety 0)"
+    ${STP} ${COUNTEREXAMPLE_ARGS} -g $MAX_CONFLICTS -r ${MULT_OPTIONS} ${INPUT_FILE} > ${OUTPUT_FILE}
+elif [[ $ACL2_STP_VARIETY == "1" ]] ; then
+    # Use underscores in max conflicts option:
+    # May be necessary for a somewhat old STP (circa 2021 is fine).
+    # echo "CALLING ${STP} (variety 1)"
+    ${STP} ${COUNTEREXAMPLE_ARGS} --max_num_confl $MAX_CONFLICTS -r ${MULT_OPTIONS} ${INPUT_FILE} > ${OUTPUT_FILE}
+elif [[ $ACL2_STP_VARIETY == "2" ]] ; then
+    # Use dashes in max conflicts option:
+    # For the latest STP, this may be needed (or maybe either dashes or underscores are ok, if the boost library is new enough):
+    # echo "CALLING ${STP} (variety 2)"
+    ${STP} ${COUNTEREXAMPLE_ARGS} --max-num-confl $MAX_CONFLICTS -r ${MULT_OPTIONS} ${INPUT_FILE} > ${OUTPUT_FILE}
+else
+    echo "Unsupported STP variety: ${ACL2_STP_VARIETY}.  Exiting."
+    exit 1
+fi
 
 EXITSTATUS=$?
 # echo "STP exit status: $EXITSTATUS"
