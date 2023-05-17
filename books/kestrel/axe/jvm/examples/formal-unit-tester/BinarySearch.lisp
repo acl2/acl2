f990927d7ad39a38bd396421b38a336fb9afcf2d
Eric Smith <ewsmith@gmail.com>
2023-05-16 23:07:18 -0700

    [axe] Add BinarySearch example for Formal Unit Tester.

---
+++ books/kestrel/axe/jvm/examples/formal-unit-tester/BinarySearch.lisp
@@ -0,0 +1,17 @@
+; Apply the Formal Unit Tester to BinarySearch
+;
+; Copyright (C) 2020-2023 Kestrel Institute
+;
+; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
+;
+; Author: Eric Smith (eric.smith@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+; (depends-on "BinarySearch.class")
+
+(include-book "kestrel/axe/jvm/formal-unit-tester" :dir :system)
+
+(test-file "BinarySearch.java" :print t :remove-rules (sbvlt-of-bvif-rules))
