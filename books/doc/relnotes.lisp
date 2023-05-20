3dbdfe952d9ce6155cb162f67e7194fece334955
Matt Kaufmann <matthew.j.kaufmann@gmail.com>
2023-05-19 21:20:52 -0500

    Added an item in :DOC note-8-6-books for demos/include-raw-examples/.  Moved topic "Group Theory Library" in note-8-6-books from "Changes to Existing Libraries" to "New Libraries".
    Thanks to Alessandro Coglio for clarifying that we can consider there
    to be a "Demos Library".

--- books/doc/relnotes.lisp
+++ books/doc/relnotes.lisp
@@ -121,6 +121,15 @@
 
    (xdoc::h3 "New Libraries")
 
+   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+   (xdoc::h4 "Group Theory Library")
+
+   (xdoc::p
+    "Added seven books to @('projects/groups/') on the topics of homomorphisms,
+     direct products, the Fundamental Theorem of Finite Abelian Groups,
+     symmetric groups, group actions, the Sylow theorems, and simple groups.")
+
    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
    (xdoc::h3 "Changes to Existing Libraries")
@@ -237,6 +246,14 @@
 
    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+   (xdoc::h4 "Demos Library")
+
+   (xdoc::p
+    "See directory @('demos/include-raw-examples/') for examples demonstrating
+     the use of @(tsee include-raw) and @(tsee partial-encapsulate).")
+
+   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
    (xdoc::h4 (xdoc::seetopic "digits-any-base" "Digits Library"))
 
    (xdoc::p
@@ -254,15 +271,6 @@
     "In the library of "
     (xdoc::seetopic "fty::defresult" "result types")
     ", some names have been improved.")
-
-   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-   (xdoc::h4 "Group Theory Library")
-
-   (xdoc::p
-    "Added seven books to @('projects/groups/') on the topics of homomorphisms,
-     direct products, the Fundamental Theorem of Finite Abelian Groups,
-     symmetric groups, group actions, the Sylow theorems, and simple groups.")
 
    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
