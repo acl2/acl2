c14cc3481f81f90a75b2cedec77a03c5a5ab5c3b
Eric Smith <ewsmith@gmail.com>
2023-06-26 23:32:39 -0700

    [utilities] Reduce deps of get-username.

--- books/kestrel/utilities/get-username.lisp
+++ books/kestrel/utilities/get-username.lisp
@@ -1,6 +1,6 @@
 ; A utility to get the user's username
 ;
-; Copyright (C) 2013-2022 Kestrel Institute
+; Copyright (C) 2013-2023 Kestrel Institute
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
 ;
@@ -10,7 +10,6 @@
 
 (in-package "ACL2")
 
-(include-book "std/util/bstar" :dir :system)
 (local (include-book "kestrel/typed-lists-light/character-listp" :dir :system))
 (local (include-book "read-acl2-oracle"))
 (local (include-book "update-acl2-oracle"))
@@ -19,24 +18,25 @@
 
 (defttag get-username) ; due to the sys-call+
 
+;move
 (defun strip-last-char (string)
   (declare (xargs :guard (stringp string)))
   (coerce (butlast (coerce string 'list) 1) 'string))
 
-;; Returns (mv username state)
+;; Returns (mv username state) where USERNAME is a string.
 ;; Could move this to a separate book...
 (defund get-username (state)
   (declare (xargs :stobjs state))
-  (b* (((mv erp username-and-newline state)
-        (sys-call+ "whoami" '() state))
-       ((when erp)
-        (er hard? 'get-username "Failed to get username.")
-        (mv "BAD-USERNAME" state))
-       ((when (not (stringp username-and-newline)))
-        (mv "BAD-USERNAME" state))
-       ;; Strip the final newline:
-       (username (strip-last-char username-and-newline)))
-    (mv username state)))
+  (mv-let (erp username-and-newline state)
+    (sys-call+ "whoami" nil state)
+    (if erp
+        (prog2$ (er hard? 'get-username "Error getting username.")
+                (mv "BAD-USERNAME" state))
+      (if (not (stringp username-and-newline))
+          (prog2$ (er hard? 'get-username "Username is the non-string: ~x0." username-and-newline)
+                  (mv "BAD-USERNAME" state))
+        (mv (strip-last-char username-and-newline) ; Strip the final newline ; todo: instead, reed up to first newline?
+            state)))))
 
 (defthm stringp-of-mv-nth-0-of-get-username
   (stringp (mv-nth 0 (get-username state)))
