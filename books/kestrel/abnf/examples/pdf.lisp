427cea0e69aab0344b0f1f220147927326b2922e
Alessandro Coglio <coglio@kestrel.edu>
2023-08-11 14:38:02 -0700

    [ABNF] Tweak file header.

--- books/kestrel/abnf/examples/pdf.lisp
+++ books/kestrel/abnf/examples/pdf.lisp
@@ -1,6 +1,7 @@
 ; ABNF (Augmented Backus-Naur Form) Library
 ;
-; Copyright (C) 2023 BAE Systems, Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2023 BAE Systems
+; Copyright (C) 2023 Kestrel Institute (http://www.kestrel.edu)
 ;
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 ;
