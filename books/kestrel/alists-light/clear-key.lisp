ccc13b408c79d5388d5d163a2006c653b57bd387
Eric Smith <ewsmith@gmail.com>
2023-09-03 19:27:55 -0700

    [alists-light] Add alistp-of-clear-key.

--- books/kestrel/alists-light/clear-key.lisp
+++ books/kestrel/alists-light/clear-key.lisp
@@ -1,7 +1,7 @@
 ; Clearing a key in an alist
 ;
 ; Copyright (C) 2008-2011 Eric Smith and Stanford University
-; Copyright (C) 2013-2020 Kestrel Institute
+; Copyright (C) 2013-2023 Kestrel Institute
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
 ;
@@ -11,6 +11,7 @@
 
 (in-package "ACL2")
 
+;; TODO: Disable
 (defun clear-key (key alist)
   (declare (xargs :guard (alistp alist)))
   (if (endp alist)
@@ -19,3 +20,8 @@
         (clear-key key (cdr alist))
       (cons (car alist)
             (clear-key key (cdr alist))))))
+
+(defthm alistp-of-clear-key
+  (implies (alistp alist)
+           (alistp (clear-key key alist)))
+  :hints (("Goal" :in-theory (enable clear-key))))
