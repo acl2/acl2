ebe354b318e8b0e7b237ce939408f7f4946fb993
Eric Smith <ewsmith@gmail.com>
2023-09-13 14:12:06 -0700

    [x86] Improve ELF parser.
    Decode the machine type field.

--- books/kestrel/x86/parsers/parse-elf-file.lisp
+++ books/kestrel/x86/parsers/parse-elf-file.lisp
@@ -1,6 +1,6 @@
 ; A parser for ELF executables
 ;
-; Copyright (C) 2022 Kestrel Institute
+; Copyright (C) 2022-2023 Kestrel Institute
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
 ;
@@ -13,6 +13,8 @@
 ;; See https://refspecs.linuxfoundation.org/elf/elf.pdf
 ;; and
 ;; https://www.uclibc.org/docs/elf-64-gen.pdf
+;; and
+;; https://refspecs.linuxfoundation.org/elf/gabi4+/ch4.eheader.html
 
 (include-book "parser-utils")
 (include-book "kestrel/alists-light/lookup-equal-safe" :dir :system)
@@ -50,6 +52,95 @@
     (#xfeff . :hios)
     (#xff00 . :loproc)
     (#xffff . :hiproc)))
+
+(defconst *elf-machine-types*
+  '((0 . :EM_NONE)
+    (1 . :EM_M32)  ;       AT&T WE 32100
+    (2 . :EM_SPARC) ;       SPARC
+    (3 . :EM_386)   ;       Intel 80386
+    (4 . :EM_68K)   ;       Motorola 68000
+    (5 . :EM_88K)   ;       Motorola 88000
+    ;; reserved        6       Reserved for future use (was EM_486)
+    (7 . :EM_860)       ;       Intel 80860
+    (8 . :EM_MIPS)      ;       MIPS I Architecture
+    (9 . :EM_S370)      ;       IBM System/370 Processor
+    (10 . :EM_MIPS_RS3_LE) ;      MIPS RS3000 Little-endian
+    ;; reserved        11-14   Reserved for future use
+    (15 . :EM_PARISC)   ;      Hewlett-Packard PA-RISC
+    ;; reserved        16      Reserved for future use
+    (17 . :EM_VPP500)   ;      Fujitsu VPP500
+    (18 . :EM_SPARC32PLUS) ;      Enhanced instruction set SPARC
+    (19 . :EM_960)         ;      Intel 80960
+    (20 . :EM_PPC)         ;      PowerPC
+    (21 . :EM_PPC64)       ;      64-bit PowerPC
+    (22 . :EM_S390)        ;      IBM System/390 Processor
+    ;; reserved        23-35   Reserved for future use
+    (36 . :EM_V800)  ;      NEC V800
+    (37 . :EM_FR20)  ;      Fujitsu FR20
+    (38 . :EM_RH32)  ;      TRW RH-32
+    (39 . :EM_RCE)   ;      Motorola RCE
+    (40 . :EM_ARM)   ;      Advanced RISC Machines ARM
+    (41 . :EM_ALPHA) ;      Digital Alpha
+    (42 . :EM_SH)    ;      Hitachi SH
+    (43 . :EM_SPARCV9) ;      SPARC Version 9
+    (44 . :EM_TRICORE) ;      Siemens TriCore embedded processor
+    (45 . :EM_ARC)     ;      Argonaut RISC Core, Argonaut Technologies Inc.
+    (46 . :EM_H8_300)  ;      Hitachi H8/300
+    (47 . :EM_H8_300H) ;      Hitachi H8/300H
+    (48 . :EM_H8S)     ;      Hitachi H8S
+    (49 . :EM_H8_500)  ;      Hitachi H8/500
+    (50 . :EM_IA_64)   ;      Intel IA-64 processor architecture
+    (51 . :EM_MIPS_X)  ;      Stanford MIPS-X
+    (52 . :EM_COLDFIRE) ;      Motorola ColdFire
+    (53 . :EM_68HC12)   ;      Motorola M68HC12
+    (54 . :EM_MMA)      ;      Fujitsu MMA Multimedia Accelerator
+    (55 . :EM_PCP)      ;      Siemens PCP
+    (56 . :EM_NCPU)     ;      Sony nCPU embedded RISC processor
+    (57 . :EM_NDR1)     ;      Denso NDR1 microprocessor
+    (58 . :EM_STARCORE) ;      Motorola Star*Core processor
+    (59 . :EM_ME16)     ;      Toyota ME16 processor
+    (60 . :EM_ST100)    ;      STMicroelectronics ST100 processor
+    (61 . :EM_TINYJ) ;      Advanced Logic Corp. TinyJ embedded processor family
+    (62 . :EM_X86_64) ;      AMD x86-64 architecture
+    (63 . :EM_PDSP)   ;      Sony DSP Processor
+    (64 . :EM_PDP10)  ;      Digital Equipment Corp. PDP-10
+    (65 . :EM_PDP11)  ;      Digital Equipment Corp. PDP-11
+    (66 . :EM_FX66)   ;      Siemens FX66 microcontroller
+    (67 . :EM_ST9PLUS) ;      STMicroelectronics ST9+ 8/16 bit microcontroller
+    (68 . :EM_ST7)     ;      STMicroelectronics ST7 8-bit microcontroller
+    (69 . :EM_68HC16)  ;      Motorola MC68HC16 Microcontroller
+    (70 . :EM_68HC11)  ;      Motorola MC68HC11 Microcontroller
+    (71 . :EM_68HC08)  ;      Motorola MC68HC08 Microcontroller
+    (72 . :EM_68HC05)  ;      Motorola MC68HC05 Microcontroller
+    (73 . :EM_SVX)     ;      Silicon Graphics SVx
+    (74 . :EM_ST19)    ;      STMicroelectronics ST19 8-bit microcontroller
+    (75 . :EM_VAX)     ;      Digital VAX
+    (76 . :EM_CRIS)    ;      Axis Communications 32-bit embedded processor
+    (77 . :EM_JAVELIN) ;      Infineon Technologies 32-bit embedded processor
+    (78 . :EM_FIREPATH) ;      Element 14 64-bit DSP Processor
+    (79 . :EM_ZSP)      ;      LSI Logic 16-bit DSP Processor
+    (80 . :EM_MMIX)     ;      Donald Knuth's educational 64-bit processor
+    (81 . :EM_HUANY) ;      Harvard University machine-independent object files
+    (82 . :EM_PRISM) ;      SiTera Prism
+    (83 . :EM_AVR)   ;      Atmel AVR 8-bit microcontroller
+    (84 . :EM_FR30)  ;      Fujitsu FR30
+    (85 . :EM_D10V)  ;      Mitsubishi D10V
+    (86 . :EM_D30V)  ;      Mitsubishi D30V
+    (87 . :EM_V850)  ;      NEC v850
+    (88 . :EM_M32R)  ;      Mitsubishi M32R
+    (89 . :EM_MN10300) ;      Matsushita MN10300
+    (90 . :EM_MN10200) ;      Matsushita MN10200
+    (91 . :EM_PJ)      ;      picoJava
+    (92 . :EM_OPENRISC) ;      OpenRISC 32-bit embedded processor
+    (93 . :EM_ARC_A5)   ;      ARC Cores Tangent-A5
+    (94 . :EM_XTENSA)   ;      Tensilica Xtensa Architecture
+    (95 . :EM_VIDEOCORE) ;      Alphamosaic VideoCore processor
+    (96 . :EM_TMM_GPP)   ;      Thompson Multimedia General Purpose Processor
+    (97 . :EM_NS32K)     ;      National Semiconductor 32000 series
+    (98 . :EM_TPC)       ;      Tenor Network TPC processor
+    (99 . :EM_SNP1K)     ;      Trebia SNP 1000 processor
+    (100 . :EM_ST200) ;     STMicroelectronics (www.st.com) ST200 microcontroller
+    ))
 
 ;; Returns (mv item bytes).
 (defun parse-half (bytes)
@@ -279,8 +370,9 @@
        ((mv e_type bytes) (parse-half bytes)) ; todo: consider endianness for these values
        (type (lookup-safe e_type *elf-file-types*))
        (result (acons :type type result))
-       ((mv e_machine bytes) (parse-half bytes)) ; todo: decode
-       (result (acons :machine e_machine result))
+       ((mv e_machine bytes) (parse-half bytes))
+       (machine (lookup-safe e_machine *elf-machine-types*))
+       (result (acons :machine machine result))
        ((mv e_version bytes) (parse-word bytes))
        (result (acons :version e_version result))
        ((mv e_entry bytes) (parse-addr 64-bitp bytes))
