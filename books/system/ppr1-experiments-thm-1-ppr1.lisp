2f3b980f76ddceff51cc4868b5a13c9ec6edeeee
Matt Kaufmann <matthew.j.kaufmann@gmail.com>
2023-09-30 07:23:06 -0500

    Added two new books from J.
    J tells me that he is writing a doc topic that will point to these two
    books.
    
    I didn't add anything for this to books/doc/relnotes.lisp, yet; I plan
    to do that when J's new :doc is added.
    
    Regarding testing:
    
    I first ran "make regression-fresh", which passed -- as expected,
    without attempting to certify
    books/system/ppr1-experiments-thm-1-ppr1.lisp.  Then, without
    cleaning, "make regression-everything" passed.  I did my testing this
    way because the book system/ppr1-experiments-thm-1-ppr1.lisp is
    included in the value of variable SLOW_BOOKS in books/GNUmakefile, so
    that it's certified with "regression-everything" but not "regression",
    since it contains a theorem that J's machine took well over a half
    hour to prove.
    
    Also synched doc.lisp etc.

---
+++ books/system/ppr1-experiments-thm-1-ppr1.lisp
@@ -0,0 +1,55 @@
+; Copyright (C) 2023, ForrestHunt, Inc.
+; License: A 3-clause BSD license.  See the LICENSE file distributed with ACL2.
+
+; Seven Proofs of that PPR1 returns a PPR-TUPLE-P
+; -- the Really Slow Proof
+
+; J Moore
+; September, 2023
+
+; This book carries out the proofs of THM-1-PPR1 and THM-1-PPR1-LST that are
+; skipped in ppr1-experiments.lisp because they are so slow.  For a description
+; of the entire ppr1-experiment, see
+
+; :DOC coarse-v-fine-grained-induction
+
+; To see how long it took inspect the .cert.out file after a certification.  In
+; September, 2023, it took 2165.19 seconds running the proto-Version 8.6 in CCL
+; on a MacBook Pro.
+
+(in-package "ACL2")
+
+(include-book "ppr1-experiments")
+
+; -----------------------------------------------------------------
+; Scenario 1
+
+(in-theory (enable ppr1 ppr1-lst))
+
+(defthm-coarse-induction-scheme ; coarse, enabled, :expand
+  (defthm thm-1-ppr1
+    (implies (and
+              (unsigned-byte-p #.*small-nat-bits* width)
+              (unsigned-byte-p #.*small-nat-bits* rpc)
+              (symbol-to-fixnat-alistp
+               (table-alist 'ppr-special-syms (w state))))
+             (ppr-tuple-p (ppr1 x print-base print-radix width rpc state eviscp)))
+    :flag ppr1
+    :rule-classes nil)
+  (defthm thm-1-ppr1-lst
+    (implies (and
+              (unsigned-byte-p #.*small-nat-bits* width)
+              (unsigned-byte-p #.*small-nat-bits* rpc)
+              (symbol-to-fixnat-alistp
+               (table-alist 'ppr-special-syms (w state))))
+             (ppr-tuple-lst-p (ppr1-lst lst print-base print-radix width rpc
+                                        pair-keywords-p state eviscp)))
+    :flag ppr1-lst
+    :rule-classes nil)
+  :hints (("Goal"
+           :expand ((:free (print-base print-radix width rpc state eviscp)
+                           (ppr1 x print-base print-radix width rpc state eviscp))
+                    (:free (print-base print-radix width rpc
+                                       pair-keywords-p state eviscp)
+                           (ppr1-lst lst print-base print-radix width rpc
+                                     pair-keywords-p state eviscp))))))
