78c632b409cb7457f7f5a925979cec6e7f9aabf4
Eric Smith <ewsmith@gmail.com>
2023-10-19 00:26:26 -0700

    [utilities] Improve make-var-names, etc.

--- books/kestrel/utilities/make-var-names.lisp
+++ books/kestrel/utilities/make-var-names.lisp
@@ -1,7 +1,7 @@
 ; Utilities to make variable names
 ;
 ; Copyright (C) 2008-2011 Eric Smith and Stanford University
-; Copyright (C) 2013-2020 Kestrel Institute
+; Copyright (C) 2013-2023 Kestrel Institute
 ; Copyright (C) 2016-2020 Kestrel Technology, LLC
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
@@ -12,15 +12,16 @@
 
 (in-package "ACL2")
 
-;; todo: compare to new-var-names
+;; See also fresh-names.lisp and fresh-names2.lisp.
+
+;; todo: consider removing "var" from these names, as these numbered symbols
+;; could be used for other purposes.
 
 (include-book "pack")
 (local (include-book "kestrel/typed-lists-light/symbol-listp" :dir :system))
 (local (include-book "kestrel/lists-light/revappend" :dir :system))
 
-;;;
-;;; make-var-names-aux
-;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Makes a list of symbols each of which is BASE-SYMBOL with a numeric suffix
 ;; added to it.  The result contains symbols from BASE-SYMBOL with a suffix of
@@ -61,69 +62,65 @@
                   (<= startnum endnum)))
   :hints (("Goal" :in-theory (enable make-var-names-aux))))
 
-;;;
-;;; make-var-names
-;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Makes a list of symbols each of which is BASE-SYMBOL with a numeric suffix
+;; added.  The first numeric suffix is START, and subsequent ones are
+;; consecutve, with a total of COUNT symbols generated.
+;rename?
+(defund make-var-name-range (base-symbol start count)
+  (declare (xargs :guard (and (symbolp base-symbol)
+                              (natp start)
+                              (natp count))))
+    (make-var-names-aux base-symbol start (+ -1 start count)))
+
+(defthm symbol-listp-of-make-var-name-range
+  (symbol-listp (make-var-name-range base-symbol start count))
+  :hints (("Goal" :in-theory (enable make-var-name-range))))
+
+(defthm len-of-make-var-name-range
+  (implies (and (natp start)
+                (natp count))
+           (equal (len (make-var-name-range base-symbol start count))
+                  count))
+  :hints (("Goal" :in-theory (enable make-var-name-range))))
+
+(defthm consp-of-make-var-name-range
+  (implies (and (natp start)
+                (natp count))
+           (equal (consp (make-var-name-range base-symbol start count))
+                  (posp count)))
+  :hints (("Goal" :in-theory (enable make-var-name-range))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Special case where the names start at 0.
 ;; Makes a list of symbols each of which is BASE-SYMBOL with a numeric suffix
 ;; added to it.  The result contains symbols from BASE-SYMBOL with a suffix of
 ;; 0 through BASE-SYMBOL with a suffix of COUNT minus 1.
 ;; Example: (make-var-names 'in 4) = (in0 in1 in2 in3).
-(defund make-var-names (base-name count)
+(defund make-var-names (base-symbol count)
   (declare (xargs :guard (and (natp count)
-                              (symbolp base-name))))
-  (make-var-names-aux base-name 0 (+ -1 count)))
+                              (symbolp base-symbol))))
+  (make-var-name-range base-symbol 0 count))
 
 (defthm symbol-listp-of-make-var-names
-  (symbol-listp (make-var-names base-name count))
+  (symbol-listp (make-var-names base-symbol count))
   :hints (("Goal" :in-theory (enable make-var-names))))
 
 (defthm true-listp-of-make-var-names
-  (true-listp (make-var-names base-name count))
+  (true-listp (make-var-names base-symbol count))
   :rule-classes :type-prescription
   :hints (("Goal" :in-theory (enable make-var-names))))
 
 (defthm len-of-make-var-names
   (implies (natp count)
-           (equal (len (make-var-names base-name count))
+           (equal (len (make-var-names base-symbol count))
                   count))
   :hints (("Goal" :in-theory (enable make-var-names))))
 
 (defthm consp-of-make-var-names
   (implies (natp count)
-           (equal (consp (make-var-names base-name count))
+           (equal (consp (make-var-names base-symbol count))
                   (posp count)))
   :hints (("Goal" :in-theory (enable make-var-names))))
-
-;; ;;;
-;; ;;; rev-make-var-names (TODO: deprecate in favor of make-var-names)
-;; ;;;
-
-;; ;; Makes a list of symbols each of which is BASE-SYMBOL with a numeric suffix
-;; ;; added to it.  The result contains symbols from BASE-SYMBOL with a suffix of
-;; ;; COUNT minus 1, through BASE-SYMBOL with a suffix of 0.
-;; (defund rev-make-var-names (count base-name)
-;;   (declare (xargs :guard (and (natp count)
-;;                               (symbolp base-name))))
-;;   (reverse (make-var-names-aux base-name 0 (+ -1 count))))
-
-;; (defthm symbol-listp-of-rev-make-var-names
-;;   (symbol-listp (rev-make-var-names count base-name))
-;;   :hints (("Goal" :in-theory (enable rev-make-var-names))))
-
-;; (defthm true-listp-of-rev-make-var-names
-;;   (true-listp (rev-make-var-names count base-name))
-;;   :rule-classes :type-prescription
-;;   :hints (("Goal" :in-theory (enable rev-make-var-names))))
-
-;; (defthm len-of-rev-make-var-names
-;;   (implies (natp count)
-;;            (equal (len (rev-make-var-names count base-name))
-;;                   count))
-;;   :hints (("Goal" :in-theory (enable rev-make-var-names))))
-
-;; (defthm consp-of-rev-make-var-names
-;;   (implies (natp count)
-;;            (equal (consp (rev-make-var-names count base-name))
-;;                   (posp count)))
-;;   :hints (("Goal" :in-theory (enable rev-make-var-names))))
