3332ebad96520e6f79ba38691432fc0c0852f450
Eric Smith <ewsmith@gmail.com>
2023-10-24 12:47:46 -0700

    [acl2-arrays] Clarify a bit.

--- books/kestrel/acl2-arrays/acl2-arrays.lisp
+++ books/kestrel/acl2-arrays/acl2-arrays.lisp
@@ -1,7 +1,7 @@
 ; A library for reasoning about ACL2 arrays (aref1, aset1, etc.)
 ;
 ; Copyright (C) 2008-2011 Eric Smith and Stanford University
-; Copyright (C) 2013-2022 Kestrel Institute
+; Copyright (C) 2013-2023 Kestrel Institute
 ; Copyright (C) 2016-2020 Kestrel Technology, LLC
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
@@ -400,22 +400,21 @@
                   len))
   :hints (("Goal" :in-theory (enable make-empty-array))))
 
-;requires that the arrays must be big enough for max-index to be a valid index
-;when copying a whole array, consider calling compress1 for speed?
-(defun copy-array-vals (max-index from-array-name from-array to-array-name to-array)
-  (declare (xargs :measure (nfix (+ 1 max-index))
-                  :guard (and (rationalp max-index)
+;; Copies the values at locations INDEX down to 0 from FROM-ARRAY to the same
+;; locations in TO-ARRAY.  Requires that the arrays be big enough for INDEX to
+;; be a valid index.  NOTE: when copying a whole array, consider calling
+;; compress1 for speed?
+(defund copy-array-vals (index from-array-name from-array to-array-name to-array)
+  (declare (xargs :measure (nfix (+ 1 index))
+                  :guard (and (rationalp index)
                               (array1p from-array-name from-array)
                               (array1p to-array-name to-array)
-                              (< max-index (alen1 from-array-name from-array))
-                              (< max-index (alen1 to-array-name to-array)))))
-  (if (not (natp max-index))
+                              (< index (alen1 from-array-name from-array))
+                              (< index (alen1 to-array-name to-array)))))
+  (if (not (natp index))
       to-array
-    (copy-array-vals (+ -1 max-index)
-                     from-array-name
-                     from-array
-                     to-array-name
-                     (aset1 to-array-name to-array max-index (aref1 from-array-name from-array max-index)))))
+    (let ((to-array (aset1 to-array-name to-array index (aref1 from-array-name from-array index))))
+      (copy-array-vals (+ -1 index) from-array-name from-array to-array-name to-array))))
 
 ;; (defthm natp-of-car-of-assoc-equal
 ;;   (equal (natp (car (assoc-equal :header array)))
