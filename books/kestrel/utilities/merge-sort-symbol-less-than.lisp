ea02eabd0ad38337ed253ff9076c089f903632dc
Eric Smith <ewsmith@gmail.com>
2023-10-26 12:13:05 -0700

    [utilities] Prove that merge-sort-symbol< returns a sorted list.

--- books/kestrel/utilities/merge-sort-symbol-less-than.lisp
+++ books/kestrel/utilities/merge-sort-symbol-less-than.lisp
@@ -12,20 +12,97 @@
 (in-package "ACL2")
 
 (local (include-book "kestrel/lists-light/reverse-list" :dir :system))
+(local (include-book "kestrel/lists-light/revappend" :dir :system))
 (local (include-book "kestrel/lists-light/intersection-equal" :dir :system))
+(local (include-book "kestrel/lists-light/no-duplicatesp-equal" :dir :system))
+(local (include-book "kestrel/lists-light/member-equal" :dir :system))
 (local (include-book "kestrel/typed-lists-light/strict-symbol-less-than-sortedp" :dir :system))
+(local (include-book "kestrel/typed-lists-light/symbol-listp" :dir :system))
 
 ;; TODO: Make sure this includes all of the rules we'd get if
 ;; merge-sort-symbol< were defined using defmergesort.
 
 ;; todo: disable merge-sort-symbol<
 
-;move
-(defthm symbol-listp-of-evens
-  (implies (symbol-listp l)
-           (symbol-listp (evens l)))
-  :hints (("Goal" :induct (evens l)
-           :in-theory (enable evens))))
+(local (in-theory (disable evens odds)))
+
+;; move
+(local
+  (progn
+    (defthm symbol-listp-of-evens
+      (implies (symbol-listp l)
+               (symbol-listp (evens l)))
+      :hints (("Goal" :induct (evens l)
+               :in-theory (enable evens))))
+
+    (defthm cons-of-evens
+      (equal (consp (evens l))
+             (consp l))
+      :hints (("Goal" ;:induct (evens l)
+               :in-theory (enable evens))))
+
+    (defthm symbol-listp-of-odds
+      (implies (symbol-listp l)
+               (symbol-listp (odds l)))
+      :hints (("Goal" ;:induct (odds l)
+               :in-theory (enable odds))))
+
+    (defthm cons-of-odds
+      (equal (consp (odds l))
+             (<= 2 (len l)))
+      :hints (("Goal" :in-theory (enable odds))))
+
+    (defthmd intersection-equal-when-intersection-equal-of-evens
+      (implies (intersection-equal (evens l1) l2)
+               (intersection-equal l1 l2))
+      :hints (("Goal" :in-theory (enable evens))))
+
+    (defthmd intersection-equal-when-intersection-equal-of-odds
+      (implies (intersection-equal (odds l1) l2)
+               (intersection-equal l1 l2))
+      :hints (("Goal" :in-theory (enable odds
+                                         intersection-equal-when-intersection-equal-of-evens
+                                         intersection-equal))))
+
+    (defthm evens-of-cddr
+      (equal (evens (cddr x))
+             (cdr (evens x)))
+      :hints (("Goal" :in-theory (enable evens))))
+
+    (defthm not-intersection-equal-when-not-intersection-equal-of-evens-and-intersection-equal-of-odds
+      (implies (and (not (intersection-equal (evens x) y))
+                    (not (intersection-equal (odds x) y)))
+               (not (intersection-equal x y)))
+      :hints (("Goal" :expand (evens x)
+               :in-theory (enable intersection-equal odds))))
+
+    (defthm not-member-equal-of-evens-when-not-member-equal
+      (implies (not (member-equal a l))
+               (not (member-equal a (evens l))))
+      :hints (("Goal" :in-theory (enable evens))))
+
+    (defthm not-intersection-equal-of-evens-and-odds-when-no-duplicatesp-equal
+      (implies (no-duplicatesp-equal l)
+               (not (intersection-equal (evens l) (odds l))))
+      :hints (("Goal" :expand (EVENS L)
+               :in-theory (enable evens odds no-duplicatesp-equal))))
+
+    (defthm not-member-equal-of-evens-when-not-member-equal
+      (implies (not (member-equal a l))
+               (not (member-equal a (evens l))))
+      :hints (("Goal" :in-theory (enable evens))))
+
+    (defthm no-duplicatesp-equal-of-evens
+      (implies (no-duplicatesp-equal l)
+               (no-duplicatesp-equal (evens l)))
+      :hints (("Goal" :expand (evens l)
+               :induct (evens l)
+               :in-theory (e/d (evens no-duplicatesp-equal) (evens-of-cddr)))))
+
+    (defthm no-duplicatesp-equal-of-odds
+      (implies (no-duplicatesp-equal l)
+               (no-duplicatesp-equal (odds l)))
+      :hints (("Goal" :in-theory (enable odds))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -43,32 +120,45 @@
              (consp acc)))
   :hints (("Goal" :in-theory (enable merge-symbol<))))
 
-(local
-  (defthm strict-symbol<-sortedp-of-merge-symbol<
-    (implies (and (acl2::strict-symbol<-sortedp lst1)
-                  (acl2::strict-symbol<-sortedp lst2)
-                  (acl2::strict-symbol<-sortedp (reverse acc))
-                  (symbol-listp lst1)
-                  (symbol-listp lst2)
-                  (symbol-listp acc)
-                  (not (intersection-equal lst1 lst2))
-                  ;; acc has the smallest items:
-                  (implies (and (consp lst1) (consp acc))
-                           (symbol< (car acc) (car lst1)))
-                  (implies (and (consp lst2) (consp acc))
-                           (symbol< (car acc) (car lst2))))
-             (acl2::strict-symbol<-sortedp (acl2::merge-symbol< lst1 lst2 acc)))
-    :hints (("Goal" :induct (acl2::merge-symbol< lst1 lst2 acc)
-             :in-theory (enable acl2::strict-symbol<-sortedp acl2::merge-symbol<)))))
+(defthm strict-symbol<-sortedp-of-merge-symbol<
+  (implies (and (strict-symbol<-sortedp lst1)
+                (strict-symbol<-sortedp lst2)
+                (strict-symbol<-sortedp (reverse acc))
+                (symbol-listp lst1)
+                (symbol-listp lst2)
+                (symbol-listp acc)
+                (not (intersection-equal lst1 lst2))
+                ;; acc has the smallest items:
+                (implies (and (consp lst1) (consp acc))
+                         (symbol< (car acc) (car lst1)))
+                (implies (and (consp lst2) (consp acc))
+                         (symbol< (car acc) (car lst2))))
+           (strict-symbol<-sortedp (merge-symbol< lst1 lst2 acc)))
+  :hints (("Goal" :induct (merge-symbol< lst1 lst2 acc)
+           :in-theory (enable strict-symbol<-sortedp merge-symbol<))))
 
 ;; Special case for acc=nil
-(defthmd strict-symbol<-sortedp-of-merge-symbol<-of-nil
-  (implies (and (acl2::strict-symbol<-sortedp lst1)
-                (acl2::strict-symbol<-sortedp lst2)
+(defthm strict-symbol<-sortedp-of-merge-symbol<-of-nil
+  (implies (and (strict-symbol<-sortedp lst1)
+                (strict-symbol<-sortedp lst2)
                 (symbol-listp lst1)
                 (symbol-listp lst2)
                 (not (intersection-equal lst1 lst2)))
-           (acl2::strict-symbol<-sortedp (acl2::merge-symbol< lst1 lst2 nil))))
+           (strict-symbol<-sortedp (merge-symbol< lst1 lst2 nil))))
+
+(defthm intersection-equal-of-merge-symbol<-arg2-iff
+  (iff (intersection-equal x (merge-symbol< lst1 lst2 acc))
+       (or (intersection-equal x lst1)
+           (intersection-equal x lst2)
+           (intersection-equal x acc)))
+  :hints (("Goal" :in-theory (enable merge-symbol<))))
+
+(defthm intersection-equal-of-merge-symbol<-arg1-iff
+  (iff (intersection-equal (merge-symbol< lst1 lst2 acc) x)
+       (or (intersection-equal lst1 x)
+           (intersection-equal lst2 x)
+           (intersection-equal acc x)))
+  :hints (("Goal" :in-theory (enable merge-symbol<))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -81,3 +171,25 @@
   (equal (consp (merge-sort-symbol< l))
          (consp l))
   :hints (("Goal" :in-theory (enable merge-sort-symbol<))))
+
+;; need perm to do this right
+;; ;; Sorting doesn't effect whether there is an intersection
+(defthm intersection-equal-of-merge-sort-symbol<-arg1-iff
+  (iff (intersection-equal (merge-sort-symbol< l1) l2)
+       (intersection-equal l1 l2))
+  :hints (("Goal" :in-theory (e/d (merge-sort-symbol<
+                                   intersection-equal-when-intersection-equal-of-evens
+                                   intersection-equal-when-intersection-equal-of-odds)
+                                  (intersection-equal-symmetric-iff)))))
+
+(defthm intersection-equal-of-merge-sort-symbol<-arg2-iff
+  (iff (intersection-equal l2 (merge-sort-symbol< l1))
+       (intersection-equal l1 l2))
+  :hints (("Goal" :use intersection-equal-of-merge-sort-symbol<-arg1-iff
+           :in-theory (disable intersection-equal-of-merge-sort-symbol<-arg1-iff))))
+
+(defthm strict-symbol<-sortedp-of-merge-sort-symbol<
+  (implies (and (symbol-listp l)
+                ;; If there are duplicates, the sorting can't be strict:
+                (no-duplicatesp-equal l))
+           (strict-symbol<-sortedp (merge-sort-symbol< l))))
