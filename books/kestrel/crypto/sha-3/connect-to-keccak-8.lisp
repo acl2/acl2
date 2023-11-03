05267727ad939cfacd1b465c5362dc326fdd1f29
Eric Smith <ewsmith@gmail.com>
2023-11-03 03:53:27 -0700

    [crytpo] Split slow connect-to-keccak book into 2.

--- books/kestrel/crypto/sha-3/connect-to-keccak-8.lisp
+++ books/kestrel/crypto/sha-3/connect-to-keccak-8.lisp
@@ -23,6 +23,7 @@
 (acl2::unroll-spec-basic *sha-3-keccak-256-8bit*
                    `(sha3::keccak-256 ,(acl2::symbolic-list 'in 8))
                    :rules :auto
+                   :memoizep nil ; for speed
                    :extra-rules '(acl2::len-of-cdr
                                   acl2::car-of-nthcdr
                                   acl2::update-nth-of-cons
@@ -51,44 +52,6 @@
 
 (acl2::prove-equivalence *sha-3-keccak-256-8bit*
                          *keccak-256-8bit*
-                         ;; :initial-rule-sets (list (make-axe-rules! (amazing-rules-bv) (w state))) ;don't bit-blast
-                         :normalize-xors nil ; todo: heap exhaustion (many xor nests, each with thousands of nodes, dag grows without bound) !
-                         :tactic :rewrite-and-sweep)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Assumes an 256-bit message
-(acl2::unroll-spec-basic *sha-3-keccak-256-256bit*
-                   `(sha3::keccak-256 ,(acl2::symbolic-list 'in 256))
-                   :rules :auto
-                   :extra-rules '(acl2::len-of-cdr
-                                  acl2::car-of-nthcdr
-                                  acl2::update-nth-of-cons
-                                  acl2::car-becomes-nth-of-0
-                                  acl2::cdr-of-append
-                                  acl2::consp-when-len-equal-constant
-                                  acl2::consp-when-len-equal-constant-alt
-                                  acl2::consp-of-cdr
-                                  acl2::nth-of-cdr))
-
-;; Assumes an 256-bit message
-(acl2::unroll-spec-basic *keccak-256-256bit*
-                         `(keccak::keccak-256 ,(acl2::symbolic-list 'in 256))
-                         :assumptions '((equal (len m) 256))
-                         :rules :auto
-                         :extra-rules '(acl2::len-of-cdr
-                                        acl2::car-of-nthcdr
-                                        acl2::update-nth-of-cons
-                                        acl2::car-becomes-nth-of-0
-                                        acl2::cdr-of-append
-                                        acl2::consp-when-len-equal-constant
-                                        acl2::consp-when-len-equal-constant-alt
-                                        acl2::consp-of-cdr
-                                        acl2::nth-of-cdr
-                                        acl2::leftrotate))
-
-(acl2::prove-equivalence *sha-3-keccak-256-256bit*
-                         *keccak-256-256bit*
                          ;; :initial-rule-sets (list (make-axe-rules! (amazing-rules-bv) (w state))) ;don't bit-blast
                          :normalize-xors nil ; todo: heap exhaustion (many xor nests, each with thousands of nodes, dag grows without bound) !
                          :tactic :rewrite-and-sweep)
