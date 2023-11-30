dcaa177af0272786f83d16d5ccae5cf2db800045
Eric Smith <ewsmith@gmail.com>
2023-11-30 13:51:22 -0800

    [helpers] Improve script.

--- books/kestrel/helpers/repair-book.sh
+++ books/kestrel/helpers/repair-book.sh
@@ -15,6 +15,11 @@ set -e
 BOOK="$1"
 echo "repair-book.sh called on ${BOOK}."
 
+if [ -z "${ACL2}" ] ; then
+  echo "ERROR: Please set your ACL2 environment variable (e.g., to your saved_acl2)."
+  exit 1
+fi
+
 export ACL2_CUSTOMIZATION=NONE # Just in case
 
 (echo '(include-book "kestrel/helpers/repair-book" :dir :system) (repair-book "'${BOOK}'")' | ${ACL2})
