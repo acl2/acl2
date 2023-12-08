2ceff1511ceba2ffbee61d7688c44737ad0a789b
Eric Smith <ewsmith@gmail.com>
2023-12-08 00:21:57 -0800

    [utilities] Add some rules about time-related functions.
    Also tweak a definition.

--- books/kestrel/utilities/real-time-since.lisp
+++ books/kestrel/utilities/real-time-since.lisp
@@ -12,6 +12,8 @@
 
 (local (include-book "get-real-time"))
 
+(in-theory (disable mv-nth))
+
 ;; Returns (mv time-difference state) where time-difference is the difference
 ;; between now and PAST-TIME, which should be in the past.  Often, PAST-TIME
 ;; will be the result of a prior call to get-real-time.  PAST-TIME and the
@@ -19,7 +21,13 @@
 (defund real-time-since (past-time state)
   (declare (xargs :guard (rationalp past-time)
                   :stobjs state))
-  (mv-let (now state)
-    (get-real-time state)
-    (mv (- now past-time)
-        state)))
+  (let ((past-time (mbe :logic (rfix past-time) :exec past-time)))
+    (mv-let (now state)
+      (get-real-time state)
+      (mv (- now past-time)
+          state))))
+
+(defthm rationalp-of-mv-nth-0-of-real-time-since
+  (rationalp (mv-nth 0 (real-time-since past-time state)))
+  :rule-classes :type-prescription
+  :hints (("Goal" :in-theory (enable real-time-since))))
