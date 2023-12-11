af86f7fe4c29863179c683350d5195fe1c316372
Warren A. Hunt, Jr. <hunt@cs.utexas.edu>
2023-12-11 10:50:36 -0600

    Documentation to help keep Warren's head straight.

--- books/projects/x86isa/console-raw.lisp
+++ books/projects/x86isa/console-raw.lisp
@@ -14,3 +14,28 @@
 
 (defun read-console (x86)
   (read-char-no-hang *console-stream*))
+
+
+#|
+; To use Yahya's version of the ACL2 x86 model, do:
+
+;  (ld "resume-linux.lisp")
+
+; > Type :? for other options.
+; 1 > :q
+;  (:STOP-LD 1)
+
+; TTAG NOTE: Printing of ttag notes has been deferred for the following
+; list of ttags:
+;   (:UNDEF-FLG :OTHER-NON-DET
+;               :INSTRUMENT :INCLUDE-RAW).
+; To print the deferred ttag notes:  (ACL2::SHOW-TTAG-NOTES).
+
+
+; ACL2 Error in ACL2::CHK-ABSSTOBJ-INVARIANTS:  Possible invariance violation
+; for an abstract stobj!
+; **PROCEED AT YOUR OWN RISK.**
+; To proceed, evaluate the following form.
+; :CONTINUE-FROM-ILLEGAL-STATE
+; See :DOC set-absstobj-debug.
+|#
--- books/projects/x86isa/resume-linux.lisp
+++ books/projects/x86isa/resume-linux.lisp
@@ -1,9 +1,37 @@
+;
+; To use the x86-ISA emulator, do:
+
+;  Start ACL2:
+;   ~/f/projects/x86-emulator/acl2-x86-boot/saved_acl2
+
+; Load the commands below
+
 (include-book "tools/execution/top")
 (defttag :include-raw)
-(include-book "tools/include-raw" :dir :system)
+; (include-book "tools/include-raw" :dir :system)
 (include-raw "console-raw.lisp")
+
+; The form above will "hang"; then we need to start (in another shell):
+;   gnc localhost 6444
+
 (restore-x86 "x86-state.bak" x86)
+
 (b* ((tlb (tlb x86))
      (tlb (make-fast-alist tlb))
      (x86 (!tlb tlb x86)))
     x86)
+
+; And then, start the x86-ISA interpreter
+
+; (x86-run-steps <big-number> x86)
+
+; At the prompt, one may (for instance) type:
+
+;  mount /dev/blkx86isa <mount_point>
+
+; do:  ls -la /bin   to see what commands exist.
+
+; Linux version be used is "alpinelinux.org".  Only taking "rootfs"; that is
+; the contents mounted under "/" directly.
+
+; Note, that the commands are provided by "busybox.net"
