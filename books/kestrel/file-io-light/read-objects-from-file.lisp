b938557bcccec10588757e132779defd16579fb9
Eric Smith <ewsmith@gmail.com>
2024-01-03 11:12:05 -0800

    [file-io-light] Add a rule.

--- books/kestrel/file-io-light/read-objects-from-file.lisp
+++ books/kestrel/file-io-light/read-objects-from-file.lisp
@@ -1,6 +1,6 @@
 ; A lightweight function to read the ACL2 objects from a channel
 ;
-; Copyright (C) 2021-2023 Kestrel Institute
+; Copyright (C) 2021-2024 Kestrel Institute
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
 ;
@@ -11,7 +11,6 @@
 (in-package "ACL2")
 
 (include-book "read-objects-from-channel")
-
 (local (include-book "open-input-channel"))
 (local (include-book "close-input-channel"))
 
@@ -34,11 +33,17 @@
               objects
               state))))))
 
+(defthm state-p1-of-mv-nth-2-of-read-objects-from-file
+  (implies (and (stringp filename)
+                (state-p1 state))
+           (state-p1 (mv-nth 2 (read-objects-from-file filename state))))
+  :hints (("Goal" :in-theory (enable read-objects-from-file))))
+
 (defthm state-p-of-mv-nth-2-of-read-objects-from-file
   (implies (and (stringp filename)
                 (state-p state))
            (state-p (mv-nth 2 (read-objects-from-file filename state))))
-  :hints (("Goal" :in-theory (enable read-objects-from-file))))
+  :hints (("Goal" :in-theory (enable state-p))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
