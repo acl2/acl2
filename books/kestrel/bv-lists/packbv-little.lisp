df5633748878efd969a5e9473898a828ea4caa1d
Eric Smith <ewsmith@gmail.com>
2024-01-13 16:42:26 -0800

    [bv] Add 2 rules about packbv-little.

--- books/kestrel/bv-lists/packbv-little.lisp
+++ books/kestrel/bv-lists/packbv-little.lisp
@@ -13,6 +13,9 @@
 (include-book "packbv")
 (include-book "kestrel/lists-light/reverse-list-def" :dir :system)
 (local (include-book "kestrel/typed-lists-light/all-integerp2" :dir :system))
+(local (include-book "../bv/unsigned-byte-p"))
+
+;todo: adapt more theorems from packbv
 
 (defund packbv-little (itemcount itemsize items)
   (declare (xargs :guard (and (natp itemcount)
@@ -28,3 +31,19 @@
   (equal (packbv-little 0 itemsize items)
          0)
   :hints (("Goal" :in-theory (enable packbv-little))))
+
+(defthm unsigned-byte-p-of-packbv-little
+  (implies (and (natp size)
+                (natp num))
+           (unsigned-byte-p (* size num)
+                            (packbv-little num size items)))
+  :hints (("Goal" :in-theory (enable packbv-little))))
+
+(defthm unsigned-byte-p-of-packbv-little-gen
+  (implies (and (<= (* size num) n)
+                (natp size)
+                (natp n)
+                (natp num))
+           (unsigned-byte-p n (packbv-little num size items)))
+  :hints (("Goal" :use (:instance unsigned-byte-p-of-packbv-little)
+           :in-theory (disable unsigned-byte-p-of-packbv-little))))
