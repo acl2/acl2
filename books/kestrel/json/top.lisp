765e492e0bf0cc94f4c0b918274a48bbac94ad48
Alessandro Coglio <>
2024-01-18 12:31:53 +0100

    [JSON] Extend top-level doc with overview.

--- books/kestrel/json/top.lisp
+++ books/kestrel/json/top.lisp
@@ -52,6 +52,24 @@
     "These are all meant to be consistent,
      at least in defining the JSON format,
      although they may contain
-     slightly different notations and recommendations."))
+     slightly different notations and recommendations.")
+   (xdoc::p
+    "Currently this library contains:")
+   (xdoc::ul
+    (xdoc::li
+     "A formalization of the syntax of JSON,
+      based on the ABNF grammar in the RFC.")
+    (xdoc::li
+     "A formalization of JSON values.")
+    (xdoc::li
+     "Some operations on JSON values,
+      including some lightweight checkers.")
+    (xdoc::li
+     "A translator from the output of
+      the JSON parser in @('kestrel/json-parser/')
+      to the JSON value representation defined in this library.")
+    (xdoc::li
+     "@(tsee b*) binders for
+      the representation of JSON values defined in this library.")))
   :order-subtopics t
   :default-parent t)
