8784e079c75e085426d090070383d25d1634c740
Alessandro Coglio <coglio@kestrel.edu>
2024-02-01 16:32:28 -0800

    [number theory] Add a theorem.

--- books/kestrel/number-theory/prime-fix.lisp
+++ books/kestrel/number-theory/prime-fix.lisp
@@ -1,10 +1,10 @@
 ; Number Theory Library
 ;
-; Copyright (C) 2022 Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2024 Kestrel Institute (http://www.kestrel.edu)
 ;
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 ;
-; Author: Alessandro Coglio (coglio@kestrel.edu)
+; Author: Alessandro Coglio (www.alessandrocoglio.info)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -18,6 +18,15 @@
 
 (std::deffixer prime-fix
   :short "Fixer for @(tsee dm::primep)."
-  :parents (acl2::number-theory)
+  :parents (number-theory)
   :pred dm::primep
   :body-fix 2)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defsection prime-fix-ext
+  :extension prime-fix
+
+  (defrule posp-of-prime-fix
+    (posp (prime-fix x))
+    :enable (dm::primep prime-fix)))
