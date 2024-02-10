3fb8335c547bc2a93bf1a3dd86f60fcbb32bc963
Mayank Manjrekar <mayank.manjrekar2@arm.com>
2024-02-10 18:01:19 +0530

    Modify expand-reduce-cp for handling mv simplifications done in const-fns-gen

--- books/projects/rac/lisp/expand-reduce-cp.lisp
+++ books/projects/rac/lisp/expand-reduce-cp.lisp
@@ -1,11 +1,25 @@
 ;; Mayank Manjrekar <mankmonjre@gmail.com>
-;; Oct 2023
+;; Feb 2024
 
 (in-package "ACL2")
 
 (include-book "clause-processors/term-vars" :dir :system)
 (include-book "clause-processors/meta-extract-user" :dir :system)
 (include-book "meta/pseudo-termp-lemmas" :dir :system)
+(include-book "misc/hons-help" :dir :system)
+
+(defevaluator ercp-ev ercp-ev-lst
+  ((typespec-check ts x)
+   (if a b c)
+   (equal a b)
+   (not a)
+   (iff a b)
+   (implies a b)
+   (mv-nth a b)
+   (cons a b))
+  :namedp t)
+
+(def-meta-extract ercp-ev ercp-ev-lst)
 
 ;; expand-all beta reduces all lambda applications, and expands function
 ;; applications for functions in the list of function symbols `fns`.
@@ -115,6 +129,74 @@
 
   (verify-guards expand-all))
 
+(defines push-mv-nths
+  ;; push mv-nths through if blocks
+  :flag-local nil
+  (define push-mv-nths ((term pseudo-termp))
+    (cond ((and (equal (len term) 3)
+                (eq (car term) 'mv-nth)
+                (equal (len (caddr term)) 4)
+                (eq (caaddr term) 'if))
+           (b* ((mv-num (cadr term))
+                (if-expr (caddr term))
+                (test (cadr if-expr))
+                (t-br (caddr if-expr))
+                (f-br (cadddr if-expr))
+                (test (push-mv-nths test))
+                (t-br (push-mv-nths (hons-list 'mv-nth mv-num t-br)))
+                (f-br (push-mv-nths (hons-list 'mv-nth mv-num f-br))))
+             (hons-list 'if test t-br f-br)))
+          ((and (equal (len term) 3)
+                (eq (car term) 'mv-nth)
+                (equal (len (caddr term)) 3)
+                (eq (caaddr term) 'cons))
+           (b* ((mv-num (cadr term))
+                (mv-term (caddr term)))
+             (if (and (quotep mv-num)
+                      (natp (unquote mv-num)))
+                 (b* ((mv-num (unquote mv-num)))
+                   (if (eql mv-num 0)
+                       (push-mv-nths (cadr mv-term))
+                     (b* ((mv-num (kwote (1- mv-num))))
+                       (push-mv-nths (hons-list 'mv-nth mv-num (caddr mv-term))))))
+               (hons-list 'mv-nth mv-num (push-mv-nths mv-term)))))
+          ((or (atom term)
+               (quotep term))
+           term)
+          (t
+           (hons (car term) (push-mv-nths-args (cdr term))))))
+  (define push-mv-nths-args ((terms pseudo-term-listp))
+    (if (consp terms)
+        (hons (push-mv-nths (car terms))
+              (push-mv-nths-args (cdr terms)))
+      terms)))
+
+(local
+ (defthm len-push-mv-nths-args
+   (equal (len (push-mv-nths-args x)) (len x))
+   :hints (("Goal" :in-theory (e/d (push-mv-nths-args) ())))))
+
+(local
+ (defthm-push-mv-nths-flag
+   (defthm return-type-of-push-mv-nths
+     (implies (pseudo-termp term)
+              (pseudo-termp (push-mv-nths term)))
+     :flag push-mv-nths)
+   (defthm return-type-of-push-mv-nths-args
+     (implies (pseudo-term-listp terms)
+              (pseudo-term-listp (push-mv-nths-args terms)))
+     :flag push-mv-nths-args)
+   :hints (("Goal" :in-theory (e/d (push-mv-nths-args
+                                    push-mv-nths) ()))
+           (and stable-under-simplificationp
+                '(:expand ((pseudo-termp (cons (car term)
+                                          (push-mv-nths-args (cdr term))))))))))
+
+(local
+ (defthm pseudo-term-listp-of-two-symbol-listp
+   (implies (and (symbol-listp x) (symbol-listp y))
+            (pseudo-term-substp (pairlis$ x y)))))
+
 (define expand-reduce-cp ((cl pseudo-term-listp)
                           fns ;; symbol-listp
                           state)
@@ -127,92 +209,122 @@
            (equal (len cl) 1)
            (consp (car cl))
            (eql (caar cl) 'equal))
-      (b* ((lhs (cadar cl))
+      (b* ((fns (cons 'mv-list fns))
+           (lhs (cadar cl))
            (rhs (caddar cl))
            (lhs-vars (simple-term-vars lhs))
            (rhs-vars (simple-term-vars rhs))
            ((mv ok lhs) (expand-all lhs (pairlis$ lhs-vars lhs-vars) fns state))
            ((unless ok) (mv nil (list cl) state))
            ((mv ok rhs) (expand-all rhs (pairlis$ rhs-vars rhs-vars) fns state))
-           ((unless ok) (mv nil (list cl) state)))
+           ((unless ok) (mv nil (list cl) state))
+           (lhs (push-mv-nths lhs))
+           (rhs (push-mv-nths rhs)))
         (if (hons-equal lhs rhs)
             (mv nil '(('t)) state)
-          (mv nil `(((equal ,lhs ,rhs))) state)))
-    (mv nil (list cl) state))
-
-  :prepwork
-  ((local
-    (defthm pseudo-term-listp-of-two-symbol-listp
-      (implies (and (symbol-listp x) (symbol-listp y))
-               (pseudo-term-substp (pairlis$ x y)))))))
+          (b* ((- (cw "~%expand-reduce clause-processor failed!~%")))
+          (mv nil `(((equal ,lhs ,rhs))) state))))
+    (mv nil (list cl) state)))
 
 ;; The following is borrowed from clause-processors/unify-substs.lisp. It
 ;; creates an alist of evaluated substitutions for a given subsitution `substs`
 ;; under the environment `al`.
 (local
- (define mextract-ev-alist (substs al)
+ (define ercp-ev-alist (substs al)
    :verify-guards nil
    (if (atom substs)
        nil
-     (cons (cons (caar substs) (mextract-ev (cdar substs) al))
-           (mextract-ev-alist (cdr substs) al)))
+     (cons (cons (caar substs) (ercp-ev (cdar substs) al))
+           (ercp-ev-alist (cdr substs) al)))
    ///
-   (defthm assoc-equal-mextract-ev-alist
-     (equal (cdr (assoc-equal k (mextract-ev-alist substs al)))
-            (mextract-ev (cdr (assoc-equal k substs)) al)))
+   (defthm assoc-equal-ercp-ev-alist
+     (equal (cdr (assoc-equal k (ercp-ev-alist substs al)))
+            (ercp-ev (cdr (assoc-equal k substs)) al)))
 
-   (defthm assoc-equal-mextract-ev-alist-iff
+   (defthm assoc-equal-ercp-ev-alist-iff
      (implies k
-              (iff (assoc-equal k (mextract-ev-alist substs al))
+              (iff (assoc-equal k (ercp-ev-alist substs al))
                    (assoc-equal k substs))))
 
-   (defthm assoc-equal-mextract-ev-alist-when-assoc
+   (defthm assoc-equal-ercp-ev-alist-when-assoc
      (implies (assoc k substs)
-              (assoc k (mextract-ev-alist substs al))))
+              (assoc k (ercp-ev-alist substs al))))
 
-   (defthm mextract-ev-alist-pairlis$
-     (equal (mextract-ev-alist (pairlis$ a b) al)
+   (defthm ercp-ev-alist-pairlis$
+     (equal (ercp-ev-alist (pairlis$ a b) al)
             (pairlis$ a
-                      (mextract-ev-lst b al))))
+                      (ercp-ev-lst b al))))
 
-   (defthm mextract-ev-alist-of-cons
-     (equal (mextract-ev-alist (cons x y) a)
-            (cons (cons (car x) (mextract-ev (cdr x) a))
-                  (mextract-ev-alist y a))))))
+   (defthm ercp-ev-alist-of-cons
+     (equal (ercp-ev-alist (cons x y) a)
+            (cons (cons (car x) (ercp-ev (cdr x) a))
+                  (ercp-ev-alist y a))))))
+
+;; Assuming (ercp-ev-global-facts), meta-extract-formula produces a theorem
+(local
+ (defthm ercp-ev-formula
+   (implies (and (ercp-ev-meta-extract-global-facts)
+                 (equal (w st) (w state)))
+            (ercp-ev (meta-extract-formula name st) a))
+   :hints(("Goal" :use ((:instance ercp-ev-meta-extract-global-badguy-sufficient
+                         (obj (list :formula name))))))))
+
+(local
+ (defthm ercp-ev-fn-get-def
+   (b* (((mv ok formals body) (fn-get-def fn st)))
+     (implies (and ok
+                   (ercp-ev-meta-extract-global-facts)
+                   (equal (w st) (w state))
+                   (equal (len args) (len formals)))
+              (equal (ercp-ev (cons fn args) a)
+                     (ercp-ev body (pairlis$ formals
+                                             (ercp-ev-lst args a))))))
+   :hints(("Goal" :in-theory (e/d (ercp-ev-of-fncall-args
+                                   acl2::match-tree-opener-theory
+                                   acl2::match-tree-alist-opener-theory)
+                                  (ercp-ev-formula
+                                   meta-extract-global-fact+
+                                   meta-extract-formula
+                                   take))
+                  :use ((:instance ercp-ev-formula
+                         (name fn)
+                         (acl2::st st)
+                         (a (pairlis$ (mv-nth 1 (fn-get-def fn st))
+                                      (ercp-ev-lst args a)))))))))
 
 ;; If expand-all returns ok, the evaluation of the redux is equal to the
 ;; evalation of the original term under a different environment
-;; `(mextract-ev-alist substs alist)`.
+;; `(ercp-ev-alist substs alist)`.
 (local
  (defthm-expand-all-flag
    (defthm expand-all-correct
      (b* (((mv ok redux) (expand-all term substs fns state)))
        (implies (and ok
-                     (mextract-ev-global-facts))
-                (equal (mextract-ev redux alist)
-                       (mextract-ev term (mextract-ev-alist substs alist)))))
+                     (ercp-ev-meta-extract-global-facts))
+                (equal (ercp-ev redux alist)
+                       (ercp-ev term (ercp-ev-alist substs alist)))))
      :flag expand-all)
    (defthm expand-all-lst-correct
      (b* (((mv ok redux-lst) (expand-all-lst terms substs fns state)))
        (implies (and ok
-                     (mextract-ev-global-facts))
-                (equal (mextract-ev-lst redux-lst alist)
-                       (mextract-ev-lst terms (mextract-ev-alist substs alist)))))
+                     (ercp-ev-meta-extract-global-facts))
+                (equal (ercp-ev-lst redux-lst alist)
+                       (ercp-ev-lst terms (ercp-ev-alist substs alist)))))
      :flag expand-all-lst)
    :hints (("Goal" :expand ()
                    :in-theory (e/d (expand-all
                                     expand-all-lst
                                     simple-term-vars-lst
                                     simple-term-vars
-                                    mextract-ev-of-fncall-args)
-                                   (mextract-ev-fn-get-def)))
+                                    ercp-ev-of-fncall-args)
+                                   (ercp-ev-fn-get-def)))
            ("Subgoal *1/4"
-            :use ((:instance mextract-ev-fn-get-def
+            :use ((:instance ercp-ev-fn-get-def
                    (fn (car term))
                    (args (cdr term))
                    (state state)
                    (st state)
-                   (a (mextract-ev-alist substs alist))))))))
+                   (a (ercp-ev-alist substs alist))))))))
 
 ;; NOTE: This is different from sublis-var1 in that the default mapping for a
 ;; variable in sublis-var1 is x -> x, while in substitute-in-term, the default
@@ -246,14 +358,14 @@
    (defthm-substitute-in-term-flag
      eval-of-substitute-in-term-lemma
      (defthm eval-of-substitute-in-term
-       (equal (mextract-ev x (mextract-ev-alist substs a))
-              (mextract-ev (substitute-in-term x substs) a))
+       (equal (ercp-ev x (ercp-ev-alist substs a))
+              (ercp-ev (substitute-in-term x substs) a))
        :hints ((and stable-under-simplificationp
-                    '(:in-theory (enable mextract-ev-of-fncall-args))))
+                    '(:in-theory (enable ercp-ev-of-fncall-args))))
        :flag substitute-in-term)
      (defthm eval-of-substitute-in-lst
-       (equal (mextract-ev-lst x (mextract-ev-alist substs a))
-              (mextract-ev-lst (substitute-in-lst x substs) a))
+       (equal (ercp-ev-lst x (ercp-ev-alist substs a))
+              (ercp-ev-lst (substitute-in-lst x substs) a))
        :flag substitute-in-lst))))
 
 ;; We now wish to show that the substitution-in-term of a term under an
@@ -319,28 +431,72 @@
         (substs (pairlis$ vars vars))
         ((mv ok redux) (expand-all term substs fns state)))
      (implies (and ok
-                   (mextract-ev-global-facts)
+                   (ercp-ev-meta-extract-global-facts)
                    (pseudo-termp term))
-              (equal (mextract-ev redux alist)
-                     (mextract-ev term alist))))
-   :hints (("Goal" :in-theory (e/d () (mextract-ev-alist-pairlis$))))))
+              (equal (ercp-ev redux alist)
+                     (ercp-ev term alist))))
+   :hints (("Goal" :in-theory (e/d () (ercp-ev-alist-pairlis$))))))
+
+(local
+ (defthm mv-nth-of-cons-alt
+   (implies (posp n)
+            (equal (mv-nth n (cons a b))
+                   (mv-nth (1- n) b)))
+   :hints (("Goal" :in-theory (e/d (mv-nth) ())))))
+
+(local
+ (defthm-push-mv-nths-flag correctness-of-push-mv-nth-lemma
+   (defthmd correctness-of-push-mv-nth
+     (b* ((redux (push-mv-nths term)))
+       (implies (pseudo-termp term)
+                (equal (ercp-ev redux alist)
+                       (ercp-ev term alist))))
+     :flag push-mv-nths)
+   (defthmd correctness-of-push-mv-nth-args
+     (b* ((redux (push-mv-nths-args terms)))
+       (implies (pseudo-term-listp terms)
+                (equal (ercp-ev-lst redux alist)
+                       (ercp-ev-lst terms alist))))
+     :flag push-mv-nths-args)
+   :hints (("Goal" :in-theory (e/d (push-mv-nths
+                                    push-mv-nths-args
+                                    ercp-ev-of-fncall-args) ())))))
 
 (local (include-book "arithmetic-5/top" :dir :system))
 
 (defthm expand-reduce-cp-correct
   (implies (and (pseudo-term-listp cl)
                 (alistp alist)
-                (mextract-ev-global-facts)
-                (mextract-ev (conjoin-clauses
-                              (clauses-result
-                               (expand-reduce-cp cl fns state)))
-                             alist))
-           (mextract-ev (disjoin cl) alist))
+                (ercp-ev-meta-extract-global-facts)
+                (ercp-ev (conjoin-clauses
+                        (clauses-result
+                         (expand-reduce-cp cl fns state)))
+                       alist))
+           (ercp-ev (disjoin cl) alist))
   :hints (("Goal" :in-theory (e/d (expand-reduce-cp
-                                   disjoin) ())
+                                   disjoin)
+                                  ())
                   :use ((:instance correctness-of-expand-all
-                         (term (cadr (car cl))))
+                         (term (cadr (car cl)))
+                         (fns (cons 'mv-list fns)))
                         (:instance correctness-of-expand-all
-                         (term (caddr (car cl)))))
+                         (term (caddr (car cl)))
+                         (fns (cons 'mv-list fns)))
+                        (:instance correctness-of-push-mv-nth
+                         (term (mv-nth
+                                1
+                                (expand-all (cadr (car cl))
+                                            (pairlis$ (simple-term-vars (cadr (car cl)))
+                                                      (simple-term-vars (cadr (car cl))))
+                                            (cons 'mv-list fns)
+                                            state))))
+                        (:instance correctness-of-push-mv-nth
+                         (term (mv-nth
+                                1
+                                (expand-all (caddr (car cl))
+                                            (pairlis$ (simple-term-vars (caddr (car cl)))
+                                                      (simple-term-vars (caddr (car cl))))
+                                            (cons 'mv-list fns)
+                                            state)))))
                   :do-not-induct t))
   :rule-classes :clause-processor)
