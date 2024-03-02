5f0d922faee7849498b10ad99750b478d668dedd
Drew Walter <mist3rwalter@gmail.com>
2024-03-02 18:44:17 -0500

    [acl2s] Actually move defxdoc-raws into doc.lisp
    Forgot to actually move some defxdoc-raws from acl2s-interface.lsp into
    doc.lisp.

--- books/acl2s/interface/doc.lisp
+++ books/acl2s/interface/doc.lisp
@@ -147,6 +147,170 @@ Note that using extended package prefix notation with a backquoted value will al
 })
 ")
 
+
+(xdoc::defxdoc acl2s-compute
+  :parents (acl2s-interface)
+  :short "Run a single-value ACL2 computation from Common Lisp"
+  :long "
+<b>General form</b>
+@({
+(acl2s-compute
+  form                   ; The form to evaluate. Should return a single value.
+  :quiet <bool>          ; Optional. Whether or not to suppress all ACL2 printed output. Defaults to nil.
+  :capture-output <bool> ; Optional. Whether or not to capture all ACL2 printed output. Defaults to nil.
+  ...)       ;; Any additional arguments will be passed to ld, except for :ld-error-action
+=>
+(list erp val)
+})
+<dl>
+<dt>Returns</dt>
+<dd>@('erp') is @('t') if an error occurred during execution of @('form'), and is @('nil') otherwise.</dd>
+<dd>@('val') is the single value that @('form') evaluated to.</dd>
+</dl>
+
+<p>
+The @('form') argument should be an ACL2 expression that evaluates to a single value. Be careful about symbol packages when using @('acl2s-compute') when inside a different package - you may need to fully specify the name of an ACL2 function when calling it. See @(see acl2s-interface-symbol-package-tips) for more information.
+</p>
+
+<p>
+When the @(':quiet') option is set to @('t'), @('acl2s-compute') will attempt to suppress all ACL2 printed output during evaluation of @('form'). This temporarily overrides the current @(see quiet-mode).
+</p>
+
+<p>
+When the @(':capture-output') option is set to @('t'), @('acl2s-compute') will attempt capture all ACL2 printed output during evaluation of @('form'). This temporarily overrides the current @(see capture-output).
+</p>
+
+<h4>Examples</h4>
+
+@({(acl2s-compute '(+ 1 2))})
+Returns (nil 3)
+@({(acl2s-compute '(+ 1 2) :quiet nil :ld-pre-eval-print t)})
+Returns (nil 3), does not attempt to suppress any ACL2 printed output, and passes @(':ld-pre-eval-print t') to @(see acl2::ld)
+@({(acl2s-compute '(append '(1 2) '(3 4)))})
+Returns (nil (1 2 3 4))
+@({(acl2s-compute '(+ 1 '(1 2)))})
+Returns (t nil) and prints out a guard violation error message
+@({(acl2s-compute '(mv nil t state))})
+")
+
+
+(xdoc::defxdoc acl2s-query
+  :parents (acl2s-interface)
+  :short "Run a multiple-value ACL2 computation from Common Lisp"
+  :long "
+<b>General form</b>
+@({
+(acl2s-query
+  form                         ; The form to evaluate. Should evaluate to an error triple.
+  :quiet <bool>                ; Optional. Whether or not to suppress all ACL2 printed output. Defaults to nil.
+  :capture-output <bool>       ; Optional. Whether or not to capture all ACL2 printed output. Defaults to nil.
+  :prover-step-limit <integer> ; Optional. Sets the prover step limit. Defaults to what ACL2 would have used
+                               ;           for a top-level evaluation (see set-prover-step-limit).
+  ...)                         ; Any additional arguments will be passed to ld, except for :ld-error-action
+=>
+(list erp val)
+})
+<dl>
+<dt>Returns</dt>
+<dd>@('erp') is the first value of the error triple that @('form') evaluated to, or @('nil') if an error occurred while evaluating @('form').</dd>
+<dd>@('val') is the second value of the error triple that @('form') evaluated to.</dd>
+</dl>
+
+<p>
+The @('form') argument should be an ACL2 expression that evaluates to an @(see acl2::error-triple). @('form') should also not change the state of the ACL2 world with respect to events. Be careful about symbol packages when using @('acl2s-query') when inside a different package - you may need to fully specify the name of an ACL2 function when calling it. See @(see acl2s-interface-symbol-package-tips) for more information.
+</p>
+
+<p>
+When the @(':quiet') option is set to @('t'), @('acl2s-query') will attempt to suppress all ACL2 printed output during evaluation of @('form'). This temporarily overrides the current @(see quiet-mode).
+</p>
+
+<p>
+When the @(':capture-output') option is set to @('t'), @('acl2s-query') will attempt capture all ACL2 printed output during evaluation of @('form'). This temporarily overrides the current @(see capture-output).
+</p>
+
+<p>
+@('acl2s-query') evaluates @('form') inside of a @(see acl2::with-prover-step-limit), where the step-limit is set to the value provided to @(':prover-step-limit'), or ACL2's set prover-step-limit if that option is not provided. See @(see acl2::set-prover-step-limit) for more information about the prover step-limit. If you don't want to limit the number of prover steps permitted for an event, set @(':prover-step-limit') to nil.
+</p>
+
+<h4>Examples</h4>
+@({(acl2s-query '(value (+ 1 2)))})
+Returns (nil 3)
+
+@({(acl2s-query '(thm (implies (natp x) (integerp x))))})
+Returns (nil :invisible)
+
+@({(acl2s-query '(mv nil t state))})
+Returns (nil t)
+
+@({(acl2s-query '(value (+ 1 2)) :quiet nil :ld-pre-eval-print t)})
+Returns (nil 3), does not attempt to suppress any ACL2 printed output, and passes @(':ld-pre-eval-print t') to @(see acl2::ld)
+
+@({(acl2s-query '(trans-eval '(+ 1 2) 'my-ctx state nil))})
+Returns (nil ((nil) . 3)). See @(see acl2::trans-eval) for more information
+about trans-eval's return value.
+
+@({(acl2s-query '(test? (implies (integerp x) (natp x))))})
+Returns (t nil)
+")
+
+
+(xdoc::defxdoc acl2s-event
+  :parents (acl2s-interface)
+  :short "Install an ACL2 event from Common Lisp"
+  :long "
+<b>General form</b>
+@({
+(acl2s-event
+  form                         ; The form to evaluate. Should return an error triple.
+  :quiet <bool>                ; Optional. Whether or not to suppress all ACL2 printed output. Defaults to nil.
+  :capture-output <bool>       ; Optional. Whether or not to capture all ACL2 printed output. Defaults to nil.
+  :prover-step-limit           ; Optional. Sets the prover step limit. Defaults to what ACL2 would have used
+                               ;           for a top-level evaluation (see acl2::set-prover-step-limit).
+  ...)                         ; Any additional arguments will be passed to ld, except for :ld-error-action.
+=>
+(list erp nil)
+})
+<dl>
+<dt>Returns</dt>
+<dd>@('erp') is `nil` if ld indicates that the form was successfully evaluated.</dd>
+</dl>
+
+<p>
+The @('form') argument should be an ACL2 expression that evaluates to an @(see acl2::error-triple). Be careful about symbol packages when using @('acl2s-event') when inside a different package - you may need to fully specify the name of an ACL2 function when calling it. See @(see acl2s-interface-symbol-package-tips) for more information.
+</p>
+
+<p>
+When the @(':quiet') option is set to @('t'), @('acl2s-event') will attempt to suppress all ACL2 printed output during evaluation of @('form'). This temporarily overrides the current @(see quiet-mode).
+</p>
+
+<p>
+When the @(':capture-output') option is set to @('t'), @('acl2s-event') will attempt capture all ACL2 printed output during evaluation of @('form'). This temporarily overrides the current @(see capture-output).
+</p>
+
+<p>
+@('acl2s-event') evaluates @('form') inside of a @(see acl2::with-prover-step-limit), where the step-limit is set to the value provided to @(':prover-step-limit'), or ACL2's set prover-step-limit if that option is not provided. See @(see acl2::set-prover-step-limit) for more information about the prover step-limit. If you don't want to limit the number of prover steps permitted for an event, set @(':prover-step-limit') to nil.
+</p>
+
+<h4>Examples</h4>
+@({(acl2s-event '(definec f (x :int) :int (* x x)))})
+Returns (nil nil), and defines the function @('f') in the ACL2 world.
+
+@({(acl2s-event '(definec g (x :int) :int (* 5 x)) :quiet t)})
+Returns (nil nil), and defines the function @('g') in the ACL2 world. Tries to suppresses all ACL2 printed output during this process.
+
+@({(acl2s-event '(defthm triangle-inequality
+                              (implies (and (natp x) (natp y))
+                                       (>= (+ (abs x) (abs y)) (abs (+ x y)))))
+              :prover-step-limit 200)})
+Returns (t nil), indicating that the @('defthm') did not successfully execute. This is because the @('defthm') was limited to 200 prover steps, and more prover steps than that are required to prove it.
+
+@({(acl2s-event '(defthm triangle-inequality
+                              (implies (and (natp x) (natp y))
+                                       (>= (+ (abs x) (abs y)) (abs (+ x y)))))
+              :prover-step-limit 1000)})
+Returns (nil nil), and defines the theorem @('triangle-inequality') in the ACL2 world. It limits the @('defthm') call to 1000 prover steps.
+")
+
 (xdoc::defxdoc acl2s-interface-utils
   :parents (acl2s-interface)
   :pkg "ACL2S-INTERFACE-EXTRAS"
