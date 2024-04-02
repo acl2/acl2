c7911028ac55d0a2fd55600f6a8496f4de34a2b9
Eric Smith <ewsmith@gmail.com>
2024-04-02 01:42:08 -0700

    [utilities] Add string-listp-of-map-symbol-name.
    Also keep map-symbol-name disabled.

--- books/kestrel/utilities/map-symbol-name.lisp
+++ books/kestrel/utilities/map-symbol-name.lisp
@@ -1,6 +1,6 @@
 ; A utility to apply symbol-name to every symbol in a list
 ;
-; Copyright (C) 2020 Kestrel Institute
+; Copyright (C) 2020-2024 Kestrel Institute
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
 ;
@@ -10,9 +10,14 @@
 
 (in-package "ACL2")
 
-(defun map-symbol-name (syms)
+;; Returns a list of strings
+(defund map-symbol-name (syms)
   (declare (xargs :guard (symbol-listp syms)))
   (if (endp syms)
       nil
     (cons (symbol-name (first syms))
           (map-symbol-name (rest syms)))))
+
+(defthm string-listp-of-map-symbol-name
+  (string-listp (map-symbol-name syms))
+  :hints (("Goal" :in-theory (enable map-symbol-name))))
