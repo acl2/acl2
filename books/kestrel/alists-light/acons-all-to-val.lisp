fb02f6f8363d2209744fab5c6572e56e976fad81
Eric Smith <ewsmith@gmail.com>
2024-04-04 10:58:18 -0700

    [alists-light] Add 2 rules.

--- books/kestrel/alists-light/acons-all-to-val.lisp
+++ books/kestrel/alists-light/acons-all-to-val.lisp
@@ -10,9 +10,23 @@
 
 (in-package "ACL2")
 
+(local (include-book "kestrel/lists-light/append" :dir :system))
+(local (include-book "kestrel/lists-light/reverse" :dir :system))
+
 (defund acons-all-to-val (keys val alist)
   (declare (xargs :guard (and (true-listp keys)
                               (alistp alist))))
   (if (endp keys)
       alist
     (acons-all-to-val (rest keys) val (acons (first keys) val alist))))
+
+(defthm alistp-of-acons-all-to-val
+  (implies (alistp alist)
+           (alistp (acons-all-to-val keys val alist)))
+  :hints (("Goal" :in-theory (enable acons-all-to-val))))
+
+(defthm strip-cars-of-acons-all-to-val
+  (equal (strip-cars (acons-all-to-val keys val alist))
+         (append (reverse keys)
+                 (strip-cars alist)))
+  :hints (("Goal" :in-theory (enable acons-all-to-val strip-cars append))))
