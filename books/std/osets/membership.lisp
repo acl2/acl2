dc34cd1dfa0acbd26411eec50dfd4e75f8d992f1
Alessandro Coglio <>
2024-04-13 16:26:12 -0700

    [Std/osets] Downcase some code.

--- books/std/osets/membership.lisp
+++ books/std/osets/membership.lisp
@@ -800,23 +800,23 @@ directed by @(see accumulated-persistence).</p>"
 	     (double-tail-induction (tail X) (tail Y)))))
 
   (local (defthm double-containment-is-equality-lemma
-	   (IMPLIES (AND (NOT (OR (EMPTYP X) (EMPTYP Y)))
-			 (IMPLIES (AND (SUBSET (TAIL X) (TAIL Y))
-				       (SUBSET (TAIL Y) (TAIL X)))
-				  (EQUAL (EQUAL (TAIL X) (TAIL Y)) T))
-			 (SETP X)
-			 (SETP Y)
-			 (SUBSET X Y)
-			 (SUBSET Y X))
-		    (EQUAL (EQUAL X Y) T))
+	   (implies (and (not (or (emptyp x) (emptyp y)))
+			 (implies (and (subset (tail x) (tail y))
+				       (subset (tail y) (tail x)))
+				  (equal (equal (tail x) (tail y)) t))
+			 (setp x)
+			 (setp y)
+			 (subset x y)
+			 (subset y x))
+		    (equal (equal x y) t))
 	   :hints(("Goal"
                    :in-theory (enable head-tail-same)
 		   :use ((:instance double-containment-lemma-tail
-				    (X X) (Y Y))
+				    (x x) (y y))
 			 (:instance double-containment-lemma-tail
-				    (X Y) (Y X))
+				    (x y) (y x))
 			 (:instance double-containment-lemma-head
-				    (X X) (Y Y)))))))
+				    (x x) (y y)))))))
 
   (local (defthmd double-containment-is-equality
 	   (implies (and (setp X)
