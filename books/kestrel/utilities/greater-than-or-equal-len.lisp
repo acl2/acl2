700935be166cbbaf1f359433afb2092c9082625d
Eric Smith <ewsmith@gmail.com>
2024-04-17 22:31:58 -0700

    [utilities] Have the book about >=-len disable it.

--- books/kestrel/utilities/greater-than-or-equal-len.lisp
+++ books/kestrel/utilities/greater-than-or-equal-len.lisp
@@ -1,6 +1,6 @@
 ; A lightweight book about the built-in function >=-len.
 ;
-; Copyright (C) 2015-2023 Kestrel Institute
+; Copyright (C) 2015-2024 Kestrel Institute
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
 ;
@@ -10,7 +10,10 @@
 
 (in-package "ACL2")
 
+(in-theory (disable >=-len))
+
 (defthm >=-len-rewrite
   (implies (natp n)
            (equal (>=-len x n)
-                  (>= (len x) n))))
+                  (>= (len x) n)))
+  :hints (("Goal" :in-theory (enable >=-len))))
