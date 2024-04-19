28a8d87fea2b7a011f910e247ddbfe15c2e0197d
Eric Smith <ewsmith@gmail.com>
2024-04-19 15:20:23 -0700

    [axe] Add missing file header.

--- books/kestrel/axe/result-alist.lisp
+++ books/kestrel/axe/result-alist.lisp
@@ -1,3 +1,13 @@
+; Fast alists for looking up what nodes rewrote to
+;
+; Copyright (C) 2023-2024 Kestrel Institute
+;
+; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
+;
+; Author: Eric Smith (eric.smith@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (in-package "ACL2")
 
 (include-book "dargp-less-than")
