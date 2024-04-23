43e4444edbd90426979c74e0709b169d25d58525
Eric Smith <ewsmith@gmail.com>
2024-04-23 05:24:44 -0700

    [axe] Get a file guard verified.

--- books/kestrel/axe/contexts2.lisp
+++ books/kestrel/axe/contexts2.lisp
@@ -1,7 +1,7 @@
 ; More material on contexts
 ;
 ; Copyright (C) 2008-2011 Eric Smith and Stanford University
-; Copyright (C) 2013-2020 Kestrel Institute
+; Copyright (C) 2013-2024 Kestrel Institute
 ; Copyright (C) 2016-2020 Kestrel Technology, LLC
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
@@ -14,104 +14,113 @@
 
 ;; This book contains context-related material used by the Equivalence Checker but not the Rewriter.
 
-;; TODO: Deal with skip-proofs
-
-;; TODO: Need to handle the presence of :default in this kind of context array
-
 (include-book "contexts")
 (local (include-book "kestrel/arithmetic-light/plus" :dir :system))
+(local (include-book "kestrel/typed-lists-light/nat-listp" :dir :system))
 
 ;could make tail rec
-;rename?
-(defund get-not-done-nodes (nodenums context-array)
-  (declare (xargs :guard (and (array1p 'context-array context-array)
-                              (true-listp nodenums)
-                              (all-natp nodenums)
-                              (all-< nodenums (alen1 'context-array context-array)))))
+(defund get-not-done-nodes (nodenums done-array)
+  (declare (xargs :guard (and (array1p 'done-array-for-context done-array)
+                              (nat-listp nodenums)
+                              (all-< nodenums (alen1 'done-array-for-context done-array)))))
   (if (endp nodenums)
       nil
-    (let* ((nodenum (first nodenums))
-           (context (aref1 'context-array context-array nodenum)))
-      (if (eq :default context)
-          (cons nodenum (get-not-done-nodes (rest nodenums) context-array))
-        (get-not-done-nodes (rest nodenums) context-array)))))
+    (let ((nodenum (first nodenums)))
+      (if (aref1 'done-array-for-context done-array nodenum)
+          (get-not-done-nodes (rest nodenums) done-array)
+        (cons nodenum (get-not-done-nodes (rest nodenums) done-array))))))
 
-(defthm all-natp-of-get-not-done-nodes
-  (implies (all-natp nodenums)
-           (all-natp (get-not-done-nodes nodenums context-array)))
-  :hints (("Goal" :in-theory (enable get-not-done-nodes))))
+(local
+  (defthm nat-listp-of-get-not-done-nodes
+    (implies (nat-listp nodenums)
+             (nat-listp (get-not-done-nodes nodenums done-array)))
+    :hints (("Goal" :in-theory (enable get-not-done-nodes)))))
 
-(defthm all-<-of-get-not-done-nodes
-  (implies (all-< nodenums val)
-           (all-< (get-not-done-nodes nodenums context-array) val))
-  :hints (("Goal" :in-theory (enable get-not-done-nodes))))
+(local
+  (defthm all-<-of-get-not-done-nodes
+    (implies (all-< nodenums val)
+             (all-< (get-not-done-nodes nodenums done-array) val))
+    :hints (("Goal" :in-theory (enable get-not-done-nodes)))))
 
 ;;fixme what if there is a node with no parents?!
 ;fills in CONTEXT-ARRAY for the nodenums in WORKLIST and all of their ancestors
-(defun make-context-array-for-ancestors (count worklist dag-array-name dag-array dag-len top-nodenum dag-parent-array context-array)
+(defun make-context-array-for-ancestors (count worklist dag-array-name dag-array dag-len dag-parent-array context-array done-array)
   (declare (xargs :measure (nfix (+ 1 count))
-                  :guard (and (integerp count)
-                              (natp top-nodenum)
+                  :guard (and (integerp count) ; may go down to -1
                               (pseudo-dag-arrayp dag-array-name dag-array dag-len)
-                              (< top-nodenum dag-len)
-                              (true-listp worklist)
-                              (all-natp worklist)
-                              (all-< worklist (+ 1 top-nodenum))
+                              (nat-listp worklist)
+                              (all-< worklist dag-len)
                               (dag-parent-arrayp 'dag-parent-array-for-context dag-parent-array)
                               (equal (alen1 'dag-parent-array-for-context dag-parent-array)
-                                     (alen1 dag-array-name dag-array))
-                              (bounded-dag-parent-entriesp top-nodenum 'dag-parent-array-for-context dag-parent-array (+ 1 top-nodenum))
-                              (context-arrayp 'context-array context-array dag-len) ; needed for set-context-of-nodenum but not true if we use :default
-                              )
+                                     dag-len)
+                              (bounded-dag-parent-entriesp (+ -1 dag-len) 'dag-parent-array-for-context dag-parent-array dag-len)
+                              (context-arrayp 'context-array context-array dag-len) ; needed for set-context-of-nodenum
+                              (array1p 'done-array-for-context done-array)
+                              (equal (alen1 'done-array-for-context done-array)
+                                     dag-len))
                   :guard-hints (("Goal" :in-theory (enable dag-parent-arrayp <-of-+-of-1-strengthen-2)))))
   (if (not (natp count))
       (prog2$ (er hard? 'make-context-array-for-ancestors "Limit reached.")
               context-array)
     (if (endp worklist)
         context-array
-      (let* ((nodenum (first worklist))
-             (context (aref1 'context-array context-array nodenum)))
-        (if (not (eq :default context))
-            ;;this node is already done
-            (make-context-array-for-ancestors (+ -1 count) (rest worklist) dag-array-name dag-array dag-len top-nodenum dag-parent-array context-array)
-          (if (eql nodenum top-nodenum)
-              ;;it's the top node:
-              (make-context-array-for-ancestors (+ -1 count) (rest worklist) dag-array-name dag-array dag-len top-nodenum dag-parent-array
-                                                (aset1 'context-array context-array nodenum (true-context))) ;don't know any context preds for the top node
-            ;;node isn't done yet (and isn't the top node):
-            (let* ((parent-nodenums (aref1 'dag-parent-array-for-context dag-parent-array nodenum))
-                   (not-done-parents (get-not-done-nodes parent-nodenums context-array)))
-              (if not-done-parents
-                  ;; do the parents first (will reprocess this node after the parents are done):
-                  (make-context-array-for-ancestors (+ -1 count) (append not-done-parents worklist) dag-array-name dag-array dag-len top-nodenum dag-parent-array context-array)
-                ;;all parents are done:
-                (make-context-array-for-ancestors (+ -1 count)
-                                                  (rest worklist) dag-array-name dag-array dag-len top-nodenum dag-parent-array
-                                                  (set-context-of-nodenum nodenum parent-nodenums dag-array-name dag-array dag-len context-array))))))))))
+      (let ((nodenum (first worklist)))
+        (if (aref1 'done-array-for-context done-array nodenum)
+            ;; this node is already done:
+            (make-context-array-for-ancestors (+ -1 count) (rest worklist) dag-array-name dag-array dag-len dag-parent-array context-array done-array)
+          ;;node isn't done yet (and isn't the top node since that one is always done):
+          (let* ((parent-nodenums (aref1 'dag-parent-array-for-context dag-parent-array nodenum))
+                 (not-done-parents (get-not-done-nodes parent-nodenums done-array)))
+            (if not-done-parents
+                ;; do the parents first (will reprocess this node after the parents are done):
+                (make-context-array-for-ancestors (+ -1 count) (append not-done-parents worklist) dag-array-name dag-array dag-len dag-parent-array context-array done-array)
+              ;;all parents are done, so now we can handle this node:
+              (make-context-array-for-ancestors (+ -1 count)
+                                                (rest worklist) dag-array-name dag-array dag-len dag-parent-array
+                                                (set-context-of-nodenum nodenum parent-nodenums dag-array-name dag-array dag-len context-array)
+                                                (aset1 'done-array-for-context done-array nodenum t)))))))))
+
+(local
+  (defthm make-context-array-for-ancestors-return-type
+    (implies (and (integerp count) ; may go down to -1
+                  (pseudo-dag-arrayp dag-array-name dag-array dag-len)
+                  (nat-listp worklist)
+                  (all-< worklist dag-len)
+                  (dag-parent-arrayp 'dag-parent-array-for-context dag-parent-array)
+                  (equal (alen1 'dag-parent-array-for-context dag-parent-array)
+                         dag-len)
+                  (bounded-dag-parent-entriesp (+ -1 dag-len) 'dag-parent-array-for-context dag-parent-array dag-len)
+                  (context-arrayp 'context-array context-array dag-len) ; needed for set-context-of-nodenum
+                  (array1p 'done-array-for-context done-array)
+                  (equal (alen1 'done-array-for-context done-array)
+                         dag-len))
+             (and (array1p 'context-array (make-context-array-for-ancestors count worklist dag-array-name dag-array dag-len dag-parent-array context-array done-array))
+                  (equal (alen1 'context-array (make-context-array-for-ancestors count worklist dag-array-name dag-array dag-len dag-parent-array context-array done-array))
+                         (alen1 'context-array context-array))))
+    :hints (("Goal" :in-theory (enable make-context-array-for-ancestors)))))
 
 ;returns the context for NODENUM (to compute it, this first computes the contexts for all of its ancestors)
 ;would be faster if we could pass in a parent array instead of making it here
-;smashes 'context-array
-(defun get-context-for-nodenum (nodenum dag-array-name dag-array dag-len ;tag-array2
+;smashes 'context-array and 'dag-parent-array-for-context and 'done-array-for-context
+(defun get-context-for-nodenum (nodenum dag-array-name dag-array
+                                        dag-len ; better would be to pass the top-nodenum since that context computation starts there?
                                         )
-;  (declare (ignore tag-array2)) ;we could perhaps use this to not consider context nodes that we still think are probably equal to other things?  maybe this all is fast enough now that we don't need to do that
   (declare (xargs :guard (and (pseudo-dag-arrayp dag-array-name dag-array dag-len)
                               (posp dag-len)
                               (natp nodenum)
-                              ;(< nodenum dag-len)
-                              )
-                  :verify-guards nil ;perhaps not true, due to the use of :default
-                  ))
-  (let* ((dag-parent-array (make-minimal-dag-parent-array-with-name dag-len dag-array-name dag-array 'dag-parent-array-for-context)) ;ffixme only compute parents for nodes above nodenum?
-         (context-array (make-empty-array-with-default 'context-array dag-len :default)) ;todo: using :default here makes it not a valid context-array!
+                              (< nodenum dag-len))))
+  (let* ((dag-parent-array (make-minimal-dag-parent-array-with-name dag-len dag-array-name dag-array 'dag-parent-array-for-context)) ; todo: only compute parents for nodes above nodenum?
+         ;; Top node has no context (only the value for top-nodenum here will actually be used):
+         (context-array (make-empty-array-with-default 'context-array dag-len (true-context)))
+         (top-nodenum (+ -1 dag-len))
+         (done-array (make-empty-array 'done-array-for-context dag-len))
+         (done-array (aset1 'done-array-for-context done-array top-nodenum t)) ; mark top node as done
          (context-array (make-context-array-for-ancestors 1000000000
                                                           (list nodenum) ;initial worklist
                                                           dag-array-name
                                                           dag-array
                                                           dag-len
-                                                          (+ -1 dag-len) ;top nodenum
                                                           dag-parent-array
-                                                          context-array)))
+                                                          context-array
+                                                          done-array)))
     (aref1 'context-array context-array nodenum)))
-
-(skip-proofs (verify-guards get-context-for-nodenum)) ;need properties of MAKE-CONTEXT-ARRAY-FOR-ANCESTORS
