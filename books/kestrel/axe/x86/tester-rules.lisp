3766ef65029bec58ed8e1936dda699980a7c81f1
Eric Smith <ewsmith@gmail.com>
2024-05-14 13:17:28 -0700

    [axe/x86] Fix typo in rule name.

--- books/kestrel/axe/x86/tester-rules.lisp
+++ books/kestrel/axe/x86/tester-rules.lisp
@@ -673,7 +673,7 @@
            (not (sbvlt 128 k (bvsx 128 64 x))))
   :hints (("Goal" :in-theory (enable acl2::bvsx-alt-def-2 acl2::booland bvlt acl2::equal-of-slice acl2::sbvlt-rewrite))))
 
-(defthm bvcat-of-repeatit-tighten-64-32 ;gen!
+(defthm bvcat-of-repeatbit-tighten-64-32 ;gen!
   (equal (bvcat 64 (repeatbit 32 bit) 32 lowval)
          (bvcat 32 (repeatbit 32 bit) 32 lowval)))
 
