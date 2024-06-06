8dc7e486d0dcfa28c697647664261b68c0916a60
Eric Smith <ewsmith@gmail.com>
2024-06-05 19:54:52 -0700

    [bv] Collect some intro rules.

--- books/kestrel/bv/intro.lisp
+++ books/kestrel/bv/intro.lisp
@@ -17,6 +17,7 @@
 (include-book "bvminus")
 (include-book "bv-syntax")
 (include-book "bvcat-def")
+(include-book "bvif")
 (include-book "defs-bitwise")
 (include-book "unsigned-byte-p-forced")
 (include-book "ihs/basic-definitions" :dir :system) ;for logmaskp
@@ -172,3 +173,17 @@
                          amt
                          0)))
   :hints (("Goal" :in-theory (enable bvcat ash))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defthm bvif-of---arg3
+  (implies (integerp x)
+           (equal (bvif size test (- x) z)
+                  (bvif size test (bvuminus size x) z)))
+  :hints (("Goal" :in-theory (enable bvif bvuminus bvminus))))
+
+(defthm bvif-of---arg4
+  (implies (integerp x)
+           (equal (bvif size test z (- x))
+                  (bvif size test z (bvuminus size x))))
+  :hints (("Goal" :in-theory (enable bvif bvuminus bvminus))))
