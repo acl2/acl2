cc1ff3fd87620a6e010aec65d43866799bee6e20
Eric Smith <ewsmith@gmail.com>
2024-07-14 14:48:12 -0700

    [terms-light] Fix printing and clean up.

--- books/kestrel/terms-light/substitute-unnecessary-lambda-vars2.lisp
+++ books/kestrel/terms-light/substitute-unnecessary-lambda-vars2.lisp
@@ -13,6 +13,9 @@
 ;; Note: Consider calling drop-trivial-lambdas after calling this utility.  Or
 ;; build that into this (perhaps as an option).
 
+;; TODO: This may have to be iterated multiple times if there are multiple
+;; nested lambdas inside the one we are operating on.  Add an example.
+
 (include-book "classify-lambda-formals")
 (include-book "count-vars")
 (include-book "substitute-lambda-formals") ; for subst-formals-in-lambda-application; make those names more consistent
@@ -51,11 +54,10 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; todo: deprecate the other one? but add back special treatment of for mv-nth (more generally, any set of functions to avoid)
+;; todo: deprecate the other one?
 (mutual-recursion
  (defun substitute-unnecessary-lambda-vars-in-term2 (term print hands-off-fns)
    (declare (xargs :guard (and (pseudo-termp term)
-                               ;; (no-duplicate-lambda-formals-in-termp term) ; because of the call of subst-formals-in-lambda-application
                                (true-listp hands-off-fns) ; usually symbols buy perhaps lambdas as well
                                )
                    :measure (acl2-count term)
@@ -74,22 +76,18 @@
                   ;;apply recursively to the lambda body:
                   (lambda-body (substitute-unnecessary-lambda-vars-in-term2 lambda-body print hands-off-fns))
                   (formal-arg-alist (pairlis$ formals args))
-                  ;; (trivial-formals (trivial-formals formals args))
-                  ;; (formals-bound-to-mv-nths (vars-bound-to-mv-nths formals args))
-                  (non-trivial-formals (non-trivial-formals formals args))
                   ;; We try to substitute for a lambda var if:
-                  ;; 1) It appears only once in the lambda-body
-                  ;; and
-                  ;; 2) It is not bound to itself (trivial formals
+                  ;; 1) It is not bound to itself (trivial formals
                   ;; don't really "count against" us, since lambdas must be closed)
                   ;; and
-                  ;; 3) It is not bound to any of the hands-off-fns (e.g. mv-nth ,to avoid messing up MV-LET patterns)
+                  ;; 2) It appears only once in the lambda-body.
+                  ;; and
+                  ;; 3) It is not bound to any of the hands-off-fns (e.g. mv-nth, to avoid messing up MV-LET patterns)
                   ;; and
                   ;; 4) It is bound to a term that does not mention any of the remaining non-trivial formals.
+                  (non-trivial-formals (non-trivial-formals formals args))
                   (formals-to-maybe-subst (vars-that-appear-only-once non-trivial-formals lambda-body))
                   (formals-to-maybe-subst (formals-whose-args-call-none formals-to-maybe-subst (map-lookup-equal formals-to-maybe-subst formal-arg-alist) hands-off-fns))
-                  ;; (formals-to-maybe-subst (set-difference-eq formals-to-maybe-subst trivial-formals))
-                  ;; (formals-to-maybe-subst (set-difference-eq formals-to-maybe-subst formals-bound-to-mv-nths)) ; todo: make this optional
                   ;; (formals-to-drop (vars-expressible-without-clashes formals-to-maybe-subst formal-arg-alist non-trivial-formals)) ; would be ok to mention formals we are substituting?
                   ;; We can't subst for any formal whose arg mentions a non-trivial formal that remains:
                   ;(bad-arg-vars (set-difference-eq non-trivial-formals formals-to-maybe-subst))
@@ -100,15 +98,14 @@
              (mv-let (formals-to-subst formals-to-keep)
                (classify-lambda-formals formals-to-maybe-subst formal-arg-alist formals-to-keep)
                (declare (ignore formals-to-keep)) ; todo
-               (progn$ (and print "Will subst for ~x0 in lambda.~%" formals-to-subst)
+               (progn$ (and print (cw "Will subst for ~x0 in lambda.~%" formals-to-subst))
                        (subst-formals-in-lambda-application formals lambda-body args formals-to-subst))))
-         ;;not a lambda application, so just rebuild the function call:
+         ;; not a lambda application, so just rebuild the function call:
          (cons-with-hint fn args term)))))
 
  (defun substitute-unnecessary-lambda-vars-in-terms2 (terms print hands-off-fns)
    (declare (xargs :measure (acl2-count terms)
                    :guard (and (pseudo-term-listp terms)
-                               ;; (no-duplicate-lambda-formals-in-termsp terms)
                                (true-listp hands-off-fns) ; usually symbols buy perhaps lambdas as well
                                )))
    (if (endp terms)
