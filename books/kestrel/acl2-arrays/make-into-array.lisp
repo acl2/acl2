afc7af86bc192b7ab5aed5302ef0e30bc40f5cd0
Eric Smith <ewsmith@gmail.com>
2024-07-18 16:56:44 -0700

    [acl2-arrays] Reduce deps.
    Also disable array1p-of-cons-header (caused problems).

--- books/kestrel/acl2-arrays/make-into-array.lisp
+++ books/kestrel/acl2-arrays/make-into-array.lisp
@@ -15,7 +15,7 @@
 (include-book "make-into-array-with-len")
 (include-book "make-empty-array")
 (local (include-book "array1p"))
-(local (include-book "acl2-arrays")) ; todo: reducen
+(local (include-book "acl2-arrays")) ; todo: reduce
 (local (include-book "kestrel/arithmetic-light/plus" :dir :system))
 
 ;Consider adding an option to reuse an existing array if large enough (well, compress1 now does that internally)?
