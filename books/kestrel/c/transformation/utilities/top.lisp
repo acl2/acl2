6b3688f4cc351f39b57c567c4265e74dd706407b
Grant Jurgensen <grant@jurgensen.dev>
2024-07-24 14:24:49 -0500

    [c2c] Add utilities/free-vars
    This walks over an AST and returns the set of free variables.

---
+++ books/kestrel/c/transformation/utilities/top.lisp
@@ -0,0 +1,22 @@
+; C Library
+;
+; Copyright (C) 2024 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Grant Jurgensen (grant@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "C2C")
+
+(include-book "xdoc/defxdoc-plus" :dir :system)
+(include-book "xdoc/constructors" :dir :system)
+
+(include-book "free-vars")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defxdoc+ utilities
+  :parents (transformation-tools)
+  :short "Utilities for C transformations")
