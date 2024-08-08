af2a6893b23e01071c3343c07bc13904a6476fad
Alessandro Coglio <coglio@kestrel.edu>
2024-08-07 22:34:10 -0700

    [AleoBFT] Complete a file header.

--- books/projects/aleobft/dynamic/top.lisp
+++ books/projects/aleobft/dynamic/top.lisp
@@ -2,6 +2,8 @@
 ;
 ; Copyright (C) 2024 Provable Inc.
 ;
+; License: See the LICENSE file distributed with this library.
+;
 ; Authors: Alessandro Coglio (www.alessandrocoglio.info)
 ;          Eric McCarthy (bendyarm on GitHub)
 
