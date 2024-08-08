2006926180c0e88841106a687696769ebc2044a1
Alessandro Coglio <coglio@kestrel.edu>
2024-08-08 14:42:27 -0700

    [AleoBFT static] Update some obsolete doc.

--- books/projects/aleobft/static/validator-states.lisp
+++ books/projects/aleobft/static/validator-states.lisp
@@ -140,15 +140,7 @@
       which events are possible and which new states they lead to.")
     (xdoc::li
      "The number of the last round at which
-      this validator has committed an anchor.
-      This is calculable from the blockchain state component,
-      since blocks include round numbers (see @(tsee block));
-      this will be stated and proved as an invariant,
-      under additional invariant conditions on
-      the blockchain's round numbers being all even
-      (because each block comes from an anchor)
-      and being in strictly increasing order from right to left
-      (because anchors are committed in order).")
+      this validator has committed an anchor.")
     (xdoc::li
      "The blockchain as seen by the validator.
       We model it as a list of blocks from right to left,
