b0cc4c2e5c1a6646a811c76eacaa9244a5bf18fe
Alessandro Coglio <coglio@kestrel.edu>
2024-08-12 20:10:14 -0700

    [C$] Add initial model of types for validator.

--- books/kestrel/c/syntax/validator.lisp
+++ books/kestrel/c/syntax/validator.lisp
@@ -39,10 +39,153 @@
      i.e. to check whether the C code is valid and compilable.
      This work has just started:
      we may start with a partial validator,
-     which we will extend to a full validator."))
+     which we will extend to a full validator.")
+   (xdoc::p
+    "We start with an approximate validator
+     that should accept all valid C code
+     but also some invalid C code (due to the approximation).
+     Even in its approximate form,
+     this may be useful to perform some validation,
+     and to calculate information (approximate types)
+     that may be useful for manipulating the abstract syntax
+     (e.g. for C-to-C transformations).")
+   (xdoc::p
+    "In a sense, the validator extends the @(see disambiguator),
+     which performs an even more approximate validation,
+     just enough to disambiguate the abstract syntax.
+     Thus, there are structural similarities between
+     the validator and the disambiguator.")
+   (xdoc::p
+    "Similarly to a compiler, the validator makes use of a symbol table,
+     which tracks information about the symbols (identifiers) in the code.
+     These symbol tables, called `validation tables', are, in some sense,
+     an extension of the disambiguation tables used by the disambiguator.")
+   (xdoc::p
+    "We use "
+    (xdoc::seetopic "acl2::error-value-tuples" "error-value tuples")
+    " to handle errors in the validator.")
+   (xdoc::p
+    "This validator is work in progress."))
   :order-subtopics t
   :default-parent t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; TODO
+(fty::deftagsum type
+  :short "Fixtype of C types [C:6.2.5]."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "Currently we do not model all the C types in detail,
+     but only an approximate version of them,
+     which still lets us perform some validation.
+     We plan to refine the types, and the rest of the validator,
+     to cover exactly all the validity checks prescribed by [C]
+     (as well as applicable GCC extensions).")
+   (xdoc::p
+    "We capture the following types:")
+   (xdoc::ul
+    (xdoc::li
+     "The @('void') type [C:6.2.5/19].")
+    (xdoc::li
+     "The plain @('char') type [C:6.2.5/3].")
+    (xdoc::li
+     "The five standard signed integer types [C:6.2.5/4]
+      and the corresponding unsigned integer types [C:6.2.5/6].")
+    (xdoc::li
+     "The three real floating point types [C:6.2.5/10].")
+    (xdoc::li
+     "The three complex types [C:6.2.5/11].
+      These are a conditional feature,
+      but they must be included in this fixtype
+      because this fixtype consists of all the possible types.")
+    (xdoc::li
+     "The @('_Bool') type [C:6.2.5/2].")
+    (xdoc::li
+     "A collective type for all structure types [C:6.2.5/20].
+      This is an approximation,
+      because there are different structure types.")
+    (xdoc::li
+     "A collective type for all union types [C:6.2.5/20].
+      This is an approximation,
+      because there are different union types.")
+    (xdoc::li
+     "A collective type for all enumeration types [C:6.2.5/20].
+      This is an approximation,
+      because there are different enumeration types.")
+    (xdoc::li
+     "A collective type for all array types [C:6.2.5/20].
+      This is an approximation,
+      because there are different array types.")
+    (xdoc::li
+     "A collective type for all pointer types [C:6.2.5/20].
+      This is an approximation,
+      because there are different pointer types.")
+    (xdoc::li
+     "A collective type for all function types [C:6.2.5/20].
+      This is an approximation,
+      because there are different function types.")
+    (xdoc::li
+     "An ``unknown'' type that we need due to our current approximation.
+      Our validator must not reject valid code.
+      But due to our approximate treatment of types,
+      we cannot always calculate a type,
+      e.g. for a member expression of the form @('s.m')
+      where @('s') is an expression with structure type.
+      Since our approximate type for all structure types
+      has no information about the members,
+      we cannot calculate any actual type for @('s.m');
+      but if the expression is used elsewhere,
+      we need to accept it, because it could have the right type.
+      We use this unknown type for this purpose:
+      the expression @('s.m') has unknown type,
+      and unknown types are always acceptable."))
+   (xdoc::p
+    "Besides the approximations noted above,
+     currently we do not capture atomic types [C:6.2.5/20],
+     which we approximate as the underlying (argument) type.
+     We also do not capture @('typedef') names,
+     which we approximate as unknown types.
+     Furthermore, we do not capture qualified types [C:6.2.5/26]."))
+  (:void ())
+  (:char ())
+  (:schar ())
+  (:uchar ())
+  (:sshort ())
+  (:ushort ())
+  (:sint ())
+  (:uint ())
+  (:slong ())
+  (:ulong ())
+  (:sllong ())
+  (:ullong ())
+  (:float ())
+  (:double ())
+  (:ldouble ())
+  (:floatc ())
+  (:doublec ())
+  (:ldoublec ())
+  (:bool ())
+  (:struct ())
+  (:union ())
+  (:enum ())
+  (:array ())
+  (:pointer ())
+  (:function ())
+  (:unknown ())
+  :pred typep)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(fty::defoption type-option
+  type
+  :short "Fixtype of optional types."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "Types are defined in @(tsee type)."))
+  :pred type-optionp)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; TODO: continue
