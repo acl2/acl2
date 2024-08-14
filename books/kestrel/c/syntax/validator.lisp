b5627077241041c953fb702ee29cf4052369e14e
Alessandro Coglio <coglio@kestrel.edu>
2024-08-13 20:58:04 -0700

    [C$] Add validation tables to validator.

--- books/kestrel/c/syntax/validator.lisp
+++ books/kestrel/c/syntax/validator.lisp
@@ -188,4 +188,131 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(fty::deftagsum linkage
+  :short "Fixtype of linkages."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "There are three kinds of linkages: external, internal, and none
+     [C:6.2.2/1]."))
+  (:external ())
+  (:internal ())
+  (:none ())
+  :pred linkagep)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(fty::deftagsum valid-ord-info
+  :short "Fixtype of validation information about ordinary identifiers."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "Ordinary identifiers [C:6.2.3/1] denote
+     objects, functions, enumeration constants, and @('typedef') names;
+     Ordinary identifiers form their own name space.
+     The other entities denoted by identifiers [C:6.2.1/1]
+     are in other name spaces, disjoint from the one of ordinary identifiers.")
+   (xdoc::p
+    "This fixtype formalizes the information about ordinary identifiers
+     tracked by our current validator.
+     Since our model of types includes both object and function types,
+     the information for both objects and functions includes (different) types;
+     that information also includes the linkage [C:6.2.2].
+     For enumeration constants and for @('typedef') names,
+     for now we only track that they are
+     enumeration constants and @('typedef') names.")
+   (xdoc::p
+    "We will refine this fixtype as we refine our validator."))
+  (:objfun ((type type)
+            (linkage linkage)))
+  (:enumconst ())
+  (:typedef ())
+  :pred valid-ord-infop)
+
+;;;;;;;;;;;;;;;;;;;;
+
+(fty::defoption valid-ord-info-option
+  valid-ord-info
+  :short "Fixtype of
+          optional validation information about ordinary identifiers."
+  :pred valid-ord-info-optionp)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(fty::defalist valid-ord-scope
+  :short "Fixtype of validation scopes of ordinary identifiers."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "Identifiers have scopes [C:6.2.1], which the validator tracks.
+     In each scope, for each name space,
+     each identifier must have one meaning (if any) [C:6.2.1/2].
+     Thus, we use an alist from identifiers
+     to the validation information about ordinary identifiers,
+     to track each scope in the name space of ordinary identifiers."))
+  :key-type ident
+  :val-type valid-ord-info
+  :true-listp t
+  :keyp-of-nil nil
+  :valp-of-nil nil
+  :pred valid-ord-scopep
+  :prepwork ((set-induction-depth-limit 1))
+  ///
+
+  (defrule valid-ord-infop-of-cdr-assoc-when-valid-ord-scopep
+    (implies (and (valid-ord-scopep scope)
+                  (assoc-equal ident scope))
+             (valid-ord-infop (cdr (assoc-equal ident scope))))
+    :induct t
+    :enable (valid-ord-scopep assoc-equal)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(fty::defprod valid-scope
+  :short "Fixtype of validation scopes."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "Identifiers have scopes [C:6.2.1], which the validator tracks.
+     This fixtype contains all the information about a scope,
+     which currently only considers the name space of ordinary identifiers.
+     We will extend this fixtype to contain additional information,
+     particularly about tag of structure, union, and enumeration types."))
+  ((ord valid-ord-scope))
+  :pred valid-scopep)
+
+;;;;;;;;;;;;;;;;;;;;
+
+(fty::deflist valid-scope-list
+  :short "Fixtype of lists of validation scopes."
+  :elt-type valid-scope
+  :true-listp t
+  :elementp-of-nil nil
+  :pred valid-scope-listp
+  :prepwork ((local (in-theory (enable nfix)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(fty::defprod valid-table
+  :short "Fixtype of validation tables."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "Scopes are treated in a stack-like manner [C:6.2.1].
+     Thus, we define a validation table as
+     containing a list (i.e. stack) of scopes.
+     The stack grows from right to left:
+     the leftmost scope is the top, and the rightmost scope is the bottom;
+     in other words, in the nesting of scopes in the stack,
+     the leftmost scope is the innermost,
+     and the rightmost scope is the outermost
+     (i.e. the file scope [C:6.2.1/4].)")
+   (xdoc::p
+    "We wrap the list of scopes into a @(tsee fty::defprod)
+     for abstraction and extensibility."))
+  ((scopes valid-scope-list))
+  :pred valid-tablep)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ; TODO: continue
