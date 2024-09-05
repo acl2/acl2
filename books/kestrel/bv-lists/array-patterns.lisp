e8cf3eaf02fd200b957da7e398d413efc3db6d9d
Eric Smith <ewsmith@gmail.com>
2024-09-04 23:57:24 -0700

    [lists-light] Split every-nth into new book.

--- books/kestrel/bv-lists/array-patterns.lisp
+++ books/kestrel/bv-lists/array-patterns.lisp
@@ -1,6 +1,6 @@
 ; A book about discarding irrelevant array values
 ;
-; Copyright (C) 2022-2023 Kestrel Institute
+; Copyright (C) 2022-2024 Kestrel Institute
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
 ;
@@ -16,6 +16,7 @@
 (include-book "kestrel/bv/bvmult" :dir :system)
 (include-book "kestrel/bv/bvplus" :dir :system)
 (include-book "kestrel/bv/bvcat" :dir :system)
+(include-book "kestrel/lists-light/every-nth" :dir :system)
 (local (include-book "kestrel/arithmetic-light/mod" :dir :system))
 (local (include-book "kestrel/arithmetic-light/mod2" :dir :system))
 (local (include-book "kestrel/arithmetic-light/floor" :dir :system))
@@ -57,86 +58,7 @@
 ;;                   (nth n (keep-vals-with-congruent-indices index vals 0 k))))
 ;;   :hints (("Goal" :induct (ind index vals n))))
 
-(defund every-nth-exec-aux (n vals acc)
-  (declare (xargs :guard (and (posp n)
-                              (true-listp vals)
-                              (true-listp acc))
-                  :measure (len vals)
-                  ))
-  (if (not (mbt (posp n))) ; for termination
-      nil
-    (if (endp vals)
-        (reverse acc)
-      (every-nth-exec-aux n (nthcdr n vals) (cons (first vals) acc)))))
 
-(defund every-nth-exec (n vals)
-  (declare (xargs :guard (and (posp n)
-                              (true-listp vals))))
-  (every-nth-exec-aux n vals nil))
-
-(defund every-nth (n vals)
-  (declare (xargs :guard (and (posp n)
-                              (true-listp vals))
-                  :verify-guards nil ; done below
-                  :measure (len vals)
-                  ))
-  (mbe :logic
-       (if (not (mbt (posp n))) ; for termination
-           nil
-         (if (endp vals)
-             nil
-           (cons (first vals)
-                 (every-nth n (nthcdr n vals)))))
-       :exec (every-nth-exec n vals)))
-
-(defthm every-nth-exec-aux-helper
-  (implies (and (true-listp acc)
-                (posp n))
-           (equal (every-nth-exec-aux n vals acc)
-                  (append (reverse acc)
-                          (every-nth n vals))))
-  :hints (("Goal" :in-theory (enable every-nth every-nth-exec-aux))))
-
-(verify-guards every-nth :hints (("Goal" :in-theory (enable every-nth-exec every-nth))))
-
-; (equal (every-nth 3 '(0 1 2 3 4 5 6 7 8 9 10)) '(0 3 6 9))
-
-(defthm consp-of-every-nth
-  (implies (posp n)
-           (equal (consp (every-nth n vals))
-                  (consp vals)))
-  :hints (("Goal" :in-theory (enable every-nth))))
-
-(local
- (defun ind (n vals n2)
-   (declare (xargs :measure (len vals)))
-   (if (not (posp n2))
-       nil
-     (if (endp vals)
-         (list n vals n2)
-       (ind (- n 1) (nthcdr n2 vals) n2)))))
-
-(defthm nth-of-every-nth
-  (implies (and (posp n2)
-                (natp n))
-           (equal (nth n (every-nth n2 vals))
-                  (nth (* n n2) vals)))
-  :hints (("Goal" :in-theory (enable every-nth)
-           :induct (ind n vals n2))))
-
-(defthmd nth-when-remainder-known
-  (implies (and (equal remainder (mod index modulus))
-                (natp index)
-                (posp modulus))
-           (equal (nth index vals)
-                  (nth (floor index modulus) (every-nth modulus (nthcdr remainder vals))))))
-
-(defthm len-of-every-nth
-  (implies (posp n)
-           (equal (len (every-nth n vals))
-                  (ceiling (len vals) n)))
-  :hints (("Goal" :cases ((equal n 1))
-           :in-theory (enable every-nth))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
