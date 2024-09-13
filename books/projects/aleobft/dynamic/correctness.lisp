0e61fea082ecb4103d6e7eb4f36491a61e75a0ec
Alessandro Coglio <coglio@kestrel.edu>
2024-09-12 21:54:22 -0700

    [AleoBFT dynamic] Add and prove an invariant.
    This is about certificate signers forming a quorum in the applicable committee.

--- books/projects/aleobft/dynamic/correctness.lisp
+++ books/projects/aleobft/dynamic/correctness.lisp
@@ -22,6 +22,7 @@
 (include-book "quorum-intersection")
 (include-book "last-blockchain-round")
 (include-book "ordered-even-blocks")
+(include-book "signers-quorum-in-committee")
 (include-book "unequivocal-certificates-def-and-init")
 (include-book "nonforking-blockchains-def-and-init")
 (include-book "same-committees")
@@ -50,6 +51,7 @@
                     quorum-intersection
                     last-blockchain-round
                     ordered-even-blocks
+                    signers-quorum-in-committee
                     unequivocal-certificates-def-and-init
                     nonforking-blockchains-def-and-init
                     same-committees))
