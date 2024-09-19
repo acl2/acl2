48df5a458fc2fe009fd8213f4ff144f3e47c4ba5
Eric Smith <ewsmith@gmail.com>
2024-09-19 00:17:34 -0700

    [utilities] Improve my-get-event.
    Add support for defmacro and deftheory.  Also add some tests.

--- books/kestrel/utilities/my-get-event-tests.lisp
+++ books/kestrel/utilities/my-get-event-tests.lisp
@@ -1,6 +1,6 @@
 ; Tests of my-get-event
 ;
-; Copyright (C) 2015-2021 Kestrel Institute
+; Copyright (C) 2015-2024 Kestrel Institute
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
 ;
@@ -14,8 +14,133 @@
 (include-book "std/testing/assert-equal" :dir :system)
 (include-book "deftest")
 
+;; TODO: Rename this to get-event$ or get-event!
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; todo! (my-get-event 'car (w state)) this gives (enter-boot-strap-mode :unix) !
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Example with a defun:
+(assert-equal
+  (my-get-event 'natp (w state))
+  '(defun natp (x)
+     (declare (xargs :guard t :mode :logic))
+     (and (integerp x) (<= 0 x))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Example with a defmacro:
+(assert-equal
+  (my-get-event 'append (w state))
+  '(defmacro append (&rest rst)
+     (cond ((null rst) nil)
+           ((null (cdr rst)) (car rst))
+           (t (xxxjoin 'binary-append rst)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Example with a mutual-recursion (the result is the entire mutual-recursion):
+
+(assert-equal
+  (my-get-event 'pseudo-termp (w state)) ; first function in the nest
+  '(mutual-recursion
+    (defun pseudo-termp (x)
+      (declare (xargs :guard t :mode :logic))
+      (cond ((atom x) (symbolp x))
+            ((eq (car x) 'quote)
+             (and (consp (cdr x))
+                  (null (cdr (cdr x)))))
+            ((not (pseudo-term-listp (cdr x))) nil)
+            (t (or (symbolp (car x))
+                   (and (true-listp (car x))
+                        (equal (len$ (car x)) 3)
+                        (eq (car (car x)) 'lambda)
+                        (symbol-listp (cadr (car x)))
+                        (pseudo-termp (caddr (car x)))
+                        (equal (len$ (cadr (car x)))
+                               (len$ (cdr x))))))))
+    (defun pseudo-term-listp (lst)
+      (declare (xargs :guard t))
+      (cond ((atom lst) (equal lst nil))
+            (t (and (pseudo-termp (car lst))
+                    (pseudo-term-listp (cdr lst))))))))
+
+(assert-equal
+  (my-get-event 'pseudo-term-listp (w state)) ; second function in the nest
+  '(mutual-recursion
+    (defun pseudo-termp (x)
+      (declare (xargs :guard t :mode :logic))
+      (cond ((atom x) (symbolp x))
+            ((eq (car x) 'quote)
+             (and (consp (cdr x))
+                  (null (cdr (cdr x)))))
+            ((not (pseudo-term-listp (cdr x))) nil)
+            (t (or (symbolp (car x))
+                   (and (true-listp (car x))
+                        (equal (len$ (car x)) 3)
+                        (eq (car (car x)) 'lambda)
+                        (symbol-listp (cadr (car x)))
+                        (pseudo-termp (caddr (car x)))
+                        (equal (len$ (cadr (car x)))
+                               (len$ (cdr x))))))))
+    (defun pseudo-term-listp (lst)
+      (declare (xargs :guard t))
+      (cond ((atom lst) (equal lst nil))
+            (t (and (pseudo-termp (car lst))
+                    (pseudo-term-listp (cdr lst))))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Example with a defaxiom:
+(assert-equal
+  (my-get-event 'car-cons (w state))
+  '(defaxiom car-cons
+     (equal (car (cons x y)) x)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Example with a defthm:
+(assert-equal
+  (my-get-event 'default-pkg-imports (w state))
+  '(defthm default-pkg-imports
+     (implies (not (stringp x))
+              (equal (pkg-imports x)
+                     nil))
+     :hints (("Goal" :use completion-of-pkg-imports))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Example with a deftheory
+
+(deftheory my-theory nil)
+
+(assert-equal
+  (my-get-event 'my-theory (w state))
+  '(deftheory my-theory nil))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Example with a stobj (todo: doesn't work for state):
+
+(defstobj my-stobj (scalar-field-1 :type integer :initially 7))
+;; todo: also test defabsstobj
+
+(assert-equal
+  (my-get-event 'my-stobj (w state))
+  '(defstobj my-stobj
+     (scalar-field-1 :type integer
+                     :initially 7)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; An example where the built-in utility GET-EVENT returns something not
-;; useful, namely (VERIFY-TERMINATION-BOOT-STRAP (DEF-BODY)):
+;; useful.
+
+;; get-event is not helpful:
+(assert-equal (get-event 'def-body (w state)) '(verify-termination-boot-strap (def-body)))
+
 (assert-equal
  (my-get-event 'def-body (w state))
  '(DEFUN
@@ -26,8 +151,19 @@
                         (TRUE-LISTP (GETPROPC FN 'DEF-BODIES NIL WRLD)))))
     (CAR (GETPROPC FN 'DEF-BODIES NIL WRLD))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; An example where the built-in utility GET-EVENT returns something not
 ;; useful:
+
+;; get-event is not helpful:
+(assert-equal
+  (get-event 'all-vars1 (w state))
+  '(verify-termination-boot-strap
+    (all-vars1 (declare (xargs :mode :logic :verify-guards nil)))
+    (all-vars1-lst (declare (xargs :mode :logic)))))
+
+;; my-get-event gives us what we want:
 (assert-equal
  (my-get-event 'all-vars1 (w state))
  '(MUTUAL-RECURSION
@@ -47,32 +183,10 @@
            (T (ALL-VARS1-LST (CDR LST)
                              (ALL-VARS1 (CAR LST) ANS)))))))
 
-;; Test on a name was introduced with mutual-recursion:
-(assert-equal
- (my-get-event 'pseudo-termp (w state))
- '(MUTUAL-RECURSION (DEFUN PSEUDO-TERMP (X)
-                      (DECLARE (XARGS :GUARD T :MODE :LOGIC))
-                      (COND ((ATOM X) (SYMBOLP X))
-                            ((EQ (CAR X) 'QUOTE)
-                             (AND (CONSP (CDR X))
-                                  (NULL (CDR (CDR X)))))
-                            ((NOT (PSEUDO-TERM-LISTP (CDR X))) NIL)
-                            (T (OR (SYMBOLP (CAR X))
-                                   (AND (TRUE-LISTP (CAR X))
-                                        (EQUAL (LEN$ (CAR X)) 3)
-                                        (EQ (CAR (CAR X)) 'LAMBDA)
-                                        (SYMBOL-LISTP (CADR (CAR X)))
-                                        (PSEUDO-TERMP (CADDR (CAR X)))
-                                        (EQUAL (LEN$ (CADR (CAR X)))
-                                               (LEN$ (CDR X))))))))
-                    (DEFUN PSEUDO-TERM-LISTP (LST)
-                      (DECLARE (XARGS :GUARD T))
-                      (COND ((ATOM LST) (EQUAL LST NIL))
-                            (T (AND (PSEUDO-TERMP (CAR LST))
-                                    (PSEUDO-TERM-LISTP (CDR LST))))))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Test on a built-in :program mode function:
-(assert-equal ; Matt K. mod: avoid translate, to avoid ACL2(p) error
+;; Test on a built-in :program mode function (get-event also works):
+(assert-equal
  (my-get-event 'translatable-p (w state))
  '(defun translatable-p (form stobjs-out bindings known-stobjs ctx wrld)
     (mv-let (erp val bindings)
@@ -80,6 +194,8 @@
                       (default-state-vars nil))
       (declare (ignore val bindings))
       (null erp))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Test on a name that was introduced with defuns:
 (deftest
