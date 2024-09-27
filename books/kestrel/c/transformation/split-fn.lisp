ae61b634e5815cbe284bf8490ddfb335106fa62f
Alessandro Coglio <coglio@kestrel.edu>
2024-09-26 22:56:17 -0700

    [C$] Add support for a GCC extension.
    This allows attributes in lists of type qualifiers.

--- books/kestrel/c/transformation/split-fn.lisp
+++ books/kestrel/c/transformation/split-fn.lisp
@@ -186,7 +186,7 @@
 (define abstract-fn
   ((new-fn-name identp)
    (spec declspec-listp)
-   (pointers type-qual-list-listp)
+   (pointers typequal/attribspec-list-listp)
    (items block-item-listp)
    (decls ident-paramdecl-mapp))
   :short "Create a new function from the block items following the @(tsee
@@ -254,7 +254,7 @@
   ((new-fn-name identp)
    (items block-item-listp)
    (spec declspec-listp)
-   (pointers type-qual-list-listp)
+   (pointers typequal/attribspec-list-listp)
    (decls ident-paramdecl-mapp)
    (split-point natp))
   :short "Transform a list of block items."
