5d56da24471ee20c5ad45fc4eedf50d66c1d29a0
Eric Smith <ewsmith@gmail.com>
2024-10-02 01:35:52 -0700

    [axe] Add more tests of defthm-stp.

--- books/kestrel/axe/defthm-stp-tests.lisp
+++ books/kestrel/axe/defthm-stp-tests.lisp
@@ -64,3 +64,39 @@
              (implies (unsigned-byte-p 64 x)
                       (equal (bvsx 32 16 x) (bvsx 32 16 y)))
              :rule-classes nil))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Some array tests:
+
+(defthm-stp array0
+  (equal (bv-array-write 8 100 7 val1 (bv-array-write 8 100 4 val2 data))
+         (bv-array-write 8 100 4 val2 (bv-array-write 8 100 7 val1 data))))
+
+(defthm-stp array1a
+  (equal (bv-array-read 8 16 index (bv-array-write 8 16 index val data))
+         (bvchop 8 val))
+  :print t)
+
+(defthm-stp array1b
+  (implies (unsigned-byte-p 8 val)
+           (equal (bv-array-read 8 16 index (bv-array-write 8 16 index val data))
+                  val))
+  :print t)
+
+(must-fail
+  ;; don't know the type of val
+  (defthm-stp array1c
+    (equal (bv-array-read 8 16 index (bv-array-write 8 16 index val data))
+           val)
+    :print t))
+
+;; arrays lengths not equal
+;; todo: improve the error message
+;; todo: add an option to print the inferred types
+(must-fail
+  (defthm-stp array2
+    (equal (bv-array-write 8 100 7 val1 (bv-array-write 8 100 4 val2 data))
+           (bv-array-write 9 100 4 val2 (bv-array-write 9 100 7 val1 data)))
+    :print t
+    ))
