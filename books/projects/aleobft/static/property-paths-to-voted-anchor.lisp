b28578cde36db2f032ce3cb38b23bbdbd2bb5a1c
Alessandro Coglio <coglio@kestrel.edu>
2024-10-13 14:38:00 -0700

    [AleoBFT static] Remove unnecessary hypothesis.

--- books/projects/aleobft/static/property-paths-to-voted-anchor.lisp
+++ books/projects/aleobft/static/property-paths-to-voted-anchor.lisp
@@ -215,9 +215,6 @@
 (defruled cardinality-of-voters/previous-intersection
   (implies (and (certificate-setp certs1)
                 (certificate-setp certs2)
-                (<= (set::cardinality
-                     (certificate-set->round-set (set::union certs1 certs2)))
-                    1)
                 (<= (set::cardinality (set::union certs1 certs2)) n)
                 (>= (set::cardinality certs1) (1+ f))
                 (equal (set::cardinality certs2) (- n f)))
