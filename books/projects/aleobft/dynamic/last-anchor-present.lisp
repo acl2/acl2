313cb9287e69f70647ed59934ed9409bd67ef70c
Alessandro Coglio <coglio@kestrel.edu>
2024-10-15 22:34:16 -0700

    [AleoBFT dynamic] Fix typo in doc.

--- books/projects/aleobft/dynamic/last-anchor-present.lisp
+++ books/projects/aleobft/dynamic/last-anchor-present.lisp
@@ -120,7 +120,7 @@
      The event is conditioned by @(tsee commit-anchors-possiblep),
      which requires the presence in the DAG of the certificate
      that becomes the new last anchor.
-     But we need to show the extension of the blockchain
+     But we need to show that the extension of the blockchain
      does not affect the choice of the leader for that round,
      so we use @('active-committee-at-round-of-extend-blockchain-no-change')
      and additional rules to discharge its hypotheses,
