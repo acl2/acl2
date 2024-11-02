425fa135c07745a606be6a8236c8317797a3130f
Alessandro Coglio <coglio@kestrel.edu>
2024-11-02 14:59:13 -0700

    [C$] Add some abstract syntax operations.
    These recognize certain lists of type specifiers that denote types.

--- books/kestrel/c/syntax/abstract-syntax-operations.lisp
+++ books/kestrel/c/syntax/abstract-syntax-operations.lisp
@@ -1249,3 +1249,432 @@
   :guard-hints (("Goal" :in-theory (enable omap::assoc-to-in-of-keys
                                            transunit-ensemble-paths)))
   :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-permp ((tyspecs1 type-spec-listp)
+                              (tyspecs2 type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if two lists of type specifiers are permutations."
+  (b* (((when (endp tyspecs1)) (endp tyspecs2))
+       (tyspec (car tyspecs1))
+       ((unless (member-equal tyspec tyspecs2)) nil))
+    (type-spec-list-permp (cdr tyspecs1) (remove1-equal tyspec tyspecs2))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-char-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form @('char')."
+  (equal (type-spec-list-fix tyspecs)
+         (list (type-spec-char)))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-signed-char-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form
+          @('signed char') or @('char signed'),
+          including the GCC underscore variations of @('signed')."
+  (or (type-spec-list-permp (type-spec-list-fix tyspecs)
+                            (list (type-spec-signed (keyword-uscores-none))
+                                  (type-spec-char)))
+      (type-spec-list-permp (type-spec-list-fix tyspecs)
+                            (list (type-spec-signed (keyword-uscores-start))
+                                  (type-spec-char)))
+      (type-spec-list-permp (type-spec-list-fix tyspecs)
+                            (list (type-spec-signed (keyword-uscores-both))
+                                  (type-spec-char))))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-unsigned-char-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form
+          @('unsigned char') or @('char unsigned')."
+  (type-spec-list-permp (type-spec-list-fix tyspecs)
+                        (list (type-spec-unsigned)
+                              (type-spec-char)))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-short-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form @('short')."
+  (equal (type-spec-list-fix tyspecs)
+         (list (type-spec-short)))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-signed-short-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form
+          @('signed short') or @('short signed'),
+          including the GCC underscore variations of @('signed')."
+  (or (type-spec-list-permp (type-spec-list-fix tyspecs)
+                            (list (type-spec-signed (keyword-uscores-none))
+                                  (type-spec-short)))
+      (type-spec-list-permp (type-spec-list-fix tyspecs)
+                            (list (type-spec-signed (keyword-uscores-start))
+                                  (type-spec-short)))
+      (type-spec-list-permp (type-spec-list-fix tyspecs)
+                            (list (type-spec-signed (keyword-uscores-both))
+                                  (type-spec-short))))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-short-int-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form
+          @('short int') or @('int short')."
+  (type-spec-list-permp (type-spec-list-fix tyspecs)
+                        (list (type-spec-short)
+                              (type-spec-int)))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-signed-short-int-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form
+          @('signed short int') or any permutation of it,
+          including the GCC underscore variations of @('signed')."
+  (or (type-spec-list-permp (type-spec-list-fix tyspecs)
+                            (list (type-spec-signed (keyword-uscores-none))
+                                  (type-spec-short)
+                                  (type-spec-int)))
+      (type-spec-list-permp (type-spec-list-fix tyspecs)
+                            (list (type-spec-signed (keyword-uscores-start))
+                                  (type-spec-short)
+                                  (type-spec-int)))
+      (type-spec-list-permp (type-spec-list-fix tyspecs)
+                            (list (type-spec-signed (keyword-uscores-both))
+                                  (type-spec-short)
+                                  (type-spec-int))))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-unsigned-short-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form
+          @('unsigned short') or @('short unsigned')."
+  (type-spec-list-permp (type-spec-list-fix tyspecs)
+                        (list (type-spec-unsigned)
+                              (type-spec-short)))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-unsigned-short-int-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form
+          @('unsigned short int') or any permutation of it."
+  (type-spec-list-permp (type-spec-list-fix tyspecs)
+                        (list (type-spec-unsigned)
+                              (type-spec-short)
+                              (type-spec-int)))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-int-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form @('int')."
+  (equal (type-spec-list-fix tyspecs)
+         (list (type-spec-int)))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-signed-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form @('signed'),
+          including the GCC underscore variations of @('signed')."
+  (or (equal (type-spec-list-fix tyspecs)
+             (list (type-spec-signed (keyword-uscores-none))))
+      (equal (type-spec-list-fix tyspecs)
+             (list (type-spec-signed (keyword-uscores-start))))
+      (equal (type-spec-list-fix tyspecs)
+             (list (type-spec-signed (keyword-uscores-both)))))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-signed-int-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form
+          @('signed int') or @('int signed'),
+          including the GCC underscore variations of @('signed')."
+  (or (type-spec-list-permp (type-spec-list-fix tyspecs)
+                            (list (type-spec-signed (keyword-uscores-none))
+                                  (type-spec-int)))
+      (type-spec-list-permp (type-spec-list-fix tyspecs)
+                            (list (type-spec-signed (keyword-uscores-start))
+                                  (type-spec-int)))
+      (type-spec-list-permp (type-spec-list-fix tyspecs)
+                            (list (type-spec-signed (keyword-uscores-both))
+                                  (type-spec-int))))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-unsigned-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form @('unsigned')."
+  (equal (type-spec-list-fix tyspecs)
+         (list (type-spec-unsigned)))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-unsigned-int-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form
+          @('unsigned int') or @('int unsigned')."
+  (type-spec-list-permp (type-spec-list-fix tyspecs)
+                        (list (type-spec-unsigned)
+                              (type-spec-int)))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-long-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form @('long')."
+  (equal (type-spec-list-fix tyspecs)
+         (list (type-spec-long)))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-signed-long-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form
+          @('signed long') or @('long signed'),
+          including the GCC underscore variations of @('signed')."
+  (or (type-spec-list-permp (type-spec-list-fix tyspecs)
+                            (list (type-spec-signed (keyword-uscores-none))
+                                  (type-spec-long)))
+      (type-spec-list-permp (type-spec-list-fix tyspecs)
+                            (list (type-spec-signed (keyword-uscores-start))
+                                  (type-spec-long)))
+      (type-spec-list-permp (type-spec-list-fix tyspecs)
+                            (list (type-spec-signed (keyword-uscores-both))
+                                  (type-spec-long))))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-long-int-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form
+          @('long int') or @('int long')."
+  (type-spec-list-permp (type-spec-list-fix tyspecs)
+                        (list (type-spec-long)
+                              (type-spec-int)))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-signed-long-int-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form
+          @('signed long int') or any permutation of it,
+          including the GCC underscore variations of @('signed')."
+  (or (type-spec-list-permp (type-spec-list-fix tyspecs)
+                            (list (type-spec-signed (keyword-uscores-none))
+                                  (type-spec-long)
+                                  (type-spec-int)))
+      (type-spec-list-permp (type-spec-list-fix tyspecs)
+                            (list (type-spec-signed (keyword-uscores-start))
+                                  (type-spec-long)
+                                  (type-spec-int)))
+      (type-spec-list-permp (type-spec-list-fix tyspecs)
+                            (list (type-spec-signed (keyword-uscores-both))
+                                  (type-spec-long)
+                                  (type-spec-int))))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-unsigned-long-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form
+          @('unsigned long') or @('long unsigned')."
+  (type-spec-list-permp (type-spec-list-fix tyspecs)
+                        (list (type-spec-unsigned)
+                              (type-spec-long)))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-unsigned-long-int-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form
+          @('unsigned long int') or any permutation of it."
+  (type-spec-list-permp (type-spec-list-fix tyspecs)
+                        (list (type-spec-unsigned)
+                              (type-spec-long)
+                              (type-spec-int)))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-long-long-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form @('long long')."
+  (equal (type-spec-list-fix tyspecs)
+         (list (type-spec-long)
+               (type-spec-long)))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-signed-long-long-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form
+          @('signed long long') or any permutation of it,
+          including the GCC underscore variations of @('signed')."
+  (or (type-spec-list-permp (type-spec-list-fix tyspecs)
+                            (list (type-spec-signed (keyword-uscores-none))
+                                  (type-spec-long)
+                                  (type-spec-long)))
+      (type-spec-list-permp (type-spec-list-fix tyspecs)
+                            (list (type-spec-signed (keyword-uscores-start))
+                                  (type-spec-long)
+                                  (type-spec-long)))
+      (type-spec-list-permp (type-spec-list-fix tyspecs)
+                            (list (type-spec-signed (keyword-uscores-both))
+                                  (type-spec-long)
+                                  (type-spec-long))))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-long-long-int-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form
+          @('long long int') or any permutation of it."
+  (type-spec-list-permp (type-spec-list-fix tyspecs)
+                        (list (type-spec-long)
+                              (type-spec-long)
+                              (type-spec-int)))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-signed-long-long-int-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form
+          @('signed long long int') or any permutation of it,
+          including the GCC underscore variations of @('signed')."
+  (or (type-spec-list-permp (type-spec-list-fix tyspecs)
+                            (list (type-spec-signed (keyword-uscores-none))
+                                  (type-spec-long)
+                                  (type-spec-long)
+                                  (type-spec-int)))
+      (type-spec-list-permp (type-spec-list-fix tyspecs)
+                            (list (type-spec-signed (keyword-uscores-start))
+                                  (type-spec-long)
+                                  (type-spec-long)
+                                  (type-spec-int)))
+      (type-spec-list-permp (type-spec-list-fix tyspecs)
+                            (list (type-spec-signed (keyword-uscores-both))
+                                  (type-spec-long)
+                                  (type-spec-long)
+                                  (type-spec-int))))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-unsigned-long-long-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form
+          @('unsigned long long') or any permutation of it."
+  (type-spec-list-permp (type-spec-list-fix tyspecs)
+                        (list (type-spec-unsigned)
+                              (type-spec-long)
+                              (type-spec-long)))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-unsigned-long-long-int-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form
+          @('unsigned long long int') or any permutation of it."
+  (type-spec-list-permp (type-spec-list-fix tyspecs)
+                        (list (type-spec-unsigned)
+                              (type-spec-long)
+                              (type-spec-long)
+                              (type-spec-int)))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-float-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form @('float')."
+  (equal (type-spec-list-fix tyspecs)
+         (list (type-spec-float)))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-double-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form @('double')."
+  (equal (type-spec-list-fix tyspecs)
+         (list (type-spec-double)))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-long-double-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form
+          @('long double') or @('double long')."
+  (type-spec-list-permp (type-spec-list-fix tyspecs)
+                        (list (type-spec-long)
+                              (type-spec-double)))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-float-complex-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form
+          @('float _Complex') or @('_Complex float')."
+  (type-spec-list-permp (type-spec-list-fix tyspecs)
+                        (list (type-spec-float)
+                              (type-spec-complex)))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-double-complex-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form
+          @('double _Complex') or @('_Complex double')."
+  (type-spec-list-permp (type-spec-list-fix tyspecs)
+                        (list (type-spec-double)
+                              (type-spec-complex)))
+  :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-spec-list-long-double-complex-p ((tyspecs type-spec-listp))
+  :returns (yes/no booleanp)
+  :short "Check if a list of type specifiers has the form
+          @('long double _Complex') or any permutation of it."
+  (type-spec-list-permp (type-spec-list-fix tyspecs)
+                        (list (type-spec-long)
+                              (type-spec-double)
+                              (type-spec-complex)))
+  :hooks (:fix))
