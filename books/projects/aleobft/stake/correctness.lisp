7399fa6cf5f51bda716fdbf9d86b7aa32a57e123
Alessandro Coglio <email@alessandrocoglio.info>
2024-11-14 22:29:10 -0800

    [AleoBFT stake] Add and prove an invariant.
    This is the backward closure of DAGs.

--- books/projects/aleobft/stake/correctness.lisp
+++ books/projects/aleobft/stake/correctness.lisp
@@ -12,6 +12,7 @@
 (in-package "ALEOBFT-STAKE")
 
 (include-book "fault-tolerance")
+(include-book "backward-closure")
 (include-book "last-blockchain-round")
 (include-book "ordered-even-blocks")
 (include-book "associated-certificates")
@@ -35,6 +36,7 @@
      culminating in the main property that we are interested in,
      namely the non-forking of blockchains."))
   :order-subtopics (fault-tolerance
+                    backward-closure
                     last-blockchain-round
                     ordered-even-blocks
                     associated-certificates
