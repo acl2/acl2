b22ca912dd32d9e0888990e23865f1994de2cbf9
Alessandro Coglio <email@alessandrocoglio.info>
2024-11-17 16:15:29 -0800

    [AleoBFT dynamic] Add fixing theorems and remove hyps.

--- books/projects/aleobft/dynamic/no-self-buffer.lisp
+++ books/projects/aleobft/dynamic/no-self-buffer.lisp
@@ -61,6 +61,9 @@
 
   ///
 
+  (fty::deffixequiv-sk no-self-buffer-p
+    :args ((systate system-statep)))
+
   (defruled no-self-buffer-p-necc-fixing
     (implies (and (no-self-buffer-p systate)
                   (set::in (address-fix val) (correct-addresses systate)))
@@ -186,25 +189,17 @@
           reachable from an initial state via a sequence of events."
 
   (defruled no-self-buffer-p-of-events-next
-    (implies (and (system-statep systate)
-                  (no-self-buffer-p systate)
+    (implies (and (no-self-buffer-p systate)
                   (no-self-messages-p systate)
                   (events-possiblep events systate))
              (and (no-self-buffer-p (events-next events systate))
                   (no-self-messages-p (events-next events systate))))
     :induct t
-    :disable ((:e tau-system))
     :enable (events-possiblep
-             events-next
-             no-self-buffer-p-of-event-next
-             no-self-messages-p-of-event-next))
+             events-next))
 
   (defruled no-self-buffer-p-when-reachable
     (implies (and (system-statep systate)
                   (system-initp systate)
                   (events-possiblep events systate))
-             (no-self-buffer-p (events-next events systate)))
-    :disable ((:e tau-system))
-    :enable (no-self-buffer-p-when-init
-             no-self-messages-p-when-init
-             no-self-buffer-p-of-events-next)))
+             (no-self-buffer-p (events-next events systate)))))
