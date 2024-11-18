183707ebe2ec864bcf66f1d68393834b4054c1f1
Eric Smith <ewsmith@gmail.com>
2024-11-18 13:49:32 -0800

    [typed-lists-light] Add rules about alist-listp.

--- books/kestrel/typed-lists-light/alist-listp.lisp
+++ books/kestrel/typed-lists-light/alist-listp.lisp
@@ -1,6 +1,6 @@
 ; Recognizing lists of alists
 ;
-; Copyright (C) 2023 Kestrel Institute
+; Copyright (C) 2023-2024 Kestrel Institute
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
 ;
@@ -37,4 +37,27 @@
 (defthm alistp-of-nth
   (implies (alist-listp alists)
            (alistp (nth n alists)))
+  :hints (("Goal" :in-theory (enable alist-listp))))
+
+(defthm alistp-listp-of-cons
+  (equal (alist-listp (cons item lst))
+         (and (alistp item)
+              (alist-listp lst)))
+  :hints (("Goal" :in-theory (enable alist-listp))))
+
+(defthm alistp-listp-of-cdr
+  (implies (alist-listp lst)
+           (alist-listp (cdr lst)))
+  :hints (("Goal" :in-theory (enable alist-listp))))
+
+(defthm alist-listp-of-revappend
+  (equal (alist-listp (revappend x y))
+         (and (alist-listp (true-list-fix x))
+              (alist-listp y)))
+  :hints (("Goal" :in-theory (enable alist-listp revappend))))
+
+;; Where should this go?
+(defthmd alistp-of-cdr-when-alist-listp-of-strip-cdrs
+  (implies (alist-listp (strip-cdrs alist))
+           (alistp (cdr (car alist))))
   :hints (("Goal" :in-theory (enable alist-listp))))
