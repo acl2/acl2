16be9f40fa2850d5486fdb64761463223331ea9d
Alessandro Coglio <email@alessandrocoglio.info>
2024-12-14 20:45:35 -0800

    [AleoBFT stake] Update some doc.

--- books/projects/aleobft/stake/system-states.lisp
+++ books/projects/aleobft/stake/system-states.lisp
@@ -83,12 +83,7 @@
      at syncing time.")
    (xdoc::p
     "Eventually, we will want to model syncing more explicitly.
-     However, we believe that the current model is already a good one,
-     at least as a first cut at handling dynamic committees.
-     Finding a problem in this model would almost certaintly reveal a problem
-     in the full AleoBFT with syncing;
-     conversely, successful proofs of properties in this model
-     will at least ensure the absence of certain flaws.")
+     However, we believe that the current model is already a good one.")
    (xdoc::p
     "Besides the validators,
      we also model the state of the network that connects the validators,
