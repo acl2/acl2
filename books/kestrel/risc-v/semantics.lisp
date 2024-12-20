350d1f712037136a775195367d6a0bda224835fd
Alessandro Coglio <email@alessandrocoglio.info>
2024-12-20 11:32:05 -0800

    [RISC-V] Add instruction semantic for RV32I.

--- books/kestrel/risc-v/semantics.lisp
+++ books/kestrel/risc-v/semantics.lisp
@@ -10,6 +10,7 @@
 
 (in-package "RISCV")
 
+(include-book "semantics32")
 (include-book "semantics64")
 
 ; cert_param: (non-acl2r)
@@ -22,6 +23,8 @@
   :long
   (xdoc::topstring
    (xdoc::p
-    "Currently we have semantics for RV64I.
-     We plan to add RV32I soon."))
-  :order-subtopics (semantics64))
+    "Currently we have two similar but slightly different semantics,
+     one for RV32I and one for RV64I.
+     We plan to consolidate them into one semantics for both."))
+  :order-subtopics (semantics32
+                    semantics64))
