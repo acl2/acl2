cdb49fb07a42b176a29cbbda100542ffd85b846d
Eric Smith <ewsmith@gmail.com>
2024-12-25 17:22:37 -0800

    [bv] Move pick-a-bit stuff out of Axe.

--- books/kestrel/axe/rules3.lisp
+++ books/kestrel/axe/rules3.lisp
@@ -55,6 +55,7 @@
 (local (include-book "kestrel/arithmetic-light/floor-and-expt" :dir :system))
 (local (include-book "kestrel/bv/floor-mod-expt" :dir :system))
 (local (include-book "kestrel/bv/trim-rules" :dir :system))
+(local (include-book "kestrel/bv/pick-a-bit" :dir :system))
 (local (include-book "kestrel/bv-lists/all-unsigned-byte-p2" :dir :system))
 (local (include-book "kestrel/arithmetic-light/integer-length2" :dir :system))
 (local (include-book "kestrel/arithmetic-light/expt2" :dir :system))
@@ -9584,65 +9585,6 @@
                   (slice (+ -1 free) 2 x)))
   :hints (("Goal" :in-theory (e/d (slice logtail UNSIGNED-BYTE-P natp)
                                   (anti-slice)))))
-
-;;
-;; PICK-A-BIT proofs
-;;
-
-;returns a bit where x and y differ (if any)
-(defund differing-bit (n x y)
-  (declare (xargs :measure (nfix (+ 1 n))))
-  (if (not (natp n))
-      -1
-    (if (not (equal (getbit n x) (getbit n y)))
-        n
-      (differing-bit (+ -1 n) x y))))
-
-(defthm differing-bit-bad-guy-lemma-helper
-  (implies (and (equal (getbit (differing-bit m x y) x)
-                       (getbit (differing-bit m x y) y))
-                (< m n)
-                (natp m)
-                (natp n))
-           (equal (slice m 0 x)
-                  (slice m 0 y)))
-  :rule-classes nil
-  :hints (("Goal" :in-theory (enable differing-bit slice-becomes-getbit bvchop-1-becomes-getbit))))
-
-;; (defthm natp-of-differing-bit
-;;   (natp (differing-bit n x y)))
-
-(defthm <-of-differing-bit
-  (implies (natp n)
-           (not (< n (differing-bit n x y))))
-  :hints (("Goal" :expand ((DIFFERING-BIT 0 X Y))
-           :in-theory (enable differing-bit))))
-
-(defthm <-of-differing-bit2
-  (implies (and (natp n)
-                (integerp k)
-                (<= (+ 1 n) k))
-           (< (differing-bit n x y) k))
-  :hints (("Goal" :expand ((DIFFERING-BIT 0 X Y))
-           :in-theory (enable differing-bit))))
-
-(defthm differing-bit-bad-guy-lemma
-  (implies (and (equal (getbit (differing-bit (+ -1 n) x y) x)
-                       (getbit (differing-bit (+ -1 n) x y) y))
-                (unsigned-byte-p n x)
-                (unsigned-byte-p n y)
-                (natp n))
-           (equal x y))
-  :rule-classes nil
-  :hints (("Goal" :use (:instance differing-bit-bad-guy-lemma-helper (m (+ -1 n)))))
-  )
-
-(defthm <-of-differing-bit-and-0
-  (implies (natp n)
-           (equal (< (differing-bit n x y) 0)
-                  (equal (bvchop (+ 1 n) x)
-                         (bvchop (+ 1 n) y))))
-  :hints (("Goal" :in-theory (enable differing-bit slice-becomes-getbit bvchop-1-becomes-getbit))))
 
 ;;(local (in-theory (enable BVOR-1-BECOMES-BITOR)))         ;Thu Mar 31 16:45:29 2011
 
