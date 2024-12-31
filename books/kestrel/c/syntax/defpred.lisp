f57621abe6bf620ff1196816c08e56a6f1861683
Alessandro Coglio <email@alessandrocoglio.info>
2024-12-30 18:37:09 -0800

    [C$] Improve `defpred`.
    Generate a `:flag-local nil` in generated `defines`.

--- books/kestrel/c/syntax/defpred.lisp
+++ books/kestrel/c/syntax/defpred.lisp
@@ -958,7 +958,12 @@
      we generate a clique of mutually recursive predicates,
      with a @(tsee fty::deffixequiv-mutual) at the end;
      the name of the clique of predicates is derived from
-     the name of the clique of types."))
+     the name of the clique of types.")
+   (xdoc::p
+    "We also generate a @(':flag-local nil') to export
+     the flag macro @('defthm-<name>-flag'),
+     where @('<name>') is the name of the @(tsee defines) clique.
+     This facilitates proving theorems by induction on the predicates."))
   (b* ((members (fty::flextypes->types clique))
        ((unless (true-listp members))
         (raise "Internal error: malformed members of type clique ~x0." clique)
@@ -979,6 +984,7 @@
     `(defines ,clique-name-suffix
        ,@events
        :hints (("Goal" :in-theory (enable o< o-finp)))
+       :flag-local nil
        ///
        (fty::deffixequiv-mutual ,clique-name-suffix))))
 
