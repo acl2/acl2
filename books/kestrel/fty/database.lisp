f0ebae7d2f0535a311d34c6798735d6e00987ec0
Alessandro Coglio <email@alessandrocoglio.info>
2025-01-10 17:24:55 -0800

    [FTY] Add two operations on FTY database types.

--- books/kestrel/fty/database.lisp
+++ books/kestrel/fty/database.lisp
@@ -14,6 +14,8 @@
 (include-book "std/util/deflist" :dir :system)
 (include-book "xdoc/defxdoc-plus" :dir :system)
 
+(local (include-book "std/typed-lists/symbol-listp" :dir :system))
+
 (local (include-book "kestrel/built-ins/disable" :dir :system))
 (local (acl2::disable-most-builtin-logic-defuns))
 (local (acl2::disable-builtin-rewrite-rules-for-defaults))
@@ -59,3 +61,31 @@
   (cond ((endp prods) nil)
         (t (cons (flexprod->kind (car prods))
                  (flexprod-list->kind-list (cdr prods))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define flex->name (info)
+  :returns (name symbolp)
+  :short "Name of a sum, list, alist, transparent sum, set, or omap type,
+          given the information associated to the type."
+  (b* ((name (cond ((flexsum-p info) (flexsum->name info))
+                   ((flexlist-p info) (flexlist->name info))
+                   ((flexalist-p info) (flexalist->name info))
+                   ((flextranssum-p info) (flextranssum->name info))
+                   ((flexset-p info) (flexset->name info))
+                   ((flexomap-p info) (flexomap->name info))
+                   (t (raise "Internal error: malformed type ~x0." info))))
+       ((unless (symbolp name))
+        (raise "Internal error: malformed type name ~x0." name)))
+    name))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define flex-list->name-list ((infos true-listp))
+  :returns (names symbol-listp)
+  :short "Lift @(tsee flex->name) to lists."
+  (cond ((endp infos) nil)
+        (t (cons (flex->name (car infos))
+                 (flex-list->name-list (cdr infos))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
