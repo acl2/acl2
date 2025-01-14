92b2207f6fbdf3c10474675727b998321d5d2be5
Eric Smith <ewsmith@gmail.com>
2025-01-13 16:53:04 -0800

    [axe] Simplify :rule-classes.

--- books/kestrel/axe/axe-syntax-functions-bv.lisp
+++ books/kestrel/axe/axe-syntax-functions-bv.lisp
@@ -1,7 +1,7 @@
 ; BV-related syntactic tests
 ;
 ; Copyright (C) 2008-2011 Eric Smith and Stanford University
-; Copyright (C) 2013-2024 Kestrel Institute
+; Copyright (C) 2013-2025 Kestrel Institute
 ; Copyright (C) 2016-2020 Kestrel Technology, LLC
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
@@ -92,7 +92,7 @@
 (defthm maybe-get-type-of-bv-function-call-type
   (or (null (maybe-get-type-of-bv-function-call fn dargs))
       (natp (maybe-get-type-of-bv-function-call fn dargs)))
-  :rule-classes (:type-prescription)
+  :rule-classes :type-prescription
   :hints (("Goal" :in-theory (enable maybe-get-type-of-bv-function-call
                                      <-of-0-and-make-bv-type))))
 
