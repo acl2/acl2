35f059cd1d56d51392ad4423db93fec37cd8d252
Alessandro Coglio <email@alessandrocoglio.info>
2025-01-28 12:47:43 -0800

    [Yul] Improve some doc.

--- books/kestrel/yul/language/abstract-syntax.lisp
+++ books/kestrel/yul/language/abstract-syntax.lisp
@@ -29,11 +29,12 @@
     (xdoc::seetopic "concrete-syntax" "concrete syntax")
     "; more precisely, on the new grammar (see description there).")
    (xdoc::p
-    "The abstract syntax defined here is fairly close to the concrete syntax,
-     more precisely to the grammar,
-     which already omits lexical entities like whitespace.
-     The reason for this closeness is that we want to keep
-     as much information as possible in the abstract syntax.
+    "The abstract syntax defined here is fairly close to the concrete syntax.
+     The reason for this closeness is to reduce incidental differences
+     between the code before and after a transformation,
+     to facilitate inspection and debugging.
+     (Although our formalization of Yul stands on its own,
+     a major motivation for it is to formalize and verify Yul transformations.)
      In some cases our abstract syntax may be broader than the concrete syntax,
      to keep the definition of the abstract syntax slightly simpler;
      the important thing is that all the concrete syntax
