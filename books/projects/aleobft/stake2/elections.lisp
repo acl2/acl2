a766555ea2a63b3ec378476e73ba85a193fb6625
Alessandro Coglio <email@alessandrocoglio.info>
2025-02-05 20:27:06 -0800

    [AleoBFT] Fix typo in doc.

--- books/projects/aleobft/stake2/elections.lisp
+++ books/projects/aleobft/stake2/elections.lisp
@@ -133,7 +133,7 @@
      as we ensure by way of formal proofs.")
    (xdoc::p
     "We go through the voters, and check whether the leader address
-     is among the refernced previous certificates or not,
+     is among the referenced previous certificates or not,
      counting its stake as part of the resulting vote stake."))
   (b* (((when (or (not (mbt (certificate-setp voters)))
                   (set::emptyp voters)))
