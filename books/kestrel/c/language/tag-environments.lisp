3e3162654a2455acbeed3e4f8ce7c7b289d1c0b4
Alessandro Coglio <email@alessandrocoglio.info>
2025-02-14 18:40:17 -0800

    [C] Update file headers.

--- books/kestrel/c/language/tag-environments.lisp
+++ books/kestrel/c/language/tag-environments.lisp
@@ -1,7 +1,7 @@
 ; C Library
 ;
-; Copyright (C) 2024 Kestrel Institute (http://www.kestrel.edu)
-; Copyright (C) 2024 Kestrel Technology LLC (http://kestreltechnology.com)
+; Copyright (C) 2025 Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2025 Kestrel Technology LLC (http://kestreltechnology.com)
 ;
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 ;
