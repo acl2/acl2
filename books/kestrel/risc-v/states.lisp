ef23ee93d8fe45b28302774fd6deb63a36c97dcc
Alessandro Coglio <email@alessandrocoglio.info>
2025-02-17 18:36:28 -0800

    [RISC-V] Formalize generic states.
    Also formalize a validity predicate w.r.t. features.

--- books/kestrel/risc-v/states.lisp
+++ books/kestrel/risc-v/states.lisp
@@ -10,8 +10,16 @@
 
 (in-package "RISCV")
 
+(include-book "features")
 (include-book "states32")
 (include-book "states64")
+
+(local (include-book "kestrel/utilities/nfix" :dir :system))
+
+(local (include-book "kestrel/built-ins/disable" :dir :system))
+(local (acl2::disable-most-builtin-logic-defuns))
+(local (acl2::disable-builtin-rewrite-rules-for-defaults))
+(set-induction-depth-limit 0)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -23,6 +31,96 @@
    (xdoc::p
     "Currently we have two similar but slightly different models,
      one for RV32I and one for RV64I.
-     We plan to consolidate them into one model for both."))
+     We plan to consolidate them into one model for both;
+     towards that end, we also provide a more generic definition here."))
+  :default-parent t
   :order-subtopics (states32
-                    states64))
+                    states64
+                    t))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(fty::defprod stat
+  :short "Fixtype of machine states."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "This captures all possible states for all possible @(see features).
+     Restrictions based on features are formalized in @(see stat-validp).")
+   (xdoc::p
+    "The processor state always includes the @('x<i>') registers,
+     whose number and size depends on the choice of base:
+     for RV32I, there are 32 registers of 32 bits each [ISA:2.1];
+     for RV64I, there are 32 registers of 64 bits each [ISA:4.1];
+     for RV32E, there are 16 registers of 32 bits each [ISA:3.1];
+     for RV64E, there are 16 registers of 64 bits each [ISA:3.1];
+     and for the upcoming RV128I, the registers are 128-bit [ISA:5].
+     Thus, here we generically define the @('x') register file
+     as a list of natural numbers.
+     In [ISA], @('XLEN') refers to the size of the registers in bits.")
+   (xdoc::p
+    "The program counter @('pc') has the same size as the @('x') registers,
+     based on the choice of base.
+     Thus, we generically define it as a natural number here.")
+   (xdoc::p
+    "The memory component models the whole addressable space,
+     which consists of @('2^XLEN') bytes [ISA:1.4].
+     We generically define it as a list of bytes here.")
+   (xdoc::p
+    "RISC-V extensions may require the extension of this fixtype,
+     with more components that support those extensions.
+     We will do that as we model those extensions,
+     which also requires extending the @(see features).")
+   (xdoc::p
+    "We also include a boolean flag that says whether an error has occurred.
+     This does not exist in the real machine;
+     it is just a modeling convenience.
+     We may refine this boolean flag into some richer data type."))
+  ((xregs nat-list)
+   (pc nat)
+   (memory ubyte8-list)
+   (error bool))
+  :pred statp)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define stat-validp ((stat statp) (feat featp))
+  :returns (yes/no booleanp)
+  :short "Check if a state is valid with respect to given features."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "As explained in @(tsee stat),
+     that fixtype models all possible machine states for all possible features.
+     Here we define restrictions based on features.")
+   (xdoc::p
+    "For now, the only features we model are
+     whether the base is RV32I or RV64I.
+     This dictates the size @('XLEN') of the registers, either 32 or 64 bits;
+     so we use contrain them to form a list of 32-bit or 64-bit unsigned values.
+     Since we do not model RV32E or RV64E yet,
+     the number of registers is always 32.
+     However, since @('x0') is hardwired to 0 [ISA:2.1],
+     we do not model that register explicitly:
+     we only model @('x1'), @('x2'), etc.;
+     so we constrain the length of the list to be 31.")
+   (xdoc::p
+    "Based on @('XLEN'), we constrain the program counter
+     to be either 32 or 64 bits.")
+   (xdoc::p
+    "The size of the memory is @('2^XLEN'),
+     so we constrain the length of the list to be that."))
+  (b* (((stat stat) stat)
+       ((feat feat) feat))
+    (and (equal (len stat.xregs) 31)
+         (feat-bits-case feat.bits
+                         :32 (ubyte32-listp stat.xregs)
+                         :64 (ubyte64-listp stat.xregs))
+         (feat-bits-case feat.bits
+                         :32 (ubyte32p stat.pc)
+                         :64 (ubyte64p stat.pc))
+         (equal (len stat.memory)
+                (expt 2 (feat-bits-case feat.bits
+                                        :32 32
+                                        :64 64)))))
+  :hooks (:fix))
