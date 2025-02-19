4dccb5c6c9de72925c8f69e3e2e0f99ab41a34f1
Eric Smith <ewsmith@gmail.com>
2025-02-19 13:20:03 -0800

    [axe] Clarify.

--- books/kestrel/axe/rule-alists.lisp
+++ books/kestrel/axe/rule-alists.lisp
@@ -1,7 +1,7 @@
 ; Rule-alists: databases of rules used by Axe
 ;
 ; Copyright (C) 2008-2011 Eric Smith and Stanford University
-; Copyright (C) 2013-2023 Kestrel Institute
+; Copyright (C) 2013-2025 Kestrel Institute
 ; Copyright (C) 2016-2020 Kestrel Technology, LLC
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
@@ -24,13 +24,15 @@
 ;; Rule-alists are structures that index rules by the top function symbol of their LHSes.
 ;; TODO: Consider using a property list world to make the lookups faster.
 
-;; A rule-alist is a database of rules used by Axe.  It maps function symbols
-;; to lists of stored rules.
+;; A rule-alist is a database of rules used by Axe.  It is a list of entries,
+;; where each entry maps a function symbol to a list of stored-rules that can
+;; rewrite calls of that function.  (All rules must have a topmost function
+;; symbol; the LHS of a rule can't be a constant or variable).
 (defund rule-alistp (alist)
   (declare (xargs :guard t))
   (if (atom alist)
       (null alist)
-    (let* ((entry (car alist)))
+    (let* ((entry (car alist))) ; (fn . stored-rules)
       (and (consp entry)
            (let ((fn (car entry))
                  (stored-rules (cdr entry)))
@@ -79,6 +81,7 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; The empty rule-alist is just an empty alist.
 (defun empty-rule-alist () (declare (xargs :guard t)) nil)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -116,28 +119,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; ;;todo: deprecate (use count-rules-in-rule-alist)
-;; (defund rule-count-in-rule-alist (rule-alist)
-;;   (declare (xargs :guard (rule-alistp rule-alist)
-;;                   :verify-guards nil ;done below
-;;                   ))
-;;   (if (endp rule-alist)
-;;       0
-;;     (let* ((entry (car rule-alist))
-;;            (stored-rules (cdr entry)))
-;;       (+ (len stored-rules)
-;;          (rule-count-in-rule-alist (cdr rule-alist))))))
-
-;; ;drop?
-;; (defthm natp-of-rule-count-in-rule-alist
-;;   (natp (rule-count-in-rule-alist rule-alist))
-;;   :rule-classes (:rewrite :type-prescription))
-
-;; (verify-guards rule-count-in-rule-alist
-;;   :hints (("Goal" :in-theory (enable rule-alistp))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 ;; Check whether RULE-ALIST contains a rule named RULE-SYMBOL.
 (defund rule-alist-containsp (rule-alist rule-symbol)
   (declare (xargs :guard (and (rule-alistp rule-alist)
@@ -156,12 +137,13 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Prints a message for all of the RULES not in the RULE-ALIST.
+;; Return value is meaningless.
 (defund print-missing-rules (rules rule-alist)
   (declare (xargs :guard (and (rule-alistp rule-alist)
                               (symbol-listp rules))
                   :guard-hints (("Goal" :in-theory (enable rule-alistp)))))
   (if (endp rules)
-      t
+      nil
     (b* ((rule (first rules))
          (presentp (rule-alist-containsp rule-alist rule))
          (- (and (not presentp)
@@ -179,9 +161,9 @@
   (if (endp alist)
       nil
     (let* ((entry (first alist))
-;           (fn (car entry))
+           ;; (fn (car entry))
            (stored-rules (cdr entry)))
-      (union-eq (rules-from-stored-axe-rules stored-rules)
+      (union-eq (stored-rule-names stored-rules)
                 (rules-from-rule-alist (rest alist))))))
 
 (defthm symbol-listp-of-rules-from-rule-alist
@@ -195,11 +177,10 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;makes an alist where each entry pairs a function symbol with a list of axe-rules that apply to that function.
-;(All rules must have a topmost function symbol; the LHS of a rule can't be a constant or variable).
-;the alist returned may have many entries for each function symbol, but we remove the shadowed entries in the caller
-;if remove-duplicate-rulesp is non-nil we ensure that two rules with the same name are never added (we don't check for rules that are the same except for the name)
-;;ffixme might be faster to do duplicate checking at the end (or while sorting!)...
+;; Adds the AXE-RULES to the RULE-ALIST, perhaps removing duplicates among the rules for the relevant functions (if REMOVE-DUPLICATE-RULESP is non-nil).
+;; The alist returned may have many entries for each function symbol, but we remove the shadowed entries in the caller.
+;; If REMOVE-DUPLICATE-RULESP is non-nil we ensure that two rules with the same name are never added (we don't check for rules that are the same except for the name).
+;; todo: might be faster to do duplicate checking at the end (or while sorting!)...
 ;; todo: optimize by using a property-list world?
 (defund extend-unprioritized-rule-alist (axe-rules remove-duplicate-rulesp rule-alist)
   (declare (xargs :guard (and (axe-rule-listp axe-rules)
@@ -214,16 +195,14 @@
            (stored-rules-for-fn (get-rules-for-fn fn rule-alist)) ;may be slow?
            (new-stored-rules-for-fn (if (and remove-duplicate-rulesp
                                              (rule-is-presentp (rule-symbol rule) stored-rules-for-fn))
-                                        ;;already there:
-                                        stored-rules-for-fn
-                                      ;;not already there (or we are not checking):
-                                      ;;ffixme, could use priorities and insert the rule in sorted order (faster to merge sort at the end?)
+                                        stored-rules-for-fn ; already there
+                                      ;; not already there (or we are not checking):
+                                      ;; todo: could use priorities and insert the rule in sorted order (faster to merge sort at the end?)
                                       (cons (make-stored-rule (fargs lhs) (rule-hyps rule) (rule-symbol rule) (rule-rhs rule))
-                                            stored-rules-for-fn))))
-      (extend-unprioritized-rule-alist (rest axe-rules)
-                                     remove-duplicate-rulesp
-                                     (acons-fast ;-unique       ;;now we uniquify the alist at the end
-                                      fn new-stored-rules-for-fn rule-alist)))))
+                                            stored-rules-for-fn)))
+           (rule-alist (acons-fast ;-unique       ;;now we uniquify the alist at the end
+                         fn new-stored-rules-for-fn rule-alist)))
+      (extend-unprioritized-rule-alist (rest axe-rules) remove-duplicate-rulesp rule-alist))))
 
 (local
  (defthm symbol-alistp-of-extend-unprioritized-rule-alist
@@ -259,7 +238,7 @@
            (stored-rules (cdr entry)))
       (acons fn
              ;; TODO: Perhaps optimize if none of the rules have priorities (but that might change existing orderings)?:
-             (merge-sort-by-rule-priority stored-rules priorities)
+             (merge-sort-by-rule-priority stored-rules priorities) ; todo: make this more stable (e.g., by sorting by rule name when the priorities are tied)
              (sort-rules-for-each-function-symbol-by-priority (rest rule-alist) priorities)))))
 
 (local
@@ -290,6 +269,7 @@
          (rule-alist (uniquify-alist-eq rule-alist))
          (rule-alist (if priorities
                          (sort-rules-for-each-function-symbol-by-priority rule-alist priorities)
+                       ;; todo: continue sorting by name in this case, for predictability:
                        rule-alist)))
     rule-alist))
 
@@ -302,7 +282,7 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Makes a rule-alist from the given axe-rules (not that these are full axe-rules,
+;; Makes a rule-alist from the given AXE-RULES (note that these are full axe-rules,
 ;; not rule-names).  TODO: Might be faster to first sort the rules by head
 ;; symbol, then remove dups, then use the fact that rules for the same symbol
 ;; are grouped together.
@@ -322,6 +302,7 @@
 ;; Makes a rule-alist from the given RULE-NAMES, using priority information from WRLD.
 ;; Returns (mv erp rule-alist).
 ;todo: optimize this routine to not make the rules first
+;; Warning: If any of these rules have :rule-classes nil, ACL2 won't allow us to use them.
 (defund make-rule-alist (rule-names wrld)
   (declare (xargs :guard (and (symbol-listp rule-names)
                               (ilks-plist-worldp wrld))))
@@ -338,8 +319,7 @@
 (defthm rule-alistp-of-mv-nth-1-of-make-rule-alist
   (implies (symbol-listp rule-names)
            (rule-alistp (mv-nth 1 (make-rule-alist rule-names wrld))))
-  :hints (("Goal" :in-theory (enable make-rule-alist
-                                     axe-rule-listp))))
+  :hints (("Goal" :in-theory (enable make-rule-alist axe-rule-listp))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -396,8 +376,8 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; This is for the special case when the AXE-RULES do not correspond to theorems in the world.
-;todo: would prefer to just pass in named formulas here
+;; This takes axe-rules, not names, and is for the unusual case when the AXE-RULES do not correspond to theorems in the world.
+;todo: would prefer to just pass in named formulas here, instead of axe-rules.
 (defund extend-rule-alist2 (axe-rules rule-alist wrld)
   (declare (xargs :guard (and (axe-rule-listp axe-rules)
                               (rule-alistp rule-alist)
@@ -436,17 +416,18 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Begin material about rule-alist-lists
+
 ;;;
 ;;; all-rule-alistp
 ;;;
 
-(defforall-simple rule-alistp) ; todo: simplify?
+(defforall-simple rule-alistp) ; todo: simplify? ; todo: switch to a rule-alist-listp?
 (verify-guards all-rule-alistp)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Returns a list of rule-names.
-;; Warning: If any of these rules have :rule-classes nil, ACL2 won't allow us to use them.
 (defund rules-from-rule-alists (alists)
   (declare (xargs :guard (and (all-rule-alistp alists)
                               (true-listp alists))))
