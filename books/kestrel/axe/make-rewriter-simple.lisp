b94e6a755ccceec4cc71cb04c39285d8d90db53e
Eric Smith <ewsmith@gmail.com>
2025-02-22 21:46:31 -0800

    [axe] Slightly improve code.

--- books/kestrel/axe/make-rewriter-simple.lisp
+++ books/kestrel/axe/make-rewriter-simple.lisp
@@ -929,16 +929,13 @@
                ;;Try looking it up in the memoization (note that the args are now simplified):
                (memo-match (and memoization (lookup-in-memoization expr memoization))) ; todo: use a more specialized version of lookup-in-memoization, since we know the shape of expr (also avoid the cons for expr here)?
                ((when memo-match)
-                (mv (erp-nil) memo-match
-                    rewrite-stobj2
-                    (add-pairs-to-memoization trees-equal-to-tree
-                                              memo-match ;the nodenum or quotep they are all equal to
-                                              memoization)
-                    hit-counts tries limits
-                    node-replacement-array))
+                (let ((memoization (add-pairs-to-memoization trees-equal-to-tree
+                                                             memo-match ;the nodenum or quotep they are all equal to
+                                                             memoization)))
+                  (mv (erp-nil) memo-match rewrite-stobj2 memoization hit-counts tries limits node-replacement-array)))
                ;; Next, try to apply rules:
                ((mv erp rhs-or-nil rewrite-stobj2 memoization hit-counts tries limits node-replacement-array)
-                (,try-to-apply-rules-name (rule-db-get fn rewrite-stobj) ;;(get-rules-for-fn fn (get-rule-alist rewrite-stobj))
+                (,try-to-apply-rules-name (rule-db-get fn rewrite-stobj) ;; (get-rules-for-fn fn (get-rule-alist rewrite-stobj))
                                           args
                                           rewrite-stobj2 memoization hit-counts tries limits
                                           node-replacement-array node-replacement-count refined-assumption-alist
