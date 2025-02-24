739a8480c34984165f578f29e361454785719752
Eric Smith <ewsmith@gmail.com>
2025-02-23 23:23:04 -0800

    [axe] Localize a rule.

--- books/kestrel/axe/unguarded-primitives.lisp
+++ books/kestrel/axe/unguarded-primitives.lisp
@@ -1,7 +1,7 @@
 ; Versions of primitive functions with guards of t
 ;
 ; Copyright (C) 2008-2011 Eric Smith and Stanford University
-; Copyright (C) 2013-2022 Kestrel Institute
+; Copyright (C) 2013-2025 Kestrel Institute
 ; Copyright (C) 2016-2020 Kestrel Technology, LLC
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
@@ -114,15 +114,16 @@
       (pkg-witness pkg)
     'acl2-pkg-witness))
 
-(defthmd equal-when-symbols
-  (implies (and (symbolp x)
-                (symbolp y))
-           (equal (equal x y)
-                  (and (equal (symbol-name x) (symbol-name y))
-                       (equal (symbol-package-name x) (symbol-package-name y)))))
-  :hints (("Goal" :use (:instance symbol-equality
-                                  (s1 x)
-                                  (s2 y)))))
+(local
+  (defthmd equal-when-symbols
+    (implies (and (symbolp x)
+                  (symbolp y))
+             (equal (equal x y)
+                    (and (equal (symbol-name x) (symbol-name y))
+                         (equal (symbol-package-name x) (symbol-package-name y)))))
+    :hints (("Goal" :use (:instance symbol-equality
+                                    (s1 x)
+                                    (s2 y))))))
 
 (defthm pkg-witness-unguarded-correct
   (equal (pkg-witness-unguarded pkg)
