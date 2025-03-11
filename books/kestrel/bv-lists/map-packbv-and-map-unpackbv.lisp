b0f11c03f02dd0857ec5f48b5baca734510a5f7c
Eric Smith <ewsmith@gmail.com>
2025-03-10 21:14:37 -0700

    [bv-lists] Add an inversion theorem.

--- books/kestrel/bv-lists/map-packbv-and-map-unpackbv.lisp
+++ books/kestrel/bv-lists/map-packbv-and-map-unpackbv.lisp
@@ -14,6 +14,7 @@
 (include-book "map-packbv")
 (include-book "map-unpackbv")
 (include-book "packbv-and-unpackbv")
+(local (include-book "kestrel/lists-light/take" :dir :system))
 
 (defthm map-packbv-of-map-unpackbv
   (implies (and (natp itemcount)
@@ -21,3 +22,13 @@
            (equal (map-packbv itemcount itemsize (map-unpackbv itemcount itemsize bvs))
                   (bvchop-list (* itemcount itemsize) bvs)))
   :hints (("Goal" :in-theory (enable packbv map-packbv map-unpackbv))))
+
+(defthm map-unpackbv-of-map-packbv
+  (implies (and (all-all-unsigned-byte-p itemsize lists)
+                (items-have-len itemcount lists)
+                (true-list-listp lists)
+                (natp itemcount)
+                (posp itemsize))
+           (equal (map-unpackbv itemcount itemsize (map-packbv itemcount itemsize lists))
+                  lists))
+  :hints (("Goal" :in-theory (enable packbv map-packbv map-unpackbv items-have-len))))
