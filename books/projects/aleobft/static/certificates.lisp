a4152c4b86c34655576fd6ac7814388799cedc7c
Alessandro Coglio <email@alessandrocoglio.info>
2025-03-25 09:07:37 -0700

    [AleoBFT] Uniformize some doc.

--- books/projects/aleobft/static/certificates.lisp
+++ books/projects/aleobft/static/certificates.lisp
@@ -95,7 +95,8 @@
   :long
   (xdoc::topstring
    (xdoc::p
-    "These are the author and the endorsers."))
+    "These are the author and the endorsers,
+     i.e. all the validators who signed the certificate."))
   (b* (((certificate cert) cert))
     (set::insert cert.author cert.endorsers)))
 
