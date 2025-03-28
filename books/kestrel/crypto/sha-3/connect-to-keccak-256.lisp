3bb328cc9ab68508a34a511994acc160c634289d
Eric Smith <ewsmith@gmail.com>
2025-03-27 17:03:56 -0700

    [sha-3] Improve connection proofs with Keccak.
    The new proofs are much simpler, avoid the possibility of rewrite loops, and are faster.

--- books/kestrel/crypto/sha-3/connect-to-keccak-256.lisp
+++ books/kestrel/crypto/sha-3/connect-to-keccak-256.lisp
@@ -15,44 +15,23 @@
 (include-book "kestrel/axe/equivalence-checker" :dir :system)
 (include-book "kestrel/axe/unroll-spec-basic" :dir :system)
 
+;; See also connect-to-keccak-8.lisp.
+
 ;; TODO: Add proofs for longer messages, or even a general proof for arbitrary messages
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Assumes a 256-bit message
 (acl2::unroll-spec-basic *sha-3-keccak-256-256bit*
-                   `(sha3::keccak-256 ,(acl2::symbolic-list 'in 256))
-                   :rules :auto
-                   :memoizep nil ; for speed
-                   :extra-rules '(acl2::update-nth-of-cons
-                                  acl2::car-becomes-nth-of-0
-                                  ))
+                         `(sha3::keccak-256 ,(acl2::symbolic-list 'in 256))
+                         :rules :auto)
 
 ;; Assumes a 256-bit message
 (acl2::unroll-spec-basic *keccak-256-256bit*
                          `(keccak::keccak-256 ,(acl2::symbolic-list 'in 256))
-                         :rules :auto
-                         :extra-rules '(acl2::update-nth-of-cons
-                                        acl2::car-becomes-nth-of-0
-                                        acl2::leftrotate))
+                         :rules :auto)
 
 (acl2::prove-equal-with-axe *sha-3-keccak-256-256bit*
                             *keccak-256-256bit*
-                            ;; :initial-rule-sets (list (make-axe-rules! (amazing-rules-bv) (w state))) ;don't bit-blast
                             :normalize-xors nil ; todo: heap exhaustion (many xor nests, each with thousands of nodes) !
-                            :tactic :rewrite-and-sweep)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Alternative proof, using rewriting alone:
-
-(acl2::prove-equal-with-axe *sha-3-keccak-256-256bit*
-                            *keccak-256-256bit*
-                            :extra-rules (append '(bitand-commutative-axe
-                                                   bitand-commutative-2-axe
-                                                   bitand-associative
-                                                   equal-same)
-                                                 (acl2::bit-blast-rules-basic)
-                                                 (acl2::core-rules-bv))
-                            :normalize-xors nil ; prevents heap exhaustion (many xor nests, each with thousands of nodes) !
                             :tactic :rewrite)
