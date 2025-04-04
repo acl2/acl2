ede3478f393c7ee60a097732fb5016664e81e3e5
Matt Kaufmann <kaufmann@cs.utexas.edu>
2025-04-03 23:42:30 -0500

    Added comments to set-theory Schroeder-Bernstein book.

--- books/projects/set-theory/schroeder-bernstein.lisp
+++ books/projects/set-theory/schroeder-bernstein.lisp
@@ -2,6 +2,18 @@
 ; Written by Matt Kaufmann
 ; License: A 3-clause BSD license.  See the LICENSE file distributed with ACL2.
 
+; This book takes advantage of Grant Jurgensen's ACL2 formalization and proof
+; of the Schroeder-Bernstein theorem, to state and prove a version here in
+; purely set-theoretic terms.  See the final theorem, Schroeder-Bernstein,
+; below.  The real work takes place in the book
+; schroeder-bernstein-support.lisp.  It may have been simpler to redo the proof
+; from scratch, using Grant's proof as a guide.  But I wanted to do the
+; experiment of deriving the new result from the old one, taking advantage of
+; ACL2's functional instantiation.  It's conceivable that future such
+; conversion efforts (from an earlier ACL2 proof into proof of a purely
+; set-theoretic formulation) could benefit from automatic the approach taken in
+; schroeder-bernstein-support.lisp.
+
 (in-package "ZF")
 
 (include-book "injective-funp")
