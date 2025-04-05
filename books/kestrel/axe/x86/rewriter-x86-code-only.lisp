68690ecf06ded1e9fa0e82674ecc7a9b748293cb
Eric Smith <ewsmith@gmail.com>
2025-04-05 10:39:56 -0700

    [axe/x86] Add rewriter-x86-code-only.

---
+++ books/kestrel/axe/x86/rewriter-x86-code-only.lisp
@@ -0,0 +1,24 @@
+; Use with-supporters to just get the code of the x86 Axe Rewriter
+;
+; Copyright (C) 2022-2025 Kestrel Institute
+;
+; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
+;
+; Author: Eric Smith (eric.smith@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "ACL2")
+
+(include-book "tools/with-supporters" :dir :system)
+
+;; todo: also need to include all the calls of add-known-boolean (making sure
+;; the functions are defined).  Or use the :tables option to get the
+;; :known-booleans-table.
+
+(with-supporters
+ (local (include-book "rewriter-x86"))
+ :names (simplify-terms-to-terms-x86
+          simplify-dag-x86
+          ;; make-rule-alist!
+          ))
