122b3713978364a18a2ea965bca704baba08f0c5
Eric Smith <ewsmith@gmail.com>
2025-04-20 11:16:12 -0700

    [axe] Add an axe-evaluator for JVM work.

--- books/kestrel/axe/jvm/top.lisp
+++ books/kestrel/axe/jvm/top.lisp
@@ -1,6 +1,6 @@
 ; Top file for the JVM-related Axe tools
 ;
-; Copyright (C) 2021 Kestrel Institute
+; Copyright (C) 2021-2025 Kestrel Institute
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
 ;
@@ -14,7 +14,8 @@
 (include-book "axe-syntax-functions-jvm")
 (include-book "axe-syntax-functions-jvm2")
 
-;; Evaluators for axe-syntaxp and axe-bind-free functions:
+;; Evaluators:
+(include-book "evaluator-jvm")
 (include-book "axe-syntaxp-evaluator-jvm")
 (include-book "axe-bind-free-evaluator-jvm")
 
