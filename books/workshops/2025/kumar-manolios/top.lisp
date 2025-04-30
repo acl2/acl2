ecec001ecdfc1b6c7f3411bd4435230b259a9daf
Ankit Kumar <ankitkumar.itbhu@gmail.com>
2025-05-01 02:19:38 +0530

    book for Correctness Proof of Floodsub

---
+++ books/workshops/2025/kumar-manolios/top.lisp
@@ -0,0 +1,9 @@
+(in-package "ACL2S")
+
+(include-book "utils")
+(include-book "bn-trx")
+(include-book "fn-trx")
+(include-book "f2b-commit")
+(include-book "good-fn")
+(include-book "trx-rels")
+(include-book "f2b-sim-ref")
