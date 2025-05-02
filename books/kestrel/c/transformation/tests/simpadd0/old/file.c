d77ad495404638720b9b11b1532f298744296127
Alessandro Coglio <email@alessandrocoglio.info>
2025-05-01 20:52:44 -0700

    [C2C] Streamline `simpadd0` tests.

--- books/kestrel/c/transformation/tests/simpadd0/old/file.c
+++ books/kestrel/c/transformation/tests/simpadd0/old/file.c
@@ -1,42 +1,4 @@
-int f(int x, int y) {
-  return (x + y) + 0;
-}
-
-int g(int a, unsigned long b) {
-  return a + 0;
-}
-
-int main() {
-  int x = 5;
-  return x + 0;
-}
-
-int ff(int x) {
-  return x + 0;
-}
-
-int main1() {
-  float y = 5.0f;
-  return y + 0;
-}
-
-int fff() {
-  int x = 5;
-  return x + 0;
-}
-
-int gg(int y) {
-  int z = (y + 0);
-  return z;
-}
-
-int h() {
-  int w = 1;
-  int u = ((w + 0));
-  return 0;
-}
-
-int i(int x) {
+int nosimp(int x) {
   int x1 = +x;
   int x2 = -x;
   int x3 = ~x;
@@ -44,8 +6,43 @@ int i(int x) {
   return x1 + x2 + x3 + x4;
 }
 
-int main2() {
-  int a = f();
-  int b = g(a);
-  return a + b;
+int var(int x) {
+  return x + 0;
+}
+
+int constant() {
+  return 17 + 0;
+}
+
+int bin(int x, int y) {
+  return (x + y) + 0;
+}
+
+int nonint_param(int a, unsigned long b) {
+  return a + 0;
+}
+
+int decl() {
+  int x = 5;
+  return x + 0;
+}
+
+int noninteger() {
+  float y = 5.0f;
+  return y + 0;
+}
+
+int gg(int y) {
+  int z = (y + 0);
+  return z;
+}
+
+int paren() {
+  int w = 1;
+  int u = ((w + 0));
+  return 0;
+}
+
+int main() {
+  return 0;
 }
