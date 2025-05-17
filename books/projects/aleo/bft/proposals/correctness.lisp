1caeeedf4ac400aa2214bdcfbd3cf4c9486ece91
Alessandro Coglio <email@alessandrocoglio.info>
2025-05-16 20:48:44 -0400

    [AleoBFT] Add and prove an invariant.

--- books/projects/aleo/bft/proposals/correctness.lisp
+++ books/projects/aleo/bft/proposals/correctness.lisp
@@ -28,6 +28,7 @@
 (include-book "certificate-in-author")
 (include-book "proposed-author-in-committee")
 (include-book "proposed-endorser-in-committee")
+(include-book "signer-quorum")
 (include-book "proposed-round1-no-previous")
 (include-book "endorsed-round1-no-previous")
 (include-book "proposed-previous-closed")
@@ -69,6 +70,7 @@
                     certificate-in-author
                     proposed-author-in-committee
                     proposed-endorser-in-committee
+                    signer-quorum
                     proposed-round1-no-previous
                     endorsed-round1-no-previous
                     proposed-previous-closed
