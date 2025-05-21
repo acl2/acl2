59eadd6018926f381707f37e18a8e6bcb5ef96d6
Eric Smith <ewsmith@gmail.com>
2025-05-21 12:18:12 -0700

    [axe] Tweak includes.

--- books/kestrel/axe/dag-size-sparse-tests.lisp
+++ books/kestrel/axe/dag-size-sparse-tests.lisp
@@ -1,7 +1,7 @@
-; Tests of dag-size2
+; Tests of dag-size-sparse
 ;
 ; Copyright (C) 2008-2011 Eric Smith and Stanford University
-; Copyright (C) 2013-2022 Kestrel Institute
+; Copyright (C) 2013-2025 Kestrel Institute
 ; Copyright (C) 2016-2020 Kestrel Technology, LLC
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
@@ -13,7 +13,7 @@
 (in-package "ACL2")
 
 (include-book "std/testing/assert" :dir :system)
-(include-book "kestrel/axe/dag-size-sparse" :dir :system)
+(include-book "dag-size-sparse")
 
 (assert-equal (size-array-for-nodes '(0 1 2 3 4 5) 'dag-array (make-into-array 'dag-array '((5 foo 4 3) (4 foo 3 0) (3 foo 0 2) (2 foo 1 0) (1 quote 17) (0 . x))) 6 'size-array)
               '((5 . 13)
