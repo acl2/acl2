b1f6b8830cbc82dd36ab4e6be9f94be3bd8b1397
Alessandro Coglio <email@alessandrocoglio.info>
2025-05-29 12:01:04 -0700

    [RISC-V] Remove library extensions now in more general libraries.

--- books/kestrel/risc-v/library-extensions.lisp
+++ books/kestrel/risc-v/library-extensions.lisp
@@ -11,16 +11,7 @@
 (in-package "RISCV")
 
 (include-book "ihs/basic-definitions" :dir :system)
-(include-book "kestrel/fty/sbyte32" :dir :system)
-(include-book "kestrel/fty/sbyte64" :dir :system)
-(include-book "kestrel/fty/ubyte3" :dir :system)
-(include-book "kestrel/fty/ubyte5" :dir :system)
-(include-book "kestrel/fty/ubyte6" :dir :system)
-(include-book "kestrel/fty/ubyte7" :dir :system)
-(include-book "kestrel/fty/ubyte8" :dir :system)
-(include-book "kestrel/fty/ubyte16" :dir :system)
-(include-book "kestrel/fty/ubyte32" :dir :system)
-(include-book "kestrel/fty/ubyte64" :dir :system)
+(include-book "std/util/defrule" :dir :system)
 
 (local (include-book "arithmetic-5/top" :dir :system))
 
@@ -43,70 +34,6 @@
 (defruled integerp-of-unary-minus
   (implies (integerp x)
            (integerp (- x))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defrule ubyte5p-of-loghead-of-5
-  (ubyte5p (loghead 5 x))
-  :enable ubyte5p)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defrule ubyte6p-of-loghead-of-6
-  (ubyte6p (loghead 6 x))
-  :enable ubyte6p)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defrule ubyte8p-of-loghead-of-8
-  (ubyte8p (loghead 8 x))
-  :enable ubyte8p)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defrule ubyte16p-of-loghead-of-16
-  (ubyte16p (loghead 16 x))
-  :enable ubyte16p)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defrule ubyte32p-of-loghead-of-32
-  (ubyte32p (loghead 32 x))
-  :enable ubyte32p)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defrule ubyte64p-of-loghead-of-64
-  (ubyte64p (loghead 64 x))
-  :enable ubyte64p)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defrule sbyte32p-of-logext-of-32
-  (sbyte32p (logext 32 x))
-  :enable sbyte32p)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defrule sbyte64p-of-logext-of-64
-  (sbyte64p (logext 64 x))
-  :enable sbyte64p)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defrule loghead-3-of-ubyte3
-  (implies (ubyte3p x)
-           (equal (loghead 3 x)
-                  x))
-  :enable ubyte3p)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defrule loghead-7-of-ubyte7
-  (implies (ubyte7p x)
-           (equal (loghead 7 x)
-                  x))
-  :enable ubyte7p)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
