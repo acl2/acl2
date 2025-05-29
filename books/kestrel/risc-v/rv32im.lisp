772974637bbe058f51735156dede61c6918135ea
Alessandro Coglio <email@alessandrocoglio.info>
2025-05-29 12:57:20 -0700

    [RISC-V] Add KT to file headers.

--- books/kestrel/risc-v/rv32im.lisp
+++ books/kestrel/risc-v/rv32im.lisp
@@ -1,6 +1,7 @@
 ; RISC-V Library
 ;
 ; Copyright (C) 2025 Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2025 Kestrel Technology LLC (http://kestreltechnology.com)
 ;
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 ;
