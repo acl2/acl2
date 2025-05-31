1727966407862a5655300c61451744ed7db7b9ae
Alessandro Coglio <email@alessandrocoglio.info>
2025-05-31 10:02:27 -0700

    [AleoBFT] Update top-level doc.

--- books/projects/aleo/bft/top.lisp
+++ books/projects/aleo/bft/top.lisp
@@ -40,33 +40,47 @@
    (xdoc::p
     "This directory contains
      a formal model and correctness proofs of AleoBFT.
-     More precisely, the subdirectory @('current')
-     contains the current version of the model and proofs,
-     while the subdirectory @('next') contains
-     an in-progress extension of the model and proofs,
-     which will replace the version in @('current') when completed.")
+     The model is an abstraction of AleoBFT
+     that mainly captures the Bullshark aspects of the protocol,
+     but with dynamic committees and with stake,
+     which are significant extensions to Bullshark.
+     The Narwhal aspects of AleoBFT
+     are modeled only at an abstract level,
+     similarly to the way that the Bullshark papers model
+     the underlying DAG consensus layer.
+     The level of abstraction of this model
+     is about the same as the Bullshark papers.
+     This model does not capture garbage collection or syncing.")
    (xdoc::p
-    "The version in @('current') models dynamic committees with stake,
-     but does not model syncing yet;
-     it models certificate creation as an atomic event.
-     The version in ('next') refines certificate creation
+    "The subdirectory @('next') contains
+     an in-progress extension of the model and proofs,
+     which will replace the current version when completed.
+     This extension refines certificate creation,
+     which in the current model is an atomic event,
      to be a multi-step process,
      with explicit creation and exchange of
-     proposals, endorsements, and certificates.
-     Modeling syncing is future work.
-     The proofs of correctness in both @('current') and @('next')
+     proposals, endorsements, and certificates;
+     that is, the extensions models the Narwhal aspects of AleoBFT
+     in more detail.
+     Modeling syncing and garbage collection
+     is not part of this @('next') extension;
+     it is future work, along with other possible refinements of the model.")
+   (xdoc::p
+    "The proofs of correctness in both the current and next version
      mainly concern the safety property that blockchains do not fork.
      Proving other correctness properties is future work.")
    (xdoc::p
-    "The current version of the model and proof is
+    "The current version of the model and proofs is
      the last one of a series of versions that we have developed.
      We started with a version with static committees without stake,
      which was therefore very similar to plain Bullshark.
      Then we made the significant extension to dynamic committees,
      initially without stake, and then with stake;
      adding stake was not a big extension.
-     The next version also has dynamic committees with stake,
-     but has a more refined model of certificate creation (see above).")
+     The @('next') version also has dynamic committees with stake,
+     but has a more refined model of certificate creation (see above).
+     Since each new version subsumes the previous ones as special cases,
+     there is no need to keep the previous versions.")
    (xdoc::p
     "The current version corresponds to the contents of "
     (xdoc::ahref "https://arxiv.org/abs/2504.16853" "this arXiv paper")
