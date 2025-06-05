94bee6782c97820f410c64ef3bdfdefc88052644
Alessandro Coglio <email@alessandrocoglio.info>
2025-06-05 09:17:23 -0700

    [PFCS] Put tests into separate file.

--- books/projects/pfcs/tokenizer.lisp
+++ books/projects/pfcs/tokenizer.lisp
@@ -189,30 +189,3 @@
        ((when (reserrp subtoken-trees))
         (reserrf "problem with structure of lexeme tree")))
     subtoken-trees))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-;; example
-
-(defun subtoken-treep (tree)
-  (or (is-tree-rulename? tree "identifier")
-      (is-tree-rulename? tree "integer")
-      (is-tree-rulename? tree "operator")
-      (is-tree-rulename? tree "separator")))
-
-(defun subtoken-tree-listp (tl)
-  (if (endp tl)
-      t
-    (and (subtoken-treep (car tl))
-         (subtoken-tree-listp (cdr tl)))))
-
-(assert-event
- (let ((subtoken-trees
-(tokenize-pfcs "boolean_and(x,y,z) := {
-  x * y == z
-}
-boolean_and(w0, w1, w2)")))
-  (and (not (reserrp subtoken-trees))
-       (subtoken-tree-listp subtoken-trees))))
