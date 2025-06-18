69c92d2bf78abd9d63de85b2d1a961b35927b553
Alessandro Coglio <email@alessandrocoglio.info>
2025-06-18 14:23:05 -0700

    [Leo early] Add top file for JSON-to-AST converter.

---
+++ books/projects/aleo/leo/early-version/json2ast/top.lisp
@@ -0,0 +1,15 @@
+; Leo Library
+;
+; Copyright (C) 2025 Provable Inc.
+;
+; License: See the LICENSE file distributed with this library.
+;
+; Authors: Alessandro Coglio (www.alessandrocoglio.info)
+;          Eric McCarthy (bendyarm on GitHub)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "LEO-EARLY")
+
+(include-book "json-span-removal")
+(include-book "json2ast")
