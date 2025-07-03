54185fba21294f2786a90854f26e12e55d45a1c5
Alessandro Coglio <email@alessandrocoglio.info>
2025-07-02 20:35:55 -0700

    [Std/util] Tweak `defmapping` user doc.

--- books/std/util/defmapping-doc.lisp
+++ books/std/util/defmapping-doc.lisp
@@ -1,6 +1,6 @@
 ; Standard Utilities Library
 ;
-; Copyright (C) 2024 Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2025 Kestrel Institute (http://www.kestrel.edu)
 ;
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 ;
@@ -72,7 +72,9 @@
       In this case, the domains are subsets of
       cartesian products of the ACL2 universe of values,
       and the conversions map tuples to tuples,
-      as shown in the `Generalization to Tuples' page of the design notes."))
+      as shown in the `Generalization to Tuples' page of the "
+     *defmapping-design-notes*
+     "."))
 
    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
