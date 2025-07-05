e9e749f91715f9ebe7b123374bf7066e8d0503d8
Alessandro Coglio <email@alessandrocoglio.info>
2025-07-04 22:05:00 -0700

    [RISC-V] Start specializing `read-xreg-unsigned` for RV32I.
    Partially evaluate and simplify to eliminate the features input.
    
    Isomorphic data transformation to use the specialized states. Simplification
    still to do.

--- books/kestrel/risc-v/specialized/states32i.lisp
+++ books/kestrel/risc-v/specialized/states32i.lisp
@@ -15,6 +15,9 @@
 
 (include-book "../specification/states")
 
+(include-book "kestrel/apt/isodata" :dir :system)
+(include-book "kestrel/apt/parteval" :dir :system)
+(include-book "kestrel/apt/simplify" :dir :system)
 (include-book "kestrel/fty/deflist-of-len" :dir :system)
 (include-book "std/util/defiso" :dir :system)
 
@@ -22,6 +25,13 @@
 (local (acl2::disable-most-builtin-logic-defuns))
 (local (acl2::disable-builtin-rewrite-rules-for-defaults))
 (set-induction-depth-limit 0)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defrulel lnfix-when-natp
+  (implies (natp x)
+           (equal (lnfix x) x))
+  :enable nfix)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -75,7 +85,14 @@
                 (stat-validp x (feat-rv32im-be))))
     :enable (stat-validp
              (:e feat-rv32i-le)
-             (:e feat-rv32im-be))))
+             (:e feat-rv32im-be)))
+
+  (defruled unsigned-byte-p-32-of-nth-of-stat-rv32i->xregs
+    (implies (and (stat-validp stat (feat-rv32i-le))
+                  (natp reg)
+                  (< reg 32))
+             (unsigned-byte-p 32 (nth (1- reg) (stat->xregs stat))))
+    :enable (stat-validp nfix (:e feat-rv32i-le))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -197,4 +214,50 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; TODO: specialize and refine operations on states
+(defsection read32i-xreg-unsigned{0}
+  :short "Partially evaluate @(tsee read-xreg-unsigned)
+          for the RV32I base."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "We pick @(tsee feat-rv32i-le),
+     but we could have picked any of the variants for RV32I."))
+
+  (apt::parteval read-xreg-unsigned
+                 ((feat (feat-rv32i-le)))
+                 :new-name read32i-xreg-unsigned{0}))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defsection read32i-xreg-unsigned{1}
+  :short "Simplify @(tsee read32i-xreg-unsigned{0}) after partial evaluation."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "We assume the guard so that we can eliminate the fixers."))
+
+  (apt::simplify read32i-xreg-unsigned{0}
+    :new-name read32i-xreg-unsigned{1}
+    :simplify-guard t
+    :assumptions :guard
+    :enable (unsigned-byte-p-32-of-nth-of-stat-rv32i->xregs
+             (:e feat-rv32i-le))
+    :disable (lnfix
+              unsigned-byte-fix)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defsection read32i-xreg-unsigned{2}
+  :short "Refine @(tsee read32i-xreg-unsigned{1})
+          to use the isomorphic states @(tsee stat32i)."
+
+  (apt::isodata read32i-xreg-unsigned{1}
+                ((stat stat32i-iso))
+                :undefined 0
+                :new-name read32i-xreg-unsigned{2}
+                :hints (("Goal" :in-theory (enable stat-rv32i-p
+                                                   (:e feat-rv32i-le))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; TODO: simplify to remove conversions and old state accessor
