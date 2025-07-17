48e8a537714f6c3b2916ce24d13f378357789ff4
Grant Jurgensen <grant@jurgensen.dev>
2025-07-17 10:57:07 -0500

    Add message utilities
    This adds msg-fix, msg$, and retmsg$. It also adds various rules and
    refactors kestrel/utilties/messages.lisp.

--- books/kestrel/utilities/messages.lisp
+++ books/kestrel/utilities/messages.lisp
@@ -1,10 +1,11 @@
 ; Messages
 ;
-; Copyright (C) 2018 Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2025 Kestrel Institute (http://www.kestrel.edu)
 ;
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 ;
 ; Author: Alessandro Coglio (coglio@kestrel.edu)
+; Supporting author: Grant Jurgensen (grant@kestrel.edu)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -14,12 +15,42 @@
 (include-book "std/util/define" :dir :system)
 (include-book "std/util/deflist" :dir :system)
 (include-book "std/util/defrule" :dir :system)
+(include-book "xdoc/constructors" :dir :system)
+
+(local (include-book "kestrel/built-ins/disable" :dir :system))
+(local (acl2::disable-most-builtin-logic-defuns))
+(local (acl2::disable-builtin-rewrite-rules-for-defaults))
+(local (in-theory (disable (tau-system))))
+(set-induction-depth-limit 0)
+
+(local (include-book "kestrel/alists-light/acons" :dir :system))
+(local (include-book "kestrel/alists-light/assoc-equal" :dir :system))
+(local (include-book "kestrel/strings-light/char" :dir :system))
+(local (include-book "kestrel/utilities/acl2-count" :dir :system))
+(local (include-book "kestrel/utilities/msgp" :dir :system))
+(local (include-book "kestrel/utilities/ordinals" :dir :system))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(std::make-define-config
+  :no-function t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defxdoc message-utilities
   :parents (kestrel-utilities)
   :short "Utilities for <see topic='@(url msgp)'>messages</see>.")
+
+(defxdoc msg$
+  :parents (msg message-utilities)
+  :short "A variant of @('msg') which is easier to reason about."
+  :long
+  (xdoc::topstring
+    (xdoc::p
+      "@('msg$') is a macro which will rewrite to a call of @('msg$-logic').
+       @('msg$-logic') is kept disabled and is known to produce a
+       @('msgp'). For execution, @('msg$') expands to something which is
+       inlined for efficiency.")))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -31,12 +62,19 @@
       (null x))
   ///
 
+  (defrule maybe-msgp-compound-recognizer
+    (if (maybe-msgp x)
+        (or (equal x nil)
+            (stringp x)
+            (and (consp x)
+                 (true-listp x)))
+      (and (not (equal x nil))
+           (not (stringp x))))
+    :rule-classes :compound-recognizer)
+
   (defrule maybe-msgp-when-msgp
     (implies (msgp x)
-             (maybe-msgp x)))
-
-  (defrule maybe-msgp-of-nil
-    (maybe-msgp nil)))
+             (maybe-msgp x))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -49,8 +87,257 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(define msg-fix ((msg msgp))
+  :returns (msg$ msgp)
+  (mbe :logic (if (msgp msg)
+                  msg
+                "")
+       :exec (the (or string cons) msg))
+  :inline t)
+
+;; Subsumed by msg-fix-type-prescription
+(in-theory (disable (:t msg-fix)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defrule msg-fix-type-prescription
+  (or (stringp (msg-fix msg))
+      (and (consp (msg-fix msg))
+           (true-listp (msg-fix msg))))
+  :rule-classes :type-prescription
+  :enable msg-fix)
+
+(defrule msg-fix-when-msgp
+  (implies (msgp msg)
+           (equal (msg-fix msg)
+                  msg))
+  :enable msg-fix)
+
+(defruled msg-fix-when-not-msgp
+  (implies (not (msgp msg))
+           (equal (msg-fix msg)
+                  ""))
+  :enable msg-fix)
+
+(defrule msg-fix-when-not-msgp-cheap
+  (implies (not (msgp msg))
+           (equal (msg-fix msg)
+                  ""))
+  :rule-classes ((:rewrite :backchain-limit-lst (0)))
+  :by msg-fix-when-not-msgp)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defrule stringp-of-car-of-msg-fix
+  (equal (stringp (car (msg-fix msg)))
+         (not (stringp (msg-fix msg))))
+  :enable msg-fix)
+
+(defrule character-alistp-of-cdr-of-msg-fix
+  (character-alistp (cdr (msg-fix msg)))
+  :enable (msg-fix
+           msgp
+           character-alistp))
+
+(defrule alistp-of-cdr-of-msg-fix
+  (alistp (cdr (msg-fix msg)))
+  :enable (msg-fix
+           msgp
+           alistp))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define msg$0 ((str stringp))
+  (mbe :logic (if (stringp str) str "")
+       :exec str)
+  :inline t)
+
+(define msg$1 ((str stringp) arg0)
+  (msg (mbe :logic (if (stringp str) str "")
+            :exec str)
+       arg0)
+  :inline t)
+
+(define msg$2 ((str stringp) arg0 arg1)
+  (msg (mbe :logic (if (stringp str) str "")
+            :exec str)
+       arg0 arg1)
+  :inline t)
+
+(define msg$3 ((str stringp) arg0 arg1 arg2)
+  (msg (mbe :logic (if (stringp str) str "")
+            :exec str)
+       arg0 arg1 arg2)
+  :inline t)
+
+(define msg$4 ((str stringp) arg0 arg1 arg2 arg3)
+  (msg (mbe :logic (if (stringp str) str "")
+            :exec str)
+       arg0 arg1 arg2 arg3)
+  :inline t)
+
+(define msg$5 ((str stringp) arg0 arg1 arg2 arg3 arg4)
+  (msg (mbe :logic (if (stringp str) str "")
+            :exec str)
+       arg0 arg1 arg2 arg3 arg4)
+  :inline t)
+
+(define msg$6 ((str stringp) arg0 arg1 arg2 arg3 arg4 arg5)
+  (msg (mbe :logic (if (stringp str) str "")
+            :exec str)
+       arg0 arg1 arg2 arg3 arg4 arg5)
+  :inline t)
+
+(define msg$7 ((str stringp) arg0 arg1 arg2 arg3 arg4 arg5 arg6)
+  (msg (mbe :logic (if (stringp str) str "")
+            :exec str)
+       arg0 arg1 arg2 arg3 arg4 arg5 arg6)
+  :inline t)
+
+(define msg$8 ((str stringp) arg0 arg1 arg2 arg3 arg4 arg5 arg6 arg7)
+  (msg (mbe :logic (if (stringp str) str "")
+            :exec str)
+       arg0 arg1 arg2 arg3 arg4 arg5 arg6 arg7)
+  :inline t)
+
+(define msg$9 ((str stringp) arg0 arg1 arg2 arg3 arg4 arg5 arg6 arg7 arg8)
+  (msg (mbe :logic (if (stringp str) str "")
+            :exec str)
+       arg0 arg1 arg2 arg3 arg4 arg5 arg6 arg7 arg8)
+  :inline t)
+
+(define msg$10 ((str stringp) arg0 arg1 arg2 arg3 arg4 arg5 arg6 arg7 arg8 arg9)
+  (msg (mbe :logic (if (stringp str) str "")
+            :exec str)
+       arg0 arg1 arg2 arg3 arg4 arg5 arg6 arg7 arg8 arg9)
+  :inline t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define msg$-logic (str args)
+  (declare (xargs :type-prescription
+                  (or (stringp (msg$-logic str args))
+                      (and (consp (msg$-logic str args))
+                           (true-listp (msg$-logic str args))))
+                  :guard (hide (comment "msg$-logic is not intended to be used in code. Use msg$ instead."
+                                        (true-listp args)))
+                  :guard-hints (("Goal" :expand ((:free (x) (hide x)))))))
+  :returns (msg msgp
+                :hints (("Goal" :in-theory (enable msgp))))
+  (if (consp args)
+      (cons (if (stringp str) str "")
+            (if (<= (len args) 10)
+                (pairlis$ (take (len args) *base-10-chars*) args)
+              nil))
+    (if (stringp str) str ""))
+
+  :prepwork
+  ((defrulel character-alistp-of-pairlis$
+     (implies (and (character-listp x)
+                   (equal (len x) (len y)))
+              (character-alistp (pairlis$ x y)))
+     :induct t
+     :enable (pairlis$
+              character-alistp
+              character-listp
+              len))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defrule msg$0-becomes-msg$-logic
+  (equal (msg$0 str)
+         (msg$-logic str nil))
+  :enable (msg$0 msg$-logic))
+
+(defrule msg$1-becomes-msg$-logic
+  (equal (msg$1 str arg0)
+         (msg$-logic str (list arg0)))
+  :enable (msg$1 msg$-logic len pairlis$))
+
+(defrule msg$2-becomes-msg$-logic
+  (equal (msg$2 str arg0 arg1)
+         (msg$-logic str (list arg0 arg1)))
+  :enable (msg$2 msg$-logic len pairlis$))
+
+(defrule msg$3-becomes-msg$-logic
+  (equal (msg$3 str arg0 arg1 arg2)
+         (msg$-logic str (list arg0 arg1 arg2)))
+  :enable (msg$3 msg$-logic len pairlis$))
+
+(defrule msg$4-becomes-msg$-logic
+  (equal (msg$4 str arg0 arg1 arg2 arg3)
+         (msg$-logic str (list arg0 arg1 arg2 arg3)))
+  :enable (msg$4 msg$-logic len pairlis$))
+
+(defrule msg$5-becomes-msg$-logic
+  (equal (msg$5 str arg0 arg1 arg2 arg3 arg4)
+         (msg$-logic str (list arg0 arg1 arg2 arg3 arg4)))
+  :enable (msg$5 msg$-logic len pairlis$))
+
+(defrule msg$6-becomes-msg$-logic
+  (equal (msg$6 str arg0 arg1 arg2 arg3 arg4 arg5)
+         (msg$-logic str (list arg0 arg1 arg2 arg3 arg4 arg5)))
+  :enable (msg$6 msg$-logic len pairlis$))
+
+(defrule msg$7-becomes-msg$-logic
+  (equal (msg$7 str arg0 arg1 arg2 arg3 arg4 arg5 arg6)
+         (msg$-logic str (list arg0 arg1 arg2 arg3 arg4 arg5 arg6)))
+  :enable (msg$7 msg$-logic len pairlis$))
+
+(defrule msg$8-becomes-msg$-logic
+  (equal (msg$8 str arg0 arg1 arg2 arg3 arg4 arg5 arg6 arg7)
+         (msg$-logic str (list arg0 arg1 arg2 arg3 arg4 arg5 arg6 arg7)))
+  :enable (msg$8 msg$-logic len pairlis$))
+
+(defrule msg$9-becomes-msg$-logic
+  (equal (msg$9 str arg0 arg1 arg2 arg3 arg4 arg5 arg6 arg7 arg8)
+         (msg$-logic str (list arg0 arg1 arg2 arg3 arg4 arg5 arg6 arg7 arg8)))
+  :enable (msg$9 msg$-logic len pairlis$))
+
+(defrule msg$10-becomes-msg$-logic
+  (equal (msg$10 str arg0 arg1 arg2 arg3 arg4 arg5 arg6 arg7 arg8 arg9)
+         (msg$-logic str
+                     (list arg0 arg1 arg2 arg3 arg4 arg5 arg6 arg7 arg8 arg9)))
+  :enable (msg$10 msg$-logic len pairlis$))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defmacro msg$ (str &rest args)
+  (declare (xargs :guard (<= (len args) 10)))
+  (case (len args)
+    (0 (list 'msg$0 str))
+    (1 (list 'msg$1 str (first args)))
+    (2 (list 'msg$2 str (first args) (second args)))
+    (3 (list 'msg$3 str (first args) (second args) (third args)))
+    (4 (list 'msg$4 str (first args) (second args) (third args) (fourth args)))
+    (5 (list 'msg$5 str (first args) (second args) (third args) (fourth args)
+             (fifth args)))
+    (6 (list 'msg$6 str (first args) (second args) (third args) (fourth args)
+             (fifth args) (sixth args)))
+    (7 (list 'msg$7 str (first args) (second args) (third args) (fourth args)
+             (fifth args) (sixth args) (seventh args)))
+    (8 (list 'msg$8 str (first args) (second args) (third args) (fourth args)
+             (fifth args) (sixth args) (seventh args) (eighth args)))
+    (9 (list 'msg$9 str (first args) (second args) (third args) (fourth args)
+             (fifth args) (sixth args) (seventh args) (eighth args)
+             (ninth args)))
+    (10 (list 'msg$10 str (first args) (second args) (third args) (fourth args)
+              (fifth args) (sixth args) (seventh args) (eighth args)
+              (ninth args) (tenth args)))
+    (otherwise nil)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defmacro retmsg$ (str &rest args)
+  (declare (xargs :guard (<= (len args) 10)))
+  `(reterr (msg$ ,str ,@args)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (define msg-downcase-first ((msg msgp))
-  :returns (new-msg msgp :hyp :guard)
+  :returns (new-msg msgp
+                    :hints (("Goal" :induct t
+                                    :in-theory (enable character-alistp))))
   :parents (message-utilities)
   :short "Convert the first character
           of a <see topic='@(url msg)'>structured message</see>
@@ -129,7 +416,8 @@
    This function leaves the message unchanged
    if some of these properties do not hold.
    </p>"
-  (b* (((when (stringp msg)) (str::downcase-first msg))
+  (b* ((msg (msg-fix msg))
+       ((when (stringp msg)) (str::downcase-first msg))
        ((cons string alist) msg)
        ((unless (and (> (length string) 0)
                      (eql (char string 0) #\~)))
@@ -158,10 +446,14 @@
                                         alist)))
                    (cons string new-alist)))
       (otherwise (prog2$ (raise "Message not supported: ~x0" msg)
-                         msg)))))
+                         msg))))
+  :no-function nil
+  :measure (acl2-count (msg-fix msg)))
 
 (define msg-upcase-first ((msg msgp))
-  :returns (new-msg msgp :hyp :guard)
+  :returns (new-msg msgp
+                    :hints (("Goal" :induct t
+                                    :in-theory (enable character-alistp))))
   :parents (message-utilities)
   :short "Convert the first character
           of a <see topic='@(url msg)'>structured message</see>
@@ -175,7 +467,8 @@
    of the tilde-directives are swapped.
    See the documentation of @(tsee msg-downcase-first).
    </p>"
-  (b* (((when (stringp msg)) (str::upcase-first msg))
+  (b* ((msg (msg-fix msg))
+       ((when (stringp msg)) (str::upcase-first msg))
        ((cons string alist) msg)
        ((unless (and (> (length string) 0)
                      (eql (char string 0) #\~)))
@@ -204,4 +497,6 @@
                                         alist)))
                    (cons string new-alist)))
       (otherwise (prog2$ (raise "Message not supported: ~x0" msg)
-                         msg)))))
+                         msg))))
+  :no-function nil
+  :measure (acl2-count (msg-fix msg)))
