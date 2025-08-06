50cc3a1e26fa755da7ab713f2f0a33334733f9e5
Grant Jurgensen <grant@jurgensen.dev>
2025-08-06 09:44:25 -0500

    [C$] Factor out GCC built-ins
    Since the disambiguator and validator both depend on this list, it was
    factored out into its own book.

--- books/kestrel/c/syntax/disambiguator.lisp
+++ books/kestrel/c/syntax/disambiguator.lisp
@@ -11,6 +11,7 @@
 
 (in-package "C$")
 
+(include-book "builtin")
 (include-book "unambiguity")
 
 (include-book "kestrel/utilities/messages" :dir :system)
@@ -3454,7 +3455,7 @@
    (xdoc::p
     "We extend the disambiguation table with the identifier @('__func__')
      [C17:6.4.2.2].
-     If the GCC flag is @('t') (i.e. allow GCC extensions),
+     If the GCC flag is enabled (i.e. GCC extensions are allowed),
      we further extend the table with the identifiers @('__FUNCTION__') and
      @('__PRETTY_FUNCTION__') (GCC manual, "
     (xdoc::ahref "https://gcc.gnu.org/onlinedocs/gcc/Function-Names.html"
@@ -3594,7 +3595,7 @@
      To add to the strangeness,
      one can change the above initializer to @('__eax + 1')
      (and presumably other similar expressions)
-     and the compiler acceptes it.")
+     and the compiler accepts it.")
    (xdoc::p
     "However, none of this matters for the disambiguator,
      which does not need to validate the code,
@@ -3618,80 +3619,7 @@
        (table (dimb-init-table))
        (table
          (if gcc
-             (dimb-add-idents-objfun
-               (list (ident "__atomic_signal_fence")
-                     (ident "__builtin___memcpy_chk")
-                     (ident "__builtin___memmove_chk")
-                     (ident "__builtin___mempcpy_chk")
-                     (ident "__builtin___memset_chk")
-                     (ident "__builtin___snprintf_chk")
-                     (ident "__builtin___sprintf_chk")
-                     (ident "__builtin___stpcpy_chk")
-                     (ident "__builtin___stpncpy_chk")
-                     (ident "__builtin___strcat_chk")
-                     (ident "__builtin___strcpy_chk")
-                     (ident "__builtin___strncat_chk")
-                     (ident "__builtin___strncpy_chk")
-                     (ident "__builtin___vsnprintf_chk")
-                     (ident "__builtin___vsprintf_chk")
-                     (ident "__builtin_add_overflow")
-                     (ident "__builtin_alloca")
-                     (ident "__builtin_apply")
-                     (ident "__builtin_apply_args")
-                     (ident "__builtin_bswap16")
-                     (ident "__builtin_bswap32")
-                     (ident "__builtin_bswap64")
-                     (ident "__builtin_choose_expr")
-                     (ident "__builtin_clz")
-                     (ident "__builtin_clzl")
-                     (ident "__builtin_clzll")
-                     (ident "__builtin_constant_p")
-                     (ident "__builtin_ctzl")
-                     (ident "__builtin_dynamic_object_size")
-                     (ident "__builtin_expect")
-                     (ident "__builtin_memchr")
-                     (ident "__builtin_memcmp")
-                     (ident "__builtin_memcpy")
-                     (ident "__builtin_memset")
-                     (ident "__builtin_mul_overflow")
-                     (ident "__builtin_object_size")
-                     (ident "__builtin_return_address")
-                     (ident "__builtin_strcpy")
-                     (ident "__builtin_strlen")
-                     (ident "__builtin_strncat")
-                     (ident "__builtin_strncpy")
-                     (ident "__builtin_sub_overflow")
-                     (ident "__builtin_unreachable")
-                     (ident "__builtin_va_arg_pack")
-                     (ident "__builtin_va_arg_pack_len")
-                     (ident "__builtin_va_end")
-                     (ident "__builtin_va_start")
-                     (ident "__eax")
-                     (ident "__ebp")
-                     (ident "__ebx")
-                     (ident "__ecx")
-                     (ident "__edi")
-                     (ident "__edx")
-                     (ident "__esi")
-                     (ident "__esp")
-                     (ident "__sync_add_and_fetch")
-                     (ident "__sync_and_and_fetch")
-                     (ident "__sync_bool_compare_and_swap")
-                     (ident "__sync_fetch_and_add")
-                     (ident "__sync_fetch_and_and")
-                     (ident "__sync_fetch_and_nand")
-                     (ident "__sync_fetch_and_or")
-                     (ident "__sync_fetch_and_sub")
-                     (ident "__sync_fetch_and_xor")
-                     (ident "__sync_lock_release")
-                     (ident "__sync_lock_test_and_set")
-                     (ident "__sync_nand_and_fetch")
-                     (ident "__sync_or_and_fetch")
-                     (ident "__sync_sub_and_fetch")
-                     (ident "__sync_synchronize")
-                     (ident "__sync_val_compare_and_swap")
-                     (ident "__sync_xor_and_fetch"))
-              table)
+             (dimb-add-idents-objfun *gcc-builtin* table)
            table))
        ((erp new-edecls &) (dimb-extdecl-list edecls table gcc)))
     (retok (make-transunit :decls new-edecls :info nil)))
