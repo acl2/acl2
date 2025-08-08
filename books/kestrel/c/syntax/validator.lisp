cc9ba0a26e1d453b7b065e98201c725997e24e3a
Grant Jurgensen <grant@jurgensen.dev>
2025-08-08 14:22:37 -0500

    [c$] Push/pop validation table for statement expressions
    This behavior now matches what is does for compound statements.
    
    A test case was also added to exercise behavior that was erroneously
    rejected before this change due to statement expressions not getting
    their own table scope.

--- books/kestrel/c/syntax/validator.lisp
+++ books/kestrel/c/syntax/validator.lisp
@@ -2291,8 +2291,10 @@
                        type
                        (set::union types1 types2)
                        table))
-       :stmt (b* (((erp new-items types type? table)
+       :stmt (b* ((table (valid-push-scope table))
+                  ((erp new-items types type? table)
                    (valid-block-item-list expr.items table ienv))
+                  (table (valid-pop-scope table))
                   (type (or type? (type-void))))
                (retok (expr-stmt new-items) type types table))
        :tycompat (b* (((erp new-type1 & types1 table)
