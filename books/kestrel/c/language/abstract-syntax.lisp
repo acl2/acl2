ca874c76902d7710638774a0eb3fbc183dad6ac9
Alessandro Coglio <email@alessandrocoglio.info>
2025-08-08 15:44:05 -0700

    [C] Refactor some code.
    Put identifiers into their own file and topic. This can help reduce
    dependencies, since some books now include the whole abstract syntax but only
    really need identifiers.

--- books/kestrel/c/language/abstract-syntax.lisp
+++ books/kestrel/c/language/abstract-syntax.lisp
@@ -11,8 +11,7 @@
 
 (in-package "C")
 
-(include-book "kestrel/fty/defresult" :dir :system)
-(include-book "kestrel/fty/defset" :dir :system)
+(include-book "identifiers")
 
 ; to generate more typed list theorems in FTY::DEFLIST:
 (local (include-book "std/lists/append" :dir :system))
@@ -65,49 +64,6 @@
      the translation phases [C17:5.1.1.2] in detail."))
   :order-subtopics t
   :default-parent t)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(fty::defprod ident
-  :short "Fixtype of identifiers [C17:6.4.2]."
-  :long
-  (xdoc::topstring
-   (xdoc::p
-    "For now we represent C identifiers as ACL2 strings,
-     which suffice to represent all the ASCII C identifiers.
-     We wrap ACL2 strings into a one-field product fixtype
-     to make it easier to modify or extend this fixtype in the future.")
-   (xdoc::p
-    "Unconstrained ACL2 strings may not be valid C ASCII identifiers.
-     In the future we may extend this fixtype
-     with suitable restrictions on the ACL2 string.")
-   (xdoc::p
-    "A C implementation may limit
-     the number of significant characters in identifiers
-     [C17:6.4.2.1/5] [C17:6.4.2.1/6] [C17:5.2.4.1],
-     to 31 for external identifiers and 63 for internal identifiers.
-     In the future, we may add this constraint to this fixtype."))
-  ((name string))
-  :tag :ident
-  :pred identp)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(fty::deflist ident-list
-  :short "Fixtype of lists of identifiers."
-  :elt-type ident
-  :true-listp t
-  :elementp-of-nil nil
-  :pred ident-listp
-  :prepwork ((local (in-theory (enable nfix)))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(fty::defset ident-set
-  :short "Fixtype of sets of identifiers."
-  :elt-type ident
-  :elementp-of-nil nil
-  :pred ident-setp)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
