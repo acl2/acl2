3a8eca0cc420e519ac8b295141e02f29b6a6952d
Grant Jurgensen <grant@jurgensen.dev>
2025-08-21 16:05:35 -0500

    [C$] Add make-self functions for abstract syntax

---
+++ books/kestrel/c/syntax/abstract-syntax-make-self.lisp
@@ -0,0 +1,19 @@
+; C Library
+;
+; Copyright (C) 2025 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Grant Jurgensen (grant@kestrel.edu)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "C$")
+
+(include-book "kestrel/fty/defmake-self" :dir :system)
+
+(include-book "code-ensembles")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(fty::defmake-self code-ensemble)
