575de6e4e5d91e5b6aaa16c3696f1a2553c853c3
Eric Smith <ewsmith@gmail.com>
2025-08-25 01:33:32 -0700

    [x86] Comment out unused function.

--- books/kestrel/x86/assumptions-new.lisp
+++ books/kestrel/x86/assumptions-new.lisp
@@ -51,12 +51,12 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Result is untranslated
-(defund symbolic-add-constant (constant term)
-  (declare (xargs :guard (integerp constant)))
-  (if (= 0 constant)
-      term
-    `(+ ,constant ,term)))
+;; ;; Result is untranslated
+;; (defund symbolic-add-constant (constant term)
+;;   (declare (xargs :guard (integerp constant)))
+;;   (if (= 0 constant)
+;;       term
+;;     `(+ ,constant ,term)))
 
 ;; Result is untranslated
 (defund symbolic-bvplus-constant (size constant term)
