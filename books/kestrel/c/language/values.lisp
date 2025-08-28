54a72fe40c5c536bf5750418e2861f581d94eb78
Alessandro Coglio <email@alessandrocoglio.info>
2025-08-28 16:16:21 -0700

    [C] Add an operation on statement values.

--- books/kestrel/c/language/values.lisp
+++ books/kestrel/c/language/values.lisp
@@ -858,3 +858,20 @@
                    :single (init-type-single (type-of-value ival.get))
                    :list (init-type-list (type-list-of-value-list ival.get)))
   :hooks (:fix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-option-of-stmt-value ((sval stmt-valuep))
+  :returns (type? type-optionp)
+  :short "Optional type of a statement value."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "We return @('nil') if the statement value is @(':none').
+     Otherwise, we map the optional value to a type,
+     which is @('void') if the value is absent."))
+  (stmt-value-case
+   sval
+   :none nil
+   :return (type-of-value-option sval.value?))
+  :hooks (:fix))
