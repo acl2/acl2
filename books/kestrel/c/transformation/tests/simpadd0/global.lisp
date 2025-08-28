2c71c507647cc4da88717c763637b3f794e881d7
Alessandro Coglio <email@alessandrocoglio.info>
2025-08-28 14:19:57 -0700

    [C2C] Add a `simpadd0` tests.

---
+++ books/kestrel/c/transformation/tests/simpadd0/global.lisp
@@ -0,0 +1,41 @@
+; C Library
+;
+; Copyright (C) 2025 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (www.alessandrocoglio.info)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "C2C")
+
+(include-book "../../simpadd0")
+
+(include-book "../../../syntax/input-files")
+(include-book "../../../syntax/output-files")
+
+(include-book "../utilities")
+
+; (depends-on "old/global.c")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(c$::input-files :files ("global.c")
+                 :path "old"
+                 :const *old-code*)
+
+(simpadd0 *old-code* *new-code*)
+
+(c$::output-files :const *new-code*
+                  :path "new")
+
+(assert-file-contents
+ :file "new/global.c"
+ :content "int x;
+int global() {
+  return x;
+}
+")
+
+; no assert-highest-thm-has-exec-fun here because of global variable
