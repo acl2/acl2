1061660eef79bee9fc89c6b85676f752696eb443
Matt Kaufmann <kaufmann@cs.utexas.edu>
2025-09-05 11:51:19 -0500

    Exclude some books from Allegro CL regressions.
    These books failed to certify with Allegro CL Version 10.1.  I'm
    pretty sure that this is evidence of a bug in that (rather old)
    version of Allegro CL.  I plan to explain this further in
    documentation and perhaps comment updates to come within the next day
    or two.

--- books/projects/aleo/vm/circuits/axe/blake2s-proof2.lisp
+++ books/projects/aleo/vm/circuits/axe/blake2s-proof2.lisp
@@ -8,6 +8,9 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+; Matt K. mod, due to regression failure 9/4/2025 using Allegro CL 10.1:
+; cert_param: (non-allegro)
+
 (in-package "R1CS")
 
 ;; STATUS: Complete but needs cleanup
