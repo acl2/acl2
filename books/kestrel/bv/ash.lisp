8fbaeed3ce10e51a80ce481766e3837af7dd1248
Eric Smith <ewsmith@gmail.com>
2025-09-15 21:45:52 -0700

    [bv] Continue improving the book about ash.

--- books/kestrel/bv/ash.lisp
+++ books/kestrel/bv/ash.lisp
@@ -35,32 +35,76 @@
 (local (include-book "kestrel/arithmetic-light/ash" :dir :system))
 
 ;move
-(defthm unsigned-byte-p-shift-lemma
-  (implies (and (natp n)
-                (unsigned-byte-p xsize x)
-                (<= n xsize))
-           (unsigned-byte-p (- xsize n) (floor (* x (expt 2 (- n))) 1)))
-  :hints (("Goal" :in-theory (enable unsigned-byte-p))))
+(local
+  (defthm unsigned-byte-p-shift-lemma
+    (implies (and (natp n)
+                  (unsigned-byte-p xsize x)
+                  (<= n xsize))
+             (unsigned-byte-p (- xsize n) (floor (* x (expt 2 (- n))) 1)))
+    :hints (("Goal" :in-theory (enable unsigned-byte-p)))))
 
+;move
+;; replaces the floor with /
+(defthm floor-of-*-of-expt2-and-expt2-when-<=
+  (implies (and (<= j i)
+                (integerp x)
+                (integerp i)
+                (integerp j))
+           (equal (floor (* x (expt 2 i)) (expt 2 j))
+                  (* x (expt 2 (- i j)))))
+  :hints (("Goal" :in-theory (enable expt-of-+))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defthm bvchop-of-ash
+  (implies (and (natp c) ; left shift
+                (natp size))
+           (equal (bvchop size (ash i c))
+                  (bvcat (- size c) i c 0)))
+  :hints (("Goal" :in-theory (enable ash slice logtail))))
+
+(defthm bvchop-of-ash-right-shift
+  (implies (and (< n 0)
+                (natp size)
+                (integerp n))
+           (equal (bvchop size (ash x n))
+                  (slice (+ -1 size (- n)) (- n) x)))
+  :hints (("Goal" :cases ((integerp x))
+           :in-theory (e/d (ash slice logtail ifix)
+                           (bvchop-of-logtail-becomes-slice)))))
+
+(defthm bvchop-of-ash-when-negative-becomes-bvshr
+  (implies (and (< c 0)
+                (integerp c)
+                (natp places))
+           (equal (bvchop places (ash i c))
+                  (bvshr (- places c) i (- c))))
+  :hints (("Goal" :in-theory (e/d (ash bvshr slice logtail ifix)
+                                  (bvchop-of-logtail-becomes-slice floor-of-2-becomes-logtail-of-1)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;combine?
 (defthm slice-of-ash
-  (implies (and (<= n low)
+  (implies (and (natp n) ; left shift
+                (<= n low) ; gen
                 (integerp low)
-                (integerp high)
-                (natp n))
+                (integerp high))
            (equal (slice high low (ash x n))
                   (slice (- high n) (- low n) x)))
-  :hints (("Goal" :in-theory (enable ash slice logtail ;floor
-                                     expt-of-+))))
+  :hints (("Goal" :cases ((<= low high))
+           :in-theory (enable ash slice logtail expt-of-+
+                              ))))
 
 ;can't just turn ash into slice because we don't know what the top bit is, so
 ;we need the overarching slice.
 (defthm slice-of-ash-right
-  (implies (and (< n 0)
+  (implies (and (< n 0) ; right shift
                 (natp low)
                 (natp high)
                 (integerp n))
            (equal (slice high low (ash x n))
-                  (slice (+ high (- n)) (+ low (- n)) x)))
+                  (slice (- high n) (- low n) x)))
   :hints (("Goal" :in-theory (enable ash slice logtail ;floor
                                      ifix
                                      expt-of-+))))
@@ -71,6 +115,8 @@
            (equal (slice high low (ash x low))
                   (bvchop (+ 1 (- high low)) x)))
   :hints (("Goal" :cases ((<= n 0)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defthmd logtail-becomes-ash
   (implies (natp n)
@@ -87,6 +133,8 @@
 
 (theory-invariant (incompatible (:rewrite ash-becomes-logtail)
                                 (:rewrite logtail-becomes-ash)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (local
   (defthm ash-negative-becomes-slice-helper
@@ -120,32 +168,11 @@
                   (bvcat (+ xsize amt) x amt 0)))
   :hints (("Goal" :in-theory (enable bvcat ash))))
 
-;rename
-(defthm bvchop-of-ash
-  (implies (and (natp size)
-                (natp n))
-           (equal (bvchop size (ash x n))
-                  (bvcat (- size n) x n 0)))
-  :hints (("Goal" :in-theory (enable ash slice logtail))))
 
-(defthm bvchop-of-ash-when-negative-becomes-bvshr
-  (implies (and (< c 0)
-                (integerp c)
-                (natp places))
-           (equal (bvchop places (ash i c))
-                  (bvshr (- places c) i (- c))))
-  :hints (("Goal" :in-theory (e/d (ash bvshr slice logtail ifix)
-                                  (acl2::bvchop-of-logtail-becomes-slice acl2::floor-of-2-becomes-logtail-of-1)))))
 
-(defthm bvchop-of-ash-right-shift
-  (implies (and (< n 0)
-                (natp size)
-                (integerp n))
-           (equal (bvchop size (ash x n))
-                  (slice (+ -1 size (- n)) (- n) x)))
-  :hints (("Goal" :cases ((integerp x))
-           :in-theory (e/d (ash slice logtail ifix)
-                           (acl2::bvchop-of-logtail-becomes-slice)))))
+
+
+
 
 (defthm ash-of-if
   (equal (ash (if test i1 i2) c)
