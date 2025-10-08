fbe0bdc62618e29b85463f8bd693a48cc8a1880d
Alessandro Coglio <email@alessandrocoglio.info>
2025-10-07 20:51:19 -0700

    [C2C] Add a `simpadd0` test with `do-while` loops.

---
+++ books/kestrel/c/transformation/tests/simpadd0/dowhile.lisp
@@ -0,0 +1,46 @@
+; C Library
+;
+; Copyright (C) 2025 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (www.alessandrocoglio.info)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "C2C")
+
+(include-book "../../simpadd0")
+
+(include-book "../../../syntax/input-files")
+(include-book "../../../syntax/output-files")
+
+(include-book "../utilities")
+
+; (depends-on "old/dowhile.c")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(c$::input-files :files '("dowhile.c")
+                 :path "old"
+                 :const *old-code*)
+
+(simpadd0 :const-old *old-code*
+          :const-new *new-code*)
+
+(c$::output-files :const *new-code*
+                  :path "new")
+
+(assert-file-contents
+ :file "new/dowhile.c"
+ :content "void f(int x, int y) {
+  do
+    x = x - 1;
+while (x >= 0);
+  do {
+    y = y * x;
+  } while (y <= 0);
+}
+")
+
+(assert-highest-thm-has-exec-fun *new-code*)
