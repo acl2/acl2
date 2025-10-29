c51962a3782f4789610d89385dc692416e1ba609
Eric Smith <ewsmith@gmail.com>
2025-10-29 01:00:07 -0700

    [bv] Add support for converting calls of ash to bv ops.

--- books/kestrel/bv/trim-elim-rules-non-bv.lisp
+++ books/kestrel/bv/trim-elim-rules-non-bv.lisp
@@ -29,6 +29,7 @@
 (local (include-book "logior-b"))
 (local (include-book "logand-b"))
 (local (include-book "logext"))
+(local (include-book "ash"))
 (local (include-book "kestrel/arithmetic-light/plus-and-minus" :dir :system))
 
 ;; WARNING: Keep these in sync with *functions-convertible-to-bv*.
@@ -106,3 +107,21 @@
   (equal (trim size (if test x y))
          (bvif size test x y))
   :hints (("Goal" :in-theory (enable trim))))
+
+;; or we could go to bvmult
+(defthm trim-of-ash-left-shift-becomes-bvcat
+  (implies (and (natp c) ; left shift
+                (natp size))
+           (equal (trim size (ash i c))
+                  (bvcat (- size c) i c 0)))
+  :hints (("Goal" :in-theory (e/d (trim) (ash)))))
+
+; todo: do I want slice or bvshr?
+(defthm trim-of-ash-right-shift-becomes-slice
+  (implies (and (< n 0) ; right shift
+                (natp size)
+                (integerp n))
+           (equal (trim size (ash x n))
+                  (slice (+ -1 size (- n)) (- n) x)))
+  :hints (("Goal" :in-theory (e/d (trim) (ash
+                                          bvchop-of-ash-when-negative-becomes-bvshr)))))
