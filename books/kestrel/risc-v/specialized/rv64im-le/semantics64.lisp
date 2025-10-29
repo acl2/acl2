475c0034fd75ed22e1fde523a663c44f1e304a6c
Alessandro Coglio <email@alessandrocoglio.info>
2025-10-29 16:37:55 -0700

    [RISC-V] Use controlled configuration in a file.

--- books/kestrel/risc-v/specialized/rv64im-le/semantics64.lisp
+++ books/kestrel/risc-v/specialized/rv64im-le/semantics64.lisp
@@ -25,6 +25,8 @@
 (local (include-book "kestrel/fty/ubyte16-ihs-theorems" :dir :system))
 (local (include-book "kestrel/fty/ubyte32-ihs-theorems" :dir :system))
 
+(acl2::controlled-configuration)
+
 ; cert_param: (non-acl2r)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -32,7 +34,11 @@
 (defrulel dab-digit-list-of-256-when-ubyte8-listp
   (implies (ubyte8-listp x)
            (acl2::dab-digit-listp 256 x))
-  :enable (ubyte8-listp ubyte8p acl2::dab-digitp))
+  :induct t
+  :enable (ubyte8-listp
+           ubyte8p
+           acl2::dab-digitp
+           unsigned-byte-p))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
