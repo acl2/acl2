c77fab3d91cb6cefa1ca31fffc42adf321a6dad3
Grant Jurgensen <grant@jurgensen.dev>
2025-11-15 12:46:35 -0600

    [C$] Fix xdoc for input-files
    Drops "when present" qualification now that :preprocess-args has a
    default and we no longer check whether it was explicitly given a value.
    
    Thanks to Alessandro Coglio for suggesting this change.

--- books/kestrel/c/syntax/input-files-doc.lisp
+++ books/kestrel/c/syntax/input-files-doc.lisp
@@ -148,7 +148,7 @@
                 (xdoc::td "@('-std=c23')")
                 (xdoc::td "@('-std=gnu23')")))
      (xdoc::p
-      "When present, the @(':preprocess-args') specifies additional arguments
+      "The @(':preprocess-args') specifies additional arguments
        to pass to the preprocessor beyond the two outlined above.
        If @(':preprocess-args') is a string list,
        this list of arguments is passed to the preprocessor for each file,
