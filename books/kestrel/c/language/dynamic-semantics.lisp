3da9e74b0a766269be27b1cfb383f46623eb29f7
Alessandro Coglio <email@alessandrocoglio.info>
2025-12-04 12:05:04 +0100

    [C] Update some doc.

--- books/kestrel/c/language/dynamic-semantics.lisp
+++ books/kestrel/c/language/dynamic-semantics.lisp
@@ -25,22 +25,15 @@
   :long
   (xdoc::topstring
    (xdoc::p
-    "We distinguish between pure (i.e. side-effect-free) expressions
-     and expressions that may have side effects.
-     We allow the latter to appear only in certain parts of statements,
-     and we put restrictions to ensure a predictable order of evaluation.
-     Pure expressions may be evaluated in any order;
-     we evaluate them left to right.")
-   (xdoc::p
     "We formalize a big-step operational interpretive semantics.
      To ensure the termination of the ACL2 mutually recursive functions
-     that formalize the execution of statements, function calls, etc.,
+     that formalize the execution of expressions, statements, etc.,
      these ACL2 functions take a limit on the depth of the recursive calls,
      which ends the recursion with an error when it reaches 0,
      which is decremented at each recursive call,
      and which is used as termination measure.
      Thus, a proof of total correctness
-     (i.e. the code terminates and produces correct results)
+     (i.e. the code terminates <i>and</i> produces correct results)
      involves showing the existence of sufficiently large limit values,
      while a proof of partial correctness
      (i.e. the code produces correct results if it terminates)
@@ -55,9 +48,12 @@
      However, it is accurate for our current uses
      (namely, supporting proof generation in "
     (xdoc::seetopic "atc" "ATC")
+    " and in "
+    (xdoc::seetopic "c2c::transformation-tools" "C2C")
     ". This dynamic semantics is work in progress;
      we plan to make it completely accurate
-     for all the covered subset of C."))
+     for all the covered subset of C,
+     and to continue extending the covered subset of C."))
   :order-subtopics t
   :default-parent t)
 
