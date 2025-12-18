cfff09d02599f1ad5022aa6dbfb297c267cd13bd
Alessandro Coglio <email@alessandrocoglio.info>
2025-12-18 14:44:17 -0800

    [ATC] Fix/improve some doc.

--- books/kestrel/c/atc/expression-generation.lisp
+++ books/kestrel/c/atc/expression-generation.lisp
@@ -119,7 +119,7 @@
                           (gin expr-ginp)
                           state)
   :returns (gout expr-goutp)
-  :short "Generate a C expression from an ACL2 variable."
+  :short "Generate a C expression and theorem from an ACL2 variable."
   :long
   (xdoc::topstring
    (xdoc::p
@@ -127,8 +127,8 @@
      Its information is looked up in the symbol table.")
    (xdoc::p
     "If the variable has pointer or array type and is not an external object,
-     its correctness theorem equates it to the @('-ptr') variable,
-     because the variable itself contains the pointed object or array."))
+     its correctness theorem equates its execution to the @('-ptr') variable,
+     because the ACL2 variable itself contains the pointed object or array."))
   (b* (((expr-gin gin) gin)
        (info (atc-get-var var gin.inscope))
        ((when (not info))
@@ -604,16 +604,13 @@
                            (gin expr-ginp)
                            state)
   :returns (mv erp (gout expr-goutp))
-  :short "Generate a C expression from an ACL2 term
+  :short "Generate a C expression and theorem from an ACL2 term
           that represents an integer conversion."
   :long
   (xdoc::topstring
    (xdoc::p
     "The expression and theorem for the argument
-     are generated in the caller, and passed here.")
-   (xdoc::p
-    "For now we do not generate the theorem;
-     we will add suppor for that later."))
+     are generated in the caller, and passed here."))
   (b* (((reterr) (irr-expr-gout))
        (wrld (w state))
        ((expr-gin gin) gin)
@@ -751,7 +748,7 @@
                                      (gin expr-ginp)
                                      state)
   :returns (mv erp (gout expr-goutp))
-  :short "Generate a C expression from an ACL2 term
+  :short "Generate a C expression and theorem from an ACL2 term
           that represents a conversion to ACL2 boolean."
   :long
   (xdoc::topstring
@@ -857,7 +854,7 @@
                            (gin expr-ginp)
                            state)
   :returns (mv erp (gout expr-goutp))
-  :short "Generate a C expression from an ACL2 term
+  :short "Generate a C expression and theorem from an ACL2 term
           that represents a ternary conditional expression."
   (b* (((reterr) (irr-expr-gout))
        (wrld (w state))
@@ -1013,7 +1010,7 @@
                           (gin expr-ginp)
                           state)
   :returns (gout expr-goutp)
-  :short "Generate a C expression from an ACL2 term
+  :short "Generate a C expression and theorem from an ACL2 term
           that represents a logical conjunction."
   :long
   (xdoc::topstring
@@ -1152,7 +1149,7 @@
                          (gin expr-ginp)
                          state)
   :returns (gout expr-goutp)
-  :short "Generate a C expressino from an ACL2 term
+  :short "Generate a C expression and theorem from an ACL2 term
           that represents a logical disjunction."
   :long
   (xdoc::topstring
@@ -1290,7 +1287,7 @@
                                      (gin expr-ginp)
                                      state)
   :returns (mv erp (gout expr-goutp))
-  :short "Generate a C expression from an ACL2 term
+  :short "Generate a C expression and theorem from an ACL2 term
           that represents a conversion from ACL2 boolean."
   :long
   (xdoc::topstring
@@ -1370,7 +1367,7 @@
                                    state)
   :guard (type-nonchar-integerp type)
   :returns (mv erp (gout expr-goutp))
-  :short "Generate a C expression from an ACL2 term
+  :short "Generate a C expression and theorem from an ACL2 term
           that represents an indirection of a pointer to integer."
   :long
   (xdoc::topstring
@@ -1392,14 +1389,14 @@
      extend the theorem about the argument to also say that
      dereferncing the pointer yields the integer variable,
      i.e. the same assertion in the symbol table:
-     doing this obviated the need to use, in the theorem generated here,
+     doing this obviates the need to use, in the theorem generated here,
      the theorem from the symbol table.
      However, that approach makes the theorem about the argument expression
-     disuniform with other theorems about expressions;
+     nonuniform with other theorems about expressions;
      in particular, @(tsee atc-gen-expr-pure-correct-thm)
      would have to be generalized.
      Thus, the approach we use here seems better for now,
-     since the only slight ``disuniformity'' is in the fact that
+     since the only slight ``nonuniformity'' is in the fact that
      we need to retrieve and use the theorem from the symbol table.
      The current approach critically depends on
      the argument of the indirection operator always being a variable;
@@ -1499,7 +1496,7 @@
                                  state)
   :guard (type-nonchar-integerp elem-type)
   :returns (mv erp (gout expr-goutp))
-  :short "Generate a C expression from an ACL2 term
+  :short "Generate a C expression and theorem from an ACL2 term
           that represents an array read."
   :long
   (xdoc::topstring
@@ -1671,7 +1668,7 @@
                                          state)
   :guard (type-nonchar-integerp mem-type)
   :returns (mv erp (gout expr-goutp))
-  :short "Generate a C expression from an ACL2 term
+  :short "Generate a C expression and theorem from an ACL2 term
           that represents a structure scalar read."
   (b* (((reterr) (irr-expr-gout))
        ((expr-gin gin) gin)
@@ -1851,7 +1848,7 @@
                                         (gin expr-ginp)
                                         state)
   :returns (mv erp (gout expr-goutp))
-  :short "Generate a C expression from an ACL2 term
+  :short "Generate a C expression and theorem from an ACL2 term
           that represents a structure array read."
   (b* (((reterr) (irr-expr-gout))
        ((expr-gin gin) gin)
@@ -2166,9 +2163,7 @@
    (xdoc::p
     "We also generate correctness theorems for the generated expressions.
      The theorems relate (the semantics of) the expressions
-     to the ACL2 terms from which they are generated.
-     Fow now we only generate theorems for some expressions,
-     but eventually we plan to extend this to all the expressions.")
+     to the ACL2 terms from which they are generated.")
    (xdoc::p
     "As we generate the code, we ensure that the ACL2 terms
      are well-typed according to the C types.
@@ -2565,11 +2560,11 @@
            None of these is @('void').")
    (terms pseudo-term-listp
           "The terms from which the expressions are generated, in order.
-          The terms are transformed by replacing @(tsee if) with @(tsee if*).")
+           The terms are transformed by replacing @(tsee if) with @(tsee if*).")
    (events pseudo-event-form-list
            "All the events generated for the expressions.")
    (thm-names symbol-list
-              "The name of the theorems about @(tsee exec-expr-pure)
+              "The names of the theorems about @(tsee exec-expr-pure)
                applied to the expressions.
                These theorems are some of the events in @('events').
                These are all @('nil') if no theorems were generated,
@@ -2605,8 +2600,7 @@
   :long
   (xdoc::topstring
    (xdoc::p
-    "This lifts @(tsee atc-gen-expr-pure) to lists.
-     However, we do not return the C types of the expressions."))
+    "This lifts @(tsee atc-gen-expr-pure) to lists."))
   (b* (((reterr) (irr-exprs-gout))
        ((exprs-gin gin) gin)
        ((when (endp terms))
