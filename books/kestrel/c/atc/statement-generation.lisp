f72efb34ce91a8b6dc13734efee591f6dd674f2d
Alessandro Coglio <email@alessandrocoglio.info>
2025-12-23 13:11:44 -0800

    [ATC] Generalize some code.
    When dividing terms into components, also recognizer `mv` and treat it the same
    as `list`. This is so we can apply this operation to the result of
    `atc-loop-body-term-subst`, which replaces calls of the recursive function with
    calls of `mv`.

--- books/kestrel/c/atc/statement-generation.lisp
+++ books/kestrel/c/atc/statement-generation.lisp
@@ -311,7 +311,7 @@
                      (consp uterm)))
         (raise "Internal error: unexpected term ~x0." uterm)
         (mv nil nil nil))
-       ((when (eq (car uterm) 'list))
+       ((when (member-eq (car uterm) '(mv list)))
         (b* ((uterms (cdr uterm))
              ((unless (eql (len uterms) comps))
               (raise "Internal error: ~x0 components for ~x1." comps uterm)
