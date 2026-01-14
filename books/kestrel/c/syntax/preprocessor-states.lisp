c9fc77dc245c121cad7dc1e58312fcb3f3279d13
Alessandro Coglio <email@alessandrocoglio.info>
2026-01-14 13:20:46 -0800

    [C$] Add a predicate on preprocessing lexemes.

--- books/kestrel/c/syntax/preprocessor-states.lisp
+++ books/kestrel/c/syntax/preprocessor-states.lisp
@@ -356,6 +356,21 @@
          (or (equal string "#")
              (equal string "%:")))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define plexeme-hashhashp ((lexeme plexemep))
+  :returns (yes/no booleanp)
+  :short "Check if a lexeme is a double hash @('##')."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "That is, check if the lexeme is the punctuator @('#'),
+     or also if the lexeme is the digraph @('%:%:') [C17:6.4.6/3]."))
+  (and (plexeme-case lexeme :punctuator)
+       (b* ((string (plexeme-punctuator->punctuator lexeme)))
+         (or (equal string "##")
+             (equal string "%:%:")))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (fty::deftagsum lexmark
