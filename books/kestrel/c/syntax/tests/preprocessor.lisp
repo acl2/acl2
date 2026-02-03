1a962e59dd67932738eebb06460dc87d363d1fa8
Alessandro Coglio <email@alessandrocoglio.info>
2026-02-03 12:51:15 -0800

    [C$] Refactor preprocessor testing code.
    Put testing macros into a separate dedicated file.

--- books/kestrel/c/syntax/tests/preprocessor.lisp
+++ books/kestrel/c/syntax/tests/preprocessor.lisp
@@ -10,71 +10,7 @@
 
 (in-package "C$")
 
-(include-book "../input-files")
-(include-book "../preprocessor")
-
-(include-book "std/testing/assert-bang-stobj" :dir :system)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-; Build a fileset from alternating paths and contents,
-; e.g. (fileset-of "file1.c" "void f() {}" "file2.c" "int g(int x);").
-
-(defmacro fileset-of (&rest paths+contents)
-  `(fileset (fileset-map (list ,@paths+contents))))
-
-(defun fileset-map (paths+contents)
-  (b* (((when (endp paths+contents)) nil)
-       (path (car paths+contents))
-       (content (cadr paths+contents))
-       (content (if (stringp content)
-                    (acl2::string=>nats content)
-                  content)))
-    (omap::update (filepath path)
-                  (filedata content)
-                  (fileset-map (cddr paths+contents)))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-; Turn fileset into map from strings to strings.
-
-(defun fileset-map-to-string-map (fileset-map)
-  (b* (((when (omap::emptyp fileset-map)) nil)
-       ((mv filepath filedata) (omap::head fileset-map)))
-    (omap::update (filepath->unwrap filepath)
-                  (acl2::nats=>string (filedata->unwrap filedata))
-                  (fileset-map-to-string-map (omap::tail fileset-map)))))
-
-(defun fileset-to-string-map (fileset)
-  (fileset-map-to-string-map (fileset->unwrap fileset)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-; Check result of preprocessing against expectation.
-
-(defmacro test-preproc (files &key expected)
-  `(assert!-stobj
-    (b* ((files ,files)
-         (base-dir ".")
-         (include-dirs nil)
-         (ienv (ienv-default))
-         ((mv erp fileset state)
-          (pproc-files files base-dir include-dirs ienv state 1000000000)))
-      (mv (if erp
-              (cw "~@0" erp) ; CW returns NIL, so ASSERT!-STOBJ fails
-            (or (equal fileset ,expected)
-                (cw "Actual:~%~x0" ; CW returns nil (see above)
-                    (fileset-to-string-map fileset))))
-          state))
-    state))
-
-;;;;;;;;;;;;;;;;;;;;
-
-; Specialization to one file.
-
-(defmacro test-preproc-1 (file expected)
-  `(test-preproc '(,file)
-                 :expected (fileset-of ,file ,expected)))
+(include-book "preprocessor-testing-macros")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
