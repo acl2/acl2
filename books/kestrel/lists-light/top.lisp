c88e41ed75df99473adfe70dd8513ce51cb9735d
Eric Smith <ewsmith@gmail.com>
2026-02-04 19:10:01 -0800

    [lists-light] Update copyright year.

--- books/kestrel/lists-light/top.lisp
+++ books/kestrel/lists-light/top.lisp
@@ -1,6 +1,6 @@
 ; Top-level book for the lightweight-lists library.
 ;
-; Copyright (C) 2019-2025 Kestrel Institute
+; Copyright (C) 2019-2026 Kestrel Institute
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
 ;
