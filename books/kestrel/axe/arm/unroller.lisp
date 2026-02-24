4f47f55b91c2d69b7255eaff801c39650c13b5da
Eric Smith <ewsmith@gmail.com>
2026-02-24 13:54:56 -0800

    [axe] Improve unrolling lifters.
    Print info about monitored rules.

--- books/kestrel/axe/arm/unroller.lisp
+++ books/kestrel/axe/arm/unroller.lisp
@@ -474,6 +474,8 @@
        (debug-rules (debug-rules32) ; todo (if 64-bitp (debug-rules64) (debug-rules32))
                     )
        (rules-to-monitor (maybe-add-debug-rules debug-rules monitor))
+       (- (and rules-to-monitor (cw "(Monitoring: ~x0)~%" rules-to-monitor)))
+       (- (and (acl2::print-missing-rules rules-to-monitor lifter-rule-alist)))
        ;; Do the symbolic execution:
        (rule-to-limit (if stop-pcs 'run-until-return-or-reach-pc-aux-opener-axe 'run-until-return-aux-opener-axe))
        ((mv erp result-dag-or-quotep hits state)
