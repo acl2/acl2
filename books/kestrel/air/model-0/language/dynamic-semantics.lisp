0c5e2b087920773d39dbe90da42edb01037db965
Alessandro Coglio <email@alessandrocoglio.info>
2026-02-26 11:53:47 -0800

    [AIR] Move examples to separate files.

--- books/kestrel/air/model-0/language/dynamic-semantics.lisp
+++ books/kestrel/air/model-0/language/dynamic-semantics.lisp
@@ -241,29 +241,6 @@
     :enable (run run*-when-halted)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Example
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Example program: INCR, INCR, INCR, DECR, HALT
-;; Starting with acc=0, this computes: 0 -> 1 -> 2 -> 3 -> 2 -> halt
-(defconst *example-program*
-  (list (opcode-incr)
-        (opcode-incr)
-        (opcode-incr)
-        (opcode-decr)
-        (opcode-halt)))
-
-;; Verify execution
-(assert-event
- (equal (vm-state->acc
-         (run (initial-state 0) *example-program* 10))
-        2))
-
-(assert-event
- (vm-state->halted
-  (run (initial-state 0) *example-program* 10)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Termination
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
