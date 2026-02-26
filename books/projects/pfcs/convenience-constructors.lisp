8d9315fac55af13817fb14a4ac4455bd901b0bcb
Alessandro Coglio <email@alessandrocoglio.info>
2026-02-25 20:21:36 -0800

    [PFCS] Shorten some code.

--- books/projects/pfcs/convenience-constructors.lisp
+++ books/projects/pfcs/convenience-constructors.lisp
@@ -1,6 +1,6 @@
 ; PFCS (Prime Field Constraint System) Library
 ;
-; Copyright (C) 2025 Kestrel Institute (https://www.kestrel.edu)
+; Copyright (C) 2026 Kestrel Institute (https://www.kestrel.edu)
 ; Copyright (C) 2025 Provable Inc. (https://www.provable.com)
 ;
 ; License: See the LICENSE file distributed with this library.
@@ -14,10 +14,7 @@
 
 (include-book "abstract-syntax-trees")
 
-(local (include-book "kestrel/built-ins/disable" :dir :system))
-(local (acl2::disable-most-builtin-logic-defuns))
-(local (acl2::disable-builtin-rewrite-rules-for-defaults))
-(set-induction-depth-limit 0)
+(acl2::controlled-configuration)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
