8cb9e5334ab11641815ac21218597ccd65c7d007
Eric Smith <ewsmith@gmail.com>
2026-03-02 15:42:22 -0800

    [axe] Split out print-as-term-or-dag.

--- books/kestrel/axe/x86/unroller.lisp
+++ books/kestrel/axe/x86/unroller.lisp
@@ -61,6 +61,7 @@
 (include-book "../prune-dag-approximately")
 (include-book "../arithmetic-rules-axe")
 (include-book "../convert-to-bv-rules-axe")
+(include-book "../dag-printing")
 (include-book "../make-evaluator") ; for make-acons-nest ; todo: split out
 (include-book "../supporting-functions") ; for get-non-built-in-supporting-fns-list
 (include-book "../evaluator-support") ; for *axe-evaluator-functions* and to support making defuns
@@ -910,45 +911,7 @@
                     ))))
     (mv (erp-nil) result-dag-or-quotep assumptions input-assumption-vars lifter-rules assumption-rules term-to-simulate state)))
 
-;; todo: use this below!
-(defund print-as-term-or-dag (dag-or-quotep
-                             max-size
-                             maybe-actual-size ; if non-nil, this is the dag-size of DAG-OR-QUOTEP
-                             maybe-term ; if non-nil, this is a term equivalent to DAG-OR-QUOTEP
-                             descriptor ; a string describing DAG-OR-QUOTEP (e.g., "Result")
-                             untranslatep
-                             state ; because of untranslate$ (uses magic-ev-fncall)
-                             )
-  (declare (xargs :guard (and (or (pseudo-dagp dag-or-quotep) ; todo: name this disjunction
-                                  (myquotep dag-or-quotep))
-                              (natp max-size) ; allow nil for no limit?
-                              (or (null maybe-actual-size)
-                                  (natp maybe-actual-size))
-                              (or (null maybe-term)
-                                  (pseudo-termp maybe-term))
-                              (stringp descriptor)
-                              (booleanp untranslatep))
-                  :stobjs state))
-  (let ((quotep (quotep dag-or-quotep)))
-    (if (or quotep
-            (and (not (acl2::len-at-least 1152921504606846974 dag-or-quotep)) ; not too big to call dag-size
-                 (if maybe-actual-size
-                     (<= maybe-actual-size max-size)
-                   (<= (dag-size dag-or-quotep) max-size))))
-        ;; Print as a term (preferred if not too big):
-        (let ((term (if quotep
-                        dag-or-quotep
-                      (let ((term (or maybe-term (dag-or-quotep-to-term dag-or-quotep))))
-                        (if untranslatep
-                            (untranslate$ term nil state)
-                          term)))))
-          (progn$ (cw "(~s0:~%" descriptor)
-                  (cw "~X01" term nil)
-                  (cw ")~%")))
-      ;; Print as a DAG because the term would be too big:
-      (progn$ (cw "(~s0:~%" descriptor)
-              (cw "~X01" dag-or-quotep nil)
-              (cw ")~%")))))
+
 
 ;; Returns (mv erp event state)
 ;; TODO: Consider using the current print-base (:auto value) by default.
