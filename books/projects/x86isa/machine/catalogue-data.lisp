b9c4908d3c7c61e3ca669c33d0fed02aea22cfb1
Alessandro Coglio <email@alessandrocoglio.info>
2026-03-03 09:59:23 -0800

    [X86ISA] Add MMX packed shift instructions.

--- books/projects/x86isa/machine/catalogue-data.lisp
+++ books/projects/x86isa/machine/catalogue-data.lisp
@@ -240,8 +240,7 @@ what the instruction. Not sure why they are listed that way.</p>")
 
 (def-sdm-instruction-section "5.4.6 MMX Shift and Rotate Instructions"
   :mnemonics '(PSLLW PSLLD PSLLQ PSRLW PSRLD PSRLQ PSRAW PSRAD)
-  :features :mmx
-  :doc "<p>MMX versions are all unimplemented.</p>")
+  :features :mmx)
 
 (def-sdm-instruction-section "5.4.7 MMX State Management Instructions"
   :mnemonics '(EMMS)
