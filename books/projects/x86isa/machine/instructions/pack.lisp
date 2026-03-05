17c07c84b85406a3975e5c1a74082a75c7835299
Alessandro Coglio <email@alessandrocoglio.info>
2026-03-04 16:30:44 -0800

    [X86ISA] Add more general pack specification functions.

--- books/projects/x86isa/machine/instructions/pack.lisp
+++ books/projects/x86isa/machine/instructions/pack.lisp
@@ -4,7 +4,7 @@
 ; http://opensource.org/licenses/BSD-3-Clause
 
 ; Copyright (C) 2015, Regents of the University of Texas
-; Copyright (C) 2018, Kestrel Technology, LLC
+; Copyright (C) 2026, Kestrel Technology, LLC
 ; All rights reserved.
 
 ; Redistribution and use in source and binary forms, with or without
@@ -37,7 +37,7 @@
 ; Original Author(s):
 ; Cuong Chau          <ckcuong@cs.utexas.edu>
 ; Contributing Author(s):
-; Alessandro Coglio   <coglio@kestrel.edu>
+; Alessandro Coglio   (www.alessandrocoglio.info)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -46,7 +46,222 @@
 (include-book "../decoding-and-spec-utils" :ttags (:undef-flg))
 (include-book "centaur/bitops/merge" :dir :system)
 
+(local (include-book "arithmetic-3/top" :dir :system))
 (local (include-book "centaur/bitops/ihs-extensions" :dir :system))
+(local (include-book "kestrel/arithmetic-light/evenp" :dir :system))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Lemmas to verify guards and return types in the pack specification functions.
+
+(defruledl pack-spec-lemma1
+  (implies (and (natp total)
+                (posp chunk)
+                (evenp chunk)
+                (equal (mod total chunk) 0))
+           (evenp total))
+  :enable acl2::evenp-becomes-equal-of-0-and-mod
+  :disable acl2::mod-zero)
+
+(defruledl pack-spec-lemma2
+  (implies (and (natp total)
+                (posp chunk)
+                (evenp chunk)
+                (equal (mod total chunk) 0))
+           (integerp (* 1/2 total)))
+  :use pack-spec-lemma1
+  :enable evenp)
+
+(defruled pack-spec-lemma3
+  (implies (and (posp total)
+                (posp chunk)
+                (equal (mod total chunk) 0)
+                (evenp chunk)
+                (> total 0)
+                (unsigned-byte-p (- (* 1/2 total)
+                                    (* 1/2 chunk))
+                                 hi))
+           (unsigned-byte-p (* 1/2 total)
+                            (logapp (* 1/2 chunk) lo hi)))
+  :use (:instance unsigned-byte-p-of-logapp
+                  (size1 (* 1/2 total))
+                  (size (* 1/2 chunk))
+                  (i lo)
+                  (j hi))
+  :disable (unsigned-byte-p
+            unsigned-byte-p-of-logapp)
+  :enable (evenp pack-spec-lemma2))
+
+(defruled pack-spec-lemma4
+  (implies (and (natp size)
+                (unsigned-byte-p (/ size 2) hi))
+           (unsigned-byte-p size (logapp (* 1/2 size) lo hi)))
+  :use (:instance unsigned-byte-p-of-logapp
+                  (size1 size)
+                  (size (/ size 2))
+                  (i lo)
+                  (j hi))
+  :disable unsigned-byte-p-of-logapp)
+
+; Specification functions for the PACK... instructions.
+
+(define packus-spec-one ((total-size natp)
+                         (chunk-size posp)
+                         (x (unsigned-byte-p total-size x)))
+  :guard (and (equal (mod total-size chunk-size) 0)
+              (evenp chunk-size))
+  :returns (result (unsigned-byte-p (* 1/2 total-size) result)
+                   :hyp :guard
+                   :hints
+                   (("Goal"
+                     :induct (packus-spec-one total-size chunk-size x)
+                     :in-theory (e/d (pack-spec-lemma2
+                                      pack-spec-lemma3)
+                                     (unsigned-byte-p)))))
+  :parents (packus-spec)
+  :short "Specification of packing with unsigned saturation,
+          for just one operand."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "This is used in @(tsee packus-spec), for each operand,
+     before joining the individual results into the full result:
+     the function @(tsee packus-spec) specifies the PACKUS... instructions.")
+   (xdoc::p
+    "See Figure 4-6 in Intel manual Volume 2 (Feb 2026):
+     each 64-bit operand is packed into 32 bits,
+     forming half of the final result.
+     Here we generalize 64 to @('total-size') and 32 to @('chunk-size'),
+     where the former must be a multiple of the latter,
+     and where the chunk size must be even.
+     Each chunk of the operand @('x') is packed into a half chunk
+     (i.e. a value half the size of the chunk,
+     e.g. 16 bits for a 32-bit chunk),
+     using unsigned saturation on the signed value of the chunk.
+     That is, the chunk value is interpreted as signed (see @('x-lo') below),
+     and this value is saturated to 0 if negative,
+     and to the maximum unsigned value representable in half chunk if larger."))
+  (b* (((when (zp total-size)) 0)
+       ((unless (mbt (and (posp chunk-size)
+                          (evenp chunk-size))))
+        0)
+       (half-chunk-size (/ chunk-size 2))
+       (max-half-chunk-val (1- (expt 2 half-chunk-size)))
+       (x-lo (logext chunk-size x))
+       (result-lo (cond ((< x-lo 0) 0)
+                        ((> x-lo max-half-chunk-val) max-half-chunk-val)
+                        (t x-lo)))
+       (x-hi (logtail chunk-size x))
+       (result-hi (packus-spec-one (- total-size chunk-size) chunk-size x-hi)))
+    (logapp half-chunk-size result-lo result-hi))
+  :measure (nfix total-size)
+  :verify-guards :after-returns
+  :guard-hints (("Goal" :in-theory (enable evenp))))
+
+(define packss-spec-one ((total-size natp)
+                         (chunk-size posp)
+                         (x (unsigned-byte-p total-size x)))
+  :guard (and (equal (mod total-size chunk-size) 0)
+              (evenp chunk-size))
+  :returns (result (unsigned-byte-p (* 1/2 total-size) result)
+                   :hyp :guard
+                   :hints
+                   (("Goal"
+                     :induct (packss-spec-one total-size chunk-size x)
+                     :in-theory (e/d (pack-spec-lemma2
+                                      pack-spec-lemma3)
+                                     (unsigned-byte-p)))))
+  :parents (packss-spec)
+  :short "Specification of packing with signed saturation,
+          for just one operand."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "This is similar to @(tsee packus-spec-one):
+     see that function's documentation first.
+     The only difference here is that we do signed saturation,
+     i.e. we consider the signed range of each half chunk.
+     This function is used in @(tsee packss-spec),
+     which is used to specify the PACKSS... instructions."))
+  (b* (((when (zp total-size)) 0)
+       ((unless (mbt (and (posp chunk-size)
+                          (evenp chunk-size))))
+        0)
+       (half-chunk-size (/ chunk-size 2))
+       (max-half-chunk-val (1- (expt 2 half-chunk-size)))
+       (x-lo (logext chunk-size x))
+       (result-lo (cond ((< x-lo 0) 0)
+                        ((> x-lo max-half-chunk-val) max-half-chunk-val)
+                        (t x-lo)))
+       (x-hi (logtail chunk-size x))
+       (result-hi (packss-spec-one (- total-size chunk-size) chunk-size x-hi)))
+    (logapp half-chunk-size result-lo result-hi))
+  :measure (nfix total-size)
+  :verify-guards :after-returns
+  :guard-hints (("Goal" :in-theory (enable evenp))))
+
+(define packus-spec ((total-size natp)
+                     (chunk-size posp)
+                     (x (unsigned-byte-p total-size x))
+                     (y (unsigned-byte-p total-size y)))
+  :guard (and (equal (mod total-size chunk-size) 0)
+              (evenp chunk-size))
+  :returns (result (unsigned-byte-p total-size result)
+                   :hyp :guard
+                   :hints (("Goal"
+                            :do-not-induct t
+                            :in-theory (e/d (pack-spec-lemma4
+                                             packus-spec-one)
+                                            (unsigned-byte-p)))))
+  :parents (instruction-semantic-functions)
+  :short "Specification of packing with unsigned saturation."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "This is for the PACKUS... instructions.")
+   (xdoc::p
+    "Given @('x') and @('y'), each of size @('total-size') in bits,
+     we use @(tsee packus-spec) to pack each operand into half bits,
+     and then we join the two half results to obtain the full result.
+     See Figure 4-6 in Intel manual Volume 2 (Feb 2026);
+     here we generalize 64 to @('total-size') and 32 to @('chunk-size')."))
+  (logapp (/ total-size 2)
+          (packus-spec-one total-size chunk-size x)
+          (packus-spec-one total-size chunk-size y))
+  :guard-hints (("Goal" :in-theory (enable evenp pack-spec-lemma2))))
+
+(define packss-spec ((total-size natp)
+                     (chunk-size posp)
+                     (x (unsigned-byte-p total-size x))
+                     (y (unsigned-byte-p total-size y)))
+  :guard (and (equal (mod total-size chunk-size) 0)
+              (evenp chunk-size))
+  :returns (result (unsigned-byte-p total-size result)
+                   :hyp :guard
+                   :hints (("Goal"
+                            :do-not-induct t
+                            :in-theory (e/d (pack-spec-lemma4
+                                             packss-spec-one)
+                                            (unsigned-byte-p)))))
+  :parents (instruction-semantic-functions)
+  :short "Specification of packing with signed saturation."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "This is for the PACKSS... instructions.")
+   (xdoc::p
+    "This is similar to @(tsee packus-spec):
+     see that function's documentation first.
+     The difference is signed saturation instead of unsigned saturation."))
+  (logapp (/ total-size 2)
+          (packss-spec-one total-size chunk-size x)
+          (packss-spec-one total-size chunk-size y))
+  :guard-hints (("Goal" :in-theory (enable evenp pack-spec-lemma2))))
+
+; The function packus-spec above generalizes the function packuswb below.
+; The latter is for total-size = 128 and chunk-size = 16,
+; and uses a recursive macro.
+; We leave packuswb, and its use in x86-packuswb-sse, at least for now.
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -137,3 +352,5 @@
        ;; Update the instruction pointer.
        (x86 (write-*ip proc-mode temp-rip x86)))
       x86))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
