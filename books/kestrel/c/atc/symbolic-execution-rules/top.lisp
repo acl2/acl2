586d36d4ab83a658293e53e88a26d485e11a5a99
Alessandro Coglio <email@alessandrocoglio.info>
2026-03-07 09:41:21 -0800

    [ATC] Add constant AST accessors to big rule list.
    This is in preparation for removing them from various individual lists.

--- books/kestrel/c/atc/symbolic-execution-rules/top.lisp
+++ books/kestrel/c/atc/symbolic-execution-rules/top.lisp
@@ -1,7 +1,7 @@
 ; C Library
 ;
-; Copyright (C) 2025 Kestrel Institute (http://www.kestrel.edu)
-; Copyright (C) 2025 Kestrel Technology LLC (http://kestreltechnology.com)
+; Copyright (C) 2026 Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2026 Kestrel Technology LLC (http://kestreltechnology.com)
 ;
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 ;
@@ -13,6 +13,8 @@
 
 (include-book "../symbolic-computation-states")
 (include-book "../shallow-embedding")
+
+(include-book "../../proof-support/const-ast-accessors")
 
 (include-book "types")
 (include-book "values")
@@ -206,7 +208,8 @@
           *atc-flexible-array-member-rules*
           *atc-pointed-integer-rules*
           *atc-apconvert-rules*
-          *atc-object-designator-rules*))
+          *atc-object-designator-rules*
+          *const-ast-accessors*))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
