03b79bac0c6b1e081d403d2c4686593c8d72c4fd
Alessandro Coglio <email@alessandrocoglio.info>
2026-03-11 23:59:44 -0700

    [def-error-checker] Fix XDOC.

--- books/kestrel/error-checking/def-error-checker.lisp
+++ books/kestrel/error-checking/def-error-checker.lisp
@@ -85,7 +85,7 @@
       This return specifier is:
       @('(erp (implies erp (equal erp error-erp)))')
       if @('<mode>') is @(':logic'); and
-      @('(erp \"\@(\'nil\') or \@(\'error-erp\').\")')
+      @('(erp \"nil or error-erp.\")')
       if @('<mode>') is @(':program').")
     (xdoc::li
      "@('<val>') is a "
@@ -100,10 +100,8 @@
       @('(val (and (implies erp (equal val error-val))
                    (implies (and (not erp) error-erp) (not val))))');
       if no @('<returns>') argument is supplied and @('<mode>') is @(':program'),
-      then @('<val>') is "
-     (xdoc::code
-      "(val \"@('nil') if @('erp') is @('nil'), otherwise @('error-val').\")")
-     "; if a @('<returns>') argument is supplied,
+      then @('<val>') is @('(val \"'nil if erp is nil, otherwise error-val.\")');
+      if a @('<returns>') argument is supplied,
       then @('<val>') is @('<returns>').")
     (xdoc::li
      "@('<mode>') is either @(':logic') or @(':program').")
