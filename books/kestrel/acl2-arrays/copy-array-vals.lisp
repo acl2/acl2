c594c120574b2d24db23b0a2c0fb26b10e4ab732
Eric Smith <ewsmith@gmail.com>
2026-04-05 20:23:48 -0700

    [acl2-arrays] Implement more suggestions from AI code review.

--- books/kestrel/acl2-arrays/copy-array-vals.lisp
+++ books/kestrel/acl2-arrays/copy-array-vals.lisp
@@ -14,6 +14,7 @@
 
 (include-book "alen1")
 (local (include-book "acl2-arrays"))
+(local (include-book "aset1"))
 
 ;; Copies the values at locations INDEX down to 0 from FROM-ARRAY to the same
 ;; locations in TO-ARRAY.  Requires that the arrays be big enough for INDEX to
@@ -30,3 +31,22 @@
       to-array
     (let ((to-array (aset1 to-array-name to-array index (aref1 from-array-name from-array index))))
       (copy-array-vals (+ -1 index) from-array-name from-array to-array-name to-array))))
+
+(defthm array1p-of-copy-array-vals
+  (implies (and (rationalp index)
+                (array1p from-array-name from-array)
+                (array1p to-array-name to-array)
+                (< index (alen1 from-array-name from-array))
+                (< index (alen1 to-array-name to-array)))
+           (array1p to-array-name (copy-array-vals index from-array-name from-array to-array-name to-array)))
+  :hints (("Goal" :in-theory (enable copy-array-vals))))
+
+(defthm alen1-of-copy-array-vals
+  (implies (and (rationalp index)
+                (array1p from-array-name from-array)
+                (array1p to-array-name to-array)
+                (< index (alen1 from-array-name from-array))
+                (< index (alen1 to-array-name to-array)))
+           (equal (alen1 to-array-name (copy-array-vals index from-array-name from-array to-array-name to-array))
+                  (alen1 to-array-name to-array)))
+  :hints (("Goal" :in-theory (enable copy-array-vals))))
