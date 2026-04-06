dcb3371dca0ea6858aa389da728e6f20273b3d61
Eric Smith <ewsmith@gmail.com>
2026-04-05 20:11:25 -0700

    [acl2-arrays] Improve as suggested by AI code review.

--- books/kestrel/acl2-arrays/copy-array-vals.lisp
+++ books/kestrel/acl2-arrays/copy-array-vals.lisp
@@ -1,7 +1,7 @@
-; A library for reasoning about ACL2 arrays (aref1, aset1, etc.)
+; Copying values between arrays
 ;
 ; Copyright (C) 2008-2011 Eric Smith and Stanford University
-; Copyright (C) 2013-2024 Kestrel Institute
+; Copyright (C) 2013-2026 Kestrel Institute
 ; Copyright (C) 2016-2020 Kestrel Technology, LLC
 ;
 ; License: A 3-clause BSD license. See the file books/3BSD-mod.txt.
