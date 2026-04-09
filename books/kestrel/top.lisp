88e83be26b26edae06541f49ad48f984173112a0
Alessandro Coglio <email@alessandrocoglio.info>
2026-04-08 20:26:32 -0700

    [Remora] Include in manual.

--- books/kestrel/top.lisp
+++ books/kestrel/top.lisp
@@ -53,6 +53,7 @@
 (include-book "lists-light/top")
 (include-book "number-theory/top")
 (include-book "prime-fields/top")
+(include-book "remora/top")
 (include-book "risc-v/top")
 (include-book "simpl-imp/top")
 (include-book "soft/top")
