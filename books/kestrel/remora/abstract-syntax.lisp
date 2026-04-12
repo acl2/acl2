067ce4ac3e7bd87a7bfbd57b17b0b455192723f1
Alessandro Coglio <email@alessandrocoglio.info>
2026-04-12 11:57:03 -0700

    [Remora] Add some structural AST ops.

--- books/kestrel/remora/abstract-syntax.lisp
+++ books/kestrel/remora/abstract-syntax.lisp
@@ -12,6 +12,7 @@
 
 (include-book "abstract-syntax-trees")
 (include-book "abstract-syntax-derived-fixtypes")
+(include-book "abstract-syntax-structural-operations")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -23,11 +24,13 @@
    (xdoc::p
     "We define an abstract syntax for core typed Remora,
      consisting of algebraic data types for abstract syntax trees (ASTs),
-     accompanied by other fixtypes derived from the AST fixtypes.")
+     accompanied by some fixtypes derived from the AST fixtypes,
+     and by some structural operations on ASTs.")
    (xdoc::p
     "We may generalize this abstract syntax
      to encompass untyped and type-erased Remora,
      or we might define alternative abstract syntax for those,
      with suitable mappings."))
   :order-subtopics (abstract-syntax-trees
-                    abstract-syntax-derived-fixtypes))
+                    abstract-syntax-derived-fixtypes
+                    abstract-syntax-structural-operations))
