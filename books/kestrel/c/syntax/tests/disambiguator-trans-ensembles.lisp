b9d2cca2c9a9b84472d5f5d860083691e7384a7f
Alessandro Coglio <email@alessandrocoglio.info>
2026-04-13 16:49:40 -0700

    [C$] Add a disambiguator test.
    This shows the preservation of `#include`s with header guards.

--- books/kestrel/c/syntax/tests/disambiguator-trans-ensembles.lisp
+++ books/kestrel/c/syntax/tests/disambiguator-trans-ensembles.lisp
@@ -48,3 +48,54 @@ void f(int y) {
   x * y;
 }
 "))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(input-files :files '("guarded-included.h"
+                      "including1.c"
+                      "including2.c"
+                      "including.c")
+             :base-dir "disamb-example2"
+             :preprocess :internal
+             :process :disambiguate
+             :const *code2*)
+
+(defconst *printed2*
+  (print-fileset (code-ensemble->trans-units *code2*)
+                 (default-priopt)
+                 (ienv->dialect (code-ensemble->ienv *code2*))))
+
+(acl2::assert-equal *printed2*
+                    (fileset
+                     (list (cons (filepath "guarded-included.h")
+                                 (filedata
+                                  (acl2::string=>nats
+                                   "#ifndef GUARD
+
+#define GUARD GUARD
+
+int x;
+
+#endif
+")))
+                           (cons (filepath "including.c")
+                                 (filedata
+                                  (acl2::string=>nats
+                                   "#include \"including1.c\"
+
+#include \"including2.c\"
+")))
+                           (cons (filepath "including1.c")
+                                 (filedata
+                                  (acl2::string=>nats
+                                   "#include \"guarded-included.h\"
+
+int y = x;
+")))
+                           (cons (filepath "including2.c")
+                                 (filedata
+                                  (acl2::string=>nats
+                                   "#include \"guarded-included.h\"
+
+int z = x;
+"))))))
