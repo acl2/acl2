7013939a5b3b912510299748ee0f583b3ab6ff19
Alessandro Coglio <email@alessandrocoglio.info>
2026-04-12 18:34:15 -0700

    [Remora] Refine type equivalence.
    Now it relaxes index identity to index equivalence, so it is slightly less of a
    placeholder. We still need to flesh out the rest of type equivalence.

--- books/kestrel/remora/type-equivalence.lisp
+++ books/kestrel/remora/type-equivalence.lisp
@@ -10,7 +10,9 @@
 
 (in-package "REMORA")
 
-(include-book "abstract-syntax-trees")
+(include-book "index-equivalence")
+
+(local (include-book "kestrel/utilities/ordinals" :dir :system))
 
 (acl2::controlled-configuration)
 
@@ -35,36 +37,72 @@
      (see @(see index-equivalence).
      We plan to formalize type equivalence,
      both at a high level and as executable code.
-     For now we provide a placeholder."))
+     For now we provide something that is mostly a placeholder."))
   :order-subtopics t
   :default-parent t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define type-equivp ((type1 typep) (type2 typep))
-  :returns (yes/no booleanp)
-  :short "Check if two types are equivalent."
+(defines types-equivp
+  :short "Check if two types or two lists of types are equivalent."
   :long
   (xdoc::topstring
    (xdoc::p
-    "This is an initial placeholder, which makes a strong check:
-     the two types must be identical.
-     This needs to be relaxed with the proper semantic equivalence,
-     which is decidable for the current version of Remora,
-     according to the arXiv paper and dissertation."))
-  (equal (type-fix type1) (type-fix type2)))
+    "This is mostly a placeholder for now.
+     It makes a strong check:
+     the types must be identical,
+     except that indices just have to be equivalent,
+     as defined in @(see index-equivalence)."))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (define type-equivp ((type1 typep) (type2 typep))
+    :returns (yes/no booleanp)
+    :parents (type-equivalence types-equivp)
+    :short "Check if two types are equivalent."
+    (type-case
+     type1
+     :var (type-case type2
+                     :var (equal type1.name type2.name)
+                     :otherwise nil)
+     :base (type-case type2
+                      :base (equal type1.type type2.type)
+                      :otherwise nil)
+     :array (type-case type2
+                       :array (and (type-equivp type1.type type2.type)
+                                   (index-equivp type1.index type2.index))
+                       :otherwise nil)
+     :fun (type-case type2
+                     :fun (and (type-list-equivp type1.in type2.in)
+                               (type-equivp type1.out type2.out))
+                     :otherwise nil)
+     :forall (type-case type2
+                        :forall (and (equal type1.vars type2.vars)
+                                     (type-equivp type1.type type2.type))
+                        :otherwise nil)
+     :pi (type-case type2
+                    :pi (and (equal type1.vars type2.vars)
+                             (type-equivp type1.type type2.type))
+                    :otherwise nil)
+     :sigma (type-case type2
+                       :sigma (and (equal type1.vars type2.vars)
+                                   (type-equivp type1.type type2.type))
+                       :otherwise nil))
+    :measure (+ (type-count type1) (type-count type2)))
 
-(define type-list-equivp ((types1 type-listp) (types2 type-listp))
-  :returns (yes/no booleanp)
-  :short "Check if two lists of types are equivalent."
-  :long
-  (xdoc::topstring
-   (xdoc::p
-    "This is also an initial placeholder, in line with @(tsee type-equivp):
-     we require the two lists to be identical for now."))
-  (equal (type-list-fix types1) (type-list-fix types2)))
+  (define type-list-equivp ((types1 type-listp) (types2 type-listp))
+    :returns (yes/no booleanp)
+    :parents (type-equivalence types-equivp)
+    :short "Check if two lists of types are equivalent."
+    (or (and (endp types1)
+             (endp types2))
+        (and (consp types1)
+             (consp types2)
+             (type-equivp (car types1) (car types2))
+             (type-list-equivp (cdr types1) (cdr types2))))
+    :measure (+ (type-list-count types1) (type-list-count types2)))
+
+  ///
+
+  (fty::deffixequiv-mutual types-equivp))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
