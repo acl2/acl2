46637794d21434233ea8cdf379ccfe4bcdc90f43
Alessandro Coglio <email@alessandrocoglio.info>
2026-04-17 15:53:48 -0700

    [Remora] Refine notion of type equivalence.
    Take renamings of bound variables into account. So this is no longer a
    placeholder, but should capture the intended notion (but it needs to be proved).

--- books/kestrel/remora/type-equivalence.lisp
+++ books/kestrel/remora/type-equivalence.lisp
@@ -11,6 +11,7 @@
 (in-package "REMORA")
 
 (include-book "index-equivalence")
+(include-book "abstract-syntax-variable-operations")
 
 (local (include-book "kestrel/utilities/ordinals" :dir :system))
 (local (include-book "std/basic/inductions" :dir :system))
@@ -45,67 +46,180 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defines types-equivp
-  :short "Check if two types or two lists of types are equivalent."
+(defines types-renamep
+  :short "Check if two types or lists or types are the same modulo renaming."
   :long
   (xdoc::topstring
    (xdoc::p
-    "This is mostly a placeholder for now.
-     It makes a strong check:
-     the types must be identical,
-     except that indices just have to be equivalent,
-     as defined in @(see index-equivalence)."))
+    "There are two independent renamings:
+     one for index variables, and one for type variables.
+     These are modeled as maps from strings to strings,
+     which should be injective
+     in well-formed types according to inference rules;
+     we may explicate injectivity as guards and invariants at some point.
+     The two renaming maps contain all the variables in scope;
+     we should explicate this invariant at some point;
+     some variables may be associated to themselves (i.e. not renamed)
+     in the renaming maps."))
 
-  (define type-equivp ((type1 typep) (type2 typep))
+  (define type-renamep ((type1 typep)
+                        (type2 typep)
+                        (index-renaming string-string-mapp)
+                        (type-renaming string-string-mapp))
     :returns (yes/no booleanp)
-    :parents (type-equivalence types-equivp)
-    :short "Check if two types are equivalent."
+    :parents (type-equivalence types-renamep)
+    :short "Check if two types are the same modulo renaming."
+    :long
+    (xdoc::topstring
+     (xdoc::p
+      "The two types must be in the same category:
+       two variables, or two base types, etc.")
+     (xdoc::p
+      "In the case of two variables,
+       since the renaming contains all the variables in scope,
+       we check that the first and second variables
+       are associated in the renaming map.")
+     (xdoc::p
+      "In the case of two base types,
+       they must be identical, because they do not contain variables.")
+     (xdoc::p
+      "In the case of two array types,
+       we recursively check the equality modulo renaming of the atom types.
+       For the indices,
+       first we apply the index variable renaming to the first index,
+       and then we check equivalence with the second index.
+       Index equivalence is defined not modulo renaming,
+       so we must apply the renaming prior to checking equivalence.")
+     (xdoc::p
+      "In the case of two function types,
+       we recursively check the equality modulo renaming
+       of the input and output types.")
+     (xdoc::p
+      "In the case of two universal types,
+       we check that they have the same number of bound variables,
+       and we form a map between their bound variables,
+       with which we update the existing type variable renaming map;
+       this update may overwrite some previous associations,
+       in line with the fact that the bound variables
+       may hide outer variables.
+       We then check that the two inner types are equal
+       modulo the updated renamings.")
+     (xdoc::p
+      "In the case of two product types or two sum types,
+       we check that they have the same number of bound variables,
+       and we form a map between their bound variables,
+       with which we update the existing index variable renaming map;
+       this update may overwrite some previous associations,
+       in line with the fact that the bound variables
+       may hide outer variables.
+       We then check that the two inner types are equal
+       modulo the updated renamings."))
     (type-case
      type1
-     :var (type-case type2
-                     :var (equal type1.name type2.name)
-                     :otherwise nil)
-     :base (type-case type2
-                      :base (equal type1.type type2.type)
-                      :otherwise nil)
-     :array (type-case type2
-                       :array (and (type-equivp type1.type type2.type)
-                                   (index-equivp type1.index type2.index))
-                       :otherwise nil)
-     :fun (type-case type2
-                     :fun (and (type-list-equivp type1.in type2.in)
-                               (type-equivp type1.out type2.out))
-                     :otherwise nil)
-     :forall (type-case type2
-                        :forall (and (equal type1.vars type2.vars)
-                                     (type-equivp type1.type type2.type))
-                        :otherwise nil)
-     :pi (type-case type2
-                    :pi (and (equal type1.vars type2.vars)
-                             (type-equivp type1.type type2.type))
-                    :otherwise nil)
-     :sigma (type-case type2
-                       :sigma (and (equal type1.vars type2.vars)
-                                   (type-equivp type1.type type2.type))
-                       :otherwise nil))
+     :var (type-case
+           type2
+           :var (b* ((type-renaming (string-string-map-fix type-renaming)))
+                  (equal (omap::assoc type1.name type-renaming)
+                         (cons type1.name type2.name)))
+           :otherwise nil)
+     :base (type-case
+            type2
+            :base (equal type1.type type2.type)
+            :otherwise nil)
+     :array (type-case
+             type2
+             :array (and (type-renamep type1.type
+                                       type2.type
+                                       index-renaming
+                                       type-renaming)
+                         (b* ((renamed-index1
+                               (rename-vars-in-index type1.index
+                                                     index-renaming)))
+                           (index-equivp renamed-index1 type2.index)))
+             :otherwise nil)
+     :fun (type-case
+           type2
+           :fun (and (type-list-renamep type1.in
+                                        type2.in
+                                        index-renaming
+                                        type-renaming)
+                     (type-renamep type1.out
+                                   type2.out
+                                   index-renaming
+                                   type-renaming))
+           :otherwise nil)
+     :forall (type-case
+              type2
+              :forall (and (equal (len type1.vars) (len type2.vars))
+                           (b* ((bound-map (omap::from-lists
+                                            (kinded-var-list->var type1.vars)
+                                            (kinded-var-list->var type2.vars)))
+                                (type-renaming
+                                 (omap::update*
+                                  bound-map
+                                  (string-string-map-fix type-renaming))))
+                             (type-renamep type1.type
+                                           type2.type
+                                           index-renaming
+                                           type-renaming)))
+              :otherwise nil)
+     :pi (type-case
+          type2
+          :pi (and (equal (len type1.vars) (len type2.vars))
+                   (b* ((bound-map (omap::from-lists
+                                    (sorted-var-list->var type1.vars)
+                                    (sorted-var-list->var type2.vars)))
+                        (index-renaming
+                         (omap::update*
+                          bound-map
+                          (string-string-map-fix index-renaming))))
+                     (type-renamep type1.type
+                                   type2.type
+                                   index-renaming
+                                   type-renaming)))
+          :otherwise nil)
+     :sigma (type-case
+             type2
+             :sigma (and (equal (len type1.vars) (len type2.vars))
+                         (b* ((bound-map (omap::from-lists
+                                          (sorted-var-list->var type1.vars)
+                                          (sorted-var-list->var type2.vars)))
+                              (index-renaming
+                               (omap::update*
+                                bound-map
+                                (string-string-map-fix index-renaming))))
+                           (type-renamep type1.type
+                                         type2.type
+                                         index-renaming
+                                         type-renaming)))
+             :otherwise nil))
     :measure (+ (type-count type1) (type-count type2)))
 
-  (define type-list-equivp ((types1 type-listp) (types2 type-listp))
+  (define type-list-renamep ((types1 type-listp)
+                             (types2 type-listp)
+                             (index-renaming string-string-mapp)
+                             (type-renaming string-string-mapp))
     :returns (yes/no booleanp)
-    :parents (type-equivalence types-equivp)
-    :short "Check if two lists of types are equivalent."
+    :parents (type-equivalence types-renamep)
+    :short "Check if two lists of types are the same modulo renaming."
     (or (and (endp types1)
              (endp types2))
         (and (consp types1)
              (consp types2)
-             (type-equivp (car types1) (car types2))
-             (type-list-equivp (cdr types1) (cdr types2))))
+             (type-renamep (car types1)
+                           (car types2)
+                           index-renaming
+                           type-renaming)
+             (type-list-renamep (cdr types1)
+                                (cdr types2)
+                                index-renaming
+                                type-renaming)))
     :measure (+ (type-list-count types1) (type-list-count types2))
 
     ///
 
-    (defrule same-len-when-type-list-equivp
-      (implies (type-list-equivp types1 types2)
+    (defrule same-len-when-type-list-renamep
+      (implies (type-list-renamep types1 types2 index-renaming type-renaming)
                (equal (len types1) (len types2)))
       :rule-classes :forward-chaining
       :hints (("Goal"
@@ -114,7 +228,36 @@
 
   ///
 
-  (fty::deffixequiv-mutual types-equivp))
+  (fty::deffixequiv-mutual types-renamep))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-equivp ((type1 typep) (type2 typep))
+  :returns (yes/no booleanp)
+  :short "Check if two types are equivalent."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "This is the case when they are equal modulo no renamings."))
+  (type-renamep type1 type2 nil nil))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define type-list-equivp ((types1 type-listp) (types2 type-listp))
+  :returns (yes/no booleanp)
+  :short "Check if two lists of types are equivalent."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "This is the case when they are equal modulo no renamings."))
+  (type-list-renamep types1 types2 nil nil)
+
+  ///
+
+  (defrule same-len-when-type-list-equivp
+    (implies (type-list-equivp types1 types2)
+             (equal (len types1) (len types2)))
+    :rule-classes :forward-chaining))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
