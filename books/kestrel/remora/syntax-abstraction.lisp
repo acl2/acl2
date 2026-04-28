6d3d69a3e09d5b9629f10d6a709371c6d58909fd
Eric McCarthy <mccarthy@kestrel.edu>
2026-04-27 22:25:03 -0700

    [remora] add author comment

--- books/kestrel/remora/syntax-abstraction.lisp
+++ books/kestrel/remora/syntax-abstraction.lisp
@@ -3,6 +3,8 @@
 ; Copyright (C) 2026 Kestrel Institute (http://www.kestrel.edu)
 ;
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Eric McCarthy (bendyarm on GitHub)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
