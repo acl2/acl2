dc379cfad79c33ddc9e6880011f338ff86836e02
Alessandro Coglio <email@alessandrocoglio.info>
2026-04-30 21:13:16 -0700

    [Remora] Shorten definition of desugaring.

--- books/kestrel/remora/desugaring.lisp
+++ books/kestrel/remora/desugaring.lisp
@@ -13,6 +13,8 @@
 (include-book "abstract-syntax-core")
 (include-book "abstract-syntax-structural-operations")
 
+(include-book "kestrel/fty/deffold-map" :dir :system)
+
 (acl2::controlled-configuration)
 
 (local (in-theory (enable* abstract-syntax-corep-rules)))
@@ -31,218 +33,86 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defines shapes-desugar
-  :short "Desugar shapes and lists of shapes."
+(fty::deffold-map desugar
+  :short "Desugar ASTs."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "A shape with a list of zero or more dimensions
+     is turned into a concatenation of single-dimension shapes.")
+   (xdoc::p
+    "A shape splice is turned into a concatenation.")
+   (xdoc::p
+    "A bracket type is turned into an array type
+     whose shape is the concatenation of the shapes."))
+  :types (shapes
+          ispace
+          ispace-list
+          ispace-list-option
+          types
+          var+type
+          var+type-list
+          ;; TODO:
+          ;; exprs/atoms/binds
+          ;; prog
+         )
+  :override
+  ((shape :dims (shape-append (shape-dim-list shape.dims)))
+   (shape :splice (shape-append (shape-list-desugar shape.shapes)))
+   (type :bracket (make-type-array :elem (type-desugar type.elem)
+                                   :shape (shape-append
+                                           (shape-list-desugar type.shapes))))))
 
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-  (define shape-desugar ((shape shapep))
-    :returns (core-shape shapep)
-    :parents (abstract-syntax-desugaring shapes-desugar)
-    :short "Desugar a shape."
-    :long
-    (xdoc::topstring
-     (xdoc::p
-      "A shape with a list of zero or more dimensions
-       is turned into a concatenation of single-dimension shapes.")
-     (xdoc::p
-      "A shape splice is turned into a concatenation."))
-    (shape-case
-     shape
-     :var (shape-var shape.name)
-     :dim (shape-dim shape.dim)
-     :dims (shape-append (shape-dim-list shape.dims))
-     :append (shape-append (shape-list-desugar shape.shapes))
-     :splice (shape-append (shape-list-desugar shape.shapes)))
-    :measure (shape-count shape))
-
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-  (define shape-list-desugar ((shapes shape-listp))
-    :returns (core-shapes shape-listp)
-    :parents (abstract-syntax-desugaring shapes-desugar)
-    :short "Desugar a list of shapes."
-    (cond ((endp shapes) nil)
-          (t (cons (shape-desugar (car shapes))
-                   (shape-list-desugar (cdr shapes)))))
-    :measure (shape-list-count shapes))
-
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-  :verify-guards :after-returns
-
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-  ///
-
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-  (fty::deffixequiv-mutual shapes-desugar)
-
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-  (std::defprojection shape-list-desugar (x)
-    :parents nil
-    (shape-desugar x)
-    :already-definedp t)
-
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defsection corep-of-desugar
+  :short "Desugaring always returns core ASTs."
 
   (defret-mutual shapes-corep-of-shapes-desugar
     (defret shape-corep-of-shape-desugar
-      (shape-corep core-shape)
+      (shape-corep fty::result)
       :fn shape-desugar)
     (defret shape-list-corep-of-shape-list-desugar
-      (shape-list-corep core-shapes)
-      :fn shape-list-desugar)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define ispace-desugar ((ispace ispacep))
-  :returns (core-ispace ispacep)
-  :short "Desugar an ispace."
-  (ispace-case
-   ispace
-   :dim (ispace-dim ispace.dim)
-   :shape (ispace-shape (shape-desugar ispace.shape)))
-
-  ///
+      (shape-list-corep fty::result)
+      :fn shape-list-desugar)
+    :mutual-recursion shapes-desugar
+    :hints (("Goal" :in-theory (enable shape-desugar shape-list-desugar))))
 
   (defret ispace-corep-of-ispace-desugar
-    (ispace-corep core-ispace)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(std::defprojection ispace-list-desugar ((x ispace-listp))
-  :returns (core-ispaces ispace-listp)
-  :short "Desugar a list of ispaces."
-  (ispace-desugar x)
-
-  ///
+    (ispace-corep fty::result)
+    :fn ispace-desugar
+    :hints (("Goal" :in-theory (enable ispace-desugar))))
 
   (defret ispace-list-corep-of-ispace-list-desugar
-    (ispace-list-corep core-ispaces)
+    (ispace-list-corep fty::result)
     :fn ispace-list-desugar
-    :hints (("Goal" :induct t))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define ispace-list-option-desugar ((ispaces? ispace-list-optionp))
-  :returns (core-ispaces? ispace-list-optionp)
-  :short "Desugar an optional list of ispaces."
-  (ispace-list-option-case
-   ispaces?
-   :some (ispace-list-option-some (ispace-list-desugar ispaces?.val))
-   :none (ispace-list-option-none))
-
-  ///
+    :hints (("Goal" :induct t :in-theory (enable ispace-list-desugar))))
 
   (defret ispace-list-option-corep-of-ispace-list-option-desugar
-    (ispace-list-option-corep core-ispaces?)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defines types-desugar
-  :short "Desugar types and lists of types."
-
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-  (define type-desugar ((type typep))
-    :returns (core-type typep)
-    :parents (abstract-syntax-desugaring types-desugar)
-    :short "Desugar a type."
-    :long
-    (xdoc::topstring
-     (xdoc::p
-      "A bracket type is turned into an array type
-       whose shape is the concatenation of the shapes."))
-    (type-case
-     type
-     :var (type-var type.var)
-     :base (type-base type.type)
-     :array (make-type-array :elem (type-desugar type.elem)
-                             :shape (shape-desugar type.shape))
-     :bracket (make-type-array :elem (type-desugar type.elem)
-                               :shape (shape-append
-                                       (shape-list-desugar type.shapes)))
-     :fun (make-type-fun :in (type-list-desugar type.in)
-                         :out (type-desugar type.out))
-     :forall (make-type-forall :params type.params
-                               :body (type-desugar type.body))
-     :pi (make-type-pi :params type.params
-                       :body (type-desugar type.body))
-     :sigma (make-type-sigma :params type.params
-                             :body (type-desugar type.body)))
-    :measure (type-count type))
-
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-  (define type-list-desugar ((types type-listp))
-    :returns (core-types type-listp)
-    :parents (abstract-syntax-desugaring types-desugar)
-    :short "Desugar a list of types."
-    (cond ((endp types) nil)
-          (t (cons (type-desugar (car types))
-                   (type-list-desugar (cdr types)))))
-    :measure (type-list-count types))
-
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-  :verify-guards :after-returns
-
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-  ///
-
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-  (fty::deffixequiv-mutual types-desugar)
-
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-  (std::defprojection type-list-desugar (x)
-    :parents nil
-    (type-desugar x)
-    :already-definedp t)
-
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    (ispace-list-option-corep fty::result)
+    :fn ispace-list-option-desugar
+    :hints (("Goal" :in-theory (enable ispace-list-option-desugar))))
 
   (defret-mutual types-corep-of-types-desugar
     (defret type-corep-of-type-desugar
-      (type-corep core-type)
+      (type-corep fty::result)
       :fn type-desugar)
     (defret type-list-corep-of-type-list-desugar
-      (type-list-corep core-types)
-      :fn type-list-desugar)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define var+type-desugar ((var+type var+type-p))
-  :returns (core-var+type var+type-p)
-  :short "Desugar a variable with type."
-  (b* ((var (var+type->var var+type))
-       (type (var+type->type var+type)))
-    (make-var+type :var var :type (type-desugar type)))
-
-  ///
+      (type-list-corep fty::result)
+      :fn type-list-desugar)
+    :mutual-recursion types-desugar
+    :hints (("Goal" :in-theory (enable type-desugar type-list-desugar))))
 
   (defret var+type-corep-of-var+type-desugar
-    (var+type-corep core-var+type)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(std::defprojection var+type-list-desugar ((x var+type-listp))
-  :returns (core-vars+types var+type-listp)
-  :short "Desugar a list of variables with types."
-  (var+type-desugar x)
-
-  ///
+    (var+type-corep fty::result)
+    :fn var+type-desugar
+    :hints (("Goal" :in-theory (enable var+type-desugar))))
 
   (defret var+type-list-corep-of-var+type-list-desugar
-    (var+type-list-corep core-vars+types)
+    (var+type-list-corep fty::result)
     :fn var+type-list-desugar
-    :hints (("Goal" :induct t))))
+    :hints (("Goal" :induct t :in-theory (enable var+type-list-desugar))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; TODO: add more when more functions are defined
 
-; TODO: continue
+)
