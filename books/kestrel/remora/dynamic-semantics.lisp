00696e79111d186a8ad97410e87f78a9b2f1615d
Alessandro Coglio <email@alessandrocoglio.info>
2026-05-04 15:09:49 -0700

    [Remora] Start dynamic semantics.

---
+++ books/kestrel/remora/dynamic-semantics.lisp
@@ -0,0 +1,28 @@
+; Remora Library
+;
+; Copyright (C) 2026 Kestrel Institute (http://www.kestrel.edu)
+;
+; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
+;
+; Author: Alessandro Coglio (www.alessandrocoglio.info)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package "REMORA")
+
+(include-book "xdoc/constructors" :dir :system)
+
+(include-book "portcullis")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defxdoc dynamic-semantics
+  :parents (remora)
+  :short "Dynamic semantics of Remora."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "The static semantics of Remora is defined via inference rules,
+     in the Remora publications [thesis] [arxiv] [esop].
+     We plan to formalize those inference rules as directly as possible,
+     but we will start by providing an executable interpreter.")))
