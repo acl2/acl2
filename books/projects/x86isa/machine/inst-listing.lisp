3efdc4eaccf4e84f6c6564fb34e5d3be71cac1a2
Alessandro Coglio <email@alessandrocoglio.info>
2026-05-22 14:33:56 -0700

    [X86ISA] Add VMOVMSKPD (VEX variants).

--- books/projects/x86isa/machine/inst-listing.lisp
+++ books/projects/x86isa/machine/inst-listing.lisp
@@ -5589,13 +5589,15 @@
               :VEX '(:0F :128 :66 :WIG)
               :FEAT '(:AVX))
           (ARG :OP1 '(G Y) :OP2 '(U PD))
-          NIL '((:EX (CHK-EXC :TYPE-7 (:AVX)))))
+          '(X86-VMOVMSKPD-VEX)
+          '((:EX (CHK-EXC :TYPE-7 (:AVX)))))
     (INST "VMOVMSKPD"
           (OP :OP #xF50
               :VEX '(:0F :256 :66 :WIG)
               :FEAT '(:AVX))
           (ARG :OP1 '(G Y) :OP2 '(U PD))
-          NIL '((:EX (CHK-EXC :TYPE-7 (:AVX)))))
+          '(X86-VMOVMSKPD-VEX)
+          '((:EX (CHK-EXC :TYPE-7 (:AVX)))))
     (INST "VMOVMSKPS"
           (OP :OP #xF50
               :VEX '(:0F :128 :WIG)
