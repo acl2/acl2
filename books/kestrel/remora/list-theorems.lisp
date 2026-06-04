4b34ecdabbb9153f93b04cde6e96b89c00c20a57
Alessandro Coglio <email@alessandrocoglio.info>
2026-06-04 09:22:53 -0700

    [Remora] Remove theorem now in library.

--- books/kestrel/remora/list-theorems.lisp
+++ books/kestrel/remora/list-theorems.lisp
@@ -10,29 +10,25 @@
 
 (in-package "REMORA")
 
-(include-book "kestrel/typed-lists-light/nat-list-listp" :dir :system)
-
 (include-book "std/lists/repeat" :dir :system)
 (include-book "std/util/defrule" :dir :system)
-(include-book "xdoc/top" :dir :system)
+(include-book "xdoc/defxdoc-plus" :dir :system)
 
 (include-book "std/basic/controlled-configuration" :dir :system)
 (acl2::controlled-configuration)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defsection list-theorems
+(defxdoc+ list-theorems
   :parents (library-extensions)
   :short "Theorems about lists."
+  :order-subtopics t
+  :default-parent t)
 
-  (defruled car-of-repeat
-    (equal (car (repeat n x))
-           (if (zp n) nil x))
-    :induct t
-    :enable repeat)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-  (defrule nat-list-listp-of-repeat
-    (equal (nat-list-listp (repeat n x))
-           (or (zp n) (nat-listp x)))
-    :induct t
-    :enable repeat))
+(defruled car-of-repeat
+  (equal (car (repeat n x))
+         (if (zp n) nil x))
+  :induct t
+  :enable repeat)
