f8cef02db10b31d914b5a63f4f8fd7a8373b5070
Alessandro Coglio <email@alessandrocoglio.info>
2026-06-04 13:11:13 -0700

    [Remora] Add some list operations.

--- books/kestrel/remora/lists.lisp
+++ books/kestrel/remora/lists.lisp
@@ -12,13 +12,14 @@
 
 (include-book "std/lists/repeat" :dir :system)
 (include-book "std/util/define" :dir :system)
+(include-book "std/util/deflist" :dir :system)
 (include-book "std/util/defrule" :dir :system)
 (include-book "xdoc/defxdoc-plus" :dir :system)
 
 (local (include-book "kestrel/utilities/true-list-listp-theorems" :dir :system))
 
 (include-book "std/basic/controlled-configuration" :dir :system)
-(acl2::controlled-configuration)
+(acl2::controlled-configuration :hooks nil)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -36,8 +37,38 @@
   (cond ((endp lists) nil)
         (t (append (mbe :logic (true-list-fix (car lists))
                         :exec (car lists))
-                   (append-all (cdr lists)))))
-  :hooks nil)
+                   (append-all (cdr lists))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define list-repeatp ((x true-listp))
+  :returns (yes/no booleanp)
+  :short "Check if all the elements of a list are the same."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "That is, check whether it is a repetition of the same element,
+     zero or more times."))
+  (or (endp x)
+      (endp (cdr x))
+      (and (equal (car x) (cadr x))
+           (list-repeatp (cdr x)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(std::deflist list-list-repeatp (x)
+  :guard (true-list-listp x)
+  :short "Lift @(tsee list-repeatp) to lists."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "That is check whether each list in the list of lists
+     is a repetition of the same element.
+     But different lists in the list of lists may differ,
+     i.e. they may be repetitions of different elements.
+     This is different from saying that
+     all the element of the lists in the list of lists are the same."))
+  (list-repeatp x))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
