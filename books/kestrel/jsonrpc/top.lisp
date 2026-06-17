79798991018c5ad82ce94dfac2af1831f4d94f38
ltmquan <ltmquan2001@gmail.com>
2026-06-17 17:26:28 -0400

    [JSONRPC] fixed doc issues

--- books/kestrel/jsonrpc/top.lisp
+++ books/kestrel/jsonrpc/top.lisp
@@ -114,39 +114,7 @@
   @(see make-internal-error). These produce @(see error) values with the
   appropriate JSON-RPC error codes.</p>
 
-  <p>Here is an example method function that subtracts two numbers:</p>
-
-  @({
-    (define subtract ((params structuredp) state)
-      :returns (mv erp (res valuep) state)
-      :stobjs state
-      (b* (((mv x y)
-        (if (equal (structured-kind params) :array)
-            (b* ((elems (structured-array->elements params)))
-              (if (= (len elems) 2)
-                  (mv (first elems) (second elems))
-                (mv nil nil)))
-          (b* ((members (structured-object->members params))
-               (x (find-member-value \"minuend\" members))
-               (y (find-member-value \"subtrahend\" members)))
-            (mv x y))))
-       ((unless (and x y))
-        (mv (make-invalid-params-error
-             \"Params must be [minuend, subtrahend] or
-             {\"minuend\":...,\"subtrahend\":...}\")
-            (value-null)
-            state))
-       ((unless (equal (value-kind x) :number))
-        (mv (make-invalid-params-error \"minuend must be a number\")
-            (value-null)
-            state))
-       ((unless (equal (value-kind y) :number))
-        (mv (make-invalid-params-error \"subtrahend must be a number\")
-            (value-null)
-            state))
-       (result (- (value-number->get x) (value-number->get y))))
-    (mv nil (value-number result) state)))
-  })
+  <p>For a worked example, see @('books/kestrel/jsonrpc/subtract-example.lisp').</p>
 
   <h3>Batch Requests</h3>
 
