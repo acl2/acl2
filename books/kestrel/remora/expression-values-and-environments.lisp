93cfd10b079ae309fd9d2f445104018dc8084f42
Alessandro Coglio <email@alessandrocoglio.info>
2026-07-08 17:25:16 -0700

    [Remora] Factor base values in new file/topic.

--- books/kestrel/remora/expression-values-and-environments.lisp
+++ books/kestrel/remora/expression-values-and-environments.lisp
@@ -13,6 +13,7 @@
 (include-book "ispace-values-and-environments")
 (include-book "type-values-and-environments")
 (include-book "abstract-syntax-structurals")
+(include-book "base-values")
 
 (include-book "kestrel/fty/nat-list-list-list" :dir :system)
 (include-book "kestrel/fty/nat-list-result" :dir :system)
@@ -23,7 +24,6 @@
 
 (local (include-book "std/basic/ifix" :dir :system))
 (local (include-book "std/basic/nfix" :dir :system))
-(local (include-book "std/basic/rfix" :dir :system))
 (local (include-book "std/lists/len" :dir :system))
 (local (include-book "std/lists/nthcdr" :dir :system))
 (local (include-book "std/typed-lists/string-listp" :dir :system))
@@ -47,114 +47,9 @@
      Expression dynamic environments capture
      the expression variables in scope
      and their associated expression values."))
-  :order-subtopics t
+  :order-subtopics (base-values
+                    t)
   :default-parent t)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(fty::defprod int-value
-  :short "Fixtype of integer values."
-  :long
-  (xdoc::topstring
-   (xdoc::p
-    "[thesis] does not pin down the details of integer values,
-     leaving them abstract.
-     [impl] uses Haskell's @('Int'),
-     which consists of fixed-precision integers with at least 30 bits.")
-   (xdoc::p
-    "We do not yet know the definite intention for integer in Remora,
-     e.g. whether it should prescribe one portable integer format,
-     or allow a range of formats,
-     or even allow multiple integer types.
-     For now, we use ACL2 integers."))
-  ((int int))
-  :pred int-valuep)
-
-;;;;;;;;;;;;;;;;;;;;
-
-(fty::deflist int-value-list
-  :short "Fixtype of lists of integer values."
-  :elt-type int-value
-  :true-listp t
-  :elementp-of-nil nil
-  :pred int-value-listp)
-
-;;;;;;;;;;;;;;;;;;;;
-
-(fty::defresult int-value-result
-  :short "Fixtype of (i) integer values and (ii) errors."
-  :ok int-value
-  :pred int-value-resultp)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(fty::deftagsum float-value
-  :short "Fixtype of float values."
-  :long
-  (xdoc::topstring
-   (xdoc::p
-    "[thesis] does not pin down the details of float values,
-     leaving them abstract.
-     [impl] uses Haskell's @('Float'),
-     which consists of single-precision floating-point numbers,
-     ``desired'' (according to the Haskell documentation)
-     to comply with the IEEE standard.")
-   (xdoc::p
-    "We do not yet know the definite intention for floats in Remora,
-     e.g. whether it should prescribe one portable float format,
-     or allow a range of formats,
-     or even allow multiple float types.
-     For now, we use ACL2 rationals,
-     with the addition of negative zero (rational 0 being the positive one),
-     negative and positive infinities, and not-a-number.
-     This is really a placeholder for
-     a more realistic representation of float values,
-     e.g. in terms of IEEE floatings from @('[books]/kestrel/floats'),
-     which we plan to use once the Remora design is clarified."))
-  (:ratio ((ratio rational)))
-  ;; no :pos0, which is just :ratio 0
-  (:neg0 ())
-  (:posinf ())
-  (:neginf ())
-  (:nan ())
-  :pred float-valuep)
-
-;;;;;;;;;;;;;;;;;;;;
-
-(fty::defresult float-value-result
-  :short "Fixtype of (i) float values and (ii) errors."
-  :ok float-value
-  :pred float-value-resultp
-  :prepwork ((local (in-theory (enable float-value-kind)))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(fty::deftagsum base-value
-  :short "Fixtype of base values."
-  :long
-  (xdoc::topstring
-   (xdoc::p
-    "There are booleans, integers, and floats."))
-  (:bool ((val bool)))
-  (:int ((val int-value)))
-  (:float ((val float-value)))
-  :pred base-valuep)
-
-;;;;;;;;;;;;;;;;;;;;
-
-(fty::deflist base-value-list
-  :short "Fixtype of lists of base values."
-  :elt-type base-value
-  :true-listp t
-  :elementp-of-nil nil
-  :pred base-value-listp)
-
-;;;;;;;;;;
-
-(std::defprojection base-value-int-list ((x int-value-listp))
-  :returns (vals base-value-listp)
-  :short "Lift @(tsee base-value-int) to lists."
-  (base-value-int x))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
