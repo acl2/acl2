2454592b8291a224c629eb2a6355686bf825d03b
Aakash Koneru <aakash@mac.lan>
2026-07-14 11:24:37 -0700

    add headers to all files

--- books/kestrel/filepath/paths.lisp
+++ books/kestrel/filepath/paths.lisp
@@ -1,7 +1,6 @@
 ; Filepath library
 ;
 ; Copyright (C) 2026 Kestrel Institute (http://www.kestrel.edu)
-; Copyright (C) 2026 Kestrel Technology LLC (http://kestreltechnology.com)
 ;
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 ;
