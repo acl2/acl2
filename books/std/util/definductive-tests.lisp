db49e0060df0d82da178fd68814fdb0204272de2
Grant Jurgensen <grant@jurgensen.dev>
2026-07-24 12:20:59 -0500

    [definductive] Incorporate AC's feedback
    Added authorship to headers and move auxillary function short to main
    documentation.

--- books/std/util/definductive-tests.lisp
+++ books/std/util/definductive-tests.lisp
@@ -5,6 +5,7 @@
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 ;
 ; Author: Alessandro Coglio (www.alessandrocoglio.info)
+; Author: Grant Jurgensen (grant@kestrel.edu)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
