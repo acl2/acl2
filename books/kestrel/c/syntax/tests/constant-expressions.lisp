d966468c2f6a1baac720b3222cfb633509742537
Grant Jurgensen <grant@jurgensen.dev>
2026-07-31 14:54:06 -0500

    [C$] Parse ICE test expressions from strings

--- books/kestrel/c/syntax/tests/constant-expressions.lisp
+++ books/kestrel/c/syntax/tests/constant-expressions.lisp
@@ -11,8 +11,11 @@
 (in-package "C$")
 
 (include-book "../constant-expressions")
+(include-book "../disambiguator")
+(include-book "../parser")
+(include-book "../validator")
 
-(include-book "std/testing/assert-equal" :dir :system)
+(include-book "std/strings/cat" :dir :system)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -22,261 +25,111 @@
 (defconst *constant-expressions-test-gcc-c17-dialect*
   (c::make-dialect :std (c::standard-c17) :gcc t))
 
-(defconst *constant-expressions-test-one*
-  (make-expr-const
-   :const
-   (const-int
-    (make-iconst
-     :core (dec/oct/hex-const-dec 1)
-     :suffix? nil
-     :info (make-iconst-vinfo :type (type-sint)
-                              :value 1)))
-   :info (make-type-vinfo :type (type-sint))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defconst *constant-expressions-test-variable*
-  (make-expr-ident
-   :ident (ident "x")
-   :info (make-var-vinfo :type (type-sint)
-                         :linkage (linkage-none)
-                         :uid (uid 0))))
-
-(defconst *constant-expressions-test-sint-tyname*
-  (make-tyname
-   :specquals (list (spec/qual-typespec (type-spec-int)))
-   :declor? nil
-   :info (make-type-vinfo :type (type-sint))))
-
-(defconst *constant-expressions-test-double-tyname*
-  (make-tyname
-   :specquals (list (spec/qual-typespec (type-spec-double)))
-   :declor? nil
-   :info (make-type-vinfo :type (type-double))))
-
-(defconst *constant-expressions-test-cast-to-double*
-  (expr-cast *constant-expressions-test-double-tyname*
-             *constant-expressions-test-one*))
-
-(defconst *constant-expressions-test-cast-through-double*
-  (expr-cast
-   *constant-expressions-test-sint-tyname*
-   *constant-expressions-test-cast-to-double*))
-
-(defconst *constant-expressions-test-typeof-variable-tyname*
-  (make-tyname
-   :specquals
-   (list
-    (spec/qual-typespec
-     (make-type-spec-typeof-expr
-      :expr *constant-expressions-test-variable*
-      :uscores (keyword-uscores-none))))
-   :declor? nil
-   :info (make-type-vinfo :type (type-sint))))
-
-(define constant-expressions-test-pointer-to-array-tyname
-  ((size exprp))
-  :returns (tyname tynamep)
-  (make-tyname
-   :specquals (list (spec/qual-typespec (type-spec-int)))
-   :declor?
-   (make-absdeclor
-    :pointers nil
-    :direct?
-    (make-dirabsdeclor-array
-     :declor?
-     (dirabsdeclor-paren
-      (make-absdeclor :pointers (list nil)
-                      :direct? nil))
-     :qualspecs nil
-     :size? size))
-   :info
-   (make-type-vinfo
-    :type
-    (make-type-pointer
-     :to (make-type-array :of (type-sint)
-                          :size nil)))))
+;; Wrap the input expression with a declaration of x and a function definition.
+;; This gives x a meaning and provides a valid context in which to disambiguate
+;; and validate the expression, adding the annotations needed by the ICE check.
+(defmacro test-ice (input expected &key dialect)
+  `(assert-event
+    (b* ((dialect (or ,dialect
+                       *constant-expressions-test-c17-dialect*))
+         (ienv (change-ienv (ienv-default) :dialect dialect))
+         (filepath (filepath "test"))
+         (source (str::cat "int x;
+void f(void) {
+  " ,input ";
+}
+"))
+         (fileset
+          (fileset
+           (omap::update filepath
+                         (filedata (acl2::string=>nats source))
+                         nil)))
+         ((mv erp1 ast) (parse-fileset fileset dialect t nil))
+         ((when erp1) (cw "~%PARSER ERROR: ~@0~%" erp1))
+         ((mv erp2 ast) (dimb-trans-ensemble ast ienv nil))
+         ((when erp2) (cw "~%DISAMBIGUATOR ERROR: ~@0~%" erp2))
+         ((mv erp3 ast) (valid-trans-ensemble ast ienv nil))
+         ((when erp3) (cw "~%VALIDATOR ERROR: ~@0~%" erp3))
+         (tunit (omap::head-val (trans-ensemble->units ast)))
+         (item (second (trans-unit->items tunit)))
+         (edecl (trans-item-declon->declon item))
+         (fundef (ext-declon-fundef->fundef edecl))
+         (block-item (first (comp-stmt->items (fundef->body fundef))))
+         (stmt (block-item-stmt->stmt block-item))
+         (expr (stmt-expr->expr? stmt)))
+      (equal (expr-ice-p expr t dialect) ,expected))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Basic operands.
 
-(acl2::assert-equal
- (expr-ice-p *constant-expressions-test-one*
-             t
-             *constant-expressions-test-c17-dialect*)
- t)
+(test-ice "1" t)
 
-(acl2::assert-equal
- (expr-ice-p *constant-expressions-test-variable*
-             t
-             *constant-expressions-test-c17-dialect*)
- nil)
+(test-ice "x" nil)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Cast restrictions.
 
-(acl2::assert-equal
- (expr-ice-p *constant-expressions-test-cast-to-double*
-             t
-             *constant-expressions-test-c17-dialect*)
- nil)
+(test-ice "(double) 1" nil)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Expression operands of sizeof.
 
-(acl2::assert-equal
- (expr-ice-p
-  (make-expr-unary
-   :op (unop-sizeof)
-   :arg *constant-expressions-test-variable*
-   :info (make-type-vinfo :type (type-ulong)))
-  t
-  *constant-expressions-test-c17-dialect*)
- nil)
+(test-ice "sizeof(x)" nil)
 
-(acl2::assert-equal
- (expr-ice-p
-  (make-expr-unary
-   :op (unop-sizeof)
-   :arg *constant-expressions-test-cast-to-double*
-   :info (make-type-vinfo :type (type-ulong)))
-  t
- *constant-expressions-test-c17-dialect*)
- t)
+(test-ice "sizeof((double) 1)" t)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Target type names of casts.
 
-(acl2::assert-equal
- (expr-ice-p
-  (make-expr-unary
-   :op (unop-sizeof)
-   :arg
-   (expr-cast
-    (constant-expressions-test-pointer-to-array-tyname
-     *constant-expressions-test-variable*)
-    *constant-expressions-test-one*)
-   :info (make-type-vinfo :type (type-ulong)))
-  t
-  *constant-expressions-test-c17-dialect*)
- nil)
+(test-ice "sizeof((int (*)[x]) 1)" nil)
 
-(acl2::assert-equal
- (expr-ice-p
-  (make-expr-unary
-   :op (unop-sizeof)
-   :arg
-   (expr-cast
-    (constant-expressions-test-pointer-to-array-tyname
-     *constant-expressions-test-cast-through-double*)
-    *constant-expressions-test-one*)
-   :info (make-type-vinfo :type (type-ulong)))
-  t
-  *constant-expressions-test-c17-dialect*)
- t)
+(test-ice "sizeof((int (*)[(int) (double) 1]) 1)" t)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Type-name operands of sizeof.
 
-(acl2::assert-equal
- (expr-ice-p
-  (expr-sizeof
-   (constant-expressions-test-pointer-to-array-tyname
-    *constant-expressions-test-variable*))
-  t
-  *constant-expressions-test-c17-dialect*)
- nil)
+(test-ice "sizeof(int (*)[x])" nil)
 
 ;; The casts are exempted, but the bound may be evaluated, and this checker
 ;; does not establish the representability of the conversions.
-(acl2::assert-equal
- (expr-ice-p
-  (expr-sizeof
-   (constant-expressions-test-pointer-to-array-tyname
-    *constant-expressions-test-cast-through-double*))
-  t
-  *constant-expressions-test-c17-dialect*)
- :unknown)
+(test-ice "sizeof(int (*)[(int) (double) 1])" :unknown)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Standard type-name operands of alignof.
 
-(acl2::assert-equal
- (expr-ice-p
-  (make-expr-alignof
-   :type
-   (constant-expressions-test-pointer-to-array-tyname
-    *constant-expressions-test-variable*)
-   :uscores (keyword-uscores-none))
-  t
-  *constant-expressions-test-c17-dialect*)
- nil)
+(test-ice "_Alignof(int (*)[x])" nil)
 
 ;; Unlike the sizeof case above, the alignof operand is definitely unevaluated.
-(acl2::assert-equal
- (expr-ice-p
-  (make-expr-alignof
-   :type
-   (constant-expressions-test-pointer-to-array-tyname
-    *constant-expressions-test-cast-through-double*)
-   :uscores (keyword-uscores-none))
-  t
-  *constant-expressions-test-c17-dialect*)
- t)
+(test-ice "_Alignof(int (*)[(int) (double) 1])" t)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; GCC expression operands of alignof.
 
-(acl2::assert-equal
- (expr-ice-p
-  (make-expr-unary
-   :op (unop-alignof (keyword-uscores-both))
-   :arg *constant-expressions-test-variable*
-   :info (make-type-vinfo :type (type-ulong)))
-  t
-  *constant-expressions-test-gcc-c17-dialect*)
- nil)
+(test-ice "__alignof__(x)"
+          nil
+          :dialect *constant-expressions-test-gcc-c17-dialect*)
 
-(acl2::assert-equal
- (expr-ice-p
-  (make-expr-unary
-   :op (unop-alignof (keyword-uscores-both))
-   :arg *constant-expressions-test-one*
-   :info (make-type-vinfo :type (type-ulong)))
-  t
-  *constant-expressions-test-gcc-c17-dialect*)
- :unknown)
+(test-ice "__alignof__(1)"
+          :unknown
+          :dialect *constant-expressions-test-gcc-c17-dialect*)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Operands of typeof.
 
-(acl2::assert-equal
- (expr-ice-p
-  (expr-sizeof *constant-expressions-test-typeof-variable-tyname*)
-  t
-  *constant-expressions-test-gcc-c17-dialect*)
- nil)
+(test-ice "sizeof(typeof(x))"
+          nil
+          :dialect *constant-expressions-test-gcc-c17-dialect*)
 
-(acl2::assert-equal
- (expr-ice-p
-  (expr-sizeof
-   (make-tyname
-    :specquals
-    (list
-     (spec/qual-typespec
-      (make-type-spec-typeof-type
-       :type
-       (constant-expressions-test-pointer-to-array-tyname
-        *constant-expressions-test-variable*)
-       :uscores (keyword-uscores-none))))
-    :declor? nil
-    :info (make-type-vinfo :type (type-sint))))
-  t
-  *constant-expressions-test-gcc-c17-dialect*)
- nil)
+(test-ice "sizeof(typeof(int (*)[x]))"
+          nil
+          :dialect *constant-expressions-test-gcc-c17-dialect*)
