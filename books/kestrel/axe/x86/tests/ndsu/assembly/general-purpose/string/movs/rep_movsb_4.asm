c2e413445d6c8576c9d0d78c17a21ba63fa696ac
Yusuf Moshood <yusuf.moshood@ndus.edu>
2026-08-06 08:28:36 -0500

    Remove rep_movs test files for now

--- books/kestrel/axe/x86/tests/ndsu/assembly/general-purpose/string/movs/rep_movsb_4.asm
+++
@@ -1,9 +0,0 @@
-        ;; REP MOVSB with RCX=4: copy 4 bytes from [RSI] to [RDI]
-        ;; CLD clears DF so the copy direction is forward (DF=0).
-        global _start
-        section .text
-_start:
-            cld
-            mov rcx, 4        ; concrete count
-            rep movsb         ; copy 4 bytes
-            ret
