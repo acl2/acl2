0149418f945b2993a8b2700296a69b6d1b38f11b
Alessandro Coglio <email@alessandrocoglio.info>
2026-08-25 22:38:35 -0700

    [Remora] Add more type equivalence rules.

--- books/kestrel/remora/type-equivalence.lisp
+++ books/kestrel/remora/type-equivalence.lisp
@@ -130,7 +130,20 @@
      whose body is the n-ary product type of the remaining parameters
      (rule @('pi3m')).
      The two rules are separate because
-     an n-ary product type cannot have just one parameter."))
+     an n-ary product type cannot have just one parameter.")
+   (xdoc::p
+    "An n-ary sum type, which has two or more parameters,
+     is sugar for a nesting of unary sum types:
+     one with exactly two parameters stands for
+     a unary sum type of the first parameter
+     whose body is the unary sum type of the second parameter
+     (rule @('sigma2')),
+     and one with three or more parameters stands for
+     a unary sum type of the first parameter
+     whose body is the n-ary sum type of the remaining parameters
+     (rule @('sigma3m')).
+     The two rules are separate because
+     an n-ary sum type cannot have just one parameter."))
 
   :preds ((type= type1 type2))
 
@@ -288,7 +301,16 @@
          (type= (type-pin (list* p1 p2 ps) ty)
                 (type-pi p1 (type-pin (cons p2 ps) ty))))
 
-   ;; TODO: more normalization rules
+   ;; normalization of n-ary sum types:
+
+   (sigma2 ((ispace-varp p1) (ispace-varp p2) (typep ty))
+           (type= (type-sigman (list p1 p2) ty)
+                  (type-sigma p1 (type-sigma p2 ty))))
+
+   (sigma3m ((ispace-varp p1) (ispace-varp p2) (ispace-var-listp ps) (consp ps)
+             (typep ty))
+            (type= (type-sigman (list* p1 p2 ps) ty)
+                   (type-sigma p1 (type-sigman (cons p2 ps) ty))))
 
   ))
 
@@ -338,4 +360,6 @@
   (verify-guards type=-forall2-validp)
   (verify-guards type=-forall3m-validp)
   (verify-guards type=-pi2-validp)
-  (verify-guards type=-pi3m-validp))
+  (verify-guards type=-pi3m-validp)
+  (verify-guards type=-sigma2-validp)
+  (verify-guards type=-sigma3m-validp))
