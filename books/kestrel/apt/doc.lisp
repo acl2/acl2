ba92b2b0d1c53a5183e2841c44ddd97cf6f41b7f
Alessandro Coglio <email@alessandrocoglio.info>
2026-08-28 15:09:20 -0700

    [APT] Include some code in manual.

--- books/kestrel/apt/doc.lisp
+++ books/kestrel/apt/doc.lisp
@@ -58,6 +58,7 @@
 (include-book "solve")
 (include-book "solve-doc")
 (include-book "solve-method-acl2-rewriter")
+(include-book "solve-method-axe-rewriter")
 
 (include-book "tailrec")
 (include-book "tailrec-doc")
