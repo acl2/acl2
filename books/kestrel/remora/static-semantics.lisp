3fd0216ca7e03d9514a52ed76778145b50274477
Alessandro Coglio <email@alessandrocoglio.info>
2026-09-01 21:36:19 -0700

    [Remora] Fix typo in doc.

--- books/kestrel/remora/static-semantics.lisp
+++ books/kestrel/remora/static-semantics.lisp
@@ -30,7 +30,7 @@
    (xdoc::p
     "The static semantics of Remora is defined via inference rules,
      in the Remora publications [thesis] [arxiv] [esop].
-     While we are woking on formalizing those inference rules,
+     While we are working on formalizing those inference rules,
      we also provide an executable type checker,
      that is meant to be equivalent to those inference rules;
      we plan to prove this equivalence."))
