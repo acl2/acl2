6e2fa56262088b138e658331423a39c2338e737b
Alessandro Coglio <email@alessandrocoglio.info>
2026-09-04 16:23:46 -0700

    [Remora] Restructuring for consistency.

--- books/kestrel/remora/type-equivalence-derived-rules.lisp
+++ books/kestrel/remora/type-equivalence-derived-rules.lisp
@@ -25,7 +25,11 @@
     "This is analogous to @(see ispace-equivalence-derived-rules):
      each derived rule comes with a proof tree constructor,
      defined in terms of the constructors of the defining rules,
-     as well as a @('make-...') macro with keyword arguments."))
+     as well as a @('make-...') macro with keyword arguments;
+     the derived rule is proved as a theorem
+     from the validity theorem of the proof tree constructor,
+     via the soundness theorem @(tsee type-eq-when-proof-validp)
+     and the witness function @('type-eq-proof')."))
   :order-subtopics t
   :default-parent t)
 
@@ -37,13 +41,6 @@
   (xdoc::topstring
    (xdoc::p
     "This is analogous to @(tsee dim-eq-trans-swapped)."))
-
-  (defruled type-eq-trans-swapped
-    (implies (and (type-eq type2 type3)
-                  (type-eq type1 type2))
-             (type-eq type1 type3))
-    :use type-eq-trans
-    :enable typep-when-type-eq)
 
   (define type-eq-proof-trans-swapped (type1
                                        type2
@@ -71,6 +68,20 @@
                          type1 type3))
                :in-theory (enable type-eq-trans-validp
                                   typep-when-type-eq-proof-validp)))))
+
+  (defruled type-eq-trans-swapped
+    (implies (and (type-eq type2 type3)
+                  (type-eq type1 type2))
+             (type-eq type1 type3))
+    :use ((:instance type-eq (type1 type2) (type2 type3))
+          (:instance type-eq (type1 type1) (type2 type2))
+          (:instance type-eq-when-proof-validp
+                     (proof (type-eq-proof-trans-swapped
+                             type1 type2 type3
+                             (type-eq-proof type2 type3)
+                             (type-eq-proof type1 type2)))
+                     (concl.type1 type1)
+                     (concl.type2 type3))))
 
   (defmacro make-type-eq-proof-trans-swapped (&key type1
                                                    type2
