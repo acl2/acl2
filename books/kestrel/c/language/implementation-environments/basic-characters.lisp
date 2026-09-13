90226428dcfafccf66abec950260e3ed77dcf692
Alessandro Coglio <email@alessandrocoglio.info>
2026-09-12 22:38:22 -0700

    [C] Add two theorems.

--- books/kestrel/c/language/implementation-environments/basic-characters.lisp
+++ books/kestrel/c/language/implementation-environments/basic-characters.lisp
@@ -11,6 +11,7 @@
 
 (in-package "C")
 
+(include-book "ascii-characters")
 (include-book "dialects")
 
 (include-book "kestrel/fty/character-set" :dir :system)
@@ -68,6 +69,10 @@
 
   ///
 
+  (defruled ascii-basic-source-chars-subset-ascii-chars
+    (set::subset (ascii-basic-source-chars std) (ascii-chars))
+    :enable set::subset)
+
   (defruled digits-in-ascii-basic-source-chars
     (set::subset '(#\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9)
                  (ascii-basic-source-chars std))))
@@ -94,6 +99,12 @@
                 (set::mergesort extra-chars)))
 
   ///
+
+  (defruled ascii-basic-exec-chars-subset-ascii-chars
+    (set::subset (ascii-basic-exec-chars std) (ascii-chars))
+    :enable (ascii-basic-source-chars-subset-ascii-chars
+             set::in
+             set::expensive-rules))
 
   (defrule ascii-basic-source-chars-subset-ascii-basic-exec-chars
     (set::subset (ascii-basic-source-chars std)
