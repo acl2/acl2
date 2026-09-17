8a6641a1a5c11fe7bf22ab66670cfd688f2d0295
Alessandro Coglio <email@alessandrocoglio.info>
2026-09-17 14:52:37 -0700

    [Remora] Add inf rule for type application.

--- books/kestrel/remora/expression-validity.lisp
+++ books/kestrel/remora/expression-validity.lisp
@@ -13,6 +13,7 @@
 (include-book "type-validity")
 (include-book "type-equivalence")
 (include-book "shape-ordering")
+(include-book "variable-substitution-operations")
 
 (include-book "nat-lists")
 
@@ -266,9 +267,42 @@
 
    ;; TODO: eappn
 
-   ;; TODO: tapp
-
-   ;; TODO: tappn
+   (tapp ((ispace-var-setp ivars)
+          (type-var-setp tvars)
+          (string-type-mapp evars)
+          (exprp fun)
+          (type-varp param)
+          (typep type)
+          (typep type-body)
+          (shapep shape-body)
+          (shapep shape-fun)
+          (expr-ok ivars tvars evars
+                   fun
+                   (type-array (type-forall param
+                                            (type-array type-body
+                                                        (ispace-shape
+                                                         shape-body)))
+                               (ispace-shape shape-fun)))
+          (type-ok ivars tvars type)
+          (implies (type-var-case param :atom)
+                   (type-atom-kindp type))
+          (type-var-case
+           param
+           :atom
+           (and (equal atom-subst
+                       (omap::update (type-var-atom->name param) type nil))
+                (equal array-subst nil))
+           :array
+           (and (equal atom-subst nil)
+                (equal array-subst
+                       (omap::update (type-var-array->name param) type nil))))
+          (type-subst-type-vars-no-capture-p type-body atom-subst array-subst))
+         (expr-ok ivars tvars evars
+                  (expr-tapp fun type)
+                  (type-array (type-subst-type-vars type-body
+                                                    atom-subst
+                                                    array-subst)
+                              (ispace-shape (shp++ shape-fun shape-body)))))
 
    ;; TODO: iapp
 
@@ -376,6 +410,7 @@
   (verify-guards expr-ok-frame-empty-validp)
   (verify-guards expr-ok-string-validp)
   (verify-guards expr-ok-eapp-validp)
+  (verify-guards expr-ok-tapp-validp)
   (verify-guards atom-ok-bool-validp)
   (verify-guards atom-ok-int-validp)
   (verify-guards atom-ok-float-validp)
