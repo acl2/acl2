052f8ee252d191c0a98745aee623da78a79c195e
Alessandro Coglio <email@alessandrocoglio.info>
2026-09-16 22:55:34 -0700

    [Remora] Tweak ordering.

--- books/kestrel/remora/static-semantics.lisp
+++ books/kestrel/remora/static-semantics.lisp
@@ -12,8 +12,8 @@
 
 (include-book "static-environments")
 (include-book "ispace-equivalence-checker")
-(include-book "ispace-matcher")
 (include-book "type-equivalence-checker")
+(include-book "ispace-matcher")
 (include-book "type-matcher")
 (include-book "type-checker")
 (include-book "ispace-validity")
@@ -42,8 +42,8 @@
      we plan to prove this equivalence."))
   :order-subtopics (static-environments
                     ispace-equivalence-checker
-                    ispace-matcher
                     type-equivalence-checker
+                    ispace-matcher
                     type-matcher
                     type-checker
                     ispace-validity
