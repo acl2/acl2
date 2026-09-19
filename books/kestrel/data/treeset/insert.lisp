c26bb132ff593849a68b2c2690a4a93a28a1b28a
Grant Jurgensen <grant@jurgensen.dev>
2026-09-19 11:14:54 -0500

    [data] Add linear cardinality rules for treeset insert and delete
    Add the strict rules cardinality-of-insert-when-not-in-linear and cardinality-of-delete-when-in-linear, and the unconditional lower and upper bounds for both operations.

--- books/kestrel/data/treeset/insert.lisp
+++ books/kestrel/data/treeset/insert.lisp
@@ -245,6 +245,24 @@
                   (+ 1 (cardinality set))))
   :enable cardinality-of-insert)
 
+(defrule cardinality-of-insert-when-not-in-linear
+  (implies (not (in x set))
+           (< (cardinality set)
+              (cardinality (insert x set))))
+  :rule-classes :linear)
+
+(defrule cardinality-of-insert-lower-bound-linear
+  (<= (cardinality set)
+      (cardinality (insert x set)))
+  :rule-classes :linear
+  :enable cardinality-of-insert)
+
+(defrule cardinality-of-insert-upper-bound-linear
+  (<= (cardinality (insert x set))
+      (+ 1 (cardinality set)))
+  :rule-classes :linear
+  :enable cardinality-of-insert)
+
 ;;;;;;;;;;;;;;;;;;;;
 
 (defrule subset-of-insert
