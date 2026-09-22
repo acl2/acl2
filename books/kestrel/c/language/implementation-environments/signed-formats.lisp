cd08c105a2792c99c9d70093367b11602a5fe653
Alessandro Coglio <email@alessandrocoglio.info>
2026-09-21 22:15:19 -0700

    [C] Add well-formedness of signed formats.

--- books/kestrel/c/language/implementation-environments/signed-formats.lisp
+++ books/kestrel/c/language/implementation-environments/signed-formats.lisp
@@ -1,7 +1,7 @@
 ; C Library
 ;
-; Copyright (C) 2025 Kestrel Institute (http://www.kestrel.edu)
-; Copyright (C) 2025 Kestrel Technology LLC (http://kestreltechnology.com)
+; Copyright (C) 2026 Kestrel Institute (http://www.kestrel.edu)
+; Copyright (C) 2026 Kestrel Technology LLC (http://kestreltechnology.com)
 ;
 ; License: A 3-clause BSD license. See the LICENSE file distributed with ACL2.
 ;
@@ -11,11 +11,8 @@
 
 (in-package "C")
 
-(include-book "../../portcullis")
+(include-book "dialects")
 
-(include-book "centaur/fty/top" :dir :system)
-
-(include-book "std/basic/controlled-configuration" :dir :system)
 (acl2::controlled-configuration)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -48,3 +45,26 @@
   (:ones-complement ())
   (:twos-complement ())
   :pred signed-formatp)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define signed-format-wfp ((format signed-formatp)
+                           (std standardp))
+  :returns (yes/no booleanp)
+  :short "Check if a signed format is well-formed for a C standard."
+  :long
+  (xdoc::topstring
+   (xdoc::p
+    "C17 allows all three signed formats [C17:6.2.6.2/2],
+     while C23 only allows two's complement [C23:6.2.6.2]."))
+  (standard-case std
+                 :c17 t
+                 :c23 (signed-format-case format :twos-complement))
+
+  ///
+
+  (defrule signed-format-wfp-of-standard-c17
+    (signed-format-wfp format (standard-c17)))
+
+  (defrule signed-format-wfp-of-signed-format-twos-complement
+    (signed-format-wfp (signed-format-twos-complement) std)))
