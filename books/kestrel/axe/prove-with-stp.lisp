380c29647e831429dc04f0c80639076c4994a831
Eric Smith <ewsmith@gmail.com>
2026-09-23 02:03:41 -0700

    [axe] Add justification theorem for bitnot.

--- books/kestrel/axe/prove-with-stp.lisp
+++ books/kestrel/axe/prove-with-stp.lisp
@@ -950,6 +950,8 @@
 
 (thm (implies (and (natp high) (natp low)) (equal (slice high low (bvchop (+ 1 high) x)) (slice high low x))))
 
+(thm (equal (bitnot (bvchop 1 x)) (bitnot x)))
+
 (thm (equal (bitand x (bvchop 1 y)) (bitand x y)))
 (thm (equal (bitor x (bvchop 1 y)) (bitor x y)))
 (thm (equal (bitxor x (bvchop 1 y)) (bitxor x y)))
